e174704299e653be5065ed373c2235c32d7b00b8
MinerPL-Automation <admin@minerpl.xyz>
2023-09-20 19:01:21 +0200

    canaryRelease: 229646

---
+++ scripts/e1cc8249f795687e05b2.js
@@ -0,0 +1,9 @@
+(this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
+    [14096, 7167], {
+        507167: (e, p, r) => {
+            e.exports = function() {
+                return new Worker(r.p + "75b1f5bdd52e9b4359e0.worker.js")
+            }
+        }
+    }
+]);
