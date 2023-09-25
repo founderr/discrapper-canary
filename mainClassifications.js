af9c8e472af3a522dd2b42cd23df67670e2261b1
MinerPL-Automation <admin@minerpl.xyz>
2023-09-25 21:25:22 +0200

    canaryRelease: 231026

--- mainClassifications.js
+++ mainClassifications.js
@@ -37033,7 +37033,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-0a7702b0db864ded0617b26c6a9828cdf575dfb4",
+                    release: "discord_web-b484ed546a9dc373e710a0bc38f25d19f4f34e6f",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37060,8 +37060,8 @@
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                X.YA("buildNumber", null !== (t = "231022") ? t : "");
-                X.YA("builtAt", String("1695668708300"));
+                X.YA("buildNumber", null !== (t = "231026") ? t : "");
+                X.YA("builtAt", String("1695669087670"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) X.YA(r, e[r]);
--- scripts/1db6538fdacb2d467ab1.js
+++
@@ -1,9 +0,0 @@
-(this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
-    [14096, 7167], {
-        507167: (p, r, e) => {
-            p.exports = function() {
-                return new Worker(e.p + "9bf318b754f9550a55be.worker.js")
-            }
-        }
-    }
-]);
