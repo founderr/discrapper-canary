420f1cb28497e1a31077c023dccd1d739e5f33bb
MinerPL-Automation <admin@minerpl.xyz>
2023-10-05 01:31:34 +0200

    canaryRelease: 234480

--- mainClassifications.js
+++ mainClassifications.js
@@ -37085,7 +37085,7 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-b12602f1eb5226f217e9a4e7accbe09dda7b4dfb",
+                    release: "discord_web-8d92f0d4dbd253e2ae109e04ff4b704b3f0da88f",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37112,8 +37112,8 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                X.YA("buildNumber", null !== (t = "234473") ? t : "");
-                X.YA("builtAt", String("1696460579259"));
+                X.YA("buildNumber", null !== (t = "234480") ? t : "");
+                X.YA("builtAt", String("1696461457639"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) X.YA(r, e[r]);
