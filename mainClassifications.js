81c0c24b93ef2be88c658012b9f1ea4efd29dfee
MinerPL-Automation <admin@minerpl.xyz>
2023-10-13 22:53:20 +0200

    canaryRelease: 237143

--- mainClassifications.js
+++ mainClassifications.js
@@ -37120,7 +37120,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-a13c48cbb4eb33226b9bb64aeb38aab979d336f1",
+                    release: "discord_web-a6c72901beb0af246f76c072d9dede31362f9c2f",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37147,8 +37147,8 @@
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                X.YA("buildNumber", null !== (t = "237133") ? t : "");
-                X.YA("builtAt", String("1697229049004"));
+                X.YA("buildNumber", null !== (t = "237143") ? t : "");
+                X.YA("builtAt", String("1697229621740"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) X.YA(r, e[r]);
