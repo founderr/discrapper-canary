5b071ce373ea9a0fd27d1ce7e0bce0491004aeca
MinerPL-Automation <admin@minerpl.xyz>
2023-10-19 23:22:13 +0200

    canaryRelease: 238807

--- scripts/0cd9dd75186c032d05c9.js
+++ scripts/0cd9dd75186c032d05c9.js
@@ -5442,7 +5442,7 @@
                                 },
                                 oldFormErrors: !0
                             }).then((function(e) {
-                                if (null == e.body || "b3e9ca7fa7454249291d81be3e3a0aaab749c5b5" === e.body.hash) return n._handleUpdateNotAvailable();
+                                if (null == e.body || "bc27151c90c643163a0d9004e4e6a1324181449f" === e.body.hash) return n._handleUpdateNotAvailable();
                                 if (e.body.required || (0, Tn.fD)()) return n._handleUpdateDownloaded(!1);
                                 var t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? Dn : Mn;
                                 if (Date.now() - Ln > t) {
