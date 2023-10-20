81b9d1083526c52f4792b717f141d0c36ee0de69
MinerPL-Automation <admin@minerpl.xyz>
2023-10-20 23:31:13 +0200

    canaryRelease: 239182

--- scripts/7ef174171c0be186dcb8.js
+++ scripts/7ef174171c0be186dcb8.js
@@ -62967,7 +62967,7 @@
                         children: [(0, r.jsx)(tn, {
                             className: hoe().icon
                         }), $.Z.Messages.DEV_NOTICE_STAGING.format({
-                            buildNumber: "239165"
+                            buildNumber: "239182"
                         }), (0, r.jsx)(u, {})]
                     })
                 }
--- scripts/e2057aafd6a53c940f5b.js
+++ scripts/e2057aafd6a53c940f5b.js
@@ -5442,7 +5442,7 @@
                                 },
                                 oldFormErrors: !0
                             }).then((function(e) {
-                                if (null == e.body || "a01f9f30bcf256639808079e6684bcc3f7393012" === e.body.hash) return n._handleUpdateNotAvailable();
+                                if (null == e.body || "4523a4441bbe6c22d8394a5b08824720be8d1993" === e.body.hash) return n._handleUpdateNotAvailable();
                                 if (e.body.required || (0, Tn.fD)()) return n._handleUpdateDownloaded(!1);
                                 var t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? Dn : Mn;
                                 if (Date.now() - Ln > t) {
