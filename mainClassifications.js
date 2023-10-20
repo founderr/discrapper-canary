a3a3cc10da88a651a043d37151340dd5e35c89d7
MinerPL-Automation <admin@minerpl.xyz>
2023-10-20 03:16:16 +0200

    canaryRelease: 238959

--- mainClassifications.js
+++ mainClassifications.js
@@ -37151,7 +37151,7 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-36439ea3d5514c0312980bfcf0b3d179bb1ffbff",
+                    release: "discord_web-3a4b236ec789808e3adfe9974b73b5a7e41ef658",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37178,8 +37178,8 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                Bt.YA("buildNumber", null !== (t = "238954") ? t : "");
-                Bt.YA("builtAt", String("1697762829380"));
+                Bt.YA("buildNumber", null !== (t = "238959") ? t : "");
+                Bt.YA("builtAt", String("1697763628619"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) Bt.YA(r, e[r]);
