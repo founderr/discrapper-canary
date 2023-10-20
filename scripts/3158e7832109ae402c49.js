d5d4b6ef4ec2a18da93db84c1405224a37c092d4
MinerPL-Automation <admin@minerpl.xyz>
2023-10-20 23:34:12 +0200

    canaryRelease: 239181

--- scripts/3158e7832109ae402c49.js
+++ scripts/3158e7832109ae402c49.js
@@ -62967,7 +62967,7 @@
                         children: [(0, r.jsx)(tn, {
                             className: hoe().icon
                         }), $.Z.Messages.DEV_NOTICE_STAGING.format({
-                            buildNumber: "239182"
+                            buildNumber: "239181"
                         }), (0, r.jsx)(u, {})]
                     })
                 }
