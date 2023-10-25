d9918361668c84e56e0aa4dc5f8d307f7545a5f2
MinerPL-Automation <admin@minerpl.xyz>
2023-10-26 01:58:07 +0200

    canaryRelease: 240586

--- scripts/f830b3983b8dc6d2fa6b.js
+++ scripts/f830b3983b8dc6d2fa6b.js
@@ -9610,7 +9610,7 @@
             function d() {
                 var e, t, s, n, d, u, c, S, E;
                 let T = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    f = (e = "e09afe2ccee6d8e6da17bcc040c50507e11029ed", e.substring(0, 7)),
+                    f = (e = "abe6da4ec137b86183fcfdf6e1a4245b62ffb672", e.substring(0, 7)),
                     _ = null === (t = r.default) || void 0 === t ? void 0 : t.remoteApp.getVersion(),
                     m = null === (n = r.default) || void 0 === n ? void 0 : null === (s = (d = n.remoteApp).getBuildNumber) || void 0 === s ? void 0 : s.call(d),
                     I = null === (c = r.default) || void 0 === c ? void 0 : null === (u = (S = c.remoteApp).getAppArch) || void 0 === u ? void 0 : u.call(S),
@@ -9623,7 +9623,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [T, " ", "240585", " ", (0, a.jsxs)("span", {
+                        children: [T, " ", "240586", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", f, ")"]
                         })]
