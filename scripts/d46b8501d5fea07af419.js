d2d29c68e872627868a8ecd16017c313ce1d0844
MinerPL-Automation <admin@minerpl.xyz>
2023-10-25 23:34:06 +0200

    canaryRelease: 240481

--- scripts/d46b8501d5fea07af419.js
+++ scripts/d46b8501d5fea07af419.js
@@ -1256,7 +1256,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "f903260a28936cc595e92e685e333c2970d57d03" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "10ba17501eb8e3d1c1a05eaab96f21418b2cb257" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, i.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - c > t) return a.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
