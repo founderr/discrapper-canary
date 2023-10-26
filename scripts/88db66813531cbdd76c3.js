799e60469f2ebc6d4d7e67a995acba89179ecf11
DiscrapperManager <admin@minerpl.xyz>
2023-10-26 21:16:24 +0200

    canaryRelease: 240755

--- scripts/88db66813531cbdd76c3.js
+++ scripts/88db66813531cbdd76c3.js
@@ -9610,7 +9610,7 @@
             function d() {
                 var e, t, s, n, d, u, c, S, E;
                 let T = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    f = (e = "a7724dfa54403183a79298ddde30509137660f77", e.substring(0, 7)),
+                    f = (e = "7e12bf6778ce78198b865d5e4f8be643714a134b", e.substring(0, 7)),
                     _ = null === (t = r.default) || void 0 === t ? void 0 : t.remoteApp.getVersion(),
                     m = null === (n = r.default) || void 0 === n ? void 0 : null === (s = (d = n.remoteApp).getBuildNumber) || void 0 === s ? void 0 : s.call(d),
                     I = null === (c = r.default) || void 0 === c ? void 0 : null === (u = (S = c.remoteApp).getAppArch) || void 0 === u ? void 0 : u.call(S),
@@ -9623,7 +9623,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [T, " ", "240748", " ", (0, a.jsxs)("span", {
+                        children: [T, " ", "240755", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", f, ")"]
                         })]
--- strings.js
+++ strings.js
@@ -17775,7 +17775,7 @@
                 L = E("782340");
             (0, r.setUpdateRules)(i.default), (0, I.default)(L.default, n.default, s.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
             let u = window.GLOBAL_ENV.RELEASE_CHANNEL;
-            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("240748", ", Version Hash: ").concat("a7724dfa54403183a79298ddde30509137660f77")), t.default.setTags({
+            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("240755", ", Version Hash: ").concat("7e12bf6778ce78198b865d5e4f8be643714a134b")), t.default.setTags({
                 appContext: l.CURRENT_APP_CONTEXT
             }), S.default.initBasic(), N.default.init(), T.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
         },
@@ -20137,8 +20137,8 @@
 
             function o() {
                 var e;
-                let _ = parseInt((e = "240748", "240748"));
-                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("240748")), _ = 0), _
+                let _ = parseInt((e = "240755", "240755"));
+                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("240755")), _ = 0), _
             }
         },
         990629: function(e, _, E) {
