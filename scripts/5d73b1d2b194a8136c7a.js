e66d9e591c637fc69101aebe8004aad4007ae2b8
DiscrapperManager <admin@minerpl.xyz>
2023-10-27 22:58:01 +0200

    canaryRelease: 241294

--- scripts/5d73b1d2b194a8136c7a.js
+++ scripts/5d73b1d2b194a8136c7a.js
@@ -240,7 +240,7 @@
             "use strict";
             n.r(t), n.d(t, {
                 default: function() {
-                    return b
+                    return L
                 }
             });
             var a = n("920040"),
@@ -270,8 +270,8 @@
                 value: e
             }));
 
-            function b(e) {
-                var t, b, L, N, O, y;
+            function L(e) {
+                var t, L, N, b, O, y;
                 let {
                     transitionState: j,
                     onClose: B
@@ -450,10 +450,10 @@
                                             className: x.formInput,
                                             children: (0, a.jsx)(s.SearchableSelect, {
                                                 value: z,
-                                                options: null !== (y = null == (t = W) ? void 0 : null === (N = t.features) || void 0 === N ? void 0 : null === (L = N.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === L ? void 0 : null === (b = L.map(e => ({
+                                                options: null !== (y = null == (t = W) ? void 0 : null === (b = t.features) || void 0 === b ? void 0 : null === (N = b.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === N ? void 0 : null === (L = N.map(e => ({
                                                     label: null !== (O = e.name) && void 0 !== O ? O : "",
                                                     value: (0, E.getFeatureId)(e)
-                                                }))) || void 0 === b ? void 0 : b.sort((e, t) => e.label.localeCompare(t.label))) && void 0 !== y ? y : [],
+                                                }))) || void 0 === L ? void 0 : L.sort((e, t) => e.label.localeCompare(t.label))) && void 0 !== y ? y : [],
                                                 isDisabled: null == W,
                                                 onChange: e => J(e)
                                             })
@@ -773,8 +773,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "241278",
-                    versionHash: "b541b5b6ffd4fba26831c1c6480b36fb3d865731"
+                    buildNumber: "241294",
+                    versionHash: "d3f2e8dd9cf9a5e0cc6827975d97da24c03f848f"
                 }
             }
             n.r(t), n.d(t, {
--- scripts/e6675e0f659b28976b07.js
+++ scripts/e6675e0f659b28976b07.js
@@ -1256,7 +1256,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "b541b5b6ffd4fba26831c1c6480b36fb3d865731" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "d3f2e8dd9cf9a5e0cc6827975d97da24c03f848f" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, i.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - c > t) return a.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
