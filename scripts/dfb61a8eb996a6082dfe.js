0702c09d294a7beb38b26984cfaab8c19573526b
DiscrapperManager <admin@minerpl.xyz>
2023-10-27 05:22:02 +0200

    canaryRelease: 241000

--- scripts/dfb61a8eb996a6082dfe.js
+++ scripts/dfb61a8eb996a6082dfe.js
@@ -271,7 +271,7 @@
             }));
 
             function L(e) {
-                var t, L, b, N, O, y;
+                var t, L, N, b, O, y;
                 let {
                     transitionState: j,
                     onClose: B
@@ -450,7 +450,7 @@
                                             className: x.formInput,
                                             children: (0, a.jsx)(s.SearchableSelect, {
                                                 value: z,
-                                                options: null !== (y = null == (t = W) ? void 0 : null === (N = t.features) || void 0 === N ? void 0 : null === (b = N.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === b ? void 0 : null === (L = b.map(e => ({
+                                                options: null !== (y = null == (t = W) ? void 0 : null === (b = t.features) || void 0 === b ? void 0 : null === (N = b.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === N ? void 0 : null === (L = N.map(e => ({
                                                     label: null !== (O = e.name) && void 0 !== O ? O : "",
                                                     value: (0, E.getFeatureId)(e)
                                                 }))) || void 0 === L ? void 0 : L.sort((e, t) => e.label.localeCompare(t.label))) && void 0 !== y ? y : [],
@@ -773,8 +773,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "240986",
-                    versionHash: "ae76a59a18bbedb5705901898924625df7c35031"
+                    buildNumber: "241000",
+                    versionHash: "a8974912a4a3922967292e90230701d3537101f3"
                 }
             }
             n.r(t), n.d(t, {
--- scripts/ed03a33334ed33fb4696.js
+++ scripts/ed03a33334ed33fb4696.js
@@ -1256,7 +1256,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "ae76a59a18bbedb5705901898924625df7c35031" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "a8974912a4a3922967292e90230701d3537101f3" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, i.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - c > t) return a.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
