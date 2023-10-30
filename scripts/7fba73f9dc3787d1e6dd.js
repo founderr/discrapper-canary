baade00971d0111c0929ab38c798c5bd930b6a83
DiscrapperManager <admin@minerpl.xyz>
2023-10-30 23:16:10 +0100

    canaryRelease: 241827

--- scripts/7fba73f9dc3787d1e6dd.js
+++ scripts/7fba73f9dc3787d1e6dd.js
@@ -9748,7 +9748,7 @@
             function d() {
                 var e, t, s, n, d, u, c, S, E;
                 let T = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    f = (e = "92f6854515507db3db29e9ce043560b308063fc7", e.substring(0, 7)),
+                    f = (e = "5aa45e1ba970da04789ad1a4d502424634883ec9", e.substring(0, 7)),
                     m = null === (t = r.default) || void 0 === t ? void 0 : t.remoteApp.getVersion(),
                     _ = null === (n = r.default) || void 0 === n ? void 0 : null === (s = (d = n.remoteApp).getBuildNumber) || void 0 === s ? void 0 : s.call(d),
                     I = null === (c = r.default) || void 0 === c ? void 0 : null === (u = (S = c.remoteApp).getAppArch) || void 0 === u ? void 0 : u.call(S),
@@ -9761,7 +9761,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [T, " ", "241822", " ", (0, a.jsxs)("span", {
+                        children: [T, " ", "241827", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", f, ")"]
                         })]
