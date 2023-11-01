977a3a258aab6b66b6618c368966fbaddfe4b8f2
DiscrapperManager <admin@minerpl.xyz>
2023-11-01 20:45:37 +0100

    canaryRelease: 242611

--- main/661e92563d607649f5de.js
+++ main/661e92563d607649f5de.js
@@ -33644,7 +33644,7 @@
                         var i;
                         let c = {
                                 environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                                build_number: "242609"
+                                build_number: "242611"
                             },
                             E = l.default.getCurrentUser();
                         null != E && (c.user_id = E.id, c.user_name = E.tag, null != E.email && (c.email = E.email));
