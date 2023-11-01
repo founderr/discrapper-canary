6c29ceec6445c864b913de7f72e272c7d4f0aec3
DiscrapperManager <admin@minerpl.xyz>
2023-11-01 23:40:38 +0100

    canaryRelease: 242776

--- strings.js
+++ strings.js
@@ -18151,7 +18151,7 @@
                 L = E("782340");
             (0, i.setUpdateRules)(s.default), (0, r.default)(L.default, n.default, T.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
             let u = window.GLOBAL_ENV.RELEASE_CHANNEL;
-            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("242770", ", Version Hash: ").concat("4cfbd3db0e62da5e2aa5dc48c97b7109f5a5c51c")), t.default.setTags({
+            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("242776", ", Version Hash: ").concat("573fb25ff0d056582faae4eaf023178dce865f72")), t.default.setTags({
                 appContext: l.CURRENT_APP_CONTEXT
             }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
         },
@@ -20533,8 +20533,8 @@
 
             function o() {
                 var e;
-                let _ = parseInt((e = "242770", "242770"));
-                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("242770")), _ = 0), _
+                let _ = parseInt((e = "242776", "242776"));
+                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("242776")), _ = 0), _
             }
         },
         990629: function(e, _, E) {
