7c43ea381cdeac9c834d3ab0c466f07821328420
DiscrapperManager <admin@minerpl.xyz>
2023-11-03 19:43:15 +0100

    canaryRelease: 243540

--- scripts/ceb72ffb70b3d646ceb4.js
+++ scripts/ceb72ffb70b3d646ceb4.js
@@ -240,7 +240,7 @@
             "use strict";
             n.r(t), n.d(t, {
                 default: function() {
-                    return b
+                    return L
                 }
             });
             var a = n("37983"),
@@ -270,8 +270,8 @@
                 value: e
             }));
 
-            function b(e) {
-                var t, b, L, N, O, y;
+            function L(e) {
+                var t, L, b, N, O, y;
                 let {
                     transitionState: j,
                     onClose: B
@@ -450,10 +450,10 @@
                                             className: x.formInput,
                                             children: (0, a.jsx)(s.SearchableSelect, {
                                                 value: z,
-                                                options: null !== (y = null == (t = W) ? void 0 : null === (N = t.features) || void 0 === N ? void 0 : null === (L = N.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === L ? void 0 : null === (b = L.map(e => ({
+                                                options: null !== (y = null == (t = W) ? void 0 : null === (N = t.features) || void 0 === N ? void 0 : null === (b = N.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === b ? void 0 : null === (L = b.map(e => ({
                                                     label: null !== (O = e.name) && void 0 !== O ? O : "",
                                                     value: (0, E.getFeatureId)(e)
-                                                }))) || void 0 === b ? void 0 : b.sort((e, t) => e.label.localeCompare(t.label))) && void 0 !== y ? y : [],
+                                                }))) || void 0 === L ? void 0 : L.sort((e, t) => e.label.localeCompare(t.label))) && void 0 !== y ? y : [],
                                                 isDisabled: null == W,
                                                 onChange: e => J(e)
                                             })
@@ -773,8 +773,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "243535",
-                    versionHash: "2b97800ba6285e827ea7124b828b7ee82499ecc6"
+                    buildNumber: "243540",
+                    versionHash: "9235e2dc874b1ed355e83a78a5b876470da192bf"
                 }
             }
             n.r(t), n.d(t, {
