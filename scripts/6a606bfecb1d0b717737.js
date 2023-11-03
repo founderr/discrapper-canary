44b44bc47b71be1c67446497cb18ac57604405ce
DiscrapperManager <admin@minerpl.xyz>
2023-11-04 00:59:02 +0100

    canaryRelease: 243735

--- scripts/6a606bfecb1d0b717737.js
+++ scripts/6a606bfecb1d0b717737.js
@@ -271,7 +271,7 @@
             }));
 
             function L(e) {
-                var t, L, b, N, O, y;
+                var t, L, N, b, O, y;
                 let {
                     transitionState: j,
                     onClose: B
@@ -450,7 +450,7 @@
                                             className: x.formInput,
                                             children: (0, a.jsx)(s.SearchableSelect, {
                                                 value: z,
-                                                options: null !== (y = null == (t = W) ? void 0 : null === (N = t.features) || void 0 === N ? void 0 : null === (b = N.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === b ? void 0 : null === (L = b.map(e => ({
+                                                options: null !== (y = null == (t = W) ? void 0 : null === (b = t.features) || void 0 === b ? void 0 : null === (N = b.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === N ? void 0 : null === (L = N.map(e => ({
                                                     label: null !== (O = e.name) && void 0 !== O ? O : "",
                                                     value: (0, E.getFeatureId)(e)
                                                 }))) || void 0 === L ? void 0 : L.sort((e, t) => e.label.localeCompare(t.label))) && void 0 !== y ? y : [],
@@ -773,8 +773,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "243721",
-                    versionHash: "747499e8646af798b63a6df0740a0132b107b3fa"
+                    buildNumber: "243735",
+                    versionHash: "f18c182c36151489105ea3d759b98f73e4903ea9"
                 }
             }
             n.r(t), n.d(t, {
--- scripts/9a1bca5cc5e3587a7fb9.js
+++ scripts/9a1bca5cc5e3587a7fb9.js
@@ -1818,7 +1818,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "243721"
+                        buildNumber: "243735"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
