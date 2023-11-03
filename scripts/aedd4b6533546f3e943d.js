ac99641d1bf55718e29ba79cb8ffb87c1f5c4b60
DiscrapperManager <admin@minerpl.xyz>
2023-11-03 18:35:04 +0100

    canaryRelease: 243495

--- scripts/aedd4b6533546f3e943d.js
+++ scripts/aedd4b6533546f3e943d.js
@@ -271,7 +271,7 @@
             }));
 
             function L(e) {
-                var t, L, b, N, O, y;
+                var t, L, N, b, O, y;
                 let {
                     transitionState: j,
                     onClose: B
@@ -450,7 +450,7 @@
                                             className: x.formInput,
                                             children: (0, a.jsx)(s.SearchableSelect, {
                                                 value: z,
-                                                options: null !== (y = null == (t = W) ? void 0 : null === (N = t.features) || void 0 === N ? void 0 : null === (b = N.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === b ? void 0 : null === (L = b.map(e => ({
+                                                options: null !== (y = null == (t = W) ? void 0 : null === (b = t.features) || void 0 === b ? void 0 : null === (N = b.filter(e => "" !== (0, E.getFeatureId)(e))) || void 0 === N ? void 0 : null === (L = N.map(e => ({
                                                     label: null !== (O = e.name) && void 0 !== O ? O : "",
                                                     value: (0, E.getFeatureId)(e)
                                                 }))) || void 0 === L ? void 0 : L.sort((e, t) => e.label.localeCompare(t.label))) && void 0 !== y ? y : [],
@@ -773,8 +773,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "243480",
-                    versionHash: "dc1cea920b83765cbad1540d5c5913801ee212b8"
+                    buildNumber: "243495",
+                    versionHash: "cbd08e36dea1c19d23656b132f477d6dd1d77e3c"
                 }
             }
             n.r(t), n.d(t, {
