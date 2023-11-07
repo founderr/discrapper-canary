7808bd2cfabcbddde5e7e634f7388bb958f38367
DiscrapperManager <admin@minerpl.xyz>
2023-11-07 20:01:08 +0100

    canaryRelease: 244230

--- scripts/f4ced421b73537cf42b4.js
+++ scripts/f4ced421b73537cf42b4.js
@@ -1818,7 +1818,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "244215"
+                        buildNumber: "244230"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
