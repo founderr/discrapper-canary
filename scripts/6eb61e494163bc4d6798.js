47a7b7a3645313e98b518e4f71de34963bfad1c4
DiscrapperManager <admin@minerpl.xyz>
2023-11-08 00:16:25 +0100

    canaryRelease: 244336

--- scripts/6eb61e494163bc4d6798.js
+++ scripts/6eb61e494163bc4d6798.js
@@ -1257,7 +1257,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "bdca6e808d1a4f30cc4fa3d965aa59f553e537ad" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "feb2073c51db5db5e6a583fd839e47531fcd3ebe" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, i.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - c > t) return a.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
--- strings.js
+++ strings.js
@@ -18141,7 +18141,7 @@
                 u = E("782340");
             (0, i.setUpdateRules)(s.default), (0, r.default)(u.default, n.default, T.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
             let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("244332", ", Version Hash: ").concat("bdca6e808d1a4f30cc4fa3d965aa59f553e537ad")), t.default.setTags({
+            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("244336", ", Version Hash: ").concat("feb2073c51db5db5e6a583fd839e47531fcd3ebe")), t.default.setTags({
                 appContext: l.CURRENT_APP_CONTEXT
             }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
         },
@@ -20530,8 +20530,8 @@
 
             function o() {
                 var e;
-                let _ = parseInt((e = "244332", "244332"));
-                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("244332")), _ = 0), _
+                let _ = parseInt((e = "244336", "244336"));
+                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("244336")), _ = 0), _
             }
         },
         990629: function(e, _, E) {
