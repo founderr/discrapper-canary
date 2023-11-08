6ea1e6ecdaa61109798e6db283e1ac9b09b9d57c
DiscrapperManager <admin@minerpl.xyz>
2023-11-08 19:49:10 +0100

    canaryRelease: 244478

--- scripts/f1972ab6da5abebfd25f.js
+++ scripts/f1972ab6da5abebfd25f.js
@@ -1257,7 +1257,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "83c4436926179bb4490d3778ea520d2786c90c20" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "4c726c0965391ebbf132840f18e5709bae5973c0" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, i.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - c > t) return a.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
