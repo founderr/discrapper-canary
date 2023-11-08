bb9a377bd916d833e0bcf5eb90579419eb6cab01
DiscrapperManager <admin@minerpl.xyz>
2023-11-08 18:28:11 +0100

    canaryRelease: 244440

--- scripts/0e66d30f826c39d570dd.js
+++ scripts/0e66d30f826c39d570dd.js
@@ -1257,7 +1257,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "cb5b513fcc6ce11929bb7795113b46d341aee089" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "dfe8e8f334ee383f82b4fe058e8abbf49af53567" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, i.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - c > t) return a.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
--- scripts/64d493a14f0ae43fa405.js
+++ scripts/64d493a14f0ae43fa405.js
@@ -9864,7 +9864,7 @@
             function d() {
                 var e, t, s, n, d, u, c, S, E;
                 let T = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    f = (e = "cb5b513fcc6ce11929bb7795113b46d341aee089", e.substring(0, 7)),
+                    f = (e = "dfe8e8f334ee383f82b4fe058e8abbf49af53567", e.substring(0, 7)),
                     m = null === (t = r.default) || void 0 === t ? void 0 : t.remoteApp.getVersion(),
                     _ = null === (n = r.default) || void 0 === n ? void 0 : null === (s = (d = n.remoteApp).getBuildNumber) || void 0 === s ? void 0 : s.call(d),
                     I = null === (c = r.default) || void 0 === c ? void 0 : null === (u = (S = c.remoteApp).getAppArch) || void 0 === u ? void 0 : u.call(S),
@@ -9877,7 +9877,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [T, " ", "244428", " ", (0, a.jsxs)("span", {
+                        children: [T, " ", "244440", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", f, ")"]
                         })]
