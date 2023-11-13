9f7a3c58ca7f5457da4b58e1563f84fb3df610f3
DiscrapperManager <admin@minerpl.xyz>
2023-11-13 21:40:08 +0100

    canaryRelease: 245191

--- main/48dedfead6276892285d.js
+++ main/48dedfead6276892285d.js
@@ -1105,7 +1105,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "37010fc6568b885829bd9da82b8b627a51dd58d6"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "c389f28239cd25a3ede892f13092648bcaf4793c"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
