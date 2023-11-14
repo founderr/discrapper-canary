0f6dd4a9efe65105548b7a59b45f8a9415eb0efb
DiscrapperManager <admin@minerpl.xyz>
2023-11-14 23:01:10 +0100

    canaryRelease: 245610

--- main/7105e33fc4ebc5497b04.js
+++ main/7105e33fc4ebc5497b04.js
@@ -1105,7 +1105,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "5c1ea98d44d6088dc371f3bf9a08ab15e6270996"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "cf9508ba73081bc96e9e846b0e056105b582b344"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
--- scripts/460cf0170749b40cab41.js
+++ scripts/460cf0170749b40cab41.js
@@ -1257,7 +1257,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "5c1ea98d44d6088dc371f3bf9a08ab15e6270996" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "cf9508ba73081bc96e9e846b0e056105b582b344" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, i.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - f > t) return s.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
