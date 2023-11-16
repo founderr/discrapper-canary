8b84c8be4261490d8e79dffa1d1d837c96daebb6
DiscrapperManager <admin@minerpl.xyz>
2023-11-16 18:07:29 +0100

    canaryRelease: 246272

--- scripts/2e901c620fcf9c358f9c.js
+++ scripts/2e901c620fcf9c358f9c.js
@@ -9857,7 +9857,7 @@
             function d() {
                 var e, t, s, n, d, u;
                 let c = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    S = (e = "819eba62cbff0c2600128d0a0a7d01bad77acd2e", e.substring(0, 7)),
+                    S = (e = "bde207d29e2e6fb1b2c93a5aadb9e03724de97da", e.substring(0, 7)),
                     E = null === r.default || void 0 === r.default ? void 0 : r.default.remoteApp.getVersion(),
                     T = null === r.default || void 0 === r.default ? void 0 : null === (t = (s = r.default.remoteApp).getBuildNumber) || void 0 === t ? void 0 : t.call(s),
                     f = null === r.default || void 0 === r.default ? void 0 : null === (n = (d = r.default.remoteApp).getAppArch) || void 0 === n ? void 0 : n.call(d),
@@ -9870,7 +9870,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [c, " ", "246271", " ", (0, a.jsxs)("span", {
+                        children: [c, " ", "246272", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", S, ")"]
                         })]
--- scripts/b20611648f53424a4dcf.js
+++ scripts/b20611648f53424a4dcf.js
@@ -1257,7 +1257,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "819eba62cbff0c2600128d0a0a7d01bad77acd2e" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "bde207d29e2e6fb1b2c93a5aadb9e03724de97da" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, r.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - f > t) return s.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
