4acfe87f273a1e097597aa427a0a076ea61158a6
DiscrapperManager <admin@minerpl.xyz>
2023-11-20 19:46:34 +0100

    canaryRelease: 247351

--- main.js
+++ main.js
@@ -247,8 +247,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "247347",
-                    versionHash: "fb57fd2bd874a877bc41c906151c9ca9786a78c3"
+                    buildNumber: "247351",
+                    versionHash: "3f12086417d3bf9d066cfe711dd0bcd824851fb2"
                 }
             }
             n.r(e), n.d(e, {
--- scripts/fbf106b667ac96550c60.js
+++ scripts/fbf106b667ac96550c60.js
@@ -1257,7 +1257,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "fb57fd2bd874a877bc41c906151c9ca9786a78c3" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "3f12086417d3bf9d066cfe711dd0bcd824851fb2" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, r.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - f > t) return s.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
