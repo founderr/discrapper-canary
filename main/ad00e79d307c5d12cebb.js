b8fef90a6dbc48174625eaacd852e3864927fd53
DiscrapperManager <admin@minerpl.xyz>
2023-11-27 23:22:14 +0100

    canaryRelease: 248812

--- main/ad00e79d307c5d12cebb.js
+++ main/ad00e79d307c5d12cebb.js
@@ -1130,7 +1130,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "27616f7edd7d62d752adbcbb2887f36334666492"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "58c80785b260657ad23f9b6955ddcb15a0fdbfc4"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
--- scripts/b039987fbb697364c3a4.js
+++ scripts/b039987fbb697364c3a4.js
@@ -1738,7 +1738,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "248805"
+                        buildNumber: "248812"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
