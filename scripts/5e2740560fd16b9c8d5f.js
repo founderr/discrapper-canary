bac4ca41510e518c0cf7385a3a97a6adc06e861d
DiscrapperManager <admin@minerpl.xyz>
2023-11-28 20:01:18 +0100

    canaryRelease: 249211

--- scripts/5e2740560fd16b9c8d5f.js
+++ scripts/5e2740560fd16b9c8d5f.js
@@ -1219,7 +1219,7 @@
                             },
                             oldFormErrors: !0
                         }).then(e => {
-                            if (null == e.body || "1d598d38642cb210a745aa74fc213a8b722b7455" === e.body.hash) return this._handleUpdateNotAvailable();
+                            if (null == e.body || "96204763569f7ede3f4cd5e8db62d353a7e45edd" === e.body.hash) return this._handleUpdateNotAvailable();
                             if (e.body.required || (0, r.probablyHasBuildOverride)()) return this._handleUpdateDownloaded(!1);
                             let t = "stable" === window.GLOBAL_ENV.RELEASE_CHANNEL ? 6048e5 : 864e5;
                             if (Date.now() - f > t) return s.default.set("lastNonRequiredUpdateShown", Date.now()), this._handleUpdateDownloaded(!1)
--- scripts/63e6b4efc2a123e1c0c4.js
+++ scripts/63e6b4efc2a123e1c0c4.js
@@ -1750,7 +1750,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "249200"
+                        buildNumber: "249211"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
