54f986bed651b8f7e7dc287a15cf9d00a7e15be1
DiscrapperManager <admin@minerpl.xyz>
2023-11-28 03:52:14 +0100

    canaryRelease: 249039

--- scripts/2a9e1c44f0e8712cbfbd.js
+++ scripts/2a9e1c44f0e8712cbfbd.js
@@ -9895,7 +9895,7 @@
             function d() {
                 var e, t, s, n, d, u;
                 let c = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    S = (e = "cbb752de0dd93662b1ee660f395abc61b11a0cc6", e.substring(0, 7)),
+                    S = (e = "d73f0528f2b7d76fbb0685e7d0bd5a7c5a7a63c6", e.substring(0, 7)),
                     E = null === r.default || void 0 === r.default ? void 0 : r.default.remoteApp.getVersion(),
                     T = null === r.default || void 0 === r.default ? void 0 : null === (t = (s = r.default.remoteApp).getBuildNumber) || void 0 === t ? void 0 : t.call(s),
                     f = null === r.default || void 0 === r.default ? void 0 : null === (n = (d = r.default.remoteApp).getAppArch) || void 0 === n ? void 0 : n.call(d),
@@ -9908,7 +9908,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [c, " ", "249033", " ", (0, a.jsxs)("span", {
+                        children: [c, " ", "249039", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", S, ")"]
                         })]
--- scripts/42b8db2b9887fbb78e17.js
+++ scripts/42b8db2b9887fbb78e17.js
@@ -1738,7 +1738,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "249033"
+                        buildNumber: "249039"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
