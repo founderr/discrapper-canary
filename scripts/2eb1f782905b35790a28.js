220a173feb5f2bf12a843ee4e6fe55cb76c10b16
DiscrapperManager <admin@minerpl.xyz>
2023-11-28 01:40:14 +0100

    canaryRelease: 248955

--- scripts/2eb1f782905b35790a28.js
+++ scripts/2eb1f782905b35790a28.js
@@ -1738,7 +1738,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "248951"
+                        buildNumber: "248955"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
