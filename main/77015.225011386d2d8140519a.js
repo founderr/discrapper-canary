4f5cd619f0621faf4ef63a35c9eb18cdad3d2309
DiscrapperManager <admin@minerpl.xyz>
2023-12-06 02:34:12 +0100

    canaryRelease: 251667

--- main/77015.225011386d2d8140519a.js
+++ main/77015.225011386d2d8140519a.js
@@ -1202,7 +1202,7 @@
                     } = e, n = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...n));
                     let l = new URLSearchParams;
-                    l.append("build_id", "17c2db7ff5b2fd7131edb20bc5a514364016afb1"), l.append("rpc", String(t)), l.append("rpc_auth_token", k), a = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(l.toString())
+                    l.append("build_id", "4638ec617ac529e623c25608c5a2c20de167e12b"), l.append("rpc", String(t)), l.append("rpc_auth_token", k), a = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(l.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
--- scripts/4f3485d76b20f1a5e1d6.js
+++ scripts/4f3485d76b20f1a5e1d6.js
@@ -9957,7 +9957,7 @@
             function d() {
                 var e, t, s, n, d, u;
                 let c = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    S = (e = "17c2db7ff5b2fd7131edb20bc5a514364016afb1", e.substring(0, 7)),
+                    S = (e = "4638ec617ac529e623c25608c5a2c20de167e12b", e.substring(0, 7)),
                     E = null === r.default || void 0 === r.default ? void 0 : r.default.remoteApp.getVersion(),
                     T = null === r.default || void 0 === r.default ? void 0 : null === (t = (s = r.default.remoteApp).getBuildNumber) || void 0 === t ? void 0 : t.call(s),
                     f = null === r.default || void 0 === r.default ? void 0 : null === (n = (d = r.default.remoteApp).getAppArch) || void 0 === n ? void 0 : n.call(d),
@@ -9970,7 +9970,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [c, " ", "251664", " ", (0, a.jsxs)("span", {
+                        children: [c, " ", "251667", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", S, ")"]
                         })]
