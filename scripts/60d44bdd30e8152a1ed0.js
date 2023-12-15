510fb1e87b4da13c78155cca6b43d267378eb1b1
DiscrapperManager <admin@minerpl.xyz>
2023-12-15 20:46:14 +0100

    canaryRelease: 254840

--- scripts/60d44bdd30e8152a1ed0.js
+++ scripts/60d44bdd30e8152a1ed0.js
@@ -1675,7 +1675,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "254822"
+                        buildNumber: "254840"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
--- scripts/6f42f2ba3f56ca078a4b.js
+++ scripts/6f42f2ba3f56ca078a4b.js
@@ -10721,7 +10721,7 @@
             function d() {
                 var e, t, s, n, d, u;
                 let c = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    S = (e = "fa31cbefcd86d555589c4a2866192c03628fa920", e.substring(0, 7)),
+                    S = (e = "fe3bdca4d3b484bca0f398b6b298773b963cc0d0", e.substring(0, 7)),
                     E = null === r.default || void 0 === r.default ? void 0 : r.default.remoteApp.getVersion(),
                     f = null === r.default || void 0 === r.default ? void 0 : null === (t = (s = r.default.remoteApp).getBuildNumber) || void 0 === t ? void 0 : t.call(s),
                     T = null === r.default || void 0 === r.default ? void 0 : null === (n = (d = r.default.remoteApp).getAppArch) || void 0 === n ? void 0 : n.call(d),
@@ -10734,7 +10734,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [c, " ", "254822", " ", (0, a.jsxs)("span", {
+                        children: [c, " ", "254840", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", S, ")"]
                         })]
