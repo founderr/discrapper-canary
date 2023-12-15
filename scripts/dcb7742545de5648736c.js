51eb433c6ffb8ac03aea5f010eef3db3d0a533aa
DiscrapperManager <admin@minerpl.xyz>
2023-12-15 21:16:13 +0100

    canaryRelease: 254851

--- scripts/dcb7742545de5648736c.js
+++ scripts/dcb7742545de5648736c.js
@@ -1675,7 +1675,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: C.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "254842"
+                        buildNumber: "254851"
                     }), (0, a.jsx)(T, {})]
                 }) : null
             }
