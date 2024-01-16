5ddd2c2a0dba335ecac8c3826d54258bedf39ebb
DiscrapperManager <admin@minerpl.xyz>
2024-01-16 20:37:15 +0100

    canaryRelease: 259230

--- scripts/96d64ac41ccceaf7673a.js
+++ scripts/96d64ac41ccceaf7673a.js
@@ -1659,7 +1659,7 @@
                     children: [(0, a.jsx)(f.default, {
                         className: h.icon
                     }), _.default.Messages.DEV_NOTICE_STAGING.format({
-                        buildNumber: "259226"
+                        buildNumber: "259230"
                     }), (0, a.jsx)(I, {})]
                 }) : null
             }
