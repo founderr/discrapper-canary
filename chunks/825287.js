7fb5967cdb696a3af265a2f27d3cb1d1703f9e1f
DiscrapperManager <admin@minerpl.xyz>
2024-01-24 00:05:57 +0100

    canaryRelease: 260799

--- chunks/825287.js
+++ chunks/825287.js
@@ -4,8 +4,8 @@ function o() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "260784",
-    versionHash: "82cec0bae586650ccd0c7a40a20f2d5652086aca"
+    buildNumber: "260799",
+    versionHash: "b5402d5e47685944344ff85bc75e22af8e656c8c"
   }
 }
 n.r(e), n.d(e, {
--- chunks/83995.js
+++ chunks/83995.js
@@ -1,29 +1,39 @@
 "use strict";
 n.r(t), n.d(t, {
   PollsExperiment: function() {
-    return l
+    return i
   },
   PollsUserExperiment: function() {
-    return s
+    return r
   }
 });
-var a = n("862205");
-let l = (0, a.createExperiment)({
+var a, l, s = n("862205");
+(a = l || (l = {}))[a.MEDIA_VIEWER = 0] = "MEDIA_VIEWER", a[a.OMNI_BUTTON = 1] = "OMNI_BUTTON";
+let i = (0, s.createExperiment)({
     kind: "guild",
     id: "2023-09_guild_polls",
     label: "Polls",
     defaultConfig: {
-      enabled: !1
+      enabled: !1,
+      mobileEntryPoint: 1
     },
     treatments: [{
       id: 1,
-      label: "Enables creation of polls within a guild",
+      label: "Enables creation of polls from the media picker",
       config: {
-        enabled: !0
+        enabled: !0,
+        mobileEntryPoint: 0
+      }
+    }, {
+      id: 2,
+      label: "Enables creation of polls from the omni button",
+      config: {
+        enabled: !0,
+        mobileEntryPoint: 1
       }
     }]
   }),
-  s = (0, a.createExperiment)({
+  r = (0, s.createExperiment)({
     kind: "user",
     id: "2023-10_poll_users",
     label: "Polls User Experiment",
