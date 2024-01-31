bde1686a3a1f14f0bb38d278a12ca97ff8ae7aa7
DiscrapperManager <admin@minerpl.xyz>
2024-01-31 20:25:35 +0100

    canaryRelease: 262826

--- chunks/131835.js
+++ chunks/131835.js
@@ -1,105 +1,120 @@
 "use strict";
 n.r(t), n.d(t, {
   setOnboardingStep: function() {
-    return D
-  },
-  dismissEducationUpsell: function() {
     return g
   },
-  toggleAdvancedSettings: function() {
-    return A
-  },
-  goToNextOnboardingStep: function() {
+  dismissEducationUpsell: function() {
     return G
   },
-  goToPrevOnboardingStep: function() {
+  toggleAdvancedSettings: function() {
     return h
   },
-  startPreview: function() {
+  goToNextOnboardingStep: function() {
     return L
+  },
+  goToPrevOnboardingStep: function() {
+    return R
+  },
+  startPreview: function() {
+    return p
   }
 }), n("222007");
 var a = n("913144"),
   l = n("404118"),
-  s = n("863636"),
-  i = n("471706"),
-  r = n("380710"),
-  d = n("937692"),
-  o = n("233706"),
-  u = n("479756"),
-  c = n("507950"),
-  N = n("42203"),
-  _ = n("305961"),
-  I = n("592407"),
-  E = n("653274"),
-  f = n("330724"),
-  O = n("259483"),
-  S = n("724210"),
-  m = n("653138"),
-  T = n("782340");
+  s = n("599417"),
+  i = n("863636"),
+  r = n("471706"),
+  d = n("380710"),
+  o = n("937692"),
+  u = n("233706"),
+  c = n("479756"),
+  N = n("507950"),
+  _ = n("42203"),
+  I = n("305961"),
+  E = n("449008"),
+  f = n("592407"),
+  O = n("653274"),
+  S = n("330724"),
+  m = n("259483"),
+  T = n("724210"),
+  D = n("653138"),
+  A = n("782340");
 
-function D(e) {
+function g(e) {
   a.default.dispatch({
     type: "GUILD_SETTINGS_ONBOARDING_STEP",
     step: e
   })
 }
 
-function g(e) {
+function G(e) {
   null != e && a.default.dispatch({
     type: "GUILD_SETTINGS_ONBOARDING_EDUCATION_UPSELL_DISMISSED",
     upsellType: e
   })
 }
-async function A(e, t) {
-  let n = s.default.getEnabled(e),
-    i = Array.from(E.default.editedDefaultChannelIds).filter(e => null != N.default.getChannel(e)),
-    [d, o] = (0, r.getChattableDefaultChannels)(e, [...i]);
-  if (n && t === m.GuildOnboardingMode.ONBOARDING_DEFAULT && (o.length < m.MIN_NUMBER_OF_DEFAULT_CHANNELS_FOR_ONBOARDING || d.length < m.NUM_DEFAULT_CHATTABLE_CHANNELS_MIN)) {
+async function h(e, t) {
+  let n = i.default.getEnabled(e),
+    r = Array.from(O.default.editedDefaultChannelIds).filter(e => null != _.default.getChannel(e)),
+    [o, u] = (0, d.getChattableDefaultChannels)(e, [...r]);
+  if (n && t === D.GuildOnboardingMode.ONBOARDING_DEFAULT && (u.length < D.MIN_NUMBER_OF_DEFAULT_CHANNELS_FOR_ONBOARDING || o.length < D.NUM_DEFAULT_CHATTABLE_CHANNELS_MIN)) {
     l.default.show({
-      title: T.default.Messages.ONBOARDING_PROMPT_SAVE_FAILED,
-      body: T.default.Messages.DEFAULT_CHANNELS_SAVE_INVALID_DEFAULT_CHANNELS
+      title: A.default.Messages.ONBOARDING_PROMPT_SAVE_FAILED,
+      body: A.default.Messages.DEFAULT_CHANNELS_SAVE_INVALID_DEFAULT_CHANNELS
     });
     return
   }
-  await (0, f.editOnboarding)(e, {
-    mode: t
-  }), a.default.dispatch({
+  try {
+    await (0, S.editOnboarding)(e, {
+      mode: t
+    })
+  } catch (n) {
+    var c;
+    let {
+      fieldName: e,
+      error: t
+    } = null !== (c = new(0, s.default)(n).getAnyErrorMessageAndField()) && void 0 !== c ? c : {};
+    l.default.show({
+      title: A.default.Messages.ONBOARDING_PROMPT_SAVE_FAILED,
+      body: [e, t].filter(E.isNotNullish).join(": ")
+    })
+  }
+  a.default.dispatch({
     type: "GUILD_SETTINGS_ONBOARDING_SET_MODE",
     guildId: e,
     mode: t
   })
 }
 
-function G(e, t) {
+function L(e, t) {
   let {
     homeSettingsEnabled: n
-  } = (0, o.getOnboardingHomeAdminExperiment)(e), a = O.ONBOARDING_STEPS.findIndex(e => e === t);
-  if (-1 !== a && a !== O.ONBOARDING_STEPS.length - 1) {
-    if (!n && O.ONBOARDING_STEPS[a + 1] === O.GuildSettingsOnboardingPage.HOME_SETTINGS) {
-      D(O.ONBOARDING_STEPS[a + 2]);
+  } = (0, u.getOnboardingHomeAdminExperiment)(e), a = m.ONBOARDING_STEPS.findIndex(e => e === t);
+  if (-1 !== a && a !== m.ONBOARDING_STEPS.length - 1) {
+    if (!n && m.ONBOARDING_STEPS[a + 1] === m.GuildSettingsOnboardingPage.HOME_SETTINGS) {
+      g(m.ONBOARDING_STEPS[a + 2]);
       return
     }
-    D(O.ONBOARDING_STEPS[a + 1])
+    g(m.ONBOARDING_STEPS[a + 1])
   }
 }
 
-function h(e, t) {
+function R(e, t) {
   let {
     homeSettingsEnabled: n
-  } = (0, o.getOnboardingHomeAdminExperiment)(e), a = O.ONBOARDING_STEPS.findIndex(e => e === t);
+  } = (0, u.getOnboardingHomeAdminExperiment)(e), a = m.ONBOARDING_STEPS.findIndex(e => e === t);
   if (-1 !== a && 0 !== a) {
-    if (!n && O.ONBOARDING_STEPS[a - 1] === O.GuildSettingsOnboardingPage.HOME_SETTINGS) {
-      D(O.ONBOARDING_STEPS[a - 2]);
+    if (!n && m.ONBOARDING_STEPS[a - 1] === m.GuildSettingsOnboardingPage.HOME_SETTINGS) {
+      g(m.ONBOARDING_STEPS[a - 2]);
       return
     }
-    D(O.ONBOARDING_STEPS[a - 1])
+    g(m.ONBOARDING_STEPS[a - 1])
   }
 }
-async function L(e) {
-  let t = _.default.getGuild(e);
-  null != t && (I.default.close(), (0, u.startImpersonating)(t.id, {
-    type: c.ImpersonateType.NEW_MEMBER,
+async function p(e) {
+  let t = I.default.getGuild(e);
+  null != t && (f.default.close(), (0, c.startImpersonating)(t.id, {
+    type: N.ImpersonateType.NEW_MEMBER,
     roles: {},
     optInChannels: new Set,
     optInEnabled: !1,
@@ -109,9 +124,9 @@ async function L(e) {
       isPending: t.hasVerificationGate(),
       flags: 0
     }
-  }), i.default.resetOnboardingStatus(t.id), (0, d.discardOnboardingPromise)(t.id), await (0, d.default)({
+  }), r.default.resetOnboardingStatus(t.id), (0, o.discardOnboardingPromise)(t.id), await (0, o.default)({
     guildId: t.id,
     isPreview: !0,
-    returnChannelId: S.StaticChannelRoute.GUILD_HOME
+    returnChannelId: T.StaticChannelRoute.GUILD_HOME
   }))
 }
--- chunks/825287.js
+++ chunks/825287.js
@@ -4,8 +4,8 @@ function o() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "262823",
-    versionHash: "1658fb5c1748be31f8b4afad9e9cf3d2fbd2946b"
+    buildNumber: "262826",
+    versionHash: "e438b4b4ff881f8e95a24ceebf169c6b905224da"
   }
 }
 n.r(e), n.d(e, {
