8f88da673a8ab9fcf242bfc836e8a793ed896093
DiscrapperManager <admin@minerpl.xyz>
2024-02-13 18:38:33 +0100

    canaryRelease: 265873

--- chunks/736131.js
+++ chunks/736131.js
@@ -8,8 +8,8 @@ s.r(t), s.d(t, {
   }
 }), s("222007");
 var T, l, n = s("37983"),
-  a = s("884691"),
-  o = s("77078"),
+  o = s("884691"),
+  a = s("77078"),
   r = s("365686"),
   _ = s("626216"),
   i = s("782340"),
@@ -19,57 +19,59 @@ var u = e => {
   let {
     onClose: t,
     transitionState: s
-  } = e, [T, l] = a.useState("ACTIONS"), u = a.useCallback(e => {
+  } = e, [T, l] = o.useState("ACTIONS"), u = o.useCallback(e => {
     let {
       text: t,
       onClick: s
     } = e;
-    return (0, n.jsx)(o.Button, {
-      look: o.Button.Looks.LINK,
-      size: o.Button.Sizes.MIN,
+    return (0, n.jsx)(a.Button, {
+      look: a.Button.Looks.LINK,
+      size: a.Button.Sizes.MIN,
       onClick: s,
-      color: o.Button.Colors.CUSTOM,
+      color: a.Button.Colors.CUSTOM,
       className: S.footerLink,
       children: t
     })
-  }, []), E = a.useCallback(() => {
+  }, []), E = o.useCallback(() => {
     if ("ABOUT_SAFETY_ALERTS" === T) return (0, n.jsx)(u, {
       text: i.default.Messages.BACK,
       onClick: () => l("ACTIONS")
     });
     return null
-  }, [T, u]), d = a.useCallback(() => {
+  }, [T, u]), d = o.useCallback(() => {
     if ("ABOUT_SAFETY_ALERTS" === T) return i.default.Messages.SAFETY_TOOLS_ACTION_SHEET_ABOUT_TITLE;
     return i.default.Messages.SAFETY_TOOLS_ACTION_SHEET_HEADER
-  }, [T]), O = a.useCallback(e => {
+  }, [T]), O = o.useCallback(e => {
     l(e)
   }, [l]);
-  return (0, n.jsxs)(o.ModalRoot, {
+  return (0, n.jsxs)(a.ModalRoot, {
     transitionState: s,
     "aria-label": i.default.Messages.SAFETY_TOOLS_ACTION_SHEET_HEADER,
-    size: o.ModalSize.SMALL,
-    children: [(0, n.jsx)(o.ModalHeader, {
+    size: a.ModalSize.SMALL,
+    children: [(0, n.jsx)(a.ModalHeader, {
       separator: !1,
       className: S.modalHeader,
-      children: (0, n.jsx)(o.Heading, {
+      children: (0, n.jsx)(a.Heading, {
         variant: "heading-xl/semibold",
         children: d()
       })
-    }), (0, n.jsxs)(o.Slides, {
-      activeSlide: T,
-      width: 440,
-      children: [(0, n.jsx)(o.Slide, {
-        id: "ACTIONS",
-        children: (0, n.jsx)(_.default, {
-          transitionToSlide: O
-        })
-      }), (0, n.jsx)(o.Slide, {
-        id: "ABOUT_SAFETY_ALERTS",
-        children: (0, n.jsx)(r.default, {
-          onClose: () => t()
-        })
-      })]
-    }), (0, n.jsxs)(o.ModalFooter, {
+    }), (0, n.jsx)(a.Scroller, {
+      children: (0, n.jsxs)(a.Slides, {
+        activeSlide: T,
+        width: 440,
+        children: [(0, n.jsx)(a.Slide, {
+          id: "ACTIONS",
+          children: (0, n.jsx)(_.default, {
+            transitionToSlide: O
+          })
+        }), (0, n.jsx)(a.Slide, {
+          id: "ABOUT_SAFETY_ALERTS",
+          children: (0, n.jsx)(r.default, {
+            onClose: () => t()
+          })
+        })]
+      })
+    }), (0, n.jsxs)(a.ModalFooter, {
       className: S.modalFooter,
       children: [(0, n.jsx)(u, {
         text: i.default.Messages.CLOSE,
--- chunks/825287.js
+++ chunks/825287.js
@@ -4,8 +4,8 @@ function o() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "265862",
-    versionHash: "4e0a56d041d3c372d0c3b3b387ed59772e69257e"
+    buildNumber: "265873",
+    versionHash: "39247e478c087b0087f0ba141160e7335e078104"
   }
 }
 n.r(e), n.d(e, {
