2933283546375a00b3022875a7d584d3edcfcd33
DiscrapperManager <admin@minerpl.xyz>
2024-02-13 17:15:13 +0100

    canaryRelease: 265844

--- chunks/945701.js
+++ chunks/945701.js
@@ -1,7 +1,7 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return x
+    return N
   }
 }), n("222007");
 var l = n("37983"),
@@ -17,70 +17,68 @@ var l = n("37983"),
   p = n("219013"),
   m = n("233069"),
   h = n("882641"),
-  E = n("945330"),
-  g = n("599110"),
-  S = n("964261"),
-  C = n("407462"),
-  T = n("526409"),
-  v = n("314848"),
-  I = n("49111"),
-  _ = n("782340"),
-  y = n("619187");
-let A = (0, m.createChannelRecord)({
+  E = n("599110"),
+  g = n("964261"),
+  S = n("407462"),
+  C = n("526409"),
+  T = n("314848"),
+  v = n("49111"),
+  I = n("782340"),
+  _ = n("619187");
+let y = (0, m.createChannelRecord)({
     id: "1",
-    type: I.ChannelTypes.DM
+    type: v.ChannelTypes.DM
   }),
-  N = i.forwardRef(function(e, t) {
+  A = i.forwardRef(function(e, t) {
     let {
       user: n,
-      hide: a,
-      onEnter: r
-    } = e, [p, m] = i.useState(""), [h, g] = i.useState((0, c.toRichValue)("")), C = (0, u.default)();
-    return (0, l.jsxs)("div", {
-      className: s([y.reply, "theme-".concat(C)]),
-      ref: t,
-      children: [(0, l.jsxs)("div", {
-        className: y.replyHeader,
-        children: [(0, l.jsxs)(o.Text, {
-          variant: "text-md/bold",
-          children: ["What do you want to say to ", n.globalName, "?"]
-        }), (0, l.jsx)(o.Clickable, {
-          className: y.closeIcon,
-          "aria-label": _.default.Messages.QUICK_REPLY_CLOSE_ARIA_LABEL,
-          onClick: a,
-          children: (0, l.jsx)(E.default, {})
-        })]
-      }), (0, l.jsx)(f.default, {
-        className: y.replyInput,
-        focused: !0,
-        showRemainingCharsAfterCount: -1,
-        allowNewLines: !1,
-        maxCharacterCount: S.MAX_CHAR_COUNT,
-        channel: A,
-        onChange: (e, t, n) => {
-          m(t), g(n)
-        },
-        type: d.ChatInputTypes.ATOMIC_REACTOR_REPLY_INPUT,
-        textValue: p,
-        richValue: h,
-        onSubmit: e => {
-          let {
-            value: t
-          } = e;
-          return t.length > S.MAX_CHAR_COUNT ? Promise.resolve({
-            shouldClear: !1,
-            shouldRefocus: !0
-          }) : (r(t), Promise.resolve({
-            shouldClear: !1,
-            shouldRefocus: !1
-          }))
-        },
-        disableThemedBackground: !0,
-        emojiPickerCloseOnModalOuterClick: !0
-      })]
+      onEnter: a,
+      showPopout: r,
+      children: p
+    } = e, [m, h] = i.useState(""), [E, S] = i.useState((0, c.toRichValue)("")), C = (0, u.default)();
+    return (0, l.jsx)(o.Popout, {
+      align: "right",
+      position: "top",
+      shouldShow: r,
+      renderPopout: () => (0, l.jsx)("div", {
+        className: s([_.reply, "theme-".concat(C)]),
+        ref: t,
+        children: (0, l.jsx)(f.default, {
+          placeholder: I.default.Messages.TEXTAREA_PLACEHOLDER.format({
+            channel: "@".concat(n.username)
+          }),
+          className: _.replyInput,
+          focused: !0,
+          showRemainingCharsAfterCount: -1,
+          allowNewLines: !1,
+          maxCharacterCount: g.MAX_CHAR_COUNT,
+          channel: y,
+          onChange: (e, t, n) => {
+            h(t), S(n)
+          },
+          type: d.ChatInputTypes.ATOMIC_REACTOR_REPLY_INPUT,
+          textValue: m,
+          richValue: E,
+          onSubmit: e => {
+            let {
+              value: t
+            } = e;
+            return t.length > g.MAX_CHAR_COUNT ? Promise.resolve({
+              shouldClear: !1,
+              shouldRefocus: !0
+            }) : (a(t), Promise.resolve({
+              shouldClear: !1,
+              shouldRefocus: !1
+            }))
+          },
+          disableThemedBackground: !0,
+          emojiPickerCloseOnModalOuterClick: !0
+        })
+      }),
+      children: () => p
     })
   });
-var x = e => {
+var N = e => {
   let {
     activity: t,
     user: n,
@@ -90,7 +88,7 @@ var x = e => {
     filename: c,
     reactor: f,
     analyticsParams: m
-  } = e, [E, S] = i.useState(!1), _ = (0, u.default)(), [x, O] = i.useState(!1), [R, M] = i.useState(!1), [L, P] = i.useState(), [b, j] = i.useState(""), U = i.useRef(null), D = i.useRef(null), k = (0, v.default)(n);
+  } = e, [g, I] = i.useState(!1), N = (0, u.default)(), [x, O] = i.useState(!1), [R, M] = i.useState(!1), [L, P] = i.useState(), [b, j] = i.useState(""), U = i.useRef(null), D = i.useRef(null), k = (0, T.default)(n);
 
   function w(e) {
     var t, n;
@@ -98,7 +96,7 @@ var x = e => {
   }
 
   function F(e) {
-    g.default.track(I.AnalyticEvents.ACTIVITY_REACTOR_INTERACTED, {
+    E.default.track(v.AnalyticEvents.ACTIVITY_REACTOR_INTERACTED, {
       application_id: t.application_id,
       interaction_type: e,
       ...m
@@ -113,52 +111,47 @@ var x = e => {
     }
   }, []), k) ? (0, l.jsxs)(l.Fragment, {
     children: [(0, l.jsxs)("div", {
-      className: y.reactions,
+      className: _.reactions,
       children: [a && (0, l.jsx)(h.default, {
         active: !1,
         tabIndex: 0,
-        className: y.reaction,
+        className: _.reaction,
         onClick: () => {
-          F(C.ActivityReactorInteractionTypes.ReactBegin), O(!0), M(!1)
+          F(S.ActivityReactorInteractionTypes.ReactBegin), O(!0), M(!1)
         }
-      }), o && (0, l.jsx)("button", {
-        style: {
-          backgroundColor: "transparent",
-          marginTop: 4,
-          marginLeft: 8
+      }), o && (0, l.jsx)(A, {
+        ref: D,
+        user: n,
+        showPopout: R,
+        onEnter: e => {
+          F(S.ActivityReactorInteractionTypes.ReplySubmit), j(e), I(!0)
         },
-        className: y.reaction,
-        onClick: () => {
-          F(C.ActivityReactorInteractionTypes.ReplyBegin), M(!0)
-        },
-        children: (0, l.jsx)(r.ArrowAngleLeftUpIcon, {})
+        children: (0, l.jsx)("button", {
+          className: s(_.reaction, _.emojiButton),
+          onClick: () => {
+            F(S.ActivityReactorInteractionTypes.ReplyBegin), M(!0)
+          },
+          children: (0, l.jsx)(r.ArrowAngleLeftUpIcon, {})
+        })
       })]
     }), x && (0, l.jsx)("div", {
-      className: s([y.picker, "theme-".concat(_)]),
+      className: s([_.picker, "theme-".concat(N)]),
       ref: U,
       children: (0, l.jsx)(p.ReactionPicker, {
         messageId: "",
-        channel: A,
+        channel: y,
         closePopout: () => {
           O(!1)
         },
         onSelectEmoji: e => {
-          null != e && (F(C.ActivityReactorInteractionTypes.ReactSubmit), P(e), O(!1), S(!0))
+          null != e && (F(S.ActivityReactorInteractionTypes.ReactSubmit), P(e), O(!1), I(!0))
         }
       })
-    }), R && (0, l.jsx)(N, {
-      ref: D,
-      user: n,
-      activity: t,
-      hide: () => M(!1),
-      onEnter: e => {
-        F(C.ActivityReactorInteractionTypes.ReplySubmit), j(e), S(!0)
-      }
-    }), E && (0, l.jsx)(f, {
+    }), g && (0, l.jsx)(f, {
       activity: t,
       user: n,
       onComplete: e => {
-        null != L ? (F(C.ActivityReactorInteractionTypes.ReactSend), (0, T.sendReaction)(e, L, n, d, c), P(void 0)) : null != b && (F(C.ActivityReactorInteractionTypes.ReplySend), (0, T.sendReply)(e, b, n, d, c), j("")), S(!1)
+        null != L ? (F(S.ActivityReactorInteractionTypes.ReactSend), (0, C.sendReaction)(e, L, n, d, c), P(void 0)) : null != b && (F(S.ActivityReactorInteractionTypes.ReplySend), (0, C.sendReply)(e, b, n, d, c), j("")), I(!1)
       }
     })]
   }) : null
