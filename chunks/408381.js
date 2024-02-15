b794e7044e618d7534f62e399c6ad6d97a0c9fa1
DiscrapperManager <admin@minerpl.xyz>
2024-02-15 02:07:57 +0100

    canaryRelease: 266557

--- chunks/408381.js
+++ chunks/408381.js
@@ -1,24 +1,23 @@
 "use strict";
 a.r(t), a.d(t, {
   MAX_CONTENT_WIDTH: function() {
-    return n
+    return r
   },
   getAssetSizeByHeight: function() {
-    return i
+    return n
   },
   getBackgroundGradient: function() {
-    return o
+    return i
   },
   getConfettiSprites: function() {
-    return d
+    return u
   }
 });
 var l = a("635058"),
-  s = a("547630"),
-  r = a("172858");
-let n = 1060,
-  i = e => e * (0, s.default)(),
-  o = function(e) {
+  s = a("547630");
+let r = 1060,
+  n = e => e * (0, s.default)(),
+  i = function(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : 180,
       {
         primary: a,
@@ -26,19 +25,17 @@ let n = 1060,
       } = e;
     return "linear-gradient(".concat(t, "deg, ").concat(a.toHslString(), ", ").concat(l.toHslString(), ")")
   },
-  c = r.COMMON_CONFETTI_SPRITES.filter((e, t) => 1 !== t && 7 !== t),
-  u = {
+  o = [a("424869"), a("714293")],
+  c = {
     ["".concat(l.CollectiblesCategorySkuId.FANTASY)]: [a("655029"), a("290216")],
     ["".concat(l.CollectiblesCategorySkuId.ANIME)]: [a("597704"), a("714293")],
     ["".concat(l.CollectiblesCategorySkuId.BREAKFAST)]: [a("286642"), a("94646")],
     ["".concat(l.CollectiblesCategorySkuId.HALLOWEEN)]: [a("638538"), a("861485")],
     ["".concat(l.CollectiblesCategorySkuId.FALL)]: [a("740683"), a("489847")],
     ["".concat(l.CollectiblesCategorySkuId.WINTER)]: [a("770704"), a("165057")],
-    ["".concat(l.CollectiblesCategorySkuId.MONSTERS)]: [a("330825"), a("125221")],
-    ["".concat(l.CollectiblesCategorySkuId.CYBERPUNK)]: [a("424869"), a("714293")],
-    ["".concat(l.CollectiblesCategorySkuId.LUNAR_NEW_YEAR)]: [a("424869"), a("714293")]
+    ["".concat(l.CollectiblesCategorySkuId.MONSTERS)]: [a("330825"), a("125221")]
   },
-  d = e => {
+  u = e => {
     var t;
-    return null == e ? c : null !== (t = u[e]) && void 0 !== t ? t : c
+    return null == e ? o : null !== (t = c[e]) && void 0 !== t ? t : o
   }
--- chunks/825287.js
+++ chunks/825287.js
@@ -4,8 +4,8 @@ function o() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "266547",
-    versionHash: "1f1a1e9aafde72ccb2086486bdc12d706cde6bd6"
+    buildNumber: "266557",
+    versionHash: "633f9ac30f37f056f670eeafeb19b33b520f533c"
   }
 }
 n.r(e), n.d(e, {
