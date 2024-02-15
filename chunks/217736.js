14d70a5872954a68e392133ef57e83887b013462
DiscrapperManager <admin@minerpl.xyz>
2024-02-15 22:23:47 +0100

    canaryRelease: 266824

--- chunks/217736.js
+++ chunks/217736.js
@@ -4,27 +4,27 @@ n.r(t), n.d(t, {
     return r
   },
   CtaEventTypes: function() {
-    return a
+    return s
   },
   trackCtaEvent: function() {
     return o
   }
 });
-var s, a, l = n("599110"),
+var a, s, l = n("599110"),
   i = n("49111");
 
 function r(e, t) {
   let {
     channelId: n,
-    warningId: s,
-    senderId: a,
+    warningId: a,
+    senderId: s,
     warningType: i
   } = t;
   l.default.track(e, {
     channel_id: n,
-    warning_id: parseInt(s),
+    warning_id: null != a ? parseInt(a) : void 0,
     warning_type: i,
-    other_user_id: a
+    other_user_id: s
   })
 }
 
@@ -32,15 +32,15 @@ function o(e) {
   let {
     channelId: t,
     warningId: n,
-    senderId: s,
-    warningType: a,
+    senderId: a,
+    warningType: s,
     cta: r
   } = e;
   l.default.track(i.AnalyticEvents.SAFETY_WARNING_CTA_CLICKED, {
     channel_id: t,
-    warning_id: parseInt(n),
-    warning_type: a,
-    other_user_id: s,
+    warning_id: null != n ? parseInt(n) : void 0,
+    warning_type: s,
+    other_user_id: a,
     cta: r
   })
-}(s = a || (a = {})).DISMISS = "dismiss", s.OPEN_MORE_TIPS = "open_more_tips", s.USER_BANNER_BLOCK_CONFIRM = "user_banner_block_confirm", s.USER_BANNER_BLOCK_CANCEL = "user_banner_block_cancel", s.USER_MODAL_BLOCK_CONFIRM = "user_modal_block_confirm", s.USER_MODAL_BLOCK_CANCEL = "user_modal_block_cancel", s.USER_MODAL_MUTE = "user_modal_mute", s.USER_MODAL_UNMUTE = "user_modal_unmute", s.FEEDBACK_UPVOTE = "feedback_upvote", s.FEEDBACK_DOWNVOTE = "feedback_downvote"
+}(a = s || (s = {})).DISMISS = "dismiss", a.OPEN_MORE_TIPS = "open_more_tips", a.USER_BANNER_BLOCK_CONFIRM = "user_banner_block_confirm", a.USER_BANNER_BLOCK_CANCEL = "user_banner_block_cancel", a.USER_MODAL_BLOCK_CONFIRM = "user_modal_block_confirm", a.USER_MODAL_BLOCK_CANCEL = "user_modal_block_cancel", a.USER_SAFETY_TOOLS_BLOCK_CONFIRM = "user_safety_tools_block_confirm", a.USER_SAFETY_TOOLS_BLOCK_CANCEL = "user_safety_tools_block_cancel", a.USER_MODAL_MUTE = "user_modal_mute", a.USER_MODAL_UNMUTE = "user_modal_unmute", a.FEEDBACK_UPVOTE = "feedback_upvote", a.FEEDBACK_DOWNVOTE = "feedback_downvote"
