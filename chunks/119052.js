78dfe0710e79c6bf5c2b745bb3d026b75c9a26ba
DiscrapperManager <admin@minerpl.xyz>
2024-02-26 19:26:46 +0100

    canaryRelease: 268958

--- chunks/119052.js
+++ chunks/119052.js
@@ -1,84 +1,64 @@
 "use strict";
 s.r(t), s.d(t, {
   InputItem: function() {
-    return o
-  },
-  SelectItem: function() {
     return d
   },
-  TextInputItem: function() {
+  SelectItem: function() {
     return u
   }
 });
-var a = s("37983");
-s("884691");
-var n = s("414456"),
-  l = s.n(n),
-  i = s("77078"),
-  r = s("925994");
+var a = s("37983"),
+  n = s("884691"),
+  l = s("414456"),
+  i = s.n(l),
+  r = s("77078"),
+  o = s("925994");
 
-function o(e) {
+function d(e) {
   let {
     title: t,
     note: s,
     children: n,
-    className: o
+    className: l,
+    id: d
   } = e;
   return (0, a.jsxs)("div", {
-    className: l(r.inputItem, o),
+    className: i(o.inputItem, l),
     children: [(0, a.jsxs)("div", {
-      className: r.inputItemText,
-      children: [(0, a.jsx)(i.Text, {
-        className: r.inputItemTitle,
+      className: o.inputItemText,
+      children: [(0, a.jsx)(r.Text, {
+        tag: "label",
         variant: "text-md/medium",
-        color: "interactive-active",
+        className: o.inputItemTitle,
+        id: d,
         children: t
-      }), null != s && (0, a.jsx)(i.FormText, {
-        className: r.note,
-        type: i.FormText.Types.DESCRIPTION,
+      }), null != s && (0, a.jsx)(r.FormText, {
+        className: o.note,
+        type: r.FormText.Types.DESCRIPTION,
         children: s
       })]
     }), n]
   })
 }
 
-function d(e) {
+function u(e) {
   let {
     options: t,
     value: s,
-    select: n,
-    selectClassName: d,
-    ...u
-  } = e;
-  return (0, a.jsx)(o, {
-    ...u,
-    children: (0, a.jsx)(i.Select, {
-      className: l(r.selectControl, d),
+    select: l,
+    selectClassName: u,
+    ...c
+  } = e, S = n.useId();
+  return (0, a.jsx)(d, {
+    ...c,
+    id: S,
+    children: (0, a.jsx)(r.Select, {
+      "aria-labelledby": S,
+      className: i(o.selectControl, u),
       options: t,
       isSelected: e => e === s,
-      select: n,
+      select: l,
       serialize: e => String(e)
-    })
-  })
-}
-
-function u(e) {
-  let {
-    value: t,
-    onChange: s,
-    editable: n,
-    textInputClassName: d,
-    onClick: u,
-    ...c
-  } = e;
-  return (0, a.jsx)(o, {
-    ...c,
-    children: (0, a.jsx)(i.TextInput, {
-      onClick: u,
-      editable: n,
-      className: l(r.selectControl, d),
-      value: t,
-      onChange: s
     })
   })
 }
--- chunks/462491.js
+++ chunks/462491.js
@@ -12,26 +12,35 @@ var n = s("446674"),
   r = s("386045"),
   o = s("803725"),
   d = s("119052"),
-  u = s("49111"),
-  c = s("782340"),
-  S = s("824905");
+  u = s("782340"),
+  c = s("824905"),
+  S = s("925994");
 
 function E() {
-  let e = (0, n.useStateFromStores)([r.default], () => r.default.getSettings().storageLocation);
+  let e = (0, n.useStateFromStores)([r.default], () => r.default.getSettings().storageLocation),
+    t = async () => {
+      let e = await i.default.fileManager.showOpenDialog({
+        properties: ["openDirectory", "createDirectory"]
+      });
+      e.length > 0 && o.updateClipsStorageLocation(e[0])
+    };
   return (0, a.jsx)(l.FormSection, {
-    children: (0, a.jsx)(d.TextInputItem, {
-      className: S.formItem,
-      title: c.default.Messages.CLIPS_SETTINGS_STORAGE_LOCATION,
-      note: c.default.Messages.CLIPS_SETTINGS_STORAGE_LOCATION_HELP,
-      value: e,
-      editable: !1,
-      onClick: async () => {
-        let e = await i.default.fileManager.showOpenDialog({
-          properties: ["openDirectory", "createDirectory"]
-        });
-        e.length > 0 && o.updateClipsStorageLocation(e[0])
-      },
-      onChange: u.NOOP_NULL
+    children: (0, a.jsx)(d.InputItem, {
+      title: u.default.Messages.CLIPS_SETTINGS_STORAGE_LOCATION,
+      note: u.default.Messages.CLIPS_SETTINGS_STORAGE_LOCATION_HELP,
+      className: c.formItem,
+      children: (0, a.jsx)(l.Clickable, {
+        className: S.selectControl,
+        "aria-label": u.default.Messages.CLIPS_SETTINGS_STORAGE_LOCATION_LABEL.format({
+          storageLocation: e
+        }),
+        onClick: t,
+        children: (0, a.jsx)(l.TextInput, {
+          tabIndex: -1,
+          value: e,
+          editable: !1
+        })
+      })
     })
   })
 }
