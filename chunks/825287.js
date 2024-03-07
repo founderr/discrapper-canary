d990707b64fabcee24623ddb85c94a389eed262f
DiscrapperManager <admin@minerpl.xyz>
2024-03-07 19:11:21 +0100

    canaryRelease: 273085

--- chunks/825287.js
+++ chunks/825287.js
@@ -4,8 +4,8 @@ function o() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "273084",
-    versionHash: "5905eace0d4fdd038034217fe1743db06f1ae847"
+    buildNumber: "273085",
+    versionHash: "133645af16875fcaf399081e9d36319b37694fe2"
   }
 }
 n.r(e), n.d(e, {
