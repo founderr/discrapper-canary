a82d1c5e1c5e9410b39c8d0791fece4298baf1f8
DiscrapperManager <admin@minerpl.xyz>
2024-03-07 15:43:35 +0100

    canaryRelease: 273026

--- chunks/407117.js
+++ chunks/407117.js
@@ -2,8 +2,9 @@
 E.r(_);
 var t = E("759843"),
   o = E("913144"),
-  n = E("271938"),
-  r = E("49111");
+  n = E("371160"),
+  r = E("271938"),
+  a = E("49111");
 (0, t.analyticsTrackingStoreMaker)({
   dispatcher: o.default,
   actionHandler: {
@@ -14,7 +15,10 @@ var t = E("759843"),
     FINGERPRINT: () => t.AnalyticsActionHandlers.handleFingerprint(),
     TRACK: e => t.AnalyticsActionHandlers.handleTrack(e)
   },
-  TRACKING_URL: r.Endpoints.TRACK,
-  waitFor: [n.default],
-  getFingerprint: n.default.getFingerprint
+  TRACKING_URL: a.Endpoints.TRACK,
+  waitFor: [r.default],
+  getFingerprint: r.default.getFingerprint,
+  getSessionId: () => (0, n.getSession)().then(e => ({
+    sessionId: null == e ? void 0 : e.uuid
+  }))
 })
