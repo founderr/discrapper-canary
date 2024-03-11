cd089a1962808b6d3611d029fc6cbb9ab6b383f2
DiscrapperManager <admin@minerpl.xyz>
2024-03-11 22:07:18 +0100

    canaryRelease: 274030

--- chunks/2973.js
+++ chunks/2973.js
@@ -17,12 +17,24 @@ let r = !1,
   _ = new Map;
 
 function h(e, t) {
+  let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : {
+    updateProgress: !1
+  };
   a = new Map(a);
-  let n = a.get(e);
-  null != n && a.set(e, {
-    ...n,
-    ...t
-  })
+  let i = a.get(e);
+  if (null != i) {
+    var s;
+    let r = null === (s = i.userStatus) || void 0 === s ? void 0 : s.streamProgressSeconds,
+      o = {
+        ...i,
+        ...t
+      },
+      l = n.updateProgress || null == o.userStatus || null == r || null != o.userStatus.completedAt || null == o.userStatus.enrolledAt;
+    !l && null != o.userStatus && (o.userStatus = {
+      ...o.userStatus,
+      streamProgressSeconds: r
+    }), a.set(e, o)
+  }
 }
 
 function E(e, t) {
@@ -108,6 +120,8 @@ var v = new S(s.default, {
     } = e;
     h(t, {
       userStatus: i
+    }, {
+      updateProgress: !0
     }), g(n)
   },
   QUESTS_SEND_HEARTBEAT_FAILURE: function(e) {
@@ -225,6 +239,8 @@ var v = new S(s.default, {
     } = e;
     h(t.questId, {
       userStatus: t
+    }, {
+      updateProgress: !0
     })
   }
 })
