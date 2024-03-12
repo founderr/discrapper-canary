1f8aad1cc67451189c156a4310271168134f8c46
DiscrapperManager <admin@minerpl.xyz>
2024-03-12 19:59:47 +0100

    canaryRelease: 274385

--- chunks/369010.js
+++ chunks/369010.js
@@ -1,9 +1,6 @@
 "use strict";
 n.r(t), n.d(t, {
   useTopCommands: function() {
-    return r
-  },
-  useTopRealCommands: function() {
     return s
   }
 });
@@ -12,23 +9,11 @@ var i = n("884691"),
   a = n("872173"),
   o = n("342564");
 
-function r(e) {
-  i.useEffect(() => {
-    a.FrecencyUserSettingsActionCreators.loadIfNecessary()
-  }, []);
-  let t = (0, l.useStateFromStores)([o.default], () => o.default.getTopCommandsWithoutLoadingLatest()),
-    n = i.useMemo(() => (0, o.getFilteredTopCommands)(t, e), [t, e]);
-  return n
-}
-
 function s(e) {
   i.useEffect(() => {
     a.FrecencyUserSettingsActionCreators.loadIfNecessary()
   }, []);
   let t = (0, l.useStateFromStores)([o.default], () => o.default.getTopCommandsWithoutLoadingLatest()),
-    n = i.useMemo(() => {
-      let n = (0, o.getFilteredTopCommands)(t, e);
-      return (0, o.getTopRealCommands)(n)
-    }, [t, e]);
+    n = i.useMemo(() => (0, o.getFilteredTopCommands)(t, e), [t, e]);
   return n
 }
--- chunks/507217.js
+++ chunks/507217.js
@@ -15,23 +15,8 @@ n.r(t), n.d(t, {
   updateApplicationGuildCommandPermissions: function() {
     return m
   },
-  querySearchManager: function() {
-    return p
-  },
   performAutocomplete: function() {
-    return S
-  },
-  fetchCommand: function() {
-    return v
-  },
-  fetchCommands: function() {
-    return T
-  },
-  fetchCommandsForApplication: function() {
-    return I
-  },
-  updateRegistry: function() {
-    return C
+    return p
   }
 }), n("222007");
 var i = n("627445"),
@@ -101,18 +86,8 @@ function m(e, t, n, i) {
     url: f.Endpoints.APPLICATION_BOT_GUILD_COMMAND_PERMISSIONS(e, t, n)
   })
 }
-let p = (e, t, n, i, s) => {
-  a.default.dispatch({
-    type: "APPLICATION_COMMAND_SEARCH_STORE_QUERY",
-    context: e,
-    query: t,
-    limit: n,
-    commandType: i,
-    applicationId: s
-  })
-};
 
-function S(e, t, n) {
+function p(e, t, n) {
   var i;
   s(null != t.autocomplete, "Missing autocomplete context");
   let {
@@ -142,46 +117,5 @@ function S(e, t, n) {
       type: "INTERACTION_FAILURE",
       nonce: h
     })
-  })
-}
-
-function v(e, t, n) {
-  a.default.dispatch({
-    type: "APPLICATION_COMMAND_FETCH",
-    channelId: t,
-    commandId: n,
-    guildId: e
-  })
-}
-
-function T(e, t, n) {
-  a.default.dispatch({
-    type: "APPLICATION_COMMANDS_FETCH",
-    channelId: t,
-    commandIds: n,
-    guildId: e
-  })
-}
-
-function I(e) {
-  let {
-    guildId: t,
-    channelId: n,
-    applicationId: i
-  } = e;
-  a.default.dispatch({
-    type: "APPLICATION_COMMANDS_FETCH_FOR_APPLICATION",
-    channelId: n,
-    guildId: t,
-    applicationId: i
-  })
-}
-
-function C(e, t, n) {
-  a.default.dispatch({
-    type: "APPLICATION_COMMAND_REGISTRY_UPDATE",
-    applications: t,
-    commands: e,
-    channelId: n
   })
 }
