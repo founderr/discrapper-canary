d12559def5ab8f9196b8b93049e73fe4e17348b7
DiscrapperManager <admin@minerpl.xyz>
2024-03-13 21:01:55 +0100

    canaryRelease: 274858

--- chunks/654852.js
+++ chunks/654852.js
@@ -5,7 +5,9 @@ e.exports = {
   container: "container__31412",
   contentDescription: "contentDescription__06a2c",
   contentImage: "contentImage__64b0b",
+  fontScaling: "fontScaling__97f56",
   selected: "selected_d0256a",
   textSection: "textSection_d5f384",
+  userName: "userName__1291b",
   userSection: "userSection__58e31"
 }
--- chunks/825287.js
+++ chunks/825287.js
@@ -4,8 +4,8 @@ function i() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "274847",
-    versionHash: "9ebc8ccd68c5d93891edb01982a69b7785202785"
+    buildNumber: "274858",
+    versionHash: "78977d6f3af0adc6243dbc70b7e96367ef894ce9"
   }
 }
 n.r(t), n.d(t, {
