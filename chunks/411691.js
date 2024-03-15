4982c27e6f1a8fb9fc8e078476713bab8319578a
DiscrapperManager <admin@minerpl.xyz>
2024-03-15 03:41:18 +0100

    canaryRelease: 275484

--- chunks/411691.js
+++ chunks/411691.js
@@ -8,7 +8,7 @@ n.r(t), n.d(t, {
   }
 });
 var i, l, a = n("862205");
-(i = l || (l = {}))[i.DEFAULT = 0] = "DEFAULT", i[i.WINTER_2023_DROP = 1] = "WINTER_2023_DROP", i[i.MONSTER_DROP = 2] = "MONSTER_DROP";
+(i = l || (l = {}))[i.DEFAULT = 0] = "DEFAULT", i[i.WINTER_2023_DROP = 1] = "WINTER_2023_DROP", i[i.MONSTER_DROP = 2] = "MONSTER_DROP", i[i.SPRINGTOONS = 4] = "SPRINGTOONS";
 let s = (0, a.createExperiment)({
   kind: "user",
   id: "2023-12_collectibles_shop_marketing",
@@ -27,6 +27,12 @@ let s = (0, a.createExperiment)({
     label: "Jan 2024",
     config: {
       variant: 2
+    }
+  }, {
+    id: 4,
+    label: "Springtoons 2024",
+    config: {
+      variant: 4
     }
   }]
 });
