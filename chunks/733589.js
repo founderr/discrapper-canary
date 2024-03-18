8c99fbc876857f339e368e24bc31bf01784870d6
DiscrapperManager <admin@minerpl.xyz>
2024-03-19 00:58:08 +0100

    canaryRelease: 276359

--- chunks/733589.js
+++ chunks/733589.js
@@ -1,7 +1,22 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return i
+    return s
   }
 });
-var i = n("106919").default
+let i = new class e {
+  enterPipMode() {
+    return Promise.resolve()
+  }
+  setEnabled(e) {}
+  setSelectedStream(e) {}
+  setFocusedStream(e) {}
+  setMirrored(e) {}
+  setPipAspectRatio(e, t) {}
+  updateSourceTrackingView() {}
+  addOnPipModeChangedListener(e) {}
+  isEnabled() {
+    return !1
+  }
+};
+var s = i
--- chunks/825287.js
+++ chunks/825287.js
@@ -1,15 +1,15 @@
 "use strict";
 
-function i() {
+function a() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "276338",
-    versionHash: "62b08bd1f76e44d2315082c9e4169fd74a977562"
+    buildNumber: "276359",
+    versionHash: "a5cae5992e62a3b3e30e1ec4d14bd38e892a781b"
   }
 }
 n.r(t), n.d(t, {
   default: function() {
-    return i
+    return a
   }
 })
