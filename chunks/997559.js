fe6b586c4eb14d8d5312c03fde520cedb87d86e1
DiscrapperManager <admin@minerpl.xyz>
2024-03-19 04:49:39 +0100

    canaryRelease: 276425

--- chunks/997559.js
+++ chunks/997559.js
@@ -2,6 +2,7 @@
 e.exports = {
   answer: "answer__07489",
   answerInner: "answerInner_a3bc8e",
+  answerSelectionIcon: "answerSelectionIcon__5bf65",
   answersContainer: "answersContainer__97393",
   attachment: "attachment_b6d123",
   avatarForSelected: "avatarForSelected_db7f86",
@@ -13,6 +14,5 @@ e.exports = {
   voteCount: "voteCount__9e7ee",
   voteCountButton: "voteCountButton__81a71",
   votePercentageBar: "votePercentageBar__4c440",
-  voterTooltip: "voterTooltip__04695",
   votesData: "votesData__4d275"
 }
