6e0757cc8300f8becc58254e7cfb476db1f4629a
DiscrapperManager <admin@minerpl.xyz>
2024-03-21 23:01:34 +0100

    canaryRelease: 277613

--- chunks/220350.js
+++ chunks/220350.js
@@ -1,5 +1,7 @@
 "use strict";
 e.exports = {
+  desktopTooltip: "desktopTooltip__33fd1",
+  infoIcon: "infoIcon_d5bdac",
   percentCompleteLabel: "percentCompleteLabel__049d5",
   percentCompleteLabelOffset: "percentCompleteLabelOffset__738e1",
   percentCompleteWrapper: "percentCompleteWrapper_fb73d6",
--- chunks/825287.js
+++ chunks/825287.js
@@ -4,8 +4,8 @@ function i() {
   return {
     logsUploaded: new Date().toISOString(),
     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-    buildNumber: "277572",
-    versionHash: "67ff7fb095e3ef1b373b2a1761a63ded4975048b"
+    buildNumber: "277613",
+    versionHash: "058e3c8b88d1507a81f7bdd6463038665d60356c"
   }
 }
 n.r(t), n.d(t, {
