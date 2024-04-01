95d75121397ab1989821a2ac52046ac5aec1f8b0
DiscrapperManager <admin@minerpl.xyz>
2024-04-01 20:47:55 +0200

    canaryRelease: 280411
    Main scripts: 21
    Lazy Loaded Scripts: 822
    Chunks:
     * Main Files: 24379
     * Scripts: 27171
    Strings: 17456
    Routes: 734
    Experiments Count: 318

--- chunks/31088.js
+++ chunks/31088.js
@@ -5,13 +5,13 @@ t.r(n), t.d(n, {
   }
 }), t("47120");
 var r = t("735250"),
-  l = t("470079"),
-  o = t("846519"),
-  a = t("481060"),
-  u = t("903958"),
+  u = t("470079"),
+  l = t("846519"),
+  o = t("481060"),
+  a = t("903958"),
   i = t("776980"),
-  s = t("641826"),
-  c = t("89509"),
+  c = t("641826"),
+  s = t("89509"),
   d = t("232700"),
   m = t("362895");
 let p = t("991740"),
@@ -22,89 +22,91 @@ function v(e) {
   let {
     onClose: n,
     transitionState: t
-  } = e, [v, b] = l.useState(!1), [g, S] = l.useState(!1), [E, h] = l.useState(null), [k, I] = l.useState(!1), [C, A] = l.useState(d.BoxAnimationScenes.ENTRY), N = l.useMemo(() => (0, d.getLootboxes)(), []), T = null != E ? N[E.openedItem] : null, B = l.useRef(null), D = l.useRef(null), w = l.useRef(null), j = l.useRef(null), y = (0, c.default)(D.current, 0), L = t === a.ModalTransitionState.EXITING || t === a.ModalTransitionState.EXITED, O = l.useCallback(() => {
-    let e = C === d.BoxAnimationScenes.IDLE ? B.current : w.current;
+  } = e, [v, b] = u.useState(!1), [g, S] = u.useState(!1), [E, h] = u.useState(null), k = u.useRef(null), [I, C] = u.useState(!1), [A, N] = u.useState(d.BoxAnimationScenes.ENTRY), T = u.useMemo(() => (0, d.getLootboxes)(), []), B = null != E ? T[E.openedItem] : null, D = u.useRef(null), w = u.useRef(null), j = u.useRef(null), y = u.useRef(null), L = (0, s.default)(w.current, 0), R = t === o.ModalTransitionState.EXITING || t === o.ModalTransitionState.EXITED, O = u.useCallback(() => {
+    let e = A === d.BoxAnimationScenes.IDLE ? D.current : j.current;
     if (null == e) return 0;
-    let n = C === d.BoxAnimationScenes.IDLE ? .3 : .9,
+    let n = A === d.BoxAnimationScenes.IDLE ? .3 : .9,
       t = e.duration * n,
       r = e.duration - t;
-    return Math.min(1, Math.max(0, (C === d.BoxAnimationScenes.IDLE ? e.currentTime - t : e.currentTime) / r))
-  }, [C]), P = l.useCallback(async () => {
+    return Math.min(1, Math.max(0, (A === d.BoxAnimationScenes.IDLE ? e.currentTime - t : e.currentTime) / r))
+  }, [A]), P = u.useCallback(async () => {
     var e;
     S(!0);
     let n = await (0, i.openLootbox)(),
-      t = null != n ? N[n.openedItem] : null,
+      t = null != n ? T[n.openedItem] : null,
       r = document.getElementsByClassName("lootbox-animation-item-image")[0],
-      l = null == r ? void 0 : r.children[0];
-    null == l || l.setAttribute("href", null !== (e = null == t ? void 0 : t.image) && void 0 !== e ? e : ""), h(n), A(d.BoxAnimationScenes.OPEN)
-  }, [N]), R = l.useCallback(e => {
+      u = null == r ? void 0 : r.children[0];
+    null == u || u.setAttribute("href", null !== (e = null == t ? void 0 : t.image) && void 0 !== e ? e : ""), h(n), k.current = n, N(d.BoxAnimationScenes.OPEN)
+  }, [T]), M = u.useCallback(e => {
     var n, t, r;
     switch (e) {
       case d.BoxAnimationScenes.ENTRY:
-        null === (n = B.current) || void 0 === n || n.play(), A(d.BoxAnimationScenes.IDLE);
+        null === (n = D.current) || void 0 === n || n.play(), N(d.BoxAnimationScenes.IDLE);
         break;
       case d.BoxAnimationScenes.OPEN:
-        null === (t = w.current) || void 0 === t || t.play(), y(0, {
+        null === (t = j.current) || void 0 === t || t.play(), L(0, {
           getPercent: O
-        }), A(d.BoxAnimationScenes.OPENED);
+        }), N(d.BoxAnimationScenes.OPENED);
         break;
       case d.BoxAnimationScenes.OPENED:
-        if (null != j.current && !v) {
-          let e = (null == E ? void 0 : E.openedItem) != null ? N[E.openedItem] : null;
-          j.current.src = null !== (r = null == e ? void 0 : e.sound) && void 0 !== r ? r : "", j.current.play()
+        if (null != y.current && !v) {
+          let e = (null == E ? void 0 : E.openedItem) != null ? T[E.openedItem] : null;
+          y.current.src = null !== (r = null == e ? void 0 : e.sound) && void 0 !== r ? r : "", y.current.play()
         }
         b(!0), S(!1)
     }
-  }, [y, O, v, null == E ? void 0 : E.openedItem, N]), M = l.useCallback((e, t) => {
-    e === d.BoxAnimationScenes.OPENED && t.currentTime >= t.totalTime - 1 && (I(!0), n(), null != E && (0, i.reportFinishedOpeningLootbox)(E.userLootboxData, E.openedItem))
-  }, [n, E]);
-  return l.useEffect(() => {
-    let e = new o.Interval;
+  }, [L, O, v, null == E ? void 0 : E.openedItem, T]), F = u.useCallback((e, t) => {
+    e === d.BoxAnimationScenes.OPENED && t.currentTime >= t.totalTime - 1 && (C(!0), n())
+  }, [n]);
+  return u.useEffect(() => {
+    let e = new l.Interval;
     return e.start(50, () => {
-      if (null == B.current || null == D.current) return;
-      let n = B.current.currentTime / B.current.duration;
-      D.current.paused && n >= .3 && (y(1, {
+      if (null == D.current || null == w.current) return;
+      let n = D.current.currentTime / D.current.duration;
+      w.current.paused && n >= .3 && (L(1, {
         getPercent: O
-      }), D.current.play(), e.stop())
+      }), w.current.play(), e.stop())
     }), () => e.stop()
-  }, [O, y]), (0, r.jsx)("div", {
-    className: L ? m.backdropClosing : m.backdrop,
+  }, [O, L]), u.useEffect(() => () => {
+    null != k.current && (0, i.reportFinishedOpeningLootbox)(k.current.userLootboxData, k.current.openedItem)
+  }, []), (0, r.jsx)("div", {
+    className: R ? m.backdropClosing : m.backdrop,
     children: (0, r.jsxs)("div", {
-      className: L ? m.wrapperClosing : m.wrapper,
-      children: [(0, r.jsx)(a.Heading, {
+      className: R ? m.wrapperClosing : m.wrapper,
+      children: [(0, r.jsx)(o.Heading, {
         className: v ? m.openedItemTextOpened : m.openedItemTextUnopened,
         variant: "heading-lg/normal",
         color: "always-white",
-        children: null == T ? void 0 : T.receivedCopy.format({
-          item: null == T ? void 0 : T.name
+        children: null == B ? void 0 : B.receivedCopy.format({
+          item: null == B ? void 0 : B.name
         })
-      }), (0, r.jsx)(a.Clickable, {
+      }), (0, r.jsx)(o.Clickable, {
         className: g || v ? m.animationClickableOpening : m.animationClickableWaiting,
         onClick: g || v ? void 0 : P,
-        children: (0, r.jsx)(u.default, {
+        children: (0, r.jsx)(a.default, {
           className: m.animation,
-          importData: s.importLootboxAnimationData,
-          nextScene: C,
+          importData: c.importLootboxAnimationData,
+          nextScene: A,
           sceneSegments: d.BoxAnimationSceneSegments,
-          onScenePlay: R,
-          onEnterFrame: M,
-          pause: k
+          onScenePlay: M,
+          onEnterFrame: F,
+          pause: I
         })
       }), (0, r.jsx)("audio", {
-        ref: B,
+        ref: D,
         src: p,
         preload: "auto"
       }), (0, r.jsx)("audio", {
-        ref: D,
+        ref: w,
         src: f,
         preload: "auto",
         loop: !0
       }), (0, r.jsx)("audio", {
-        ref: w,
+        ref: j,
         src: x,
         preload: "auto"
       }), (0, r.jsx)("audio", {
-        ref: j
+        ref: y
       })]
     })
   })
