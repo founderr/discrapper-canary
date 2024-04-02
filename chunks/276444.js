c238f4bb1c8caf4396fa766456078596199471a5
DiscrapperManager <admin@minerpl.xyz>
2024-04-02 20:29:12 +0200

    canaryRelease: 280847
    Main scripts: 21
    Lazy Loaded Scripts: 822
    Chunks:
     * Main Files: 24382
     * Scripts: 27217
    Strings: 17460
    Routes: 734
    Experiments Count: 320

--- chunks/276444.js
+++ chunks/276444.js
@@ -17,20 +17,21 @@ let E = null,
   N = 0,
   O = null,
   p = [],
-  R = () => !0;
+  R = !1,
+  C = () => !0;
 
-function C(e) {
+function g(e) {
   h.add(e)
 }
 
-function g(e) {
+function L(e) {
   let {
     messages: t
   } = e;
-  t.forEach(e => L(e))
+  t.forEach(e => D(e))
 }
 
-function L(e) {
+function D(e) {
   let t = e.type === o.MessageTypes.PREMIUM_REFERRAL ? e.content : null;
   if (null == t) return !1;
   if (!A.has(t) && !h.has(t)) {
@@ -38,9 +39,9 @@ function L(e) {
     n = t, h.add(n), u.default.wait(() => (0, _.resolveReferralTrialOffer)(t).catch(c.NOOP_NULL))
   }
 }
-class D extends(i = l.default.Store) {
+class v extends(i = l.default.Store) {
   initialize() {
-    this.waitFor(d.default), this.syncWith([d.default], R)
+    this.waitFor(d.default), this.syncWith([d.default], C)
   }
   checkAndFetchReferralsRemaining() {
     null == E && !S && N < 5 && (null == O || O < Date.now()) && (0, _.fetchReferralsRemaining)()
@@ -69,13 +70,16 @@ class D extends(i = l.default.Store) {
   getEligibleUsers() {
     return p
   }
+  getFetchingEligibleUsers() {
+    return R
+  }
 }
-a = "ReferralTrialStore", (s = "displayName") in(r = D) ? Object.defineProperty(r, s, {
+a = "ReferralTrialStore", (s = "displayName") in(r = v) ? Object.defineProperty(r, s, {
   value: a,
   enumerable: !0,
   configurable: !0,
   writable: !0
-}) : r[s] = a, t.default = new D(u.default, {
+}) : r[s] = a, t.default = new v(u.default, {
   BILLING_REFERRAL_TRIAL_OFFER_UPDATE: function(e) {
     let {
       userTrialOfferId: t,
@@ -138,15 +142,27 @@ a = "ReferralTrialStore", (s = "displayName") in(r = D) ? Object.defineProperty(
     } = e;
     h.delete(t), A.add(t)
   },
-  LOAD_MESSAGES_SUCCESS: g,
+  REFERRALS_FETCH_ELIGIBLE_USER_START: function() {
+    R = !0
+  },
+  REFERRALS_FETCH_ELIGIBLE_USER_SUCCESS: function(e) {
+    let {
+      users: t
+    } = e;
+    R = !1, p = t
+  },
+  REFERRALS_FETCH_ELIGIBLE_USER_FAIL: function() {
+    R = !1
+  },
+  LOAD_MESSAGES_SUCCESS: L,
   MESSAGE_CREATE: function(e) {
     let {
       message: t
     } = e;
-    L(t)
+    D(t)
   },
-  LOAD_MESSAGES_AROUND_SUCCESS: g,
+  LOAD_MESSAGES_AROUND_SUCCESS: L,
   LOGOUT: function() {
-    E = null, I = {}, T = [], f = new Set, S = !1, h = new Set, A = new Set, m = {}, N = 0, O = null, p = []
+    E = null, I = {}, T = [], f = new Set, S = !1, h = new Set, A = new Set, m = {}, N = 0, O = null, p = [], R = !1
   }
 })
