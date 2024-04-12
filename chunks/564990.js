6fffdcf2d13b2ec4ccf5ac115fd1a78a7bd2eb18
DiscrapperManager <admin@minerpl.xyz>
2024-04-13 01:41:51 +0200

    canaryRelease: 284369
    Main scripts: 21
    Lazy Loaded Scripts: 825
    Chunks:
     * Main Files: 24592
     * Scripts: 27335
    Strings: 17618
    Routes: 733
    Experiments Count: 323

--- chunks/564990.js
+++ chunks/564990.js
@@ -9,9 +9,15 @@ var i = n("544891"),
   s = n("981631");
 let a = async () => {
   try {
-    return (await i.HTTP.get({
-      url: s.Endpoints.MY_CONTENT_INVENTORY
-    })).body
+    let e = (await i.HTTP.get({
+        url: s.Endpoints.MY_CONTENT_INVENTORY
+      })).body,
+      t = e.wait_ms_until_next_fetch;
+    if (null != t) {
+      let n = new Date(Date.now() + t);
+      e.expired_at = n.toISOString()
+    }
+    return e
   } catch (e) {
     throw new r.APIError(e)
   }
