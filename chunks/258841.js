0d90d1b49e08d508a53cf5dadbb2ee7b79832613
DiscrapperManager <admin@minerpl.xyz>
2024-04-15 19:03:07 +0200

    canaryRelease: 284512
    Main scripts: 21
    Lazy Loaded Scripts: 825
    Chunks:
     * Main Files: 24625
     * Scripts: 27339
    Strings: 17620
    Routes: 733
    Experiments Count: 325

--- chunks/258841.js
+++ chunks/258841.js
@@ -1,47 +1,53 @@
 "use strict";
 a.r(t), a.d(t, {
   default: function() {
-    return c
+    return f
   }
 });
 var n = a("735250"),
   s = a("470079"),
   l = a("481060"),
   i = a("725568"),
-  r = a("603839"),
-  o = a("308083"),
-  u = a("689938"),
-  d = a("290305");
+  r = a("963202"),
+  o = a("603839"),
+  u = a("308083"),
+  d = a("689938"),
+  c = a("290305");
 
-function c(e) {
+function f(e) {
   let {
     guildId: t
-  } = e, a = s.useCallback(() => {
+  } = e, {
+    enableClanCreation: a,
+    defaultGameId: f
+  } = r.ClanExperiment.useExperiment({
+    location: "ClanUpsellNotice"
+  }), E = s.useCallback(() => {
     (0, i.openFullScreenLayer)(e => {
       let {
         closeLayer: a
       } = e;
-      return (0, n.jsx)(r.default, {
+      return (0, n.jsx)(o.default, {
         onClose: a,
         guildId: t,
-        gameId: o.VALORANT_ID
+        gameId: f
       })
     }, {
-      layerKey: o.CLAN_SETUP_MODAL_LAYER_KEY
+      layerKey: u.CLAN_SETUP_MODAL_LAYER_KEY
     })
-  }, [t]);
-  return (0, n.jsxs)(l.Clickable, {
-    onClick: a,
-    className: d.upsellContainer,
+  }, [f, t]);
+  return a ? (0, n.jsxs)(l.Clickable, {
+    onClick: E,
+    className: c.upsellContainer,
     children: [(0, n.jsx)(l.Text, {
       variant: "text-xs/bold",
       color: "text-warning",
-      className: d.exclusive,
-      children: u.default.Messages.CLAN_UPSELL_INVITE
+      className: c.exclusive,
+      children: d.default.Messages.CLAN_UPSELL_INVITE
     }), (0, n.jsx)(l.Text, {
       variant: "text-sm/semibold",
       color: "header-primary",
-      children: u.default.Messages.CLAN_UPSELL_CONTENT
+      children: f === u.VALORANT_ID ? d.default.Messages.CLAN_UPSELL_CONTENT_VALORANT : f === u.GENSHIN_ID ? d.default.Messages.CLAN_UPSELL_CONTENT_GENSHIN : d.default.Messages.CLAN_UPSELL_CONTENT
     })]
-  })
+  }) : null
 }
