e87d0178c7a91097ec26083db4bb2ce93f71fe45
DiscrapperManager <admin@minerpl.xyz>
2024-04-16 22:31:11 +0200

    canaryRelease: 285050
    Main scripts: 21
    Lazy Loaded Scripts: 816
    Chunks:
     * Main Files: 24940
     * Scripts: 27826
    Strings: 17621
    Routes: 733
    Experiments Count: 318

--- chunks/20186.js
+++ chunks/20186.js
@@ -1,11 +1,18 @@
 "use strict";
 n.r(t), n.d(t, {
-  getGlobalTagsArray: function() {
+  addGlobalTag: function() {
     return r
+  },
+  getGlobalTagsArray: function() {
+    return s
   }
 });
 let i = {};
 
-function r() {
+function r(e, t) {
+  i[e] = t
+}
+
+function s() {
   return Object.keys(i).map(e => "".concat(e, ":").concat(i[e]))
 }
