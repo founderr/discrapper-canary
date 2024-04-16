9bb1a1fd9f46844b063fefcad8ca86c8afdb6542
DiscrapperManager <admin@minerpl.xyz>
2024-04-16 23:17:49 +0200

    canaryRelease: 285108
    Main scripts: 21
    Lazy Loaded Scripts: 816
    Chunks:
     * Main Files: 24940
     * Scripts: 27830
    Strings: 17621
    Routes: 733
    Experiments Count: 319

--- chunks/523147.js
+++ chunks/523147.js
@@ -1,13 +1,9 @@
 "use strict";
 n.r(t);
-var i = n("594174"),
-  o = n("585483"),
-  l = n("981631");
+var i = n("585483"),
+  o = n("981631");
 t.default = {
   binds: ["mod+shift+b"],
   comboKeysBindGlobal: !0,
-  action() {
-    let e = i.default.getCurrentUser();
-    return (null == e ? void 0 : e.isStaff()) === !0 && (o.ComponentDispatch.dispatch(l.ComponentActions.TOGGLE_SOUNDBOARD), !1)
-  }
+  action: () => (i.ComponentDispatch.dispatch(o.ComponentActions.TOGGLE_SOUNDBOARD), !1)
 }
