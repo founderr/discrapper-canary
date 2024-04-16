c7225d7c50b81138381eca5cb5594685177a5011
DiscrapperManager <admin@minerpl.xyz>
2024-04-16 21:24:55 +0200

    canaryRelease: 285017
    Main scripts: 21
    Lazy Loaded Scripts: 816
    Chunks:
     * Main Files: 24946
     * Scripts: 27826
    Strings: 17621
    Routes: 733
    Experiments Count: 318

--- chunks/480222.js
+++ chunks/480222.js
@@ -5,27 +5,33 @@ var n = a("735250"),
   l = a("143927"),
   i = a("481060"),
   r = a("207796"),
-  o = a("689938"),
-  u = a("860484");
+  o = a("420212"),
+  u = a("689938"),
+  d = a("860484");
 t.default = function(e) {
   let {
     children: t
-  } = e, a = (0, r.useClanDiscoveryUIStore)(e => e.setMode, l.default), d = s.useCallback(() => {
+  } = e, a = (0, r.useClanDiscoveryUIStore)(e => e.setMode, l.default), c = s.useCallback(() => {
     a(r.ClanDiscoveryMode.DISCOVERY)
   }, [a]);
-  return (0, n.jsxs)("div", {
-    className: u.container,
+  return s.useEffect(() => {
+    let e = e => {
+      e.key === o.KeyboardKeysUpdated.ESCAPE && a(r.ClanDiscoveryMode.DISCOVERY)
+    };
+    return window.addEventListener("keydown", e), () => window.removeEventListener("keydown", e)
+  }, [a]), (0, n.jsxs)("div", {
+    className: d.container,
     children: [(0, n.jsx)("div", {
-      className: u.saveContainer,
+      className: d.saveContainer,
       children: (0, n.jsx)(i.Button, {
-        onClick: d,
+        onClick: c,
         look: i.ButtonLooks.OUTLINED,
         color: i.ButtonColors.PRIMARY,
-        children: o.default.Messages.SAVE
+        children: u.default.Messages.SAVE
       })
     }), (0, n.jsx)(i.Scroller, {
       fade: !0,
-      className: u.content,
+      className: d.content,
       children: t
     })]
   })
