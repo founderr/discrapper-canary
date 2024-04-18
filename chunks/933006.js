c63098ae63ea38df09f505116b86157860d01529
DiscrapperManager <admin@minerpl.xyz>
2024-04-19 00:37:49 +0200

    canaryRelease: 286028
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25015
     * Scripts: 27527
    Strings: 17656
    Routes: 733
    Experiments Count: 320

--- chunks/933006.js
+++ chunks/933006.js
@@ -1,72 +1,71 @@
 "use strict";
 s.r(t), s.d(t, {
   default: function() {
-    return d
+    return O
   }
 }), s("47120");
 var a = s("735250"),
   n = s("470079"),
   r = s("481060"),
-  u = s("2052"),
-  i = s("906732"),
-  l = s("626135"),
-  E = s("905661"),
-  _ = s("940165"),
-  o = s("474936"),
-  S = s("981631"),
-  M = s("677804");
+  _ = s("230711"),
+  l = s("2052"),
+  u = s("906732"),
+  E = s("174609"),
+  i = s("626135"),
+  S = s("790527"),
+  o = s("940165"),
+  M = s("474936"),
+  d = s("981631"),
+  I = s("689938"),
+  T = s("677804");
 
-function d(e) {
+function O(e) {
   let {
-    transitionState: t,
-    onClose: d
-  } = e, c = (0, u.useAnalyticsContext)(), {
-    analyticsLocations: I
-  } = (0, i.default)(), [T, N] = n.useState(!1);
+    onClose: t,
+    ...O
+  } = e, N = (0, l.useAnalyticsContext)(), {
+    analyticsLocations: c
+  } = (0, u.default)(), [A, g] = n.useState(!1);
   return n.useEffect(() => {
-    !T && (l.default.track(S.AnalyticEvents.PREMIUM_UPSELL_VIEWED, {
-      type: o.PremiumUpsellTypes.CUSTOM_NOTIFICATION_SOUNDS_UPSELL,
-      location: c.location,
-      location_stack: I
-    }), N(!0))
-  }, [c.location, I, T]), (0, a.jsxs)(r.ModalRoot, {
-    transitionState: t,
-    children: [(0, a.jsx)(r.ModalCloseButton, {
-      className: M.close,
-      onClick: d
-    }), (0, a.jsx)("img", {
-      alt: "",
-      src: s("98165"),
-      className: M.image
-    }), (0, a.jsxs)("div", {
-      className: M.bodyContainer,
-      children: [(0, a.jsx)(r.Heading, {
-        variant: "heading-xl/bold",
-        className: M.heading,
-        children: "Hear that? Set the tone with custom notification sounds!"
-      }), (0, a.jsx)(r.FormItem, {
-        title: "Try it out!",
-        className: M.optionsContainerWrapper,
-        children: (0, a.jsxs)("div", {
-          className: M.optionsContainer,
-          children: [(0, a.jsx)(_.default, {
-            showGradient: !0
-          }), (0, a.jsx)(_.default, {
-            showGradient: !1
-          }), (0, a.jsx)(_.default, {
-            showGradient: !1
-          }), (0, a.jsx)(_.default, {
-            showGradient: !1
-          })]
-        })
-      }), (0, a.jsx)(r.Text, {
-        variant: "text-xs/normal",
-        className: M.description,
-        children: "Access custom notification sounds, HD streaming, personalized profile, and more with Nitro."
-      })]
-    }), (0, a.jsx)(E.default, {
-      onClose: d,
-      section: S.AnalyticsSections.PREMIUM_CUSTOM_NOTIFICATION_SOUND_UPSELL_MODAL
-    })]
+    !A && (i.default.track(d.AnalyticEvents.PREMIUM_UPSELL_VIEWED, {
+      type: M.PremiumUpsellTypes.CUSTOM_NOTIFICATION_SOUNDS_UPSELL,
+      location: N.location,
+      location_stack: c
+    }), g(!0))
+  }, [N.location, c, A]), (0, a.jsx)(S.default, {
+    type: M.PremiumUpsellTypes.CUSTOM_NOTIFICATION_SOUNDS_UPSELL,
+    artContainerClassName: T.image,
+    artURL: s("98165"),
+    title: I.default.Messages.PREMIUM_CUSTOM_NOTIFICATION_SOUND_ROADBLOCK_TITLE,
+    glowUp: I.default.Messages.PREMIUM_CUSTOM_NOTIFICATION_SOUND_ROADBLOCK_TITLE,
+    body: I.default.Messages.PREMIUM_CUSTOM_NOTIFICATION_SOUND_ROADBLOCK_DESCRIPTION,
+    showEnhancedUpsell: !0,
+    analyticsLocation: {
+      section: d.AnalyticsSections.PREMIUM_CUSTOM_NOTIFICATION_SOUND_UPSELL_MODAL
+    },
+    enableArtBoxShadow: !1,
+    onSubscribeClick: E.default,
+    secondaryCTA: I.default.Messages.PREMIUM_ROADBLOCK_UPSELL_SECONDARY_CTA,
+    onSecondaryClick: () => {
+      t(), _.default.open(d.UserSettingsSections.PREMIUM)
+    },
+    onClose: t,
+    ...O,
+    children: (0, a.jsx)(r.FormItem, {
+      title: I.default.Messages.PREMIUM_CUSTOM_NOTIFICATION_SOUND_ROADBLOCK_LABEL,
+      className: T.optionsContainerWrapper,
+      children: (0, a.jsxs)("div", {
+        className: T.optionsContainer,
+        children: [(0, a.jsx)(o.default, {
+          showGradient: !0
+        }), (0, a.jsx)(o.default, {
+          showGradient: !1
+        }), (0, a.jsx)(o.default, {
+          showGradient: !1
+        }), (0, a.jsx)(o.default, {
+          showGradient: !1
+        })]
+      })
+    })
   })
 }
--- chunks/944411.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-e.exports = {
-  nitroButton: "nitroButton_fd99cd",
-  perksButton: "perksButton__84e9c",
-  spacer: "spacer_a49b59"
-}
