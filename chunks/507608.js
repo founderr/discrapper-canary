b26c2988ac38b795829f09211f1ec142b6351da0
DiscrapperManager <admin@minerpl.xyz>
2024-04-19 22:50:13 +0200

    canaryRelease: 286445
    Main scripts: 21
    Lazy Loaded Scripts: 825
    Chunks:
     * Main Files: 25063
     * Scripts: 27733
    Strings: 17669
    Routes: 734
    Experiments Count: 322

--- chunks/507608.js
+++ chunks/507608.js
@@ -1,28 +1,28 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return B
+    return O
   }
 });
 var i = n("735250"),
   r = n("470079"),
-  s = n("905322"),
-  l = n("512100"),
+  l = n("905322"),
+  s = n("512100"),
   a = n("200207"),
   o = n("269210"),
   c = n("752843"),
   d = n("368072"),
   u = n("790472"),
   f = n("442837"),
-  h = n("481060"),
-  m = n("607070"),
+  m = n("481060"),
+  h = n("607070"),
   p = n("100527"),
   S = n("906732"),
   v = n("87484"),
   g = n("171246"),
   x = n("889989"),
-  C = n("696906"),
-  T = n("551428"),
+  T = n("696906"),
+  C = n("551428"),
   j = n("768581"),
   N = n("937615"),
   L = n("73346"),
@@ -34,15 +34,15 @@ var i = n("735250"),
   b = n("689938"),
   A = n("836092");
 
-function B(e) {
+function O(e) {
   var t, n;
   let {
-    app: l,
+    app: s,
     subscriptionGroupListing: a,
     guildId: o,
     products: c
   } = e, d = null !== (n = null == a ? void 0 : null === (t = a.subscription_listings) || void 0 === t ? void 0 : t.filter(e => e.published)) && void 0 !== n ? n : [], u = r.useMemo(() => {
-    let e = l.getIconSource(100);
+    let e = s.getIconSource(100);
     if (null != e && "number" != typeof e) {
       let n;
       if (Array.isArray(e) ? e.length > 0 && (n = e[0].uri) : n = e.uri, null != n) {
@@ -50,9 +50,9 @@ function B(e) {
         return null !== (t = I.default.toURLSafe(n)) && void 0 !== t ? t : void 0
       }
     }
-  }, [l]), {
+  }, [s]), {
     bot: p
-  } = l, S = r.useMemo(() => {
+  } = s, S = r.useMemo(() => {
     var e;
     if ((null == p ? void 0 : p.banner) == null) return;
     let t = (0, j.getUserBannerURL)({
@@ -72,28 +72,25 @@ function B(e) {
       canAnimate: !0
     });
     return null != t && null !== (e = I.default.toURLSafe(t)) && void 0 !== e ? e : void 0
-  }, [p]), x = (0, f.useStateFromStores)([m.default], () => m.default.useReducedMotion);
+  }, [p]), x = (0, f.useStateFromStores)([h.default], () => h.default.useReducedMotion);
   return (0, i.jsxs)("div", {
     className: A.wrapper,
-    children: [(0, i.jsx)("div", {
-      className: A.banner,
-      children: (0, i.jsx)(s.Banner, {
-        title: l.name,
-        iconSrc: u,
-        backgroundSrc: S,
-        animatedBackgroundSrc: v,
-        prefersReducedMotion: x
-      })
+    children: [(0, i.jsx)(l.Banner, {
+      title: s.name,
+      iconSrc: u,
+      backgroundSrc: S,
+      animatedBackgroundSrc: v,
+      prefersReducedMotion: x
     }), d.length > 0 && null != a && (0, i.jsxs)("div", {
       className: A.productSection,
-      children: [(0, i.jsx)(h.Heading, {
+      children: [(0, i.jsx)(m.Heading, {
         variant: "heading-lg/semibold",
         color: "header-secondary",
         children: b.default.Messages.STOREFRONT_APP_SUBSCRIPTIONS
       }), (0, i.jsx)("div", {
         className: A.productList,
         children: d.map(e => (0, i.jsx)(M, {
-          appId: l.id,
+          appId: s.id,
           groupListingId: a.id,
           guildId: o,
           listing: e,
@@ -102,17 +99,24 @@ function B(e) {
       })]
     }), c.length > 0 && (0, i.jsxs)("div", {
       className: A.productSection,
-      children: [(0, i.jsx)(h.Heading, {
+      children: [(0, i.jsx)(m.Heading, {
         variant: "heading-lg/semibold",
         color: "header-secondary",
         children: b.default.Messages.STOREFRONT_APP_PRODUCTS
       }), (0, i.jsx)("div", {
         className: A.productList,
-        children: c.map(e => (0, i.jsx)(O, {
+        children: c.map(e => (0, i.jsx)(B, {
           sku: e,
-          appId: l.id
+          appId: s.id
         }, e.id))
       })]
+    }), (0, i.jsx)(m.Text, {
+      variant: "text-md/normal",
+      className: A.legalText,
+      children: null != s.termsOfServiceUrl && null != s.privacyPolicyUrl ? b.default.Messages.STOREFRONT_VIEW_TOS_PP.format({
+        tosUrl: s.termsOfServiceUrl,
+        ppUrl: s.privacyPolicyUrl
+      }) : b.default.Messages.STOREFRONT_NO_TOS_PP
     })]
   })
 }
@@ -121,18 +125,18 @@ function M(e) {
   let {
     appId: t,
     groupListingId: n,
-    guildId: s,
+    guildId: l,
     listing: a,
     groupListingType: u
   } = e, {
     openModal: f
-  } = (0, C.default)({
+  } = (0, T.default)({
     listing: a,
-    guildId: s,
+    guildId: l,
     groupListingId: n,
     showBenefitsFirst: !1,
     analyticsLocation: R.AnalyticsLocations.BOT_PROFILE_POPOUT
-  }), m = r.useMemo(() => {
+  }), h = r.useMemo(() => {
     var e;
     return null === (e = a.store_listing_benefits) || void 0 === e ? void 0 : e.map(e => ({
       id: e.id,
@@ -146,9 +150,9 @@ function M(e) {
   }, [t, a.image_asset]), v = a.subscription_plans.length > 0 ? a.subscription_plans[0] : null;
   if (null === v) return null;
   let g = "guild" === u || "user" === u && !p,
-    T = e => {
+    C = e => {
       var t;
-      return g ? (0, i.jsx)(h.Button, {
+      return g ? (0, i.jsx)(m.Button, {
         onClick: null !== (t = e.onClick) && void 0 !== t ? t : f,
         className: e.className,
         children: b.default.Messages.STOREFRONT_SUBSCRIBE_FOR.format({
@@ -165,23 +169,23 @@ function M(e) {
     imgSrc: S,
     subscriptionType: u,
     onPurchase: f,
-    PurchaseButton: T,
-    benefitItems: null != m ? m.map(e => (0, i.jsx)(o.SubscriptionCardBenefit, {
+    PurchaseButton: C,
+    benefitItems: null != h ? h.map(e => (0, i.jsx)(o.SubscriptionCardBenefit, {
       name: e.title,
       icon: e.icon
     }, e.id)) : void 0,
-    onDetails: null != m ? () => {
-      (0, h.openModal)(e => {
+    onDetails: null != h ? () => {
+      (0, m.openModal)(e => {
         let {
           onClose: t,
           transitionState: n
         } = e;
-        return (0, i.jsxs)(h.ModalRoot, {
+        return (0, i.jsxs)(m.ModalRoot, {
           transitionState: n,
-          size: h.ModalSize.DYNAMIC,
+          size: m.ModalSize.DYNAMIC,
           className: A.subDetailsModal,
-          children: [(0, i.jsx)(l.CardDetails, {
-            benefits: m.map(e => (0, i.jsx)(o.SubscriptionCardBenefit, {
+          children: [(0, i.jsx)(s.CardDetails, {
+            benefits: h.map(e => (0, i.jsx)(o.SubscriptionCardBenefit, {
               name: e.title,
               icon: e.icon,
               description: e.description
@@ -194,19 +198,19 @@ function M(e) {
             }),
             onClose: t,
             FallbackIcon: d.BadgeIcon
-          }), (0, i.jsxs)(h.ModalFooter, {
+          }), (0, i.jsxs)(m.ModalFooter, {
             className: A.footer,
-            children: [(0, i.jsx)(h.Button, {
+            children: [(0, i.jsx)(m.Button, {
               className: A.backButton,
               onClick: t,
-              look: h.ButtonLooks.BLANK,
-              children: (0, i.jsx)(h.Text, {
+              look: m.ButtonLooks.BLANK,
+              children: (0, i.jsx)(m.Text, {
                 color: "header-primary",
                 tag: "p",
                 variant: "heading-md/semibold",
                 children: b.default.Messages.BACK
               })
-            }), (0, i.jsx)(T, {})]
+            }), (0, i.jsx)(C, {})]
           })]
         })
       })
@@ -214,31 +218,31 @@ function M(e) {
   })
 }
 
-function O(e) {
+function B(e) {
   var t, n, r;
   let {
-    sku: s,
+    sku: l,
     appId: o
   } = e, {
     analyticsLocations: d
-  } = (0, S.default)(p.default.APP_STOREFRONT), m = (0, f.useStateFromStores)([T.default], () => T.default.getForSKU(s.id), [s]), g = s.name, x = null !== (n = null == m ? void 0 : null === (t = m.description) || void 0 === t ? void 0 : t.trim()) && void 0 !== n ? n : void 0, C = (null == m ? void 0 : m.headerBackground) != null && null !== (r = I.default.toURLSafe((0, L.getAssetURL)(o, m.headerBackground, 256))) && void 0 !== r ? r : void 0, j = (0, _.useHasActiveEntitlement)(s.id), y = s.type === R.SKUTypes.DURABLE && j, B = s.type === R.SKUTypes.DURABLE ? y ? b.default.Messages.STOREFRONT_DURABLE_AVAILABILITY_NONE : b.default.Messages.STOREFRONT_DURABLE_AVAILABILITY : void 0, {
+  } = (0, S.default)(p.default.APP_STOREFRONT), h = (0, f.useStateFromStores)([C.default], () => C.default.getForSKU(l.id), [l]), g = l.name, x = null !== (n = null == h ? void 0 : null === (t = h.description) || void 0 === t ? void 0 : t.trim()) && void 0 !== n ? n : void 0, T = (null == h ? void 0 : h.headerBackground) != null && null !== (r = I.default.toURLSafe((0, L.getAssetURL)(o, h.headerBackground, 256))) && void 0 !== r ? r : void 0, j = (0, _.useHasActiveEntitlement)(l.id), y = l.type === R.SKUTypes.DURABLE && j, O = l.type === R.SKUTypes.DURABLE ? y ? b.default.Messages.STOREFRONT_DURABLE_AVAILABILITY_NONE : b.default.Messages.STOREFRONT_DURABLE_AVAILABILITY : void 0, {
     price: M
-  } = s;
+  } = l;
   if (null == M) return null;
-  let O = () => {
+  let B = () => {
       (0, v.default)({
         applicationId: o,
-        skuId: s.id,
+        skuId: l.id,
         analyticsLocations: d
       })
     },
-    F = e => {
+    P = e => {
       var t;
       return y ? (0, i.jsx)(U, {
         className: e.className,
         children: b.default.Messages.COLLECTIBLES_ALREADY_OWNED
-      }) : (0, i.jsx)(h.Button, {
-        onClick: null !== (t = e.onClick) && void 0 !== t ? t : O,
+      }) : (0, i.jsx)(m.Button, {
+        onClick: null !== (t = e.onClick) && void 0 !== t ? t : B,
         className: e.className,
         children: b.default.Messages.STOREFRONT_PURCHASE_FOR.format({
           price: (0, N.formatPrice)(M.amount, M.currency)
@@ -248,46 +252,46 @@ function O(e) {
   return (0, i.jsx)(a.ProductCard, {
     title: g,
     description: x,
-    headerImage: C,
-    availabilityLabel: B,
+    headerImage: T,
+    availabilityLabel: O,
     onDetails: (0, E.isNullOrEmpty)(x) ? void 0 : () => {
-      (0, h.openModal)(e => {
+      (0, m.openModal)(e => {
         let {
           onClose: t,
           transitionState: n
         } = e;
-        return (0, i.jsxs)(h.ModalRoot, {
+        return (0, i.jsxs)(m.ModalRoot, {
           transitionState: n,
-          size: h.ModalSize.DYNAMIC,
+          size: m.ModalSize.DYNAMIC,
           className: A.subDetailsModal,
-          children: [(0, i.jsx)(l.CardDetails, {
+          children: [(0, i.jsx)(s.CardDetails, {
             title: g,
             description: x,
-            imgSrc: C,
-            tag: null != B ? (0, i.jsx)(c.Tag, {
-              text: B
+            imgSrc: T,
+            tag: null != O ? (0, i.jsx)(c.Tag, {
+              text: O
             }) : void 0,
             onClose: t,
             FallbackIcon: u.TicketIcon
-          }), (0, i.jsxs)(h.ModalFooter, {
+          }), (0, i.jsxs)(m.ModalFooter, {
             className: A.footer,
-            children: [(0, i.jsx)(h.Button, {
+            children: [(0, i.jsx)(m.Button, {
               className: A.backButton,
               onClick: t,
-              look: h.ButtonLooks.BLANK,
-              children: (0, i.jsx)(h.Text, {
+              look: m.ButtonLooks.BLANK,
+              children: (0, i.jsx)(m.Text, {
                 color: "header-primary",
                 tag: "p",
                 variant: "heading-md/semibold",
                 children: b.default.Messages.BACK
               })
-            }), (0, i.jsx)(F, {})]
+            }), (0, i.jsx)(P, {})]
           })]
         })
       })
     },
-    PurchaseButton: F,
-    onPurchase: O
+    PurchaseButton: P,
+    onPurchase: B
   })
 }
 
@@ -296,12 +300,12 @@ function U(e) {
     className: t,
     children: n
   } = e;
-  return (0, i.jsx)(h.Button, {
+  return (0, i.jsx)(m.Button, {
     disabled: !0,
     className: t,
-    look: h.Button.Looks.OUTLINED,
-    color: h.Button.Colors.PRIMARY,
-    size: h.Button.Sizes.SMALL,
+    look: m.Button.Looks.OUTLINED,
+    color: m.Button.Colors.PRIMARY,
+    size: m.Button.Sizes.SMALL,
     children: n
   })
 }
