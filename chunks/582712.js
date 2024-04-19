61a441cbb6828df40c93037043472f61edebc69a
DiscrapperManager <admin@minerpl.xyz>
2024-04-19 16:55:15 +0200

    canaryRelease: 286210
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25024
     * Scripts: 27438
    Strings: 17652
    Routes: 734
    Experiments Count: 322

--- chunks/582712.js
+++ chunks/582712.js
@@ -4,16 +4,16 @@ a.r(t), a.d(t, {
     return f
   }
 }), a("47120");
-var s = a("735250"),
-  n = a("470079"),
+var n = a("735250"),
+  s = a("470079"),
   l = a("442837"),
   i = a("481060"),
   u = a("920440"),
-  d = a("695346"),
-  r = a("430824"),
+  r = a("695346"),
+  d = a("430824"),
   o = a("346656"),
-  S = a("88658"),
-  c = a("401416"),
+  c = a("88658"),
+  S = a("401416"),
   T = a("689938"),
   _ = a("92522"),
   N = a("444812");
@@ -23,71 +23,72 @@ function f(e) {
     transitionState: t,
     guildId: f,
     onClose: g
-  } = e, I = (0, l.useStateFromStores)([r.default], () => r.default.getGuild(f));
-  return (n.useEffect(() => {
-    (0, c.acknowledgeDmSettingsUpsell)(f)
-  }, [f]), null == I) ? null : (0, s.jsx)(i.ModalRoot, {
+  } = e, I = (0, l.useStateFromStores)([d.default], () => d.default.getGuild(f));
+  return (s.useEffect(() => {
+    (0, S.acknowledgeDmSettingsUpsell)(f)
+  }, [f]), null == I) ? null : (0, n.jsx)(i.ModalRoot, {
     transitionState: t,
-    children: (0, s.jsxs)("form", {
+    className: _.modal,
+    children: (0, n.jsxs)("form", {
       onSubmit: e => {
         e.preventDefault();
-        let t = new Set((0, S.getSanitizedRestrictedGuilds)());
-        t.add(f), d.RestrictedGuildIds.updateSetting(Array.from(t)).then(() => (0, i.showToast)((0, i.createToast)(T.default.Messages.DM_SETTINGS_UPSELL_SUCCESS_TOAST, i.ToastType.SUCCESS))), g()
+        let t = new Set((0, c.getSanitizedRestrictedGuilds)());
+        t.add(f), r.RestrictedGuildIds.updateSetting(Array.from(t)).then(() => (0, i.showToast)((0, i.createToast)(T.default.Messages.DM_SETTINGS_UPSELL_SUCCESS_TOAST, i.ToastType.SUCCESS))), g()
       },
-      children: [(0, s.jsx)("img", {
+      children: [(0, n.jsx)("img", {
         alt: "",
         className: _.headerImage,
         src: N
-      }), (0, s.jsxs)(i.ModalHeader, {
+      }), (0, n.jsxs)(i.ModalHeader, {
         separator: !1,
         className: _.header,
-        children: [(0, s.jsx)(i.Heading, {
+        children: [(0, n.jsx)(i.Heading, {
           variant: "heading-lg/bold",
           className: _.title,
           children: T.default.Messages.DM_SETTINGS_UPSELL_TITLE
-        }), (0, s.jsx)(i.Text, {
+        }), (0, n.jsx)(i.Text, {
           variant: "text-md/normal",
+          color: "header-secondary",
           className: _.body,
           children: T.default.Messages.DM_SETTINGS_UPSELL_BODY.format({
             guild_name: I.name
           })
-        }), (0, s.jsx)(i.ModalCloseButton, {
-          onClick: g,
-          className: _.modalCloseButton
         })]
-      }), (0, s.jsxs)(i.ModalContent, {
-        children: [(0, s.jsx)(i.Text, {
-          variant: "text-sm/bold",
+      }), (0, n.jsxs)(i.ModalContent, {
+        children: [(0, n.jsx)(i.Text, {
+          variant: "eyebrow",
+          color: "header-secondary",
           className: _.subtitle,
           children: T.default.Messages.DM_SETTINGS_UPSELL_SUBTITLE
-        }), (0, s.jsx)("div", {
+        }), (0, n.jsx)("div", {
           className: _.guildContainer,
-          children: (0, s.jsxs)("div", {
+          children: (0, n.jsxs)("div", {
             className: _.guildInfo,
-            children: [(0, s.jsx)(o.default, {
+            children: [(0, n.jsx)(o.default, {
               guild: I,
               size: o.default.Sizes.SMALL
-            }), (0, s.jsx)(i.Text, {
+            }), (0, n.jsx)(i.Text, {
               className: _.guildName,
               variant: "text-md/semibold",
               children: I.name
             })]
           })
-        }), (0, s.jsxs)("div", {
+        }), (0, n.jsxs)("div", {
           className: _.content,
-          children: [(0, s.jsx)(i.Button, {
+          children: [(0, n.jsx)(i.Button, {
             className: _.button,
             type: "submit",
             color: i.Button.Colors.BRAND,
             size: i.Button.Sizes.MEDIUM,
             children: T.default.Messages.DM_SETTINGS_UPSELL_YES_BUTTON
-          }), (0, s.jsx)(i.Button, {
+          }), (0, n.jsx)(i.Button, {
             className: _.button,
             color: i.Button.Colors.PRIMARY,
             onClick: g,
             children: T.default.Messages.DM_SETTINGS_UPSELL_NO_BUTTON
-          }), (0, s.jsx)(i.Text, {
+          }), (0, n.jsx)(i.Text, {
             variant: "text-xs/normal",
+            color: "header-secondary",
             className: _.footer,
             children: T.default.Messages.DM_SETTINGS_UPSELL_FOOTER.format({
               onClick: () => {
@@ -95,7 +96,7 @@ function f(e) {
                   let {
                     default: e
                   } = await Promise.all([a.e("99387"), a.e("59500")]).then(a.bind(a, "241420"));
-                  return t => (0, s.jsx)(e, {
+                  return t => (0, n.jsx)(e, {
                     ...t,
                     guild: I
                   })
