5d6d70b84c9872ce3c5e7de3a99dcbae851952c3
DiscrapperManager <admin@minerpl.xyz>
2024-04-23 23:52:01 +0200

    canaryRelease: 287292
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25136
     * Scripts: 27704
    Strings: 17704
    Routes: 735
    Experiments Count: 325

--- chunks/371991.js
+++ chunks/371991.js
@@ -1,49 +1,78 @@
 "use strict";
-n.r(t), n("47120");
-var a = n("735250"),
-  l = n("470079"),
+n.r(t), n.d(t, {
+  ActiveTimestampFromDuration: function() {
+    return E
+  }
+}), n("47120");
+var i = n("735250"),
+  r = n("470079"),
   s = n("442837"),
-  i = n("846519"),
-  r = n("481060"),
-  o = n("706454"),
-  u = n("506071"),
-  d = n("561308");
+  a = n("846519"),
+  o = n("481060"),
+  l = n("706454"),
+  u = n("709054"),
+  d = n("506071"),
+  _ = n("561308");
 let c = e => {
-  let {
-    entry: t,
-    textColor: n
-  } = e, [s, o] = l.useState(Date.now()), c = (0, u.useIsWindowFocused)(), f = l.useMemo(() => {
     let {
-      hours: e
-    } = (0, d.calculateActiveTimestampDurations)(t, s);
-    return e > 0 ? 3e5 : 1e3
-  }, [t, s]);
-  l.useEffect(() => {
-    let e = new i.Interval;
-    return e.start(f, () => {
-      o(Date.now())
-    }), !c && e.stop(), () => e.stop()
-  }, [f, c]);
-  let h = l.useMemo(() => (0, d.formatActiveTimestamp)(t, s), [t, s]);
-  return (0, a.jsx)(r.Text, {
-    variant: "text-xs/normal",
-    tabularNumbers: !0,
-    color: n,
-    children: h
-  })
-};
+      entry: t,
+      textColor: n
+    } = e, s = u.default.extractTimestamp(t.id), [a, o] = r.useState(Date.now()), {
+      seconds: l,
+      minutes: d,
+      hours: c
+    } = (0, _.calculateActiveTimestampDurations)(t, a);
+    return (0, i.jsx)(E, {
+      textColor: n,
+      startTime: s,
+      seconds: l,
+      minutes: d,
+      hours: c,
+      now: a,
+      setNow: o
+    })
+  },
+  E = e => {
+    let {
+      startTime: t,
+      seconds: n,
+      minutes: s,
+      hours: l,
+      now: u,
+      setNow: c,
+      textColor: E
+    } = e, I = (0, d.useIsWindowFocused)(), T = r.useMemo(() => {
+      let {
+        hours: e
+      } = (0, _.calculateActiveTimestampDurationsFromStart)(t, u);
+      return e > 0 ? 3e5 : 1e3
+    }, [t, u]);
+    r.useEffect(() => {
+      let e = new a.Interval;
+      return e.start(T, () => {
+        c(Date.now())
+      }), !I && e.stop(), () => e.stop()
+    }, [T, I, c]);
+    let f = r.useMemo(() => (0, _.formatActiveTimestampFromUnits)(n, s, l), [n, s, l]);
+    return (0, i.jsx)(o.Text, {
+      variant: "text-xs/normal",
+      tabularNumbers: !0,
+      color: E,
+      children: f
+    })
+  };
 t.default = e => {
   let {
     entry: t,
     textColor: n
-  } = e, l = (0, d.isEntryActive)(t), i = (0, s.useStateFromStores)([o.default], () => o.default.locale);
-  return l ? (0, a.jsx)(c, {
+  } = e, r = (0, _.isEntryActive)(t), a = (0, s.useStateFromStores)([l.default], () => l.default.locale);
+  return r ? (0, i.jsx)(c, {
     entry: t,
     textColor: n
-  }) : (0, a.jsx)(r.Text, {
+  }) : (0, i.jsx)(o.Text, {
     variant: "text-xs/normal",
     color: n,
     lineClamp: 1,
-    children: (0, d.formatEndedTimestamp)(t, i)
+    children: (0, _.formatEndedTimestamp)(t, a)
   })
 }
