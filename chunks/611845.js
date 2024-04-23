f4588207574435b1df514c76d4f4e053aba63f76
DiscrapperManager <admin@minerpl.xyz>
2024-04-23 23:02:38 +0200

    canaryRelease: 287250
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25118
     * Scripts: 27723
    Strings: 17706
    Routes: 735
    Experiments Count: 325

--- chunks/611845.js
+++ chunks/611845.js
@@ -1,88 +1,102 @@
 "use strict";
 n.r(t), n.d(t, {
   StreamButtonDemoPopout: function() {
-    return C
+    return g
   }
-});
+}), n("47120");
 var a = n("735250"),
   l = n("470079"),
   s = n("120356"),
   i = n.n(s),
   r = n("481060"),
-  o = n("170671"),
-  u = n("937995"),
-  d = n("332475"),
-  c = n("465670"),
-  f = n("153850"),
-  h = n("689938"),
-  m = n("324464"),
-  p = n("946790");
+  o = n("570140"),
+  u = n("170671"),
+  d = n("937995"),
+  c = n("332475"),
+  f = n("465670"),
+  h = n("153850"),
+  m = n("689938"),
+  p = n("324464"),
+  E = n("946790");
 
-function E(e) {
+function C(e) {
   let {
     hidden: t,
     onDismiss: n
   } = e;
   return (0, a.jsxs)("div", {
-    className: i()(m.demoCta, {
-      [m.hidden]: t
+    className: i()(p.demoCta, {
+      [p.hidden]: t
     }),
     children: [(0, a.jsxs)("div", {
       children: [(0, a.jsx)("div", {
-        className: m.wumpusContainer,
-        children: (0, a.jsx)(d.default, {
-          src: p,
+        className: p.wumpusContainer,
+        children: (0, a.jsx)(c.default, {
+          src: E,
           width: 72,
           height: 52,
           zoomable: !1
         })
       }), (0, a.jsx)(r.Button, {
-        "aria-label": h.default.Messages.CLOSE,
+        "aria-label": m.default.Messages.CLOSE,
         look: r.Button.Looks.BLANK,
         size: r.Button.Sizes.NONE,
         onClick: () => n(),
-        className: m.closeButton,
-        children: (0, a.jsx)(c.default, {
+        className: p.closeButton,
+        children: (0, a.jsx)(f.default, {
           width: 24,
           height: 24,
-          className: m.closeIcon,
+          className: p.closeIcon,
           color: "white"
         })
       })]
     }), (0, a.jsx)("div", {
-      className: m.textContainer,
+      className: p.textContainer,
       children: (0, a.jsx)(r.Text, {
         variant: "text-md/semibold",
         color: "always-white",
-        children: h.default.Messages.GO_LIVE_MODAL_PERK_DEMO_HD_STREAMING_UPSELL_TOOLTIP
+        children: m.default.Messages.GO_LIVE_MODAL_PERK_DEMO_HD_STREAMING_UPSELL_TOOLTIP
       })
     }), (0, a.jsxs)("div", {
-      className: m.nitroTextAndBadge,
-      children: [(0, a.jsx)(o.default, {
+      className: p.nitroTextAndBadge,
+      children: [(0, a.jsx)(u.default, {
         isSelected: !0
       }), (0, a.jsx)(r.Text, {
         variant: "text-xs/medium",
         color: "always-white",
-        children: h.default.Messages.POWERED_BY_NITRO
+        children: m.default.Messages.POWERED_BY_NITRO
       })]
     })]
   })
 }
 
-function C(e) {
+function g(e) {
   let {
     buttonRef: t,
     dismissed: n,
     onDismiss: s
-  } = e, i = l.useContext(u.IdleStateContext);
-  return (null == t ? void 0 : t.current) == null ? null : (0, a.jsx)(r.Layer, {
-    layerContext: f.appLayerContext,
+  } = e, i = l.useContext(d.IdleStateContext), [u, c] = l.useState(""), [f, m] = l.useState(!1);
+
+  function p(e) {
+    var t;
+    let {
+      settings: n
+    } = e, a = n.callHeaderHeight;
+    c(null !== (t = null == a ? void 0 : a.toString()) && void 0 !== t ? t : ""), m(void 0 !== a && a < 300)
+  }
+  return (l.useEffect(() => (o.default.subscribe("UNSYNCED_USER_SETTINGS_UPDATE", p), () => {
+    o.default.unsubscribe("UNSYNCED_USER_SETTINGS_UPDATE", p)
+  }), []), (null == t ? void 0 : t.current) == null) ? null : (0, a.jsx)(r.Layer, {
+    layerContext: h.appLayerContext,
     children: (0, a.jsx)(r.ReferencePositionLayer, {
       reference: t,
       position: "top",
       align: "center",
-      children: () => (0, a.jsx)(E, {
-        hidden: i || n,
+      spacing: 0,
+      nudgeAlignIntoViewport: !1,
+      positionKey: u,
+      children: () => (0, a.jsx)(C, {
+        hidden: f || i || n,
         onDismiss: s
       })
     })
