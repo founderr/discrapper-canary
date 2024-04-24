cc496b26a1f9814d6b0db6a3840c48b9e319731d
DiscrapperManager <admin@minerpl.xyz>
2024-04-24 22:14:54 +0200

    canaryRelease: 287644
    Main scripts: 21
    Lazy Loaded Scripts: 824
    Chunks:
     * Main Files: 25220
     * Scripts: 27682
    Strings: 17714
    Routes: 736
    Experiments Count: 320

--- chunks/702328.js
+++ chunks/702328.js
@@ -194,7 +194,9 @@ t.default = e => {
         }),
         children: [(0, a.jsx)(O.default, {
           isFullscreen: s
-        }), (0, a.jsx)(P.default, {}), s && eE && (0, a.jsx)(M.default, {
+        }), (0, a.jsx)(P.default, {
+          isFullscreen: s
+        }), s && eE && (0, a.jsx)(M.default, {
           localizedPricingPromo: ec
         }), (0, a.jsx)(o.VisibilitySensor, {
           onChange: e => G(e),
--- chunks/764081.js
+++ chunks/764081.js
@@ -2,74 +2,81 @@
 s.r(t);
 var a = s("735250");
 s("470079");
-var r = s("990547"),
-  i = s("399606"),
-  n = s("481060"),
-  l = s("100527"),
-  o = s("182567"),
-  u = s("906732"),
-  d = s("213609"),
-  c = s("78839"),
-  _ = s("63063"),
-  E = s("958589"),
-  T = s("276800"),
-  I = s("409100"),
-  R = s("474936"),
-  f = s("981631"),
-  S = s("689938"),
-  m = s("810623");
-let A = "April2024MarketingBanner";
-t.default = function() {
-  let e = (0, i.useStateFromStores)([c.default], () => c.default.getPremiumTypeSubscription()),
-    t = E.default.useExperiment({
-      location: A
-    }).enabled,
-    {
-      analyticsLocations: s
-    } = (0, u.default)();
-  if ((0, d.default)({
-      type: r.ImpressionTypes.VIEW,
-      name: r.ImpressionNames.PREMIUM_MARKETING_BANNER,
+var r = s("120356"),
+  i = s.n(r),
+  n = s("990547"),
+  l = s("399606"),
+  o = s("481060"),
+  u = s("100527"),
+  d = s("182567"),
+  c = s("906732"),
+  _ = s("213609"),
+  E = s("78839"),
+  T = s("63063"),
+  I = s("958589"),
+  R = s("104494"),
+  f = s("639119"),
+  S = s("276800"),
+  m = s("409100"),
+  A = s("474936"),
+  N = s("981631"),
+  p = s("689938"),
+  g = s("810623");
+let C = "April2024MarketingBanner";
+t.default = function(e) {
+  let {
+    isFullscreen: t
+  } = e, s = (0, l.useStateFromStores)([E.default], () => E.default.getPremiumTypeSubscription()), r = (0, f.usePremiumTrialOffer)(), P = (0, R.usePremiumDiscountOffer)(), O = null != r || null != P, M = I.default.useExperiment({
+    location: C
+  }).enabled, {
+    analyticsLocations: h
+  } = (0, c.default)();
+  if ((0, _.default)({
+      type: n.ImpressionTypes.VIEW,
+      name: n.ImpressionNames.PREMIUM_MARKETING_BANNER,
       properties: {
-        location_stack: s,
-        banner_id: A
+        location_stack: h,
+        banner_id: C
       }
     }, {
-      disableTrack: !t
-    }), !t) return null;
-  let N = new Date("2024-05-01T17:00:00Z");
-  return (0, a.jsx)(o.AnalyticsLocationsProvider, {
-    newLocations: [l.default.PREMIUM_MARKETING_PAGE_BANNER],
+      disableTrack: !M
+    }), !M) return null;
+  let L = new Date("2024-05-01T17:00:00Z");
+  return (0, a.jsx)(d.AnalyticsLocationsProvider, {
+    newLocations: [u.default.PREMIUM_MARKETING_PAGE_BANNER],
     children: (0, a.jsx)("div", {
-      className: m.banner,
+      className: i()(g.banner, {
+        [g.negative48pxMarginTop]: t && !O,
+        [g.negative24pxMarginTop]: t && O
+      }),
       children: (0, a.jsxs)("div", {
-        className: m.bannerContent,
+        className: g.bannerContent,
         children: [(0, a.jsxs)("div", {
-          children: [(0, a.jsx)(n.Heading, {
+          children: [(0, a.jsx)(o.Heading, {
             variant: "display-sm",
             color: "always-white",
-            children: S.default.Messages.PREMIUM_2024_APRIL_NITRO_OFFER_HEADER
-          }), (0, a.jsx)(n.Text, {
-            className: m.countdown,
+            children: p.default.Messages.PREMIUM_2024_APRIL_NITRO_OFFER_HEADER
+          }), (0, a.jsx)(o.Text, {
+            className: g.countdown,
             variant: "text-sm/medium",
-            children: (0, a.jsx)(T.default, {
-              endDate: N
+            children: (0, a.jsx)(S.default, {
+              endDate: L
             })
-          }), (0, a.jsx)(n.Text, {
-            className: m.bannerSubheader,
+          }), (0, a.jsx)(o.Text, {
+            className: g.bannerSubheader,
             variant: "text-sm/medium",
             color: "always-white",
-            children: null == e ? S.default.Messages.PREMIUM_2024_APRIL_NITRO_OFFER_BANNER_BODY_NON_SUB.format({
-              helpCenterLink: _.default.getArticleURL(f.HelpdeskArticles.PREMIUM_APRIL_2024_MARKETING_MOMENT)
-            }) : S.default.Messages.PREMIUM_2024_APRIL_NITRO_OFFER_BANNER_BODY_UPGRADE.format({
-              helpCenterLink: _.default.getArticleURL(f.HelpdeskArticles.PREMIUM_APRIL_2024_MARKETING_MOMENT)
+            children: null == s ? p.default.Messages.PREMIUM_2024_APRIL_NITRO_OFFER_BANNER_BODY_NON_SUB.format({
+              helpCenterLink: T.default.getArticleURL(N.HelpdeskArticles.PREMIUM_APRIL_2024_MARKETING_MOMENT)
+            }) : p.default.Messages.PREMIUM_2024_APRIL_NITRO_OFFER_BANNER_BODY_UPGRADE.format({
+              helpCenterLink: T.default.getArticleURL(N.HelpdeskArticles.PREMIUM_APRIL_2024_MARKETING_MOMENT)
             })
           })]
-        }), (0, a.jsx)(I.default, {
-          buttonText: S.default.Messages.BOGO_CLAIM_OFFER,
-          buttonTextClassName: m.subscribeButtonText,
-          subscriptionTier: R.PremiumSubscriptionSKUs.TIER_2,
-          className: m.subscribeButton,
+        }), (0, a.jsx)(m.default, {
+          buttonText: p.default.Messages.BOGO_CLAIM_OFFER,
+          buttonTextClassName: g.subscribeButtonText,
+          subscriptionTier: A.PremiumSubscriptionSKUs.TIER_2,
+          className: g.subscribeButton,
           showIcon: !1,
           forceInverted: !0
         })]
