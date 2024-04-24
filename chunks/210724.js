24e07b9f1c7a9c4156802fd385dd8d8761f13e5e
DiscrapperManager <admin@minerpl.xyz>
2024-04-24 23:44:16 +0200

    canaryRelease: 287725
    Main scripts: 21
    Lazy Loaded Scripts: 824
    Chunks:
     * Main Files: 25217
     * Scripts: 27674
    Strings: 17710
    Routes: 736
    Experiments Count: 319

--- chunks/210724.js
+++ chunks/210724.js
@@ -2,82 +2,85 @@
 a.r(t), a("47120");
 var n = a("735250"),
   s = a("470079"),
-  l = a("718017"),
-  i = a("442837"),
-  r = a("607070"),
-  o = a("819640"),
-  u = a("617136"),
-  d = a("57655"),
-  c = a("113434"),
-  f = a("569984"),
-  E = a("497505"),
-  h = a("918701"),
-  _ = a("874137"),
-  C = a("78826"),
-  m = a("909389"),
-  S = a("941348"),
-  I = a("46140"),
-  p = a("981631");
+  l = a("120356"),
+  i = a.n(l),
+  r = a("718017"),
+  o = a("442837"),
+  u = a("607070"),
+  d = a("819640"),
+  c = a("617136"),
+  f = a("57655"),
+  E = a("113434"),
+  h = a("569984"),
+  _ = a("497505"),
+  C = a("918701"),
+  m = a("874137"),
+  S = a("78826"),
+  I = a("909389"),
+  p = a("941348"),
+  T = a("46140"),
+  g = a("981631"),
+  A = a("177630");
 t.default = function(e) {
-  var t, a, T;
+  var t, a, l;
   let {
-    overrideQuest: g
-  } = e, A = (0, d.useIsEligibleForQuests)({
-    location: I.QuestsExperimentLocations.QUESTS_BAR
-  }), N = (0, i.useStateFromStores)([r.default], () => r.default.useReducedMotion), v = (0, i.useStateFromStores)([f.default], () => null != g ? g : (0, h.getQuestForTargetedContent)(f.default.quests, E.QuestContent.QUEST_BAR)), R = (0, i.useStateFromStores)([o.default], () => o.default.hasLayers()), O = s.useRef(-1), L = s.useRef(!1), [P, M] = s.useState(!1), [x, y] = s.useState(!1), [D, b] = s.useState(!1), [U, j] = s.useState(!0), [G, w] = s.useState(240), k = s.useRef(null), F = (null == v ? void 0 : null === (t = v.userStatus) || void 0 === t ? void 0 : t.enrolledAt) != null, B = s.useRef(F), H = (null == v ? void 0 : null === (a = v.userStatus) || void 0 === a ? void 0 : a.completedAt) != null, V = (null == v ? void 0 : null === (T = v.userStatus) || void 0 === T ? void 0 : T.claimedAt) != null, Y = (null == v ? void 0 : v.userStatus) != null && (0, h.isDismissed)(null == v ? void 0 : v.userStatus, E.QuestContent.QUEST_BAR), W = (0, c.useIsQuestExpired)(v), {
-    hasError: K,
-    isLoading: z
-  } = (0, C.useQuestsAssetsLoadState)(), q = s.useCallback(() => {
+    overrideQuest: N
+  } = e, v = (0, f.useIsEligibleForQuests)({
+    location: T.QuestsExperimentLocations.QUESTS_BAR
+  }), R = (0, o.useStateFromStores)([u.default], () => u.default.useReducedMotion), O = (0, o.useStateFromStores)([h.default], () => null != N ? N : (0, C.getQuestForTargetedContent)(h.default.quests, _.QuestContent.QUEST_BAR)), L = (0, o.useStateFromStores)([d.default], () => d.default.hasLayers()), P = s.useRef(-1), M = s.useRef(!1), [x, y] = s.useState(!1), [D, b] = s.useState(!1), [U, j] = s.useState(!1), [G, w] = s.useState(!0), [k, F] = s.useState(240), B = s.useRef(null), H = (null == O ? void 0 : null === (t = O.userStatus) || void 0 === t ? void 0 : t.enrolledAt) != null, V = s.useRef(H), Y = (null == O ? void 0 : null === (a = O.userStatus) || void 0 === a ? void 0 : a.completedAt) != null, W = (null == O ? void 0 : null === (l = O.userStatus) || void 0 === l ? void 0 : l.claimedAt) != null, K = (null == O ? void 0 : O.userStatus) != null && (0, C.isDismissed)(null == O ? void 0 : O.userStatus, _.QuestContent.QUEST_BAR), z = (0, E.useIsQuestExpired)(O), {
+    hasError: q,
+    isLoading: Q
+  } = (0, S.useQuestsAssetsLoadState)(), Z = s.useCallback(() => {
     var e, t;
-    w((null !== (t = null === (e = k.current) || void 0 === e ? void 0 : e.offsetHeight) && void 0 !== t ? t : 84) + 16), y(!0)
-  }, []), Q = s.useCallback(() => {
-    M(!0)
-  }, []), Z = s.useCallback(() => {
-    M(!1), !L.current && !H && y(!1)
-  }, [H]), X = s.useCallback(() => {
-    M(!1), !H && y(!1), L.current = !1
-  }, [H]), J = s.useCallback(function() {
+    F((null !== (t = null === (e = B.current) || void 0 === e ? void 0 : e.offsetHeight) && void 0 !== t ? t : 84) + 16), b(!0)
+  }, []), X = s.useCallback(() => {
+    y(!0)
+  }, []), J = s.useCallback(() => {
+    y(!1), !M.current && !Y && b(!1)
+  }, [Y]), $ = s.useCallback(() => {
+    y(!1), !Y && b(!1), M.current = !1
+  }, [Y]), ee = s.useCallback(function() {
     let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : {},
       {
         withDelay: t = !1
       } = e;
-    t ? O.current = window.setTimeout(q, 75) : q()
-  }, [q]), $ = s.useCallback(() => {
-    if (window.clearTimeout(O.current), !H && !P) !L.current && y(!1)
-  }, [P, H]), ee = s.useCallback(() => {
-    null != v && (0, u.trackQuestEvent)({
-      questId: v.id,
-      event: p.AnalyticEvents.QUEST_HOVER
-    }), L.current = !0, J({
+    t ? P.current = window.setTimeout(Z, 75) : Z()
+  }, [Z]), et = s.useCallback(() => {
+    if (window.clearTimeout(P.current), !Y && !x) !M.current && b(!1)
+  }, [x, Y]), ea = s.useCallback(() => {
+    null != O && (0, c.trackQuestEvent)({
+      questId: O.id,
+      event: g.AnalyticEvents.QUEST_HOVER
+    }), M.current = !0, ee({
       withDelay: !0
     })
-  }, [J, v]), et = s.useCallback(() => {
-    L.current = !1, $()
-  }, [$]);
+  }, [ee, O]), en = s.useCallback(() => {
+    M.current = !1, et()
+  }, [et]);
   s.useLayoutEffect(() => {
-    H && q()
-  }, [H, q]), s.useLayoutEffect(() => {
-    F && !B.current && L.current && q()
-  }, [F, q]), s.useLayoutEffect(() => {
-    !H && F && !B.current && !L.current && y(!1)
-  }, [F, H]);
-  let ea = A && !Y && !V && null != v && !W && !z,
-    en = s.useRef(ea);
+    Y && Z()
+  }, [Y, Z]), s.useLayoutEffect(() => {
+    H && !V.current && M.current && Z()
+  }, [H, Z]), s.useLayoutEffect(() => {
+    !Y && H && !V.current && !M.current && b(!1)
+  }, [H, Y]);
+  let es = v && !K && !W && null != O && !z && !Q,
+    el = s.useRef(es);
   s.useLayoutEffect(() => {
-    ea !== en.current && j(!1), en.current = ea
-  }, [ea]), s.useEffect(() => {
-    B.current = F
-  }, [F]);
+    es !== el.current && w(!1), el.current = es
+  }, [es]), s.useEffect(() => {
+    V.current = H
+  }, [H]);
   let {
-    expansionSpring: es
-  } = (0, l.useSpring)({
+    expansionSpring: ei
+  } = (0, r.useSpring)({
     from: {
-      expansionSpring: x ? 0 : 1
+      expansionSpring: D ? 0 : 1
     },
     to: {
-      expansionSpring: x ? 1 : 0
+      expansionSpring: D ? 1 : 0
     },
-    config: F ? {
+    config: H ? {
       tension: 900,
       friction: 45,
       clamp: !0
@@ -86,28 +89,7 @@ t.default = function(e) {
       friction: 30,
       clamp: !0
     },
-    immediate: N,
-    onRest: () => {
-      b(!0)
-    },
-    onStart: () => {
-      b(!1)
-    }
-  }), {
-    visibilitySpring: el
-  } = (0, l.useSpring)({
-    from: {
-      visibilitySpring: 0
-    },
-    to: {
-      visibilitySpring: ea ? 1 : 0
-    },
-    config: {
-      tension: 250,
-      friction: 10,
-      clamp: !0
-    },
-    immediate: N,
+    immediate: R,
     onRest: () => {
       j(!0)
     },
@@ -115,56 +97,85 @@ t.default = function(e) {
       j(!1)
     }
   }), {
-    primaryVariant: ei
-  } = d.QuestBarExperiment.useExperiment({
-    location: I.QuestsExperimentLocations.QUESTS_BAR
+    visibilitySpring: er
+  } = (0, r.useSpring)({
+    from: {
+      visibilitySpring: 0
+    },
+    to: {
+      visibilitySpring: es ? 1 : 0
+    },
+    config: {
+      tension: 250,
+      friction: 10,
+      clamp: !0
+    },
+    immediate: R,
+    onRest: () => {
+      w(!0)
+    },
+    onStart: () => {
+      w(!1)
+    }
+  }), {
+    primaryVariant: eo
+  } = f.QuestBarExperiment.useExperiment({
+    location: T.QuestsExperimentLocations.QUESTS_BAR
   }, {
-    autoTrackExposure: ea && !K
+    autoTrackExposure: es && !q
   });
-  return A && null != v && (ea || !U || z) && !K ? (0, n.jsx)(_.QuestContentImpressionTracker, {
-    questOrQuests: v,
-    questContent: E.QuestContent.QUEST_BAR,
-    overrideVisibility: !R && ea,
-    children: () => ei === d.QuestBarExperimentVariants.V2 ? (0, n.jsx)(S.default, {
-      onBlur: $,
-      onCtxMenuClosed: Z,
-      onCtxMenuOpened: Q,
-      onCtxMenuSelection: X,
-      onFocus: J,
-      onMouseEnter: ee,
-      onMouseLeave: et,
-      expandedContentHeight: G,
-      expandedContentRef: k,
-      expansionSpring: es,
-      isExpanded: x,
-      isExpansionAnimationComplete: D,
-      isQuestAccepted: F,
-      isQuestBarVisible: ea,
-      isQuestCompleted: H,
-      isVisibilityAnimationAtRest: U,
-      quest: v,
-      useReducedMotion: N,
-      visibilitySpring: el
-    }) : (0, n.jsx)(m.default, {
-      onBlur: $,
-      onCtxMenuClosed: Z,
-      onCtxMenuOpened: Q,
-      onCtxMenuSelection: X,
-      onFocus: J,
-      onMouseEnter: ee,
-      onMouseLeave: et,
-      expandedContentHeight: G,
-      expandedContentRef: k,
-      expansionSpring: es,
-      isExpanded: x,
-      isExpansionAnimationComplete: D,
-      isQuestAccepted: F,
-      isQuestBarVisible: ea,
-      isQuestCompleted: H,
-      isVisibilityAnimationAtRest: U,
-      quest: v,
-      useReducedMotion: N,
-      visibilitySpring: el
-    })
+  return v && null != O && (es || !G || Q) && !q ? (0, n.jsx)(m.QuestContentImpressionTracker, {
+    questOrQuests: O,
+    questContent: _.QuestContent.QUEST_BAR,
+    overrideVisibility: !L && es,
+    children: () => {
+      let e = eo === f.QuestBarExperimentVariants.V2 ? p.default : I.default;
+      return (0, n.jsx)(r.animated.div, {
+        "aria-hidden": !es,
+        className: i()(A.wrapper, {
+          [A.wrapperInvisible]: !es,
+          [A.wrapperVisible]: es && G
+        }),
+        style: {
+          color: O.config.colors.secondary,
+          height: er.to({
+            range: [0, 1],
+            output: [0, Y && D ? k : 40]
+          })
+        },
+        children: (0, n.jsx)(r.animated.div, {
+          onMouseLeave: en,
+          onMouseEnter: ea,
+          onFocus: () => ee(),
+          onBlur: et,
+          className: i()(A.contentWrapper, {
+            [A.contentWrapperExpanded]: D,
+            [A.contentWrapperAccepted]: H
+          }),
+          style: {
+            backgroundImage: H ? "linear-gradient(90deg, ".concat(O.config.colors.primary, ", ").concat(O.config.colors.secondary, ")") : void 0,
+            height: ei.to({
+              range: [0, 1],
+              output: [40, H ? k : 240]
+            }),
+            transform: er.to({
+              range: [0, 1],
+              output: [100, 0]
+            }).to(e => "translateY(".concat(e, "%)"))
+          },
+          children: (0, n.jsx)(e, {
+            expandedContentRef: B,
+            expansionSpring: ei,
+            isExpanded: D,
+            isExpansionAnimationComplete: U,
+            onCtxMenuClosed: J,
+            onCtxMenuOpened: X,
+            onCtxMenuSelection: $,
+            quest: O,
+            useReducedMotion: R
+          })
+        })
+      })
+    }
   }) : null
 }
