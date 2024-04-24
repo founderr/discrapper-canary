f57cd176f2fd4c1698d41766df479ccb1b52378f
DiscrapperManager <admin@minerpl.xyz>
2024-04-24 19:34:36 +0200

    canaryRelease: 287512
    Main scripts: 21
    Lazy Loaded Scripts: 828
    Chunks:
     * Main Files: 25145
     * Scripts: 27746
    Strings: 17705
    Routes: 735
    Experiments Count: 317

--- chunks/596583.js
+++ chunks/596583.js
@@ -9,41 +9,43 @@ var r = t("512722"),
   C = t("780384"),
   o = t("481060"),
   d = t("493683"),
-  c = t("410030"),
-  u = t("204197"),
-  f = t("63063"),
-  x = t("51144"),
-  h = t("276444"),
-  p = t("792451"),
-  _ = t("814667"),
-  R = t("981631"),
-  j = t("689938"),
-  E = t("970422");
-let L = (e, s) => {
-    d.default.openPrivateChannel(e), s()
+  c = t("37234"),
+  u = t("410030"),
+  f = t("204197"),
+  x = t("819640"),
+  p = t("63063"),
+  h = t("51144"),
+  _ = t("276444"),
+  R = t("792451"),
+  j = t("814667"),
+  L = t("981631"),
+  E = t("689938"),
+  m = t("970422");
+let g = (e, s) => {
+    x.default.hasLayers() && (0, c.popLayer)(), d.default.openPrivateChannel(e), s()
   },
-  m = e => {
+  S = e => {
     let {
       user: s,
       onClose: t
     } = e, {
       avatarSrc: r,
       eventHandlers: l
-    } = (0, u.default)({
+    } = (0, f.default)({
       user: s,
       size: o.AvatarSizes.SIZE_56
-    }), i = x.default.getName(s), n = s.username !== i;
+    }), i = h.default.getName(s), n = s.username !== i;
     return (0, a.jsxs)("div", {
-      className: E.referredUserRowContainer,
+      className: m.referredUserRowContainer,
       children: [(0, a.jsx)(o.Avatar, {
         src: r,
         "aria-label": i,
         size: o.AvatarSizes.SIZE_56,
         ...l
       }), (0, a.jsxs)("div", {
-        className: E.referredUserRowBody,
+        className: m.referredUserRowBody,
         children: [(0, a.jsx)(o.Tooltip, {
-          text: j.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_COMPLETE_TOOL_TOP,
+          text: E.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_COMPLETE_TOOL_TOP,
           position: "right",
           children: e => {
             let {
@@ -51,20 +53,20 @@ let L = (e, s) => {
               onMouseLeave: l
             } = e;
             return (0, a.jsx)(o.Clickable, {
-              onClick: () => L(s.id, t),
-              className: E.displayNameClickableContainer,
+              onClick: () => g(s.id, t),
+              className: m.displayNameClickableContainer,
               onMouseEnter: r,
               onMouseLeave: l,
               children: (0, a.jsx)(o.Text, {
                 variant: "text-md/medium",
-                className: E.displayName,
+                className: m.displayName,
                 children: i
               })
             })
           }
         }), n && (0, a.jsx)(o.Text, {
           variant: "text-md/medium",
-          className: E.userName,
+          className: m.userName,
           children: s.username
         })]
       })]
@@ -74,39 +76,39 @@ s.default = e => {
   let {
     selectedUsers: s,
     onClose: t
-  } = e, r = [...s].map(e => (0, a.jsx)(m, {
+  } = e, r = [...s].map(e => (0, a.jsx)(S, {
     user: e,
     onClose: t
-  }, e.id)), d = (0, n.useStateFromStores)([h.default], () => h.default.getReferralsRemaining());
+  }, e.id)), d = (0, n.useStateFromStores)([_.default], () => _.default.getReferralsRemaining());
   l()(null !== d, "Referrals remaining should not be null");
-  let u = (0, c.useTheme)(),
-    x = (0, C.isThemeLight)(u) ? _.default : p.default,
-    L = j.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_RESENT_COMPLETE_HEADER;
-  return d > 0 && (L = j.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_COMPLETE_HEADER.format({
+  let c = (0, u.useTheme)(),
+    f = (0, C.isThemeLight)(c) ? j.default : R.default,
+    x = E.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_RESENT_COMPLETE_HEADER;
+  return d > 0 && (x = E.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_COMPLETE_HEADER.format({
     numReferrals: r.length
   })), (0, a.jsxs)("div", {
-    className: E.confrimationContainer,
+    className: m.confrimationContainer,
     children: [(0, a.jsx)(o.Clickable, {
       onClick: t,
-      className: E.closeButtonContainer,
+      className: m.closeButtonContainer,
       children: (0, a.jsx)(i.CloseSmallIcon, {
         width: 20,
         height: 20
       })
-    }), (0, a.jsx)(x, {
-      className: E.headerIcon
+    }), (0, a.jsx)(f, {
+      className: m.headerIcon
     }), (0, a.jsx)(o.Heading, {
       variant: "heading-lg/bold",
-      className: E.heading,
-      children: L
+      className: m.heading,
+      children: x
     }), (0, a.jsx)("div", {
-      className: E.confirmedUserContainer,
+      className: m.confirmedUserContainer,
       children: r
     }), (0, a.jsx)(o.Text, {
       variant: "text-md/medium",
-      children: j.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_COMPLETE_SUBHEADER.format({
+      children: E.default.Messages.REFERRAL_PROGRAM_FRIEND_SELECT_COMPLETE_SUBHEADER.format({
         days: 10,
-        helpdeskArticle: f.default.getArticleURL(R.HelpdeskArticles.REFERRAL_PROGRAM)
+        helpdeskArticle: p.default.getArticleURL(L.HelpdeskArticles.REFERRAL_PROGRAM)
       })
     })]
   })
