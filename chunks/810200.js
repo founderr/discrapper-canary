71a3ca5bc20dd2bc239d57333e8428ef9691d241
DiscrapperManager <admin@minerpl.xyz>
2024-04-25 23:33:00 +0200

    canaryRelease: 288061
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25246
     * Scripts: 27747
    Strings: 17732
    Routes: 736
    Experiments Count: 322

---
+++ chunks/810200.js
@@ -0,0 +1,8 @@
+"use strict";
+e.exports = {
+  button: "button__96098",
+  buttonContainer: "buttonContainer_de2133",
+  cardContainer: "cardContainer__94a1c",
+  container: "container__86b23",
+  tooltipFooter: "tooltipFooter__11121"
+}
