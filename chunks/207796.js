8807b66b047c65869628344f01083f15145233ba
DiscrapperManager <admin@minerpl.xyz>
2024-04-27 00:14:57 +0200

    canaryRelease: 288428
    Main scripts: 21
    Lazy Loaded Scripts: 822
    Chunks:
     * Main Files: 25264
     * Scripts: 28771
    Strings: 17737
    Routes: 737
    Experiments Count: 321

--- chunks/207796.js
+++ chunks/207796.js
@@ -7,16 +7,17 @@ a.r(t), a.d(t, {
     return l
   },
   buildSearchCriteriaFromUIState: function() {
-    return d
+    return c
   },
   useClanDiscoveryUIStore: function() {
-    return u
+    return d
   }
 });
 var n, s, l, i, r = a("652874"),
-  o = a("868888");
+  o = a("868888"),
+  u = a("433517");
 (n = l || (l = {})).GET_STARTED = "get_started", n.DISCOVERY = "discovery", n.PLAYSTYLE = "playstyle", n.TRAITS = "traits", n.GAMES = "games", (s = i || (i = {})).VALORANT = "valorant", s.GENSHIN = "genshin";
-let u = (0, r.default)((0, o.persist)(e => ({
+let d = (0, r.default)((0, o.persist)(e => ({
   mode: "get_started",
   game: "valorant",
   selectedGames: [],
@@ -39,6 +40,14 @@ let u = (0, r.default)((0, o.persist)(e => ({
   })
 }), {
   name: "clan-discovery",
+  getStorage: () => ({
+    getItem: e => {
+      var t;
+      return null !== (t = u.Storage.get(e)) && void 0 !== t ? t : null
+    },
+    setItem: (e, t) => u.Storage.set(e, t),
+    removeItem: e => u.Storage.remove(e)
+  }),
   partialize: e => ({
     mode: e.mode,
     game: e.game,
@@ -48,7 +57,7 @@ let u = (0, r.default)((0, o.persist)(e => ({
   })
 }));
 
-function d(e) {
+function c(e) {
   var t;
   let a = e.selectedGames,
     n = null !== (t = e.selectedPlaystyle) && void 0 !== t ? t : void 0;
