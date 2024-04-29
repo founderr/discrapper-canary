823eb9d04aeb88544a64aa61c32056df829ad4ac
DiscrapperManager <admin@minerpl.xyz>
2024-04-29 21:39:39 +0200

    canaryRelease: 288738
    Main scripts: 21
    Lazy Loaded Scripts: 822
    Chunks:
     * Main Files: 25231
     * Scripts: 28793
    Strings: 17742
    Routes: 737
    Experiments Count: 321

--- chunks/420491.js
+++ chunks/420491.js
@@ -109,7 +109,10 @@ a = "GatewayZstdStore", (s = "displayName") in(r = O) ? Object.defineProperty(r,
       location: "GatewayZstdStore"
     }).useZstd), p(t), A = 0
   },
-  CONNECTION_INTERRUPTED: function() {
-    N() && m && (A += 1) > 3 && (S.error("Disabling zstd due to consecutive errors"), p(!1), h = !0)
+  CONNECTION_INTERRUPTED: function(e) {
+    let {
+      code: t
+    } = e;
+    N() && m && 1e3 !== t && (A += 1) > 3 && (S.error("Disabling zstd due to consecutive errors"), p(!1), h = !0)
   }
 })
