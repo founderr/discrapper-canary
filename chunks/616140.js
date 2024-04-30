f5ef540e0ab8ea801caccf91c7e58b270a9e9528
DiscrapperManager <admin@minerpl.xyz>
2024-04-30 20:25:37 +0200

    canaryRelease: 289152
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25276
     * Scripts: 28902
    Strings: 17755
    Routes: 735
    Experiments Count: 318

--- chunks/616140.js
+++ chunks/616140.js
@@ -1,7 +1,7 @@
 "use strict";
 s.r(t), s.d(t, {
   default: function() {
-    return T
+    return U
   }
 }), s("47120"), s("627341");
 var l = s("735250"),
@@ -16,68 +16,69 @@ var l = s("735250"),
   f = s("621853"),
   S = s("944546"),
   m = s("470900"),
-  v = s("988246"),
-  I = s("834534"),
+  I = s("988246"),
+  v = s("834534"),
   E = s("228168"),
   x = s("689938"),
-  A = s("788741");
-let _ = e => {
-  let [t, s] = (0, n.useStateFromStoresArray)([f.default], () => {
-    var t, s, l, a;
-    return [null !== (l = null === (t = f.default.getMutualFriends(e.id)) || void 0 === t ? void 0 : t.length) && void 0 !== l ? l : 0, null !== (a = null === (s = f.default.getMutualGuilds(e.id)) || void 0 === s ? void 0 : s.length) && void 0 !== a ? a : 0]
+  _ = s("788741");
+let A = e => {
+  let {
+    user: t,
+    currentUser: s
+  } = e, [l, a] = (0, n.useStateFromStoresArray)([f.default], () => {
+    var e, s;
+    return [null === (e = f.default.getMutualFriends(t.id)) || void 0 === e ? void 0 : e.length, null === (s = f.default.getMutualGuilds(t.id)) || void 0 === s ? void 0 : s.length]
   });
-  return [{
+  return t.id === (null == s ? void 0 : s.id) ? [{
+    section: E.UserProfileSections.USER_INFO,
+    text: x.default.Messages.USER_PROFILE_ABOUT_ME
+  }] : [{
     section: E.UserProfileSections.USER_INFO,
     text: x.default.Messages.USER_PROFILE_ABOUT_ME
   }, {
     section: E.UserProfileSections.MUTUAL_FRIENDS,
-    text: 0 === t ? x.default.Messages.USER_PROFILE_NO_MUTUAL_FRIENDS : x.default.Messages.USER_PROFILE_MUTUAL_FRIENDS.format({
-      count: t
-    })
+    text: (0, i.match)(l).with(void 0, () => x.default.Messages.USER_PROFILE_MUTUAL_FRIENDS_PLACEHOLDER).with(0, () => x.default.Messages.USER_PROFILE_NO_MUTUAL_FRIENDS).otherwise(e => x.default.Messages.USER_PROFILE_MUTUAL_FRIENDS.format({
+      count: e
+    }))
   }, {
     section: E.UserProfileSections.MUTUAL_GUILDS,
-    text: 0 === s ? x.default.Messages.USER_PROFILE_NO_MUTUAL_SERVERS : x.default.Messages.USER_PROFILE_MUTUAL_GUILDS.format({
-      count: s
-    })
+    text: (0, i.match)(a).with(void 0, () => x.default.Messages.USER_PROFILE_MUTUAL_GUILDS_PLACEHOLDER).with(0, () => x.default.Messages.USER_PROFILE_NO_MUTUAL_SERVERS).otherwise(e => x.default.Messages.USER_PROFILE_MUTUAL_GUILDS.format({
+      count: e
+    }))
   }]
 };
 
-function T(e) {
+function U(e) {
   var t, s;
   let {
     user: f,
     displayProfile: x,
-    initialSection: T = E.UserProfileSections.USER_INFO,
-    autoFocusNote: N,
-    onClose: U
+    initialSection: U = E.UserProfileSections.USER_INFO,
+    autoFocusNote: T,
+    onClose: N
   } = e, {
     trackUserProfileAction: C
-  } = (0, c.useUserProfileAnalyticsContext)(), g = (0, n.useStateFromStores)([d.default], () => d.default.getCurrentUser()), h = (0, n.useStateFromStores)([u.default], () => u.default.hidePersonalInformation), p = _(f), [j, R] = a.useState(N), [P, M] = a.useState(null !== (s = null === (t = p.find(e => {
+  } = (0, c.useUserProfileAnalyticsContext)(), g = (0, n.useStateFromStores)([d.default], () => d.default.getCurrentUser()), h = (0, n.useStateFromStores)([u.default], () => u.default.hidePersonalInformation), p = A({
+    user: f,
+    currentUser: g
+  }), [R, j] = a.useState(T), [P, M] = a.useState(null !== (s = null === (t = p.find(e => {
     let {
       section: t
     } = e;
-    return t === T
+    return t === U
   })) || void 0 === t ? void 0 : t.section) && void 0 !== s ? s : E.UserProfileSections.USER_INFO), y = a.useCallback(e => {
     C({
       action: "PRESS_SECTION",
       section: e
-    }), M(e), R(!1)
+    }), M(e), j(!1)
   }, [C, M]);
   return null == g ? null : h ? (0, l.jsx)("div", {
-    className: A.container,
-    children: (0, l.jsx)(v.default, {})
-  }) : f.id === g.id ? (0, l.jsx)("div", {
-    className: A.container,
-    children: (0, l.jsx)(I.default, {
-      user: f,
-      currentUser: g,
-      displayProfile: x,
-      autoFocusNote: j
-    })
+    className: _.container,
+    children: (0, l.jsx)(I.default, {})
   }) : (0, l.jsxs)("div", {
-    className: A.container,
+    className: _.container,
     children: [(0, l.jsx)(r.TabBar, {
-      className: A.tabBar,
+      className: _.tabBar,
       type: "top",
       selectedItem: P,
       onItemSelect: y,
@@ -87,7 +88,7 @@ function T(e) {
           text: s
         } = e;
         return (0, l.jsx)(r.TabBar.Item, {
-          className: A.tabBarItem,
+          className: _.tabBarItem,
           id: t,
           "aria-label": s,
           children: (0, l.jsx)(o.Text, {
@@ -98,16 +99,16 @@ function T(e) {
       })
     }), (0, i.match)(P).with(E.UserProfileSections.MUTUAL_FRIENDS, () => (0, l.jsx)(S.default, {
       user: f,
-      onClose: U
+      onClose: N
     })).with(E.UserProfileSections.MUTUAL_GUILDS, () => (0, l.jsx)(m.default, {
       user: f,
-      onClose: U
-    })).otherwise(() => (0, l.jsx)(I.default, {
-      className: A.infoTab,
+      onClose: N
+    })).otherwise(() => (0, l.jsx)(v.default, {
+      className: _.infoTab,
       user: f,
       currentUser: g,
       displayProfile: x,
-      autoFocusNote: j
+      autoFocusNote: R
     }))]
   })
 }
