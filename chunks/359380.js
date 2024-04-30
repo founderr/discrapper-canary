95cb428c4f836708b69cca02f21719053036c27e
DiscrapperManager <admin@minerpl.xyz>
2024-04-30 20:06:29 +0200

    canaryRelease: 289140
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25276
     * Scripts: 28900
    Strings: 17755
    Routes: 735
    Experiments Count: 318

--- chunks/359380.js
+++ chunks/359380.js
@@ -1,21 +1,21 @@
 "use strict";
-n.r(t), n("47120");
-var a = n("735250"),
-  s = n("470079"),
-  l = n("120356"),
-  i = n.n(l),
-  r = n("481060"),
-  o = n("90062"),
-  u = n("964309"),
-  d = n("898543"),
-  c = n("87576"),
-  f = n("308083"),
-  E = n("689938"),
-  h = n("418571");
+a.r(t), a("47120");
+var n = a("735250"),
+  s = a("470079"),
+  l = a("120356"),
+  i = a.n(l),
+  r = a("481060"),
+  o = a("90062"),
+  u = a("964309"),
+  d = a("898543"),
+  c = a("87576"),
+  f = a("308083"),
+  E = a("689938"),
+  h = a("418571");
 let _ = e => {
   let {
     title: t,
-    icon: n,
+    icon: a,
     traits: l,
     interests: o,
     handleUpdate: u
@@ -27,24 +27,24 @@ let _ = e => {
     let t = new Set(o);
     t.add(e), u(t)
   }, [u, o]);
-  return (0, a.jsxs)(a.Fragment, {
-    children: [(0, a.jsx)(r.Text, {
+  return (0, n.jsxs)(n.Fragment, {
+    children: [(0, n.jsx)(r.Text, {
       className: h.interestsCategoryTitle,
       variant: "text-xs/semibold",
       color: "text-muted",
       children: t
-    }), (0, a.jsx)("div", {
+    }), (0, n.jsx)("div", {
       className: h.interestsCategory,
       children: l.map(e => {
         let t = o.has(e);
-        return (0, a.jsxs)(r.Clickable, {
+        return (0, n.jsxs)(r.Clickable, {
           className: i()(h.interestsTag, {
             [h.selected]: t
           }),
           onClick: () => t ? d(e) : c(e),
-          children: [(0, a.jsx)(n, {
+          children: [(0, n.jsx)(a, {
             className: h.utilityTagIcon
-          }), (0, a.jsx)(r.Text, {
+          }), (0, n.jsx)(r.Text, {
             variant: "text-xs/medium",
             color: "none",
             children: e
@@ -57,7 +57,7 @@ let _ = e => {
 t.default = e => {
   let {
     title: t,
-    description: n,
+    description: a,
     handleUpdate: l,
     interests: i,
     requiredGameId: C
@@ -68,47 +68,47 @@ t.default = e => {
     let t = S.filter(e => !f.LANGUAGES_SET.has(e));
     l(new Set([...t, ...e]))
   }, [l, S]), T = s.useMemo(() => S.filter(e => f.ALL_TRAITS_SET.has(e) || f.LANGUAGES_SET.has(e)), [S]);
-  return (0, a.jsxs)("div", {
+  return (0, n.jsxs)("div", {
     className: h.slideContentWithSidebar,
-    children: [(0, a.jsx)(r.Heading, {
+    children: [(0, n.jsx)(r.Heading, {
       variant: "heading-xxl/medium",
       className: h.title,
       children: t
-    }), (0, a.jsx)(r.Text, {
+    }), (0, n.jsx)(r.Text, {
       variant: "text-md/normal",
       color: "header-secondary",
       className: h.subtitle,
-      children: n
-    }), (0, a.jsxs)("div", {
+      children: a
+    }), (0, n.jsxs)("div", {
       className: h.content,
-      children: [(0, a.jsxs)("div", {
+      children: [(0, n.jsxs)("div", {
         className: h.mainPanelContainer,
-        children: [C === f.VALORANT_ID && (0, a.jsx)(_, {
+        children: [C === f.VALORANT_ID && (0, n.jsx)(_, {
           title: E.default.Messages.CLAN_SETUP_UTILITY_TRAITS_CATEGORY_VALORANT,
           icon: d.default,
           traits: f.VALORANT_TRAITS,
           interests: i,
           handleUpdate: l
-        }), (0, a.jsx)(_, {
+        }), (0, n.jsx)(_, {
           title: E.default.Messages.CLAN_SETUP_UTILITY_TRAITS_CATEGORY_AGE,
           icon: o.default,
           traits: f.AGE_TRAITS,
           interests: i,
           handleUpdate: l
-        }), (0, a.jsx)(_, {
+        }), (0, n.jsx)(_, {
           title: E.default.Messages.CLAN_SETUP_UTILITY_TRAITS_CATEGORY_IDENTITY,
           icon: u.default,
           traits: f.IDENTITY_TRAITS,
           interests: i,
           handleUpdate: l
-        }), (0, a.jsx)(r.Text, {
+        }), (0, n.jsx)(r.Text, {
           className: h.interestsCategoryTitle,
           variant: "text-xs/semibold",
           color: "text-muted",
           children: E.default.Messages.CLAN_SETUP_UTILITY_TRAITS_CATEGORY_LANGUAGE
-        }), (0, a.jsx)("div", {
+        }), (0, n.jsx)("div", {
           className: h.languageSelect,
-          children: (0, a.jsx)(r.SearchableSelect, {
+          children: (0, n.jsx)(r.SearchableSelect, {
             wrapperClassName: h.input,
             options: m,
             value: p,
@@ -117,9 +117,9 @@ t.default = e => {
             multi: !0
           })
         })]
-      }), (0, a.jsx)("div", {
+      }), (0, n.jsx)("div", {
         className: h.fixedWidthSidebar,
-        children: (0, a.jsx)(c.default, {
+        children: S.length > 0 && (0, n.jsx)(c.default, {
           traits: S,
           traitsToHighlight: T
         })
--- chunks/796918.js
+++ chunks/796918.js
@@ -1,15 +1,15 @@
 "use strict";
-n.r(t), n("47120");
-var a = n("735250"),
-  s = n("470079"),
-  l = n("120356"),
-  i = n.n(l),
-  r = n("481060"),
-  o = n("729285"),
-  u = n("87576"),
-  d = n("308083"),
-  c = n("689938"),
-  f = n("418571");
+a.r(t), a("47120");
+var n = a("735250"),
+  s = a("470079"),
+  l = a("120356"),
+  i = a.n(l),
+  r = a("481060"),
+  o = a("729285"),
+  u = a("87576"),
+  d = a("308083"),
+  c = a("689938"),
+  f = a("418571");
 
 function E(e) {
   return !d.ALL_TRAITS_SET.has(e) && !d.SUGGESTED_TRAITS_SET.has(e) && !d.LANGUAGES_SET.has(e)
@@ -17,85 +17,85 @@ function E(e) {
 t.default = e => {
   let {
     handleUpdate: t,
-    interests: n,
+    interests: a,
     error: l
-  } = e, [h, _] = s.useState(""), C = s.useMemo(() => Array.from(n), [n]), m = s.useMemo(() => C.filter(E), [C]), S = s.useMemo(() => [...d.SUGGESTED_TRAITS_SET, ...m], [m]), p = s.useMemo(() => C.filter(e => !d.ALL_TRAITS_SET.has(e) && !d.LANGUAGES_SET.has(e)), [C]), I = e => {
-    let a = new Set(n);
-    a.delete(e), t({
-      interests: a
+  } = e, [h, _] = s.useState(""), C = s.useMemo(() => Array.from(a), [a]), m = s.useMemo(() => C.filter(E), [C]), S = s.useMemo(() => [...d.SUGGESTED_TRAITS_SET, ...m], [m]), p = s.useMemo(() => C.filter(e => !d.ALL_TRAITS_SET.has(e) && !d.LANGUAGES_SET.has(e)), [C]), I = e => {
+    let n = new Set(a);
+    n.delete(e), t({
+      interests: n
     })
   }, T = s.useCallback(e => {
-    if (n.size === d.MAX_NUM_INTERESTS) return;
-    let a = null != e ? e : h.trim();
-    if (0 === a.length) return;
-    let s = new Set(n);
-    s.add(a), t({
+    if (a.size === d.MAX_NUM_INTERESTS) return;
+    let n = null != e ? e : h.trim();
+    if (0 === n.length) return;
+    let s = new Set(a);
+    s.add(n), t({
       interests: s
     }), _("")
-  }, [t, h, n]), g = s.useCallback(e => {
+  }, [t, h, a]), g = s.useCallback(e => {
     switch (e.key) {
       case "Enter":
       case "Tab":
         e.preventDefault(), e.stopPropagation(), T()
     }
   }, [T]);
-  return (0, a.jsxs)("div", {
+  return (0, n.jsxs)("div", {
     className: f.slideContentWithSidebar,
-    children: [(0, a.jsx)(r.Heading, {
+    children: [(0, n.jsx)(r.Heading, {
       variant: "heading-xxl/medium",
       className: f.title,
       children: c.default.Messages.CLAN_SETUP_INTERESTS_TITLE
-    }), (0, a.jsx)(r.Text, {
+    }), (0, n.jsx)(r.Text, {
       variant: "text-md/normal",
       color: "header-secondary",
       className: f.subtitle,
       children: c.default.Messages.CLAN_SETUP_INTERESTS_SUBTITLE
-    }), null != l && (0, a.jsx)(r.Text, {
+    }), null != l && (0, n.jsx)(r.Text, {
       variant: "text-sm/normal",
       color: "status-danger",
       className: f.errorText,
       children: l
-    }), (0, a.jsxs)("div", {
+    }), (0, n.jsxs)("div", {
       className: f.content,
-      children: [(0, a.jsxs)("div", {
+      children: [(0, n.jsxs)("div", {
         className: f.mainPanelContainer,
-        children: [(0, a.jsxs)("div", {
+        children: [(0, n.jsxs)("div", {
           className: f.inputContainer,
-          children: [(0, a.jsx)(r.TextInput, {
+          children: [(0, n.jsx)(r.TextInput, {
             inputClassName: f.input,
             value: h,
             onKeyDown: g,
             onChange: _,
             placeholder: c.default.Messages.CLAN_SETUP_INTERESTS_PLACEHOLDER,
             maxLength: d.MAX_INTEREST_LENGTH,
-            disabled: n.size === d.MAX_NUM_INTERESTS
-          }), h.length > 0 && (0, a.jsx)(r.Clickable, {
+            disabled: a.size === d.MAX_NUM_INTERESTS
+          }), h.length > 0 && (0, n.jsx)(r.Clickable, {
             onClick: () => T(h.trim()),
             className: i()(f.plusIcon, f.clickable),
-            children: (0, a.jsx)(o.default, {
+            children: (0, n.jsx)(o.default, {
               className: f.icon
             })
-          }), h.length > 0 && (0, a.jsx)(r.Text, {
+          }), h.length > 0 && (0, n.jsx)(r.Text, {
             color: "text-muted",
             variant: "text-xs/normal",
             className: f.enterToSearchText,
             children: c.default.Messages.CLAN_SETUP_INTERESTS_HELP
           })]
-        }), (0, a.jsx)(r.Text, {
+        }), (0, n.jsx)(r.Text, {
           className: f.interestsCategoryTitle,
           variant: "text-xs/semibold",
           color: "text-muted",
           children: c.default.Messages.CLAN_SETUP_SUGGESTED_INTERESTS
-        }), (0, a.jsx)("div", {
+        }), (0, n.jsx)("div", {
           className: f.interestsCategory,
           children: S.map(e => {
-            let t = n.has(e);
-            return (0, a.jsx)(r.Clickable, {
+            let t = a.has(e);
+            return (0, n.jsx)(r.Clickable, {
               className: i()(f.interestsTag, {
                 [f.selected]: t
               }),
               onClick: () => t ? I(e) : T(e),
-              children: (0, a.jsx)(r.Text, {
+              children: (0, n.jsx)(r.Text, {
                 variant: "text-xs/medium",
                 color: "none",
                 children: e
@@ -103,9 +103,9 @@ t.default = e => {
             }, e)
           })
         })]
-      }), (0, a.jsx)("div", {
+      }), (0, n.jsx)("div", {
         className: f.fixedWidthSidebar,
-        children: (0, a.jsx)(u.default, {
+        children: C.length > 0 && (0, n.jsx)(u.default, {
           traits: C,
           traitsToHighlight: p
         })
