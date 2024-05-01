4e008ee996a1921eb6b6b970a91e55d7c2ce16a7
DiscrapperManager <admin@minerpl.xyz>
2024-05-01 21:35:08 +0200

    canaryRelease: 289638
    Main scripts: 21
    Lazy Loaded Scripts: 824
    Chunks:
     * Main Files: 25297
     * Scripts: 28887
    Strings: 17776
    Routes: 737
    Experiments Count: 317

--- chunks/456774.js
+++ chunks/456774.js
@@ -1,6 +1,6 @@
 "use strict";
 n.r(t), n.d(t, {
-  ActivityCategory: function() {
+  CardCategory: function() {
     return r
   },
   default: function() {
@@ -335,7 +335,7 @@ function ei(e) {
       isUserLurking: e,
       isUnverifiedAccount: !y.default.getCheck(t.id).canChat
     }
-  }), l = (0, W.useGuildActivity)(t), _ = "empty" === l[0].category, c = a.useMemo(() => l.map(e => {
+  }), l = (0, W.useGuildActivity)(t), _ = a.useMemo(() => l.map(e => {
     if ("hangout" === e.category) return (0, s.jsx)(ee, {
       hangoutActivity: e,
       guildId: t.id,
@@ -386,7 +386,8 @@ function ei(e) {
       isUnverifiedAccount: o
     }, "".concat(e.category, "-").concat(e.channelId))
   }), [l, t.id, o, r]);
-  if (_) return i ? (0, s.jsxs)(s.Fragment, {
+  if (0 === l.length) return null;
+  if ("empty" === l[0].category) return i ? (0, s.jsxs)(s.Fragment, {
     children: [(0, s.jsxs)("div", {
       className: X.emptyActivityCardPrompt,
       children: [(0, s.jsxs)("div", {
@@ -404,10 +405,10 @@ function ei(e) {
         src: n("121727"),
         alt: ""
       })]
-    }), c]
+    }), _]
   }) : null;
   return (0, s.jsx)(s.Fragment, {
-    children: c
+    children: _
   })
 }
 
