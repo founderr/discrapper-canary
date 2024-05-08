aef26bd5130588b70fcc2b2f9cad4586339f40ae
DiscrapperManager <admin@minerpl.xyz>
2024-05-08 19:34:22 +0200

    canaryRelease: 291497
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25402
     * Scripts: 29088
    Strings: 17830
    Routes: 739
    Experiments Count: 322

--- chunks/979554.js
+++ chunks/979554.js
@@ -4,4 +4,4 @@ n.r(t), n.d(t, {
   CollectiblesItemType: function() {
     return i
   }
-}), (r = i || (i = {}))[r.NONE = 100] = "NONE", r[r.AVATAR_DECORATION = 0] = "AVATAR_DECORATION", r[r.PROFILE_EFFECT = 1] = "PROFILE_EFFECT"
+}), (r = i || (i = {}))[r.NONE = 100] = "NONE", r[r.AVATAR_DECORATION = 0] = "AVATAR_DECORATION", r[r.PROFILE_EFFECT = 1] = "PROFILE_EFFECT", r[r.BUNDLE = 1e3] = "BUNDLE"
