954fd96cc40c1ac00029cd642555f8d4100f30b3
DiscrapperManager <admin@minerpl.xyz>
2024-05-09 00:15:13 +0200

    canaryRelease: 291660
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25408
     * Scripts: 29087
    Strings: 17832
    Routes: 739
    Experiments Count: 322

--- chunks/269203.js
+++ chunks/269203.js
@@ -1,69 +1,63 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return I
+    return p
   }
-}), n("47120"), n("773603");
-var a = n("735250"),
-  l = n("470079"),
-  s = n("442837"),
-  i = n("481060"),
-  r = n("693546"),
-  o = n("826581"),
-  u = n("246364"),
-  d = n("937111"),
-  c = n("705563"),
-  f = n("430824"),
-  h = n("496675"),
-  m = n("594174"),
-  p = n("346656"),
-  E = n("709054"),
-  C = n("967128"),
-  g = n("981631"),
-  S = n("689938"),
-  _ = n("17057");
+});
+var a = n("735250");
+n("470079");
+var l = n("442837"),
+  s = n("481060"),
+  i = n("805519"),
+  r = n("246364"),
+  o = n("705563"),
+  u = n("594174"),
+  d = n("346656"),
+  c = n("967128"),
+  f = n("689938"),
+  h = n("17057");
 
-function T(e) {
+function m(e) {
   var t;
   let {
     joinRequest: n,
-    guild: l
-  } = e, r = (0, s.useStateFromStores)([m.default], () => m.default.getUser(n.userId));
+    guild: i
+  } = e, o = (0, l.useStateFromStores)([u.default], () => u.default.getUser(n.userId));
   return (0, a.jsxs)("div", {
-    className: _.summaryContainer,
+    className: h.summaryContainer,
     children: [(0, a.jsxs)("div", {
-      className: _.summaryHeader,
-      children: [null != l && (0, a.jsxs)("div", {
-        className: _.summaryHeaderClanInfo,
-        children: [(0, a.jsx)(p.default, {
-          guild: l,
+      className: h.summaryHeader,
+      children: [null != i && (0, a.jsxs)("div", {
+        className: h.summaryHeaderClanInfo,
+        children: [(0, a.jsx)(d.default, {
+          guild: i,
           active: !0,
-          size: p.default.Sizes.SMOL,
-          className: _.guildIcon
-        }), (0, a.jsx)(i.Heading, {
+          size: d.default.Sizes.SMOL,
+          className: h.guildIcon
+        }), (0, a.jsx)(s.Heading, {
           variant: "heading-sm/semibold",
           color: "header-primary",
-          children: l.name
+          children: i.name
         })]
-      }), null != r && (0, a.jsx)(i.Heading, {
+      }), null != o && (0, a.jsx)(s.Heading, {
         variant: "heading-xl/semibold",
         color: "header-primary",
-        children: S.default.Messages.MEMBER_VERIFICATION_USERS_APPLICATION.format({
-          username: r.globalName
+        children: f.default.Messages.MEMBER_VERIFICATION_USERS_APPLICATION.format({
+          username: o.globalName
         })
       })]
-    }), null === (t = n.formResponses) || void 0 === t ? void 0 : t.filter(e => e.field_type !== u.VerificationFormFieldTypes.TERMS).map(e => {
-      let t = e.field_type === u.VerificationFormFieldTypes.MULTIPLE_CHOICE && null != e.response ? e.choices[e.response] : e.response;
+    }), null === (t = n.formResponses) || void 0 === t ? void 0 : t.filter(e => e.field_type !== r.VerificationFormFieldTypes.TERMS).map(e => {
+      let t = e.field_type === r.VerificationFormFieldTypes.MULTIPLE_CHOICE && null != e.response ? e.choices[e.response] : e.response;
       return (0, a.jsxs)(a.Fragment, {
         children: [(0, a.jsx)("hr", {
-          className: _.summarySeparator
+          className: h.summarySeparator
         }), (0, a.jsxs)("div", {
-          className: _.formResponseContainer,
-          children: [(0, a.jsx)(i.Text, {
+          className: h.formResponseContainer,
+          children: [(0, a.jsx)(s.Text, {
             variant: "text-xs/semibold",
             color: "text-muted",
             children: e.label
-          }), (0, a.jsx)(i.Text, {
+          }), (0, a.jsx)(s.Text, {
             variant: "text-md/medium",
             color: "text-primary",
             children: t
@@ -74,49 +68,30 @@ function T(e) {
   })
 }
 
-function I(e) {
+function p(e) {
   let {
     channel: t
-  } = e, [n, u] = l.useState(!1), [m, p] = l.useState(!1), S = E.default.cast(t.id), {
-    joinRequest: I,
-    guild: A
-  } = (0, s.useStateFromStoresObject)([o.default, d.default, f.default, h.default], () => {
-    var e;
-    let t = o.default.getRequest(S);
-    if (null == t) return {
-      joinRequest: null,
-      isModmin: !1,
-      guild: null
-    };
-    let n = null !== (e = f.default.getGuild(t.guildId)) && void 0 !== e ? e : d.default.getJoinRequestGuild(t.guildId);
-    return {
-      joinRequest: t,
-      isModmin: null != n && h.default.can(g.Permissions.KICK_MEMBERS, n),
-      guild: n
-    }
-  });
-  return l.useEffect(() => {
-    null == A && !m && (p(!0), r.default.fetchRequestToJoinGuilds())
-  }, [A, m]), l.useEffect(() => {
-    null == I && (u(!0), r.default.fetchGuildJoinRequest(S).finally(() => {
-      u(!1)
-    }))
-  }, [I, S]), (0, a.jsx)(C.default, {
+  } = e, {
+    loading: n,
+    joinRequest: l,
+    joinRequestGuild: r
+  } = (0, i.default)(t.id);
+  return (0, a.jsx)(c.default, {
     channelId: t.id,
-    className: _.container,
-    children: (0, a.jsx)(C.EmptyMessageBody, {
-      children: null != I && null != I.formResponses ? (0, a.jsxs)("div", {
-        className: _.formContainer,
+    className: h.container,
+    children: (0, a.jsx)(c.EmptyMessageBody, {
+      children: null != l && null != l.formResponses ? (0, a.jsxs)("div", {
+        className: h.formContainer,
         children: [(0, a.jsx)("div", {
-          children: (0, a.jsx)(T, {
-            guild: A,
-            joinRequest: I
+          children: (0, a.jsx)(m, {
+            guild: r,
+            joinRequest: l
           })
-        }), (0, a.jsx)(c.default, {
+        }), (0, a.jsx)(o.default, {
           channelId: t.id,
           showProfile: !0
         })]
-      }) : n ? (0, a.jsx)(i.Spinner, {}) : null
+      }) : n ? (0, a.jsx)(s.Spinner, {}) : null
     })
   })
 }
--- routes.js
+++ routes.js
@@ -2618,7 +2618,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-029eaef2e9896b31949d26e5dd5f96fc6e3d0dee",
+                    release: "discord_web-a32c80a6137160f6b96c0da3b8e35370f44e44d2",
                     beforeSend: _ => {
                         var E, e;
                         return !(null != (E = _).exception && null != E.exception.values && E.exception.values.every(_ => null == _.stacktrace || null != _.stacktrace.frames && 1 === _.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || a.some(_ => window.navigator.appVersion.toLowerCase().indexOf(_) >= 0)) && !R() && !("Aborted" === (e = _).message || "cancel captcha" === e.message) && t() ? _ : null
@@ -2636,7 +2636,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "ResizeObserver loop completed with undelivered notifications.", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), I.setTag("buildNumber", (_ = "291658", "291658")), I.setTag("builtAt", String("1715205532559"));
+                }), I.setTag("buildNumber", (_ = "291660", "291660")), I.setTag("builtAt", String("1715205844042"));
                 let E = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != E && "object" == typeof E)
                     for (let _ in E) I.setTag(_, E[_]);
