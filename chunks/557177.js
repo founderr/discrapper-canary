4e0cf0098919c61277be4953ec4bb05bd14fdcfb
DiscrapperManager <admin@minerpl.xyz>
2024-05-09 00:11:20 +0200

    canaryRelease: 291658
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25408
     * Scripts: 29084
    Strings: 17832
    Routes: 739
    Experiments Count: 322

--- chunks/557177.js
+++ chunks/557177.js
@@ -2,46 +2,50 @@
 let i;
 n.r(t), n.d(t, {
   createSound: function() {
-    return u
+    return _
   },
   createSoundForPack: function() {
-    return l
+    return d
   },
   playSound: function() {
-    return d
+    return c
   }
 });
-var r = n("292619"),
-  a = n("474873"),
-  s = n("135906"),
-  o = n("246946");
+var r = n("259443"),
+  a = n("292619"),
+  s = n("474873"),
+  o = n("135906"),
+  l = n("246946");
 {
-  let e = (0, r.isEligibleForWebAudioAPI)({
+  let e = (0, a.isEligibleForWebAudioAPI)({
       location: "SoundUtils"
     }),
     t = n("902653");
   i = e ? t.WebAudioAPISound : t.WebAudioSound
 }
+let u = new r.Logger("SoundUtils");
 
-function l(e, t) {
+function d(e, t) {
   var n;
   let i = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : 1;
-  return u(null !== (n = (0, s.default)(t)[e]) && void 0 !== n ? n : e, e, i)
+  return _(null !== (n = (0, o.default)(t)[e]) && void 0 !== n ? n : e, e, i)
 }
 
-function u(e, t) {
+function _(e, t) {
   let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : 1;
   return new i(e, t, n)
 }
 
-function d(e) {
+function c(e) {
   var t;
   let n = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : 1,
     i = arguments.length > 2 ? arguments[2] : void 0,
     r = arguments.length > 3 ? arguments[3] : void 0;
-  if (o.default.disableSounds) return;
-  let l = u(null !== (t = (0, s.default)(null != r ? r : a.default.getSoundpack())[e]) && void 0 !== t ? t : e, e, n);
-  return null != i ? l.playWithListener().then(e => {
+  if (l.default.disableSounds) return;
+  let a = (0, o.default)(null != r ? r : s.default.getSoundpack());
+  null == a && u.log("Unable to find sound for pack name: ".concat(r));
+  let d = _(null !== (t = a[e]) && void 0 !== t ? t : e, e, n);
+  return null != i ? d.playWithListener().then(e => {
     e && i()
-  }) : l.play(), l
+  }) : d.play(), d
 }
