21f3e9f9f5fa90a5281c194a8fc78c4a71c1fbb1
DiscrapperManager <admin@minerpl.xyz>
2024-05-09 22:43:02 +0200

    canaryRelease: 292000
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25423
     * Scripts: 30199
    Strings: 17839
    Routes: 739
    Experiments Count: 325

---
+++ chunks/346610.js
@@ -0,0 +1,30 @@
+"use strict";
+n.r(t), n.d(t, {
+  MessageForwardingExperiment: function() {
+    return i
+  }
+});
+let i = (0, n("818083").createExperiment)({
+  kind: "user",
+  id: "2024-05_message_forwarding",
+  label: "Message forwarding",
+  defaultConfig: {
+    canForwardMessages: !1,
+    hasInlineForwardButton: !1
+  },
+  treatments: [{
+    id: 1,
+    label: "Enabled",
+    config: {
+      canForwardMessages: !0,
+      hasInlineForwardButton: !1
+    }
+  }, {
+    id: 2,
+    label: "Enabled + inline forward button",
+    config: {
+      canForwardMessages: !0,
+      hasInlineForwardButton: !0
+    }
+  }]
+})
