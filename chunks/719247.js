dc2889c051684dcf7ef3fea8ca5bce860718f8e1
DiscrapperManager <admin@minerpl.xyz>
2024-05-10 21:08:59 +0200

    canaryRelease: 292431
    Main scripts: 21
    Lazy Loaded Scripts: 828
    Chunks:
     * Main Files: 25426
     * Scripts: 29988
    Strings: 17854
    Routes: 740
    Experiments Count: 327

--- chunks/719247.js
+++ chunks/719247.js
@@ -1,77 +1,90 @@
 "use strict";
 n.r(t), n("47120");
-var a, l, s, i, r = n("661869"),
-  o = n("876215"),
-  u = n("423875"),
-  d = n("442837"),
-  c = n("570140"),
-  f = n("158776"),
-  h = n("146282"),
-  m = n("26033"),
-  p = n("561308"),
-  E = n("981631"),
-  C = n("616922");
-let g = new Map;
+var a, l, s, i, r = n("392711"),
+  o = n.n(r),
+  u = n("661869"),
+  d = n("876215"),
+  c = n("423875"),
+  f = n("442837"),
+  h = n("570140"),
+  m = n("158776"),
+  p = n("146282"),
+  E = n("26033"),
+  C = n("561308"),
+  g = n("981631"),
+  S = n("616922");
+let _ = new Map;
 
-function S(e) {
+function T(e) {
   return "".concat(e.author_id, ":").concat(e.id)
 }
 
-function _(e) {
-  let t = new Set;
-  for (let n of e) {
+function I(e) {
+  let t = new Set,
+    n = new Set;
+  for (let a of e) {
     let e = function(e) {
-      return (0, p.isEntryExpired)(e) ? null : (0, p.isEntryActive)(e) && e.author_type === r.ContentInventoryAuthorType.USER ? f.default.getActivities(e.author_id).find(t => {
-        if (t.type === E.ActivityTypes.PLAYING && (0, m.isGamingLikeEntry)(e)) return function(e, t) {
-          let n = e.extra;
-          return null != n && ("application_id" in t && t.application_id === n.application_id || "game_name" in n && t.name === n.game_name)
-        }(e, t);
-        if (t.type === E.ActivityTypes.LISTENING && e.content_type === o.ContentInventoryEntryType.LISTENED_SESSION) {
-          var n, a, l, s, i;
-          return n = e, a = t, (null === (s = n.extra.entries[0]) || void 0 === s ? void 0 : null === (l = s.media) || void 0 === l ? void 0 : l.provider) === u.ContentInventoryListenedMediaProvider.SPOTIFY && (0, C.isSpotifyParty)(null === (i = a.party) || void 0 === i ? void 0 : i.id)
-        }
-        return !1
-      }) : void 0
-    }(n.content);
-    if (void 0 !== e) {
-      let a = S(n.content);
-      e !== g.get(a) && (t.add(a), g.set(a, e))
-    }
+        return (0, C.isEntryExpired)(e) ? null : (0, C.isEntryActive)(e) && e.author_type === u.ContentInventoryAuthorType.USER ? m.default.getActivities(e.author_id).find(t => {
+          if (t.type === g.ActivityTypes.PLAYING && (0, E.isGamingLikeEntry)(e)) return function(e, t) {
+            let n = e.extra;
+            return null != n && ("application_id" in t && t.application_id === n.application_id || "game_name" in n && t.name === n.game_name)
+          }(e, t);
+          if (t.type === g.ActivityTypes.LISTENING && e.content_type === d.ContentInventoryEntryType.LISTENED_SESSION) {
+            var n, a, l, s, i;
+            return n = e, a = t, (null === (s = n.extra.entries[0]) || void 0 === s ? void 0 : null === (l = s.media) || void 0 === l ? void 0 : l.provider) === c.ContentInventoryListenedMediaProvider.SPOTIFY && (0, S.isSpotifyParty)(null === (i = a.party) || void 0 === i ? void 0 : i.id)
+          }
+          return !1
+        }) : void 0
+      }(a.content),
+      l = T(a.content);
+    n.add(l), void 0 !== e && e !== _.get(l) && (t.add(l), _.set(l, e))
   }
-  return t
+  return {
+    updatedKeys: t,
+    visitedKeys: n
+  }
 }
 
-function T() {
+function A() {
   let e = !1,
-    t = new Set;
-  for (let n of h.default.getFeeds().values()) {
-    let a = _(t.size > 0 ? n.entries.filter(e => !t.has(S(e.content))) : n.entries);
-    for (let e of a) t.add(e);
-    e = e || a.size > 0
+    t = Array.from(_.keys()),
+    n = new Set,
+    a = new Set;
+  for (let t of p.default.getFeeds().values()) {
+    let {
+      updatedKeys: l,
+      visitedKeys: s
+    } = I(n.size > 0 ? t.entries.filter(e => !n.has(T(e.content))) : t.entries);
+    for (let e of l) n.add(e);
+    for (let e of s) a.add(e);
+    e = e || l.size > 0
   }
+  for (let n of o().difference(t, [...a])) _.delete(n), e = !0;
   return e
 }
-class I extends(a = d.default.Store) {
+class N extends(a = f.default.Store) {
   initialize() {
-    this.waitFor(h.default, f.default), this.syncWith([f.default], T)
+    this.waitFor(p.default, m.default), this.syncWith([m.default], A)
   }
   getMatchingActivity(e) {
-    return (0, p.isEntryExpired)(e) ? null : g.get(S(e))
+    return (0, C.isEntryExpired)(e) ? null : _.get(T(e))
   }
 }
-i = "ContentInventoryActivityStore", (s = "displayName") in(l = I) ? Object.defineProperty(l, s, {
+i = "ContentInventoryActivityStore", (s = "displayName") in(l = N) ? Object.defineProperty(l, s, {
   value: i,
   enumerable: !0,
   configurable: !0,
   writable: !0
-}) : l[s] = i, t.default = new I(c.default, {
+}) : l[s] = i, t.default = new N(h.default, {
   CONNECTION_OPEN: function() {
-    g.clear()
+    _.clear()
   },
   CONTENT_INVENTORY_SET_FEED: function(e) {
     let {
       feed: t
-    } = e;
-    return _(t.entries).size > 0
+    } = e, {
+      updatedKeys: n
+    } = I(t.entries);
+    return n.size > 0
   }
 })
