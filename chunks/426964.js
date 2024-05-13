42c3e1de9e7bad38e0c8e897babd92eb7ce32517
DiscrapperManager <admin@minerpl.xyz>
2024-05-13 22:14:00 +0200

    canaryRelease: 292808
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25426
     * Scripts: 33493
    Strings: 17867
    Routes: 740
    Experiments Count: 327

--- chunks/426964.js
+++ chunks/426964.js
@@ -2,119 +2,148 @@
 n.r(t);
 var i = n("735250"),
   r = n("470079"),
-  a = n("481060"),
-  s = n("442837"),
-  o = n("100527"),
-  l = n("231467"),
-  u = n("741606"),
-  d = n("703656"),
-  _ = n("314897"),
-  c = n("271383"),
-  E = n("594174"),
-  I = n("970606"),
-  T = n("645896"),
-  f = n("905362"),
-  S = n("353093"),
-  h = n("169559"),
-  A = n("284019"),
-  m = n("981631"),
-  N = n("689938"),
-  p = n("534856");
+  a = n("524437"),
+  s = n("481060"),
+  o = n("442837"),
+  l = n("100527"),
+  u = n("231467"),
+  d = n("605236"),
+  _ = n("741606"),
+  c = n("703656"),
+  E = n("314897"),
+  I = n("271383"),
+  T = n("594174"),
+  f = n("970606"),
+  S = n("645896"),
+  h = n("905362"),
+  A = n("353093"),
+  m = n("954138"),
+  N = n("169559"),
+  p = n("284019"),
+  O = n("981631"),
+  C = n("689938"),
+  R = n("534856");
 
-function O(e) {
+function g(e) {
   e.stopPropagation()
 }
 t.default = r.memo(function(e) {
-  var t, n;
+  var t;
   let {
-    clan: C,
-    isLoading: R,
-    onClose: g,
-    profileViewedAnalytics: L
-  } = e, v = (0, T.useClanInfo)(null !== (n = null == C ? void 0 : C.identityGuildId) && void 0 !== n ? n : null), D = null == C ? void 0 : C.tag, M = (0, h.default)("clan_guild_profile"), y = null == C ? void 0 : C.identityGuildId, P = (0, s.useStateFromStores)([_.default], () => _.default.getId()), U = (0, s.useStateFromStores)([c.default], () => null != y ? c.default.getMember(y, P) : null, [y, P]), b = (0, s.useStateFromStores)([E.default], () => E.default.getUser(P), [P]), G = (null == U ? void 0 : U.joinedAt) != null, w = (0, S.isGuildAdoptedUserClanIdentity)(y, null == b ? void 0 : b.clan), B = null != (0, u.default)(y);
+    clan: n,
+    isLoading: L,
+    onClose: v,
+    profileViewedAnalytics: D
+  } = e, M = null == n ? void 0 : n.identityGuildId, y = (0, S.useClanInfo)(null != M ? M : null), P = null == n ? void 0 : n.tag, U = (0, N.default)("clan_guild_profile"), b = (0, m.default)("base_clan_tag_chiplet"), G = (0, o.useStateFromStores)([E.default], () => E.default.getId()), w = (0, o.useStateFromStores)([I.default], () => null != M ? I.default.getMember(M, G) : null, [M, G]), B = (0, d.useIsDismissibleContentDismissed)(a.DismissibleContent.CLAN_TAG_WAITLIST), k = (0, o.useStateFromStores)([T.default], () => T.default.getUser(G), [G]), V = (null == w ? void 0 : w.joinedAt) != null, x = (0, A.isGuildAdoptedUserClanIdentity)(M, null == k ? void 0 : k.clan), F = null != (0, _.default)(M);
   r.useEffect(() => {
-    null != y && (0, I.trackClanProfileViewed)({
-      guildId: y,
-      hasJoinRequest: B,
-      ...L,
-      location: o.default.CLAN_GUILD_PROFILE
+    null != M && (0, f.trackClanProfileViewed)({
+      guildId: M,
+      hasJoinRequest: F,
+      ...D,
+      location: l.default.CLAN_GUILD_PROFILE
     })
-  }, [y, B, L]);
-  let k = r.useCallback(e => {
-      null != y && (e.stopPropagation(), e.preventDefault(), null == g || g(), (0, f.openAdoptClanIdentityModal)(y))
-    }, [y, g]),
-    V = r.useCallback(e => {
-      if (null != v && null != y) e.stopPropagation(), e.preventDefault(), null == g || g(), !B && (0, f.openClanApplyFlow)(y, v, {
-        source: null == L ? void 0 : L.source,
-        messageId: null == L ? void 0 : L.messageId,
-        tagUserId: null == L ? void 0 : L.tagUserId,
-        location: o.default.CLAN_GUILD_PROFILE
+  }, [M, F, D]);
+  let H = r.useCallback(() => {
+      !B && ((0, f.trackClanApplyWaitlist)({
+        originGuildId: null == D ? void 0 : D.guildId,
+        tagGuildId: M,
+        messageId: null == D ? void 0 : D.messageId,
+        tagUserId: null == D ? void 0 : D.tagUserId,
+        location: l.default.CLAN_GUILD_PROFILE
+      }), (0, s.showToast)((0, s.createToast)(C.default.Messages.CLAN_USER_WAITLIST_TOAST_SUCCESS, s.ToastType.SUCCESS)))
+    }, [M, B, D]),
+    Y = r.useCallback(e => {
+      null != M && (e.stopPropagation(), e.preventDefault(), null == v || v(), (0, h.openAdoptClanIdentityModal)(M))
+    }, [M, v]),
+    j = r.useCallback(e => {
+      if (null != y && null != M) e.stopPropagation(), e.preventDefault(), null == v || v(), !F && (0, h.openClanApplyFlow)(M, y, {
+        source: null == D ? void 0 : D.source,
+        messageId: null == D ? void 0 : D.messageId,
+        tagUserId: null == D ? void 0 : D.tagUserId,
+        location: l.default.CLAN_GUILD_PROFILE
       })
-    }, [v, y, B, g, L]),
-    x = r.useCallback(() => {
-      null != y && (0, d.transitionTo)(m.Routes.GUILD_MEMBER_VERIFICATION(y))
-    }, [y]),
-    F = r.useCallback(() => {
-      null != y && (0, d.transitionToGuild)(y)
-    }, [y]),
-    H = r.useMemo(() => {
+    }, [y, M, F, v, D]),
+    W = r.useCallback(() => {
+      null != M && (0, c.transitionTo)(O.Routes.GUILD_MEMBER_VERIFICATION(M))
+    }, [M]),
+    K = r.useCallback(() => {
+      null != M && (0, c.transitionToGuild)(M)
+    }, [M]),
+    z = r.useMemo(() => {
       var e, t;
       return {
-        color: a.Button.Colors.BRAND,
-        size: a.Button.Sizes.SMALL,
+        color: s.Button.Colors.BRAND,
+        size: s.Button.Sizes.SMALL,
         fullWidth: !0,
-        className: p.button,
-        themeColor: null !== (t = null == v ? void 0 : null === (e = v.branding) || void 0 === e ? void 0 : e.primaryColor) && void 0 !== t ? t : null
+        className: R.button,
+        themeColor: null !== (t = null == y ? void 0 : null === (e = y.branding) || void 0 === e ? void 0 : e.primaryColor) && void 0 !== t ? t : null
       }
-    }, [null == v ? void 0 : null === (t = v.branding) || void 0 === t ? void 0 : t.primaryColor]),
-    Y = r.useMemo(() => {
+    }, [null == y ? void 0 : null === (t = y.branding) || void 0 === t ? void 0 : t.primaryColor]),
+    Z = r.useMemo(() => {
       switch (!0) {
-        case !M:
+        case !b:
           return null;
-        case !G && B:
-          return (0, i.jsx)(A.default, {
-            onClick: x,
-            ...H,
-            children: N.default.Messages.CLAN_USER_APPLICATION_EXISTS_TAG_GUILD_PROFILE_CTA
+        case !U && !B:
+          return (0, i.jsx)(s.Button, {
+            onClick: H,
+            color: s.Button.Colors.PRIMARY,
+            size: s.Button.Sizes.SMALL,
+            fullWidth: !0,
+            children: C.default.Messages.CLAN_USER_APPLY_TO_WAITLIST
           });
-        case !G:
-          return (0, i.jsx)(A.default, {
-            onClick: V,
-            ...H,
-            children: N.default.Messages.CLAN_USER_APPLY_TAG_GUILD_PROFILE_CTA
+        case !U && B:
+          return (0, i.jsx)(s.Button, {
+            onClick: H,
+            color: s.Button.Colors.PRIMARY,
+            size: s.Button.Sizes.SMALL,
+            fullWidth: !0,
+            disabled: !0,
+            children: C.default.Messages.CLAN_USER_WAITLISTED
           });
-        case G && !w:
-          return (0, i.jsx)(A.default, {
-            onClick: k,
-            ...H,
-            children: N.default.Messages.CLAN_USER_ADOPT_TAG_GUILD_PROFILE_CTA
+        case !V && F:
+          return (0, i.jsx)(p.default, {
+            onClick: W,
+            ...z,
+            children: C.default.Messages.CLAN_USER_APPLICATION_EXISTS_TAG_GUILD_PROFILE_CTA
           });
-        case G && w:
-          return (0, i.jsx)(A.default, {
-            onClick: F,
-            ...H,
-            children: N.default.Messages.CLAN_USER_GO_TO_CLAN_CTA
+        case !V:
+          return (0, i.jsx)(p.default, {
+            onClick: j,
+            ...z,
+            children: C.default.Messages.CLAN_USER_APPLY_TAG_GUILD_PROFILE_CTA
+          });
+        case V && !x:
+          return (0, i.jsx)(p.default, {
+            onClick: Y,
+            ...z,
+            children: C.default.Messages.CLAN_USER_ADOPT_TAG_GUILD_PROFILE_CTA
+          });
+        case V && x:
+          return (0, i.jsx)(p.default, {
+            onClick: K,
+            ...z,
+            children: C.default.Messages.CLAN_USER_GO_TO_CLAN_CTA
           });
         default:
           return null
       }
-    }, [H, k, V, x, F, B, w, G, M]),
-    j = r.useMemo(() => ({
-      className: p.container,
-      onClick: O
+    }, [z, Y, j, W, K, F, x, U, B, H, b, V]),
+    X = r.useMemo(() => ({
+      className: R.container,
+      onClick: g
     }), []);
-  return null == D || null == v || null == y || R ? (0, i.jsx)("div", {
-    ...j,
-    children: (0, i.jsx)(a.Spinner, {})
+  return null == P || null == y || null == M || L ? (0, i.jsx)("div", {
+    ...X,
+    children: (0, i.jsx)(s.Spinner, {})
   }) : (0, i.jsxs)("div", {
-    ...j,
-    children: [(0, i.jsx)(l.ClanDiscoveryCardView, {
-      clan: v,
-      className: p.cardContainer,
-      isMember: G
-    }), null != Y && (0, i.jsx)("div", {
-      className: p.buttonContainer,
-      children: Y
+    ...X,
+    children: [(0, i.jsx)(u.ClanDiscoveryCardView, {
+      clan: y,
+      className: R.cardContainer,
+      isMember: V
+    }), null != Z && (0, i.jsx)("div", {
+      className: R.buttonContainer,
+      children: Z
     })]
   })
 })
--- chunks/970606.js
+++ chunks/970606.js
@@ -1,149 +1,156 @@
 "use strict";
 n.r(t), n.d(t, {
   getProgressStepAnalyticsName: function() {
-    return f
-  },
-  trackClanAdminInviteClicked: function() {
-    return m
-  },
-  trackClanAdminInviteViewed: function() {
     return A
   },
-  trackClanAdoptIdentity: function() {
-    return d
-  },
-  trackClanApplicationAction: function() {
-    return R
-  },
-  trackClanApplicationNavigation: function() {
-    return C
-  },
-  trackClanApplicationViewed: function() {
-    return g
-  },
-  trackClanApplyToJoinViewed: function() {
-    return u
-  },
-  trackClanDiscoveryCardClicked: function() {
-    return N
-  },
-  trackClanDiscoveryUserNuxClicked: function() {
-    return h
-  },
-  trackClanDiscoveryUserNuxViewed: function() {
-    return S
-  },
-  trackClanDiscoveryViewed: function() {
-    return T
-  },
-  trackClanProfileViewed: function() {
-    return l
-  },
-  trackClanSendInterviewMessage: function() {
+  trackClanAdminInviteClicked: function() {
     return O
   },
-  trackConvertStepViewed: function() {
-    return _
-  },
-  trackConvertSuccessModalConfirm: function() {
-    return I
-  },
-  trackConvertSuccessModalViewed: function() {
-    return E
-  },
-  trackMemberVerificationApplicationViewed: function() {
+  trackClanAdminInviteViewed: function() {
     return p
   },
-  trackSettingsViewed: function() {
+  trackClanAdoptIdentity: function() {
+    return E
+  },
+  trackClanApplicationAction: function() {
+    return v
+  },
+  trackClanApplicationNavigation: function() {
+    return L
+  },
+  trackClanApplicationViewed: function() {
+    return D
+  },
+  trackClanApplyToJoinViewed: function() {
     return c
+  },
+  trackClanApplyWaitlist: function() {
+    return M
+  },
+  trackClanDiscoveryCardClicked: function() {
+    return C
+  },
+  trackClanDiscoveryUserNuxClicked: function() {
+    return N
+  },
+  trackClanDiscoveryUserNuxViewed: function() {
+    return m
+  },
+  trackClanDiscoveryViewed: function() {
+    return h
+  },
+  trackClanProfileViewed: function() {
+    return _
+  },
+  trackClanSendInterviewMessage: function() {
+    return g
+  },
+  trackConvertStepViewed: function() {
+    return I
+  },
+  trackConvertSuccessModalConfirm: function() {
+    return S
+  },
+  trackConvertSuccessModalViewed: function() {
+    return f
+  },
+  trackMemberVerificationApplicationViewed: function() {
+    return R
+  },
+  trackSettingsViewed: function() {
+    return T
   }
 });
-var i = n("314897"),
-  r = n("271383"),
-  a = n("626135"),
-  s = n("308083"),
-  o = n("981631");
+var i = n("524437"),
+  r = n("605236"),
+  a = n("314897"),
+  s = n("271383"),
+  o = n("626135"),
+  l = n("308083"),
+  u = n("981631"),
+  d = n("921944");
 
-function l(e) {
+function _(e) {
   let {
     guildId: t,
     hasJoinRequest: n,
-    source: s,
-    messageId: l,
-    tagUserId: u
-  } = e, d = i.default.getId(), _ = r.default.getMember(t, i.default.getId()), c = (null == _ ? void 0 : _.joinedAt) != null;
-  a.default.track(o.AnalyticEvents.CLAN_PROFILE_VIEWED, {
+    source: i,
+    messageId: r,
+    tagUserId: l,
+    location: d
+  } = e, _ = a.default.getId(), c = s.default.getMember(t, a.default.getId()), E = (null == c ? void 0 : c.joinedAt) != null;
+  o.default.track(u.AnalyticEvents.CLAN_PROFILE_VIEWED, {
     guild_id: t,
-    is_member: c,
+    is_member: E,
     has_join_request: n,
-    source: s,
-    location: s,
-    viewing_user_id: d,
-    tag_owner_user_id: u,
-    message_id: l
+    source: i,
+    location: d,
+    viewing_user_id: _,
+    tag_owner_user_id: l,
+    message_id: r
   })
 }
 
-function u(e) {
+function c(e) {
   let {
     guildId: t,
     source: n,
-    location: r,
-    messageId: s,
-    tagUserId: l
-  } = e, u = i.default.getId();
-  a.default.track(o.AnalyticEvents.CLAN_APPLY_TO_JOIN_VIEWED, {
+    location: i,
+    messageId: r,
+    tagUserId: s
+  } = e, l = a.default.getId();
+  o.default.track(u.AnalyticEvents.CLAN_APPLY_TO_JOIN_VIEWED, {
     guild_id: t,
     source: n,
-    location: r,
-    viewing_user_id: u,
-    tag_owner_user_id: l,
-    message_id: s
+    location: i,
+    viewing_user_id: l,
+    tag_owner_user_id: s,
+    message_id: r
   })
 }
 
-function d(e) {
+function E(e) {
   let {
     guildId: t,
     userId: n,
     source: i
   } = e;
-  a.default.track(o.AnalyticEvents.CLAN_ADOPT_IDENTITY, {
+  o.default.track(u.AnalyticEvents.CLAN_ADOPT_IDENTITY, {
     guild_id: t,
     user_id: n,
     source: i
   })
 }
 
-function _(e, t) {
-  a.default.track(o.AnalyticEvents.CLAN_CONVERT_STEP_VIEWED, {
+function I(e, t) {
+  o.default.track(u.AnalyticEvents.CLAN_CONVERT_STEP_VIEWED, {
     guild_id: e,
     step: t
   })
 }
 
-function c(e, t) {
-  a.default.track(o.AnalyticEvents.CLAN_SETTINGS_VIEWED, {
+function T(e, t) {
+  o.default.track(u.AnalyticEvents.CLAN_SETTINGS_VIEWED, {
     guild_id: e,
     page: t
   })
 }
 
-function E(e) {
-  a.default.track(o.AnalyticEvents.CLAN_CONVERT_SUCCESS_MODAL_VIEWED, {
+function f(e) {
+  o.default.track(u.AnalyticEvents.CLAN_CONVERT_SUCCESS_MODAL_VIEWED, {
     guild_id: e
   })
 }
 
-function I(e, t) {
-  a.default.track(o.AnalyticEvents.CLAN_CONVERT_SUCCESS_MODAL_CONFIRM, {
+function S(e, t) {
+  o.default.track(u.AnalyticEvents.CLAN_CONVERT_SUCCESS_MODAL_CONFIRM, {
     guild_id: e,
     enable_tag: t
   })
 }
 
-function T(e, t, n) {
-  a.default.track(o.AnalyticEvents.CLAN_DISCOVERY_VIEWED, {
+function h(e, t, n) {
+  o.default.track(u.AnalyticEvents.CLAN_DISCOVERY_VIEWED, {
     guild_ids: e,
     section: t,
     application_ids: n.games,
@@ -152,139 +159,160 @@ function T(e, t, n) {
   })
 }
 
-function f(e) {
+function A(e) {
   switch (e) {
-    case s.ClanSetupSteps.GAMES:
+    case l.ClanSetupSteps.GAMES:
       return "games";
-    case s.ClanSetupSteps.PLAYSTYLE:
+    case l.ClanSetupSteps.PLAYSTYLE:
       return "playstyle";
-    case s.ClanSetupSteps.UTILITY_TRAITS:
+    case l.ClanSetupSteps.UTILITY_TRAITS:
       return "utility_traits";
-    case s.ClanSetupSteps.INTERESTS:
+    case l.ClanSetupSteps.INTERESTS:
       return "interests";
-    case s.ClanSetupSteps.DESCRIPTION:
+    case l.ClanSetupSteps.DESCRIPTION:
       return "description";
-    case s.ClanSetupSteps.CUSTOMIZE_TAG_BADGE:
+    case l.ClanSetupSteps.CUSTOMIZE_TAG_BADGE:
       return "tag";
-    case s.ClanSetupSteps.CUSTOMIZE_BANNER:
+    case l.ClanSetupSteps.CUSTOMIZE_BANNER:
       return "banner";
-    case s.ClanSetupSteps.MEMBER_APPLICATION:
+    case l.ClanSetupSteps.MEMBER_APPLICATION:
       return "member_application";
     default:
       return e
   }
 }
 
-function S() {
-  a.default.track(o.AnalyticEvents.CLAN_DISCOVERY_USER_NUX_VIEWED)
+function m() {
+  o.default.track(u.AnalyticEvents.CLAN_DISCOVERY_USER_NUX_VIEWED)
 }
 
-function h() {
-  a.default.track(o.AnalyticEvents.CLAN_DISCOVERY_USER_NUX_CLICKED)
-}
-
-function A(e) {
-  let {
-    guildId: t,
-    location: n
-  } = e;
-  a.default.track(o.AnalyticEvents.CLAN_ADMIN_INVITE_VIEWED, {
-    guild_id: t,
-    location: n
-  })
-}
-
-function m(e) {
-  let {
-    guildId: t,
-    location: n
-  } = e;
-  a.default.track(o.AnalyticEvents.CLAN_ADMIN_INVITE_CLICKED, {
-    guild_id: t,
-    location: n
-  })
-}
-
-function N(e) {
-  let {
-    guildId: t,
-    isMember: n,
-    hasJoinRequest: i,
-    affinity: r,
-    index: s
-  } = e;
-  a.default.track(o.AnalyticEvents.CLAN_DISCOVERY_CARD_CLICKED, {
-    guild_id: t,
-    is_member: n,
-    has_join_request: i,
-    affinity: r,
-    index: s
-  })
+function N() {
+  o.default.track(u.AnalyticEvents.CLAN_DISCOVERY_USER_NUX_CLICKED)
 }
 
 function p(e) {
-  a.default.track(o.AnalyticEvents.MEMBER_VERIFICATION_APPLICATION_VIEWED, {
-    guild_id: e
+  let {
+    guildId: t,
+    location: n
+  } = e;
+  o.default.track(u.AnalyticEvents.CLAN_ADMIN_INVITE_VIEWED, {
+    guild_id: t,
+    location: n
   })
 }
 
 function O(e) {
-  var t;
   let {
-    guildId: n,
-    messageId: s,
-    channelId: l,
-    joinRequestStatus: u,
-    joinRequestUserId: d
-  } = e, _ = i.default.getId(), c = (null === (t = r.default.getMember(n, _)) || void 0 === t ? void 0 : t.joinedAt) != null;
-  a.default.track(o.AnalyticEvents.CLAN_SEND_INTERVIEW_MESSAGE, {
-    guild_id: n,
-    channel_id: l,
-    message_id: s,
-    message_user_id: _,
-    is_member: c,
-    join_request_status: u,
-    join_request_user_id: d
+    guildId: t,
+    location: n
+  } = e;
+  o.default.track(u.AnalyticEvents.CLAN_ADMIN_INVITE_CLICKED, {
+    guild_id: t,
+    location: n
   })
 }
 
 function C(e) {
   let {
     guildId: t,
-    source: n,
-    tab: i
+    isMember: n,
+    hasJoinRequest: i,
+    affinity: r,
+    index: a
   } = e;
-  a.default.track(o.AnalyticEvents.CLAN_APPLICATION_NAVIGATION, {
+  o.default.track(u.AnalyticEvents.CLAN_DISCOVERY_CARD_CLICKED, {
     guild_id: t,
-    source: n,
-    tab: i
+    is_member: n,
+    has_join_request: i,
+    affinity: r,
+    index: a
   })
 }
 
 function R(e) {
-  let {
-    guildId: t,
-    actionType: n,
-    applicationUserId: r
-  } = e;
-  a.default.track(o.AnalyticEvents.CLAN_APPLICATION_ACTION, {
-    guild_id: t,
-    action_type: n,
-    application_user_id: r,
-    viewing_user_id: i.default.getId()
+  o.default.track(u.AnalyticEvents.MEMBER_VERIFICATION_APPLICATION_VIEWED, {
+    guild_id: e
   })
 }
 
 function g(e) {
+  var t;
+  let {
+    guildId: n,
+    messageId: i,
+    channelId: r,
+    joinRequestStatus: l,
+    joinRequestUserId: d
+  } = e, _ = a.default.getId(), c = (null === (t = s.default.getMember(n, _)) || void 0 === t ? void 0 : t.joinedAt) != null;
+  o.default.track(u.AnalyticEvents.CLAN_SEND_INTERVIEW_MESSAGE, {
+    guild_id: n,
+    channel_id: r,
+    message_id: i,
+    message_user_id: _,
+    is_member: c,
+    join_request_status: l,
+    join_request_user_id: d
+  })
+}
+
+function L(e) {
+  let {
+    guildId: t,
+    source: n,
+    tab: i
+  } = e;
+  o.default.track(u.AnalyticEvents.CLAN_APPLICATION_NAVIGATION, {
+    guild_id: t,
+    source: n,
+    tab: i
+  })
+}
+
+function v(e) {
+  let {
+    guildId: t,
+    actionType: n,
+    applicationUserId: i
+  } = e;
+  o.default.track(u.AnalyticEvents.CLAN_APPLICATION_ACTION, {
+    guild_id: t,
+    action_type: n,
+    application_user_id: i,
+    viewing_user_id: a.default.getId()
+  })
+}
+
+function D(e) {
   let {
     guildId: t,
     applicationUserId: n,
-    applicationStatus: r
+    applicationStatus: i
   } = e;
-  a.default.track(o.AnalyticEvents.CLAN_APPLICATION_VIEWED, {
+  o.default.track(u.AnalyticEvents.CLAN_APPLICATION_VIEWED, {
     guild_id: t,
-    viewing_user_id: i.default.getId(),
+    viewing_user_id: a.default.getId(),
     application_user_id: n,
-    application_status: r
+    application_status: i
   })
+}
+
+function M(e) {
+  let {
+    originGuildId: t,
+    tagGuildId: n,
+    tagUserId: s,
+    messageId: l,
+    location: _
+  } = e;
+  !(0, r.isDismissibleContentDismissed)(i.DismissibleContent.CLAN_TAG_WAITLIST) && (o.default.track(u.AnalyticEvents.CLAN_APPLY_WAITLIST, {
+    user_id: a.default.getId(),
+    origin_guild_id: t,
+    tag_guild_id: n,
+    tag_user_id: s,
+    message_id: l,
+    location: _
+  }), (0, r.markDismissibleContentAsDismissed)(i.DismissibleContent.CLAN_TAG_WAITLIST, {
+    dismissAction: d.ContentDismissActionType.TAKE_ACTION,
+    forceTrack: !0
+  }))
 }
