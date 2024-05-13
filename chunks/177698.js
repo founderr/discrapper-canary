a3bab543c4f054172ca01e35417b2469872a4bc0
DiscrapperManager <admin@minerpl.xyz>
2024-05-13 19:47:30 +0200

    canaryRelease: 292747
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25426
     * Scripts: 33493
    Strings: 17864
    Routes: 740
    Experiments Count: 327

--- chunks/177698.js
+++ chunks/177698.js
@@ -8,62 +8,67 @@ var s = n("481060"),
   r = n("69580"),
   o = n("358085"),
   u = n("998502"),
-  d = n("703912"),
-  c = n("981631");
-t.default = (0, d.default)(e => {
+  d = n("996106"),
+  c = n("703912"),
+  f = n("981631");
+t.default = (0, c.default)(e => {
   var t;
   let {
     clientId: n,
     authorizations: o,
     scopes: u,
-    parsedPermissions: d,
-    responseType: c,
-    redirectUri: f,
-    codeChallenge: E,
-    codeChallengeMethod: h,
-    state: _,
-    guildId: C,
-    channelId: m,
-    prompt: S,
-    disableGuildSelect: p,
-    disclosures: I,
-    integrationType: g
-  } = e, T = "OAuth2Authorize_".concat(n, "_").concat(C, "_").concat(m), A = null != g ? null == o ? void 0 : o.get(g) : void 0, N = null !== (t = null == A ? void 0 : A.application) && void 0 !== t ? t : i.default.getApplication(n);
-  return new Promise(e => {
-    let t = t => {
+    parsedPermissions: c,
+    responseType: E,
+    redirectUri: h,
+    codeChallenge: _,
+    codeChallengeMethod: C,
+    state: m,
+    guildId: S,
+    channelId: p,
+    prompt: I,
+    disableGuildSelect: g,
+    disclosures: T,
+    integrationType: A
+  } = e, N = "OAuth2Authorize_".concat(n, "_").concat(S, "_").concat(p), v = null != A ? null == o ? void 0 : o.get(A) : void 0, R = null !== (t = null == v ? void 0 : v.application) && void 0 !== t ? t : i.default.getApplication(n);
+  return new Promise((e, t) => {
+    let i = n => {
       let {
-        location: n
-      } = t;
-      e(n)
+        location: a
+      } = n;
+      null != location ? e(a) : t(new d.default({
+        errorCode: f.RPCErrors.OAUTH2_ERROR
+      }, "User cancelled authorization"))
     };
     (0, s.openModal)(e => (0, a.jsx)(r.OAuth2AuthorizeModal, {
       ...e,
       authorizations: o,
       clientId: n,
       scopes: null != u ? u : [],
-      disclosures: null != I ? I : [],
-      callback: t,
-      responseType: c,
-      redirectUri: f,
-      codeChallenge: E,
-      codeChallengeMethod: h,
-      state: _,
-      permissions: d,
-      guildId: C,
-      channelId: m,
-      prompt: S,
-      disableGuildSelect: "boolean" == typeof p ? p : "true" === p,
-      integrationType: g
+      disclosures: null != T ? T : [],
+      callback: i,
+      responseType: E,
+      redirectUri: h,
+      codeChallenge: _,
+      codeChallengeMethod: C,
+      state: m,
+      permissions: c,
+      guildId: S,
+      channelId: p,
+      prompt: I,
+      disableGuildSelect: "boolean" == typeof g ? g : "true" === g,
+      integrationType: A
     }), {
-      modalKey: T,
+      modalKey: N,
       onCloseRequest: () => {
-        (0, s.closeModal)(T), e(void 0)
+        (0, s.closeModal)(N), t(new d.default({
+          errorCode: f.RPCErrors.OAUTH2_ERROR
+        }, "User cancelled authorization"))
       }
-    }, null != N && (0, l.default)(N, m) ? s.POPOUT_MODAL_CONTEXT : s.DEFAULT_MODAL_CONTEXT)
+    }, null != R && (0, l.default)(R, p) ? s.POPOUT_MODAL_CONTEXT : s.DEFAULT_MODAL_CONTEXT)
   })
 }, function(e, t) {
   if (o.isPlatformEmbedded) {
-    let n = (0, l.default)(e, t) ? c.PopoutWindowKeys.CHANNEL_CALL_POPOUT : null;
+    let n = (0, l.default)(e, t) ? f.PopoutWindowKeys.CHANNEL_CALL_POPOUT : null;
     (0, o.isWindows)() ? u.default.minimize(n): u.default.restore(n), u.default.focus(n)
   }
 })
