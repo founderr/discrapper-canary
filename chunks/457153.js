2a8fd32502fb8a4c3345c71f2507dea1bf1b1de1
DiscrapperManager <admin@minerpl.xyz>
2024-05-14 17:35:14 +0200

    canaryRelease: 293059
    Main scripts: 21
    Lazy Loaded Scripts: 825
    Chunks:
     * Main Files: 25420
     * Scripts: 33497
    Strings: 17879
    Routes: 740
    Experiments Count: 328

--- chunks/457153.js
+++ chunks/457153.js
@@ -1,78 +1,114 @@
 "use strict";
 n.r(t);
-var a = n("735250");
-n("470079");
-var s = n("120356"),
-  i = n.n(s),
-  l = n("685626"),
-  r = n("481060"),
-  o = n("497505"),
-  u = n("685613"),
-  d = n("340100"),
-  c = n("644646"),
-  f = n("898170"),
-  E = n("689938"),
-  h = n("481900");
+var a = n("735250"),
+  s = n("470079"),
+  i = n("120356"),
+  l = n.n(i),
+  r = n("685626"),
+  o = n("481060"),
+  u = n("810090"),
+  d = n("497505"),
+  c = n("918701"),
+  f = n("685613"),
+  E = n("340100"),
+  h = n("644646"),
+  _ = n("78826"),
+  C = n("898170"),
+  m = n("689938"),
+  S = n("481900");
 t.default = function(e) {
-  var t, s, _;
+  var t, i, p;
   let {
-    className: C,
-    expansionSpring: m,
-    isExpanded: S,
-    isExpansionAnimationComplete: p,
-    quest: g,
-    useReducedMotion: I
-  } = e, T = (null === (t = g.userStatus) || void 0 === t ? void 0 : t.enrolledAt) != null, A = (null !== (_ = null === (s = g.userStatus) || void 0 === s ? void 0 : s.streamProgressSeconds) && void 0 !== _ ? _ : 0) > 0;
-  return (0, a.jsxs)(l.animated.div, {
-    "aria-hidden": S && p,
-    className: i()(C, h.contentCollapsed, {
-      [h.contentCollapsedExpanded]: S,
-      [h.contentCollapsedAccepted]: T
+    className: g,
+    expansionSpring: I,
+    isExpanded: T,
+    isExpansionAnimationComplete: A,
+    quest: N,
+    useReducedMotion: v
+  } = e, R = (null === (t = N.userStatus) || void 0 === t ? void 0 : t.enrolledAt) != null, L = (null !== (p = null === (i = N.userStatus) || void 0 === i ? void 0 : i.streamProgressSeconds) && void 0 !== p ? p : 0) > 0, O = s.useMemo(() => (0, c.isAssetAnimated)(N.config.assets.questBarHero), [N]), y = (0, c.getQuestBarHeroAssetUrl)(N);
+  return (0, a.jsxs)(r.animated.div, {
+    "aria-hidden": T && A,
+    className: l()(g, S.contentCollapsed, {
+      [S.contentCollapsedExpanded]: T,
+      [S.contentCollapsedAccepted]: R
     }),
     style: {
-      opacity: m.to({
+      opacity: I.to({
         range: [0, 1],
         output: [1, 0]
       })
     },
-    children: [(0, a.jsx)(f.QuestBarBackgroundAnimation, {
-      className: h.background,
-      animationClassName: h.backgroundAnimation,
-      quest: g,
-      useReducedMotion: I
+    children: [(0, a.jsxs)("div", {
+      className: S.heroAssetWrapper,
+      children: [(0, a.jsx)("div", {
+        className: S.heroAssetDarken
+      }), (0, a.jsx)("div", {
+        className: S.heroAssetTint,
+        style: {
+          backgroundColor: N.config.colors.primary
+        }
+      }), O ? (0, a.jsx)(_.QuestsAsset, {
+        id: "QuestBarV2ContentExpanded_heroAnimated",
+        children: e => (0, a.jsx)(u.default, {
+          ref: e,
+          autoPlay: !1,
+          loop: !0,
+          muted: !0,
+          playsInline: !0,
+          className: S.heroAsset,
+          controls: !1,
+          children: (0, a.jsx)("source", {
+            src: y,
+            type: (0, c.getVideoAssetMimeType)(y)
+          })
+        })
+      }) : (0, a.jsx)(_.QuestsAsset, {
+        id: "QuestBarV2ContentExpanded_heroStatic",
+        children: e => (0, a.jsx)("img", {
+          ref: e,
+          alt: "",
+          className: S.heroAsset,
+          src: y
+        })
+      })]
+    }), (0, a.jsx)(C.QuestBarBackgroundAnimation, {
+      className: S.background,
+      animationClassName: S.backgroundAnimation,
+      quest: N,
+      useReducedMotion: v
     }), (0, a.jsx)("div", {
-      className: h.contentCollapsedWrapper,
-      children: T ? (0, a.jsxs)("div", {
-        className: h.questProgressWrapper,
-        children: [(0, a.jsx)(c.default, {
-          className: h.questProgressRewardTile,
-          quest: g,
-          questContent: o.QuestContent.QUEST_BAR,
+      className: S.contentCollapsedWrapper,
+      children: R ? (0, a.jsxs)("div", {
+        className: S.questProgressWrapper,
+        children: [(0, a.jsx)(h.default, {
+          className: S.questProgressRewardTile,
+          quest: N,
+          questContent: d.QuestContent.QUEST_BAR,
           autoplay: !1
-        }), A ? (0, a.jsx)(d.default, {
-          className: h.questProgressBar,
-          quest: g
-        }) : (0, a.jsx)(r.Text, {
-          className: h.questProgressHint,
+        }), L ? (0, a.jsx)(E.default, {
+          className: S.questProgressBar,
+          quest: N
+        }) : (0, a.jsx)(o.Text, {
+          className: S.questProgressHint,
           color: "always-white",
           variant: "text-sm/semibold",
-          children: E.default.Messages.QUESTS_COMPLETION_PROGRESS_NOT_STARTED
+          children: m.default.Messages.QUESTS_COMPLETION_PROGRESS_NOT_STARTED
         })]
       }) : (0, a.jsxs)("div", {
-        className: h.preEnrollmentWrapper,
+        className: S.preEnrollmentWrapper,
         children: [(0, a.jsx)("img", {
           alt: "",
-          className: h.wreathIcon,
+          className: S.wreathIcon,
           src: n("896145")
-        }), (0, a.jsx)(u.default, {
-          className: h.partnerBranding,
-          quest: g,
+        }), (0, a.jsx)(f.default, {
+          className: S.partnerBranding,
+          quest: N,
           withGameTile: !1
-        }), (0, a.jsx)(r.Text, {
+        }), (0, a.jsx)(o.Text, {
           color: "always-white",
           variant: "text-xs/medium",
-          className: h.questAvailable,
-          children: E.default.Messages.QUEST_AVAILABLE
+          className: S.questAvailable,
+          children: m.default.Messages.QUEST_AVAILABLE
         })]
       })
     })]
