a94c32bc80586229984693168d218dda21caa26a
DiscrapperManager <admin@minerpl.xyz>
2024-05-14 22:18:02 +0200

    canaryRelease: 293268
    Main scripts: 21
    Lazy Loaded Scripts: 825
    Chunks:
     * Main Files: 25453
     * Scripts: 33305
    Strings: 17891
    Routes: 740
    Experiments Count: 329

--- chunks/901952.js
+++ chunks/901952.js
@@ -1,89 +1,94 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return m
+    return p
   }
 }), n("411104"), n("47120");
 var i = n("735250"),
   r = n("470079"),
-  a = n("442837"),
-  s = n("493683"),
-  o = n("904245"),
-  l = n("906732"),
-  u = n("541716"),
-  d = n("752305"),
-  _ = n("893718"),
-  c = n("957730"),
+  a = n("493683"),
+  s = n("904245"),
+  o = n("906732"),
+  l = n("541716"),
+  u = n("752305"),
+  d = n("893718"),
+  _ = n("957730"),
+  c = n("131704"),
   E = n("592125"),
   I = n("5192"),
   T = n("838440"),
   f = n("785717"),
-  S = n("689938"),
-  h = n("112147");
-let A = async e => {
-  let {
-    userId: t,
-    content: n,
-    location: i
-  } = e, {
-    valid: r,
-    failureReason: a
-  } = await (0, T.applyChatRestrictions)({
-    type: u.ChatInputTypes.NORMAL,
-    content: n,
-    channel: null
-  });
-  if (!r) throw Error(a);
-  let l = await s.default.openPrivateChannel(t, !1, !1, i),
-    d = E.default.getChannel(l);
-  if (null == d) throw Error("Failed to open private channel");
-  let _ = c.default.parse(d, n);
-  return o.default.sendMessage(d.id, _)
-};
+  S = n("981631"),
+  h = n("689938"),
+  A = n("112147");
+let m = (0, c.createChannelRecord)({
+    id: "1",
+    type: S.ChannelTypes.DM
+  }),
+  N = async e => {
+    let {
+      userId: t,
+      content: n,
+      location: i
+    } = e, {
+      valid: r,
+      failureReason: o
+    } = await (0, T.applyChatRestrictions)({
+      type: l.ChatInputTypes.NORMAL,
+      content: n,
+      channel: null
+    });
+    if (!r) throw Error(o);
+    let u = await a.default.openPrivateChannel(t, !1, !1, i),
+      d = E.default.getChannel(u);
+    if (null == d) throw Error("Failed to open private channel");
+    let c = _.default.parse(d, n);
+    return s.default.sendMessage(d.id, c)
+  };
 
-function m(e) {
+function p(e) {
   let {
     user: t,
     guildId: n,
-    channelId: s,
-    onClose: o
+    channelId: a,
+    onClose: s
   } = e, {
-    newestAnalyticsLocation: c
-  } = (0, l.default)(), {
-    trackUserProfileAction: T
-  } = (0, f.useUserProfileAnalyticsContext)(), m = (0, a.useStateFromStores)([E.default], () => E.default.getChannel(s)), [N, p] = r.useState(""), [O, C] = r.useState((0, d.toRichValue)(N)), R = r.useRef(!1);
-  return null == m ? null : (0, i.jsx)(_.default, {
-    innerClassName: h.inner,
-    editorClassName: h.editor,
-    type: u.ChatInputTypes.USER_PROFILE,
-    placeholder: S.default.Messages.QUICK_DM_USER.format({
-      name: I.default.getName(n, s, t)
+    newestAnalyticsLocation: _
+  } = (0, o.default)(), {
+    trackUserProfileAction: c
+  } = (0, f.useUserProfileAnalyticsContext)(), [E, T] = r.useState(""), [S, p] = r.useState((0, u.toRichValue)(E)), O = r.useRef(!1);
+  return (0, i.jsx)(d.default, {
+    innerClassName: A.inner,
+    editorClassName: A.editor,
+    type: l.ChatInputTypes.USER_PROFILE,
+    placeholder: h.default.Messages.QUICK_DM_USER.format({
+      name: I.default.getName(n, a, t)
     }),
     channel: m,
-    textValue: N,
-    richValue: O,
+    textValue: E,
+    richValue: S,
     onChange: (e, t, n) => {
-      t !== N && (p(t), C(n))
+      t !== E && (T(t), p(n))
     },
-    focused: R.current,
+    focused: O.current,
     onFocus: () => {
-      R.current = !0
+      O.current = !0
     },
     onBlur: () => {
-      R.current = !1
+      O.current = !1
     },
     onSubmit: async e => {
       let {
         value: n
       } = e;
       try {
-        return T({
+        return c({
           action: "SEND_DIRECT_MESSAGE"
-        }), await A({
+        }), await N({
           userId: t.id,
           content: n.trim(),
-          location: c
-        }), null == o || o(), {
+          location: _
+        }), null == s || s(), {
           shouldClear: !0,
           shouldRefocus: !1
         }
