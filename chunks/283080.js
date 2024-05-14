e0ad84ce162996fc404c067530c422661a107c1b
DiscrapperManager <admin@minerpl.xyz>
2024-05-14 20:50:30 +0200

    canaryRelease: 293189
    Main scripts: 21
    Lazy Loaded Scripts: 825
    Chunks:
     * Main Files: 25453
     * Scripts: 33305
    Strings: 17893
    Routes: 740
    Experiments Count: 329

--- chunks/283080.js
+++ chunks/283080.js
@@ -13,9 +13,9 @@ function r(e, t) {
 
 function a() {
   if (0 === Object.keys(i).length) return "";
-  let e = "|".concat(r("id", 20), "|").concat(r("Mounts", 8), "|").concat(r("Mount Average", 20), "|").concat(r("Updates", 8), "|").concat(r("Update Average", 20), "|").concat(r("Nested", 8), "|").concat(r("Nested Average", 20), "|\n");
-  return "Component Render Averages:\n" + e + Object.entries(i).map(e => {
+  let e = "|".concat(r("id", 20), "|").concat(r("Mounts", 8), "|").concat(r("Mount Mean", 20), "|").concat(r("Updates", 8), "|").concat(r("Update Mean", 20), "|").concat(r("Nested", 8), "|").concat(r("Nested Mean", 20), "|\n");
+  return "Component Render Stats (microseconds):\n" + e + Object.entries(i).map(e => {
     let [t, n] = e;
-    return "|".concat(r(t, 20), "|").concat(r(n.mountAverage.count.toString(), 8), "|").concat(r(n.mountAverage.currentAverage.toString(), 20), "|").concat(r(n.updateAverage.count.toString(), 8), "|").concat(r(n.updateAverage.currentAverage.toString(), 20), "|").concat(r(n.nestedUpdateAverage.count.toString(), 8), "|").concat(r(n.nestedUpdateAverage.currentAverage.toString(), 20), "|\n")
+    return "|".concat(r(t, 20), "|").concat(r(n.mount.count.toString(), 8), "|").concat(r(n.mount.mean.toString(), 20), "|").concat(r(n.update.count.toString(), 8), "|").concat(r(n.update.mean.toString(), 20), "|").concat(r(n.nestedUpdate.count.toString(), 8), "|").concat(r(n.nestedUpdate.mean.toString(), 20), "|\n")
   })
 }
