50370ba6bb32dbbb5aa45ae026fcef95d7bda0b0
DiscrapperManager <admin@minerpl.xyz>
2024-05-17 17:46:53 +0200

    canaryRelease: 294397
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25414
     * Scripts: 32451
    Strings: 17909
    Routes: 740
    Experiments Count: 333

--- chunks/602147.js
+++ chunks/602147.js
@@ -1,41 +1,44 @@
 "use strict";
 n.r(t), n.d(t, {
   handleDiscoveryButtonClick: function() {
-    return f
+    return h
   }
 });
 var l = n("735250"),
   a = n("470079"),
   s = n("212093"),
   i = n("703656"),
-  r = n("104259"),
-  o = n("603592"),
-  u = n("981631"),
-  d = n("731455"),
-  c = n("689938");
+  r = n("683301"),
+  o = n("104259"),
+  u = n("603592"),
+  d = n("981631"),
+  c = n("731455"),
+  f = n("689938");
 
-function f() {
-  window.location.pathname.startsWith(u.Routes.GUILD_DISCOVERY) && ((0, s.clearSearch)(), (0, s.selectCategory)(d.DISCOVERY_ALL_CATEGORIES_ID, !0));
-  let e = (0, i.getHistory)().location.search;
-  (0, i.transitionTo)(u.Routes.GUILD_DISCOVERY, {
-    search: e
+function h() {
+  let e = window.location.pathname.startsWith(d.Routes.GUILD_DISCOVERY),
+    t = r.default.getCurrentCategoryId() === c.CategoryId.Clans;
+  e && !t && ((0, s.clearSearch)(), (0, s.selectCategory)(c.DISCOVERY_ALL_CATEGORIES_ID, !0));
+  let n = (0, i.getHistory)().location.search;
+  (0, i.transitionTo)(d.Routes.GUILD_DISCOVERY, {
+    search: n
   })
 }
-let h = a.forwardRef(function(e, t) {
+let p = a.forwardRef(function(e, t) {
   let {
     selected: n,
     tooltip: a,
     className: s,
     onClick: i
   } = e;
-  return (0, l.jsx)(o.default, {
+  return (0, l.jsx)(u.default, {
     id: "guild-discover-button",
     ref: t,
     className: s,
-    onClick: null != i ? i : f,
+    onClick: null != i ? i : h,
     selected: n,
-    tooltip: null != a ? a : c.default.Messages.GUILD_DISCOVERY_TOOLTIP,
-    icon: r.default
+    tooltip: null != a ? a : f.default.Messages.GUILD_DISCOVERY_TOOLTIP,
+    icon: o.default
   })
 });
-t.default = h
+t.default = p
