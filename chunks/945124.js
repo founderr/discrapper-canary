9c43070944bcf4bbe297ed377e510a00bc1989ae
DiscrapperManager <admin@minerpl.xyz>
2024-05-21 20:43:34 +0200

    canaryRelease: 295144
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25462
     * Scripts: 32519
    Strings: 17938
    Routes: 740
    Experiments Count: 336

--- chunks/945124.js
+++ chunks/945124.js
@@ -1,12 +1,12 @@
     "use strict";
-    r.r(t), r.d(t, {
+    r.r(e), r.d(e, {
       getClanDiscoveryAffinity: function() {
         return i
       }
     });
     var n = r("137920");
 
-    function i(e, t) {
+    function i(t, e) {
       let r = 0;
-      return null != t.games && (r += 2 * t.games.filter(t => e.games.includes(t)).length), null != t.playstyle && (t.playstyle === e.playstyle ? r += 2 : n.PLAYSTYLE_GROUPS[e.playstyle] === n.PLAYSTYLE_GROUPS[t.playstyle] && (r += 1)), null != t.traits && (r += 2 * Array.from(t.traits).filter(t => e.traits.includes(t)).length), r
+      return null != e.games && (r += 2 * e.games.filter(e => t.games.includes(e)).length), null != e.playstyle && (e.playstyle === t.playstyle ? r += 2 : n.PLAYSTYLE_GROUPS[t.playstyle] === n.PLAYSTYLE_GROUPS[e.playstyle] && (r += 1)), null != e.traits && (r += 2 * Array.from(e.traits).filter(e => t.traits.includes(e)).length), r += Math.random()
     }
