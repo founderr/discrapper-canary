93d2aba48f8b40a096da22d6a5ba07462181a0a3
DiscrapperManager <admin@minerpl.xyz>
2024-05-21 21:20:49 +0200

    canaryRelease: 295186
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25462
     * Scripts: 32519
    Strings: 17938
    Routes: 740
    Experiments Count: 336

--- chunks/168551.js
+++ chunks/168551.js
@@ -9,8 +9,8 @@ n.r(t), n.d(t, {
 }), n("47120");
 var i = n("470079"),
   r = n("688619"),
-  a = n.n(r),
-  s = n("691324"),
+  s = n.n(r),
+  a = n("691324"),
   o = n("442837"),
   l = n("780384"),
   u = n("813852"),
@@ -28,8 +28,8 @@ let _ = "data-client-themes",
       if (null == e) return null;
       let n = d.default.getLinearGradient();
       if (null == n) return null;
-      let [i, r] = [...e.colors].sort((e, t) => t.stop - e.stop).slice(0, 2).map(e => a()(s.ColorDetails[e.token].hex)), [o, u] = i.luminance() > r.luminance() ? [i, r] : [r, i], _ = (0, l.isThemeLight)(e.theme) ? u.set("lch.l", 10) : o.set("lch.l", 98).set("lch.c", 15), E = (0, l.isThemeLight)(e.theme) ? o.set("hsl.s", 1).set("hsl.l", .95) : u.set("hsl.s", 1).set("hsl.l", .05), [I, T, f] = E.hsl();
-      return ".".concat(c, " {\n      --custom-theme-background: ").concat(n, ";\n      --custom-theme-mix-base-hsl: ").concat(I, " ").concat(100 * T, "% ").concat(100 * f, "%;\n      --custom-theme-mix-base: ").concat(E.css(), ";\n      --custom-theme-mix-text: ").concat(_.css(), ";\n      --custom-theme-mix-amount-base: ").concat(t ? 30 : 0, "%;\n      --custom-theme-mix-amount-text: ").concat(t ? 40 : 0, "%;\n    }")
+      let [i, r] = [...e.colors].sort((e, t) => t.stop - e.stop).slice(0, 2).map(e => s()(a.ColorDetails[e.token].hex)), [o, u] = i.luminance() > r.luminance() ? [i, r] : [r, i], _ = (0, l.isThemeLight)(e.theme) ? u.set("lch.l", 10) : o.set("lch.l", 98).set("lch.c", 15), E = (0, l.isThemeLight)(e.theme) ? o.set("hsl.s", 1).set("hsl.l", .95) : u.set("hsl.s", 1).set("hsl.l", .05), [I, T, f] = E.hsl(), S = t ? "\n      --custom-theme-mix-base-hsl: ".concat(Number.isNaN(I) ? 0 : I, " ").concat(100 * T, "% ").concat(100 * f, "%;\n      --custom-theme-mix-base: ").concat(E.css(), ";\n      --custom-theme-mix-text: ").concat(_.css(), ";\n      --custom-theme-mix-amount-base: ").concat(t ? 30 : 0, "%;\n      --custom-theme-mix-amount-text: ").concat(t ? 40 : 0, "%;\n    ") : "";
+      return ".".concat(c, " {\n      --custom-theme-background: ").concat(n, ";\n      ").concat(S, "\n    }")
     }, [t, e])
   };
 t.default = () => {
