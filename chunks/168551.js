fb4ce89ff69f6868208ca5b5aa9490c8e7031661
DiscrapperManager <admin@minerpl.xyz>
2024-05-21 19:14:10 +0200

    canaryRelease: 295084
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25453
     * Scripts: 32498
    Strings: 17934
    Routes: 740
    Experiments Count: 335

--- chunks/168551.js
+++ chunks/168551.js
@@ -6,7 +6,7 @@ n.r(t), n.d(t, {
   CUSTOM_THEME_BACKGROUND_CLASS_NAME: function() {
     return c
   }
-});
+}), n("47120");
 var i = n("470079"),
   r = n("688619"),
   s = n.n(r),
@@ -28,21 +28,8 @@ let _ = "data-client-themes",
       if (null == e) return null;
       let n = d.default.getLinearGradient();
       if (null == n) return null;
-      let i = s().average(e.colors.map(e => {
-          let {
-            token: t
-          } = e;
-          return a.ColorDetails[t].hex
-        }), "lab", e.colors.map((t, n) => {
-          var i, r;
-          let {
-            stop: s
-          } = t;
-          return (s - (null !== (r = null === (i = e.colors[n - 1]) || void 0 === i ? void 0 : i.stop) && void 0 !== r ? r : 0)) / 100
-        })),
-        r = (0, l.isThemeLight)(e.theme) ? i.saturate(2).luminance(.1) : i.set("hsl.s", 1).set("hsl.l", .8),
-        o = (0, l.isThemeLight)(e.theme) ? i.saturate(2).brighten(1.75) : i.set("hsl.s", .9).set("hsl.l", .2);
-      return ".".concat(c, " {\n      --custom-theme-background: ").concat(n, ";\n      --custom-theme-mix-base: ").concat(o.css(), ";\n      --custom-theme-mix-text: ").concat(r.css(), ";\n      --custom-theme-mix-amount-base: ").concat(t ? 20 : 0, "%;\n      --custom-theme-mix-amount-text: ").concat(t ? 40 : 0, "%;\n    }")
+      let [i, r] = [...e.colors].sort((e, t) => t.stop - e.stop).slice(0, 2).map(e => s()(a.ColorDetails[e.token].hex)), [o, u] = i.luminance() > r.luminance() ? [i, r] : [r, i], _ = (0, l.isThemeLight)(e.theme) ? u.set("lch.l", 10) : o.set("lch.l", 98).set("lch.c", 15), E = (0, l.isThemeLight)(e.theme) ? o.set("hsl.s", 1).set("hsl.l", .95) : u.set("hsl.s", 1).set("hsl.l", .05), [I, T, f] = E.hsl();
+      return ".".concat(c, " {\n      --custom-theme-background: ").concat(n, ";\n      --custom-theme-mix-base-hsl: ").concat(I, " ").concat(100 * T, "% ").concat(100 * f, "%;\n      --custom-theme-mix-base: ").concat(E.css(), ";\n      --custom-theme-mix-text: ").concat(_.css(), ";\n      --custom-theme-mix-amount-base: ").concat(t ? 30 : 0, "%;\n      --custom-theme-mix-amount-text: ").concat(t ? 40 : 0, "%;\n    }")
     }, [t, e])
   };
 t.default = () => {
