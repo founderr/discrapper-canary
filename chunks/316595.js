c52f9362370a36cfb03c0db46dd45024974eb088
DiscrapperManager <admin@minerpl.xyz>
2024-05-21 23:55:00 +0200

    canaryRelease: 295290
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25462
     * Scripts: 31923
    Strings: 17941
    Routes: 740
    Experiments Count: 335

--- chunks/316595.js
+++ chunks/316595.js
@@ -1,7 +1,7 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return f
+    return d
   }
 }), n("653041");
 var a = n("735250"),
@@ -9,43 +9,26 @@ var a = n("735250"),
   s = n("442837"),
   i = n("524437"),
   r = n("243778"),
-  o = n("79390"),
-  u = n("966153"),
-  d = n("375954"),
-  c = n("423490");
+  o = n("375954"),
+  u = n("423490");
 
-function f(e) {
+function d(e) {
   let {
-    channel: t,
-    isOmniMenuOpen: n,
-    openOmniMenu: f,
-    children: h
-  } = e, m = (0, s.useStateFromStores)([d.default], () => d.default.hasCurrentUserSentMessageSinceAppStart()), p = (0, o.useCanPostPollsInChannel)(t), E = (0, o.useCanShowPollsChatInputCoachmarkInGuild)(null == t ? void 0 : t.guild_id), C = (null == t ? void 0 : t.isPrivate()) || E, g = l.useMemo(() => {
+    children: t
+  } = e, n = (0, s.useStateFromStores)([o.default], () => o.default.hasCurrentUserSentMessageSinceAppStart()), d = l.useMemo(() => {
     let e = [];
-    return m && e.push(i.DismissibleContent.ACTIVITIES_CHAT_BUTTON_NUX_V2), p && C && e.push(i.DismissibleContent.POLLS_CHAT_INPUT_COACHMARK), e
-  }, [m, p, C]);
+    return n && e.push(i.DismissibleContent.ACTIVITIES_CHAT_BUTTON_NUX_V2), e
+  }, [n]);
   return (0, a.jsx)(r.default, {
-    contentTypes: g,
+    contentTypes: d,
     children: e => {
       let {
-        visibleContent: t,
-        markAsDismissed: l
+        visibleContent: n
       } = e;
-      switch (t) {
-        case i.DismissibleContent.ACTIVITIES_CHAT_BUTTON_NUX_V2:
-          return (0, a.jsx)(c.OmniButtonNuxAnimation, {
-            children: h
-          });
-        case i.DismissibleContent.POLLS_CHAT_INPUT_COACHMARK:
-          return (0, a.jsx)(u.default, {
-            markAsDismissed: l,
-            onClickCta: f,
-            shouldShow: !n,
-            children: h
-          });
-        default:
-          return h
-      }
+      if (n === i.DismissibleContent.ACTIVITIES_CHAT_BUTTON_NUX_V2) return (0, a.jsx)(u.OmniButtonNuxAnimation, {
+        children: t
+      });
+      return t
     }
   })
 }
--- chunks/79390.js
+++ chunks/79390.js
@@ -1,43 +1,40 @@
 "use strict";
 n.r(t), n.d(t, {
   createPollServerDataFromCreateRequest: function() {
-    return M
-  },
-  filterOutUUID: function() {
-    return p
-  },
-  generateEmptyPollAnswer: function() {
-    return N
-  },
-  getPollAnswerVotesTooltipText: function() {
-    return U
-  },
-  getPollResultsReplyPreview: function() {
-    return y
-  },
-  getTotalVotes: function() {
-    return P
-  },
-  hasNonVoteReactions: function() {
-    return O
-  },
-  isAnswerFilled: function() {
     return v
   },
-  isIncompleteAnswer: function() {
+  filterOutUUID: function() {
+    return N
+  },
+  generateEmptyPollAnswer: function() {
+    return m
+  },
+  getPollAnswerVotesTooltipText: function() {
+    return y
+  },
+  getPollResultsReplyPreview: function() {
     return D
   },
-  isPollCreationEmpty: function() {
-    return L
+  getTotalVotes: function() {
+    return M
   },
-  useCanPostImagePolls: function() {
+  hasNonVoteReactions: function() {
+    return p
+  },
+  isAnswerFilled: function() {
     return g
   },
-  useCanPostPollsInChannel: function() {
+  isIncompleteAnswer: function() {
+    return L
+  },
+  isPollCreationEmpty: function() {
+    return R
+  },
+  useCanPostImagePolls: function() {
     return C
   },
-  useCanShowPollsChatInputCoachmarkInGuild: function() {
-    return R
+  useCanPostPollsInChannel: function() {
+    return O
   }
 }), n("757143"), n("47120"), n("724458");
 var i = n("392711"),
@@ -54,12 +51,11 @@ var i = n("392711"),
   I = n("699516"),
   T = n("70956"),
   f = n("5192"),
-  S = n("192616"),
-  h = n("489887"),
-  A = n("981631"),
-  m = n("689938");
+  S = n("489887"),
+  h = n("981631"),
+  A = n("689938");
 
-function N() {
+function m() {
   return {
     text: void 0,
     image: void 0,
@@ -69,39 +65,29 @@ function N() {
   }
 }
 
-function p(e) {
+function N(e) {
   return e.replace(/\b[a-f\d]{8}-(?:[a-f\d]{4}-){3}[a-f\d]{12}-\b/i, "")
 }
 
-function O(e) {
+function p(e) {
   for (let t of e.reactions)
     if (null == t.me_vote) return !0;
   return !1
 }
 
-function C(e) {
-  return (0, l.useStateFromStores)([E.default], () => !!(null != e && A.ChannelTypesSets.POLLS.has(e.type)) && (!!e.isPrivate() || E.default.can(A.Permissions.SEND_MESSAGES, e) && E.default.can(A.Permissions.SEND_POLLS, e)))
+function O(e) {
+  return (0, l.useStateFromStores)([E.default], () => !!(null != e && h.ChannelTypesSets.POLLS.has(e.type)) && (!!e.isPrivate() || E.default.can(h.Permissions.SEND_MESSAGES, e) && E.default.can(h.Permissions.SEND_POLLS, e)))
 }
 
-function R(e) {
-  let {
-    enabled: t
-  } = S.CreatePollsGuildCoachmarkExperiment.useExperiment({
-    guildId: e,
-    location: "useCanShowPollsChatInputCoachmarkInGuild"
-  });
-  return t
-}
-
-function g() {
+function C() {
   return !1
 }
 
-function L(e, t, n) {
-  return 0 === e.length && null == t.find(e => v(e, n))
+function R(e, t, n) {
+  return 0 === e.length && null == t.find(e => g(e, n))
 }
 
-function v(e, t) {
+function g(e, t) {
   if (t === a.PollLayoutTypes.IMAGE_ONLY_ANSWERS) return null != e.image;
   {
     var n;
@@ -110,13 +96,13 @@ function v(e, t) {
   }
 }
 
-function D(e, t) {
+function L(e, t) {
   var n;
   let i = null === (n = e.text) || void 0 === n ? void 0 : n.trim();
   return t === a.PollLayoutTypes.DEFAULT && null != e.image && (null == i || 0 === i.length)
 }
 
-function M(e) {
+function v(e) {
   var t, n;
   if (null == e) return;
   let i = null == e ? void 0 : null === (t = e.answers) || void 0 === t ? void 0 : t.map((e, t) => {
@@ -143,25 +129,25 @@ function M(e) {
   }
 }
 
-function y(e) {
+function D(e) {
   let t = function(e, t) {
     var n, i, r, s;
     let a = null !== (s = null === (r = e.embeds[0]) || void 0 === r ? void 0 : null === (i = r.fields) || void 0 === i ? void 0 : null === (n = i.find(e => "poll_question_text" === e.rawName)) || void 0 === n ? void 0 : n.rawValue) && void 0 !== s ? s : "";
     return null != t ? (0, o.truncateText)(a, t) : a
-  }(e, h.POLL_RESULT_MESSAGE_POLL_TITLE_MAX_VISIBLE_CHARS);
-  return m.default.Messages.POLL_RESULT_REPLY_TEXT.format({
+  }(e, S.POLL_RESULT_MESSAGE_POLL_TITLE_MAX_VISIBLE_CHARS);
+  return A.default.Messages.POLL_RESULT_REPLY_TEXT.format({
     title: t
   })
 }
 
-function P(e) {
+function M(e) {
   return e.reduce((e, t) => {
     var n, i;
     return e + (null !== (i = null === (n = t.count_details) || void 0 === n ? void 0 : n.vote) && void 0 !== i ? i : 0)
   }, 0)
 }
 
-function U(e, t, n) {
+function y(e, t, n) {
   var i, s;
   let a = c.default.getMessage(t, e);
   if (null == a) return "";
@@ -177,34 +163,34 @@ function U(e, t, n) {
           id: t,
           name: "",
           animated: !1
-        }, h.VOTES_TOOLTIP_MAX_USERS, u.ReactionTypes.VOTE),
+        }, S.VOTES_TOOLTIP_MAX_USERS, u.ReactionTypes.VOTE),
         s = d.default.getChannel(n),
         a = null == s || s.isPrivate() ? null : s.getGuildId();
-      return r()(i).reject(e => I.default.isBlocked(e.id)).take(h.VOTES_TOOLTIP_MAX_USERS).map(e => f.default.getName(a, null == s ? void 0 : s.id, e)).value()
+      return r()(i).reject(e => I.default.isBlocked(e.id)).take(S.VOTES_TOOLTIP_MAX_USERS).map(e => f.default.getName(a, null == s ? void 0 : s.id, e)).value()
     }(a, n);
   return 0 === E.length ? "" : function(e, t) {
     let n = Math.max(0, t - e.length);
-    if (1 === e.length) return n > 0 ? m.default.Messages.POLL_VOTES_TOOLTIP_1_N.format({
+    if (1 === e.length) return n > 0 ? A.default.Messages.POLL_VOTES_TOOLTIP_1_N.format({
       a: e[0],
       n: n.toLocaleString()
     }) : e[0];
-    if (2 === e.length) return n > 0 ? m.default.Messages.POLL_VOTES_TOOLTIP_2_N.format({
+    if (2 === e.length) return n > 0 ? A.default.Messages.POLL_VOTES_TOOLTIP_2_N.format({
       a: e[0],
       b: e[1],
       n: n.toLocaleString()
-    }) : m.default.Messages.POLL_VOTES_TOOLTIP_2.format({
+    }) : A.default.Messages.POLL_VOTES_TOOLTIP_2.format({
       a: e[0],
       b: e[1]
     });
-    if (3 !== e.length) return m.default.Messages.POLL_VOTES_TOOLTIP_N.format({
+    if (3 !== e.length) return A.default.Messages.POLL_VOTES_TOOLTIP_N.format({
       n: n.toLocaleString()
     });
-    else return n > 0 ? m.default.Messages.POLL_VOTES_TOOLTIP_3_N.format({
+    else return n > 0 ? A.default.Messages.POLL_VOTES_TOOLTIP_3_N.format({
       a: e[0],
       b: e[1],
       c: e[2],
       n: n.toLocaleString()
-    }) : m.default.Messages.POLL_VOTES_TOOLTIP_3.format({
+    }) : A.default.Messages.POLL_VOTES_TOOLTIP_3.format({
       a: e[0],
       b: e[1],
       c: e[2]
