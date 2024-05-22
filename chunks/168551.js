f68b2dee40874b2f8abc108029961f78650ac16e
DiscrapperManager <admin@minerpl.xyz>
2024-05-22 23:58:56 +0200

    canaryRelease: 295634
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25480
     * Scripts: 31575
    Strings: 17946
    Routes: 740
    Experiments Count: 334

--- chunks/168551.js
+++ chunks/168551.js
@@ -1,44 +1,51 @@
 "use strict";
 n.r(t), n.d(t, {
   CLIENT_THEMES_DATA_ATTRIBUTE: function() {
-    return _
+    return d
   },
   CUSTOM_THEME_BACKGROUND_CLASS_NAME: function() {
-    return c
+    return _
   }
 }), n("47120");
 var i = n("470079"),
-  r = n("688619"),
-  s = n.n(r),
-  a = n("691324"),
-  o = n("442837"),
-  l = n("780384"),
-  u = n("813852"),
-  d = n("514361");
-let _ = "data-client-themes",
-  c = "custom-theme-background",
-  E = () => {
-    let e = (0, o.useStateFromStores)([d.default], () => d.default.gradientPreset),
+  r = n("691324"),
+  s = n("442837"),
+  a = n("780384"),
+  o = n("813852"),
+  l = n("514361"),
+  u = n("629935");
+let d = "data-client-themes",
+  _ = "custom-theme-background",
+  c = () => {
+    let e = (0, s.useStateFromStores)([l.default], () => l.default.gradientPreset),
       {
         enabled: t
-      } = u.ColorMixDesktopClientThemesExperiment.useExperiment({
+      } = o.ColorMixDesktopClientThemesExperiment.useExperiment({
         location: "useBackgroundGradientCSS"
       });
     return (0, i.useMemo)(() => {
       if (null == e) return null;
-      let n = d.default.getLinearGradient();
+      let n = l.default.getLinearGradient();
       if (null == n) return null;
-      let [i, r] = [...e.colors].sort((e, t) => t.stop - e.stop).slice(0, 2).map(e => s()(a.ColorDetails[e.token].hex)), [o, u] = i.luminance() > r.luminance() ? [i, r] : [r, i], _ = (0, l.isThemeLight)(e.theme) ? u.set("lch.l", 10) : o.set("lch.l", 98).set("lch.c", 15), E = (0, l.isThemeLight)(e.theme) ? o.set("hsl.s", 1).set("hsl.l", .95) : u.set("hsl.s", 1).set("hsl.l", .05), [I, T, f] = E.hsl(), S = t ? "\n      --custom-theme-mix-base-hsl: ".concat(Number.isNaN(I) ? 0 : I, " ").concat(100 * T, "% ").concat(100 * f, "%;\n      --custom-theme-mix-base: ").concat(E.css(), ";\n      --custom-theme-mix-text: ").concat(_.css(), ";\n      --custom-theme-mix-amount-base: ").concat(t ? 30 : 0, "%;\n      --custom-theme-mix-amount-text: ").concat(t ? 40 : 0, "%;\n    ") : "";
-      return ".".concat(c, " {\n      --custom-theme-background: ").concat(n, ";\n      ").concat(S, "\n    }")
+      let [i, s] = [...e.colors].sort((e, t) => t.stop - e.stop).slice(0, 2).map(e => r.ColorDetails[e.token].hex), o = (0, u.getThemeColorMixValues)({
+        enabled: t,
+        primaryColor: i,
+        secondaryColor: s,
+        isDarkTheme: (0, a.isThemeDark)(e.theme)
+      }), d = t ? "\n      ".concat(Object.entries(o).map(e => {
+        let [t, n] = e;
+        return "".concat(t, ": ").concat(n, ";")
+      }).join("\n"), "\n    ") : "";
+      return ".".concat(_, " {\n      --custom-theme-background: ").concat(n, ";\n      ").concat(d, "\n    }")
     }, [t, e])
   };
 t.default = () => {
-  let e = E();
+  let e = c();
   return null === e ? {
     clientThemesCSS: "",
     clientThemesClassName: ""
   } : {
     clientThemesCSS: e,
-    clientThemesClassName: c
+    clientThemesClassName: _
   }
 }
---
+++ chunks/629935.js
@@ -0,0 +1,26 @@
+"use strict";
+n.r(t), n.d(t, {
+  getThemeColorMixValues: function() {
+    return s
+  }
+}), n("47120");
+var i = n("688619"),
+  r = n.n(i);
+
+function s(e) {
+  let {
+    enabled: t,
+    primaryColor: n,
+    secondaryColor: i,
+    isDarkTheme: s,
+    baseMixAmount: a = 30,
+    textMixAmount: o = 40
+  } = e, l = r()(n), u = r()(i), [d, _] = l.luminance() > u.luminance() ? [l, u] : [u, l], c = s ? d.set("lch.l", 98).set("lch.c", 15) : _.set("lch.l", 10), E = s ? _.set("hsl.s", 1).set("hsl.l", .05) : d.set("hsl.s", 1).set("hsl.l", .95), [I, T, f] = E.hsl();
+  return {
+    "--custom-theme-mix-base-hsl": "".concat(Number.isNaN(I) ? 0 : I, " ").concat(100 * T, "% ").concat(100 * f, "%"),
+    "--custom-theme-mix-base": E.css(),
+    "--custom-theme-mix-text": c.css(),
+    "--custom-theme-mix-amount-base": "".concat(t ? a : 0, "%"),
+    "--custom-theme-mix-amount-text": "".concat(t ? o : 0, "%")
+  }
+}
