b9d81e627f94a71060d934da2af4303dc3667d9a
DiscrapperManager <admin@minerpl.xyz>
2024-05-23 02:29:18 +0200

    canaryRelease: 295727
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25480
     * Scripts: 31575
    Strings: 17946
    Routes: 740
    Experiments Count: 334

--- chunks/139674.js
+++ chunks/139674.js
@@ -6,51 +6,53 @@ var r, s, a, o, l = n("442837"),
   d = n("570140"),
   _ = n("902676"),
   c = n("626135"),
-  E = n("704133"),
-  I = n("981631");
-let T = "BlockedDomainStore",
-  f = "BlockedDomainRevision",
-  S = null;
-class h extends(r = l.default.Store) {
+  E = n("358085"),
+  I = n("704133"),
+  T = n("981631");
+let f = "BlockedDomainStore",
+  S = "BlockedDomainRevision",
+  h = null;
+class A extends(r = l.default.Store) {
   initialize() {
-    i = null, u.Storage.get(T) && (u.Storage.remove(f), u.Storage.remove(T))
+    i = null, u.Storage.get(f) && (u.Storage.remove(S), u.Storage.remove(f))
   }
   getCurrentRevision() {
+    if (!(0, E.isDesktop)()) return null;
     if (null == i) {
       var e;
-      i = null !== (e = u.Storage.get(f)) && void 0 !== e ? e : null
+      i = null !== (e = u.Storage.get(S)) && void 0 !== e ? e : null
     }
     return i
   }
   async getBlockedDomainList() {
-    return null == S && (S = new Set(await E.default.getBlockedDomains())), S
+    return null == h && (h = new Set(await I.default.getBlockedDomains())), h
   }
   isBlockedDomain(e) {
     let t = n("647229");
-    if (null == S) return this.getBlockedDomainList(), null;
+    if (null == h) return this.getBlockedDomainList(), null;
     let i = (0, _.getHostname)(e),
       r = new t.hash.sha256,
       s = t.codec.hex.fromBits(r.update(i).finalize()),
       a = "";
-    S.has(s) && (a = i);
+    h.has(s) && (a = i);
     let o = i.indexOf(".");
-    for (; - 1 !== o && "" === a;) i = i.substring(o + 1), r.reset(), s = t.codec.hex.fromBits(r.update(i).finalize()), S.has(s) && (a = i), o = i.indexOf(".");
-    return "" !== a ? (c.default.track(I.AnalyticEvents.LINK_SECURITY_CHECK_BLOCKED, {
+    for (; - 1 !== o && "" === a;) i = i.substring(o + 1), r.reset(), s = t.codec.hex.fromBits(r.update(i).finalize()), h.has(s) && (a = i), o = i.indexOf(".");
+    return "" !== a ? (c.default.track(T.AnalyticEvents.LINK_SECURITY_CHECK_BLOCKED, {
       blocked_domain: a
     }), a) : null
   }
 }
-o = "BlockedDomainStore", (a = "displayName") in(s = h) ? Object.defineProperty(s, a, {
+o = "BlockedDomainStore", (a = "displayName") in(s = A) ? Object.defineProperty(s, a, {
   value: o,
   enumerable: !0,
   configurable: !0,
   writable: !0
-}) : s[a] = o, t.default = new h(d.default, {
+}) : s[a] = o, t.default = new A(d.default, {
   BLOCKED_DOMAIN_LIST_FETCHED: function(e) {
     let {
       list: t,
       revision: n
     } = e;
-    S = null, i = null, E.default.saveBlockedDomains(t), u.Storage.set("BlockedDomainRevision", n)
+    i = null, h = new Set(t), I.default.saveBlockedDomains(t), u.Storage.set("BlockedDomainRevision", n)
   }
 })
--- routes.js
+++ routes.js
@@ -2623,7 +2623,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-6cb49e01ff5d4263b820c5fc7cabd74446858988",
+                    release: "discord_web-0bccb651d3dbab2775f658f463ec805e4b0968ee",
                     beforeSend: _ => {
                         var E, e;
                         return !(null != (E = _).exception && null != E.exception.values && E.exception.values.every(_ => null == _.stacktrace || null != _.stacktrace.frames && 1 === _.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || a.some(_ => window.navigator.appVersion.toLowerCase().indexOf(_) >= 0)) && !R() && !("Aborted" === (e = _).message || "cancel captcha" === e.message) && t() ? _ : null
@@ -2641,7 +2641,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "ResizeObserver loop completed with undelivered notifications.", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), I.setTag("buildNumber", (_ = "295709", "295709")), I.setTag("builtAt", String("1716420753606"));
+                }), I.setTag("buildNumber", (_ = "295727", "295727")), I.setTag("builtAt", String("1716423538369"));
                 let E = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != E && "object" == typeof E)
                     for (let _ in E) I.setTag(_, E[_]);
