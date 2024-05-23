b047ab3877d5515c344c4725d7bcf577f5a2a28b
DiscrapperManager <admin@minerpl.xyz>
2024-05-23 16:45:14 +0200

    canaryRelease: 295763
    Main scripts: 21
    Lazy Loaded Scripts: 826
    Chunks:
     * Main Files: 25486
     * Scripts: 31585
    Strings: 17946
    Routes: 740
    Experiments Count: 334

--- chunks/688798.js
+++ chunks/688798.js
@@ -6,20 +6,18 @@ var i = n("570140"),
   a = n("904245"),
   o = n("147913"),
   l = n("3148"),
-  u = n("960412"),
-  d = n("706454"),
-  _ = n("695346"),
-  c = n("375954"),
-  E = n("709054"),
-  I = n("839627"),
-  T = n("802098"),
-  f = n("128014"),
-  S = n("163379"),
-  h = n("596401"),
-  A = n("981631"),
-  m = n("930441");
+  u = n("706454"),
+  d = n("695346"),
+  _ = n("375954"),
+  c = n("709054"),
+  E = n("839627"),
+  I = n("802098"),
+  T = n("128014"),
+  f = n("163379"),
+  S = n("596401"),
+  h = n("981631");
 
-function N(e, t, n) {
+function A(e, t, n) {
   return t in e ? Object.defineProperty(e, t, {
     value: n,
     enumerable: !0,
@@ -27,15 +25,15 @@ function N(e, t, n) {
     writable: !0
   }) : e[t] = n, e
 }
-async function p(e) {
-  if (_.LastReceivedChangelogId.getSetting() >= e) return;
-  let t = await s.default.getOrEnsurePrivateChannel(h.SYSTEM_UPDATES_USER_ID);
+async function m(e) {
+  if (d.LastReceivedChangelogId.getSetting() >= e) return;
+  let t = await s.default.getOrEnsurePrivateChannel(S.SYSTEM_UPDATES_USER_ID);
   if (null == t) return;
   await a.default.fetchMessages({
     channelId: t,
     limit: 1
   });
-  let n = c.default.getLastMessage(t);
+  let n = _.default.getLastMessage(t);
   if (null == n) return;
   let i = (0, l.default)({
     ...n,
@@ -46,18 +44,18 @@ async function p(e) {
   });
   a.default.receiveMessage(t, {
     ...i,
-    state: A.MessageStates.SENT,
+    state: h.MessageStates.SENT,
     channel_id: t
   }, !0, {})
 }
-class O extends o.default {
+class N extends o.default {
   constructor(...e) {
-    super(...e), N(this, "actions", {
+    super(...e), A(this, "actions", {
       POST_CONNECTION_OPEN: e => this.handleConnectionOpen(e)
-    }), N(this, "handleConnectionOpen", async e => {
+    }), A(this, "handleConnectionOpen", async e => {
       let {
         canReceiveMessage: t
-      } = I.default.getCurrentConfig({
+      } = E.default.getCurrentConfig({
         location: "changelog_manager"
       }, {
         autoTrackExposure: !1
@@ -68,29 +66,28 @@ class O extends o.default {
             min_version: s
           }] of Object.entries(e)) s <= t && s > n && (n = s, i = r);
         return i
-      }(s, (0, f.getClientVersionForChangelog)());
+      }(s, (0, T.getClientVersionForChangelog)());
       if (i.default.dispatch({
           type: "CHANGE_LOG_SET_CONFIG",
           config: n.body,
           latestChangelogId: a
         }), null == a) return;
-      let o = await (0, u.fetchEmailSettings)();
-      if (t && o.categories[m.EmailCategories.UPDATES_AND_ANNOUNCEMENTS]) {
-        p(a);
+      if (t) {
+        m(a);
         return
       }
       if (!0 !== s[a].show_on_startup) return;
-      let l = T.default.lastSeenChangelogId(),
-        _ = T.default.lastSeenChangelogDate();
-      if (null != l && 0 >= E.default.compare(a, l)) return;
-      let c = await r.default.fetchChangelog(a, d.default.locale);
-      if (null != c) {
-        if (null == _ || null == T.default.lastSeenChangelogDate()) {
-          r.default.markChangelogAsSeen(a, c.date);
+      let o = I.default.lastSeenChangelogId(),
+        l = I.default.lastSeenChangelogDate();
+      if (null != o && 0 >= c.default.compare(a, o)) return;
+      let d = await r.default.fetchChangelog(a, u.default.locale);
+      if (null != d) {
+        if (null == l || null == I.default.lastSeenChangelogDate()) {
+          r.default.markChangelogAsSeen(a, d.date);
           return
-        }!T.default.isLocked() && new Date(c.date) > new Date(_) && (0, S.openChangelog)()
+        }!I.default.isLocked() && new Date(d.date) > new Date(l) && (0, f.openChangelog)()
       }
     })
   }
 }
-t.default = new O
+t.default = new N
--- chunks/960412.js
+++ chunks/960412.js
@@ -7,50 +7,50 @@ n.r(t), n.d(t, {
     return d
   },
   updateMarketingEmailSettings: function() {
-    return _
+    return c
   }
 }), n("724458");
-var i = n("990547"),
-  r = n("283693"),
-  s = n("570140"),
-  a = n("573261"),
-  o = n("930441"),
-  l = n("981631");
+var a = n("990547"),
+  s = n("283693"),
+  l = n("570140"),
+  i = n("573261"),
+  r = n("930441"),
+  o = n("981631");
 async function u() {
   try {
-    let e = await a.default.get({
-      url: l.Endpoints.EMAIL_SETTINGS,
+    let e = await i.default.get({
+      url: o.Endpoints.EMAIL_SETTINGS,
       trackedActionData: {
-        event: i.NetworkActionNames.EMAIL_SETTINGS_FETCH,
+        event: a.NetworkActionNames.EMAIL_SETTINGS_FETCH,
         properties: e => {
           var t;
           let n = null == e ? void 0 : null === (t = e.body) || void 0 === t ? void 0 : t.initialized;
-          return (0, r.exact)({
+          return (0, s.exact)({
             initialized: n
           })
         }
       }
     });
-    return s.default.dispatch({
+    l.default.dispatch({
       type: "EMAIL_SETTINGS_FETCH_SUCCESS",
       settings: e.body
-    }), e.body
+    })
   } catch {
-    s.default.dispatch({
+    l.default.dispatch({
       type: "EMAIL_SETTINGS_FETCH_FAILURE"
     })
   }
 }
 async function d(e, t) {
-  s.default.dispatch({
+  l.default.dispatch({
     type: "EMAIL_SETTINGS_UPDATE",
     updates: {
       [e]: t
     }
   });
   try {
-    let n = await a.default.patch({
-      url: l.Endpoints.EMAIL_SETTINGS,
+    let n = await i.default.patch({
+      url: o.Endpoints.EMAIL_SETTINGS,
       body: {
         settings: {
           categories: {
@@ -59,54 +59,54 @@ async function d(e, t) {
         }
       },
       trackedActionData: {
-        event: i.NetworkActionNames.EMAIL_SETTINGS_UPDATE,
+        event: a.NetworkActionNames.EMAIL_SETTINGS_UPDATE,
         properties: {
           category: e,
           value: t
         }
       }
     });
-    s.default.dispatch({
+    l.default.dispatch({
       type: "EMAIL_SETTINGS_UPDATE_SUCCESS",
       settings: n.body
     })
   } catch (e) {
-    s.default.dispatch({
+    l.default.dispatch({
       type: "EMAIL_SETTINGS_UPDATE_FAILURE"
     })
   }
 }
-async function _() {
-  let e = o.MarketingEmailCategories.reduce((e, t) => ({
+async function c() {
+  let e = r.MarketingEmailCategories.reduce((e, t) => ({
     ...e,
     [t]: !1
   }), {});
-  s.default.dispatch({
+  l.default.dispatch({
     type: "EMAIL_SETTINGS_UPDATE",
     updates: e
   });
   try {
-    let t = await a.default.patch({
-      url: l.Endpoints.EMAIL_SETTINGS,
+    let t = await i.default.patch({
+      url: o.Endpoints.EMAIL_SETTINGS,
       body: {
         settings: {
           categories: e
         }
       },
       trackedActionData: {
-        event: i.NetworkActionNames.EMAIL_SETTINGS_UPDATE,
+        event: a.NetworkActionNames.EMAIL_SETTINGS_UPDATE,
         properties: {
           category: "marketing",
           value: !1
         }
       }
     });
-    s.default.dispatch({
+    l.default.dispatch({
       type: "EMAIL_SETTINGS_UPDATE_SUCCESS",
       settings: t.body
     })
   } catch (e) {
-    s.default.dispatch({
+    l.default.dispatch({
       type: "EMAIL_SETTINGS_UPDATE_FAILURE"
     })
   }
