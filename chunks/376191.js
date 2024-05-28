22e109a27da299ac989d6b2f124f911957420968
DiscrapperManager <admin@minerpl.xyz>
2024-05-28 16:44:33 +0200

    canaryRelease: 296508
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25558
     * Scripts: 31719
    Strings: 17999
    Routes: 740
    Experiments Count: 335

--- chunks/376191.js
+++ chunks/376191.js
@@ -1,7 +1,7 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return A
+    return N
   }
 });
 var a = n("735250");
@@ -15,82 +15,87 @@ var s = n("442837"),
   d = n("43267"),
   c = n("522289"),
   f = n("699516"),
-  E = n("819570"),
-  h = n("51144"),
-  _ = n("998502"),
-  C = n("620924"),
-  m = n("332712"),
-  S = n("81471"),
-  p = n("689938"),
-  I = n("326486");
-let g = e => {
+  E = n("546591"),
+  h = n("819570"),
+  _ = n("51144"),
+  C = n("998502"),
+  m = n("620924"),
+  S = n("332712"),
+  p = n("81471"),
+  I = n("689938"),
+  g = n("326486");
+let T = e => {
     let {
       userId: t
-    } = e, n = (0, m.useMutualGuilds)(t);
-    return null == n || 0 === n.length ? null : (0, a.jsxs)("div", {
-      className: I.mutualGuildsContainer,
+    } = e, n = (0, S.useMutualGuilds)(t);
+    return null == n || 0 === n.length ? (0, a.jsx)(l.Text, {
+      className: g.mutualGuilds,
+      variant: "text-sm/normal",
+      color: "text-muted",
+      children: I.default.Messages.USER_PROFILE_NO_MUTUAL_SERVERS
+    }) : (0, a.jsxs)("div", {
+      className: g.mutualGuildsContainer,
       children: [(0, a.jsx)(c.default, {
         guilds: n,
         maxGuilds: 3,
-        size: E.GuildIcon.Sizes.SMOL,
+        size: h.GuildIcon.Sizes.SMOL,
         hideOverflowCount: !0
       }), (0, a.jsx)(l.Text, {
-        className: I.mutualGuilds,
+        className: g.mutualGuilds,
         variant: "text-sm/normal",
-        children: p.default.Messages.USER_PROFILE_MUTUAL_GUILDS.format({
+        color: "text-muted",
+        children: I.default.Messages.USER_PROFILE_MUTUAL_GUILDS.format({
           count: n.length
         })
       })]
     })
   },
-  T = _.default.getEnableHardwareAcceleration() ? l.AnimatedAvatar : l.Avatar;
+  A = C.default.getEnableHardwareAcceleration() ? l.AnimatedAvatar : l.Avatar;
 
-function A(e) {
+function N(e) {
   var t;
   let {
     channel: n,
     otherUser: c,
-    active: E
-  } = e, _ = (0, s.useStateFromStores)([r.default], () => r.default.useReducedMotion), m = (0, s.useStateFromStores)([f.default], () => null == c ? null : f.default.getNickname(c.id)), A = (0, C.useMessageRequestRelativeTimestampText)(n), {
-    avatarDecorationSrc: N
+    active: h
+  } = e, C = (0, s.useStateFromStores)([r.default], () => r.default.useReducedMotion), S = (0, s.useStateFromStores)([f.default], () => null == c ? null : f.default.getNickname(c.id)), N = !C && h, v = (0, m.useMessageRequestRelativeTimestampText)(n), {
+    avatarDecorationSrc: R
   } = (0, u.default)({
     user: c,
     size: (0, o.getDecorationSizeForAvatarSize)(l.AvatarSizes.SIZE_40),
     animateOnHover: !0
   });
   return (0, a.jsxs)(a.Fragment, {
-    children: [(0, a.jsx)(T, {
-      className: I.avatar,
-      src: (0, d.getChannelIconURL)(n, 40, !_ && E),
-      avatarDecoration: N,
+    children: [(0, a.jsx)(A, {
+      className: g.avatar,
+      src: (0, d.getChannelIconURL)(n, 40, N),
+      avatarDecoration: R,
       size: l.AvatarSizes.SIZE_40,
-      "aria-label": null !== (t = null == c ? void 0 : c.username) && void 0 !== t ? t : p.default.Messages.UNKNOWN_USER
+      "aria-label": null !== (t = null == c ? void 0 : c.username) && void 0 !== t ? t : I.default.Messages.UNKNOWN_USER
     }), (0, a.jsxs)("div", {
-      className: I.userPreview,
+      className: g.userPreview,
       children: [(0, a.jsxs)("div", {
-        className: I.userContainerWithPreview,
+        className: g.userContainerWithPreview,
         children: [(0, a.jsx)(i.default, {
-          nick: m,
+          nick: S,
           user: c,
           showAccountIdentifier: !0,
-          className: I.tagContainer,
-          usernameClass: I.username,
-          discriminatorClass: null != h.default.getGlobalName(c) ? I.globalName : I.discriminator
+          className: g.tagContainer,
+          usernameClass: g.username,
+          discriminatorClass: null != _.default.getGlobalName(c) ? g.globalName : g.discriminator
         }), (0, a.jsx)(l.Text, {
-          className: I.timestampWithPreview,
+          className: g.timestampWithPreview,
           color: "text-muted",
           variant: "text-xs/normal",
-          children: A
+          children: v
         })]
-      }), (0, a.jsx)("div", {
-        className: I.mutualGuilds,
-        children: (0, a.jsx)(g, {
-          userId: c.id
-        })
-      }), (0, a.jsx)("div", {
-        className: I.messagePreview,
-        children: (0, a.jsx)(S.default, {
+      }), (0, a.jsx)(E.default, {
+        hoverText: (0, a.jsx)(p.default, {
           channel: n
+        }),
+        forceHover: h,
+        children: (0, a.jsx)(T, {
+          userId: c.id
         })
       })]
     })]
