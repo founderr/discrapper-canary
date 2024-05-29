f56538ce1294a1998f510afad4fc9239dffd6e40
DiscrapperManager <admin@minerpl.xyz>
2024-05-29 03:14:19 +0200

    canaryRelease: 296912
    Main scripts: 22
    Lazy Loaded Scripts: 832
    Chunks:
     * Main Files: 25567
     * Scripts: 32600
    Strings: 18025
    Routes: 740
    Experiments Count: 335

--- chunks/339768.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var r = n("85733"),
-  i = n("692538"),
-  a = n("606956");
-e.exports = {
-  formats: a,
-  parse: i,
-  stringify: r
-}
--- chunks/606956.js
+++ chunks/606956.js
@@ -1,20 +1,52 @@
 "use strict";
-var t = String.prototype.replace,
-  n = /%20/g,
-  r = {
-    RFC1738: "RFC1738",
-    RFC3986: "RFC3986"
-  };
-e.exports = {
-  default: r.RFC3986,
-  formatters: {
-    RFC1738: function(e) {
-      return t.call(e, n, "+")
-    },
-    RFC3986: function(e) {
-      return String(e)
+n.r(t);
+var a, l, s, i, r, o, u = n("442837"),
+  d = n("570140");
+(a = r || (r = {})).FAILED = "failed", a.SUCCEEDED = "succeeded";
+let c = {};
+class f extends(o = u.default.Store) {
+  getFollowerStatsForChannel(e) {
+    return c[e]
+  }
+}
+i = "ChannelFollowerStatsStore", (s = "displayName") in(l = f) ? Object.defineProperty(l, s, {
+  value: i,
+  enumerable: !0,
+  configurable: !0,
+  writable: !0
+}) : l[s] = i, t.default = new f(d.default, {
+  CONNECTION_OPEN: function() {
+    c = {}
+  },
+  CHANNEL_FOLLOWER_STATS_FETCH_SUCCESS: function(e) {
+    let {
+      channelId: t,
+      stats: n
+    } = e;
+    n = null != n ? n : {}, c[t] = {
+      loadingStatus: "succeeded",
+      lastFetched: Date.now(),
+      channelsFollowing: n.channels_following,
+      guildMembers: n.guild_members,
+      guildsFollowing: n.guilds_following,
+      usersSeenEver: n.users_seen_ever,
+      subscribersGainedSinceLastPost: n.subscribers_gained_since_last_post,
+      subscribersLostSinceLastPost: n.subscribers_lost_since_last_post
     }
   },
-  RFC1738: r.RFC1738,
-  RFC3986: r.RFC3986
-}
+  CHANNEL_FOLLOWER_STATS_FETCH_FAILURE: function(e) {
+    let {
+      channelId: t
+    } = e;
+    c[t] = {
+      loadingStatus: "failed",
+      lastFetched: Date.now(),
+      channelsFollowing: 0,
+      guildMembers: 0,
+      guildsFollowing: 0,
+      usersSeenEver: 0,
+      subscribersGainedSinceLastPost: 0,
+      subscribersLostSinceLastPost: 0
+    }
+  }
+})
