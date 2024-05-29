003ecfe166af797a77e0c39b98731aa7898cafa5
DiscrapperManager <admin@minerpl.xyz>
2024-05-29 23:15:26 +0200

    canaryRelease: 297180
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25558
     * Scripts: 30283
    Strings: 18036
    Routes: 740
    Experiments Count: 336

--- chunks/409116.js
+++ chunks/409116.js
@@ -1,5 +1,5 @@
 "use strict";
-a.r(t);
+a.r(t), a("757143");
 var l = a("735250");
 a("470079");
 var s = a("120356"),
@@ -7,6 +7,7 @@ var s = a("120356"),
   n = a("481060"),
   i = a("937615"),
   o = a("87951");
+let c = e => e.length > 5 ? e.replace(/.00$/, "") : e;
 t.default = e => {
   var t;
   let {
@@ -14,23 +15,23 @@ t.default = e => {
       amount: a,
       currency: s
     },
-    renderPrice: c,
-    icon: u,
-    className: d,
-    discount: f,
-    variant: C = "heading-md/semibold"
-  } = e, m = (0, i.formatPrice)(a, s), p = null != f && f > 0 ? (0, l.jsxs)(l.Fragment, {
-    children: [m, (0, l.jsx)(n.Heading, {
-      variant: C,
+    renderPrice: u,
+    icon: d,
+    className: f,
+    discount: C,
+    variant: m = "heading-md/semibold"
+  } = e, p = c((0, i.formatPrice)(a, s)), g = null != C && C > 0 ? (0, l.jsxs)(l.Fragment, {
+    children: [p, (0, l.jsx)(n.Heading, {
+      variant: m,
       color: "text-positive",
-      children: " (-".concat(f, "%)")
+      children: " (-".concat(C, "%)")
     })]
-  }) : m;
+  }) : p;
   return (0, l.jsx)(l.Fragment, {
     children: (0, l.jsxs)(n.Heading, {
-      variant: C,
-      className: r()(o.heading, d),
-      children: [u, null !== (t = null == c ? void 0 : c(p)) && void 0 !== t ? t : p]
+      variant: m,
+      className: r()(o.heading, f),
+      children: [d, null !== (t = null == u ? void 0 : u(g)) && void 0 !== t ? t : g]
     })
   })
 }
