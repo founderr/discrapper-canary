d5c7544e3cffebd7b914f97ba475ca3ac7b9bd86
DiscrapperManager <admin@minerpl.xyz>
2024-05-29 21:52:34 +0200

    canaryRelease: 297118
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25558
     * Scripts: 30277
    Strings: 18031
    Routes: 740
    Experiments Count: 336

--- chunks/688798.js
+++ chunks/688798.js
@@ -4,22 +4,23 @@ var i = n("570140"),
   r = n("661111"),
   s = n("493683"),
   a = n("904245"),
-  o = n("147913"),
-  l = n("3148"),
-  u = n("960412"),
-  d = n("706454"),
-  _ = n("695346"),
-  c = n("375954"),
-  E = n("709054"),
-  I = n("839627"),
-  T = n("802098"),
-  f = n("128014"),
-  S = n("163379"),
-  h = n("596401"),
-  A = n("981631"),
-  m = n("930441");
+  o = n("232567"),
+  l = n("147913"),
+  u = n("3148"),
+  d = n("960412"),
+  _ = n("706454"),
+  c = n("695346"),
+  E = n("375954"),
+  I = n("709054"),
+  T = n("839627"),
+  f = n("802098"),
+  S = n("128014"),
+  h = n("163379"),
+  A = n("596401"),
+  m = n("981631"),
+  N = n("930441");
 
-function N(e, t, n) {
+function p(e, t, n) {
   return t in e ? Object.defineProperty(e, t, {
     value: n,
     enumerable: !0,
@@ -27,17 +28,17 @@ function N(e, t, n) {
     writable: !0
   }) : e[t] = n, e
 }
-async function p(e) {
-  if (_.LastReceivedChangelogId.getSetting() >= e) return;
-  let t = await s.default.getOrEnsurePrivateChannel(h.SYSTEM_UPDATES_USER_ID);
+async function O(e) {
+  if (c.LastReceivedChangelogId.getSetting() >= e) return;
+  let t = await s.default.getOrEnsurePrivateChannel(A.SYSTEM_UPDATES_USER_ID);
   if (null == t) return;
   await a.default.fetchMessages({
     channelId: t,
     limit: 1
   });
-  let n = c.default.getLastMessage(t);
+  let n = E.default.getLastMessage(t);
   if (null == n) return;
-  let i = (0, l.default)({
+  let i = (0, u.default)({
     ...n,
     channelId: t,
     messageReference: void 0,
@@ -46,18 +47,18 @@ async function p(e) {
   });
   a.default.receiveMessage(t, {
     ...i,
-    state: A.MessageStates.SENT,
+    state: m.MessageStates.SENT,
     channel_id: t
   }, !0, {})
 }
-class O extends o.default {
+class C extends l.default {
   constructor(...e) {
-    super(...e), N(this, "actions", {
+    super(...e), p(this, "actions", {
       POST_CONNECTION_OPEN: e => this.handleConnectionOpen(e)
-    }), N(this, "handleConnectionOpen", async e => {
+    }), p(this, "handleConnectionOpen", async e => {
       let {
         canReceiveMessage: t
-      } = I.default.getCurrentConfig({
+      } = T.default.getCurrentConfig({
         location: "changelog_manager"
       }, {
         autoTrackExposure: !1
@@ -68,29 +69,29 @@ class O extends o.default {
             min_version: s
           }] of Object.entries(e)) s <= t && s > n && (n = s, i = r);
         return i
-      }(s, (0, f.getClientVersionForChangelog)());
+      }(s, (0, S.getClientVersionForChangelog)());
       if (i.default.dispatch({
           type: "CHANGE_LOG_SET_CONFIG",
           config: n.body,
           latestChangelogId: a
         }), null == a) return;
-      let o = await (0, u.fetchEmailSettings)();
-      if (t && (null == o ? void 0 : o.categories[m.EmailCategories.UPDATES_AND_ANNOUNCEMENTS])) {
-        p(a);
+      let l = await (0, d.fetchEmailSettings)();
+      if (t && (null == l ? void 0 : l.categories[N.EmailCategories.UPDATES_AND_ANNOUNCEMENTS])) {
+        (0, o.getUser)(A.SYSTEM_UPDATES_USER_ID), O(a);
         return
       }
       if (!0 !== s[a].show_on_startup) return;
-      let l = T.default.lastSeenChangelogId(),
-        _ = T.default.lastSeenChangelogDate();
-      if (null != l && 0 >= E.default.compare(a, l)) return;
-      let c = await r.default.fetchChangelog(a, d.default.locale);
-      if (null != c) {
-        if (null == _ || null == T.default.lastSeenChangelogDate()) {
-          r.default.markChangelogAsSeen(a, c.date);
+      let u = f.default.lastSeenChangelogId(),
+        c = f.default.lastSeenChangelogDate();
+      if (null != u && 0 >= I.default.compare(a, u)) return;
+      let E = await r.default.fetchChangelog(a, _.default.locale);
+      if (null != E) {
+        if (null == c || null == f.default.lastSeenChangelogDate()) {
+          r.default.markChangelogAsSeen(a, E.date);
           return
-        }!T.default.isLocked() && new Date(c.date) > new Date(_) && (0, S.openChangelog)()
+        }!f.default.isLocked() && new Date(E.date) > new Date(c) && (0, h.openChangelog)()
       }
     })
   }
 }
-t.default = new O
+t.default = new C
