66efdfa40438e640a0295329d71bce5e5afb0c9e
DiscrapperManager <admin@minerpl.xyz>
2024-05-31 00:02:18 +0200

    canaryRelease: 297620
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25582
     * Scripts: 30283
    Strings: 18044
    Routes: 742
    Experiments Count: 335

--- chunks/754700.js
+++ chunks/754700.js
@@ -4,4 +4,4 @@ n.r(t), n.d(t, {
   FirstPartyQuestTaskTypes: function() {
     return i
   }
-}), (r = i || (i = {})).STREAM_ON_DESKTOP = "STREAM_ON_DESKTOP", r.PLAY_ON_DESKTOP = "PLAY_ON_DESKTOP", r.PLAY_ON_XBOX = "PLAY_ON_XBOX", r.PLAY_ON_PLAYSTATION = "PLAY_ON_PLAYSTATION"
+}), n("47120"), (r = i || (i = {})).STREAM_ON_DESKTOP = "STREAM_ON_DESKTOP", r.PLAY_ON_DESKTOP = "PLAY_ON_DESKTOP", r.PLAY_ON_XBOX = "PLAY_ON_XBOX", r.PLAY_ON_PLAYSTATION = "PLAY_ON_PLAYSTATION"
