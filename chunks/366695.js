03d4dba18d8a055486083de37723ac4c16db55db
DiscrapperManager <admin@minerpl.xyz>
2024-05-31 01:00:12 +0200

    canaryRelease: 297656
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25582
     * Scripts: 30285
    Strings: 18044
    Routes: 742
    Experiments Count: 335

--- chunks/366695.js
+++ chunks/366695.js
@@ -4,108 +4,90 @@ var i = n("735250");
 n("470079");
 var r = n("120356"),
   s = n.n(r),
-  a = n("692547"),
-  o = n("829968"),
-  l = n("346656"),
-  u = n("438071"),
-  d = n("474936"),
-  _ = n("689938"),
-  c = n("458217");
-let E = {
-    XXSMALL: c.xxsmall,
-    XSMALL: c.xsmall,
-    SMALL: c.small,
-    MEDIUM: c.medium,
-    LARGE: c.large
+  a = n("829968"),
+  o = n("346656"),
+  l = n("438071"),
+  u = n("474936"),
+  d = n("689938"),
+  _ = n("458217");
+let c = {
+    XXSMALL: _.xxsmall,
+    XSMALL: _.xsmall,
+    SMALL: _.small,
+    MEDIUM: _.medium,
+    LARGE: _.large
   },
-  I = e => {
+  E = e => {
     let t, {
       game: r,
-      guild: I,
-      skuId: T,
-      pid: f,
-      className: S,
-      guildClassName: h,
-      size: A = E.MEDIUM
+      guild: E,
+      skuId: I,
+      pid: T,
+      className: f,
+      guildClassName: S,
+      size: h = c.MEDIUM
     } = e;
-    if (null != T && (t = function(e) {
+    if (null != I && (t = function(e) {
         if (null == e) return null;
         switch (e) {
-          case d.PremiumSubscriptionSKUs.GUILD:
+          case u.PremiumSubscriptionSKUs.GUILD:
             return n("632342");
-          case d.PremiumSubscriptionSKUs.TIER_0:
+          case u.PremiumSubscriptionSKUs.TIER_0:
             return n("467596");
-          case d.PremiumSubscriptionSKUs.TIER_1:
+          case u.PremiumSubscriptionSKUs.TIER_1:
             return n("670957");
-          case d.PremiumSubscriptionSKUs.TIER_2:
-          case d.PremiumSubscriptionSKUs.LEGACY:
+          case u.PremiumSubscriptionSKUs.TIER_2:
+          case u.PremiumSubscriptionSKUs.LEGACY:
             return n("480768");
           default:
             return null
         }
-      }(T)), null != r && null == t && (t = r.getIconURL(function(e) {
+      }(I)), null != r && null == t && (t = r.getIconURL(function(e) {
         switch (e) {
-          case E.XXSMALL:
+          case c.XXSMALL:
             return 16;
-          case E.XSMALL:
+          case c.XSMALL:
             return 24;
-          case E.SMALL:
+          case c.SMALL:
             return 30;
-          case E.MEDIUM:
+          case c.MEDIUM:
             return 40;
-          case E.LARGE:
+          case c.LARGE:
             return 60;
           default:
             return 80
         }
-      }(A))), null == (t = (0, o.default)(f, t)) && null != I) {
+      }(h))), null == (t = (0, a.default)(T, t)) && null != E) {
       let e = function(e) {
         switch (e) {
-          case E.XSMALL:
-            return l.default.Sizes.SMALLER;
-          case E.SMALL:
-            return l.default.Sizes.SMALL;
-          case E.LARGE:
-            return l.default.Sizes.LARGE;
+          case c.XSMALL:
+            return o.default.Sizes.SMALLER;
+          case c.SMALL:
+            return o.default.Sizes.SMALL;
+          case c.LARGE:
+            return o.default.Sizes.LARGE;
           default:
-          case E.MEDIUM:
-            return l.default.Sizes.MEDIUM
+          case c.MEDIUM:
+            return o.default.Sizes.MEDIUM
         }
-      }(A);
-      return (0, i.jsx)(l.default, {
-        className: s()(c.gameIcon, h, S),
-        guild: I,
+      }(h);
+      return (0, i.jsx)(o.default, {
+        className: s()(_.gameIcon, S, f),
+        guild: E,
         size: e
       })
     }
-    if (null == t) return (0, i.jsx)(u.default, {
-      className: s()(c.gameIcon, A, S)
+    if (null == t) return (0, i.jsx)(l.default, {
+      className: s()(_.gameIcon, h, f)
     });
-    let m = null == r ? void 0 : r.name,
-      N = null != m && "" !== m ? _.default.Messages.APPLICATION_ICON_A11Y_LABEL.format({
-        applicationName: m
-      }) : _.default.Messages.APPLICATION_ICON_NO_NAME_A11Y_LABEL;
+    let A = null == r ? void 0 : r.name,
+      m = null != A && "" !== A ? d.default.Messages.APPLICATION_ICON_A11Y_LABEL.format({
+        applicationName: A
+      }) : d.default.Messages.APPLICATION_ICON_NO_NAME_A11Y_LABEL;
     return (0, i.jsx)("img", {
-      alt: N,
+      alt: m,
       src: t,
-      className: s()(c.gameIcon, A, S),
-      style: {
-        borderRadius: function(e) {
-          switch (e) {
-            case E.XXSMALL:
-              return a.default.radii.xs;
-            case E.XSMALL:
-            case E.SMALL:
-              return a.default.radii.sm;
-            case E.MEDIUM:
-              return a.default.radii.md;
-            case E.LARGE:
-              return a.default.radii.lg;
-            default:
-              return a.default.radii.sm
-          }
-        }(A)
-      }
+      className: s()(_.gameIcon, h, f)
     })
   };
-I.Sizes = E, t.default = I
+E.Sizes = c, t.default = E
