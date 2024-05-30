48f6ffe7b4596d188be4e0805ba0881264aa1cd3
DiscrapperManager <admin@minerpl.xyz>
2024-05-31 00:31:18 +0200

    canaryRelease: 297640
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25582
     * Scripts: 30285
    Strings: 18044
    Routes: 742
    Experiments Count: 335

--- chunks/227172.js
+++ chunks/227172.js
@@ -2,106 +2,121 @@
 n.r(t);
 var a = n("735250"),
   l = n("470079"),
-  s = n("442837"),
-  i = n("706454"),
-  r = n("192918"),
-  o = n("22211"),
-  u = n("561308"),
-  d = n("919394"),
-  c = n("438226"),
-  f = n("206295"),
-  h = n("91140"),
-  m = n("297781"),
-  p = n("591853"),
-  E = n("265048"),
-  C = n("689938");
+  s = n("758713"),
+  i = n("442837"),
+  r = n("481060"),
+  o = n("706454"),
+  u = n("374129"),
+  d = n("639351"),
+  c = n("192918"),
+  f = n("22211"),
+  h = n("561308"),
+  m = n("919394"),
+  p = n("438226"),
+  E = n("206295"),
+  C = n("91140"),
+  g = n("297781"),
+  S = n("591853"),
+  _ = n("265048"),
+  T = n("689938");
+let I = {
+  [s.PlatformType.DESKTOP]: null,
+  [s.PlatformType.XBOX]: d.default,
+  [s.PlatformType.PLAYSTATION]: u.default
+};
 t.default = e => {
   let {
     channel: t,
     entry: n,
-    requestId: g,
-    closePopout: S
+    requestId: s,
+    closePopout: u
   } = e, {
-    iconUrl: _,
-    iconTitle: T,
-    user: I,
-    details: A,
-    gameTitle: v
-  } = (0, E.useGamingContentData)(n), {
-    primaryColor: N,
-    secondaryColor: x
-  } = (0, f.default)(_), M = (0, s.useStateFromStores)([i.default], () => i.default.locale), {
-    streamPreviewUrl: R,
-    stream: y,
-    channel: L
-  } = (0, o.default)(n), {
-    displayParticipants: O,
-    participant1: j,
-    participant2: P,
-    numOtherParticipants: D
-  } = (0, r.default)(n, 3), b = l.useCallback(e => {
-    if (null == _ || null == t || null == I) return;
-    let a = D > 0 ? (0, c.formatGroupCanvasDescription)({
+    iconUrl: d,
+    iconTitle: A,
+    user: v,
+    details: N,
+    gameTitle: x
+  } = (0, _.useGamingContentData)(n), {
+    primaryColor: M,
+    secondaryColor: R
+  } = (0, E.default)(d), y = (0, i.useStateFromStores)([o.default], () => o.default.locale), {
+    streamPreviewUrl: L,
+    stream: O,
+    channel: j
+  } = (0, f.default)(n), {
+    displayParticipants: P,
+    participant1: D,
+    participant2: b,
+    numOtherParticipants: U
+  } = (0, c.default)(n, 3), F = (0, r.useToken)(r.tokens.colors.CONTENT_INVENTORY_OVERLAY_TEXT_PRIMARY), w = l.useCallback(e => {
+    if (null == d || null == t || null == v) return;
+    let a = U > 0 ? (0, p.formatGroupCanvasDescription)({
       entry: n,
       channel: t,
-      users: [j, P],
-      countOthers: D
-    }) : (0, c.formatCanvasDescription)(n, t, I);
-    return (0, d.generateGamingContentImage)({
+      users: [D, b],
+      countOthers: U
+    }) : (0, p.formatCanvasDescription)(n, t, v);
+    return (0, m.generateGamingContentImage)({
       entry: n,
-      applicationImageSrc: _,
-      avatarSrcs: O.map(e => e.getAvatarURL(t.guild_id, 128)),
+      applicationImageSrc: d,
+      avatarSrcs: P.map(e => e.getAvatarURL(t.guild_id, 128)),
       description: a,
-      timestamp: (0, u.formatEntryTimestamp)(n, M),
-      colors: [N, x],
+      timestamp: (0, h.formatEntryTimestamp)(n, y),
+      colors: [M, R],
       channelId: e
     })
-  }, [_, t, O, n, M, D, j, P, N, x, I]);
-  if (null == I) return null;
-  let U = (0, a.jsx)(m.BadgesContainer, {
-      location: m.BadgeLocation.POPOUT,
-      children: h.GAMING_BADGES.map((e, t) => (0, a.jsx)(e, {
+  }, [d, t, P, n, y, U, D, b, M, R, v]);
+  if (null == v) return null;
+  let k = null != n.extra.platform ? I[n.extra.platform] : null,
+    B = (0, a.jsx)(g.BadgesContainer, {
+      location: g.BadgeLocation.POPOUT,
+      children: C.GAMING_BADGES.map((e, t) => (0, a.jsx)(e, {
         entry: n
       }, t))
     }),
-    F = null == R ? (0, a.jsx)(p.PopoutContent, {
-      thumbnailSrc: _,
-      thumbnailTitle: T,
+    H = null == L ? (0, a.jsx)(S.PopoutContent, {
+      thumbnailSrc: d,
+      thumbnailTitle: A,
       channel: t,
-      userDescription: (0, u.isEntryActive)(n) ? C.default.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYING : C.default.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYED,
-      title: v,
-      subtitle: A,
-      badges: U,
+      headerIcons: null != k ? (0, a.jsx)(k, {
+        color: F.hex(),
+        width: 16,
+        height: 16
+      }) : null,
+      userDescription: (0, h.isEntryActive)(n) ? T.default.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYING : T.default.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYED,
+      title: x,
+      subtitle: N,
+      badges: B,
       entry: n
-    }) : (0, a.jsx)(p.StreamingPopoutContent, {
+    }) : (0, a.jsx)(S.StreamingPopoutContent, {
       channel: t,
-      streamPreviewSrc: R,
+      streamPreviewSrc: L,
       title: n.extra.game_name,
-      badges: U,
-      userDescription: C.default.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_STREAMING,
+      badges: B,
+      userDescription: T.default.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_STREAMING,
       entry: n,
-      stream: y
+      stream: O
     });
-  return (0, a.jsxs)(p.Popout, {
-    children: [F, (0, a.jsx)(p.PopoutInteractionsContainer, {
-      children: null != L ? (0, a.jsx)(p.VoiceChannelPopoutReactor, {
-        closePopout: S,
-        user: I,
+  return (0, a.jsxs)(S.Popout, {
+    children: [H, (0, a.jsx)(S.PopoutInteractionsContainer, {
+      children: null != j ? (0, a.jsx)(S.VoiceChannelPopoutReactor, {
+        closePopout: u,
+        user: v,
         channel: t,
-        generateReactionImage: b,
-        reactionImageAltText: (0, c.getContentAltText)(n, I),
+        generateReactionImage: w,
+        reactionImageAltText: (0, p.getContentAltText)(n, v),
         entry: n,
-        requestId: g,
-        voiceChannel: L,
-        isStreaming: null != R
-      }) : (0, a.jsx)(p.PopoutReactor, {
-        closePopout: S,
-        user: I,
+        requestId: s,
+        voiceChannel: j,
+        isStreaming: null != L
+      }) : (0, a.jsx)(S.PopoutReactor, {
+        closePopout: u,
+        user: v,
         channel: t,
-        generateReactionImage: b,
-        reactionImageAltText: (0, c.getContentAltText)(n, I),
+        generateReactionImage: w,
+        reactionImageAltText: (0, p.getContentAltText)(n, v),
         entry: n,
-        requestId: g
+        requestId: s
       })
     })]
   })
---
+++ chunks/758713.js
@@ -0,0 +1,7 @@
+"use strict";
+var a, l;
+n.r(t), n.d(t, {
+  PlatformType: function() {
+    return a
+  }
+}), (l = a || (a = {}))[l.DESKTOP = 0] = "DESKTOP", l[l.XBOX = 1] = "XBOX", l[l.PLAYSTATION = 2] = "PLAYSTATION"
