df92fb8cbfb4c006ff025f9ba6cae8fd18cc9735
DiscrapperManager <admin@minerpl.xyz>
2024-05-30 23:38:53 +0200

    canaryRelease: 297603
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25582
     * Scripts: 30283
    Strings: 18044
    Routes: 742
    Experiments Count: 335

--- chunks/348121.js
+++ chunks/348121.js
@@ -1,27 +1,21 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return l
+    return s
   }
 });
-var i = n("442837"),
-  r = n("594174"),
-  s = n("74538"),
-  a = n("639119"),
-  o = n("474936");
+var i = n("639119"),
+  r = n("474936");
 
-function l() {
-  let e = (0, i.useStateFromStores)([r.default], () => r.default.getCurrentUser()),
-    t = (0, a.usePremiumTrialOffer)(),
-    n = null != t,
-    l = null != e && (0, s.isPremium)(e),
-    u = o.PremiumTypes.TIER_2;
-  if (n) {
-    let e = t.subscription_trial;
-    (null == e ? void 0 : e.sku_id) === o.PremiumSubscriptionSKUs.TIER_0 ? u = o.PremiumTypes.TIER_0 : (null == e ? void 0 : e.sku_id) === o.PremiumSubscriptionSKUs.TIER_2 && (u = o.PremiumTypes.TIER_2)
+function s() {
+  let e = (0, i.usePremiumTrialOffer)(),
+    t = r.PremiumTypes.TIER_2;
+  if (null != e) {
+    let n = e.subscription_trial;
+    (null == n ? void 0 : n.sku_id) === r.PremiumSubscriptionSKUs.TIER_0 ? t = r.PremiumTypes.TIER_0 : (null == n ? void 0 : n.sku_id) === r.PremiumSubscriptionSKUs.TIER_2 && (t = r.PremiumTypes.TIER_2)
   }
   return {
-    isLoading: !l && !n,
-    suggestedPremiumType: u
+    isLoading: !1,
+    suggestedPremiumType: t
   }
 }
