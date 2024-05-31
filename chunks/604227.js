1f993de7f3661db74a3d7f9a0f46f880befc32fc
DiscrapperManager <admin@minerpl.xyz>
2024-05-31 23:33:00 +0200

    canaryRelease: 298055
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25603
     * Scripts: 30306
    Strings: 18051
    Routes: 741
    Experiments Count: 337

--- chunks/604227.js
+++ chunks/604227.js
@@ -1,7 +1,7 @@
 "use strict";
 s.r(t), s.d(t, {
   default: function() {
-    return c
+    return S
   }
 }), s("47120"), s("757143"), s("653041");
 var a = s("735250");
@@ -10,19 +10,21 @@ var n = s("525654"),
   l = s.n(n),
   i = s("481060"),
   r = s("579806"),
-  o = s("969832"),
-  d = s("689938"),
-  u = s("107287");
+  o = s("304761"),
+  d = s("969832"),
+  u = s("689938"),
+  c = s("107287");
 
-function c() {
-  var e, t, s, n, c;
-  let S = window.GLOBAL_ENV.RELEASE_CHANNEL,
-    E = "298016",
-    T = (e = "46d4acc0c7b623958a0f291a95f109ac4dc0bf40", e.substring(0, 7)),
-    _ = null === r.default || void 0 === r.default ? void 0 : r.default.remoteApp.getVersion(),
-    f = null === r.default || void 0 === r.default ? void 0 : null === (t = (s = r.default.remoteApp).getBuildNumber) || void 0 === t ? void 0 : t.call(s),
-    m = null === r.default || void 0 === r.default ? void 0 : null === (n = (c = r.default.remoteApp).getAppArch) || void 0 === n ? void 0 : n.call(c),
-    g = function() {
+function S() {
+  var e, t, s, n, S, E;
+  let T = window.GLOBAL_ENV.RELEASE_CHANNEL,
+    _ = "298055",
+    f = (e = "405d448060a407c8c36fdb371efca01b975ebf2c", e.substring(0, 7)),
+    m = null === r.default || void 0 === r.default ? void 0 : r.default.remoteApp.getVersion(),
+    g = null === r.default || void 0 === r.default ? void 0 : null === (t = (s = r.default.remoteApp).getBuildNumber) || void 0 === t ? void 0 : t.call(s),
+    I = null === r.default || void 0 === r.default ? void 0 : null === (n = (S = r.default.remoteApp).getAppArch) || void 0 === n ? void 0 : n.call(S),
+    N = null === (E = o.default.getCurrentBuildOverride().overrides) || void 0 === E ? void 0 : E.discord_web,
+    h = function() {
       var e;
       let t = null === l() || void 0 === l() ? void 0 : null === (e = l().os) || void 0 === e ? void 0 : e.toString();
       if (null == t || null == r.default) return null;
@@ -30,42 +32,42 @@ function c() {
         [a, n, i] = s.split(".").map(e => parseInt(e, 10));
       return t.includes("Windows 10") && void 0 !== i && i >= 22e3 && (t = t.replace("Windows 10", "Windows 11")), t.includes("OS X 10.15.7") && void 0 !== a && a >= 20 && (t = "macOS ".concat(a - 9)), "".concat(t, " (").concat(s, ")")
     }(),
-    I = [S, E, "(".concat(T, ")")];
-  return null != _ && (I.push("Host ".concat(_)), null != m && I.push(m.toLowerCase()), null != f && I.push("(".concat(f, ")"))), null != g && I.push(g), (0, a.jsx)(o.CopiableField, {
-    copyValue: I.join(" "),
+    C = [T, _, "(".concat(f, ")")];
+  return null != m && (C.push("Host ".concat(m)), null != I && C.push(I.toLowerCase()), null != g && C.push("(".concat(g, ")"))), C.push("Build Override: ".concat(null != N ? N.id : "N/A")), null != h && C.push(h), (0, a.jsx)(d.CopiableField, {
+    copyValue: C.join(" "),
     disableCopy: !1,
-    copyTooltip: d.default.Messages.COPY_VERSION,
+    copyTooltip: u.default.Messages.COPY_VERSION,
     showCopyIcon: !1,
     children: (0, a.jsxs)("div", {
-      className: u.info,
+      className: c.info,
       children: [(0, a.jsxs)(i.Text, {
         tag: "span",
-        className: u.line,
+        className: c.line,
         variant: "text-xs/normal",
         color: "text-muted",
-        children: [S, " ", E, " ", (0, a.jsxs)("span", {
-          className: u.versionHash,
-          children: ["(", T, ")"]
+        children: [T, " ", _, " ", (0, a.jsxs)("span", {
+          className: c.versionHash,
+          children: ["(", f, ")"]
         })]
-      }), " ", null != _ ? (0, a.jsxs)(a.Fragment, {
+      }), " ", null != m ? (0, a.jsxs)(a.Fragment, {
         children: [(0, a.jsxs)(i.Text, {
           tag: "span",
-          className: u.line,
+          className: c.line,
           variant: "text-xs/normal",
           color: "text-muted",
-          children: ["Host ", _, " ", null != m ? (0, a.jsxs)("span", {
-            className: u.appArch,
-            children: [m.toLowerCase(), " "]
-          }) : null, null != f ? (0, a.jsxs)("span", {
-            children: ["(", f, ")"]
+          children: ["Host ", m, " ", null != I ? (0, a.jsxs)("span", {
+            className: c.appArch,
+            children: [I.toLowerCase(), " "]
+          }) : null, null != g ? (0, a.jsxs)("span", {
+            children: ["(", g, ")"]
           }) : null]
         }), " "]
-      }) : null, null != g ? (0, a.jsx)(i.Text, {
+      }) : null, null != h ? (0, a.jsx)(i.Text, {
         tag: "span",
-        className: u.line,
+        className: c.line,
         variant: "text-xs/normal",
         color: "text-muted",
-        children: g
+        children: h
       }) : null]
     })
   })
