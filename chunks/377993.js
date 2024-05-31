5fc518048feb84ac197c694e0e50f1392fb0e585
DiscrapperManager <admin@minerpl.xyz>
2024-05-31 02:26:29 +0200

    canaryRelease: 297709
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25582
     * Scripts: 30285
    Strings: 18044
    Routes: 742
    Experiments Count: 335

--- chunks/377993.js
+++ chunks/377993.js
@@ -1,9 +1,9 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return F
+    return w
   }
-}), n("47120");
+}), n("47120"), n("653041");
 var a = n("735250"),
   l = n("470079"),
   s = n("873546"),
@@ -14,40 +14,38 @@ var a = n("735250"),
   d = n("100527"),
   c = n("906732"),
   f = n("556084"),
-  h = n("484459"),
-  m = n("103575"),
-  p = n("158776"),
-  E = n("699516"),
-  C = n("111583"),
-  g = n("594174"),
-  S = n("467679"),
-  _ = n("729285"),
-  T = n("360048"),
-  I = n("151827"),
-  A = n("626135"),
-  v = n("768581"),
-  N = n("585483"),
-  x = n("233870"),
-  M = n("51144"),
-  R = n("998502"),
-  y = n("276264"),
-  L = n("981631"),
-  O = n("689938"),
-  j = n("895294"),
-  P = n("67431");
-let D = R.default.getEnableHardwareAcceleration();
+  h = n("280234"),
+  m = n("484459"),
+  p = n("103575"),
+  E = n("158776"),
+  C = n("699516"),
+  g = n("111583"),
+  S = n("594174"),
+  _ = n("467679"),
+  T = n("729285"),
+  I = n("360048"),
+  A = n("151827"),
+  v = n("626135"),
+  N = n("768581"),
+  x = n("585483"),
+  M = n("233870"),
+  R = n("51144"),
+  y = n("998502"),
+  L = n("276264"),
+  O = n("981631"),
+  j = n("689938"),
+  P = n("895294"),
+  D = n("67431");
+let b = y.default.getEnableHardwareAcceleration();
 
-function b(e) {
+function U(e) {
   let {
     user: t,
-    channel: l
-  } = e, {
+    channel: l,
     status: d,
-    activities: c
-  } = (0, i.useStateFromStoresObject)([E.default, g.default, p.default], () => E.default.isFriend(t.id) || t === g.default.getCurrentUser() ? {
-    status: p.default.getStatus(t.id),
-    activities: p.default.getActivities(t.id)
-  } : {}), f = (0, i.useStateFromStores)([C.default], () => null != C.default.getTypingUsers(l.id)[t.id]), S = (0, i.useStateFromStores)([g.default], () => g.default.getCurrentUser()), _ = (0, i.useStateFromStores)([p.default], () => p.default.isMobileOnline(t.id)), T = (0, i.useStateFromStores)([E.default], () => E.default.getNickname(t.id)), I = e => {
+    activities: c,
+    lastOnlineTimestamp: f
+  } = e, h = (0, i.useStateFromStores)([g.default], () => null != g.default.getTypingUsers(l.id)[t.id]), _ = (0, i.useStateFromStores)([S.default], () => S.default.getCurrentUser()), T = (0, i.useStateFromStores)([E.default], () => E.default.isMobileOnline(t.id)), I = (0, i.useStateFromStores)([C.default], () => C.default.getNickname(t.id)), A = e => {
     (0, o.openContextMenuLazy)(e, async () => {
       let {
         default: e
@@ -60,10 +58,10 @@ function b(e) {
     })
   };
   return (0, a.jsx)(r.Popout, {
-    preload: () => (0, h.maybeFetchUserProfileForPopout)(t, {
+    preload: () => (0, m.maybeFetchUserProfileForPopout)(t, {
       channelId: l.id
     }),
-    renderPopout: e => (0, a.jsx)(m.default, {
+    renderPopout: e => (0, a.jsx)(p.default, {
       ...e,
       location: "PrivateChannelRecipients",
       userId: t.id,
@@ -72,34 +70,35 @@ function b(e) {
     position: s.isMobile ? "window_center" : "left",
     spacing: 16,
     onShiftClick: () => {
-      let e = "@".concat(M.default.getUserTag(t, {
+      let e = "@".concat(R.default.getUserTag(t, {
           decoration: "never"
         })),
         n = "<@".concat(t.id, ">");
-      N.ComponentDispatch.dispatchToLastSubscribed(L.ComponentActions.INSERT_TEXT, {
+      x.ComponentDispatch.dispatchToLastSubscribed(O.ComponentActions.INSERT_TEXT, {
         plainText: e,
         rawText: n
       }), u.default.startTyping(l.id)
     },
-    children: e => (0, a.jsx)(y.default, {
+    children: e => (0, a.jsx)(L.default, {
       user: t,
-      currentUser: S,
+      currentUser: _,
       isOwner: t.id === l.ownerId,
-      ownerTooltipText: O.default.Messages.GROUP_OWNER,
-      shouldAnimateStatus: D,
-      isTyping: f,
+      ownerTooltipText: j.default.Messages.GROUP_OWNER,
+      shouldAnimateStatus: b,
+      isTyping: h,
       status: d,
       activities: c,
+      lastOnlineTimestamp: f,
       channel: l,
-      onContextMenu: I,
-      isMobile: _,
-      nick: T,
+      onContextMenu: A,
+      isMobile: T,
+      nick: I,
       ...e
     }, t.id)
   })
 }
 
-function U(e) {
+function F(e) {
   var t;
   let {
     integration: i,
@@ -115,17 +114,17 @@ function U(e) {
         integration: i
       })
     })
-  }, [i, u]), c = i.application.bot, f = v.default.getApplicationIconURL({
+  }, [i, u]), c = i.application.bot, f = N.default.getApplicationIconURL({
     id: i.application.id,
     icon: i.application.icon,
     bot: null === (t = i.application) || void 0 === t ? void 0 : t.bot,
     botIconFirst: !0
   });
   return null != c ? (0, a.jsx)(r.Popout, {
-    preload: () => (0, h.maybeFetchUserProfileForPopout)(c.id, f, {
+    preload: () => (0, m.maybeFetchUserProfileForPopout)(c.id, f, {
       channelId: u.id
     }),
-    renderPopout: e => (0, a.jsx)(m.default, {
+    renderPopout: e => (0, a.jsx)(p.default, {
       ...e,
       location: "PrivateChannelRecipients",
       userId: c.id,
@@ -133,11 +132,11 @@ function U(e) {
     }),
     position: s.isMobile ? "window_center" : "left",
     spacing: 16,
-    children: e => (0, a.jsx)(T.default, {
+    children: e => (0, a.jsx)(I.default, {
       onContextMenu: d,
-      className: j.member,
+      className: P.member,
       name: (0, a.jsx)("span", {
-        className: j.username,
+        className: P.username,
         children: i.application.name
       }),
       avatar: (0, a.jsx)(r.Avatar, {
@@ -146,8 +145,8 @@ function U(e) {
         "aria-label": i.application.name,
         statusTooltip: !0
       }),
-      decorators: (0, a.jsx)(S.default, {
-        className: j.botTag,
+      decorators: (0, a.jsx)(_.default, {
+        className: P.botTag,
         verified: null == c ? void 0 : c.isVerifiedBot()
       }),
       id: i.application.id,
@@ -164,59 +163,108 @@ function U(e) {
   }) : null
 }
 
-function F(e) {
+function w(e) {
   let {
     channel: t
-  } = e, s = g.default.getCurrentUser(), o = null == s ? void 0 : s.isStaff(), {
+  } = e, s = S.default.getCurrentUser(), o = null == s ? void 0 : s.isStaff(), {
     analyticsLocations: u
-  } = (0, c.default)(d.default.MEMBER_LIST), m = (0, i.useStateFromStoresArray)([g.default], () => (0, x.getRecipients)(t.recipients, g.default), [t.recipients]), {
-    installedIntegrations: p,
-    applicationsShelf: E,
-    fetched: C,
-    appsInGDMEnabled: v,
-    availableApplications: N
+  } = (0, c.default)(d.default.MEMBER_LIST), p = function(e) {
+    let {
+      isRecentlyOnlineEnabled: t
+    } = h.default.useExperiment({
+      location: "n/a"
+    }, {
+      autoTrackExposure: !1
+    }), n = (0, i.useStateFromStoresArray)([S.default], () => (0, M.getRecipients)(e.recipients, S.default), [e.recipients]), a = (0, i.useStateFromStoresObject)([C.default, S.default, E.default], () => {
+      let e = {};
+      for (let s of n) {
+        var t, a, l;
+        if (C.default.isFriend(s.id) || s.id === (null === (t = S.default.getCurrentUser()) || void 0 === t ? void 0 : t.id)) {
+          let t = E.default.getLastOnlineTimestamp(s.id),
+            n = null != t && (0, h.isRecentlyOnline)(t);
+          e[s.id] = {
+            status: null !== (a = E.default.getStatus(s.id)) && void 0 !== a ? a : O.StatusTypes.OFFLINE,
+            activities: null !== (l = E.default.getActivities(s.id)) && void 0 !== l ? l : [],
+            lastOnlineTimestamp: n ? t : void 0
+          }
+        } else e[s.id] = {
+          status: O.StatusTypes.OFFLINE,
+          activities: []
+        }
+      }
+      return e
+    });
+    return l.useMemo(() => {
+      let e = [];
+      for (let t of n) {
+        let n = {
+          user: t,
+          status: a[t.id].status,
+          activities: a[t.id].activities,
+          lastOnlineTimestamp: a[t.id].lastOnlineTimestamp
+        };
+        e.push(n)
+      }
+      if (!t) return e;
+      let l = [O.StatusTypes.OFFLINE, O.StatusTypes.INVISIBLE, null, void 0],
+        s = [],
+        i = [],
+        r = [];
+      return e.forEach(e => {
+        l.includes(e.status) ? null != e.lastOnlineTimestamp ? i.push(e) : r.push(e) : s.push(e)
+      }), [...s, ...i, ...r]
+    }, [t, n, a])
+  }(t), {
+    installedIntegrations: g,
+    applicationsShelf: N,
+    fetched: x,
+    appsInGDMEnabled: R,
+    availableApplications: y
   } = (0, f.usePrivateChannelIntegrationState)({
     channelId: t.id
   });
   l.useEffect(() => {
     if (o)
-      for (let e of m)(0, h.maybeFetchUserProfileForPopout)(e, {
+      for (let e of p)(0, m.maybeFetchUserProfileForPopout)(e.user, {
         dispatchWait: !0,
         channelId: t.id
       })
-  }, [o, m, t.id]), l.useEffect(() => {
-    A.default.track(L.AnalyticEvents.MEMBER_LIST_VIEWED, {
+  }, [o, p, t.id]), l.useEffect(() => {
+    v.default.track(O.AnalyticEvents.MEMBER_LIST_VIEWED, {
       channel_id: t.id,
       channel_type: t.type,
       guild_id: t.guild_id
     })
   }, [t.guild_id, t.id, t.type]);
-  let M = o && m.every(e => e.isStaff());
+  let L = o && p.every(e => e.user.isStaff());
   return (0, a.jsx)(c.AnalyticsLocationProvider, {
     value: u,
     children: (0, a.jsx)("div", {
-      className: P.membersWrap,
+      className: D.membersWrap,
       children: (0, a.jsxs)(r.Scroller, {
-        className: P.members,
+        className: D.members,
         fade: !0,
-        children: [(0, a.jsxs)(I.default, {
-          className: P.membersGroup,
-          children: ["".concat(O.default.Messages.MEMBERS, "—").concat(m.length, " "), M ? (0, a.jsx)(S.default, {
-            className: P.__invalid_decorator,
-            type: S.default.Types.STAFF_ONLY_DM
+        children: [(0, a.jsxs)(A.default, {
+          className: D.membersGroup,
+          children: ["".concat(j.default.Messages.MEMBERS, "—").concat(p.length, " "), L ? (0, a.jsx)(_.default, {
+            className: D.__invalid_decorator,
+            type: _.default.Types.STAFF_ONLY_DM
           }) : null]
-        }), m.map(e => (0, a.jsx)(b, {
-          user: e,
+        }), p.map(e => (0, a.jsx)(U, {
+          user: e.user,
+          status: e.status,
+          activities: e.activities,
+          lastOnlineTimestamp: e.lastOnlineTimestamp,
           channel: t
-        }, e.id)), v && (p.length > 0 || C && E.length > 0) && (0, a.jsxs)(a.Fragment, {
-          children: [(0, a.jsx)(I.default, {
-            className: P.membersGroup,
-            children: "".concat(O.default.Messages.APPS, "—").concat(p.length)
-          }), p.map(e => (0, a.jsx)(U, {
+        }, e.user.id)), R && (g.length > 0 || x && N.length > 0) && (0, a.jsxs)(a.Fragment, {
+          children: [(0, a.jsx)(A.default, {
+            className: D.membersGroup,
+            children: "".concat(j.default.Messages.APPS, "—").concat(g.length)
+          }), g.map(e => (0, a.jsx)(F, {
             integration: e,
             channel: t
-          }, e.application.id)), N.length > 0 && (0, a.jsx)(T.default, {
-            className: j.member,
+          }, e.application.id)), y.length > 0 && (0, a.jsx)(I.default, {
+            className: P.member,
             onClick: () => {
               (0, r.openModalLazy)(async () => {
                 let {
@@ -229,13 +277,13 @@ function F(e) {
               })
             },
             avatar: (0, a.jsx)("div", {
-              className: P.appIconWrapper,
-              children: (0, a.jsx)(_.default, {
+              className: D.appIconWrapper,
+              children: (0, a.jsx)(T.default, {
                 width: 18,
                 height: 18
               })
             }),
-            name: O.default.Messages.PRIVATE_CHANNEL_ADD_APPS
+            name: j.default.Messages.PRIVATE_CHANNEL_ADD_APPS
           })]
         })]
       })
