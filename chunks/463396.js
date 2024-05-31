d0c083ae8d0503e1403ee9a45a40ac04f9a5b820
DiscrapperManager <admin@minerpl.xyz>
2024-05-31 02:15:08 +0200

    canaryRelease: 297701
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25582
     * Scripts: 30285
    Strings: 18044
    Routes: 742
    Experiments Count: 335

--- chunks/463396.js
+++ chunks/463396.js
@@ -1,5 +1,9 @@
 "use strict";
-n.r(t), n("789020");
+n.r(t), n.d(t, {
+  isRemix: function() {
+    return s
+  }
+}), n("789020");
 var i = n("630388"),
   r = n("981631");
-t.default = e => null != e && null != e.attachments && !(e.attachments.length < 1) && e.attachments.some(e => null != e.flags && (0, i.hasFlag)(e.flags, r.MessageAttachmentFlags.IS_REMIX))
+let s = e => null != e && null != e.attachments && !(e.attachments.length < 1) && e.attachments.some(e => null != e.flags && (0, i.hasFlag)(e.flags, r.MessageAttachmentFlags.IS_REMIX))
