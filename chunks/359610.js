49ded89aa54cc19c99b6affc23249a918d5fd1b5
DiscrapperManager <admin@minerpl.xyz>
2024-06-01 00:00:37 +0200

    canaryRelease: 298073
    Main scripts: 21
    Lazy Loaded Scripts: 823
    Chunks:
     * Main Files: 25603
     * Scripts: 30306
    Strings: 18051
    Routes: 741
    Experiments Count: 337

--- chunks/359610.js
+++ chunks/359610.js
@@ -1,27 +1,34 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return l
+    return o
   }
 });
 var i = n("735250");
 n("470079");
-var r = n("120356"),
-  s = n.n(r),
-  a = n("481060"),
-  o = n("795548");
+var r = n("393586"),
+  s = n("481060"),
+  a = n("795548");
 
-function l(e) {
+function o(e) {
   let {
     children: t,
     className: n,
-    size: r = a.Button.Sizes.SMALL,
+    size: o = s.Button.Sizes.SMALL,
     ...l
   } = e;
-  return (0, i.jsx)(a.Button, {
+  return (0, i.jsx)(s.Button, {
     ...l,
-    size: r,
-    className: s()(o.button, n),
-    children: t
+    color: s.Button.Colors.BRAND,
+    size: o,
+    className: n,
+    children: (0, i.jsxs)("div", {
+      className: a.button,
+      children: [(0, i.jsx)(r.ShopIcon, {
+        color: "white",
+        height: 16,
+        width: 16
+      }), t]
+    })
   })
 }
--- chunks/987106.js
+++ chunks/987106.js
@@ -8,106 +8,114 @@ var l = n("735250");
 n("470079");
 var a = n("120356"),
   u = n.n(a),
-  i = n("481060"),
-  o = n("239091"),
-  s = n("596454"),
-  r = n("911969"),
-  d = n("574399"),
-  c = n("441957"),
-  p = n("572004"),
-  m = n("49012"),
-  f = n("970184"),
-  S = n("280501"),
-  C = n("689938"),
-  v = n("580961");
+  i = n("393586"),
+  o = n("481060"),
+  s = n("239091"),
+  r = n("596454"),
+  d = n("911969"),
+  c = n("574399"),
+  p = n("441957"),
+  m = n("572004"),
+  f = n("49012"),
+  S = n("970184"),
+  C = n("280501"),
+  v = n("689938"),
+  E = n("580961");
 
-function E(e) {
+function h(e) {
   let {
     url: t,
     onSelect: n
   } = e;
-  return p.SUPPORTS_COPY && null != t ? (0, l.jsx)(i.Menu, {
+  return m.SUPPORTS_COPY && null != t ? (0, l.jsx)(o.Menu, {
     navId: "component-button",
-    onClose: o.closeContextMenu,
-    "aria-label": C.default.Messages.MESSAGE_BUTTON_COMPONENT_LINK_CONTEXT_MENU_NAME,
+    onClose: s.closeContextMenu,
+    "aria-label": v.default.Messages.MESSAGE_BUTTON_COMPONENT_LINK_CONTEXT_MENU_NAME,
     onSelect: n,
-    children: (0, l.jsx)(i.MenuGroup, {
-      children: (0, l.jsx)(i.MenuItem, {
+    children: (0, l.jsx)(o.MenuGroup, {
+      children: (0, l.jsx)(o.MenuItem, {
         id: "copy",
-        label: C.default.Messages.MESSAGE_BUTTON_COMPONENT_LINK_CONTEXT_MENU_COPY,
-        action: () => (0, p.copy)(t)
+        label: v.default.Messages.MESSAGE_BUTTON_COMPONENT_LINK_CONTEXT_MENU_COPY,
+        action: () => (0, m.copy)(t)
       })
     })
   }) : null
 }
 
 function N(e) {
-  let t, n;
+  let t;
   let {
-    label: a,
-    style: p,
-    disabled: C,
-    emoji: N,
-    url: h,
+    label: n,
+    style: a,
+    disabled: m,
+    emoji: v,
+    url: N,
     sku_id: g
   } = e, {
-    executeStateUpdate: T,
-    visualState: y,
+    executeStateUpdate: y,
+    visualState: T,
     isDisabled: I
-  } = (0, f.useComponentState)(e), x = null != a && a.length > 0, O = p === r.ButtonStyle.LINK && null != h && h.length > 0, j = y === S.ActionComponentState.LOADING, L = (0, d.useLoadStoreListingDetails)(g);
-  return O ? n = () => {
-    (0, m.handleClick)({
-      href: null != h ? h : "",
+  } = (0, S.useComponentState)(e), x = null != n && n.length > 0, O = a === d.ButtonStyle.LINK && null != N && N.length > 0, j = T === C.ActionComponentState.LOADING, L = (0, c.useLoadStoreListingDetails)(g), _ = a === d.ButtonStyle.PREMIUM;
+  return t = O ? () => {
+    (0, f.handleClick)({
+      href: null != N ? N : "",
       shouldConfirm: !0
     })
-  } : null != L ? (n = L, t = v.premium) : n = () => T(), (0, l.jsxs)(i.Button, {
-    className: t,
+  } : null != L ? L : () => y(), (0, l.jsxs)(o.Button, {
     color: function(e) {
       switch (e) {
-        case r.ButtonStyle.PRIMARY:
-          return i.Button.Colors.BRAND;
-        case r.ButtonStyle.SUCCESS:
-          return i.Button.Colors.GREEN;
-        case r.ButtonStyle.DESTRUCTIVE:
-          return i.Button.Colors.RED;
+        case d.ButtonStyle.PRIMARY:
+        case d.ButtonStyle.PREMIUM:
+          return o.Button.Colors.BRAND;
+        case d.ButtonStyle.SUCCESS:
+          return o.Button.Colors.GREEN;
+        case d.ButtonStyle.DESTRUCTIVE:
+          return o.Button.Colors.RED;
         default:
-          return i.Button.Colors.PRIMARY
+          return o.Button.Colors.PRIMARY
       }
-    }(p),
-    size: i.Button.Sizes.SMALL,
-    disabled: C || y === S.ActionComponentState.DISABLED || I || null != g && void 0 === L,
-    onClick: n,
+    }(a),
+    size: o.Button.Sizes.SMALL,
+    disabled: m || T === C.ActionComponentState.DISABLED || I || null != g && void 0 === L,
+    onClick: t,
     onContextMenu: e => {
-      O && (0, o.openContextMenu)(e, e => (0, l.jsx)(E, {
+      O && (0, s.openContextMenu)(e, e => (0, l.jsx)(h, {
         ...e,
-        url: h
+        url: N
       }))
     },
     role: O ? "link" : "button",
     children: [(0, l.jsxs)("div", {
-      className: u()(v.content, {
-        [v.hidden]: j
+      className: u()(E.content, {
+        [E.hidden]: j
       }),
       "aria-hidden": j,
-      children: [null != N ? (0, l.jsx)(s.default, {
+      children: [_ ? (0, l.jsx)("div", {
+        className: E.premium,
+        children: (0, l.jsx)(i.ShopIcon, {
+          color: "white",
+          height: 16,
+          width: 16
+        })
+      }) : null, null != v ? (0, l.jsx)(r.default, {
         className: u()({
-          [v.textEmoji]: x
+          [E.textEmoji]: x
         }),
-        src: N.src,
-        emojiId: N.id,
-        emojiName: N.name,
-        animated: N.animated
+        src: v.src,
+        emojiId: v.id,
+        emojiName: v.name,
+        animated: v.animated
       }) : null, x ? (0, l.jsx)("div", {
-        className: v.label,
-        children: a
-      }) : null, O ? (0, l.jsx)(c.default, {
-        className: v.launchIcon,
+        className: E.label,
+        children: n
+      }) : null, O ? (0, l.jsx)(p.default, {
+        className: E.launchIcon,
         width: 16,
         height: 16
       }) : null]
     }), j ? (0, l.jsx)("div", {
-      className: v.loading,
-      children: (0, l.jsx)(i.Dots, {
+      className: E.loading,
+      children: (0, l.jsx)(o.Dots, {
         dotRadius: 3.5,
         themed: !0
       })
