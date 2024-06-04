daa34988313c018eefe1e19536ac7179cd68610c
DiscrapperManager <admin@minerpl.xyz>
2024-06-04 19:23:32 +0200

    canaryRelease: 298666
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25615
     * Scripts: 30614
    Strings: 18081
    Routes: 741
    Experiments Count: 337

--- chunks/921801.js
+++ chunks/921801.js
@@ -1,18 +1,23 @@
 "use strict";
 s.r(t), s.d(t, {
   Subsetting: function() {
-    return l
+    return i
   }
 });
 var a = s("735250");
 s("470079");
-var n = s("394644");
-let l = e => {
+var n = s("394644"),
+  l = s("295545");
+let i = e => {
   let {
     setting: t,
     children: s
-  } = e, l = (0, n.useViewableSettingsFilterFromSearch)(t);
-  return l.size > 0 && !l.has(t) ? null : (0, a.jsx)("div", {
+  } = e, i = (0, l.useIsEligibleForUserSettingsSearchDesktop)({
+    location: "subsettings"
+  }), r = (0, n.useViewableSettingsFilterFromSearch)(t);
+  return i ? r.size > 0 && !r.has(t) ? null : (0, a.jsx)("div", {
+    children: s
+  }) : (0, a.jsx)(a.Fragment, {
     children: s
   })
 }
