4750b5faf64c0e950951c6acf450fb4452705a90
DiscrapperManager <admin@minerpl.xyz>
2024-06-04 20:19:53 +0200

    canaryRelease: 298700
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25615
     * Scripts: 30614
    Strings: 18084
    Routes: 741
    Experiments Count: 337

--- chunks/989804.js
+++ chunks/989804.js
@@ -3,26 +3,27 @@ a.r(t);
 var s = a("735250"),
   l = a("470079"),
   n = a("442837"),
-  o = a("37234"),
-  i = a("230711"),
-  u = a("410575"),
+  u = a("37234"),
+  o = a("230711"),
+  i = a("410575"),
   r = a("503089"),
-  d = a("110924"),
-  c = a("100527"),
+  c = a("110924"),
+  d = a("100527"),
   f = a("367907"),
   S = a("906732"),
   g = a("254901"),
   p = a("45493"),
-  E = a("332473"),
-  _ = a("113434"),
-  m = a("663389"),
-  h = a("210887"),
-  b = a("394644"),
-  k = a("295545"),
+  _ = a("332473"),
+  E = a("113434"),
+  h = a("663389"),
+  m = a("210887"),
+  b = a("128277"),
+  k = a("394644"),
+  y = a("295545"),
   T = a("839469"),
-  y = a("981631"),
-  A = a("689938");
-let P = l.forwardRef(function() {
+  A = a("981631"),
+  P = a("689938");
+let v = l.forwardRef(function() {
   p.default.trackExposure({
     location: "cd25e4_1"
   }), g.default.useExperiment({
@@ -32,20 +33,20 @@ let P = l.forwardRef(function() {
   }), g.default.trackExposure({
     location: "user_setting_manual"
   });
-  let e = (0, n.useStateFromStores)([h.default], () => h.default.theme),
-    t = (0, n.useStateFromStores)([h.default], () => h.default.darkSidebar ? y.ThemeTypes.DARK : void 0),
+  let e = (0, n.useStateFromStores)([m.default], () => m.default.theme),
+    t = (0, n.useStateFromStores)([m.default], () => m.default.darkSidebar ? A.ThemeTypes.DARK : void 0),
     {
       section: a,
-      subsection: P,
-      analyticsLocation: v,
-      analyticsLocations: x
-    } = (0, n.useStateFromStoresObject)([m.default], () => {
-      let e = m.default.getSection(),
-        t = m.default.getSubsection(),
+      subsection: v,
+      analyticsLocation: x,
+      analyticsLocations: F
+    } = (0, n.useStateFromStoresObject)([h.default], () => {
+      let e = h.default.getSection(),
+        t = h.default.getSubsection(),
         {
           analyticsLocation: a,
           analyticsLocations: s
-        } = m.default.getProps();
+        } = h.default.getProps();
       return {
         section: e,
         subsection: t,
@@ -53,78 +54,82 @@ let P = l.forwardRef(function() {
         analyticsLocations: s
       }
     }),
-    F = (0, d.default)(a),
-    I = (0, d.default)(P),
+    I = (0, c.default)(a),
+    R = (0, c.default)(v),
+    U = (0, b.useSettingSearchQuery)(),
     {
-      analyticsLocations: R
-    } = (0, S.default)(c.default.USER_SETTINGS);
+      analyticsLocations: j
+    } = (0, S.default)(d.default.USER_SETTINGS);
   l.useEffect(() => {
-    null != a && (a !== F || P !== I) && ! function(e) {
+    null != a && (a !== I || v !== R) && ! function(e) {
       let {
         destinationPane: t,
         originPane: a = null,
         source: s = null,
         subsection: l = null,
-        locationStack: n = null
+        locationStack: n = null,
+        searchQuery: u = null
       } = e;
-      f.default.trackWithMetadata(y.AnalyticEvents.SETTINGS_PANE_VIEWED, {
+      f.default.trackWithMetadata(A.AnalyticEvents.SETTINGS_PANE_VIEWED, {
         settings_type: "user",
         origin_pane: a,
         destination_pane: t,
         location_stack: n,
         source: s,
-        subsection: l
+        subsection: l,
+        search_query: u
       })
     }({
       destinationPane: a,
-      originPane: null != F ? F : null,
-      source: v,
-      subsection: P,
-      locationStack: x
+      originPane: null != I ? I : null,
+      source: x,
+      subsection: v,
+      locationStack: F,
+      searchQuery: "" !== U ? U : null
     })
-  }, [a, F, P, I, v, x]);
-  let U = (0, k.useIsEligibleForUserSettingsSearchDesktop)({
+  }, [a, I, v, R, x, F, U]);
+  let L = (0, y.useIsEligibleForUserSettingsSearchDesktop)({
       location: "user_settings"
     }),
     {
-      hasSearchResults: j,
-      searchResults: L
+      hasSearchResults: N,
+      searchResults: C
     } = (0, T.useSettingSearchResults)(),
-    N = l.useMemo(() => U && j, [U, j]),
-    C = (0, b.useDefaultUserSettingsSections)(),
-    w = (0, b.useSearchUserSettingsSections)(L),
-    D = l.useMemo(() => N ? w : C, [N, w, C]),
-    G = (0, E.useIsEligibleForPomelo)(),
-    M = (0, _.useQuestsForSettingsBadge)(),
+    w = l.useMemo(() => L && N, [L, N]),
+    D = (0, k.useDefaultUserSettingsSections)(),
+    G = (0, k.useSearchUserSettingsSections)(C),
+    M = l.useMemo(() => w ? G : D, [w, G, D]),
+    Q = (0, _.useIsEligibleForPomelo)(),
+    q = (0, E.useQuestsForSettingsBadge)(),
     B = l.useCallback(e => {
-      i.default.setSection(e)
+      o.default.setSection(e)
     }, []);
   return (0, s.jsx)(S.AnalyticsLocationProvider, {
-    value: R,
-    children: (0, s.jsx)(u.default, {
+    value: j,
+    children: (0, s.jsx)(i.default, {
       root: !0,
-      page: y.AnalyticsPages.USER_SETTINGS,
+      page: A.AnalyticsPages.USER_SETTINGS,
       children: (0, s.jsx)(r.default, {
         theme: e,
-        title: A.default.Messages.USER_SETTINGS,
+        title: P.default.Messages.USER_SETTINGS,
         sidebarTheme: t,
         section: a,
         onSetSection: B,
-        onClose: o.popLayer,
-        sections: D,
-        isEligibleForPomelo: G,
-        questsForBadge: M
+        onClose: u.popLayer,
+        sections: M,
+        isEligibleForPomelo: Q,
+        questsForBadge: q
       })
     })
   })
 });
 t.default = l.forwardRef(function() {
-  let e = (0, n.useStateFromStores)([m.default], () => m.default.getProps().analyticsLocations),
+  let e = (0, n.useStateFromStores)([h.default], () => h.default.getProps().analyticsLocations),
     {
       analyticsLocations: t
     } = (0, S.default)(e);
   return (0, s.jsx)(S.AnalyticsLocationProvider, {
     value: t,
-    children: (0, s.jsx)(P, {})
+    children: (0, s.jsx)(v, {})
   })
 })
