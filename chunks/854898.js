e1cd44cf1391ef63ec68ab8940317ffa2f8bf984
DiscrapperManager <admin@minerpl.xyz>
2024-06-04 19:37:00 +0200

    canaryRelease: 298679
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25615
     * Scripts: 30614
    Strings: 18084
    Routes: 741
    Experiments Count: 337

--- chunks/854898.js
+++ chunks/854898.js
@@ -1,8 +1,6 @@
 "use strict";
 e.exports = {
-  badgeList: "badgeList__70639",
   banner: "banner__45a39",
   container: "container__020e3",
-  overlay: "overlay__15b40",
-  usernameBadgeList: "usernameBadgeList__9a1fb"
+  overlay: "overlay__15b40"
 }
