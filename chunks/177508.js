3251df5cd1c8acbbcf82015a9ddec3285a1adab8
DiscrapperManager <admin@minerpl.xyz>
2024-06-04 23:25:31 +0200

    canaryRelease: 298804
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25615
     * Scripts: 30622
    Strings: 18084
    Routes: 741
    Experiments Count: 338

--- chunks/177508.js
+++ chunks/177508.js
@@ -1,39 +1,94 @@
 "use strict";
 s.r(t), s.d(t, {
   default: function() {
-    return u
+    return S
   }
 }), s("390547"), s("47120");
-var a = s("735250");
-s("470079");
-var n = s("120356"),
-  l = s.n(n),
-  i = s("481060"),
-  r = s("359420");
-let o = Object.keys(i.Button.Colors).filter(e => "CUSTOM" !== e),
-  d = Object.keys(i.Button.Looks).filter(e => "BLANK" !== e);
+var a = s("735250"),
+  n = s("470079"),
+  l = s("120356"),
+  i = s.n(l),
+  r = s("971809"),
+  o = s("481060"),
+  d = s("359420");
+let u = Object.keys(o.Button.Colors).filter(e => "CUSTOM" !== e),
+  c = Object.keys(o.Button.Looks).filter(e => "BLANK" !== e);
 
-function u() {
-  let e = o.flatMap(e => [...d.map(t => (0, a.jsxs)(i.Button, {
-    color: i.Button.Colors[e],
-    look: i.Button.Looks[t],
+function S() {
+  let e = u.flatMap(e => [...c.map(t => (0, a.jsxs)(o.Button, {
+    color: o.Button.Colors[e],
+    look: o.Button.Looks[t],
     children: [e, " - ", t]
   }, e + t)), (0, a.jsx)("hr", {
-    className: r.sectionDivider
+    className: d.sectionDivider
   }, e + "divider")]);
   return (0, a.jsxs)("div", {
-    children: [(0, a.jsx)(i.Heading, {
+    children: [(0, a.jsx)(o.Heading, {
       variant: "heading-xl/semibold",
       children: "Buttons"
     }), (0, a.jsxs)("div", {
-      className: r.buttons,
+      className: d.buttons,
       children: [(0, a.jsx)("div", {
-        className: l()("theme-dark", r.section),
+        className: i()("theme-dark", d.section),
         children: e
       }), (0, a.jsx)("div", {
-        className: l()("theme-light", r.section),
+        className: i()("theme-light", d.section),
         children: e
       })]
+    }), (0, a.jsx)(E, {})]
+  })
+}
+
+function E() {
+  let [e, t] = n.useState(null), [s, l] = n.useState(null), [i, u] = n.useState([]), [c, S] = n.useState([]), [E, T] = n.useState([]);
+  return (0, a.jsxs)("div", {
+    className: d.searchableSelectExamples,
+    children: [(0, a.jsx)(o.Heading, {
+      variant: "heading-xl/semibold",
+      children: "SearchableSelect"
+    }), (0, a.jsx)(o.SearchableSelect, {
+      placeholder: "Single select",
+      value: e,
+      onChange: e => t(e),
+      options: r.States
+    }), (0, a.jsx)(o.SearchableSelect, {
+      placeholder: "Single select (clearable)",
+      clearable: !0,
+      value: s,
+      onChange: e => l(e),
+      options: r.States
+    }), (0, a.jsx)(o.SearchableSelect, {
+      placeholder: "Multi select",
+      multi: !0,
+      value: i,
+      onChange: e => u(e),
+      options: r.States
+    }), (0, a.jsx)(o.SearchableSelect, {
+      placeholder: "Multi select (clearable)",
+      multi: !0,
+      clearable: !0,
+      value: E,
+      onChange: e => T(e),
+      options: r.States
+    }), (0, a.jsx)(o.SearchableSelect, {
+      placeholder: "Multi select (custom pills)",
+      multi: !0,
+      value: c,
+      onChange: e => S(e),
+      options: r.States,
+      hidePills: !0,
+      customPillContainerClassName: d.customPillContainer,
+      renderCustomPill: e => (0, a.jsx)("div", {
+        style: {
+          padding: 8,
+          border: "1px solid var(--bg-brand)",
+          borderRadius: 8
+        },
+        children: (0, a.jsx)(o.Text, {
+          variant: "text-md/bold",
+          children: e.label
+        })
+      })
     })]
   })
 }
