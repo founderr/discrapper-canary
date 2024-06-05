a7c14516ec1d317ed852cc3f5d3c55e1033d8652
DiscrapperManager <admin@minerpl.xyz>
2024-06-05 07:48:46 +0200

    canaryRelease: 298981
    Main scripts: 21
    Lazy Loaded Scripts: 827
    Chunks:
     * Main Files: 25618
     * Scripts: 30623
    Strings: 18096
    Routes: 741
    Experiments Count: 338

--- chunks/426171.js
+++ chunks/426171.js
@@ -1,118 +1,122 @@
 "use strict";
-a.r(t), a.d(t, {
+s.r(t), s.d(t, {
   useCategoryDeepLinking: function() {
-    return E
+    return h
   },
   useProductDetailsDeepLinking: function() {
-    return g
+    return E
   },
   useProductDetailsLinkableRoute: function() {
-    return p
+    return m
   }
-}), a("757143"), a("47120"), a("653041");
-var s = a("470079"),
-  l = a("266067"),
-  r = a("442837"),
-  n = a("100527"),
-  i = a("906732"),
-  o = a("328347"),
-  c = a("237031"),
-  u = a("981631");
-let d = "".concat("#").concat("itemSkuId", "="),
-  f = new RegExp("^".concat(d, "(\\d+)$")),
-  C = [u.Routes.COLLECTIBLES_SHOP, u.Routes.COLLECTIBLES_SHOP_FULLSCREEN],
-  p = e => {
+}), s("757143"), s("47120"), s("653041");
+var a = s("470079"),
+  l = s("266067"),
+  r = s("442837"),
+  n = s("607070"),
+  i = s("100527"),
+  o = s("906732"),
+  c = s("328347"),
+  u = s("237031"),
+  d = s("981631");
+let f = "".concat("#").concat("itemSkuId", "="),
+  C = new RegExp("^".concat(f, "(\\d+)$")),
+  p = [d.Routes.COLLECTIBLES_SHOP, d.Routes.COLLECTIBLES_SHOP_FULLSCREEN],
+  m = e => {
     let t = (0, l.useLocation)();
-    s.useEffect(() => {
-      if (null != e && C.includes(t.pathname)) return window.location.replace("".concat(d).concat(e.skuId)), () => {
-        window.location.hash.startsWith(d) && window.location.replace("#")
+    a.useEffect(() => {
+      if (null != e && p.includes(t.pathname)) return window.location.replace("".concat(f).concat(e.skuId)), () => {
+        window.location.hash.startsWith(f) && window.location.replace("#")
       }
     }, [])
-  },
-  m = e => {
-    let {
-      categories: t,
-      productSkuId: a,
-      analyticsLocations: s,
-      analyticsSource: l,
-      initialItemCardRef: r
-    } = e;
-    for (let e of t.values()) {
-      let t = e.products.find(e => e.skuId === a);
-      if (null != t) {
-        var n;
-        null === (n = r.current) || void 0 === n || n.scrollIntoView({
-          behavior: "smooth",
-          block: "center",
-          inline: "center"
-        });
-        let a = setTimeout(() => {
-          let a = document.getElementById("shop-item-".concat(t.skuId));
-          a !== document.activeElement && (null == a || a.focus()), (0, c.openCollectiblesShopProductDetailsModal)({
-            product: t,
-            category: e,
-            analyticsSource: l,
-            analyticsLocations: s,
-            returnRef: r
-          })
-        }, 750);
-        return () => clearTimeout(a)
-      }
-    }
-    return () => {}
   },
   g = e => {
     let {
       categories: t,
-      isFetchingCategories: a,
-      isLayer: c,
-      initialItemCardRef: d
-    } = e, C = s.useRef(null), p = (0, l.useLocation)(), g = p.pathname === u.Routes.COLLECTIBLES_SHOP ? n.default.HOME_PAGE_SHOP_TAB : p.pathname === u.Routes.COLLECTIBLES_SHOP_FULLSCREEN ? n.default.COLLECTIBLES_SHOP_FULLSCREEN : n.default.COLLECTIBLES_SHOP, {
-      analyticsLocations: E
-    } = (0, i.default)(g);
-    s.useEffect(() => {
-      if (c) return;
-      let e = f.exec(p.hash);
+      productSkuId: s,
+      analyticsLocations: a,
+      analyticsSource: l,
+      initialItemCardRef: r,
+      reducedMotion: n = !1
+    } = e;
+    for (let e of t.values()) {
+      let t = e.products.find(e => e.skuId === s);
+      if (null != t) {
+        var i;
+        null === (i = r.current) || void 0 === i || i.scrollIntoView({
+          behavior: n ? "instant" : "smooth",
+          block: "center",
+          inline: "center"
+        });
+        let s = setTimeout(() => {
+          let s = document.getElementById("shop-item-".concat(t.skuId));
+          s !== document.activeElement && (null == s || s.focus()), (0, u.openCollectiblesShopProductDetailsModal)({
+            product: t,
+            category: e,
+            analyticsSource: l,
+            analyticsLocations: a,
+            returnRef: r
+          })
+        }, 750);
+        return () => clearTimeout(s)
+      }
+    }
+    return () => {}
+  },
+  E = e => {
+    let {
+      categories: t,
+      isFetchingCategories: s,
+      isLayer: u,
+      initialItemCardRef: f
+    } = e, p = (0, r.useStateFromStores)([n.default], () => n.default.useReducedMotion), m = a.useRef(null), E = (0, l.useLocation)(), h = E.pathname === d.Routes.COLLECTIBLES_SHOP ? i.default.HOME_PAGE_SHOP_TAB : E.pathname === d.Routes.COLLECTIBLES_SHOP_FULLSCREEN ? i.default.COLLECTIBLES_SHOP_FULLSCREEN : i.default.COLLECTIBLES_SHOP, {
+      analyticsLocations: b
+    } = (0, o.default)(h);
+    a.useEffect(() => {
+      if (u) return;
+      let e = C.exec(E.hash);
       if (null != e) {
         let t = e[1];
-        C.current = t
+        m.current = t
       }
     }, []);
-    let h = (0, r.useStateFromStores)([o.default], () => o.default.initialProductSkuId);
-    s.useEffect(() => {
-      if (a) return;
+    let x = (0, r.useStateFromStores)([c.default], () => c.default.initialProductSkuId);
+    a.useEffect(() => {
+      if (s) return;
       let e = null;
-      if (c && null != h && (e = h), !c && null != C.current && (e = C.current), null != e) {
-        let a = [],
-          s = setTimeout(() => {
-            let s = m({
+      if (u && null != x && (e = x), !u && null != m.current && (e = m.current), null != e) {
+        let s = [],
+          a = setTimeout(() => {
+            let a = g({
               categories: t,
               productSkuId: e,
-              analyticsLocations: E,
-              analyticsSource: g,
-              initialItemCardRef: d
+              analyticsLocations: b,
+              analyticsSource: h,
+              initialItemCardRef: f,
+              reducedMotion: p
             });
-            a.push(s)
+            s.push(a)
           }, 250);
-        return a.push(() => clearTimeout(s)), () => {
-          a.forEach(e => e())
+        return s.push(() => clearTimeout(a)), () => {
+          s.forEach(e => e())
         }
       }
-    }, [c, E, g, t, a, h, d])
+    }, [u, b, h, t, s, x, f, p])
   },
-  E = () => {
-    let e = s.useRef({}),
-      t = s.useCallback((t, a) => {
-        e.current[t] = a
+  h = () => {
+    let e = (0, r.useStateFromStores)([n.default], () => n.default.useReducedMotion),
+      t = a.useRef({}),
+      s = a.useCallback((e, s) => {
+        t.current[e] = s
       }, []);
     return {
-      setCategoryRef: t,
-      handleScrollToCategory: s.useCallback(t => {
+      setCategoryRef: s,
+      handleScrollToCategory: a.useCallback(s => {
         var a;
-        null === (a = e.current[t]) || void 0 === a || a.scrollIntoView({
-          behavior: "smooth",
+        null === (a = t.current[s]) || void 0 === a || a.scrollIntoView({
+          behavior: e ? "instant" : "smooth",
           block: "start"
         })
-      }, [])
+      }, [e])
     }
   }
