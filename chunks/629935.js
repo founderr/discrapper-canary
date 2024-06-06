88a588b684ca7ca9e155625339744f1bab16fcbd
DiscrapperManager <admin@minerpl.xyz>
2024-06-06 23:58:31 +0200

    canaryRelease: 299779
    Main scripts: 22
    Lazy Loaded Scripts: 818
    Chunks:
     * Main Files: 25663
     * Scripts: 34480
    Strings: 18136
    Routes: 741
    Experiments Count: 344

--- chunks/629935.js
+++ chunks/629935.js
@@ -17,12 +17,14 @@ function s(e) {
     textMixAmount: o = 38
   } = e, l = r()(n), u = r()(i), [d, _] = l.luminance() > u.luminance() ? [l, u] : [u, l], c = _.get("hsl.h");
   (0 === c || Number.isNaN(c)) && (_ = d);
-  let E = s ? d.set("hsl.s", 1).set("hsl.l", .82) : _.set("hsl.s", 1).set("hsl.l", .25),
-    I = s ? _.set("hsl.s", 1).set("hsl.l", .05) : d.set("hsl.s", 1).set("hsl.l", .9),
-    [T, f, S] = I.hsl();
+  let E = s ? d.set("hsl.s", 1) : _.set("hsl.s", 1),
+    I = E.get("hsl.h");
+  E = E.set("hsl.l", I >= 200 && I <= 300 ? s ? .8 : .25 : s ? .95 : .15);
+  let T = s ? _.set("hsl.s", 1).set("hsl.l", .05) : d.set("hsl.s", 1).set("hsl.l", .9),
+    [f, S, h] = T.hsl();
   return {
-    "--custom-theme-mix-base-hsl": "".concat(Number.isNaN(T) ? 0 : T, " ").concat(100 * f, "% ").concat(100 * S, "%"),
-    "--custom-theme-mix-base": I.css(),
+    "--custom-theme-mix-base-hsl": "".concat(Number.isNaN(f) ? 0 : f, " ").concat(100 * S, "% ").concat(100 * h, "%"),
+    "--custom-theme-mix-base": T.css(),
     "--custom-theme-mix-text": E.css(),
     "--custom-theme-mix-amount-base": "".concat(t ? a : 0, "%"),
     "--custom-theme-mix-amount-text": "".concat(t ? o : 0, "%")
