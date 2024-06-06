0d205d79dd45e21cd1dae860d258a023bcffc18b
DiscrapperManager <admin@minerpl.xyz>
2024-06-06 03:13:34 +0200

    canaryRelease: 299440
    Main scripts: 21
    Lazy Loaded Scripts: 815
    Chunks:
     * Main Files: 25642
     * Scripts: 32360
    Strings: 18106
    Routes: 741
    Experiments Count: 340

--- chunks/826646.js
+++ chunks/826646.js
@@ -1,7 +1,7 @@
 "use strict";
 t.r(a), t.d(a, {
   default: function() {
-    return I
+    return S
   }
 });
 var l = t("735250");
@@ -10,73 +10,67 @@ var i = t("442837"),
   r = t("954138"),
   n = t("979264"),
   s = t("741308"),
-  o = t("724593"),
-  u = t("530"),
-  d = t("993409"),
-  c = t("790711"),
-  f = t("67152"),
-  m = t("271383"),
-  v = t("246946"),
-  p = t("654904"),
-  g = t("228168"),
+  o = t("530"),
+  u = t("993409"),
+  d = t("790711"),
+  c = t("67152"),
+  f = t("271383"),
+  m = t("246946"),
+  v = t("654904"),
+  p = t("228168"),
   h = t("689938"),
-  S = t("493069");
+  g = t("493069");
 
-function I(e) {
+function S(e) {
   var a;
   let {
     user: t,
-    displayProfile: I,
-    guild: T,
-    pendingAvatar: P,
-    pendingNickname: A,
-    pendingGlobalName: E,
-    pendingBio: y,
-    pendingPronouns: x,
-    isTryItOutFlow: j,
+    displayProfile: S,
+    guild: I,
+    pendingAvatar: T,
+    pendingNickname: P,
+    pendingGlobalName: A,
+    pendingBio: E,
+    pendingPronouns: y,
+    isTryItOutFlow: x,
     hideBioSection: U,
-    hideExampleButton: N
-  } = e, C = (0, i.useStateFromStores)([m.default], () => null == T ? null : m.default.getMember(T.id, t.id)), b = (0, i.useStateFromStores)([v.default], () => v.default.hidePersonalInformation), _ = (0, r.default)("SimplifiedProfileCustomizationPreviewBody"), w = null == I ? void 0 : null === (a = I.getPreviewBio(y)) || void 0 === a ? void 0 : a.value, M = null != x ? x : null == I ? void 0 : I.pronouns, B = (0, p.getPreviewName)({
-    pendingNickname: A,
-    pendingGlobalName: E,
+    hideExampleButton: j
+  } = e, N = (0, i.useStateFromStores)([f.default], () => null == I ? null : f.default.getMember(I.id, t.id)), C = (0, i.useStateFromStores)([m.default], () => m.default.hidePersonalInformation), b = (0, r.default)("SimplifiedProfileCustomizationPreviewBody"), _ = null == S ? void 0 : null === (a = S.getPreviewBio(E)) || void 0 === a ? void 0 : a.value, w = null != y ? y : null == S ? void 0 : S.pronouns, M = (0, v.getPreviewName)({
+    pendingNickname: P,
+    pendingGlobalName: A,
     user: t,
-    guildMember: C
+    guildMember: N
   });
   return (0, l.jsxs)("div", {
-    className: S.body,
-    children: [(0, l.jsx)(u.default, {
+    className: g.body,
+    children: [(0, l.jsx)(o.default, {
       user: t,
-      profileType: g.UserProfileTypes.BITE_SIZE,
+      profileType: p.UserProfileTypes.BITE_SIZE,
       usernameIcon: (() => {
-        if (!(null != C)) return;
-        if (null !== P) {
-          if (!(null == C.avatar && null == P)) return (0, l.jsx)(c.default, {
+        if (!(null != N)) return;
+        if (null !== T) {
+          if (!(null == N.avatar && null == T)) return (0, l.jsx)(d.default, {
             user: t,
-            nickname: B
+            nickname: M
           })
         }
       })(),
-      nickname: B,
-      pronouns: M,
-      isTryItOut: j,
+      nickname: M,
+      pronouns: w,
+      isTryItOut: x,
       tags: (0, l.jsx)(s.default, {
-        displayProfile: I,
-        profileType: g.UserProfileTypes.BITE_SIZE
-      }),
-      nicknameIcons: (0, l.jsx)(o.default, {
-        user: t,
-        isHovering: !0,
-        disabled: !0
+        displayProfile: S,
+        profileType: p.UserProfileTypes.BITE_SIZE
       })
-    }), _ && (0, l.jsx)(n.default, {
+    }), b && (0, l.jsx)(n.default, {
       userId: t.id,
       inline: !1
-    }), !U && null != w && "" !== w && (0, l.jsx)(f.default, {
+    }), !U && null != _ && "" !== _ && (0, l.jsx)(c.default, {
       user: t,
-      bio: w,
-      hidePersonalInformation: b,
+      bio: _,
+      hidePersonalInformation: C,
       disableViewFullBio: !0
-    }), !N && (0, l.jsx)(d.default, {
+    }), !j && (0, l.jsx)(u.default, {
       text: h.default.Messages.USER_SETTINGS_CUSTOMIZE_PROFILE_EXAMPLE_BUTTON
     })]
   })
