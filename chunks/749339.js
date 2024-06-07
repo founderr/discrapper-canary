7193e9c86d9ec0a4c77b92233e5d221b0c71a3b2
DiscrapperManager <admin@minerpl.xyz>
2024-06-07 02:42:49 +0200

    canaryRelease: 299906
    Main scripts: 22
    Lazy Loaded Scripts: 817
    Chunks:
     * Main Files: 25672
     * Scripts: 34493
    Strings: 18137
    Routes: 741
    Experiments Count: 345

--- chunks/749339.js
+++ chunks/749339.js
@@ -1,23 +1,24 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return r
+    return o
   }
 });
 var a = n("735250");
 n("470079");
 var i = n("481060"),
-  l = n("757206"),
-  s = n("572004"),
-  u = n("689938");
+  l = n("39154"),
+  s = n("757206"),
+  u = n("572004"),
+  r = n("689938");
 
-function r(e) {
-  return s.SUPPORTS_COPY ? (0, a.jsx)(i.MenuItem, {
+function o(e) {
+  return u.SUPPORTS_COPY ? (0, a.jsx)(i.MenuItem, {
     id: "copy-text",
-    label: u.default.Messages.COPY_TEXT,
-    icon: l.default,
+    label: r.default.Messages.COPY_TEXT,
+    icon: s.default,
     action: () => {
-      (0, s.copy)(e.content)
+      e = (0, l.default)(e), (0, u.copy)(e.content)
     }
   }) : null
 }
--- routes.js
+++ routes.js
@@ -2621,7 +2621,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-269870db2819308cbb88c981af4d52ecd9b85947",
+                    release: "discord_web-fe66d268b8701fddd96c72a2b4e7ef96cc17bc55",
                     beforeSend: _ => {
                         var E, e;
                         return !(null != (E = _).exception && null != E.exception.values && E.exception.values.every(_ => null == _.stacktrace || null != _.stacktrace.frames && 1 === _.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || a.some(_ => window.navigator.appVersion.toLowerCase().indexOf(_) >= 0)) && !R() && !("Aborted" === (e = _).message || "cancel captcha" === e.message) && t() ? _ : null
@@ -2639,7 +2639,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "ResizeObserver loop completed with undelivered notifications.", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), I.setTag("buildNumber", (_ = "299899", "299899")), I.setTag("builtAt", String("1717719241603"));
+                }), I.setTag("buildNumber", (_ = "299906", "299906")), I.setTag("builtAt", String("1717720349382"));
                 let E = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != E && "object" == typeof E)
                     for (let _ in E) I.setTag(_, E[_]);
