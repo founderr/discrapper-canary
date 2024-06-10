3efb7c6e69a397ffd2f65771bd68dbc7f87565a5
DiscrapperManager <admin@minerpl.xyz>
2024-06-10 19:04:12 +0200

    canaryRelease: 300400
    Main scripts: 22
    Lazy Loaded Scripts: 817
    Chunks:
     * Main Files: 25663
     * Scripts: 34543
    Strings: 18120
    Routes: 741
    Experiments Count: 344

--- chunks/834534.js
+++ chunks/834534.js
@@ -1,7 +1,7 @@
 "use strict";
 l.r(t), l.d(t, {
   default: function() {
-    return x
+    return A
   }
 });
 var s = l("735250");
@@ -14,62 +14,69 @@ var i = l("120356"),
   u = l("706454"),
   d = l("430824"),
   c = l("785717"),
-  f = l("648067"),
-  S = l("588822"),
-  E = l("900927"),
-  I = l("678738"),
-  m = l("652853"),
-  _ = l("335191"),
+  f = l("532776"),
+  S = l("648067"),
+  E = l("588822"),
+  I = l("900927"),
+  m = l("678738"),
+  _ = l("652853"),
+  p = l("335191"),
   v = l("351707"),
-  p = l("228168"),
-  T = l("689938"),
-  g = l("363175");
+  T = l("228168"),
+  g = l("689938"),
+  x = l("363175");
 
-function x(e) {
+function A(e) {
   let {
     user: t,
     currentUser: l,
     displayProfile: i,
-    autoFocusNote: x,
-    className: A
+    autoFocusNote: A,
+    className: C
   } = e, {
     theme: N
-  } = (0, m.useUserProfileThemeContext)(), {
-    trackUserProfileAction: C
-  } = (0, c.useUserProfileAnalyticsContext)(), U = null == i ? void 0 : i.guildId, R = (0, n.useStateFromStores)([d.default], () => null != U ? d.default.getGuild(U) : null), h = (0, n.useStateFromStores)([u.default], () => u.default.locale), P = (0, f.default)(t.id);
+  } = (0, _.useUserProfileThemeContext)(), {
+    trackUserProfileAction: U
+  } = (0, c.useUserProfileAnalyticsContext)(), R = null == i ? void 0 : i.guildId, h = (0, n.useStateFromStores)([d.default], () => null != R ? d.default.getGuild(R) : null), P = (0, n.useStateFromStores)([u.default], () => u.default.locale), M = (0, S.default)(t.id), y = (0, f.default)(t.id);
   return (0, s.jsxs)(o.ScrollerThin, {
     fade: !0,
-    className: a()(g.scroller, A),
-    children: [(null == i ? void 0 : i.bio) != null && (null == i ? void 0 : i.bio) !== "" && (0, s.jsx)(S.default, {
+    className: a()(x.scroller, C),
+    children: [(null == i ? void 0 : i.bio) != null && (null == i ? void 0 : i.bio) !== "" && (0, s.jsx)(E.default, {
       userBio: i.bio,
       setLineClamp: !1
-    }), null != R && (0, s.jsx)(v.default, {
+    }), null != h && (0, s.jsx)(v.default, {
       user: t,
       currentUser: l,
-      guild: R
-    }), (0, s.jsx)(I.default, {
-      title: T.default.Messages.USER_PROFILE_MEMBER_SINCE,
-      children: (0, s.jsx)(E.default, {
+      guild: h
+    }), (0, s.jsx)(m.default, {
+      title: g.default.Messages.USER_PROFILE_MEMBER_SINCE,
+      children: (0, s.jsx)(I.default, {
         userId: t.id,
         guildId: null == i ? void 0 : i.guildId,
-        tooltipDelay: p.USER_PROFILE_TOOLTIP_DELAY
+        tooltipDelay: T.USER_PROFILE_TOOLTIP_DELAY
       })
-    }), P.length > 0 && (0, s.jsx)(I.default, {
-      title: T.default.Messages.CONNECTIONS,
-      children: (0, s.jsx)(_.ConnectedUserAccounts, {
-        connectedAccounts: P,
-        className: g.connections,
+    }), M.length > 0 && (0, s.jsx)(m.default, {
+      title: g.default.Messages.CONNECTIONS,
+      children: (0, s.jsx)(p.ConnectedUserAccounts, {
+        connectedAccounts: M,
+        className: x.connections,
         userId: t.id,
         theme: N,
-        locale: h
+        locale: P
       })
-    }), (0, s.jsx)(I.default, {
-      title: T.default.Messages.NOTE,
+    }), y.length > 0 && (0, s.jsx)(m.default, {
+      title: g.default.Messages.APPS,
+      children: y.map(e => (0, s.jsx)(p.ConnectedApplicationUserRoleAccount, {
+        applicationRoleConnection: e,
+        locale: P
+      }, e.application.id))
+    }), (0, s.jsx)(m.default, {
+      title: g.default.Messages.NOTE,
       children: (0, s.jsx)(r.default, {
         userId: t.id,
-        className: g.note,
-        autoFocus: x,
-        onUpdate: () => C({
+        className: x.note,
+        autoFocus: A,
+        onUpdate: () => U({
           action: "SET_NOTE"
         })
       })
