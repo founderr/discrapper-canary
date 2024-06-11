844d915be3e164164615d0dda9d2e07b37f12f0b
DiscrapperManager <admin@minerpl.xyz>
2024-06-12 01:33:28 +0200

    canaryRelease: 301093
    Main scripts: 22
    Lazy Loaded Scripts: 810
    Chunks:
     * Main Files: 26530
     * Scripts: 32930
    Strings: 18010
    Routes: 742
    Experiments Count: 335

--- chunks/225138.js
+++ chunks/225138.js
@@ -1,10 +1,10 @@
 "use strict";
 n.r(t), n.d(t, {
   default: function() {
-    return U
+    return F
   },
   useMessageMenu: function() {
-    return F
+    return w
   }
 });
 var a = n("735250");
@@ -28,22 +28,23 @@ var i = n("374470"),
   h = n("601184"),
   I = n("6148"),
   A = n("519110"),
-  _ = n("168405"),
-  T = n("536639"),
-  C = n("720904"),
-  R = n("88791"),
-  O = n("235047"),
-  x = n("385302"),
-  y = n("873699"),
-  N = n("134323"),
-  j = n("429260"),
-  b = n("1626"),
-  P = n("279329"),
-  D = n("627938"),
-  L = n("481300"),
-  G = n("689938");
+  _ = n("759875"),
+  T = n("168405"),
+  C = n("536639"),
+  R = n("720904"),
+  O = n("88791"),
+  x = n("235047"),
+  y = n("385302"),
+  N = n("873699"),
+  j = n("134323"),
+  b = n("429260"),
+  P = n("1626"),
+  D = n("279329"),
+  L = n("627938"),
+  G = n("481300"),
+  U = n("689938");
 
-function U(e) {
+function F(e) {
   var t, n;
   let a, l, u, r, {
       channel: o,
@@ -62,7 +63,7 @@ function U(e) {
   else
     for (;
       (0, i.isElement)(m);)(0, i.isElement)(m, HTMLImageElement) && null != m.src && (l = m.src), (0, i.isElement)(m, HTMLAnchorElement) && null != m.href && (a = m.href, r = m.textContent, null == l && "img" === m.getAttribute("data-role") && (l = a, m.hasAttribute("data-safe-src") && "" !== m.getAttribute("data-safe-src") && (u = m.getAttribute("data-safe-src")))), m = m.parentNode;
-  return F({
+  return w({
     message: d,
     channel: o,
     mediaItem: f,
@@ -79,69 +80,69 @@ function U(e) {
     onSelect: M,
     onClose: s.closeContextMenu,
     navId: "message",
-    ariaLabel: G.default.Messages.MESSAGE_ACTIONS_MENU_LABEL,
+    ariaLabel: U.default.Messages.MESSAGE_ACTIONS_MENU_LABEL,
     shouldHideMediaOptions: E
   })
 }
 
-function F(e) {
+function w(e) {
   let {
     message: t,
     channel: n,
     mediaItem: i,
     textSelection: s,
-    favoriteableType: U,
-    favoriteableId: F,
-    favoriteableName: w,
-    itemHref: k,
-    itemSrc: V,
-    itemSafeSrc: K,
-    itemTextContent: Y,
-    canReport: H,
-    onHeightUpdate: X,
-    onSelect: B,
-    onClose: Z,
+    favoriteableType: F,
+    favoriteableId: w,
+    favoriteableName: k,
+    itemHref: V,
+    itemSrc: K,
+    itemSafeSrc: Y,
+    itemTextContent: H,
+    canReport: X,
+    onHeightUpdate: B,
+    onSelect: Z,
+    onClose: W,
     navId: z,
-    ariaLabel: W,
-    shouldHideMediaOptions: J = !1
-  } = e, q = (0, o.default)(s), $ = (0, f.default)(s), Q = (0, C.default)(t, n), ee = (0, m.default)(t, n), et = (0, I.default)(t, n), en = (0, T.default)(t, n), ea = (0, A.default)({
-    type: U,
-    id: F,
-    name: w
-  }), ei = (0, b.default)(t, n), el = (0, _.default)(t, n), es = (0, R.default)(t), eu = (0, v.default)(t, n), er = (0, p.default)(t, n), eo = (0, x.default)(t, n), ed = (0, y.default)(t, n), ec = (0, h.default)(t, n), ef = (0, j.default)(t), eE = (0, P.default)(t, n), eM = (0, c.default)(null != k ? k : V, Y, t, {
-    shouldHideMediaOptions: J
-  }), eg = (0, d.default)(K, t, {
-    shouldHideMediaOptions: J
-  }), em = (0, L.default)(t, i), ev = (0, r.default)({
+    ariaLabel: J,
+    shouldHideMediaOptions: q = !1
+  } = e, $ = (0, o.default)(s), Q = (0, f.default)(s), ee = (0, R.default)(t, n), et = (0, m.default)(t, n), en = (0, I.default)(t, n), ea = (0, C.default)(t, n), ei = (0, A.default)({
+    type: F,
+    id: w,
+    name: k
+  }), el = (0, P.default)(t, n), es = (0, T.default)(t, n), eu = (0, O.default)(t), er = (0, v.default)(t, n), eo = (0, p.default)(t, n), ed = (0, y.default)(t, n), ec = (0, N.default)(t, n), ef = (0, h.default)(t, n), eE = (0, b.default)(t), eM = (0, D.default)(t, n), eg = (0, c.default)(null != V ? V : K, H, t, {
+    shouldHideMediaOptions: q
+  }), em = (0, d.default)(Y, t, {
+    shouldHideMediaOptions: q
+  }), ev = (0, G.default)(t, i), ep = (0, r.default)({
     id: t.id,
-    label: G.default.Messages.COPY_ID_MESSAGE,
+    label: U.default.Messages.COPY_ID_MESSAGE,
     shiftId: "".concat(t.channel_id, "-").concat(t.id)
-  }), ep = (0, N.default)(t, n), eS = (0, D.default)(t, n), eh = (0, g.default)(s, n.getGuildId()), eI = (0, O.default)(t), eA = (0, S.default)(t), e_ = (0, M.default)({
+  }), eS = (0, j.default)(t, n), eh = (0, _.default)(t, n), eI = (0, L.default)(t, n), eA = (0, g.default)(s, n.getGuildId()), e_ = (0, x.default)(t), eT = (0, S.default)(t), eC = (0, M.default)({
     commandType: u.ApplicationCommandType.MESSAGE,
     commandTargetId: t.id,
     channel: n,
     guildId: void 0,
-    onHeightUpdate: X
-  }), eT = (0, E.default)(t);
+    onHeightUpdate: B
+  }), eR = (0, E.default)(t);
   return (0, a.jsxs)(l.Menu, {
     navId: z,
-    onClose: Z,
-    "aria-label": W,
-    onSelect: B,
+    onClose: W,
+    "aria-label": J,
+    onSelect: Z,
     children: [(0, a.jsx)(l.MenuGroup, {
-      children: q
-    }), (0, a.jsx)(l.MenuGroup, {
       children: $
-    }), (0, a.jsxs)(l.MenuGroup, {
-      children: ["" === s ? Q : null, ee, es, et, en, ea, ep, eS, eA, e_, ei, el, eI, eu, er, eE, eT, ed, eo, ec, H && ef]
-    }), null != eh && (0, a.jsx)(l.MenuGroup, {
-      children: eh
-    }), (0, a.jsxs)(l.MenuGroup, {
-      children: [eg, em]
     }), (0, a.jsx)(l.MenuGroup, {
-      children: eM
+      children: Q
+    }), (0, a.jsxs)(l.MenuGroup, {
+      children: ["" === s ? ee : null, et, eu, en, ea, ei, eS, eh, eI, eT, eC, el, es, e_, er, eo, eM, eR, ec, ed, ef, X && eE]
+    }), null != eA && (0, a.jsx)(l.MenuGroup, {
+      children: eA
+    }), (0, a.jsxs)(l.MenuGroup, {
+      children: [em, ev]
     }), (0, a.jsx)(l.MenuGroup, {
-      children: ev
+      children: eg
+    }), (0, a.jsx)(l.MenuGroup, {
+      children: ep
     })]
   })
 }
---
+++ chunks/759875.js
@@ -0,0 +1,31 @@
+"use strict";
+n.r(t), n.d(t, {
+  default: function() {
+    return d
+  }
+});
+var a = n("735250");
+n("470079");
+var i = n("138270"),
+  l = n("481060"),
+  s = n("346610"),
+  u = n("868643"),
+  r = n("996861"),
+  o = n("689938");
+
+function d(e, t) {
+  let n = (0, u.useCanForwardMessage)(e),
+    {
+      canForwardMessages: d
+    } = s.MessageForwardingExperiment.useExperiment({
+      location: "MessageActionsMenu"
+    });
+  return n && d ? (0, a.jsx)(l.MenuItem, {
+    id: "forward",
+    label: o.default.Messages.MESSAGE_ACTION_FORWARD,
+    icon: i.ArrowAngleRightUpIcon,
+    action: () => {
+      (0, r.forwardMessage)(t, e)
+    }
+  }) : null
+}
