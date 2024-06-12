c42a6d9b3de91157e157a45f1d90fa45836ac71f
DiscrapperManager <admin@minerpl.xyz>
2024-06-12 20:32:40 +0200

    canaryRelease: 301310
    Main scripts: 22
    Lazy Loaded Scripts: 810
    Chunks:
     * Main Files: 26539
     * Scripts: 32929
    Strings: 18012
    Routes: 741
    Experiments Count: 335

--- chunks/658590.js
+++ chunks/658590.js
@@ -1,7 +1,14 @@
 "use strict";
 s.r(t), s.d(t, {
   shouldForceQuestBarV2: function() {
-    return a
+    return l
   }
 });
-let a = e => "1193992107035983872" === e.id
+var a = s("918701"),
+  n = s("46140");
+let l = e => {
+  let t = "1193992107035983872" === e.id,
+    s = (0, a.hasVariant)(e, n.QuestVariants.IN_HOUSE_CONSOLE_QUEST),
+    l = (0, a.hasVariant)(e, n.QuestVariants.MOBILE_CONSOLE_QUEST);
+  return t || s || l
+}
