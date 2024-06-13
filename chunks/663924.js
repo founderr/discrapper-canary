262bde118ed0373487a8725b2dd1d17f1cbd3ecd
DiscrapperManager <admin@minerpl.xyz>
2024-06-13 23:19:17 +0200

    canaryRelease: 301801
    Main scripts: 22
    Lazy Loaded Scripts: 817
    Chunks:
     * Main Files: 26596
     * Scripts: 32888
    Strings: 18052
    Routes: 742
    Experiments Count: 340

--- chunks/663924.js
+++ chunks/663924.js
@@ -20,59 +20,52 @@ var i = n("735250"),
   m = n("314734"),
   N = n("981631"),
   p = n("175323");
-t.default = r.memo(function(e) {
+
+function O(e) {
   let t, {
-      positionTargetRef: n,
-      channel: s,
-      closeOnModalOuterClick: O = !1,
-      parentModalKey: C
+      channel: n,
+      closeOnModalOuterClick: s = !1,
+      parentModalKey: a
     } = e,
-    R = c.AppLauncherRecommendationsExperiment.useExperiment({
+    d = c.AppLauncherRecommendationsExperiment.useExperiment({
       location: "App Launcher Popup"
     }, {
       autoTrackExposure: !0
     }),
-    g = r.useRef(null),
-    L = r.useRef(null),
+    E = r.useRef(null),
     {
-      renderWindow: v,
-      windowDispatch: D
+      renderWindow: O,
+      windowDispatch: C
     } = r.useContext(_.default),
-    M = null != C,
-    y = (0, l.useIsModalAtTop)(null != C ? C : ""),
-    P = () => {
-      f.dismissAppLauncherPopup()
-    },
-    U = r.useCallback(e => {
+    R = null != a,
+    g = (0, l.useIsModalAtTop)(null != a ? a : ""),
+    L = r.useCallback(e => {
       var t;
-      if (!M && (0, l.hasAnyModalOpen)() || M && !(y && O)) return;
+      if (!R && (0, l.hasAnyModalOpen)() || R && !(g && s)) return;
       let {
         target: n
       } = e;
       if ((0, o.isElement)(n) && null != n.closest("." + m.APP_LAUNCHER_ELEMENT_CLASSNAME)) return;
       for (;
         (0, o.isElement)(n);) {
-        if (n === L.current) return;
+        if (n === E.current) return;
         n = n.parentNode
       }
-      P();
+      f.dismissAppLauncherPopup();
       let i = null === (t = (0, T.eventOwnerDocument)(e)) || void 0 === t ? void 0 : t.activeElement;
       (null == i || "BODY" === i.tagName) && I.ComponentDispatch.dispatchToLastSubscribed(N.ComponentActions.TEXTAREA_FOCUS)
-    }, [O, y, M]),
-    b = r.useCallback(() => {
-      P()
-    }, []);
-  r.useLayoutEffect(() => (v.addEventListener("mousedown", U), v.addEventListener("contextmenu", U), D.subscribe(N.ComponentActions.POPOUT_CLOSE, b), () => {
-    v.removeEventListener("mousedown", U), v.removeEventListener("contextmenu", U), D.unsubscribe(N.ComponentActions.POPOUT_CLOSE, b)
-  }), [b, U, v, D]), (0, u.useFocusLock)(g), r.useEffect(() => {
-    (!M && (0, l.hasAnyModalOpen)() || M && !y) && P()
-  }, [y, M]);
+    }, [s, g, R]);
+  r.useLayoutEffect(() => (O.addEventListener("mousedown", L), O.addEventListener("contextmenu", L), C.subscribe(N.ComponentActions.POPOUT_CLOSE, f.dismissAppLauncherPopup), () => {
+    O.removeEventListener("mousedown", L), O.removeEventListener("contextmenu", L), C.unsubscribe(N.ComponentActions.POPOUT_CLOSE, f.dismissAppLauncherPopup)
+  }), [L, O, C]), (0, u.useFocusLock)(E), r.useEffect(() => {
+    (!R && (0, l.hasAnyModalOpen)() || R && !g) && f.dismissAppLauncherPopup()
+  }, [g, R]);
   let {
-    history: G,
-    setHistory: w,
-    currentView: k,
-    pushHistory: B,
-    goBack: x
+    history: v,
+    setHistory: D,
+    currentView: M,
+    pushHistory: y,
+    goBack: P
   } = function() {
     let [e, t] = r.useState([{
       type: S.HistoryItemType.HOME
@@ -91,61 +84,67 @@ t.default = r.memo(function(e) {
       }
     }
   }();
-  switch (null == k ? void 0 : k.type) {
+  switch (null == M ? void 0 : M.type) {
     case S.HistoryItemType.HOME:
       t = (0, i.jsx)(A.default, {
-        channel: s,
-        enableRecommendations: R.enabled,
-        enableRecents: R.recentsDropdownEnabled
+        channel: n,
+        enableRecommendations: d.enabled,
+        enableRecents: d.recentsDropdownEnabled
       });
       break;
     case S.HistoryItemType.APPLICATION:
       t = (0, i.jsx)(h.default, {
-        channel: s,
-        application: k.application,
-        sectionName: k.sectionName
+        channel: n,
+        application: M.application,
+        sectionName: M.sectionName
       });
       break;
     default:
       t = null
   }
+  return (0, i.jsx)("div", {
+    className: p.drawerSizingWrapper,
+    ref: E,
+    children: (0, i.jsx)("div", {
+      className: p.contentWrapper,
+      children: (0, i.jsx)(S.AppLauncherHistoryContext.Provider, {
+        value: {
+          history: v,
+          setHistory: D,
+          currentView: M,
+          pushHistory: y,
+          goBack: P
+        },
+        children: t
+      })
+    })
+  })
+}
+t.default = r.memo(function(e) {
+  let {
+    positionTargetRef: t,
+    ...n
+  } = e;
   return (0, i.jsx)(d.default, {
     section: N.AnalyticsSections.EXPRESSION_PICKER,
     children: (0, i.jsx)(E.AppReferencePositionLayer, {
       className: p.positionLayer,
-      targetRef: n,
+      targetRef: t,
       position: "top",
       align: "right",
       spacing: 8,
       autoInvert: !0,
       children: e => {
         let {
-          isPositioned: n
+          isPositioned: t
         } = e;
         return (0, i.jsx)("section", {
           className: a()(p.positionContainer),
-          ref: g,
           role: "dialog",
           "aria-label": "Application Launcher",
-          children: n ? (0, i.jsxs)("div", {
-            className: p.drawerSizingWrapper,
-            ref: L,
-            children: [(0, i.jsx)("div", {
-              className: p.resizeHandle
-            }), (0, i.jsx)("div", {
-              className: p.contentWrapper,
-              children: (0, i.jsx)(S.AppLauncherHistoryContext.Provider, {
-                value: {
-                  history: G,
-                  setHistory: w,
-                  currentView: k,
-                  pushHistory: B,
-                  goBack: x
-                },
-                children: t
-              })
-            })]
-          }) : null
+          children: t && (0, i.jsx)(O, {
+            ...n
+          })
         })
       }
     })
