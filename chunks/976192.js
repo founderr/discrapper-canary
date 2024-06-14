4faab86f4086117fb260ae511024f45aafaad984
DiscrapperManager <admin@minerpl.xyz>
2024-06-14 20:23:01 +0200

    canaryRelease: 302130
    Main scripts: 22
    Lazy Loaded Scripts: 817
    Chunks:
     * Main Files: 26650
     * Scripts: 32866
    Strings: 18070
    Routes: 742
    Experiments Count: 345

--- chunks/976192.js
+++ chunks/976192.js
@@ -1,7 +1,7 @@
 "use strict";
 l.r(t), l.d(t, {
   default: function() {
-    return g
+    return R
   }
 });
 var a = l("735250"),
@@ -9,65 +9,67 @@ var a = l("735250"),
   n = l("442837"),
   s = l("481060"),
   d = l("58540"),
-  r = l("300284"),
-  i = l("484459"),
-  o = l("314897"),
-  f = l("271383"),
-  c = l("430824"),
-  E = l("496675"),
-  M = l("594174"),
-  S = l("981631"),
-  m = l("689938"),
-  _ = l("104288");
+  r = l("40851"),
+  i = l("300284"),
+  o = l("484459"),
+  f = l("314897"),
+  c = l("271383"),
+  E = l("430824"),
+  M = l("496675"),
+  S = l("594174"),
+  m = l("981631"),
+  _ = l("689938"),
+  g = l("104288");
 
-function g(e) {
+function R(e) {
   let {
     guildId: t,
-    userId: g,
-    analyticsLocation: R,
+    userId: R,
+    analyticsLocation: C,
     analyticsLocations: A,
-    context: C,
-    icon: N
-  } = e, I = c.default.getGuild(t), O = o.default.getId(), h = (0, n.useStateFromStores)([M.default], () => M.default.getUser(g)), T = (0, n.useStateFromStores)([f.default], () => f.default.isGuestOrLurker(t, g), [t, g]);
-  (0, n.useStateFromStores)([E.default], () => E.default.getGuildVersion(t), [t]);
-  let p = u.useMemo(() => ({
-    [t]: [g]
-  }), [t, g]);
-  (0, d.useSubscribeGuildMembers)(p);
-  let v = C === S.AppContext.POPOUT,
-    U = (0, r.default)({
-      guild: I,
-      analyticsLocation: R
-    });
-  if (null == I || v) return null;
-  let b = O === g && (E.default.can(S.Permissions.CHANGE_NICKNAME, I) || E.default.can(S.Permissions.MANAGE_NICKNAMES, I)),
-    x = O === g,
-    F = E.default.canManageUser(S.Permissions.MANAGE_NICKNAMES, g, I);
-  if (!(b || F || x) || null == h || T) return null;
-  let j = I.hasFeature(S.GuildFeatures.HUB) ? m.default.Messages.HUB_EDIT_PROFILE : m.default.Messages.CHANGE_IDENTITY,
-    L = x ? j : m.default.Messages.CHANGE_NICKNAME;
+    context: N,
+    icon: O
+  } = e, h = E.default.getGuild(t), p = f.default.getId(), I = (0, n.useStateFromStores)([S.default], () => S.default.getUser(R)), T = (0, n.useStateFromStores)([c.default], () => c.default.isGuestOrLurker(t, R), [t, R]);
+  (0, n.useStateFromStores)([M.default], () => M.default.getGuildVersion(t), [t]);
+  let U = u.useMemo(() => ({
+    [t]: [R]
+  }), [t, R]);
+  (0, d.useSubscribeGuildMembers)(U);
+  let v = N === m.AppContext.POPOUT,
+    b = (0, i.default)({
+      guild: h,
+      analyticsLocation: C
+    }),
+    x = (0, r.useWindowDispatch)();
+  if (null == h || v) return null;
+  let F = p === R && (M.default.can(m.Permissions.CHANGE_NICKNAME, h) || M.default.can(m.Permissions.MANAGE_NICKNAMES, h)),
+    j = p === R,
+    L = M.default.canManageUser(m.Permissions.MANAGE_NICKNAMES, R, h);
+  if (!(F || L || j) || null == I || T) return null;
+  let P = h.hasFeature(m.GuildFeatures.HUB) ? _.default.Messages.HUB_EDIT_PROFILE : _.default.Messages.CHANGE_IDENTITY,
+    G = j ? P : _.default.Messages.CHANGE_NICKNAME;
   return (0, a.jsx)(s.MenuItem, {
     id: "change-nickname",
     label: (0, a.jsx)("div", {
-      className: _.labelWrapper,
+      className: g.labelWrapper,
       children: (0, a.jsx)("span", {
-        className: _.label,
-        children: L
+        className: g.label,
+        children: G
       })
     }),
-    icon: N,
+    icon: O,
     action: () => {
-      x ? ((0, i.default)(h.id, h.getAvatarURL(t, 80), {
+      j ? ((0, o.default)(I.id, I.getAvatarURL(t, 80), {
         guildId: t
-      }), U()) : (0, s.openModalLazy)(async () => {
+      }), b(), x.dispatch(m.ComponentActions.POPOUT_CLOSE)) : (0, s.openModalLazy)(async () => {
         let {
           default: e
         } = await Promise.all([l.e("99387"), l.e("17712")]).then(l.bind(l, "620021"));
         return l => (0, a.jsx)(e, {
           ...l,
           guildId: t,
-          user: h,
-          analyticsSource: R,
+          user: I,
+          analyticsSource: C,
           analyticsLocations: A
         })
       })
--- routes.js
+++ routes.js
@@ -2633,7 +2633,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-46e6bf4db5917d3527d3e9a0a1b58bcef5f7d01f",
+                    release: "discord_web-9804275b077b65766535f67c43c3392a00be361b",
                     beforeSend: _ => {
                         var E, e;
                         return !(null != (E = _).exception && null != E.exception.values && E.exception.values.every(_ => null == _.stacktrace || null != _.stacktrace.frames && 1 === _.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || a.some(_ => window.navigator.appVersion.toLowerCase().indexOf(_) >= 0)) && !R() && !("Aborted" === (e = _).message || "cancel captcha" === e.message) && t() ? _ : null
@@ -2651,7 +2651,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "ResizeObserver loop completed with undelivered notifications.", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), I.setTag("buildNumber", (_ = "302122", "302122")), I.setTag("builtAt", String("1718388062202"));
+                }), I.setTag("buildNumber", (_ = "302130", "302130")), I.setTag("builtAt", String("1718388741816"));
                 let E = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != E && "object" == typeof E)
                     for (let _ in E) I.setTag(_, E[_]);
