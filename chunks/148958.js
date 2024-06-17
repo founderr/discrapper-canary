64e949dd07927e7e2647be3a4a9a902696dc11aa
DiscrapperManager <admin@minerpl.xyz>
2024-06-18 00:04:37 +0200

    canaryRelease: 302713
    Main scripts: 23
    Lazy Loaded Scripts: 821
    Chunks:
     * Main Files: 6520
     * Scripts: 2125
    Strings: 18109
    Routes: 0
    Experiments Count: 0

--- chunks/148958.js
+++ chunks/148958.js
@@ -1,25 +1,34 @@
 "use strict";
 n.d(t, {
   h: function() {
-    return a
+    return u
   }
-}), n(47120);
+}), n(47120), n(653041);
 var i = n(470079),
   r = n(442837),
-  s = n(675478),
-  o = n(822245);
+  s = n(581364),
+  o = n(675478),
+  a = n(709054),
+  l = n(822245);
 
-function a(e) {
+function u(e, t) {
   i.useEffect(() => {
-    s.DZ.loadIfNecessary()
+    o.DZ.loadIfNecessary()
   }, []);
-  let t = (0, r.e7)([o.Z], () => o.Z.getApplicationFrecencyWithoutLoadingLatest());
+  let n = (0, r.e7)([l.Z], () => l.Z.getApplicationFrecencyWithoutLoadingLatest()),
+    u = i.useMemo(() => null == t ? void 0 : t.filter(t => !e.some(e => e.id === t.application.id)), [e, t]);
   return i.useMemo(() => {
-    let n = [...e];
-    return n.sort((e, n) => {
+    var t;
+    null == u || u.forEach(e => {
+      let t = a.default.extractTimestamp(e.id);
+      null == n.getEntry(e.application.id) && n.track(e.application.id, t)
+    }), n.compute();
+    let i = null !== (t = null == u ? void 0 : u.map(e => (0, s.X0)(e.application))) && void 0 !== t ? t : [],
+      r = [...e];
+    return r.push(...i), r.sort((e, t) => {
       var i, r;
-      let s = (null !== (i = t.getScore(n.id)) && void 0 !== i ? i : 0) - (null !== (r = t.getScore(e.id)) && void 0 !== r ? r : 0);
-      return 0 !== s ? s : e.name.localeCompare(n.name)
-    }), n
-  }, [e, t])
+      let s = (null !== (i = n.getScore(t.id)) && void 0 !== i ? i : 0) - (null !== (r = n.getScore(e.id)) && void 0 !== r ? r : 0);
+      return 0 !== s ? s : e.name.localeCompare(t.name)
+    }), r
+  }, [e, n, u])
 }
