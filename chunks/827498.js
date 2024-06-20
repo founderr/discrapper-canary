58c2343d5fce22b4fc85c880449d37fbcc790b67
DiscrapperManager <admin@minerpl.xyz>
2024-06-21 00:32:57 +0200

    canaryRelease: 303678
    Main scripts: 23
    Lazy Loaded Scripts: 816
    Chunks:
     * Main Files: 8921
     * Scripts: 7580
    Strings: 18130
    Routes: 256
    Experiments Count: 338

--- chunks/827498.js
+++ chunks/827498.js
@@ -7,7 +7,7 @@ n.d(t, {
   _: function() {
     return i
   }
-}), (r = i || (i = {}))[r.NONE = 0] = "NONE", r[r.TEXT = 1] = "TEXT", r[r.VOICE = 2] = "VOICE";
+}), (r = i || (i = {})).NONE = "NONE", r.TEXT = "TEXT", r.VOICE = "VOICE";
 let s = {
   SEARCH: "search",
   RECENT: "recent",
