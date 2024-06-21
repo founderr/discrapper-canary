34aab1818ce16a5f12e5945eec71be3f33289d7d
DiscrapperManager <admin@minerpl.xyz>
2024-06-21 02:12:57 +0200

    canaryRelease: 303742
    Main scripts: 23
    Lazy Loaded Scripts: 816
    Chunks:
     * Main Files: 8921
     * Scripts: 7580
    Strings: 18131
    Routes: 256
    Experiments Count: 338

--- chunks/4646.js
+++ chunks/4646.js
@@ -8,18 +8,21 @@ function i(e, t, n) {
     writable: !0
   }) : e[t] = n, e
 }
-async function r(e) {
-  let t = new s,
-    n = [];
-  for (let i of Object.keys(e)) n.push(t.loadRemoteImage(i, e[i]));
-  return await Promise.all(n), t
-}
 n.d(t, {
   k2: function() {
-    return r
+    return s
   }
-}), n(653041), n(47120);
-class s {
+}), n(47120);
+let r = new(n(259443)).Y("AssetMap");
+async function s(e) {
+  let t = new o,
+    n = Object.entries(e).map(e => {
+      let [n, i] = e;
+      return t.loadRemoteImage(n, i).catch(e => r.warn("Failed to load canvas asset", e, n, i))
+    });
+  return await Promise.all(n), t
+}
+class o {
   loadFonts() {
     return Promise.resolve()
   }
