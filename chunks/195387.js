34a9b2c3a76f3ed76b1be9e7313ba81c549d1339
DiscrapperManager <admin@minerpl.xyz>
2024-06-21 19:01:59 +0200

    canaryRelease: 303890
    Main scripts: 23
    Lazy Loaded Scripts: 816
    Chunks:
     * Main Files: 8932
     * Scripts: 7580
    Strings: 18170
    Routes: 260
    Experiments Count: 340

--- chunks/195387.js
+++ chunks/195387.js
@@ -1,7 +1,7 @@
 "use strict";
 n.d(t, {
   Z: function() {
-    return h
+    return S
   }
 });
 var i = n(735250);
@@ -11,36 +11,37 @@ var r = n(442837),
   o = n(906732),
   a = n(189432),
   l = n(189357),
-  u = n(314897),
-  _ = n(944486),
-  c = n(785717),
-  d = n(993409),
-  E = n(176505),
-  I = n(50493),
-  T = n(689938);
+  u = n(434404),
+  _ = n(314897),
+  c = n(944486),
+  d = n(785717),
+  E = n(993409),
+  I = n(176505),
+  T = n(50493),
+  h = n(689938);
 
-function h(e) {
+function S(e) {
   let {
     user: t,
     guildId: n,
-    channelId: h,
-    onClose: S
+    channelId: S,
+    onClose: f
   } = e, {
-    trackUserProfileAction: f
-  } = (0, c.KZ)(), {
-    newestAnalyticsLocation: N
-  } = (0, o.ZP)(), A = (0, r.e7)([u.default], () => u.default.getId() === (null == t ? void 0 : t.id)), m = (0, l.ms)(null != n ? n : null, !0), O = (0, r.e7)([_.Z], () => null != h ? h : _.Z.getChannelId(n, !0), [h, n]);
-  return null == n || !m || A ? null : (0, i.jsx)(d.Z, {
+    trackUserProfileAction: N
+  } = (0, d.KZ)(), {
+    newestAnalyticsLocation: A
+  } = (0, o.ZP)(), m = (0, r.e7)([_.default], () => _.default.getId() === (null == t ? void 0 : t.id)), O = (0, l.ms)(null != n ? n : null, !0), R = (0, r.e7)([c.Z], () => null != S ? S : c.Z.getChannelId(n, !0), [S, n]);
+  return null == n || !O || m ? null : (0, i.jsx)(E.Z, {
     variant: "banner",
     icon: s.ModerationIcon,
-    text: T.Z.Messages.GUILD_MEMBER_MOD_VIEW_TITLE,
+    text: h.Z.Messages.GUILD_MEMBER_MOD_VIEW_TITLE,
     onClick: () => {
-      f({
+      u.Z.close(), N({
         action: "PRESS_MOD_VIEW"
-      }), (0, a.r)(n, t.id, null != O ? O : E.oC.MEMBER_SAFETY, {
-        modViewPanel: I.k.INFO,
-        sourceLocation: N
-      }), null == S || S()
+      }), (0, a.r)(n, t.id, null != R ? R : I.oC.MEMBER_SAFETY, {
+        modViewPanel: T.k.INFO,
+        sourceLocation: A
+      }), null == f || f()
     }
   })
 }
