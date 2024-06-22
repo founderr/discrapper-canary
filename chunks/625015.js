046c9fe5b2f79c84ded5ac01c0533c70b9d55367
DiscrapperManager <admin@minerpl.xyz>
2024-06-22 02:13:01 +0200

    canaryRelease: 304170
    Main scripts: 23
    Lazy Loaded Scripts: 815
    Chunks:
     * Main Files: 8931
     * Scripts: 7584
    Strings: 18174
    Routes: 260
    Experiments Count: 341

--- chunks/625015.js
+++ chunks/625015.js
@@ -33,6 +33,7 @@ function I(e) {
         "aria-label": n
       }), (0, l.jsx)(r.Text, {
         variant: "text-sm/medium",
+        className: _.contentText,
         children: C.Z.Messages.AFFINITIES_MARKETING_HEADER_V2_1.format({
           username: n
         })
@@ -68,16 +69,90 @@ function I(e) {
 
 function x(e) {
   let {
+    avatarSrc: t,
+    username: n
+  } = e, [i, c] = (0, o.U)([s.z.NITRO_ON_PROFILE_SIDE_PANEL], g.R.SIDEBAR, !0);
+  return i !== s.z.NITRO_ON_PROFILE_SIDE_PANEL ? null : (0, l.jsx)(p.Z.Overlay, {
+    className: _.overlay2,
+    children: (0, l.jsxs)("div", {
+      className: _.contentGroup,
+      children: [(0, l.jsx)(a.qE, {
+        src: t,
+        size: r.AvatarSizes.SIZE_32,
+        status: r.StatusTypes.UNKNOWN,
+        "aria-label": n
+      }), (0, l.jsx)(r.Text, {
+        variant: "text-sm/medium",
+        className: _.contentText,
+        children: C.Z.Messages.AFFINITIES_MARKETING_HEADER_V2_1_WITH_LINK.format({
+          username: n,
+          onClickLearnMore: () => {
+            (0, d.uL)(E.Z5c.APPLICATION_STORE)
+          }
+        })
+      }), (0, l.jsx)(r.Clickable, {
+        onClick: () => c(g.L.USER_DISMISS),
+        "aria-label": C.Z.Messages.DISMISS,
+        className: _.closeIconWrapper,
+        children: (0, l.jsx)(r.CloseSmallIcon, {
+          size: "md",
+          color: "currentColor",
+          className: _.closeIcon
+        })
+      })]
+    })
+  })
+}
+
+function T(e) {
+  let {
+    avatarSrc: t,
+    username: n
+  } = e;
+  return (0, l.jsx)(p.Z.Overlay, {
+    className: _.overlay2,
+    children: (0, l.jsxs)("div", {
+      className: _.contentGroup,
+      children: [(0, l.jsx)(a.qE, {
+        src: t,
+        size: r.AvatarSizes.SIZE_32,
+        status: r.StatusTypes.UNKNOWN,
+        "aria-label": n
+      }), (0, l.jsx)(r.Text, {
+        variant: "text-sm/medium",
+        className: _.contentText,
+        children: C.Z.Messages.AFFINITIES_MARKETING_HEADER_V2_1_WITH_LINK.format({
+          username: n,
+          onClickLearnMore: () => {
+            (0, d.uL)(E.Z5c.APPLICATION_STORE)
+          }
+        })
+      })]
+    })
+  })
+}
+
+function N(e) {
+  let {
     variant: t,
     avatarSrc: n,
     username: i
   } = e;
-  return 1 === t ? (0, l.jsx)(I, {
+  if (1 === t) return (0, l.jsx)(I, {
     avatarSrc: n,
     username: i
-  }) : null
+  });
+  if (2 === t) return (0, l.jsx)(x, {
+    avatarSrc: n,
+    username: i
+  });
+  if (3 === t) return (0, l.jsx)(T, {
+    avatarSrc: n,
+    username: i
+  });
+  return null
 }
-let T = i.memo(function(e) {
+let Z = i.memo(function(e) {
   let {
     user: t,
     displayProfile: n
@@ -95,10 +170,10 @@ let T = i.memo(function(e) {
     disable: s,
     autoTrackExposure: !s
   });
-  return a ? (0, l.jsx)(x, {
+  return a ? (0, l.jsx)(N, {
     variant: o,
     avatarSrc: i,
     username: t.username
   }) : null
 });
-t.Z = T
+t.Z = Z
--- chunks/780104.js
+++ chunks/780104.js
@@ -1,6 +1,8 @@
 e.exports = {
   overlay: "overlay_a465ac",
+  overlay2: "overlay2_a465ac",
   contentGroup: "contentGroup_a465ac",
+  contentText: "contentText_a465ac",
   buttonGroup: "buttonGroup_a465ac",
   getNitroButton: "getNitroButton_a465ac",
   learnMoreButton: "learnMoreButton_a465ac",
