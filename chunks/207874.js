db9e4b16da4721d3597d501aed3cb093c52119b8
DiscrapperManager <admin@minerpl.xyz>
2024-06-25 01:26:27 +0200

    canaryRelease: 304523
    Main scripts: 22
    Lazy Loaded Scripts: 816
    Chunks:
     * Main Files: 8933
     * Scripts: 7605
    Strings: 18194
    Routes: 260
    Experiments Count: 343

--- chunks/207874.js
+++ chunks/207874.js
@@ -16,20 +16,12 @@ function o() {
     }, []),
     t = i.useCallback(() => {
       (0, l.QH)(!1), (0, l.yN)("")
-    }, []),
-    o = i.useRef(null);
-  return i.useEffect(() => {
-    let e = () => {
-      let e = o.current;
-      null == e || e.focus()
-    };
-    return window.addEventListener("keydown", e), () => window.removeEventListener("keydown", e)
-  }, []), (0, n.jsx)("div", {
+    }, []);
+  return (0, n.jsx)("div", {
     style: {
       marginBottom: "8px"
     },
     children: (0, n.jsx)(a.ZP, {
-      ref: o,
       size: a.ZP.Sizes.MEDIUM,
       query: e,
       onClear: t,
