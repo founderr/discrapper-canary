cc998ed7ee29cad3b1262617d9654c7b2f40d5e9
DiscrapperManager <admin@minerpl.xyz>
2024-06-26 00:24:30 +0200

    canaryRelease: 305010
    Main scripts: 22
    Lazy Loaded Scripts: 809
    Chunks:
     * Main Files: 8931
     * Scripts: 7568
    Strings: 18223
    Routes: 260
    Experiments Count: 343

---
+++ chunks/332390.js
@@ -0,0 +1,54 @@
+"use strict";
+n.d(t, {
+  l: function() {
+    return r
+  }
+});
+let i = (0, n(818083).B)({
+    kind: "user",
+    id: "2024-06_profile_engagement_Ctas",
+    label: "Simplified Profile Primary Engagement Ctas",
+    defaultConfig: {
+      messageCtaEnabled: !1,
+      activeInviteToCallCtaEnabled: !1,
+      persistentCallCtaEnabled: !1
+    },
+    treatments: [{
+      id: 1,
+      label: "Message Cta on all profiles",
+      config: {
+        messageCtaEnabled: !0,
+        activeInviteToCallCtaEnabled: !1,
+        persistentCallCtaEnabled: !1
+      }
+    }, {
+      id: 2,
+      label: "Message Cta and invite to call Cta when active in call",
+      config: {
+        messageCtaEnabled: !0,
+        activeInviteToCallCtaEnabled: !0,
+        persistentCallCtaEnabled: !1
+      }
+    }, {
+      id: 3,
+      label: "Message Cta and persistent call Cta",
+      config: {
+        messageCtaEnabled: !0,
+        activeInviteToCallCtaEnabled: !1,
+        persistentCallCtaEnabled: !0
+      }
+    }]
+  }),
+  r = e => {
+    let {
+      location: t,
+      autoTrackExposure: n = !0,
+      trackExposureOptions: r = {}
+    } = e;
+    return i.useExperiment({
+      location: t
+    }, {
+      autoTrackExposure: n,
+      trackExposureOptions: r
+    })
+  }
--- chunks/901952.js
+++ chunks/901952.js
@@ -1,94 +1,116 @@
 "use strict";
 n.d(t, {
   Z: function() {
-    return O
+    return L
   }
 }), n(411104), n(47120);
 var i = n(735250),
   r = n(470079),
-  s = n(493683),
-  o = n(904245),
-  a = n(906732),
-  l = n(541716),
-  u = n(752305),
-  _ = n(893718),
-  c = n(957730),
-  d = n(131704),
-  E = n(592125),
-  I = n(5192),
-  T = n(838440),
-  h = n(785717),
-  S = n(981631),
-  f = n(689938),
-  N = n(682583);
-let A = (0, d.kt)({
+  s = n(120356),
+  o = n.n(s),
+  a = n(481060),
+  l = n(493683),
+  u = n(904245),
+  _ = n(906732),
+  c = n(541716),
+  d = n(752305),
+  E = n(893718),
+  I = n(957730),
+  T = n(131704),
+  h = n(592125),
+  S = n(5192),
+  f = n(838440),
+  N = n(332390),
+  A = n(785717),
+  m = n(993409),
+  O = n(981631),
+  R = n(689938),
+  p = n(717633),
+  g = n(682583);
+let C = (0, T.kt)({
     id: "1",
-    type: S.d4z.DM
+    type: O.d4z.DM
   }),
-  m = async e => {
+  v = async e => {
     let {
       userId: t,
       content: n,
       location: i
     } = e, {
       valid: r,
-      failureReason: a
-    } = await (0, T.v)({
-      type: l.I.NORMAL,
+      failureReason: s
+    } = await (0, f.v)({
+      type: c.I.NORMAL,
       content: n,
       channel: null
     });
-    if (!r) throw Error(a);
-    let u = await s.Z.openPrivateChannel(t, !1, !1, i),
-      _ = E.Z.getChannel(u);
-    if (null == _) throw Error("Failed to open private channel");
-    let d = c.ZP.parse(_, n);
-    return o.Z.sendMessage(_.id, d)
+    if (!r) throw Error(s);
+    let o = await l.Z.openPrivateChannel(t, !1, !1, i),
+      a = h.Z.getChannel(o);
+    if (null == a) throw Error("Failed to open private channel");
+    let _ = I.ZP.parse(a, n);
+    return u.Z.sendMessage(a.id, _)
   };
 
-function O(e) {
+function L(e) {
   let {
     user: t,
     guildId: n,
     channelId: s,
-    onClose: o
+    onClose: u
   } = e, {
-    newestAnalyticsLocation: c
-  } = (0, a.ZP)(), {
-    trackUserProfileAction: d
-  } = (0, h.KZ)(), [E, T] = r.useState(""), [S, O] = r.useState((0, u.JM)(E)), R = r.useRef(!1);
-  return (0, i.jsx)(_.Z, {
-    innerClassName: N.inner,
-    editorClassName: N.editor,
-    type: l.I.USER_PROFILE,
-    placeholder: f.Z.Messages.QUICK_DM_USER.format({
-      name: I.ZP.getName(n, s, t)
-    }),
-    channel: A,
-    textValue: E,
-    richValue: S,
-    onChange: (e, t, n) => {
-      if (t !== E) T(t), O(n)
+    newestAnalyticsLocation: I
+  } = (0, _.ZP)(), {
+    trackUserProfileAction: T
+  } = (0, A.KZ)(), {
+    messageCtaEnabled: h
+  } = (0, N.l)({
+    location: "BiteSizeProfileDirectMessageSection"
+  }), [f, O] = r.useState(""), [L, D] = r.useState((0, d.JM)(f)), M = r.useRef(!1);
+  return h ? (0, i.jsx)(m.Z, {
+    fullWidth: !0,
+    variant: "text",
+    look: a.Button.Looks.BLANK,
+    color: o()(a.Button.Colors.BRAND, p.color),
+    onClick: () => {
+      T({
+        action: "SEND_MESSAGE"
+      }), l.Z.openPrivateChannel(t.id, !1, !1, I), null == u || u()
     },
-    focused: R.current,
+    icon: a.ChatIcon,
+    text: R.Z.Messages.USER_PROFILE_MESSAGE
+  }) : (0, i.jsx)(E.Z, {
+    innerClassName: g.inner,
+    editorClassName: g.editor,
+    type: c.I.USER_PROFILE,
+    placeholder: R.Z.Messages.QUICK_DM_USER.format({
+      name: S.ZP.getName(n, s, t)
+    }),
+    channel: C,
+    textValue: f,
+    richValue: L,
+    onChange: (e, t, n) => {
+      if (t !== f) O(t), D(n)
+    },
+    focused: M.current,
     onFocus: () => {
-      R.current = !0
+      M.current = !0
     },
     onBlur: () => {
-      R.current = !1
+      M.current = !1
     },
     onSubmit: async e => {
       let {
         value: n
       } = e;
       try {
-        return d({
+        return T({
           action: "SEND_DIRECT_MESSAGE"
-        }), await m({
+        }), await v({
           userId: t.id,
           content: n.trim(),
-          location: c
-        }), null == o || o(), {
+          location: I
+        }), null == u || u(), {
           shouldClear: !0,
           shouldRefocus: !1
         }
