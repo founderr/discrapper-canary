b52194ee7a3325dfd4473f8451f00dd076bf566e
DiscrapperManager <admin@minerpl.xyz>
2024-06-26 19:55:16 +0200

    canaryRelease: 305230
    Main scripts: 20
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 7034
     * Scripts: 10484
    Strings: 18235
    Routes: 260
    Experiments Count: 348

--- chunks/331042.js
+++ chunks/331042.js
@@ -1,6 +1,6 @@
 n.r(t), n.d(t, {
   CollectiblesCollectedModalInner: function() {
-    return M
+    return P
   }
 }), n(47120), n(627341);
 var r = n(735250),
@@ -17,113 +17,119 @@ var r = n(735250),
   g = n(100527),
   v = n(906732),
   C = n(1585),
-  h = n(125988),
-  E = n(300284),
+  E = n(125988),
+  h = n(300284),
   b = n(876917),
   m = n(642619),
-  y = n(594174),
-  x = n(884697),
-  A = n(318028),
-  T = n(635552),
-  L = n(328456),
+  T = n(594174),
+  y = n(884697),
+  A = n(617452),
+  x = n(318028),
+  L = n(635552),
+  I = n(328456),
   Z = n(624377),
-  I = n(530618),
-  N = n(141011),
-  _ = n(372654),
-  S = n(216541),
+  N = n(530618),
+  _ = n(141011),
+  S = n(372654),
+  O = n(216541),
   R = n(302800),
-  O = n(215023),
+  M = n(215023),
   k = n(689938),
-  j = n(432015),
-  B = n(660097);
-let M = e => {
+  B = n(432015),
+  j = n(660097);
+let P = e => {
   var t, n;
   let {
     product: o,
     onClose: l,
     confettiTarget: R,
-    confettiCanvas: M,
+    confettiCanvas: P,
     hideConfetti: w = !1,
-    analyticsLocations: P
+    analyticsLocations: D
   } = e, {
-    confettiColors: D
-  } = (0, Z.Z)(o.styles), F = (0, d.e7)([y.default], () => {
-    let e = y.default.getCurrentUser();
+    confettiColors: F
+  } = (0, Z.Z)(o.styles), H = (0, d.e7)([T.default], () => {
+    let e = T.default.getCurrentUser();
     return i()(null != e, "User cannot be undefined"), e
-  }), H = (0, d.e7)([p.Z], () => p.Z.useReducedMotion), [z] = o.items, {
-    avatarDecorationSrc: U
-  } = (0, h.Z)({
-    user: F,
-    avatarDecorationOverride: (null == z ? void 0 : z.type) === c.Z.AVATAR_DECORATION ? z : null,
+  }), z = (0, d.e7)([p.Z], () => p.Z.useReducedMotion), U = (0, A.R)("CollectiblesCollectedModal"), [V] = o.items, {
+    avatarDecorationSrc: W
+  } = (0, E.Z)({
+    user: H,
+    avatarDecorationOverride: (null == V ? void 0 : V.type) === c.Z.AVATAR_DECORATION ? V : null,
     size: (0, C.y9)(u.AvatarSizes.SIZE_120)
-  }), V = a.useRef(null), {
-    analyticsLocations: W
-  } = (0, v.ZP)([...P, g.Z.COLLECTIBLES_COLLECTED_MODAL]), G = (0, E.Z)({
-    analyticsLocations: W
-  }), K = a.useCallback(() => {
-    if (l(), (0, f.xf)(), G(), o.type === c.Z.AVATAR_DECORATION) {
-      i()(z.type === c.Z.AVATAR_DECORATION, "product type and item type are the same"), (0, C.ps)({
-        initialSelectedDecoration: z,
-        analyticsLocations: W
+  }), G = a.useRef(null), {
+    analyticsLocations: K
+  } = (0, v.ZP)([...D, g.Z.COLLECTIBLES_COLLECTED_MODAL]), Y = (0, h.Z)({
+    analyticsLocations: K
+  }), Q = a.useCallback(() => {
+    if (l(), (0, f.xf)(), Y(), o.type === c.Z.AVATAR_DECORATION) {
+      i()(V.type === c.Z.AVATAR_DECORATION, "product type and item type are the same"), (0, C.ps)({
+        initialSelectedDecoration: V,
+        analyticsLocations: K
       });
       return
     }
     o.type === c.Z.PROFILE_EFFECT && (0, m.H)({
-      initialSelectedEffectId: z.id,
-      analyticsLocations: W
+      initialSelectedEffectId: V.id,
+      analyticsLocations: K
     })
-  }, [l, G, o.type, z, W]), {
-    handleUseNow: Y
-  } = (0, T.W)({
+  }, [l, Y, o.type, V, K]), {
+    handleUseNow: q
+  } = (0, L.W)({
     product: o,
-    onSuccess: l,
+    onSuccess: () => {
+      (0, u.showToast)((0, u.createToast)(k.Z.Messages.COLLECTIBLES_PROFILE_UPDATE_NOTIFICATION, u.ToastType.MESSAGE, {
+        duration: 6e3,
+        position: u.ToastPosition.TOP
+      })), l()
+    },
     onError: l
   }), {
-    firstAvatarDecoration: Q,
-    firstProfileEffect: q
-  } = (0, L.R)(o), J = null != Q && null != q, {
-    category: X,
-    isFetching: $
-  } = (0, A.l)(o.skuId), ee = null !== (n = null == X ? void 0 : X.successModalBg) && void 0 !== n ? n : o.banner;
+    firstAvatarDecoration: J,
+    firstProfileEffect: X
+  } = (0, I.R)(o), $ = U || null != J && null != X, {
+    category: ee,
+    isFetching: et
+  } = (0, x.l)(o.skuId), en = null !== (n = null == ee ? void 0 : ee.successModalBg) && void 0 !== n ? n : o.banner;
   return (0, r.jsxs)(v.Gt, {
-    value: W,
+    value: K,
     children: [(0, r.jsxs)("div", {
-      className: j.modalInner,
-      ref: V,
+      className: B.modalInner,
+      ref: G,
       children: [(0, r.jsxs)("div", {
-        className: j.bannerContainer,
-        children: [(0, r.jsx)(N.Z, {
-          asset: $ ? void 0 : ee,
-          addGradient: null === (t = O.Ve[o.categorySkuId]) || void 0 === t ? void 0 : t.addGradient,
-          size: (0, _.ML)(880),
-          className: j.decorationBanner,
+        className: B.bannerContainer,
+        children: [(0, r.jsx)(_.Z, {
+          asset: et ? void 0 : en,
+          addGradient: null === (t = M.Ve[o.categorySkuId]) || void 0 === t ? void 0 : t.addGradient,
+          size: (0, S.ML)(880),
+          className: B.decorationBanner,
           blur: !0
         }), (0, s.EQ)(o.type).with(c.Z.PROFILE_EFFECT, () => (0, r.jsx)("div", {
-          className: j.profileEffectShopPreview,
+          className: B.profileEffectShopPreview,
           children: (0, r.jsx)(b.Z, {
             forCollectedModal: !0,
-            profileEffectId: z.id
+            profileEffectId: V.id
           })
-        })).with(c.Z.AVATAR_DECORATION, () => (i()(z.type === c.Z.AVATAR_DECORATION, "ts-match already checked the type"), (0, r.jsx)(u.Avatar, {
+        })).with(c.Z.AVATAR_DECORATION, () => (i()(V.type === c.Z.AVATAR_DECORATION, "ts-match already checked the type"), (0, r.jsx)(u.Avatar, {
           size: u.AvatarSizes.SIZE_120,
-          src: B,
-          avatarDecoration: U,
-          className: j.avatarDecoration,
+          src: j,
+          avatarDecoration: W,
+          className: B.avatarDecoration,
           "aria-label": o.name
         }))).with(c.Z.BUNDLE, () => (0, r.jsx)("div", {
-          className: j.bundlePreview,
-          children: (0, r.jsx)(S.d, {
+          className: B.bundlePreview,
+          children: (0, r.jsx)(O.d, {
             product: o,
-            user: F,
+            user: H,
             isPurchased: !1,
             isHighlighted: !0,
-            profileEffectClassName: j.bundleProfileEffectCard
+            profileEffectClassName: B.bundleProfileEffectCard
           })
         })).otherwise(() => null)]
       }), (0, r.jsxs)("div", {
-        className: j.collectedInfoContainer,
+        className: B.collectedInfoContainer,
         children: [(0, r.jsxs)("div", {
-          className: j.collectedTextContainer,
+          className: B.collectedTextContainer,
           children: [(0, r.jsx)(u.Heading, {
             variant: "heading-lg/bold",
             children: k.Z.Messages.COLLECTIBLES_YOU_COLLECTED.format({
@@ -133,30 +139,30 @@ let M = e => {
             variant: "text-sm/normal",
             children: (0, s.EQ)(null == o ? void 0 : o.type).with(c.Z.BUNDLE, () => k.Z.Messages.COLLECTIBLES_PERMANENT_COLLECTION_BUNDLE).with(c.Z.PROFILE_EFFECT, () => k.Z.Messages.COLLECTIBLES_PROFILE_EFFECT_PERMANENT_COLLECTION).otherwise(() => k.Z.Messages.COLLECTIBLES_PERMANENT_COLLECTION)
           })]
-        }), (0, x.x6)(o) ? (0, r.jsxs)("div", {
-          className: j.buttons,
+        }), (0, y.x6)(o) || U ? (0, r.jsxs)("div", {
+          className: B.buttons,
           children: [(0, r.jsx)(u.Button, {
-            color: J ? u.ButtonColors.PRIMARY : u.ButtonColors.BRAND,
-            onClick: K,
+            color: $ ? u.ButtonColors.PRIMARY : u.ButtonColors.BRAND,
+            onClick: Q,
             children: k.Z.Messages.USER_SETTINGS_EDIT_USER_PROFILE
-          }), J ? (0, r.jsx)(u.Button, {
-            onClick: Y,
+          }), $ ? (0, r.jsx)(u.Button, {
+            onClick: q,
             children: k.Z.Messages.COLLECTIBLES_USE_NOW
           }) : null]
         }) : (0, r.jsx)(u.Button, {
-          onClick: K,
+          onClick: Q,
           children: k.Z.Messages.COLLECTIBLES_USE_NOW
         })]
       }), (0, r.jsx)(u.ModalCloseButton, {
         onClick: l,
-        className: j.modalCloseButton,
+        className: B.modalCloseButton,
         withCircleBackground: !0
       })]
-    }), !w && !H && (0, r.jsx)(I.Z, {
-      confettiTarget: null != R ? R : V.current,
-      confettiCanvas: M,
-      sprites: (0, _.vK)(o.categorySkuId),
-      colors: null == D ? void 0 : D.map(e => e.toHexString())
+    }), !w && !z && (0, r.jsx)(N.Z, {
+      confettiTarget: null != R ? R : G.current,
+      confettiCanvas: P,
+      sprites: (0, S.vK)(o.categorySkuId),
+      colors: null == F ? void 0 : F.map(e => e.toHexString())
     })]
   })
 };
@@ -170,22 +176,22 @@ t.default = e => {
   return (0, r.jsxs)(r.Fragment, {
     children: [(0, r.jsx)(o.O_, {
       ref: d,
-      className: j.confettiCanvas,
+      className: B.confettiCanvas,
       environment: s.current
     }), (0, r.jsxs)(u.ModalRoot, {
       hideShadow: !0,
       transitionState: t,
       size: u.ModalSize.DYNAMIC,
-      className: j.modalRoot,
+      className: B.modalRoot,
       children: [null != f && (0, r.jsx)("img", {
         src: f.confettiAssetSrc,
-        className: j.customConfetti,
+        className: B.customConfetti,
         style: f.style,
         alt: "",
         "aria-hidden": "true"
       }), (0, r.jsx)(u.ModalContent, {
-        className: j.modalContent,
-        children: (0, r.jsx)(M, {
+        className: B.modalContent,
+        children: (0, r.jsx)(P, {
           product: n,
           onClose: l,
           confettiCanvas: c,
--- chunks/617452.js
+++ chunks/617452.js
@@ -1,5 +1,8 @@
 "use strict";
 n.d(t, {
+  R: function() {
+    return s
+  },
   e: function() {
     return r
   }
@@ -9,16 +12,28 @@ let i = (0, n(818083).B)({
     id: "2024-05_collectibles_shop_bundle",
     label: "Collectibles Shop Bundle",
     defaultConfig: {
-      enabled: !1
+      enabled: !1,
+      useNowForAllCollectedModals: !1
     },
     treatments: [{
       id: 1,
       label: "Shop with bundle",
       config: {
-        enabled: !0
+        enabled: !0,
+        useNowForAllCollectedModals: !1
+      }
+    }, {
+      id: 2,
+      label: "Bundle + Use Now Non-Bundle",
+      config: {
+        enabled: !0,
+        useNowForAllCollectedModals: !0
       }
     }]
   }),
   r = e => i.useExperiment({
+    location: e
+  }).enabled,
+  s = e => i.useExperiment({
     location: e
   }).enabled
