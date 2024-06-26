3087e5abc90ed304e109b80022980e717018d4c0
DiscrapperManager <admin@minerpl.xyz>
2024-06-26 22:42:29 +0200

    canaryRelease: 305368
    Main scripts: 20
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 7037
     * Scripts: 10491
    Strings: 18245
    Routes: 260
    Experiments Count: 350

--- chunks/207874.js
+++ chunks/207874.js
@@ -1,38 +1,48 @@
 n.d(t, {
   Z: function() {
-    return a
+    return c
   }
 });
 var i = n(735250),
   s = n(470079),
   l = n(461745),
-  r = n(128277),
-  o = n(689938);
+  r = n(5967),
+  o = n(128277),
+  a = n(689938);
 
-function a(e) {
+function c(e) {
   let {
     style: t
-  } = e, n = (0, r.Z7)(), a = s.useCallback((e, t) => {
-    t.stopPropagation(), t.preventDefault(), (0, r.QH)(e)
-  }, []), c = s.useCallback(() => {
-    (0, r.QH)(!1), (0, r.yN)("")
-  }, []);
-  return (0, i.jsx)("div", {
+  } = e, n = (0, o.Z7)(), c = s.useCallback((e, t) => {
+    t.stopPropagation(), t.preventDefault(), (0, o.QH)(e)
+  }, []), u = s.useCallback(() => {
+    (0, o.QH)(!1), (0, o.yN)("")
+  }, []), d = s.useRef(null);
+  return s.useEffect(() => {
+    let e = e => {
+      var t;
+      let n = d.current,
+        i = null === (t = (0, r.uB)(e)) || void 0 === t ? void 0 : t.activeElement;
+      !(i !== d.current && (0, r.VG)(i)) && (null == n || n.focus())
+    };
+    return window.addEventListener("keydown", e), () => window.removeEventListener("keydown", e)
+  }, []), (0, i.jsx)("div", {
     style: {
       marginBottom: "8px"
     },
     children: (0, i.jsx)(l.ZP, {
+      ref: d,
       size: l.ZP.Sizes.MEDIUM,
       query: n,
-      onClear: c,
-      onQueryChange: r.yN,
-      placeholder: o.Z.Messages.SEARCH,
+      onClear: u,
+      onQueryChange: o.yN,
+      placeholder: a.Z.Messages.SEARCH,
       className: t,
       inputProps: {
-        "aria-label": o.Z.Messages.USER_SETTINGS_SEARCH_BAR,
+        "aria-label": a.Z.Messages.USER_SETTINGS_SEARCH_BAR,
         "aria-expanded": !0,
-        onFocus: e => a(!0, e),
-        onBlur: e => a(!1, e)
+        onFocus: e => c(!0, e),
+        onBlur: e => c(!1, e)
       }
     })
   })
