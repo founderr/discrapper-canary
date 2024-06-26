27dddf112c4be63bbe56c36efc2e440e11971a44
DiscrapperManager <admin@minerpl.xyz>
2024-06-26 04:55:52 +0200

    canaryRelease: 305119
    Main scripts: 20
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 7033
     * Scripts: 10485
    Strings: 18218
    Routes: 260
    Experiments Count: 343

--- chunks/248702.js
+++ chunks/248702.js
@@ -3,5 +3,6 @@ e.exports = {
   footer: "footer_c5a065",
   authorize: "authorize_c5a065",
   inApp: "inApp_c5a065",
-  content: "content_c5a065"
+  content: "content_c5a065",
+  intObserver: "intObserver_c5a065"
 }
