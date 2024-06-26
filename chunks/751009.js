8e56b52ac8b57dea78eba1b81f7ead15760be67d
DiscrapperManager <admin@minerpl.xyz>
2024-06-26 20:23:28 +0200

    canaryRelease: 305257
    Main scripts: 20
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 7034
     * Scripts: 10484
    Strings: 18234
    Routes: 260
    Experiments Count: 348

--- chunks/751009.js
+++ chunks/751009.js
@@ -9,29 +9,13 @@ let i = (0, n(818083).B)({
     id: "2024-06_enhanced_custom_status",
     label: "Enhanced Custom Status Experiment",
     defaultConfig: {
-      modifyStatusEnabled: !1,
-      permanentStatusEnabled: !1
+      modifyStatusEnabled: !1
     },
     treatments: [{
       id: 1,
       label: "Modifiable custom status",
       config: {
-        modifyStatusEnabled: !0,
-        permanentStatusEnabled: !1
-      }
-    }, {
-      id: 2,
-      label: "Permanent custom status",
-      config: {
-        modifyStatusEnabled: !1,
-        permanentStatusEnabled: !0
-      }
-    }, {
-      id: 3,
-      label: "Modifiable and permament custom status",
-      config: {
-        modifyStatusEnabled: !0,
-        permanentStatusEnabled: !0
+        modifyStatusEnabled: !0
       }
     }]
   }),
--- routes.js
+++ routes.js
@@ -2828,7 +2828,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-834e663e1d4ae15707a0c733a54a760c17224f9c",
+                    release: "discord_web-e3f403892a15174546572cdb201c9551d699180f",
                     beforeSend: _ => {
                         var E, I;
                         return !(null != (E = _).exception && null != E.exception.values && E.exception.values.every(_ => null == _.stacktrace || null != _.stacktrace.frames && 1 === _.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || A.some(_ => window.navigator.appVersion.toLowerCase().indexOf(_) >= 0)) && !T() && !("Aborted" === (I = _).message || "cancel captcha" === I.message) && r() ? _ : null
@@ -2846,8 +2846,8 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "ResizeObserver loop completed with undelivered notifications.", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), R.YA("buildNumber", (_ = "305251", "305251"));
-                R.YA("builtAt", String("1719425282100"));
+                }), R.YA("buildNumber", (_ = "305257", "305257"));
+                R.YA("builtAt", String("1719425709274"));
                 let E = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != E && "object" == typeof E)
                     for (let _ in E) R.YA(_, E[_]);
