2f10f672b70137d2f666c4151aab0f5ed5c479cb
DiscrapperManager <admin@minerpl.xyz>
2024-06-27 23:27:03 +0200

    canaryRelease: 305846
    Main scripts: 20
    Lazy Loaded Scripts: 834
    Chunks:
     * Main Files: 6950
     * Scripts: 10622
    Strings: 18251
    Routes: 260
    Experiments Count: 348

--- chunks/376406.js
+++ chunks/376406.js
@@ -2,5 +2,6 @@
 e.exports = {
   viewFullBio: "viewFullBio_fc5c48",
   viewFullBioColor: "viewFullBioColor_fc5c48",
-  descriptionClamp: "descriptionClamp_fc5c48"
+  descriptionClamp: "descriptionClamp_fc5c48",
+  maxBioHeight: "maxBioHeight_fc5c48"
 }
--- chunks/67152.js
+++ chunks/67152.js
@@ -2,61 +2,64 @@
 n(47120);
 var i = n(735250),
   r = n(470079),
-  s = n(481060),
-  o = n(906732),
-  a = n(785717),
-  l = n(588822),
-  u = n(171368),
-  _ = n(981631),
-  c = n(689938),
-  d = n(376406);
+  s = n(120356),
+  o = n.n(s),
+  a = n(481060),
+  l = n(906732),
+  u = n(785717),
+  _ = n(588822),
+  c = n(171368),
+  d = n(981631),
+  E = n(689938),
+  I = n(376406);
+let T = 57.75;
 t.Z = r.memo(function(e) {
   let {
     user: t,
     onClose: n,
-    bio: E,
-    hidePersonalInformation: I
+    bio: s,
+    hidePersonalInformation: h
   } = e, {
-    guildId: T,
-    channelId: h,
-    messageId: f,
-    roleId: S
-  } = (0, a.KZ)(), {
-    analyticsLocations: N
-  } = (0, o.ZP)(), [A, m] = r.useState(!1);
-  return I || null == E || "" === E ? null : (0, i.jsxs)("div", {
+    guildId: f,
+    channelId: S,
+    messageId: A,
+    roleId: N
+  } = (0, u.KZ)(), {
+    analyticsLocations: m
+  } = (0, l.ZP)(), [O, R] = r.useState(!1), [p, g] = r.useState(!1);
+  return h || null == s || "" === s ? null : (0, i.jsxs)("div", {
     children: [(0, i.jsx)("div", {
       ref: e => {
-        null != e && m(e.scrollHeight - e.clientHeight > 1)
+        null != e && (R(!p && e.scrollHeight - e.clientHeight > 1), e.getBoundingClientRect().height > T && g(!0))
       },
-      className: d.descriptionClamp,
-      children: (0, i.jsx)(l.Z, {
-        userBio: E,
+      className: o()(I.descriptionClamp, p && I.maxBioHeight),
+      children: (0, i.jsx)(_.Z, {
+        userBio: s,
         setLineClamp: !1,
         textColor: "header-primary"
       })
-    }), A && (0, i.jsx)(s.Button, {
-      look: s.Button.Looks.BLANK,
-      size: s.Button.Sizes.NONE,
-      className: d.viewFullBio,
-      color: d.viewFullBioColor,
+    }), (O || p) && (0, i.jsx)(a.Button, {
+      look: a.Button.Looks.BLANK,
+      size: a.Button.Sizes.NONE,
+      className: I.viewFullBio,
+      color: I.viewFullBioColor,
       onClick: () => {
-        null == n || n(), (0, u.openUserProfileModal)({
+        null == n || n(), (0, c.openUserProfileModal)({
           userId: t.id,
-          guildId: null != T ? T : void 0,
-          channelId: null != h ? h : void 0,
-          messageId: null != f ? f : void 0,
-          roleId: null != S ? S : void 0,
+          guildId: null != f ? f : void 0,
+          channelId: null != S ? S : void 0,
+          messageId: null != A ? A : void 0,
+          roleId: null != N ? N : void 0,
           analyticsLocation: {
-            section: _.jXE.BITE_SIZE_PROFILE_POPOUT
+            section: d.jXE.BITE_SIZE_PROFILE_POPOUT
           },
-          sourceAnalyticsLocations: N
+          sourceAnalyticsLocations: m
         })
       },
-      children: (0, i.jsx)(s.Text, {
+      children: (0, i.jsx)(a.Text, {
         variant: "text-xs/normal",
         color: "none",
-        children: c.Z.Messages.USER_PROFILE_VIEW_FULL_BIO
+        children: E.Z.Messages.USER_PROFILE_VIEW_FULL_BIO
       })
     })]
   })
