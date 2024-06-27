167b6dfa300cde506da7335632d253d9b1ef7aad
DiscrapperManager <admin@minerpl.xyz>
2024-06-27 14:04:57 +0200

    canaryRelease: 305634
    Main scripts: 20
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 7036
     * Scripts: 10494
    Strings: 18250
    Routes: 260
    Experiments Count: 349

--- chunks/231664.js
+++ chunks/231664.js
@@ -11,7 +11,7 @@ var i = n(848246),
 
 function o(e, t) {
   let n = (0, l.e7)([r.Z], () => null != e ? r.Z.getStreamParticipants(e).find(e => e.user.id === t) : void 0, [e, t]);
-  if ((0, a.So)(i.q.STREAM_HIGH_QUALITY)) return;
+  if (!(0, a.So)(i.q.STREAM_HIGH_QUALITY)) return;
   let o = null != n ? (0, s.Wc)(n) : void 0;
   return null != o ? "".concat((0, s.ml)(o.maxResolution), " ").concat((0, s.bp)(o.maxFrameRate)) : void 0
 }
