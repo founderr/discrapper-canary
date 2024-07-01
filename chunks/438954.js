4d5719c1313448854bf892cf83351c74e94d51c6
DiscrapperManager <admin@minerpl.xyz>
2024-07-01 18:49:43 +0200

    canaryRelease: 306532
    Main scripts: 3
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 10991
     * Scripts: 9684
    Strings: 18281
    Routes: 664
    Experiments Count: 348

--- chunks/438954.js
+++ chunks/438954.js
@@ -1,6 +1,13 @@
-var r, i;
 n.d(t, {
     a: function () {
         return r;
     }
-}), (i = r || (r = {}))[i.GIFT_INVENTORY_SETTINGS_BADGE = 1] = 'GIFT_INVENTORY_SETTINGS_BADGE', i[i.QUEST_BAR = 2] = 'QUEST_BAR', i[i.ACTIVITY_PANEL = 4] = 'ACTIVITY_PANEL', i[i.QUEST_LIVE_STREAM = 8] = 'QUEST_LIVE_STREAM';
+});
+let r = {
+    GIFT_INVENTORY_SETTINGS_BADGE: 1,
+    QUEST_BAR: 2,
+    QUEST_BAR_V2: 2,
+    QUEST_BAR_MOBILE: 2,
+    ACTIVITY_PANEL: 4,
+    QUEST_LIVE_STREAM: 8
+};
