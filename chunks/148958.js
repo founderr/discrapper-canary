76528c61406fd56b71fd1670bccaa0bdcf1f5663
DiscrapperManager <admin@minerpl.xyz>
2024-07-08 23:50:24 +0200

    canaryRelease: 307914
    Main scripts: 3
    Lazy Loaded Scripts: 834
    Chunks:
     * Main Files: 10992
     * Scripts: 9694
    Strings: 18309
    Routes: 663
    Experiments Count: 350

--- chunks/148958.js
+++ chunks/148958.js
@@ -20,22 +20,48 @@ function c(e, n) {
         ]), u = i.useMemo(() => null == n ? void 0 : n.filter(n => !e.some(e => e.id === n.application.id)), [
             e,
             n
+        ]), d = i.useMemo(() => {
+            var e;
+            null == u || u.forEach(e => {
+                let n = s.default.extractTimestamp(e.id);
+                null == t.getEntry(e.application.id) && t.track(e.application.id, n);
+            }), t.compute();
+            let n = null !== (e = null == u ? void 0 : u.map(e => (0, a.X0)(e.application, !0))) && void 0 !== e ? e : [], i = [...c];
+            return i.push(...n), i.sort((e, n) => {
+                var i, l;
+                let a = (null !== (i = t.getScore(n.id)) && void 0 !== i ? i : 0) - (null !== (l = t.getScore(e.id)) && void 0 !== l ? l : 0);
+                return 0 !== a ? a : e.name.localeCompare(n.name);
+            }), i;
+        }, [
+            c,
+            t,
+            u
         ]);
     return i.useMemo(() => {
-        var e;
-        null == u || u.forEach(e => {
-            let n = s.default.extractTimestamp(e.id);
-            null == t.getEntry(e.application.id) && t.track(e.application.id, n);
-        }), t.compute();
-        let n = null !== (e = null == u ? void 0 : u.map(e => (0, a.X0)(e.application, !0))) && void 0 !== e ? e : [], i = [...c];
-        return i.push(...n), i.sort((e, n) => {
-            var i, l;
-            let a = (null !== (i = t.getScore(n.id)) && void 0 !== i ? i : 0) - (null !== (l = t.getScore(e.id)) && void 0 !== l ? l : 0);
-            return 0 !== a ? a : e.name.localeCompare(n.name);
-        }), i;
+        let e, i;
+        null == n || n.forEach(n => {
+            let t = s.default.extractTimestamp(n.id);
+            (null == i || t > i) && (e = n, i = t);
+        }), c.forEach(n => {
+            var l, a;
+            let r = Math.max(...null !== (a = null === (l = t.getEntry(n.id)) || void 0 === l ? void 0 : l.recentUses) && void 0 !== a ? a : []);
+            (null == i || r > i) && (e = n, i = r);
+        });
+        let l = d.filter(n => {
+            var t, i;
+            return i = n.id === (null == e ? void 0 : null === (t = e.application) || void 0 === t ? void 0 : t.id), i;
+        });
+        return [
+            ...l,
+            ...d.filter(n => {
+                var t, i;
+                return i = n.id !== (null == e ? void 0 : null === (t = e.application) || void 0 === t ? void 0 : t.id), i;
+            })
+        ];
     }, [
+        d,
         c,
         t,
-        u
+        n
     ]);
 }
