50aceb7dc447dd79e83d949ab59d9c1be1783d33
DiscrapperManager <admin@minerpl.xyz>
2024-07-08 19:22:53 +0200

    canaryRelease: 307733
    Main scripts: 3
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 10987
     * Scripts: 9694
    Strings: 18296
    Routes: 663
    Experiments Count: 349

--- chunks/802463.js
+++ chunks/802463.js
@@ -1,61 +1,56 @@
 n(47120);
-var r, i, a, o, s = n(873011), l = n(442837), u = n(570140), c = n(70956);
-let d = [], _ = new Set(), E = 0;
-class f extends (r = l.ZP.Store) {
-    initialize() {
-        setInterval(() => {
-            this.emitChange();
-        }, 1 * c.Z.Millis.MINUTE);
-    }
+var r, i, a, o, s = n(873011), l = n(442837), u = n(570140);
+let c = [], d = new Set(), _ = 0;
+class E extends (r = l.ZP.Store) {
     getMessageReminders() {
-        return d;
+        return c;
     }
     isMessageReminder(e) {
-        let t = d.find(t => t.messageId === e);
+        let t = c.find(t => t.messageId === e);
         return null != t && !t.complete;
     }
     getOverdueMessageReminderCount() {
-        return d.filter(e => null == e.dueAt || new Date() > e.dueAt).length;
+        return c.filter(e => null == e.dueAt || new Date() > e.dueAt).length;
     }
     getLastFetched() {
-        return E;
+        return _;
     }
     hasSentNotification(e) {
-        return _.has(e);
+        return d.has(e);
     }
     getState() {
-        return { messages: d };
+        return { messages: c };
     }
 }
-o = 'MessageRemindersStore', (a = 'displayName') in (i = f) ? Object.defineProperty(i, a, {
+o = 'MessageRemindersStore', (a = 'displayName') in (i = E) ? Object.defineProperty(i, a, {
     value: o,
     enumerable: !0,
     configurable: !0,
     writable: !0
-}) : i[a] = o, t.Z = new f(u.Z, {
+}) : i[a] = o, t.Z = new E(u.Z, {
     SAVED_MESSAGES_UPDATE: function (e) {
         let {messages: t} = e;
-        E = new Date().getTime(), d = t.filter(e => e.type === s.J.REMINDER).map(e => ({
+        _ = new Date().getTime(), c = t.filter(e => e.type === s.J.REMINDER).map(e => ({
             ...e,
             complete: !1
         })), t.forEach(e => {
-            null != e.dueAt && e.dueAt > new Date() && _.delete(e.messageId), null != e.dueAt && e.dueAt < new Date() && _.add(e.messageId);
+            null != e.dueAt && e.dueAt > new Date() && d.delete(e.messageId), null != e.dueAt && e.dueAt < new Date() && d.add(e.messageId);
         });
     },
     MESSAGE_REMINDER_TOGGLE: function (e) {
         let {
                 messageId: t,
                 complete: n
-            } = e, r = d.findIndex(e => e.messageId === t);
+            } = e, r = c.findIndex(e => e.messageId === t);
         if (-1 === r)
             return !1;
-        d[r] = {
-            ...d[r],
+        c[r] = {
+            ...c[r],
             complete: n
         };
     },
     MESSAGE_REMINDER_NOTIFIED: function (e) {
         let {messageId: t} = e;
-        _.add(t);
+        d.add(t);
     }
 });
--- chunks/937603.js
+++ chunks/937603.js
@@ -1,6 +1,6 @@
 n(47120);
-var r = n(570140), i = n(147913), a = n(703656), o = n(70956), s = n(654769), l = n(742989), u = n(802463), c = n(981631);
-function d(e, t, n) {
+var r = n(570140), i = n(147913), a = n(703656), o = n(654769), s = n(742989), l = n(802463), u = n(981631);
+function c(e, t, n) {
     return t in e ? Object.defineProperty(e, t, {
         value: n,
         enumerable: !0,
@@ -8,33 +8,44 @@ function d(e, t, n) {
         writable: !0
     }) : e[t] = n, e;
 }
-class _ extends i.Z {
+let d = null;
+function _() {
+    if (s.Z.getCurrentConfig({ location: '8fe27d_1' }, { autoTrackExposure: !1 }).showReminders) {
+        l.Z.getMessageReminders().filter(e => null != e.dueAt && new Date() >= e.dueAt).filter(e => !l.Z.hasSentNotification(e.messageId)).forEach(e => {
+            var t, n;
+            o.Z.showNotification(null, 'Overdue reminder in '.concat(e.channelSummary), null !== (n = null !== (t = e.notes) && void 0 !== t ? t : e.messageSummary) && void 0 !== n ? n : '', {
+                notif_type: 'OVERDUE_MESSAGE_REMINDER',
+                notif_user_id: e.authorId,
+                message_id: e.messageId,
+                channel_id: e.channelId,
+                guild_id: e.guildId
+            }, {
+                omitViewTracking: !0,
+                tag: e.messageId,
+                onClick() {
+                    (0, a.uL)(u.Z5c.CHANNEL(e.guildId, e.channelId, e.messageId));
+                }
+            }), r.Z.dispatch({
+                type: 'MESSAGE_REMINDER_NOTIFIED',
+                messageId: e.messageId
+            });
+        });
+        let e = l.Z.getMessageReminders().filter(e => null != e.dueAt && new Date() < e.dueAt).sort((e, t) => e.dueAt.getTime() - t.dueAt.getTime());
+        null != d && clearTimeout(d), e.length > 0 && (d = setTimeout(() => {
+            _();
+        }, new Date().getTime() - e[0].dueAt.getTime()));
+    }
+}
+class E extends i.Z {
     constructor(...e) {
-        super(...e), d(this, 'actions', { POST_CONNECTION_OPEN: () => this.handlePostConnectionOpen() }), d(this, 'handlePostConnectionOpen', () => {
-            !function e() {
-                l.Z.getCurrentConfig({ location: '8fe27d_1' }, { autoTrackExposure: !1 }).showReminders && (u.Z.getMessageReminders().filter(e => null != e.dueAt && new Date() > e.dueAt).filter(e => !u.Z.hasSentNotification(e.messageId)).forEach(e => {
-                    var t, n;
-                    s.Z.showNotification(null, 'Overdue reminder in '.concat(e.channelSummary), null !== (n = null !== (t = e.notes) && void 0 !== t ? t : e.messageSummary) && void 0 !== n ? n : '', {
-                        notif_type: 'OVERDUE_MESSAGE_REMINDER',
-                        notif_user_id: e.authorId,
-                        message_id: e.messageId,
-                        channel_id: e.channelId,
-                        guild_id: e.guildId
-                    }, {
-                        omitViewTracking: !0,
-                        tag: e.messageId,
-                        onClick() {
-                            (0, a.uL)(c.Z5c.CHANNEL(e.guildId, e.channelId, e.messageId));
-                        }
-                    }), r.Z.dispatch({
-                        type: 'MESSAGE_REMINDER_NOTIFIED',
-                        messageId: e.messageId
-                    });
-                }), setTimeout(() => {
-                    e();
-                }, 5 * o.Z.Millis.MINUTE));
-            }();
+        super(...e), c(this, 'actions', {
+            POST_CONNECTION_OPEN: () => this.handlePostConnectionOpen(),
+            SAVED_MESSAGES_UPDATE: () => this.handleUpdates()
+        }), c(this, 'handlePostConnectionOpen', () => {
+            _();
+        }), c(this, 'handleUpdates', () => {
+            _();
         });
     }
 }
-t.Z = new _();
+t.Z = new E();
