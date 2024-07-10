6e05b9ab7f8d4348e61c9311b2a58681c648abeb
DiscrapperManager <admin@minerpl.xyz>
2024-07-10 17:44:18 +0200

    canaryRelease: 308442
    Main scripts: 3
    Lazy Loaded Scripts: 833
    Chunks:
     * Main Files: 10997
     * Scripts: 9701
    Strings: 18314
    Routes: 664
    Experiments Count: 351

--- chunks/395727.js
+++ chunks/395727.js
@@ -15,7 +15,7 @@ class u extends r.Z {
     _handleHistoryResponse(e) {
         if (null == e)
             return;
-        let t = 0 === Math.floor(1000 * Math.random());
+        let t = 0 === Math.floor(500 * Math.random());
         e.forEach(e => {
             'analytics' === e.type ? e.name === l.rMx.UPDATER_METRICS_DOWNLOAD || e.name === l.rMx.UPDATER_METRICS_INSTALL || e.name === l.rMx.UPDATER_METRICS_COMBINED || e.name === l.rMx.UPDATER_METRICS_TRANSITION_STATUS ? t && i.default.track(e.name, e.data) : console.warn('Unknown updater analytic event '.concat(e.name)) : this._tracker.trackEvent(e);
         }), this._tracker.submissionReady() && (i.default.track(l.rMx.APP_MODULES_UPDATED, this._tracker.getStats()), this._tracker.reset());
