28a4580e121bea41fe185fbafe04c61c224718ef
DiscrapperManager <admin@minerpl.xyz>
2024-07-11 23:01:18 +0200

    canaryRelease: 309006
    Main scripts: 3
    Lazy Loaded Scripts: 834
    Chunks:
     * Main Files: 11001
     * Scripts: 9708
    Strings: 18355
    Routes: 664
    Experiments Count: 352

--- chunks/593214.js
+++ chunks/593214.js
@@ -1,46 +1,46 @@
 t.d(n, {
     Mt: function () {
-        return v;
+        return g;
     },
     li: function () {
-        return h;
+        return m;
     },
     s4: function () {
-        return f;
+        return h;
     },
     up: function () {
-        return x;
+        return v;
     },
     zv: function () {
-        return g;
+        return f;
     }
 }), t(47120), t(653041);
 var r = t(470079), i = t(106351), o = t(442837);
 t(592125);
-var l = t(984933), a = t(914010), d = t(594174);
+var l = t(984933), a = t(914010);
 t(709054);
-var c = t(853856), u = t(362658), s = t(434065), m = t(981631);
-function h(e) {
+var d = t(853856), c = t(362658), s = t(434065), u = t(981631);
+function m(e) {
     let {
             favoritesEnabled: n,
             hasStaffPrivileges: t
-        } = (0, u.z)('useCanFavoriteChannel'), r = (0, o.e7)([c.Z], () => c.Z.isFavorite(e.id)), i = (0, o.e7)([d.default], () => d.default.getCurrentUser()), l = !(e.isDM() || e.isThread()) || t && (null == i ? void 0 : i.isStaff()) === !0;
-    return n && !__OVERLAY__ && !r && l;
+        } = (0, c.z)('useCanFavoriteChannel'), r = (0, o.e7)([d.Z], () => d.Z.isFavorite(e.id)), i = e.isDM() || e.isThread();
+    return n && !__OVERLAY__ && !r && (!i || t);
 }
-function f(e) {
-    return (0, o.e7)([c.Z], () => c.Z.getFavorite(e));
+function h(e) {
+    return (0, o.e7)([d.Z], () => d.Z.getFavorite(e));
 }
-function g() {
-    return (0, o.e7)([l.ZP], () => l.ZP.getChannels(m.I_8))[i.d.GUILD_CATEGORY].map(e => ({
+function f() {
+    return (0, o.e7)([l.ZP], () => l.ZP.getChannels(u.I_8))[i.d.GUILD_CATEGORY].map(e => ({
         id: 'null' === e.channel.id ? null : e.channel.id,
         name: e.channel.name
     }));
 }
-function v() {
-    return (0, o.e7)([a.Z], () => a.Z.getGuildId()) === m.I_8;
+function g() {
+    return (0, o.e7)([a.Z], () => a.Z.getGuildId()) === u.I_8;
 }
-function x() {
-    let {isFavoritesPerk: e} = (0, u.z)('useFavoriteAdded'), n = (0, s.r)(), t = r.useCallback(() => {
+function v() {
+    let {isFavoritesPerk: e} = (0, c.z)('useFavoriteAdded'), n = (0, s.r)(), t = r.useCallback(() => {
             e && n.notifyFavoriteAdded();
         }, [
             n,
