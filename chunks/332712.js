4ba68b7cb29168499d9138ddff7aac3e39612d9a
DiscrapperManager <admin@minerpl.xyz>
2024-07-12 19:21:45 +0200

    canaryRelease: 309280
    Main scripts: 3
    Lazy Loaded Scripts: 834
    Chunks:
     * Main Files: 11001
     * Scripts: 9711
    Strings: 18376
    Routes: 665
    Experiments Count: 352

--- chunks/332712.js
+++ chunks/332712.js
@@ -1,12 +1,19 @@
 n.d(t, {
-    _: function () {
-        return a;
+    O: function () {
+        return c;
     }
 });
-var i = n(442837), s = n(621853);
-function a(e) {
-    return (0, i.Wu)([s.Z], () => {
-        var t, n;
-        return null !== (n = null === (t = s.Z.getMutualGuilds(e)) || void 0 === t ? void 0 : t.map(e => e.guild)) && void 0 !== n ? n : [];
-    });
+var i = n(470079), s = n(442837), a = n(570140), r = n(621853), l = n(484459), o = n(594174);
+function c(e) {
+    let t = (0, s.e7)([o.default], () => o.default.getUser(e)), n = (0, s.Wu)([r.Z], () => {
+            var t, n;
+            return null !== (n = null === (t = r.Z.getMutualGuilds(e)) || void 0 === t ? void 0 : t.map(e => e.guild)) && void 0 !== n ? n : [];
+        });
+    return i.useEffect(() => {
+        0 === n.length && null != t && null == r.Z.getMutualGuilds(e) && a.Z.wait(() => (0, l.Z)(e, void 0, { withMutualGuilds: !0 }));
+    }, [
+        n,
+        t,
+        e
+    ]), n;
 }
