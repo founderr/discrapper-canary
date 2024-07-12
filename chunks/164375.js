f8e4951fbb69632877c1144f644e8a021508b82d
DiscrapperManager <admin@minerpl.xyz>
2024-07-12 18:52:44 +0200

    canaryRelease: 309258
    Main scripts: 3
    Lazy Loaded Scripts: 834
    Chunks:
     * Main Files: 11001
     * Scripts: 9710
    Strings: 18356
    Routes: 665
    Experiments Count: 352

--- chunks/164375.js
+++ chunks/164375.js
@@ -1,106 +1,105 @@
-a.r(s), a.d(s, {
+n.r(s), n.d(s, {
     default: function () {
         return l;
     }
 });
-var n = a(735250);
-a(470079);
-var t = a(481060), r = a(493683), o = a(594174), i = a(799352), c = a(689938), _ = a(251846), d = a(444812);
+var t = n(735250);
+n(470079);
+var a = n(481060), o = n(493683), r = n(594174), i = n(799352), c = n(689938), _ = n(251846), d = n(444812);
 function l(e) {
     var s;
     let {
-            channelId: a,
+            channelId: n,
             blockedUserIds: l,
             transitionState: E,
             onClose: u
-        } = e, N = l.map(e => o.default.getUser(e)), h = () => {
-            u(), (0, i.O)(a);
-        }, m = N.length > 1;
-    return (0, n.jsxs)(t.ModalRoot, {
+        } = e, N = l.map(e => r.default.getUser(e)), R = () => {
+            u(), (0, i.O)(n);
+        }, h = N.length > 1;
+    return (0, t.jsxs)(a.ModalRoot, {
         transitionState: E,
         children: [
-            (0, n.jsx)('img', {
+            (0, t.jsx)('img', {
                 alt: '',
                 className: _.headerImage,
                 src: d
             }),
-            (0, n.jsx)(t.ModalHeader, {
+            (0, t.jsx)(a.ModalHeader, {
                 separator: !1,
                 className: _.header,
-                children: (0, n.jsxs)(t.Heading, {
+                children: (0, t.jsxs)(a.Heading, {
                     variant: 'heading-lg/bold',
                     className: _.title,
                     children: [
-                        m ? c.Z.Messages.GDM_MULTIPLE_BLOCKED_USERS_WARNING_HEADER_MODAL : c.Z.Messages.GDM_SINGLE_BLOCKED_USER_WARNING_HEADER_MODAL,
-                        (0, n.jsx)('br', {}),
+                        h ? c.Z.Messages.GDM_MULTIPLE_BLOCKED_USERS_WARNING_HEADER_MODAL : c.Z.Messages.GDM_SINGLE_BLOCKED_USER_WARNING_HEADER_MODAL,
+                        (0, t.jsx)('br', {}),
                         c.Z.Messages.GDM_BLOCKED_USER_WARNING_LEAVE_GROUP_MODAL
                     ]
                 })
             }),
-            (0, n.jsxs)(t.ModalContent, {
+            (0, t.jsxs)(a.ModalContent, {
                 className: _.content,
                 children: [
-                    (0, n.jsxs)('div', {
+                    (0, t.jsxs)('div', {
                         className: _.infoGroup,
                         children: [
-                            (0, n.jsx)('div', {
+                            (0, t.jsx)('div', {
                                 className: _.infoRow,
-                                children: m ? (0, n.jsxs)(n.Fragment, {
+                                children: h ? (0, t.jsxs)(t.Fragment, {
                                     children: [
-                                        (0, n.jsx)(t.GroupIcon, {}),
-                                        (0, n.jsx)(t.Text, {
+                                        (0, t.jsx)(a.GroupIcon, {}),
+                                        (0, t.jsx)(a.Text, {
                                             variant: 'text-md/medium',
                                             children: c.Z.Messages.GDM_MULTIPLE_BLOCKED_USERS_WARNING_PRESENCE.format({ blockedUserCount: N.length })
                                         })
                                     ]
-                                }) : (0, n.jsxs)(n.Fragment, {
+                                }) : (0, t.jsxs)(t.Fragment, {
                                     children: [
-                                        (0, n.jsx)(t.UserIcon, {}),
-                                        (0, n.jsx)(t.Text, {
+                                        (0, t.jsx)(a.UserIcon, {}),
+                                        (0, t.jsx)(a.Text, {
                                             variant: 'text-md/medium',
                                             children: c.Z.Messages.GDM_SINGLE_BLOCKED_USER_WARNING_PRESENCE.format({ userName: null === (s = N[0]) || void 0 === s ? void 0 : s.username })
                                         })
                                     ]
                                 })
                             }),
-                            (0, n.jsxs)('div', {
+                            (0, t.jsxs)('div', {
                                 className: _.infoRow,
                                 children: [
-                                    (0, n.jsx)(t.BellSlashIcon, {}),
-                                    (0, n.jsx)(t.Text, {
+                                    (0, t.jsx)(a.BellSlashIcon, {}),
+                                    (0, t.jsx)(a.Text, {
                                         variant: 'text-md/medium',
                                         children: c.Z.Messages.GDM_BLOCKED_USER_WARNING_NO_NOTIFY
                                     })
                                 ]
                             }),
-                            (0, n.jsxs)('div', {
+                            (0, t.jsxs)('div', {
                                 className: _.infoRow,
                                 children: [
-                                    (0, n.jsx)(t.EnvelopeIcon, {}),
-                                    (0, n.jsx)(t.Text, {
+                                    (0, t.jsx)(a.EnvelopeIcon, {}),
+                                    (0, t.jsx)(a.Text, {
                                         variant: 'text-md/medium',
-                                        color: 'header-primary',
                                         children: c.Z.Messages.GDM_BLOCKED_USER_WARNING_INVITE_NEEDED
                                     })
                                 ]
                             })
                         ]
                     }),
-                    (0, n.jsxs)('div', {
+                    (0, t.jsxs)('div', {
                         className: _.buttonGroup,
                         children: [
-                            (0, n.jsx)(t.Button, {
+                            (0, t.jsx)(a.Button, {
                                 type: 'submit',
-                                color: t.Button.Colors.BRAND,
-                                size: t.Button.Sizes.MEDIUM,
+                                color: a.Button.Colors.BRAND,
+                                size: a.Button.Sizes.MEDIUM,
                                 onClick: () => {
-                                    h(), r.Z.closePrivateChannel(a, !0, !0);
+                                    R(), o.Z.closePrivateChannel(n, !0, !0);
                                 },
                                 children: c.Z.Messages.GDM_BLOCKED_USER_WARNING_LEAVE
                             }),
-                            (0, n.jsx)(t.Button, {
-                                color: t.Button.Colors.PRIMARY,
-                                onClick: h,
+                            (0, t.jsx)(a.Button, {
+                                color: a.Button.Colors.PRIMARY,
+                                onClick: R,
                                 children: c.Z.Messages.GDM_BLOCKED_USER_WARNING_STAY
                             })
                         ]
