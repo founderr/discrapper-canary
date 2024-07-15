966b9e55ba9e84fb5cf946f8aa34a9899bbe6e2d
DiscrapperManager <admin@minerpl.xyz>
2024-07-16 00:38:17 +0200

    canaryRelease: 309872
    Main scripts: 3
    Lazy Loaded Scripts: 834
    Chunks:
     * Main Files: 11025
     * Scripts: 9715
    Strings: 18400
    Routes: 668
    Experiments Count: 351

--- chunks/121711.js
+++ chunks/121711.js
@@ -1,85 +1,87 @@
 var i = n(735250),
   a = n(470079),
   s = n(948789),
-  r = n(481060),
-  l = n(100527),
-  o = n(252618),
-  c = n(212476),
-  d = n(207986),
-  u = n(374939),
-  _ = n(791734),
-  E = n(63063),
-  h = n(113434),
-  I = n(709158),
-  m = n(710914),
-  g = n(452081),
-  p = n(981631),
-  T = n(689938),
-  f = n(852595);
+  r = n(780384),
+  l = n(481060),
+  o = n(410030),
+  c = n(100527),
+  d = n(252618),
+  u = n(212476),
+  _ = n(207986),
+  E = n(374939),
+  h = n(791734),
+  I = n(63063),
+  m = n(113434),
+  g = n(709158),
+  p = n(710914),
+  T = n(452081),
+  f = n(981631),
+  S = n(689938),
+  C = n(852595);
 t.Z = function(e) {
-  let {} = e, t = (0, I.Z)(l.Z.QUEST_HOME_PAGE), {
+  let {} = e, t = (0, g.Z)(c.Z.QUEST_HOME_PAGE), {
 tabs: n,
-selectedTab: S,
-onSelectTab: C
-  } = (0, g.z)(), {
-handleScroll: N,
-scrollPosition: A
-  } = (0, c.c)(), v = (0, d.Z)({
-scrollPosition: A
-  }), {
-quests: Z
-  } = (0, h.bA)(S.id);
+selectedTab: N,
+onSelectTab: A
+  } = (0, T.z)(), {
+handleScroll: v,
+scrollPosition: Z
+  } = (0, u.c)(), L = (0, _.Z)({
+scrollPosition: Z
+  }), O = (0, r.wj)((0, o.ZP)()), {
+quests: R
+  } = (0, m.bA)(N.id), x = a.useMemo(() => O ? 'https://cdn.discordapp.com/assets/discovery/quest-mountain-hero.png' : 'https://cdn.discordapp.com/assets/discovery/quest-mountain-hero-light-mode.png', [O]);
   a.useEffect(() => {
-!t && (0, s.uL)(p.Z5c.FRIENDS);
+!t && (0, s.uL)(f.Z5c.FRIENDS);
   }, [t]), a.useEffect(() => {}, []);
-  let L = a.useCallback(() => {
-window.open(E.Z.getArticleURL(p.BhN.QUESTS_LEARN_MORE));
+  let b = a.useCallback(() => {
+window.open(I.Z.getArticleURL(f.BhN.QUESTS_LEARN_MORE));
   }, []);
-  return ((0, o.Tt)({
-location: T.Z.Messages.QUESTS
+  return ((0, d.Tt)({
+location: S.Z.Messages.QUESTS
   }), t) ? (0, i.jsxs)('div', {
-className: f.container,
+className: C.container,
 children: [
-  (0, i.jsx)(_.Z, {
-    leading: (0, i.jsx)(r.QuestsIcon, {
-      color: r.tokens.colors.HEADER_PRIMARY,
+  (0, i.jsx)(h.Z, {
+    leading: (0, i.jsx)(l.QuestsIcon, {
+      color: l.tokens.colors.HEADER_PRIMARY,
       size: 'md'
     }),
     tabs: n,
-    selectedTab: S,
-    onTabSelect: C,
-    scrollPosition: A,
-    backgroundStyle: v
+    selectedTab: N,
+    onTabSelect: A,
+    scrollPosition: Z,
+    backgroundStyle: L
   }),
-  (0, i.jsx)(u.Z, {
-    title: T.Z.Messages.QUESTS_HOME_HERO_TITLE,
-    description: T.Z.Messages.QUESTS_HOME_HERO_DESCRIPTION,
-    backgroundImageUrl: 'https://cdn.discordapp.com/assets/discovery/quest-mountain-hero.png',
-    onScroll: N,
-    bannerContainerClassName: f.hero,
-    button: (0, i.jsxs)(r.Button, {
+  (0, i.jsx)(E.Z, {
+    title: S.Z.Messages.QUESTS_HOME_HERO_TITLE,
+    description: S.Z.Messages.QUESTS_HOME_HERO_DESCRIPTION,
+    backgroundImageUrl: x,
+    onScroll: v,
+    bannerContainerClassName: C.hero,
+    button: (0, i.jsxs)(l.Button, {
       size: 'medium',
-      color: r.ButtonColors.TRANSPARENT,
-      className: f.button,
-      onClick: L,
-      innerClassName: f.innerButton,
+      color: l.ButtonColors.TRANSPARENT,
+      className: C.button,
+      onClick: b,
+      innerClassName: C.innerButton,
       children: [
-        (0, i.jsx)(r.Text, {
+        (0, i.jsx)(l.Text, {
           variant: 'text-md/semibold',
           color: 'always-white',
-          className: f.externalLinkText,
-          children: T.Z.Messages.LEARN_MORE
+          className: C.externalLinkText,
+          children: S.Z.Messages.LEARN_MORE
         }),
-        (0, i.jsx)(r.LinkExternalSmallIcon, {
-          className: f.icon,
+        (0, i.jsx)(l.LinkExternalSmallIcon, {
+          className: C.icon,
           color: 'var(--white-500)'
         })
       ]
     }),
     children: (0, i.jsx)('div', {
-      className: f.gridContainer,
-      children: (0, i.jsx)(m.Z, {
-        quests: Z
+      className: C.gridContainer,
+      children: (0, i.jsx)(p.Z, {
+        quests: R
       })
     })
   })
