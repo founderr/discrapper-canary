7ea01300b7d028688fa9be0d94f6568ed60e55f2
DiscrapperManager <admin@minerpl.xyz>
2024-07-23 02:47:15 +0200

    canaryRelease: 312031
    Main scripts: 3
    Lazy Loaded Scripts: 838
    Chunks:
     * Main Files: 11047
     * Scripts: 9751
    Strings: 18449
    Routes: 670
    Experiments Count: 352

--- chunks/207205.js
+++ chunks/207205.js
@@ -1,59 +1,65 @@
 n.d(t, {
   $U: function() {
-return h;
+return m;
   },
   Jb: function() {
 return i;
   },
   cn: function() {
-return u;
+return E;
   },
   em: function() {
-return _;
+return h;
+  },
+  mV: function() {
+return p;
   },
   rK: function() {
-return I;
+return g;
   },
   wV: function() {
-return E;
+return I;
   }
-}), n(47120), n(724458), n(653041), n(873546), n(442837), n(638395);
-var i, s, a = n(786761);
+}), n(47120), n(724458), n(653041), n(873546);
+var i, s, a = n(661869),
+  r = n(876215);
+n(442837), n(638395);
+var l = n(786761);
 n(375954);
-var r = n(306680),
-  l = n(709054),
-  o = n(505369);
+var o = n(306680),
+  c = n(709054),
+  d = n(505369);
 n(109911);
-var c = n(265164),
-  d = n(761080);
+var u = n(265164),
+  _ = n(761080);
 
-function u(e) {
+function E(e) {
   return e < -0.9 ? 3 : e < 0 ? 2 : e > 0 ? 1 : 0;
 }
-async function _(e, t, n) {
-let i = c.Z.getHydratedItems(),
+async function h(e, t, n) {
+let i = u.Z.getHydratedItems(),
   s = e.slice(t, n);
 if (0 === s.length)
   return;
 let a = s.filter(e => null == i[e.id]),
-  r = a.filter(e => e.type === d.Rr.MESSAGE).map(e => ({
+  r = a.filter(e => e.type === _.Rr.MESSAGE).map(e => ({
     channel_id: e.data.channel_id,
     message_id: e.data.message_id
   })),
-  l = a.filter(e => e.type === d.Rr.SUMMARY).map(e => ({
+  l = a.filter(e => e.type === _.Rr.SUMMARY).map(e => ({
     guild_id: e.data.guild_id,
     channel_id: e.data.channel_id,
     summary_id: e.data.summary_id
   })),
-  u = a.filter(e => e.type === d.Rr.ACTIVITY).map(e => ({
+  o = a.filter(e => e.type === _.Rr.ACTIVITY).map(e => ({
     user_id: e.data.user_id,
     content_id: e.data.content_id
   }));
-await o.Z.fetchHydrated(r, l, u, t, n);
+await d.Z.fetchHydrated(r, l, o, t, n);
   }
   (s = i || (i = {}))[s.DEFAULT = 0] = 'DEFAULT', s[s.MORE_FROM_GUILD = 1] = 'MORE_FROM_GUILD', s[s.LESS_FROM_GUILD = 2] = 'LESS_FROM_GUILD', s[s.MUTED_GUILD = 3] = 'MUTED_GUILD';
 
-function E(e, t) {
+function I(e, t) {
   let n = [],
 i = [],
 s = 0;
@@ -61,8 +67,8 @@ s = 0;
 if (null != t.reactions) {
   if (n.length < 5)
     for (let e of t.reactions) {
-      var r, l;
-      (null == e.count_details || (null !== (r = e.count_details.burst) && void 0 !== r ? r : 0) > 0 || (null !== (l = e.count_details.normal) && void 0 !== l ? l : 0) > 0) && n.push(e);
+      var a, r;
+      (null == e.count_details || (null !== (a = e.count_details.burst) && void 0 !== a ? a : 0) > 0 || (null !== (r = e.count_details.normal) && void 0 !== r ? r : 0) > 0) && n.push(e);
     }
   s += function(e) {
     let t = 0;
@@ -78,7 +84,7 @@ if (null != t.reactions) {
 }
   let o = null;
   for (let t of e.messages)
-if (null != t.author && o !== t.author.id && (i.push((0, a.e5)(t)), o = t.author.id, i.length >= 3))
+if (null != t.author && o !== t.author.id && (i.push((0, l.e5)(t)), o = t.author.id, i.length >= 3))
   break;
   return {
 id: e.id,
@@ -100,11 +106,35 @@ source: e.source
   };
 }
 
-function h(e, t) {
-  let n = r.ZP.ackMessageId(e);
-  return null != n && l.default.extractTimestamp(t) > l.default.extractTimestamp(n);
+function m(e, t) {
+  let n = o.ZP.ackMessageId(e);
+  return null != n && c.default.extractTimestamp(t) > c.default.extractTimestamp(n);
 }
 
-function I(e) {
+function g(e) {
   return !1;
+}
+
+function p(e) {
+  var t;
+  return {
+id: e.id,
+type: _.Rr.ACTIVITY,
+activity: {
+  id: e.id,
+  author_id: e.data.user_id,
+  author_type: a.i.USER,
+  traits: [],
+  participants: [],
+  content_type: r.s.CUSTOM_STATUS,
+  extra: {
+    status: null !== (t = e.data.text) && void 0 !== t ? t : '',
+    emoji_id: e.data.emoji_id,
+    emoji_name: e.data.emoji_name,
+    emoji_animated: e.data.emoji_animated
+  }
+},
+score: e.score,
+score_components: e.score_components
+  };
 }
--- chunks/761080.js
+++ chunks/761080.js
@@ -15,9 +15,10 @@ return o;
 var i, s, a = n(981631);
 let r = 15,
   l = 3;
-(s = i || (i = {}))[s.MESSAGE = 0] = 'MESSAGE', s[s.SUMMARY = 1] = 'SUMMARY', s[s.ACTIVITY = 2] = 'ACTIVITY';
+(s = i || (i = {}))[s.MESSAGE = 0] = 'MESSAGE', s[s.SUMMARY = 1] = 'SUMMARY', s[s.ACTIVITY = 2] = 'ACTIVITY', s[s.CUSTOM_STATUS = 3] = 'CUSTOM_STATUS';
 let o = new Set([
   0,
-  1
+  1,
+  3
 ]);
 a.hBH.IMAGE, a.hBH.VIDEO, a.hBH.GIFV;
