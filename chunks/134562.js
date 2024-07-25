72a0fa1822000ba0f5a90795ce7d1b9a91da5b17
DiscrapperManager <admin@minerpl.xyz>
2024-07-25 23:01:50 +0200

    canaryRelease: 312912
    Main scripts: 3
    Lazy Loaded Scripts: 843
    Chunks:
     * Main Files: 11083
     * Scripts: 9748
    Strings: 18460
    Routes: 671
    Experiments Count: 361

--- chunks/134562.js
+++ chunks/134562.js
@@ -1,66 +1,67 @@
-t.d(s, {
+n.d(s, {
   Z: function() {
-return r;
+return i;
   }
 });
-var n = t(735250);
-t(470079);
-var o = t(481060),
-  a = t(689938),
-  l = t(660817);
+var t = n(735250);
+n(470079);
+var o = n(481060),
+  a = n(259076),
+  l = n(689938),
+  r = n(660817);
 
-function r(e) {
+function i(e) {
   let {
 onNext: s,
-onClose: t
+onClose: n
   } = e;
-  return (0, n.jsxs)('form', {
+  return (0, t.jsxs)('form', {
 onSubmit: s,
 children: [
-  (0, n.jsxs)(o.ModalHeader, {
+  (0, t.jsxs)(o.ModalHeader, {
     separator: !1,
-    className: l.header,
+    className: r.header,
     children: [
-      (0, n.jsx)(o.Heading, {
+      (0, t.jsx)(o.Heading, {
         variant: 'heading-xl/extrabold',
-        children: a.Z.Messages.EMAIL_CHANGE_REASONING_WARNING_HEADER
+        children: l.Z.Messages.EMAIL_CHANGE_REASONING_WARNING_HEADER
       }),
-      (0, n.jsx)(o.ModalCloseButton, {
-        onClick: t,
-        className: l.modalCloseButton
+      (0, t.jsx)(o.ModalCloseButton, {
+        onClick: n,
+        className: r.modalCloseButton
       })
     ]
   }),
-  (0, n.jsxs)(o.ModalContent, {
-    className: l.content,
+  (0, t.jsxs)(o.ModalContent, {
+    className: r.content,
     children: [
-      (0, n.jsx)(o.Text, {
-        className: l.description,
+      (0, t.jsx)(o.Text, {
+        className: r.description,
         variant: 'text-md/normal',
-        children: a.Z.Messages.EMAIL_CHANGE_REASONING_IMPERSONATION_EDUCATION.format({
-          hcArticle: 'https://discord.com/safety/common-scams-what-to-look-out-for#title-3'
+        children: l.Z.Messages.EMAIL_CHANGE_REASONING_IMPERSONATION_EDUCATION.format({
+          hcArticle: a.j1
         })
       }),
-      (0, n.jsx)(o.Text, {
-        className: l.description,
+      (0, t.jsx)(o.Text, {
+        className: r.description,
         variant: 'text-md/normal',
-        children: a.Z.Messages.EMAIL_CHANGE_REASONING_DISCORD_NEVER_ASKS_FOR_EMAIL_CHANGE
+        children: l.Z.Messages.EMAIL_CHANGE_REASONING_DISCORD_NEVER_ASKS_FOR_EMAIL_CHANGE
       })
     ]
   }),
-  (0, n.jsxs)(o.ModalFooter, {
-    className: l.modalFooter,
+  (0, t.jsxs)(o.ModalFooter, {
+    className: r.modalFooter,
     children: [
-      (0, n.jsx)(o.Button, {
+      (0, t.jsx)(o.Button, {
         color: o.Button.Colors.BRAND,
-        onClick: t,
-        children: a.Z.Messages.CANCEL
+        onClick: n,
+        children: l.Z.Messages.CANCEL
       }),
-      (0, n.jsx)(o.Button, {
+      (0, t.jsx)(o.Button, {
         color: o.Button.Colors.PRIMARY,
         look: o.Button.Looks.LINK,
         onClick: s,
-        children: a.Z.Messages.EMAIL_CHANGE_REASONING_CONTINUE
+        children: l.Z.Messages.EMAIL_CHANGE_REASONING_CONTINUE
       })
     ]
   })
--- chunks/259076.js
+++ chunks/259076.js
@@ -1,15 +1,18 @@
-var n, o;
-t.d(s, {
+var t, o;
+n.d(s, {
   Mr: function() {
 return l;
   },
+  j1: function() {
+return r;
+  },
   pw: function() {
-return n;
+return t;
   },
   zf: function() {
 return a;
   }
-}), t(47120), (o = n || (n = {}))[o.DISCORD_EMPLOYEE_ASKED_ME_TO = 0] = 'DISCORD_EMPLOYEE_ASKED_ME_TO', o[o.SOMEONE_ASKED_ME_TO = 1] = 'SOMEONE_ASKED_ME_TO', o[o.NEW_EMAIL = 2] = 'NEW_EMAIL', o[o.SOMETHING_ELSE = 3] = 'SOMETHING_ELSE';
+}), n(47120), (o = t || (t = {}))[o.DISCORD_EMPLOYEE_ASKED_ME_TO = 0] = 'DISCORD_EMPLOYEE_ASKED_ME_TO', o[o.SOMEONE_ASKED_ME_TO = 1] = 'SOMEONE_ASKED_ME_TO', o[o.NEW_EMAIL = 2] = 'NEW_EMAIL', o[o.SOMETHING_ELSE = 3] = 'SOMETHING_ELSE';
 let a = [
 0,
 1,
@@ -19,4 +22,5 @@ let a = [
   l = new Set([
 0,
 1
-  ]);
+  ]),
+  r = 'https://discord.com/safety/understanding-and-avoiding-common-scams';
