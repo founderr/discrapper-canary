6b867b896042723932fdde450cecfce91ff611be
DiscrapperManager <admin@minerpl.xyz>
2024-07-26 04:24:08 +0200

    canaryRelease: 313103
    Main scripts: 3
    Lazy Loaded Scripts: 843
    Chunks:
     * Main Files: 11083
     * Scripts: 9767
    Strings: 18465
    Routes: 671
    Experiments Count: 362

--- chunks/73131.js
+++ chunks/73131.js
@@ -1,3 +1,7 @@
 e.exports = {
-  container: 'container_d19b33'
+  popoutContainer: 'popoutContainer_d19b33',
+  headerTitle: 'headerTitle_d19b33',
+  headerTitleIcon: 'headerTitleIcon_d19b33',
+  tabBar: 'tabBar_d19b33',
+  tabBarItem: 'tabBarItem_d19b33'
 };
