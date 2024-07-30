fcc2303274d3ccc6a4857d1c69e8126cb9239788
DiscrapperManager <admin@minerpl.xyz>
2024-07-30 20:03:29 +0200

    canaryRelease: 313913
    Main scripts: 3
    Lazy Loaded Scripts: 844
    Chunks:
     * Main Files: 11098
     * Scripts: 9764
    Strings: 18480
    Routes: 672
    Experiments Count: 367

--- chunks/370298.js
+++ chunks/370298.js
@@ -1,41 +1,44 @@
 t.d(n, {
   Z: function() {
-return g;
+return T;
   }
-});
+}), t(47120);
 var i = t(735250),
   o = t(470079),
-  a = t(120356),
-  l = t.n(a),
+  l = t(120356),
+  a = t.n(l),
   s = t(892814),
   r = t(442837),
   u = t(481060),
-  c = t(688465),
-  d = t(978983),
-  m = t(768581),
-  h = t(823379),
-  I = t(399654),
-  E = t(844439),
-  p = t(957552),
-  x = t(689938),
-  C = t(89311),
-  M = t(206227);
+  d = t(688465),
+  c = t(978983),
+  m = t(592125),
+  h = t(626135),
+  I = t(768581),
+  E = t(823379),
+  p = t(399654),
+  x = t(844439),
+  C = t(957552),
+  M = t(981631),
+  g = t(689938),
+  f = t(89311),
+  N = t(206227);
 
-function g(e) {
+function T(e) {
   let {
 toggleShowMenu: n,
 showMenu: t,
 className: o,
-channelId: a
+channelId: l
   } = e;
   return (0, i.jsx)(u.Popout, {
 renderPopout: e => {
   let {
     closePopout: n
   } = e;
-  return (0, i.jsx)(f, {
+  return (0, i.jsx)(j, {
     onClose: n,
-    channelId: a
+    channelId: l
   });
 },
 position: 'right',
@@ -48,7 +51,7 @@ children: e => {
     onClick: t
   } = e;
   return (0, i.jsx)(u.Tooltip, {
-    text: x.Z.Messages.APP_IMAGE_REC_USE_APP_TOOLTIP,
+    text: g.Z.Messages.APP_IMAGE_REC_USE_APP_TOOLTIP,
     children: e => (0, i.jsx)(u.Clickable, {
       ...e,
       className: o,
@@ -56,7 +59,7 @@ children: e => {
         offset: 2
       },
       onClick: n,
-      'aria-label': x.Z.Messages.APP_IMAGE_REC_USE_APP_TOOLTIP,
+      'aria-label': g.Z.Messages.APP_IMAGE_REC_USE_APP_TOOLTIP,
       children: (0, i.jsx)(u.AppsIcon, {
         size: 'custom',
         color: 'currentColor',
@@ -69,14 +72,14 @@ children: e => {
   });
 }
 
-function f(e) {
+function j(e) {
   let {
 onSelect: n,
 onClose: t,
-channelId: a
+channelId: l
   } = e, {
-fetchState: l,
-recommendationsSections: p
+fetchState: a,
+recommendationsSections: C
   } = function(e) {
 let {
   channelId: n
@@ -89,23 +92,23 @@ let {
   t
 ]);
 o.useEffect(() => {
-  (0, I.a)(i);
+  (0, p.a)(i);
 }, [i]);
 let {
-  fetchState: a,
-  recommendationsSections: l
-} = (0, r.cj)([E.ZP], () => ({
-  fetchState: E.ZP.getFetchState(i),
-  recommendationsSections: E.ZP.getRecommendations(i)
+  fetchState: l,
+  recommendationsSections: a
+} = (0, r.cj)([x.ZP], () => ({
+  fetchState: x.ZP.getFetchState(i),
+  recommendationsSections: x.ZP.getRecommendations(i)
 }));
 return {
-  fetchState: a,
-  recommendationsSections: l
+  fetchState: l,
+  recommendationsSections: a
 };
   }({
-channelId: a
-  }), M = l === E.M.FETCHING, g = o.useMemo(() => {
-let e = p.length > 0 ? p[0].items : void 0;
+channelId: l
+  }), N = m.Z.getChannel(l), T = a === x.M.FETCHING, j = o.useMemo(() => {
+let e = C.length > 0 ? C[0].items : void 0;
 return void 0 === e ? [] : e.map(e => {
   var n, t;
   let {
@@ -114,54 +117,67 @@ return void 0 === e ? [] : e.map(e => {
   } = e;
   if (void 0 === o || 0 === o.length)
     return null;
-  let a = o[0],
-    l = m.ZP.getApplicationIconURL({
+  let l = o[0],
+    a = I.ZP.getApplicationIconURL({
       id: i.id,
       icon: i.icon
     });
   return {
     application: i,
-    id: a.id,
-    name: null !== (n = a.name_localized) && void 0 !== n ? n : a.name,
-    description: null !== (t = a.description_localized) && void 0 !== t ? t : a.description,
-    icon: l
+    id: l.id,
+    name: null !== (n = l.name_localized) && void 0 !== n ? n : l.name,
+    description: null !== (t = l.description_localized) && void 0 !== t ? t : l.description,
+    icon: a
   };
-}).filter(h.lm);
-  }, [p]);
-  return (0, i.jsxs)(u.Menu, {
+}).filter(E.lm);
+  }, [C]);
+  return o.useEffect(() => {
+var e, n;
+if (!T && 0 !== j.length)
+  h.default.track(M.rMx.APP_IMAGE_RECS_MENU_VIEWED, {
+    guild_id: null !== (e = null == N ? void 0 : N.guild_id) && void 0 !== e ? e : '',
+    channel_id: null !== (n = null == N ? void 0 : N.id) && void 0 !== n ? n : '',
+    application_ids: [...j.map(e => Number(e.application.id))],
+    command_ids: [...j.map(e => Number(e.id))]
+  });
+  }, [
+j,
+N,
+T
+  ]), (0, i.jsxs)(u.Menu, {
 onSelect: n,
 navId: 'image-context-commands-popout',
 onClose: t,
-'aria-label': x.Z.Messages.APP_IMAGE_REC_MENU_ARIA_LABEL,
+'aria-label': g.Z.Messages.APP_IMAGE_REC_MENU_ARIA_LABEL,
 children: [
   (0, i.jsx)(u.MenuItem, {
     id: 'menu-image-rec-header',
     render: () => (0, i.jsxs)('div', {
-      className: C.flexRow,
+      className: f.flexRow,
       children: [
         (0, i.jsx)(u.Text, {
-          className: C.title,
+          className: f.title,
           variant: 'text-md/bold',
           color: 'header-primary',
-          children: x.Z.Messages.APP_IMAGE_REC_MENU_HEADER
+          children: g.Z.Messages.APP_IMAGE_REC_MENU_HEADER
         }),
-        (0, i.jsx)(c.Z, {})
+        (0, i.jsx)(d.Z, {})
       ]
     }),
     navigable: !1,
     disabled: !0
   }, 'menu-image-rec-header'),
   (0, i.jsx)(u.MenuSeparator, {}),
-  M ? (0, i.jsx)(u.MenuItem, {
+  T ? (0, i.jsx)(u.MenuItem, {
     id: 'menu-image-recs-placeholder',
-    render: () => (0, i.jsx)(d.Z, {}),
+    render: () => (0, i.jsx)(c.Z, {}),
     navigable: !1,
     disabled: !0
   }, 'menu-image-recs-placeholder') : (0, i.jsx)(i.Fragment, {
-    children: g.map(e => (0, i.jsx)(u.MenuItem, {
+    children: j.map(e => (0, i.jsx)(u.MenuItem, {
       id: e.id,
       action: () => {},
-      render: n => (0, i.jsx)(N, {
+      render: n => (0, i.jsx)(_, {
         command: e,
         isFocused: n.isFocused
       })
@@ -171,41 +187,41 @@ children: [
   });
 }
 
-function N(e) {
+function _(e) {
   let {
 command: n,
 isFocused: t
   } = e;
   return (0, i.jsxs)('div', {
-className: l()(M.item, M.labelContainer, M.colorDefault, {
-  [M.focused]: t
+className: a()(N.item, N.labelContainer, N.colorDefault, {
+  [N.focused]: t
 }),
 'data-menu-item': 'true',
 children: [
   (0, i.jsx)('div', {
-    className: l()(M.iconContainerLeft, C.appIcon),
-    children: (0, i.jsx)(p.Z, {
+    className: a()(N.iconContainerLeft, f.appIcon),
+    children: (0, i.jsx)(C.Z, {
       application: n.application,
-      className: C.appIcon
+      className: f.appIcon
     })
   }),
   (0, i.jsxs)('div', {
-    className: l()(M.label),
+    className: a()(N.label),
     children: [
       (0, i.jsx)('div', {
-        className: l()(C.name, {
-          [C.focused]: t
+        className: a()(f.name, {
+          [f.focused]: t
         }),
         children: n.name
       }),
       (0, i.jsx)('div', {
-        className: l()(C.description, M.subtext),
+        className: a()(f.description, N.subtext),
         children: n.description
       })
     ]
   }),
   (0, i.jsx)('div', {
-    className: l()(M.iconContainer, C.icon),
+    className: a()(N.iconContainer, f.icon),
     children: (0, i.jsx)(u.SendMessageIcon, {})
   })
 ]
