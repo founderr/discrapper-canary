aa8996cba5e4e12afd2699b24f2af25b8b665967
DiscrapperManager <admin@minerpl.xyz>
2024-07-30 15:18:19 +0200

    canaryRelease: 313828
    Main scripts: 3
    Lazy Loaded Scripts: 844
    Chunks:
     * Main Files: 11097
     * Scripts: 9761
    Strings: 18472
    Routes: 672
    Experiments Count: 366

--- chunks/248042.js
+++ chunks/248042.js
@@ -16,19 +16,19 @@ var r = n(913527),
   i = n.n(r),
   a = n(399606),
   s = n(355467),
-  o = n(774078),
-  l = n(211242),
-  u = n(706454),
-  c = n(594174),
-  d = n(78839),
-  _ = n(295226),
-  E = n(74538),
-  f = n(937579),
-  h = n(436783),
-  p = n(617799),
-  m = n(61196),
-  I = n(104494),
-  T = n(639119),
+  o = n(211242),
+  l = n(706454),
+  u = n(594174),
+  c = n(78839),
+  d = n(295226),
+  _ = n(74538),
+  E = n(937579),
+  f = n(436783),
+  h = n(617799),
+  p = n(61196),
+  m = n(104494),
+  I = n(639119),
+  T = n(921022),
   g = n(748770),
   S = n(725727),
   A = n(1844),
@@ -42,28 +42,28 @@ function R(e) {
 experimentEnabled: a,
 premiumSubscription: s,
 mostRecentSubscription: o
-  } = e, l = !1, u = !1;
+  } = e, l = !1, c = !1;
   if (null != o && o.status === v.O0b.ENDED) {
 let e = (null === (t = o.metadata) || void 0 === t ? void 0 : t.ended_at) != null ? i()(o.metadata.ended_at) : null,
-  r = null === (n = (0, E.Af)(o)) || void 0 === n ? void 0 : n.planId,
-  a = null != r && E.ZP.getPremiumType(r) === N.p9.TIER_2;
-u = null != e && a && e.add(10, 'days').isAfter(i()());
+  r = null === (n = (0, _.Af)(o)) || void 0 === n ? void 0 : n.planId,
+  a = null != r && _.ZP.getPremiumType(r) === N.p9.TIER_2;
+c = null != e && a && e.add(10, 'days').isAfter(i()());
   }
   if (null != s) {
-let e = null === (r = (0, E.Af)(s)) || void 0 === r ? void 0 : r.planId,
-  t = null != e && E.ZP.getPremiumType(e) === N.p9.TIER_2,
-  n = c.default.getCurrentUser(),
-  i = (null == s ? void 0 : s.trialId) != null && E.ZP.isPremiumExactly(n, N.p9.TIER_0);
+let e = null === (r = (0, _.Af)(s)) || void 0 === r ? void 0 : r.planId,
+  t = null != e && _.ZP.getPremiumType(e) === N.p9.TIER_2,
+  n = u.default.getCurrentUser(),
+  i = (null == s ? void 0 : s.trialId) != null && _.ZP.isPremiumExactly(n, N.p9.TIER_0);
 l = t || i;
   }
-  return !a || l || u;
+  return !a || l || c;
 }
 
 function C() {
   var e;
   let {
 paymentsBlocked: t
-  } = l.Z.useExperiment({
+  } = o.Z.useExperiment({
 location: '153d31_1'
   }, {
 autoTrackExposure: !1
@@ -71,63 +71,65 @@ autoTrackExposure: !1
 promotion: n
   } = (0, S.mq)(), {
 enabled: r
-  } = (0, m.ZP)(), {
+  } = (0, p.ZP)(), {
 mostRecentSubscription: i,
 premiumSubscription: s
-  } = (0, a.cj)([d.ZP], () => ({
-mostRecentSubscription: d.ZP.getMostRecentPremiumTypeSubscription(),
-premiumSubscription: d.ZP.getPremiumTypeSubscription()
-  })), u = (0, T.N)(), c = (0, I.Ng)(), _ = new Date(null !== (e = null == n ? void 0 : n.endDate) && void 0 !== e ? e : 0).valueOf(), {
-enabled: E
-  } = (0, h.Z)();
-  (0, o.Z)(_, 1000, void 0, _ + 2000 < Date.now() || E);
-  let f = !R({
+  } = (0, a.cj)([c.ZP], () => ({
+mostRecentSubscription: c.ZP.getMostRecentPremiumTypeSubscription(),
+premiumSubscription: c.ZP.getPremiumTypeSubscription()
+  })), l = (0, I.N)(), u = (0, m.Ng)(), d = new Date(null !== (e = null == n ? void 0 : n.endDate) && void 0 !== e ? e : 0).valueOf(), _ = Date.now(), E = _ > d, {
+enabled: g
+  } = (0, f.Z)();
+  (0, T.Z)({
+delay: E ? -1 : d - _,
+disable: g
+  });
+  let A = !R({
   experimentEnabled: r,
   premiumSubscription: s,
   mostRecentSubscription: i
-}) && null == u && null != n && !t && null == c,
-g = Date.now(),
+}) && null == l && null != n && !t && null == u,
 {
-  enabled: A
-} = p.Z.useExperiment({
+  enabled: N
+} = h.Z.useExperiment({
   location: '153d31_2'
 }, {
-  autoTrackExposure: f,
-  disable: !f
+  autoTrackExposure: A,
+  disable: !A
 });
-  return !(g > _) && A;
+  return !E && N;
 }
 async function y() {
   var e;
-  let t = c.default.getCurrentUser(),
+  let t = u.default.getCurrentUser(),
 n = !(null == t ? void 0 : t.isClaimed()),
 {
   paymentsBlocked: r
-} = l.Z.getCurrentConfig({
+} = o.Z.getCurrentConfig({
   location: '153d31_3'
 }, {
   autoTrackExposure: !1
 }),
 {
   enabled: i
-} = (0, m.aW)(),
+} = (0, p.aW)(),
 {
   enabled: a
-} = p.Z.getCurrentConfig({
+} = h.Z.getCurrentConfig({
   location: '153d31_4'
 }, {
   autoTrackExposure: !1
 });
   if (n || r || !i || !a)
 return !1;
-  if (_.Z.shouldFetchOffer() && !(0, E.I5)(t) && await (0, f.T)('BogoPromotionUtils'), _.Z.hasFetchedOffer() && (_.Z.hasAnyUnexpiredOffer() || _.Z.hasAnyUnexpiredDiscountOffer()))
+  if (d.Z.shouldFetchOffer() && !(0, _.I5)(t) && await (0, E.T)('BogoPromotionUtils'), d.Z.hasFetchedOffer() && (d.Z.hasAnyUnexpiredOffer() || d.Z.hasAnyUnexpiredDiscountOffer()))
 return !1;
-  !d.ZP.hasFetchedMostRecentPremiumTypeSubscription() && (null === (e = c.default.getCurrentUser()) || void 0 === e ? void 0 : e.hasHadSKU(N.Si.TIER_2)) && await (0, s.ou)(), !d.ZP.hasFetchedSubscriptions() && await (0, s.jg)();
-  let o = d.ZP.getMostRecentPremiumTypeSubscription();
+  !c.ZP.hasFetchedMostRecentPremiumTypeSubscription() && (null === (e = u.default.getCurrentUser()) || void 0 === e ? void 0 : e.hasHadSKU(N.Si.TIER_2)) && await (0, s.ou)(), !c.ZP.hasFetchedSubscriptions() && await (0, s.jg)();
+  let l = c.ZP.getMostRecentPremiumTypeSubscription();
   return !R({
 experimentEnabled: i,
-premiumSubscription: d.ZP.getPremiumTypeSubscription(),
-mostRecentSubscription: o
+premiumSubscription: c.ZP.getPremiumTypeSubscription(),
+mostRecentSubscription: l
   });
 }
 async function D() {
@@ -143,14 +145,14 @@ t = null != e && new Date(e.endDate).valueOf() >= Date.now() && new Date(e.start
 n = await y(),
 {
   enabled: r
-} = m.Am.getCurrentConfig({
+} = p.Am.getCurrentConfig({
   location: '153d31_6'
 }, {
   autoTrackExposure: !1
 }),
 {
   enabled: i
-} = p.Z.getCurrentConfig({
+} = h.Z.getCurrentConfig({
   location: '153d31_7'
 }, {
   autoTrackExposure: t && r && n
@@ -159,7 +161,7 @@ n = await y(),
 }
 
 function b() {
-  switch (u.default.locale) {
+  switch (l.default.locale) {
 case 'de':
 case 'es-ES':
 case 'fr':
