b2291f7b720c0f80bf28d3889c8e41b8c92e709e
DiscrapperManager <admin@minerpl.xyz>
2024-07-30 19:20:05 +0200

    canaryRelease: 313897
    Main scripts: 3
    Lazy Loaded Scripts: 844
    Chunks:
     * Main Files: 11097
     * Scripts: 9764
    Strings: 18480
    Routes: 672
    Experiments Count: 367

--- chunks/929498.js
+++ chunks/929498.js
@@ -1,32 +1,33 @@
 n.d(t, {
   Z: function() {
-return d;
+return _;
   }
 }), n(47120);
 var r = n(470079),
-  i = n(442837),
-  a = n(719247),
-  s = n(26033),
-  o = n(561308),
-  l = n(199902),
-  u = n(158776),
-  c = n(981631);
+  i = n(392711),
+  a = n(442837),
+  s = n(719247),
+  o = n(26033),
+  l = n(561308),
+  u = n(199902),
+  c = n(158776),
+  d = n(981631);
 
-function d(e) {
-  let t = (0, i.e7)([u.Z], () => u.Z.getActivities(e)),
-n = (0, i.e7)([a.ZP], () => a.ZP.getUserOutbox(e)),
-d = (0, i.e7)([l.Z], () => l.Z.getAnyStreamForUser(e)),
+function _(e) {
+  let t = (0, a.e7)([c.Z], () => c.Z.getActivities(e)),
+n = (0, a.e7)([s.ZP], () => s.ZP.getUserOutbox(e)),
+_ = (0, a.e7)([u.Z], () => u.Z.getAnyStreamForUser(e)),
 {
-  live: _,
-  recent: E
+  live: E,
+  recent: f
 } = (0, r.useMemo)(() => {
-  let e = t.filter(e => {
+  let e = (0, i.uniqWith)(t.filter(e => {
       let {
         type: t
       } = e;
-      return t !== c.IIU.CUSTOM_STATUS;
-    }),
-    r = null == n ? void 0 : n.entries.filter(t => (0, s.r5)(t) ? !e.some(e => null != e && (0, a.RL)(t, e)) : !(0, o.kr)(t));
+      return t !== d.IIU.CUSTOM_STATUS;
+    }), (e, t) => null != e.application_id && null != t.application_id && e.application_id === t.application_id),
+    r = null == n ? void 0 : n.entries.filter(t => (0, o.r5)(t) ? !e.some(e => null != e && (0, s.RL)(t, e)) : !(0, l.kr)(t));
   return {
     live: e,
     recent: null != r ? r : []
@@ -36,8 +37,8 @@ d = (0, i.e7)([l.Z], () => l.Z.getAnyStreamForUser(e)),
   n
 ]);
   return {
-live: _,
-recent: E,
-stream: d
+live: E,
+recent: f,
+stream: _
   };
 }
