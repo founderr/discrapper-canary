c9feb9213f8b318fb7b2a81ab40bb6cfbf5fdbaa
DiscrapperManager <admin@minerpl.xyz>
2024-08-01 20:11:55 +0200

    canaryRelease: 314852
    Main scripts: 3
    Lazy Loaded Scripts: 838
    Chunks:
     * Main Files: 11130
     * Scripts: 9756
    Strings: 18493
    Routes: 673
    Experiments Count: 372

--- chunks/316210.js
+++ chunks/316210.js
@@ -1,96 +1,98 @@
-a.r(s), a.d(s, {
+t.r(s), t.d(s, {
   default: function() {
-return T;
+return u;
   }
 });
-var t = a(735250),
-  o = a(470079),
-  n = a(481060),
-  r = a(285952),
-  c = a(63063),
-  l = a(497505),
-  i = a(918701),
-  d = a(46140),
-  S = a(981631),
-  _ = a(689938),
-  m = a(712381),
-  E = a(708901);
+var a = t(735250),
+  o = t(470079),
+  n = t(481060),
+  i = t(285952),
+  r = t(63063),
+  l = t(113434),
+  c = t(497505),
+  d = t(918701),
+  S = t(46140),
+  _ = t(981631),
+  m = t(689938),
+  E = t(712381),
+  T = t(708901);
 
-function T(e) {
+function u(e) {
   let {
 transitionState: s,
-onClose: a,
-questContent: T,
+onClose: t,
+questContent: l,
 quest: u
-  } = e, g = (0, i.zK)(u, d.S7.IN_HOUSE_CONSOLE_QUEST), x = o.useMemo(() => g ? T === l.jn.QUEST_BAR || T === l.jn.QUEST_BAR_V2 ? _.Z.Messages.QUESTS_DISCLOSURE_IN_HOUSE_QUEST_BAR : _.Z.Messages.QUESTS_DISCLOSURE_IN_HOUSE : T === l.jn.QUEST_BAR || T === l.jn.QUEST_BAR_V2 ? _.Z.Messages.QUESTS_DISCLOSURE_PUBLISHER_TARGETING.format({
-gamePublisher: g ? 'Discord' : u.config.messages.gamePublisher
-  }) : _.Z.Messages.QUESTS_DISCLOSURE_PUBLISHER_PROMOTING.format({
+  } = e, U = (0, d.zK)(u, S.S7.IN_HOUSE_CONSOLE_QUEST), x = o.useMemo(() => U ? l === c.jn.QUEST_BAR || l === c.jn.QUEST_BAR_V2 ? m.Z.Messages.QUESTS_DISCLOSURE_IN_HOUSE_QUEST_BAR : m.Z.Messages.QUESTS_DISCLOSURE_IN_HOUSE : l === c.jn.QUEST_BAR || l === c.jn.QUEST_BAR_V2 ? m.Z.Messages.QUESTS_DISCLOSURE_PUBLISHER_TARGETING.format({
+gamePublisher: U ? 'Discord' : u.config.messages.gamePublisher
+  }) : m.Z.Messages.QUESTS_DISCLOSURE_PUBLISHER_PROMOTING.format({
 gamePublisher: u.config.messages.gamePublisher,
 gameTitle: u.config.messages.gameTitle
   }), [
 u,
-T,
-g
+l,
+U
   ]);
-  return (0, t.jsxs)(n.ModalRoot, {
+  return (0, a.jsxs)(n.ModalRoot, {
 transitionState: s,
 size: n.ModalSize.DYNAMIC,
-className: m.modalRoot,
+className: E.modalRoot,
 children: [
-  (0, t.jsxs)(n.ModalHeader, {
-    direction: r.Z.Direction.VERTICAL,
+  (0, a.jsxs)(n.ModalHeader, {
+    direction: i.Z.Direction.VERTICAL,
     separator: !1,
-    className: m.modalHeader,
+    className: E.modalHeader,
     children: [
-      (0, t.jsxs)('div', {
-        className: m.modalTop,
+      (0, a.jsxs)('div', {
+        className: E.modalTop,
         children: [
-          (0, t.jsx)('div', {
-            className: m.iconFrame,
-            children: (0, t.jsx)('img', {
-              className: m.icon,
+          (0, a.jsx)('div', {
+            className: E.iconFrame,
+            children: (0, a.jsx)('img', {
+              className: E.icon,
               alt: '',
-              src: E
+              src: T
             })
           }),
-          (0, t.jsx)(n.ModalCloseButton, {
-            className: m.closeBtn,
-            onClick: a
+          (0, a.jsx)(n.ModalCloseButton, {
+            className: E.closeBtn,
+            onClick: t
           })
         ]
       }),
-      (0, t.jsx)(n.Heading, {
+      (0, a.jsx)(n.Heading, {
         variant: 'heading-xl/bold',
-        children: _.Z.Messages.QUESTS_DISCLOSURE_LABEL
+        children: m.Z.Messages.QUESTS_DISCLOSURE_LABEL
       })
     ]
   }),
-  (0, t.jsxs)(n.ModalContent, {
-    className: m.modalContent,
+  (0, a.jsxs)(n.ModalContent, {
+    className: E.modalContent,
     children: [
-      (0, t.jsx)(n.Text, {
+      (0, a.jsx)(n.Text, {
         variant: 'text-sm/medium',
         color: 'header-secondary',
-        className: m.upperBodyText,
+        className: E.upperBodyText,
         children: x
       }),
-      (0, t.jsx)(U, {
-        questContent: T
+      (0, a.jsx)(g, {
+        quest: u,
+        questContent: l
       }),
-      (0, t.jsx)(n.Text, {
+      (0, a.jsx)(n.Text, {
         variant: 'text-xs/medium',
         color: 'text-muted',
-        children: _.Z.Messages.QUESTS_DISCLOSURE_LEARN_MORE.format({
-          privacySettingsUrl: c.Z.getArticleURL(S.BhN.QUESTS_PRIVACY_CONTROLS)
+        children: m.Z.Messages.QUESTS_DISCLOSURE_LEARN_MORE.format({
+          privacySettingsUrl: r.Z.getArticleURL(_.BhN.QUESTS_PRIVACY_CONTROLS)
         })
       })
     ]
   }),
-  (0, t.jsx)(n.ModalFooter, {
-    className: m.footer,
-    children: (0, t.jsx)(n.Button, {
-      onClick: a,
-      children: _.Z.Messages.CLOSE
+  (0, a.jsx)(n.ModalFooter, {
+    className: E.footer,
+    children: (0, a.jsx)(n.Button, {
+      onClick: t,
+      children: m.Z.Messages.CLOSE
     })
   })
 ]
@@ -99,58 +101,59 @@ children: [
 
 function U(e) {
   let {
-questContent: s
+location: s,
+age: t,
+activity: o
   } = e;
-  switch (s) {
-case l.jn.QUEST_BAR:
-case l.jn.QUEST_BAR_V2:
-  return (0, t.jsxs)('ul', {
-    className: m.targetList,
-    children: [
-      (0, t.jsx)(u, {
-        icon: n.GlobeEarthIcon,
-        children: _.Z.Messages.QUESTS_DISCLOSURE_LOCATION
-      }),
-      (0, t.jsx)(u, {
-        icon: n.UserIcon,
-        children: _.Z.Messages.QUESTS_DISCLOSURE_AGE
-      }),
-      (0, t.jsx)(u, {
-        icon: n.GameControllerIcon,
-        children: _.Z.Messages.QUESTS_DISCLOSURE_ACTIVITY
-      })
-    ]
+  return (0, a.jsxs)('ul', {
+className: E.targetList,
+children: [
+  s && (0, a.jsx)(x, {
+    icon: n.GlobeEarthIcon,
+    children: m.Z.Messages.QUESTS_DISCLOSURE_LOCATION
+  }),
+  t && (0, a.jsx)(x, {
+    icon: n.UserIcon,
+    children: m.Z.Messages.QUESTS_DISCLOSURE_AGE
+  }),
+  o && (0, a.jsx)(x, {
+    icon: n.GameControllerIcon,
+    children: m.Z.Messages.QUESTS_DISCLOSURE_ACTIVITY
+  })
+]
   });
-case l.jn.ACTIVITY_PANEL:
-case l.jn.QUESTS_EMBED:
-case l.jn.QUEST_LIVE_STREAM:
-  return (0, t.jsx)('ul', {
-    className: m.targetList,
-    children: (0, t.jsx)(u, {
-      icon: n.GameControllerIcon,
-      children: _.Z.Messages.QUESTS_DISCLOSURE_ACTIVITY
-    })
-  });
-default:
-  return null;
-  }
 }
 
-function u(e) {
+function g(e) {
+  var s;
+  let {
+questContent: t,
+quest: o
+  } = e, n = (0, d.Mi)(o, c.jn.GIFT_INVENTORY_FOR_YOU), i = (0, l.tP)(o), r = (null === (s = o.userStatus) || void 0 === s ? void 0 : s.claimedAt) != null;
+  return t !== c.jn.QUEST_BAR && t !== c.jn.QUEST_BAR_V2 && (t !== c.jn.QUEST_HOME_DESKTOP || !n || i || r) ? t === c.jn.ACTIVITY_PANEL || t === c.jn.QUESTS_EMBED || t === c.jn.QUEST_LIVE_STREAM ? (0, a.jsx)(U, {
+activity: !0
+  }) : null : (0, a.jsx)(U, {
+location: !0,
+age: !0,
+activity: !0
+  });
+}
+
+function x(e) {
   let {
 icon: s,
-children: a
+children: t
   } = e;
-  return (0, t.jsxs)(n.Text, {
+  return (0, a.jsxs)(n.Text, {
 variant: 'text-sm/medium',
 tag: 'li',
-className: m.targetItem,
+className: E.targetItem,
 children: [
-  (0, t.jsx)(s, {
+  (0, a.jsx)(s, {
     size: 'xs',
     color: 'currentColor'
   }),
-  a
+  t
 ]
   });
 }
