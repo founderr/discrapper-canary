a1d2f07cfc35f8ff93c1a734d9b4b8dfacc839ed
DiscrapperManager <admin@minerpl.xyz>
2024-08-02 20:43:57 +0200

    canaryRelease: 315234
    Main scripts: 3
    Lazy Loaded Scripts: 840
    Chunks:
     * Main Files: 11135
     * Scripts: 9769
    Strings: 18511
    Routes: 673
    Experiments Count: 373

--- chunks/207874.js
+++ chunks/207874.js
@@ -1,48 +1,52 @@
 n.d(t, {
   Z: function() {
-return c;
+return h;
   }
 });
 var i = n(735250),
   s = n(470079),
-  l = n(461745),
-  r = n(5967),
-  o = n(996733),
-  a = n(689938);
+  l = n(442837),
+  r = n(461745),
+  o = n(663389),
+  a = n(5967),
+  c = n(996733),
+  u = n(981631),
+  d = n(689938);
 
-function c(e) {
+function h(e) {
   let {
 style: t
-  } = e, n = (0, o.Z7)(), c = s.useCallback((e, t) => {
-t.stopPropagation(), t.preventDefault(), (0, o.QH)(e);
-  }, []), u = s.useCallback(() => {
-(0, o.QH)(!1), (0, o.yN)('');
-  }, []), d = s.useRef(null);
+  } = e, n = (0, c.Z7)(), h = s.useCallback((e, t) => {
+t.stopPropagation(), t.preventDefault(), (0, c.QH)(e);
+  }, []), m = s.useCallback(() => {
+(0, c.QH)(!1), (0, c.yN)('');
+  }, []), p = s.useRef(null), f = (0, l.e7)([o.Z], () => o.Z.getSection());
   return s.useEffect(() => {
 let e = e => {
   var t;
-  let n = d.current,
-    i = null === (t = (0, r.uB)(e)) || void 0 === t ? void 0 : t.activeElement;
-  !(i !== d.current && (0, r.VG)(i)) && (null == n || n.focus());
+  let n = p.current,
+    i = null === (t = (0, a.uB)(e)) || void 0 === t ? void 0 : t.activeElement;
+  if (!(i !== p.current && (0, a.VG)(i)))
+    f !== u.oAB.PROFILE_CUSTOMIZATION && (null == n || n.focus());
 };
 return window.addEventListener('keydown', e), () => window.removeEventListener('keydown', e);
-  }, []), (0, i.jsx)('div', {
+  }, [f]), (0, i.jsx)('div', {
 style: {
   marginBottom: '8px'
 },
-children: (0, i.jsx)(l.ZP, {
-  ref: d,
-  size: l.ZP.Sizes.MEDIUM,
+children: (0, i.jsx)(r.ZP, {
+  ref: p,
+  size: r.ZP.Sizes.MEDIUM,
   query: n,
-  onClear: u,
-  onQueryChange: o.yN,
-  placeholder: a.Z.Messages.SEARCH,
+  onClear: m,
+  onQueryChange: c.yN,
+  placeholder: d.Z.Messages.SEARCH,
   className: t,
   inputProps: {
-    'aria-label': a.Z.Messages.USER_SETTINGS_SEARCH_BAR,
+    'aria-label': d.Z.Messages.USER_SETTINGS_SEARCH_BAR,
     'aria-expanded': !0,
-    onFocus: e => c(!0, e),
-    onBlur: e => c(!1, e)
+    onFocus: e => h(!0, e),
+    onBlur: e => h(!1, e)
   }
 })
   });
