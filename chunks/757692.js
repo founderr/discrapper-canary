a5b0d4a0c574b90be3e854e4f12d5bfd7c01d26b
DiscrapperManager <admin@minerpl.xyz>
2024-08-06 00:54:42 +0200

    canaryRelease: 315772
    Main scripts: 3
    Lazy Loaded Scripts: 841
    Chunks:
     * Main Files: 11146
     * Scripts: 9778
    Strings: 18539
    Routes: 673
    Experiments Count: 376

--- chunks/757692.js
+++ chunks/757692.js
@@ -8,6 +8,6 @@ var i = n(451467),
   s = n(37113);
 
 function l(e, t) {
-  var n;
-  return null != e && e.type === a.fO.STREAM && null != e.maxResolution && null != e.maxFrameRate && (0, i.Z)(s.tI.PRESET_VIDEO, (0, s.aW)(null == e ? void 0 : null === (n = e.maxResolution) || void 0 === n ? void 0 : n.height), (0, s.L9)(e.maxFrameRate), t);
+  var n, l;
+  return null != e && e.type === a.fO.STREAM && null != e.maxResolution && null != e.maxFrameRate && (null == e ? void 0 : null === (n = e.maxResolution) || void 0 === n ? void 0 : n.height) in s.LY && e.maxFrameRate in s.ws && (0, i.Z)(s.tI.PRESET_VIDEO, (0, s.aW)(null == e ? void 0 : null === (l = e.maxResolution) || void 0 === l ? void 0 : l.height), (0, s.L9)(e.maxFrameRate), t);
 }
