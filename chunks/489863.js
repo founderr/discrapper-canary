1562b6d161459b28eb38c09a58cf394bd6dd375d
DiscrapperManager <admin@minerpl.xyz>
2024-08-07 09:29:39 +0200

    canaryRelease: 316286
    Main scripts: 3
    Lazy Loaded Scripts: 844
    Chunks:
     * Main Files: 11145
     * Scripts: 9799
    Strings: 18586
    Routes: 673
    Experiments Count: 376

--- chunks/489863.js
+++ chunks/489863.js
@@ -1,114 +1,102 @@
 n.d(t, {
   Iq: function() {
-return c;
+return l;
   },
   UR: function() {
-return _;
+return c;
   },
   Ww: function() {
-return d;
-  },
-  c$: function() {
-return E;
-  },
-  g: function() {
 return u;
   },
+  c$: function() {
+return d;
+  },
+  g: function() {
+return o;
+  },
   i2: function() {
-return p;
+return f;
   },
   tR: function() {
-return h;
+return E;
   },
   tV: function() {
-return f;
+return _;
   }
 });
 var r = n(860911),
   i = n(544891),
   a = n(893776),
-  s = n(592125),
-  o = n(944486),
-  l = n(981631);
+  s = n(981631);
 
-function u(e) {
+function o(e) {
   return i.tn.post({
-url: l.ANM.OAUTH2_WHITELIST_ACCEPT,
+url: s.ANM.OAUTH2_WHITELIST_ACCEPT,
 query: {
   token: e
 },
 oldFormErrors: !0
   });
 }
-async function c(e) {
+async function l(e) {
   let {
 authorize: t,
 clientId: n,
 scopes: r,
 responseType: a,
-redirectUri: u,
-codeChallenge: c,
-codeChallengeMethod: d,
-state: _,
-permissions: E,
-guildId: f,
-channelId: h,
-integrationType: p,
-nonce: m
+redirectUri: o,
+codeChallenge: l,
+codeChallengeMethod: u,
+state: c,
+permissions: d,
+guildId: _,
+channelId: E,
+integrationType: f,
+nonce: h
   } = e;
   return (await i.tn.post({
-url: l.ANM.OAUTH2_AUTHORIZE,
+url: s.ANM.OAUTH2_AUTHORIZE,
 query: {
   client_id: n,
   response_type: a,
-  redirect_uri: u,
-  code_challenge: c,
-  code_challenge_method: d,
+  redirect_uri: o,
+  code_challenge: l,
+  code_challenge_method: u,
   scope: r.join(' '),
-  state: _,
-  nonce: m
+  state: c,
+  nonce: h
 },
 body: {
-  guild_id: f,
-  webhook_channel_id: null != f && null != h ? h : void 0,
-  channel_id: null == f && null != h ? h : void 0,
-  permissions: E,
+  guild_id: _,
+  webhook_channel_id: null != _ && null != E ? E : void 0,
+  channel_id: null == _ && null != E ? E : void 0,
+  permissions: d,
   authorize: t,
-  integration_type: p,
-  location_context: function() {
-    var e;
-    let t = o.Z.getChannelId(),
-      n = s.Z.getChannel(t);
-    return {
-      guild_id: null == n ? void 0 : n.guild_id,
-      channel_id: t,
-      channel_type: null !== (e = null == n ? void 0 : n.type) && void 0 !== e ? e : l.d4z.UNKNOWN
-    };
-  }()
+  integration_type: f
 },
 oldFormErrors: !0
   })).body;
 }
-async function d(e) {
+async function u(e) {
   let {
 clientId: t,
 scopes: n,
 responseType: r,
 redirectUri: a,
-codeChallenge: s,
-codeChallengeMethod: o,
+codeChallenge: o,
+codeChallengeMethod: l,
 state: u,
 integrationType: c,
 nonce: d
   } = e;
   return (await i.tn.get({
-url: l.ANM.OAUTH2_AUTHORIZE,
+url: s.ANM.OAUTH2_AUTHORIZE,
 query: {
   client_id: t,
   response_type: r,
   redirect_uri: a,
-  code_challenge: s,
-  code_challenge_method: o,
+  code_challenge: o,
+  code_challenge_method: l,
   scope: n.join(' '),
   state: u,
   integration_type: c,
@@ -118,11 +106,11 @@ retries: 3,
 oldFormErrors: !0
   })).body;
 }
-async function _(e) {
+async function c(e) {
   let {
 body: t
   } = await i.tn.get({
-url: l.ANM.OAUTH2_AUTHORIZE_WEBHOOK_CHANNELS,
+url: s.ANM.OAUTH2_AUTHORIZE_WEBHOOK_CHANNELS,
 query: {
   guild_id: e
 },
@@ -131,29 +119,29 @@ oldFormErrors: !0
   return t;
 }
 
-function E(e) {
+function d(e) {
   a.Z.logout((0, r.U)(e.pathname + e.search, !1));
 }
-async function f(e) {
+async function _(e) {
   return await i.tn.post({
-url: l.ANM.OAUTH2_DEVICE_VERIFY,
+url: s.ANM.OAUTH2_DEVICE_VERIFY,
 body: {
   user_code: e
 }
   });
 }
-async function h(e, t) {
+async function E(e, t) {
   return await i.tn.post({
-url: l.ANM.OAUTH2_DEVICE_FINISH,
+url: s.ANM.OAUTH2_DEVICE_FINISH,
 body: {
   user_code: e,
   result: t
 }
   });
 }
-async function p(e, t, n) {
+async function f(e, t, n) {
   return await i.tn.post({
-url: l.ANM.OAUTH2_DEVICE_FINISH,
+url: s.ANM.OAUTH2_DEVICE_FINISH,
 body: {
   user_code: e,
   result: 'two_way_link_error',
