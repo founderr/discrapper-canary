413cae25afb9e0eb48ae7bee17d11cdcbdf8071a
DiscrapperManager <admin@minerpl.xyz>
2024-08-09 18:24:23 +0200

    canaryRelease: 317129
    Main scripts: 3
    Lazy Loaded Scripts: 844
    Chunks:
     * Main Files: 11172
     * Scripts: 9807
    Strings: 18616
    Routes: 674
    Experiments Count: 378

--- chunks/615830.js
+++ chunks/615830.js
@@ -10,36 +10,38 @@ configurable: !0,
 writable: !0
   }) : e[t] = n, e;
 }
-let o = !1,
-  l = [];
-class u extends(r = i.ZP.PersistedStore) {
+let o = [],
+  l = !1,
+  u = o;
+class c extends(r = i.ZP.PersistedStore) {
   initialize(e) {
-null != e && (o = e.persistentCodesEnabled, l = e.uploadedKeyVersions);
+var t, n;
+l = null !== (t = null == e ? void 0 : e.persistentCodesEnabled) && void 0 !== t && t, u = null !== (n = null == e ? void 0 : e.uploadedKeyVersions) && void 0 !== n ? n : o;
   }
   getState() {
 return {
-  persistentCodesEnabled: o,
-  uploadedKeyVersions: l
+  persistentCodesEnabled: l,
+  uploadedKeyVersions: u
 };
   }
   getPersistentCodesEnabled() {
-return o;
-  }
-  getUploadedKeyVersionsCached() {
 return l;
   }
+  getUploadedKeyVersionsCached() {
+return u;
+  }
 }
-s(u, 'displayName', 'SecureFramesPersistedStore'), s(u, 'persistKey', 'SecureFramesPersistedStore'), t.Z = new u(a.Z, {
+s(c, 'displayName', 'SecureFramesPersistedStore'), s(c, 'persistKey', 'SecureFramesPersistedStore'), t.Z = new c(a.Z, {
   SECURE_FRAMES_SETTINGS_UPDATE: function(e) {
-o = e.persistentCodesEnabled;
+l = e.persistentCodesEnabled;
   },
   SECURE_FRAMES_UPLOAD_PUBLIC_KEY_SUCCESS: function(e) {
 let t = [];
-for (let n of l) {
+for (let n of u) {
   if (n === e.keyVersion)
     return;
   t.push(n);
 }
-t.push(e.keyVersion), l = t;
+t.push(e.keyVersion), u = t;
   }
 });
