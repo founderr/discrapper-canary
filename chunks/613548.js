19171509a52f2b96303ab1514de970ac957707d4
DiscrapperManager <admin@minerpl.xyz>
2024-08-09 22:48:42 +0200

    canaryRelease: 317280
    Main scripts: 3
    Lazy Loaded Scripts: 845
    Chunks:
     * Main Files: 11171
     * Scripts: 9810
    Strings: 18627
    Routes: 674
    Experiments Count: 378

--- chunks/613548.js
+++ chunks/613548.js
@@ -1,6 +1,6 @@
 n.d(t, {
   Z: function() {
-return F;
+return z;
   }
 }), n(653041);
 var i = n(735250),
@@ -21,48 +21,50 @@ var i = n(735250),
   g = n(6242),
   C = n(757692),
   I = n(141321),
-  x = n(44136),
-  T = n(651183),
-  N = n(618158),
-  v = n(390322),
-  S = n(616286),
-  Z = n(961048),
-  A = n(179424),
-  M = n(786915),
-  b = n(975146),
-  R = n(991617),
-  j = n(597998),
-  L = n(606304),
-  P = n(594174),
-  O = n(74538),
-  y = n(358221),
-  D = n(887012),
-  k = n(444253),
-  U = n(355827),
-  w = n(354459),
-  B = n(981631),
-  H = n(689938),
-  G = n(867358);
+  x = n(121254),
+  T = n(954551),
+  N = n(44136),
+  v = n(651183),
+  S = n(618158),
+  Z = n(390322),
+  A = n(616286),
+  M = n(961048),
+  b = n(179424),
+  R = n(786915),
+  j = n(975146),
+  L = n(991617),
+  P = n(597998),
+  O = n(606304),
+  y = n(594174),
+  D = n(74538),
+  k = n(358221),
+  U = n(887012),
+  w = n(444253),
+  B = n(355827),
+  H = n(354459),
+  G = n(981631),
+  V = n(689938),
+  F = n(867358);
 
-function V(e) {
+function W(e) {
   let {
 channelId: t,
 guildId: n
   } = e, a = Date.now(), s = (0, u.e7)([
-L.Z,
-y.Z
-  ], () => o()(L.Z.getSpeakers()).map(e => y.Z.getParticipant(t, e)).filter(e => null != e && e.type === w.fO.USER && e.speaking && !(0, x.ZP)(e)).sortBy(e => -L.Z.getSpeakingDuration(e.user.id, a)).slice(0, 3).value());
+O.Z,
+k.Z
+  ], () => o()(O.Z.getSpeakers()).map(e => k.Z.getParticipant(t, e)).filter(e => null != e && e.type === H.fO.USER && e.speaking && !(0, N.ZP)(e)).sortBy(e => -O.Z.getSpeakingDuration(e.user.id, a)).slice(0, 3).value());
   return 0 === s.length ? null : (0, i.jsx)(i.Fragment, {
 children: s.map((e, t) => (0, i.jsx)(d.Tooltip, {
   position: 'bottom',
   color: d.Tooltip.Colors.GREY,
-  text: H.Z.Messages.CHANNEL_CALL_CURRENT_SPEAKER.format({
+  text: V.Z.Messages.CHANNEL_CALL_CURRENT_SPEAKER.format({
     username: e.user.username
   }),
-  children: a => (0, i.jsx)(j.Z, {
+  children: a => (0, i.jsx)(P.Z, {
     ...a,
-    className: l()(G.speaker, {
-      [G.last]: t === s.length - 1
+    className: l()(F.speaker, {
+      [F.last]: t === s.length - 1
     }),
     user: e.user,
     speaking: !0,
@@ -73,7 +75,7 @@ children: s.map((e, t) => (0, i.jsx)(d.Tooltip, {
   });
 }
 
-function F(e) {
+function z(e) {
   let {
 inPopout: t,
 channel: n,
@@ -81,49 +83,55 @@ appContext: s,
 inCall: l,
 isChatOpen: r,
 exitFullScreen: o
-  } = e, x = n.id, {
-voiceParticipantsHidden: j,
-selectedParticipant: L,
-userParticipantCount: H
-  } = (0, u.cj)([y.Z], () => ({
-selectedParticipant: y.Z.getSelectedParticipant(x),
-voiceParticipantsHidden: y.Z.getVoiceParticipantsHidden(x),
-userParticipantCount: y.Z.getUserParticipantCount(x)
-  }), [x]), F = (0, u.e7)([P.default], () => P.default.getCurrentUser()), W = (0, D.Z)(n), z = (0, O.So)(c.q.STREAM_HIGH_QUALITY), Y = (0, C.o)(L, F), K = (0, g.lL)('CallHeader', !0, F, Y), {
-preventIdle: q,
-allowIdle: X
-  } = (0, N.Y)('popup'), Q = [];
-  if (W) {
-let e = (null == L ? void 0 : L.type) === w.fO.STREAM ? (0, _.my)(L.id) : void 0,
-  t = (null == L ? void 0 : L.type) === w.fO.ACTIVITY ? L.id : void 0;
-Q.push((0, i.jsx)(M.Z, {
-  className: G.button,
+  } = e, N = n.id, {
+voiceParticipantsHidden: P,
+selectedParticipant: O,
+userParticipantCount: V
+  } = (0, u.cj)([k.Z], () => ({
+selectedParticipant: k.Z.getSelectedParticipant(N),
+voiceParticipantsHidden: k.Z.getVoiceParticipantsHidden(N),
+userParticipantCount: k.Z.getUserParticipantCount(N)
+  }), [N]), z = (0, u.e7)([y.default], () => y.default.getCurrentUser()), Y = (0, U.Z)(n), K = (0, D.So)(c.q.STREAM_HIGH_QUALITY), q = (0, C.o)(O, z), X = (0, g.lL)('CallHeader', !0, z, q), {
+enabled: Q
+  } = x.Z.useExperiment({
+location: 'ChannelCallHeaderToolbar'
+  }, {
+autoTrackExposure: !1
+  }), {
+preventIdle: J,
+allowIdle: $
+  } = (0, S.Y)('popup'), ee = [];
+  if (Y) {
+let e = (null == O ? void 0 : O.type) === H.fO.STREAM ? (0, _.my)(O.id) : void 0,
+  t = (null == O ? void 0 : O.type) === H.fO.ACTIVITY ? O.id : void 0;
+ee.push((0, i.jsx)(R.Z, {
+  className: F.button,
   channel: n,
   applicationId: t,
   stream: e,
   exitFullScreen: o,
-  analyticsLocation: n.type === B.d4z.GUILD_VOICE ? B.ZY5.GUILD_CHANNEL : B.ZY5.DM_CHANNEL
+  analyticsLocation: n.type === G.d4z.GUILD_VOICE ? G.ZY5.GUILD_CHANNEL : G.ZY5.DM_CHANNEL
 }, 'invite-button'));
   }
-  return j && Q.push((0, i.jsx)(V, {
-channelId: x,
+  return P && ee.push((0, i.jsx)(W, {
+channelId: N,
 guildId: n.guild_id
-  }, 'current-speaker')), Q.push((0, i.jsx)(m.Z, {
-className: G.button,
-channelId: x
-  }, 'clips-enabled-indicator')), (null == L ? void 0 : L.type) === w.fO.STREAM && (Q.push((0, i.jsx)(T.Z, {
-className: G.button,
-participant: L
-  }, 'warning')), Q.push((0, i.jsx)(f.Z, {
+  }, 'current-speaker')), ee.push((0, i.jsx)(m.Z, {
+className: F.button,
+channelId: N
+  }, 'clips-enabled-indicator')), (null == O ? void 0 : O.type) === H.fO.STREAM && (ee.push((0, i.jsx)(v.Z, {
+className: F.button,
+participant: O
+  }, 'warning')), ee.push((0, i.jsx)(f.Z, {
 size: h.ZP.Sizes.LARGE,
-className: G.button,
-participant: L,
+className: F.button,
+participant: O,
 showQuality: !0,
-premiumIndicator: z || K.enabled
-  }, 'live-indicator'))), j && Q.push((0, i.jsx)(d.Popout, {
+premiumIndicator: K || X.enabled
+  }, 'live-indicator'))), P && ee.push((0, i.jsx)(d.Popout, {
 position: 'bottom',
-renderPopout: () => (0, i.jsx)(v.Z, {
-  children: (0, i.jsx)(U.Z, {
+renderPopout: () => (0, i.jsx)(Z.Z, {
+  children: (0, i.jsx)(B.Z, {
     channel: n
   })
 }),
@@ -131,36 +139,38 @@ children: (e, t) => {
   let {
     isShown: n
   } = t;
-  return (0, a.createElement)(b.Z, {
+  return (0, a.createElement)(j.Z, {
     ...e,
     isActive: n,
-    count: H,
+    count: V,
     key: 'call-members',
-    className: G.button
+    className: F.button
   });
 }
-  }, 'call-members-popout')), l && Q.push(null != L ? (0, i.jsx)(A.Z, {
-className: G.button,
-channelId: x
-  }, 'deselect-participant') : (0, i.jsx)(R.Z, {
-className: G.button,
-channelId: x,
+  }, 'call-members-popout')), l && ee.push(null != O ? (0, i.jsx)(b.Z, {
+className: F.button,
+channelId: N
+  }, 'deselect-participant') : (0, i.jsx)(L.Z, {
+className: F.button,
+channelId: N,
 isHorizontal: !0
-  }, 'select-participant')), !t && Q.push((0, i.jsx)(p.Z, {
-className: G.button
-  }, 'clips')), Q.push((0, i.jsx)(I.Z, {
-onOpen: q,
-onClose: X,
-className: G.button
-  }, 'recents')), Q.push((0, i.jsx)(d.Popout, {
+  }, 'select-participant')), !t && ee.push((0, i.jsx)(p.Z, {
+className: F.button
+  }, 'clips')), ee.push((0, i.jsx)(I.Z, {
+onOpen: J,
+onClose: $,
+className: F.button
+  }, 'recents')), Q && ee.push((0, i.jsx)(T.Z, {
+className: F.button
+  }, 'for-later')), ee.push((0, i.jsx)(d.Popout, {
 position: 'bottom',
 renderPopout: e => {
   let {
     closePopout: t
   } = e;
-  return (0, i.jsx)(v.Z, {
-    children: (0, i.jsx)(k.Z, {
-      channelId: x,
+  return (0, i.jsx)(Z.Z, {
+    children: (0, i.jsx)(w.Z, {
+      channelId: N,
       onClose: t,
       appContext: s,
       exitFullScreen: o
@@ -171,20 +181,20 @@ children: (e, t) => {
   let {
     isShown: n
   } = t;
-  return (0, a.createElement)(Z.Z, {
+  return (0, a.createElement)(M.Z, {
     ...e,
     key: 'more-options',
     isActive: n,
-    className: G.lastButton
+    className: F.lastButton
   });
 }
-  }, 'more-options-popout')), !r && (Q.push((0, i.jsx)(E.ZP.Divider, {
-className: G.divider
-  }, 'divider')), Q.push((0, i.jsx)(S.T, {
+  }, 'more-options-popout')), !r && (ee.push((0, i.jsx)(E.ZP.Divider, {
+className: F.divider
+  }, 'divider')), ee.push((0, i.jsx)(A.T, {
 channelId: n.id,
-className: G.chatButtonSpacer,
+className: F.chatButtonSpacer,
 disabled: !0
   }, 'chat-spacer'))), (0, i.jsx)(i.Fragment, {
-children: Q
+children: ee
   });
 }
