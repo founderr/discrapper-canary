032b2394487b597f433b380af7c566deac0f0c91
DiscrapperManager <admin@minerpl.xyz>
2024-08-12 20:04:49 +0000

    canaryRelease: 317627
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 11181
     * Scripts: 9817
    Strings: 18641
    Routes: 676
    Experiments Count: 378

--- chunks/2660.js
+++ chunks/2660.js
@@ -5,28 +5,35 @@ var a = n(442837),
   r = n(204418),
   l = n(594174),
   o = n(113434),
-  c = n(182294),
-  u = n(267423);
-t.Z = e => {
-  let {
-questConfig: t,
-fallback: n,
-isFocused: d
-  } = e, _ = (0, a.e7)([l.default], () => l.default.getCurrentUser()), {
-avatarDecoration: E,
-isFetching: I
-  } = (0, o.DU)(t);
-  return I ? (0, i.jsx)('div', {
-className: u.questsCollectibleReward,
-children: (0, i.jsx)(s.Spinner, {})
-  }) : null == _ || null == E ? n : (0, i.jsx)('div', {
-className: u.questsCollectibleReward,
-children: (0, i.jsx)(r.Z, {
-  avatarSize: c.EF.SIZE_56,
-  user: _,
-  guildId: null,
-  avatarDecorationOverride: E,
-  animateOnHover: !d
-})
-  });
-};
+  c = n(46140),
+  u = n(182294),
+  d = n(267423);
+let _ = () => (0, i.jsx)('div', {
+className: d.questsCollectibleReward
+  }),
+  E = e => {
+let {
+  questConfig: t,
+  fallback: n,
+  isFocused: c
+} = e, _ = (0, a.e7)([l.default], () => l.default.getCurrentUser()), {
+  avatarDecoration: E,
+  isFetching: I
+} = (0, o.DU)(t);
+return I ? (0, i.jsx)('div', {
+  className: d.questsCollectibleReward,
+  children: (0, i.jsx)(s.Spinner, {})
+}) : null == _ || null == E ? n : (0, i.jsx)('div', {
+  className: d.questsCollectibleReward,
+  children: (0, i.jsx)(r.Z, {
+    avatarSize: u.EF.SIZE_56,
+    user: _,
+    guildId: null,
+    avatarDecorationOverride: E,
+    animateOnHover: !c
+  })
+});
+  };
+t.Z = e => e.questConfig.features.includes(c.S7.FRACTIONS_QUEST) ? (0, i.jsx)(_, {}) : (0, i.jsx)(E, {
+  ...e
+});
