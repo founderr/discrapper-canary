228f17112c182d32d2630080044b3612eba4cb39
DiscrapperManager <admin@minerpl.xyz>
2024-08-12 22:11:15 +0000

    canaryRelease: 317729
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 11182
     * Scripts: 9818
    Strings: 18640
    Routes: 676
    Experiments Count: 377

--- chunks/892814.js
+++ chunks/892814.js
@@ -3,4 +3,4 @@ n.d(t, {
   I: function() {
 return i;
   }
-}), (a = i || (i = {})).APP_LAUNCHER_TEXT = 'app_launcher_text', a.CONTEXTUAL_IMAGE = 'contextual_image';
+}), (a = i || (i = {})).APP_LAUNCHER_TEXT = 'app_launcher_text', a.APP_LAUNCHER_VOICE = 'app_launcher_voice', a.CONTEXTUAL_IMAGE = 'contextual_image';
