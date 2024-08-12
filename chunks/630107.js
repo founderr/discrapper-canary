105eb2e67c295d338224bdc9504f70f392bd03ec
DiscrapperManager <admin@minerpl.xyz>
2024-08-12 21:14:27 +0000

    canaryRelease: 317683
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 11182
     * Scripts: 9818
    Strings: 18641
    Routes: 676
    Experiments Count: 377

--- chunks/630107.js
+++ chunks/630107.js
@@ -1,6 +1,6 @@
 t.r(n), t.d(n, {
   default: function() {
-return h;
+return A;
   }
 });
 var a = t(735250),
@@ -8,76 +8,98 @@ var a = t(735250),
   i = t(873546),
   r = t(442837),
   l = t(481060),
-  o = t(830064),
-  c = t(388905),
-  u = t(625128),
-  d = t(362762),
-  p = t(703656),
-  m = t(981631),
-  x = t(186901),
-  b = t(689938),
-  _ = t(521934),
-  N = t(821744);
+  o = t(893776),
+  c = t(830064),
+  u = t(388905),
+  d = t(625128),
+  p = t(362762),
+  m = t(353926),
+  x = t(703656),
+  b = t(314897),
+  _ = t(709158),
+  E = t(46140),
+  N = t(981631),
+  f = t(186901),
+  g = t(689938),
+  h = t(521934),
+  I = t(821744);
 r.ZP.initialize();
-let E = i.tq || i.Em,
-  g = 'inventory';
+let j = i.tq || i.Em,
+  v = 'inventory';
 
-function h() {
-  let e = (0, r.e7)([d.Z], () => d.Z.getState('quests'));
+function A(e) {
+  let {
+match: n
+  } = e, t = (0, r.e7)([p.Z], () => p.Z.getState('quests')), i = (0, r.e7)([m.Z], () => m.Z.hasLoadedExperiments), A = n.params.questId, T = (0, _.Z)(E.dr.CODED_LINK), O = (0, r.e7)([b.default], () => b.default.isAuthenticated());
   if (s.useEffect(() => {
-  if (!E)
-    null == e ? u.Z.openNativeAppModal('quests', m.Etm.DEEP_LINK, {
-      type: x.jE.USER_SETTINGS,
+  O && !i && o.Z.getExperiments();
+}, [
+  O,
+  i
+]), s.useEffect(() => {
+  if (!j)
+    null == t && i ? T ? d.Z.openNativeAppModal('quests', N.Etm.DEEP_LINK, {
+      type: f.jE.QUEST_HOME,
       params: {
-        section: g
+        questId: A
       }
-    }) : e === m.kEZ.OPEN_FAIL && (0, p.dL)(m.Z5c.SETTINGS(g));
-}, [e]), E)
+    }) : d.Z.openNativeAppModal('quests', N.Etm.DEEP_LINK, {
+      type: f.jE.USER_SETTINGS,
+      params: {
+        section: v
+      }
+    }) : t === N.kEZ.OPEN_FAIL && (T ? (0, x.dL)(N.Z5c.QUEST_HOME) : (0, x.dL)(N.Z5c.SETTINGS(v)));
+}, [
+  t,
+  i,
+  T,
+  A
+]), j)
 return (0, a.jsxs)('div', {
-  className: _.mobileWebContainer,
+  className: h.mobileWebContainer,
   children: [
     (0, a.jsx)('img', {
-      src: N,
+      src: I,
       alt: '',
-      className: _.mobileWebImage
+      className: h.mobileWebImage
     }),
     (0, a.jsx)(l.Heading, {
       variant: 'display-lg',
       color: 'text-brand',
-      children: b.Z.Messages.QUESTS_LANDING_PAGE_ERROR_HEADING
+      children: g.Z.Messages.QUESTS_LANDING_PAGE_ERROR_HEADING
     }),
     (0, a.jsx)(l.Text, {
       variant: 'text-md/normal',
-      className: _.mobileWebCopy,
-      children: b.Z.Messages.QUESTS_LANDING_PAGE_MOBILE_WEB_BODY_DESKTOP_AND_MOBILE
+      className: h.mobileWebCopy,
+      children: g.Z.Messages.QUESTS_LANDING_PAGE_MOBILE_WEB_BODY_DESKTOP_AND_MOBILE
     })
   ]
 });
-  let n = null == e || e === m.kEZ.OPENING || e === m.kEZ.OPEN_FAIL;
+  let L = null == t || t === N.kEZ.OPENING || t === N.kEZ.OPEN_FAIL;
   return (0, a.jsxs)(a.Fragment, {
 children: [
-  (0, a.jsx)(o.Z, {
-    className: _.backgroundArtwork,
+  (0, a.jsx)(c.Z, {
+    className: h.backgroundArtwork,
     preserveAspectRatio: 'xMinYMin slice'
   }),
   (0, a.jsx)('div', {
-    className: _.container,
-    children: (0, a.jsx)(c.ZP, {
-      children: n ? (0, a.jsxs)(a.Fragment, {
+    className: h.container,
+    children: (0, a.jsx)(u.ZP, {
+      children: L ? (0, a.jsxs)(a.Fragment, {
         children: [
-          (0, a.jsx)(c.Dx, {
-            children: b.Z.Messages.APP_OPENING
+          (0, a.jsx)(u.Dx, {
+            children: g.Z.Messages.APP_OPENING
           }),
-          (0, a.jsx)(c.Hh, {})
+          (0, a.jsx)(u.Hh, {})
         ]
       }) : (0, a.jsxs)(a.Fragment, {
         children: [
-          (0, a.jsx)(c.Dx, {
-            className: _.appOpenedTitle,
-            children: b.Z.Messages.APP_OPENED_TITLE
+          (0, a.jsx)(u.Dx, {
+            className: h.appOpenedTitle,
+            children: g.Z.Messages.APP_OPENED_TITLE
           }),
-          (0, a.jsx)(c.DK, {
-            children: b.Z.Messages.DEEPLINK_BROWSER_APP_OPENED
+          (0, a.jsx)(u.DK, {
+            children: g.Z.Messages.DEEPLINK_BROWSER_APP_OPENED
           })
         ]
       })
