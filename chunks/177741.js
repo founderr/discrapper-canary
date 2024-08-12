c78bc95807a6c8a04cc16afb0919c0b1533dc36a
DiscrapperManager <admin@minerpl.xyz>
2024-08-12 20:27:28 +0000

    canaryRelease: 317644
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 11181
     * Scripts: 9818
    Strings: 18641
    Routes: 676
    Experiments Count: 377

--- chunks/177741.js
+++ chunks/177741.js
@@ -7,48 +7,47 @@ return a;
 return v;
   }
 }), i(315314), i(610138), i(216116), i(78328), i(815648), i(47120);
-var a, o, r = i(735250),
-  s = i(470079),
-  c = i(120356),
-  u = i.n(c),
+var a, o, c = i(735250),
+  r = i(470079),
+  s = i(120356),
+  u = i.n(s),
   d = i(338545),
-  m = i(442837),
-  l = i(481060),
-  p = i(570140),
-  M = i(607070),
-  w = i(70097),
+  p = i(442837),
+  M = i(481060),
+  m = i(570140),
+  w = i(607070),
+  l = i(70097),
   _ = i(626135),
   h = i(526167),
-  W = i(585483),
-  E = i(138464),
-  g = i(438820),
-  T = i(831474),
-  f = i(631274),
+  E = i(585483),
+  W = i(138464),
+  T = i(438820),
+  I = i(631274),
   S = i(194077),
-  I = i(724688),
-  b = i(981631),
+  g = i(724688),
+  f = i(981631),
   P = i(238115),
   y = i(422138),
-  x = i(113386),
-  C = i(48547);
+  b = i(113386),
+  x = i(48547);
 async function v(e) {
 try {
-  p.Z.dispatch({
+  m.Z.dispatch({
     type: 'PURCHASED_ITEMS_FESTIVITY_IS_FETCHING_WOW_MOMENT_MEDIA',
     value: !0
   });
-  let t = e ? f : y;
-  window.matchMedia('(min-width: 1012px) and (max-width: 1980px)').matches || window.matchMedia('(min-height: 720px) and (max-height: 1408px)').matches ? t = e ? S : x : (window.matchMedia('(min-width: 1980px)').matches || window.matchMedia('(min-height: 1408px)').matches) && (t = e ? I : C);
+  let t = e ? I : y;
+  window.matchMedia('(min-width: 1012px) and (max-width: 1980px)').matches || window.matchMedia('(min-height: 720px) and (max-height: 1408px)').matches ? t = e ? S : b : (window.matchMedia('(min-width: 1980px)').matches || window.matchMedia('(min-height: 1408px)').matches) && (t = e ? g : x);
   let i = await fetch(t).then(async e => {
     let t = await e.blob();
     return window.URL.createObjectURL(t);
   });
-  p.Z.dispatch({
+  m.Z.dispatch({
     type: 'PURCHASED_ITEMS_FESTIVITY_FETCH_WOW_MOMENT_MEDIA_SUCCESS',
     wumpusMedia: i
   });
 } catch (e) {
-  p.Z.dispatch({
+  m.Z.dispatch({
     type: 'PURCHASED_ITEMS_FESTIVITY_IS_FETCHING_WOW_MOMENT_MEDIA',
     value: !1
   });
@@ -56,111 +55,106 @@ try {
   }
   (o = a || (a = {})).WUMPUS_FLIGHT = 'wumpus_flight', o.GRADIENT_HIGHLIGHT = 'gradient_highlight';
 t.default = function() {
-  let e = (0, m.e7)([M.Z], () => M.Z.useReducedMotion),
+  let e = (0, p.e7)([w.Z], () => w.Z.useReducedMotion),
 {
   wumpusMedia: t,
   isFetchingMedia: i,
   canPlayWowMoment: a
-} = (0, m.cj)([g.Z], () => ({
-  wumpusMedia: g.Z.wowMomentWumpusMedia,
-  isFetchingMedia: g.Z.isFetchingWowMomentMedia,
-  canPlayWowMoment: g.Z.canPlayWowMoment
+} = (0, p.cj)([T.Z], () => ({
+  wumpusMedia: T.Z.wowMomentWumpusMedia,
+  isFetchingMedia: T.Z.isFetchingWowMomentMedia,
+  canPlayWowMoment: T.Z.canPlayWowMoment
 })),
-[o, c] = s.useState(!1),
-p = (0, h.vu)(),
-f = (0, h.rO)(),
-S = p > 52 || -1 === p || f,
-I = f ? 'video/mp4' : 'video/webm';
-  S && !e && a && null === t && !1 === i && v(f), s.useEffect(() => {
+[o, s] = r.useState(!1),
+m = (0, h.vu)(),
+I = (0, h.rO)(),
+S = m > 52 || -1 === m || I,
+g = I ? 'video/mp4' : 'video/webm';
+  S && !e && a && null === t && !1 === i && v(I), r.useEffect(() => {
 function t() {
-  let {
-    enabled: t
-  } = T.o.getCurrentConfig({
-    location: 'PremumSubscriptionWowMoment.tsx'
-  });
-  t && !e && g.Z.canPlayWowMoment && (c(!0), n = setTimeout(() => {
-    c(!1), x(!0), _.default.track(b.rMx.PREMIUM_WOW_MOMENT_VIEWED, {
+  !e && T.Z.canPlayWowMoment && (s(!0), n = setTimeout(() => {
+    s(!1), b(!0), _.default.track(f.rMx.PREMIUM_WOW_MOMENT_VIEWED, {
       wow_moment_type: 'gradient_highlight'
     });
   }, 2000));
 }
-return W.S.subscribe(b.CkL.PREMIUM_SUBSCRIPTION_CREATED, t), () => {
-  W.S.unsubscribe(b.CkL.PREMIUM_SUBSCRIPTION_CREATED, t);
+return E.S.subscribe(f.CkL.PREMIUM_SUBSCRIPTION_CREATED, t), () => {
+  E.S.unsubscribe(f.CkL.PREMIUM_SUBSCRIPTION_CREATED, t);
 };
   }, [
 e,
 S
   ]);
-  let [y, x] = s.useState(!1), [C, F] = s.useState(!1), U = (0, l.useSpring)({
+  let [y, b] = r.useState(!1), [x, U] = r.useState(!1), C = (0, M.useSpring)({
 opacity: y ? 0.2 : 0,
 config: {
   duration: 100
 }
-  }), H = (0, l.useSpring)({
-x: C ? '100%' : '-100%',
+  }), F = (0, M.useSpring)({
+x: x ? '100%' : '-100%',
 config: {
   duration: 500
 }
-  }, C ? 'respect-motion-settings' : 'animate-never');
-  s.useEffect(() => {
+  }, x ? 'respect-motion-settings' : 'animate-never');
+  r.useEffect(() => {
 let e = -1;
 return y && (e = window.setTimeout(() => {
-  F(!0);
+  U(!0);
 }, 1000)), () => {
   window.clearTimeout(e);
 };
-  }, [y]), s.useEffect(() => {
+  }, [y]), r.useEffect(() => {
 let e = -1;
-return C && (e = window.setTimeout(() => {
-  F(!1), x(!1);
+return x && (e = window.setTimeout(() => {
+  U(!1), b(!1);
 }, 1000)), () => {
   window.clearTimeout(e);
 };
-  }, [C]);
-  let N = o && null !== t,
-O = N || y;
-  return (0, r.jsxs)('div', {
+  }, [x]);
+  let H = o && null !== t,
+O = H || y;
+  return (0, c.jsxs)('div', {
 className: u()({
   [P.wrapper]: !O,
   [P.activeWrapper]: O
 }),
 children: [
-  N && (0, r.jsx)(w.Z, {
+  H && (0, c.jsx)(l.Z, {
     autoPlay: !0,
     className: P.videoWrapper,
     onPlay: () => {
-      clearTimeout(n), _.default.track(b.rMx.PREMIUM_WOW_MOMENT_VIEWED, {
+      clearTimeout(n), _.default.track(f.rMx.PREMIUM_WOW_MOMENT_VIEWED, {
         wow_moment_type: 'wumpus_flight'
       });
     },
     onTimeUpdate: e => {
-      e.currentTarget.currentTime > 4 && (0, E.H)(!0);
+      e.currentTarget.currentTime > 4 && (0, W.H)(!0);
     },
     onEnded: () => {
-      c(!1);
+      s(!1);
     },
-    children: (0, r.jsx)('source', {
+    children: (0, c.jsx)('source', {
       src: t,
-      type: I
+      type: g
     })
   }),
-  (0, r.jsx)(d.animated.div, {
+  (0, c.jsx)(d.animated.div, {
     className: P.gadientHighlight,
-    style: U
+    style: C
   }),
-  (0, r.jsx)(d.animated.div, {
+  (0, c.jsx)(d.animated.div, {
     className: P.swipeWrapper,
-    style: H,
-    children: (0, r.jsxs)('svg', {
+    style: F,
+    children: (0, c.jsxs)('svg', {
       className: P.swipe,
       viewBox: '0 0 848 1024',
       fill: 'none',
       children: [
-        (0, r.jsx)('path', {
+        (0, c.jsx)('path', {
           d: 'M257.206 1024L0 0H341.333L598.539 1024H257.206Z',
           fill: 'white'
         }),
-        (0, r.jsx)('path', {
+        (0, c.jsx)('path', {
           d: 'M847.975 1024H690.436L433.231 0H590.769L847.975 1024Z',
           fill: 'white'
         })
