50df0808d01472b5974aac2711a40b32cc85581a
DiscrapperManager <admin@minerpl.xyz>
2024-08-12 20:51:54 +0000

    canaryRelease: 317664
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 11182
     * Scripts: 9818
    Strings: 18641
    Routes: 676
    Experiments Count: 377

--- chunks/22211.js
+++ chunks/22211.js
@@ -1,46 +1,49 @@
 n.d(t, {
   Z: function() {
-return E;
+return f;
   }
 });
 var r = n(442837),
   i = n(661869),
-  a = n(543882),
-  s = n(199902),
-  o = n(592125),
-  l = n(496675),
-  u = n(158776),
-  c = n(979651),
-  d = n(561308),
-  _ = n(231338);
+  a = n(387343),
+  s = n(543882),
+  o = n(199902),
+  l = n(592125),
+  u = n(496675),
+  c = n(158776),
+  d = n(979651),
+  _ = n(561308),
+  E = n(231338);
 
-function E(e) {
+function f(e) {
   let t = (0, r.e7)([
-  c.Z,
-  o.Z
+  d.Z,
+  l.Z,
+  u.Z
 ], () => {
-  if (!(0, d.kr)(e) || e.author_type !== i.i.USER)
+  if (!(0, _.kr)(e) || e.author_type !== i.i.USER)
     return null;
-  let t = c.Z.getVoiceStateForUser(e.author_id);
-  return o.Z.getChannel(null == t ? void 0 : t.channelId);
+  let t = d.Z.getVoiceStateForUser(e.author_id),
+    n = l.Z.getChannel(null == t ? void 0 : t.channelId);
+  return null == n || (0, a.Z)(n, u.Z) ? n : null;
 }, [e]),
-n = (0, r.e7)([u.Z], () => u.Z.getPrimaryActivity(e.author_id, null == t ? void 0 : t.guild_id), [
+n = (0, r.e7)([c.Z], () => c.Z.getPrimaryActivity(e.author_id, null == t ? void 0 : t.guild_id), [
   t,
   e
 ]),
 {
-  streamPreviewUrl: E,
-  stream: f
+  streamPreviewUrl: f,
+  stream: h
 } = (0, r.cj)([
-  s.Z,
-  l.Z,
-  a.Z
+  o.Z,
+  u.Z,
+  s.Z
 ], () => {
   let n;
   if (null == t)
     return {};
-  let r = s.Z.getAnyStreamForUser(e.author_id);
-  return null == r ? {} : (l.Z.canBasicChannel(_.S7.CONNECT, t) && (null == r ? void 0 : r.channelId) === t.id && (n = a.Z.getPreviewURL(r.guildId, r.channelId, r.ownerId)), {
+  let r = o.Z.getAnyStreamForUser(e.author_id);
+  return null == r ? {} : (u.Z.canBasicChannel(E.S7.CONNECT, t) && (null == r ? void 0 : r.channelId) === t.id && (n = s.Z.getPreviewURL(r.guildId, r.channelId, r.ownerId)), {
     stream: r,
     streamPreviewUrl: n
   });
@@ -51,7 +54,7 @@ n = (0, r.e7)([u.Z], () => u.Z.getPrimaryActivity(e.author_id, null == t ? void 
   return {
 channel: t,
 activity: n,
-streamPreviewUrl: E,
-stream: f
+streamPreviewUrl: f,
+stream: h
   };
 }
--- chunks/382182.js
+++ chunks/382182.js
@@ -16,16 +16,16 @@ return T;
   }
 }), n(653041), n(47120);
 var r, i, a = n(442837),
-  s = n(258609),
-  o = n(131704),
-  l = n(984933),
-  u = n(430824),
-  c = n(496675),
-  d = n(979651),
-  _ = n(934415),
-  E = n(981631),
-  f = n(689938);
-let h = (e, t) => (0, o.hv)(e.type) || t.canBasicChannel(E.S7T.CONNECT | E.S7T.VIEW_CHANNEL, e);
+  s = n(387343),
+  o = n(258609),
+  l = n(131704),
+  u = n(984933),
+  c = n(430824),
+  d = n(496675),
+  _ = n(979651),
+  E = n(934415),
+  f = n(981631),
+  h = n(689938);
 
 function p(e, t, n) {
   var r;
@@ -33,20 +33,20 @@ function p(e, t, n) {
   if (e.isPrivate())
 return !0;
   let a = t.getGuild(e.getGuildId()),
-s = null !== (r = null == a ? void 0 : a.maxStageVideoChannelUsers) && void 0 !== r ? r : 0;
-  return (!e.isGuildStageVoice() || !(s <= 0)) && (!i || !!h(e, n)) && !!n.can(E.Plq.STREAM, e) && null != a && a.afkChannelId !== e.id && !0;
+o = null !== (r = null == a ? void 0 : a.maxStageVideoChannelUsers) && void 0 !== r ? r : 0;
+  return (!e.isGuildStageVoice() || !(o <= 0)) && (!i || !!(0, s.Z)(e, n)) && !!n.can(f.Plq.STREAM, e) && null != a && a.afkChannelId !== e.id && !0;
 }
 
 function m(e) {
   switch (e) {
 case 0:
-  return f.Z.Messages.CONSOLE_STREAM_UNAVAILABLE;
+  return h.Z.Messages.CONSOLE_STREAM_UNAVAILABLE;
 case 1:
-  return f.Z.Messages.UNABLE_TO_JOIN_CHANNEL_FULL_MODAL_HEADER;
+  return h.Z.Messages.UNABLE_TO_JOIN_CHANNEL_FULL_MODAL_HEADER;
 case 2:
-  return f.Z.Messages.STREAM_NO_PERMISSION_CTA;
+  return h.Z.Messages.STREAM_NO_PERMISSION_CTA;
 default:
-  return f.Z.Messages.WATCH_STREAM;
+  return h.Z.Messages.WATCH_STREAM;
   }
 }
 
@@ -57,23 +57,23 @@ return [
   !1,
   2
 ];
-  let s = t.isInChannel(e.id),
-l = e instanceof o.Sf && (0, _.rY)(e, t, n),
-u = h(e, r),
-c = null != i.getAwaitingRemoteSessionInfo() || null != i.getRemoteSessionId();
-  return c ? a = 0 : u || s ? l && !s && (a = 1) : a = 2, [
-!__OVERLAY__ && !c && (s || u && !l),
+  let o = t.isInChannel(e.id),
+u = e instanceof l.Sf && (0, E.rY)(e, t, n),
+c = (0, s.Z)(e, r),
+d = null != i.getAwaitingRemoteSessionInfo() || null != i.getRemoteSessionId();
+  return d ? a = 0 : c || o ? u && !o && (a = 1) : a = 2, [
+!__OVERLAY__ && !d && (o || c && !u),
 a
   ];
 }
 
 function T(e) {
   return (0, a.Wu)([
-d.Z,
-u.Z,
+_.Z,
 c.Z,
-s.Z
-  ], () => I(e, d.Z, u.Z, c.Z, s.Z));
+d.Z,
+o.Z
+  ], () => I(e, _.Z, c.Z, d.Z, o.Z));
 }
 
 function g(e, t, n) {
@@ -81,7 +81,7 @@ function g(e, t, n) {
   for (let {
   channel: i
 }
-of e[l.Zb])
+of e[u.Zb])
 p(i, t, n) && r.push(i);
   return r;
 }
