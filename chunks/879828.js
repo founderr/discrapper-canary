5de54adf37daaec36edf284d42d594729b869b31
DiscrapperManager <admin@minerpl.xyz>
2024-08-13 21:36:04 +0000

    canaryRelease: 318167
    Main scripts: 3
    Lazy Loaded Scripts: 850
    Chunks:
     * Main Files: 11185
     * Scripts: 9857
    Strings: 18678
    Routes: 677
    Experiments Count: 374

--- chunks/879828.js
+++ chunks/879828.js
@@ -4,8 +4,8 @@ return m;
   }
 });
 var i = n(735250),
-  l = n(470079),
-  t = n(704215),
+  t = n(470079),
+  l = n(704215),
   o = n(481060),
   a = n(605236),
   r = n(929498),
@@ -26,10 +26,10 @@ onClose: m
 live: S,
 recent: x,
 stream: v
-  } = (0, r.Z)(s.id), Z = s.id === n.id, p = (0, a.wE)(t.z.USER_PROFILE_ACTIVITY_EDUCATION_TOOLTIP);
-  return l.useEffect(() => {
+  } = (0, r.Z)(s.id), Z = s.id === n.id, p = (0, a.wE)(l.z.USER_PROFILE_ACTIVITY_EDUCATION_TOOLTIP);
+  return t.useEffect(() => {
 if (!!Z && !p)
-  (0, a.EW)(t.z.USER_PROFILE_ACTIVITY_EDUCATION_TOOLTIP, {
+  (0, a.EW)(l.z.USER_PROFILE_ACTIVITY_EDUCATION_TOOLTIP, {
     dismissAction: I.L.AUTO_DISMISS,
     forceTrack: !0
   });
@@ -48,12 +48,12 @@ children: [
         stream: v,
         onClose: m
       }),
-      S.map((e, l) => (0, i.jsx)(c.Z, {
+      S.map((e, t) => (0, i.jsx)(c.Z, {
         user: s,
         currentUser: n,
         activity: e,
         onClose: m
-      }, 'live-'.concat(l)))
+      }, 'live-'.concat(t)))
     ]
   }),
   (0, i.jsx)(d.Z, {
@@ -62,6 +62,7 @@ children: [
       children: [
         f.Z.Messages.USER_PROFILE_ACTIVITY_RECENT_ACTIVITY,
         (0, i.jsx)(o.Tooltip, {
+          tooltipContentClassName: E.tooltipContent,
           text: f.Z.Messages.USER_PROFILE_RECENT_ACTIVITY_INFORMATION_TOOLTIP,
           children: e => (0, i.jsx)(o.CircleInformationIcon, {
             ...e,
