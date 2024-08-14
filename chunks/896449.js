a748b3adaca18b95f8bd3e06db22d62f159f3d3a
DiscrapperManager <admin@minerpl.xyz>
2024-08-14 17:06:07 +0000

    canaryRelease: 318386
    Main scripts: 3
    Lazy Loaded Scripts: 850
    Chunks:
     * Main Files: 11244
     * Scripts: 9871
    Strings: 18675
    Routes: 677
    Experiments Count: 374

--- chunks/896449.js
+++ chunks/896449.js
@@ -1,76 +1,80 @@
 n.d(t, {
   Z: function() {
-return m;
+return p;
   }
-}), n(47120);
-var i = n(735250),
-  a = n(470079),
-  s = n(481060),
-  l = n(239091),
-  r = n(299206),
-  o = n(810568),
-  c = n(168524),
-  u = n(725119),
-  d = n(26033),
-  h = n(689938),
-  p = n(243113);
+});
+var i = n(735250);
+n(470079);
+var a = n(481060),
+  s = n(239091),
+  l = n(299206),
+  r = n(810568),
+  o = n(168524),
+  c = n(725119),
+  u = n(26033),
+  d = n(689938),
+  h = n(243113);
 
-function m(e) {
+function p(e) {
   var t;
   let {
 user: n,
-guildId: m,
-channel: _,
-entry: f,
-onSelect: E
-  } = e, g = (0, u.Z)({
+guildId: p,
+channel: m,
+entry: _,
+onSelect: f
+  } = e, E = (0, c.Z)({
 userId: n.id,
-guildId: m,
-channelId: _.id
-  }), C = (0, r.Z)({
+guildId: p,
+channelId: m.id
+  }), g = (0, l.Z)({
 id: n.id,
-label: h.Z.Messages.COPY_ID_USER
-  }), I = (0, d.dX)(f), x = (0, c.Z)({
+label: d.Z.Messages.COPY_ID_USER
+  }), C = (0, u.dX)(_), I = (0, o.Z)({
 location: 'ContentPopoutContextMenu',
-applicationId: I ? null === (t = f.extra) || void 0 === t ? void 0 : t.application_id : void 0,
-source: o.m1.ActivityCardContextMenu,
+applicationId: C ? null === (t = _.extra) || void 0 === t ? void 0 : t.application_id : void 0,
+source: r.m1.ActivityCardContextMenu,
 trackEntryPointImpression: !0,
-sourceUserId: f.author_id
-  }), [T, N] = a.useState(!1);
-  return (0, i.jsx)(s.Popout, {
+sourceUserId: _.author_id
+  });
+  return (0, i.jsx)(a.Popout, {
 align: 'top',
 position: 'right',
-shouldShow: T,
 disablePointerEvents: !1,
-renderPopout: () => (0, i.jsx)(s.Menu, {
-  navId: 'content-inventory-context',
-  onClose: l.Zy,
-  'aria-label': h.Z.Messages.USER_ACTIONS_MENU_LABEL,
-  onSelect: E,
-  children: (0, i.jsxs)(i.Fragment, {
-    children: [
-      (0, i.jsxs)(s.MenuGroup, {
-        children: [
-          g,
-          null != x && (0, i.jsx)(s.MenuItem, {
-            id: 'game-profile',
-            label: h.Z.Messages.GAME_PROFILE,
-            action: x
-          })
-        ]
-      }),
-      (0, i.jsx)(s.MenuGroup, {
-        children: C
-      })
-    ]
-  })
-}),
-children: () => (0, i.jsx)(s.Clickable, {
-  className: p.menuIcon,
-  onClick: () => {
-    N(!0);
-  },
-  children: (0, i.jsx)(s.MoreHorizontalIcon, {
+renderPopout: e => {
+  let {
+    closePopout: t
+  } = e;
+  return (0, i.jsx)(a.Menu, {
+    navId: 'content-inventory-context',
+    onClose: () => {
+      (0, s.Zy)(), t();
+    },
+    'aria-label': d.Z.Messages.USER_ACTIONS_MENU_LABEL,
+    onSelect: f,
+    children: (0, i.jsxs)(i.Fragment, {
+      children: [
+        (0, i.jsxs)(a.MenuGroup, {
+          children: [
+            E,
+            null != I && (0, i.jsx)(a.MenuItem, {
+              id: 'game-profile',
+              label: d.Z.Messages.GAME_PROFILE,
+              action: I
+            })
+          ]
+        }),
+        (0, i.jsx)(a.MenuGroup, {
+          children: g
+        })
+      ]
+    })
+  });
+},
+children: e => (0, i.jsx)(a.Clickable, {
+  className: h.menuIcon,
+  ...e,
+  children: (0, i.jsx)(a.MoreHorizontalIcon, {
     color: 'currentColor',
     size: 'custom',
     width: 16,
