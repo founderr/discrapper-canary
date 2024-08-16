b2dd72fba19133273a849f55263debbe2eab19bc
DiscrapperManager <admin@minerpl.xyz>
2024-08-16 19:05:02 +0000

    canaryRelease: 319380
    Main scripts: 3
    Lazy Loaded Scripts: 855
    Chunks:
     * Main Files: 11165
     * Scripts: 9889
    Strings: 18696
    Routes: 677
    Experiments Count: 373

--- chunks/625252.js
+++ chunks/625252.js
@@ -1,6 +1,6 @@
 t.d(s, {
     Z: function () {
-        return A;
+        return p;
     }
 });
 var n = t(735250),
@@ -13,20 +13,29 @@ var n = t(735250),
     d = t(37234),
     _ = t(410030),
     E = t(605236),
-    u = t(471985),
-    I = t(341907),
-    T = t(906739),
-    S = t(46140),
-    N = t(689938),
-    C = t(74242),
-    m = t(66094);
-function A() {
-    let e = (0, u.Z)(S.dr.USER_SETTINGS_GIFT_INVENTORY),
-        s = (0, T.Z)(),
+    u = t(626135),
+    I = t(617136),
+    T = t(497505),
+    S = t(471985),
+    N = t(341907),
+    C = t(906739),
+    m = t(46140),
+    A = t(981631),
+    O = t(689938),
+    g = t(74242),
+    h = t(66094);
+function p() {
+    let e = (0, S.Z)(m.dr.USER_SETTINGS_GIFT_INVENTORY),
+        s = (0, C.Z)(),
         t = (0, l.wj)((0, _.ZP)()),
         i = a.useMemo(() => ({ backgroundImage: 'url('.concat(s, ')') }), [s]),
-        A = a.useCallback(() => {
-            (0, d.xf)(), (0, I.navigateToQuestHome)(S.dr.USER_SETTINGS_GIFT_INVENTORY);
+        p = a.useCallback(() => {
+            (0, d.xf)(),
+                (0, N.navigateToQuestHome)(m.dr.USER_SETTINGS_GIFT_INVENTORY),
+                u.default.track(A.rMx.QUEST_CONTENT_CLICKED, {
+                    cta_name: I.jZ.VIEW_QUESTS,
+                    ...(0, I.mH)(T.jn.QUEST_HOME_MOVE_CALLOUT)
+                });
         }, []);
     return (a.useCallback(() => {
         (0, E.EW)(o.z.GIFT_INVENTORY_QUESTS_MOVED_TO_QUEST_HOME);
@@ -34,43 +43,43 @@ function A() {
     e)
         ? ((0, E.un)(o.z.GIFT_INVENTORY_QUESTS_MOVED_TO_QUEST_HOME),
           (0, n.jsxs)('div', {
-              className: r()(C.container),
+              className: r()(g.container),
               style: i,
               children: [
                   !1,
                   (0, n.jsxs)('div', {
-                      className: C.textContainer,
+                      className: g.textContainer,
                       children: [
                           (0, n.jsxs)('div', {
-                              className: C.headerContainer,
+                              className: g.headerContainer,
                               children: [
                                   (0, n.jsx)('img', {
                                       alt: '',
-                                      src: m,
-                                      className: C.icon
+                                      src: h,
+                                      className: g.icon
                                   }),
                                   (0, n.jsx)(c.Heading, {
                                       variant: 'heading-lg/bold',
                                       color: 'always-white',
-                                      children: N.Z.Messages.QUESTS_HOME_MOVED_CALLOUT_TITLE
+                                      children: O.Z.Messages.QUESTS_HOME_MOVED_CALLOUT_TITLE
                                   })
                               ]
                           }),
                           (0, n.jsx)(c.Text, {
                               variant: 'text-md/medium',
                               color: 'text-muted',
-                              children: N.Z.Messages.QUESTS_HOME_MOVED_CALLOUT_DESCRIPTION_V2
+                              children: O.Z.Messages.QUESTS_HOME_MOVED_CALLOUT_DESCRIPTION_V2
                           }),
                           (0, n.jsx)(c.Button, {
                               size: 'large',
                               color: t ? c.ButtonColors.BRAND : c.ButtonColors.WHITE,
-                              className: C.button,
-                              onClick: A,
-                              innerClassName: C.innerButton,
+                              className: g.button,
+                              onClick: p,
+                              innerClassName: g.innerButton,
                               children: (0, n.jsx)(c.Text, {
                                   variant: 'text-md/medium',
                                   color: t ? 'always-white' : 'text-normal',
-                                  children: N.Z.Messages.QUESTS_HOME_MOVED_CALLOUT_BUTTON
+                                  children: O.Z.Messages.QUESTS_HOME_MOVED_CALLOUT_BUTTON
                               })
                           })
                       ]
