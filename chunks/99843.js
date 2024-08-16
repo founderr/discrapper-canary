9b9d5826fe0c53e755600a984b20ca9778380e81
DiscrapperManager <admin@minerpl.xyz>
2024-08-16 16:11:53 +0000

    canaryRelease: 319253
    Main scripts: 3
    Lazy Loaded Scripts: 854
    Chunks:
     * Main Files: 11166
     * Scripts: 9894
    Strings: 18696
    Routes: 677
    Experiments Count: 374

--- chunks/99843.js
+++ chunks/99843.js
@@ -1,33 +1,55 @@
 var i = n(735250);
 n(470079);
-var a = n(493683),
-    s = n(599059),
-    l = n(762914),
-    r = n(933557),
-    o = n(981631),
-    c = n(883478);
+var a = n(481060),
+    s = n(493683),
+    l = n(599059),
+    r = n(762914),
+    o = n(933557),
+    c = n(210975),
+    u = n(981631),
+    d = n(689938),
+    h = n(883478);
 t.Z = (e) => {
     let { channel: t } = e,
-        n = (0, r.ZP)(t),
-        u = (0, l.Dd)(t.id);
+        n = (0, o.ZP)(t),
+        m = (0, r.Dd)(t.id),
+        p = (0, c.m$)({
+            channelId: t.id,
+            location: 'ChannelName'
+        });
     return (0, i.jsx)('div', {
-        className: c.container,
+        className: h.container,
         onDoubleClick: (e) => e.stopPropagation(),
-        children: u
+        children: m
             ? (0, i.jsx)('div', {
-                  className: c.channelName,
+                  className: h.channelName,
                   children: n
               })
-            : (0, i.jsx)(s.Z, {
-                  minLen: 1,
-                  maxLen: o.HN8,
-                  className: c.channelName,
-                  name: 'channel_name',
-                  autoComplete: 'off',
-                  value: null != n ? n : '',
-                  onBlur: (e) => {
-                      n !== e.currentTarget.value && a.Z.setName(t.id, e.currentTarget.value);
-                  }
+            : (0, i.jsxs)(i.Fragment, {
+                  children: [
+                      (0, i.jsx)(l.Z, {
+                          minLen: 1,
+                          maxLen: u.HN8,
+                          className: h.channelName,
+                          name: 'channel_name',
+                          autoComplete: 'off',
+                          value: null != n ? n : '',
+                          onBlur: (e) => {
+                              n !== e.currentTarget.value && s.Z.setName(t.id, e.currentTarget.value);
+                          }
+                      }),
+                      p &&
+                          (0, i.jsx)(a.Tooltip, {
+                              text: d.Z.Messages.E2EE_CALL_VERIFIED_TOOLTIP,
+                              children: (e) =>
+                                  (0, i.jsx)(a.ShieldLockIcon, {
+                                      ...e,
+                                      size: 'xs',
+                                      'aria-label': d.Z.Messages.E2EE_CALL_VERIFIED_A11Y,
+                                      color: a.tokens.colors.INTERACTIVE_NORMAL
+                                  })
+                          })
+                  ]
               })
     });
 };
