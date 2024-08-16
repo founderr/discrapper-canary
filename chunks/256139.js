1db16b38033bbe5d26b2be9bbea7f9c7d3986a17
DiscrapperManager <admin@minerpl.xyz>
2024-08-16 16:43:51 +0000

    canaryRelease: 319269
    Main scripts: 3
    Lazy Loaded Scripts: 854
    Chunks:
     * Main Files: 11166
     * Scripts: 9894
    Strings: 18696
    Routes: 677
    Experiments Count: 374

--- chunks/256139.js
+++ chunks/256139.js
@@ -9,10 +9,11 @@ var t = l(735250),
     a = l(481060),
     u = l(671656);
 function s(e) {
-    let { className: n, onClick: l, ...r } = e;
-    return (0, t.jsx)(a.Clickable, {
-        ...r,
-        className: i()(null != l && u.clickable, n),
-        onClick: l
-    });
+    let { className: n, onClick: l, ...r } = e,
+        s = (0, t.jsx)(a.Clickable, {
+            ...r,
+            className: i()(null != l && u.clickable, n),
+            onClick: l
+        });
+    return null == l ? (0, t.jsx)(a.BlockInteractions, { children: s }) : s;
 }
