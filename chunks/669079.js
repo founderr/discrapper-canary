f812b6d7b8fd7cd2e587039d0f0824d1e0281434
DiscrapperManager <admin@minerpl.xyz>
2024-08-19 18:53:05 +0000

    canaryRelease: 319807
    Main scripts: 3
    Lazy Loaded Scripts: 856
    Chunks:
     * Main Files: 11168
     * Scripts: 9888
    Strings: 18679
    Routes: 678
    Experiments Count: 371

--- chunks/669079.js
+++ chunks/669079.js
@@ -1,57 +1,57 @@
 n.d(t, {
     Bg: function () {
-        return U;
+        return P;
     },
     E5: function () {
-        return q;
-    },
-    Fp: function () {
-        return G;
-    },
-    JT: function () {
         return z;
     },
+    Fp: function () {
+        return x;
+    },
+    JT: function () {
+        return K;
+    },
     L2: function () {
-        return Y;
-    },
-    MY: function () {
-        return M;
-    },
-    Nz: function () {
-        return B;
-    },
-    Q_: function () {
-        return k;
-    },
-    TO: function () {
-        return H;
-    },
-    Z0: function () {
-        return w;
-    },
-    bT: function () {
-        return F;
-    },
-    dM: function () {
-        return V;
-    },
-    dQ: function () {
         return Z;
     },
-    e$: function () {
-        return W;
+    MY: function () {
+        return b;
     },
-    iM: function () {
+    Nz: function () {
+        return k;
+    },
+    Q_: function () {
+        return G;
+    },
+    TO: function () {
+        return V;
+    },
+    Z0: function () {
+        return U;
+    },
+    bT: function () {
+        return B;
+    },
+    dM: function () {
+        return F;
+    },
+    dQ: function () {
+        return H;
+    },
+    e$: function () {
         return j;
     },
+    iM: function () {
+        return Y;
+    },
     pO: function () {
-        return P;
+        return M;
     },
     xr: function () {
         return r;
     },
     z2: function () {
-        return K;
+        return W;
     }
 }),
     n(47120),
@@ -68,32 +68,31 @@ var r,
     d = n(594174),
     _ = n(626135),
     E = n(823379),
-    f = n(358085),
-    h = n(74538),
-    p = n(226951),
-    I = n(73346),
+    f = n(74538),
+    h = n(226951),
+    p = n(73346),
     m = n(981631),
-    T = n(474936),
-    g = n(689938);
-let S = p.Z.escape(window.GLOBAL_ENV.GIFT_CODE_HOST),
-    A = [S, ...['discordapp.com/gifts', 'discord.com/gifts'].map((e) => p.Z.escape(e))].join('|'),
-    N = RegExp('(?: |^|https?://)(?:'.concat(A, ')/([a-z0-9-]+)'), 'gi'),
-    v = [...['discord.com/billing/promotions', 'promos.discord.gg'].map((e) => p.Z.escape(e))].join('|'),
-    O = RegExp('(?: |^|https?://)(?:'.concat(v, ')/([a-z0-9-]+)'), 'gi'),
-    R = (e, t) =>
+    I = n(474936),
+    T = n(689938);
+let g = h.Z.escape(window.GLOBAL_ENV.GIFT_CODE_HOST),
+    S = [g, ...['discordapp.com/gifts', 'discord.com/gifts'].map((e) => h.Z.escape(e))].join('|'),
+    A = RegExp('(?: |^|https?://)(?:'.concat(S, ')/([a-z0-9-]+)'), 'gi'),
+    N = [...['discord.com/billing/promotions', 'promos.discord.gg'].map((e) => h.Z.escape(e))].join('|'),
+    v = RegExp('(?: |^|https?://)(?:'.concat(N, ')/([a-z0-9-]+)'), 'gi'),
+    O = (e, t) =>
         Array(t)
             .fill(void 0)
             .map(() => '['.concat('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789', ']{').concat(e, '}'))
             .join('-?'),
-    C = R(4, 4),
-    y = R(4, 6),
-    D = R(5, 3),
-    L = [C, y, D, '[a-zA-Z]{4}-?[0-9a-zA-Z]{4}-?[a-zA-Z]{4}'].join('|'),
-    b = new RegExp('^('.concat('WUMP-?', ')?(').concat(L, ')$'));
+    R = O(4, 4),
+    C = O(4, 6),
+    y = O(5, 3),
+    D = [R, C, y, '[a-zA-Z]{4}-?[0-9a-zA-Z]{4}-?[a-zA-Z]{4}'].join('|'),
+    L = new RegExp('^('.concat('WUMP-?', ')?(').concat(D, ')$'));
 ((i = r || (r = {}))[(i.DEFAULT = 0)] = 'DEFAULT'), (i[(i.CUSTOM_STYLE = 1)] = 'CUSTOM_STYLE'), (i[(i.CUSTOM_MESSAGE_EMOJI_SOUNDBOARD = 2)] = 'CUSTOM_MESSAGE_EMOJI_SOUNDBOARD');
-let M = (e, t) => (s.tq || s.Em ? 0 : null != e || t ? 2 : 1),
-    P = (e) => 0 !== M(e);
-function U(e) {
+let b = (e, t) => (s.tq || s.Em ? 0 : null != e || t ? 2 : 1),
+    M = (e) => 0 !== b(e);
+function P(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null,
         n = arguments.length > 2 ? arguments[2] : void 0;
     return ''
@@ -101,7 +100,7 @@ function U(e) {
         .concat(null != t ? t : '', ':')
         .concat(null != n ? n : '');
 }
-function w(e) {
+function U(e) {
     let [t, n, r] = e.split(':');
     return {
         skuId: t,
@@ -109,30 +108,30 @@ function w(e) {
         giftStyle: '' !== r && null != r ? Number.parseInt(r) : void 0
     };
 }
-function x(e) {
+function w(e) {
     return e.replace(/[^A-Za-z0-9]/g, '');
 }
-let G = (e) => (null == e ? void 0 : e.type) === m.uaV.CUSTOM_GIFT && (null == e ? void 0 : e.embeds.length) === 1 && (null == e ? void 0 : e.embeds[0].type) === m.hBH.GIFT,
-    k = (e) => {
+let x = (e) => (null == e ? void 0 : e.type) === m.uaV.CUSTOM_GIFT && (null == e ? void 0 : e.embeds.length) === 1 && (null == e ? void 0 : e.embeds[0].type) === m.hBH.GIFT,
+    G = (e) => {
         let t;
         if (null == e) return [];
         let n = new Set();
-        for (; null != (t = N.exec(e)) && n.size < 3; ) n.add(x(t[1]));
-        for (; null != (t = O.exec(e)) && n.size < 3; ) n.add(x(t[1]));
+        for (; null != (t = A.exec(e)) && n.size < 3; ) n.add(w(t[1]));
+        for (; null != (t = v.exec(e)) && n.size < 3; ) n.add(w(t[1]));
         return Array.from(n);
     };
-function B() {
+function k() {
     let e,
         t = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : '',
         n = window.GLOBAL_ENV.GIFT_CODE_HOST;
-    return null == n || (0, f.isAndroid)() ? ((n = location.host), (e = '/gifts/'.concat(t))) : (e = '/'.concat(t)), ''.concat(location.protocol, '//').concat(n).concat(e);
+    return null != n ? (e = '/'.concat(t)) : ((n = location.host), (e = '/gifts/'.concat(t))), ''.concat(location.protocol, '//').concat(n).concat(e);
 }
-async function F(e) {
+async function B(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1],
         n = arguments.length > 2 && void 0 !== arguments[2] && arguments[2];
     try {
         let r = (
-            await (0, I.Kb)({
+            await (0, p.Kb)({
                 url: m.ANM.GIFT_CODE_RESOLVE(e),
                 query: {
                     with_application: t,
@@ -167,48 +166,48 @@ async function F(e) {
         );
     }
 }
-function V(e, t) {
+function F(e, t) {
     _.default.track(m.rMx.GIFT_CODE_COPIED, {
         ...(0, u.Z)(t, !1, !1),
         ...e.analyticsData
     });
 }
-function H(e, t, n, r, i, a, s) {
+function V(e, t, n, r, i, a, s) {
     return null == n && (r || i || null == e) ? (!s || a || r || i ? (r && (t.isSubscription || null != e) ? m.wZ8.SUCCESS : m.wZ8.CONFIRM) : m.wZ8.OPEN) : m.wZ8.ERROR;
+}
+function H(e, t, n) {
+    switch (e) {
+        case m.wZ8.ERROR:
+            return T.Z.Messages.GIFT_CONFIRMATION_HEADER_FAIL;
+        case m.wZ8.SUCCESS:
+            return t.isSubscription ? T.Z.Messages.GIFT_CONFIRMATION_HEADER_SUCCESS_NITRO.format({ skuName: n.name }) : T.Z.Messages.GIFT_CONFIRMATION_HEADER_SUCCESS;
+        case m.wZ8.CONFIRM:
+        default:
+            return t.isSubscription ? T.Z.Messages.GIFT_CONFIRMATION_HEADER_CONFIRM_NITRO.format({ skuName: n.name }) : T.Z.Messages.GIFT_CONFIRMATION_HEADER_CONFIRM;
+    }
 }
 function Z(e, t, n) {
     switch (e) {
         case m.wZ8.ERROR:
-            return g.Z.Messages.GIFT_CONFIRMATION_HEADER_FAIL;
+            return T.Z.Messages.GIFT_CONFIRMATION_BUTTON_FAIL;
         case m.wZ8.SUCCESS:
-            return t.isSubscription ? g.Z.Messages.GIFT_CONFIRMATION_HEADER_SUCCESS_NITRO.format({ skuName: n.name }) : g.Z.Messages.GIFT_CONFIRMATION_HEADER_SUCCESS;
-        case m.wZ8.CONFIRM:
-        default:
-            return t.isSubscription ? g.Z.Messages.GIFT_CONFIRMATION_HEADER_CONFIRM_NITRO.format({ skuName: n.name }) : g.Z.Messages.GIFT_CONFIRMATION_HEADER_CONFIRM;
-    }
-}
-function Y(e, t, n) {
-    switch (e) {
-        case m.wZ8.ERROR:
-            return g.Z.Messages.GIFT_CONFIRMATION_BUTTON_FAIL;
-        case m.wZ8.SUCCESS:
-            if (__OVERLAY__) return g.Z.Messages.GIFT_CONFIRMATION_BUTTON_NOICE;
-            if (t.isSubscription) return g.Z.Messages.GIFT_CONFIRMATION_BUTTON_SUBSCRIPTION_SUCCESS;
-            return g.Z.Messages.GIFT_CONFIRMATION_BUTTON_GO_TO_LIBRARY;
+            if (__OVERLAY__) return T.Z.Messages.GIFT_CONFIRMATION_BUTTON_NOICE;
+            if (t.isSubscription) return T.Z.Messages.GIFT_CONFIRMATION_BUTTON_SUBSCRIPTION_SUCCESS;
+            return T.Z.Messages.GIFT_CONFIRMATION_BUTTON_GO_TO_LIBRARY;
         case m.wZ8.OPEN:
-            return g.Z.Messages.GIFT_OPEN_PROMPT;
+            return T.Z.Messages.GIFT_OPEN_PROMPT;
         case m.wZ8.CONFIRM:
         default:
-            if (null != n && n) return g.Z.Messages.GIFT_CODE_AUTH_ACCEPT;
-            if (null != t.giftStyle) return t.isClaimed ? g.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM : g.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM_MOBILE;
-            return t.isSubscription ? g.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM_SUBSCRIPTION : g.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM;
+            if (null != n && n) return T.Z.Messages.GIFT_CODE_AUTH_ACCEPT;
+            if (null != t.giftStyle) return t.isClaimed ? T.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM : T.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM_MOBILE;
+            return t.isSubscription ? T.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM_SUBSCRIPTION : T.Z.Messages.GIFT_CONFIRMATION_BUTTON_CONFIRM;
     }
 }
-function j(e) {
+function Y(e) {
     let { step: t, sku: n, libraryApplication: r, error: i, accepted: s, accepting: o, onGoToLibrary: l, subscriptionPlan: u = null } = e;
     switch (t) {
         case m.wZ8.ERROR:
-            return W(r, i, s, o, l);
+            return j(r, i, s, o, l);
         case m.wZ8.SUCCESS:
             if (null != u) {
                 var c;
@@ -217,48 +216,48 @@ function j(e) {
                     (0, a.EQ)(c)
                         .with(
                             {
-                                interval: T.rV.MONTH,
-                                premiumSubscriptionType: T.p9.TIER_2
+                                interval: I.rV.MONTH,
+                                premiumSubscriptionType: I.p9.TIER_2
                             },
-                            () => g.Z.Messages.PREMIUM_FEATURES_TIER_2.format({ timeInterval: g.Z.Messages.PREMIUM_SUBSCRIPTION_INTERVAL_MONTH })
+                            () => T.Z.Messages.PREMIUM_FEATURES_TIER_2.format({ timeInterval: T.Z.Messages.PREMIUM_SUBSCRIPTION_INTERVAL_MONTH })
                         )
                         .with(
                             {
-                                interval: T.rV.YEAR,
-                                premiumSubscriptionType: T.p9.TIER_2
+                                interval: I.rV.YEAR,
+                                premiumSubscriptionType: I.p9.TIER_2
                             },
-                            () => g.Z.Messages.PREMIUM_FEATURES_TIER_2.format({ timeInterval: g.Z.Messages.PREMIUM_SUBSCRIPTION_INTERVAL_YEAR })
+                            () => T.Z.Messages.PREMIUM_FEATURES_TIER_2.format({ timeInterval: T.Z.Messages.PREMIUM_SUBSCRIPTION_INTERVAL_YEAR })
                         )
                         .with(
                             {
-                                interval: T.rV.MONTH,
-                                premiumSubscriptionType: T.p9.TIER_1
+                                interval: I.rV.MONTH,
+                                premiumSubscriptionType: I.p9.TIER_1
                             },
-                            () => g.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS_PREMIUM_TIER_1_MONTHLY.format({ intervalCount: c.intervalCount })
+                            () => T.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS_PREMIUM_TIER_1_MONTHLY.format({ intervalCount: c.intervalCount })
                         )
                         .with(
                             {
-                                interval: T.rV.YEAR,
-                                premiumSubscriptionType: T.p9.TIER_1
+                                interval: I.rV.YEAR,
+                                premiumSubscriptionType: I.p9.TIER_1
                             },
-                            () => g.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS_PREMIUM_TIER_1_YEARLY.format({ intervalCount: c.intervalCount })
+                            () => T.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS_PREMIUM_TIER_1_YEARLY.format({ intervalCount: c.intervalCount })
                         )
-                        .otherwise(() => g.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS_GENERIC)
+                        .otherwise(() => T.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS_GENERIC)
                 );
             }
-            return g.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS.format({ skuName: n.name });
+            return T.Z.Messages.GIFT_CONFIRMATION_BODY_SUCCESS.format({ skuName: n.name });
         case m.wZ8.CONFIRM:
         default:
             if (null != u)
-                return (u.interval === T.rV.MONTH ? g.Z.Messages.GIFT_CONFIRMATION_BODY_SUBSCRIPTION_MONTHLY_CONFIRM : g.Z.Messages.GIFT_CONFIRMATION_BODY_SUBSCRIPTION_YEARLY_CONFIRM).format({
+                return (u.interval === I.rV.MONTH ? T.Z.Messages.GIFT_CONFIRMATION_BODY_SUBSCRIPTION_MONTHLY_CONFIRM : T.Z.Messages.GIFT_CONFIRMATION_BODY_SUBSCRIPTION_YEARLY_CONFIRM).format({
                     skuName: n.name,
                     intervalCount: u.intervalCount
                 });
-            return g.Z.Messages.GIFT_CONFIRMATION_BODY_CONFIRM.format({ skuName: n.name });
+            return T.Z.Messages.GIFT_CONFIRMATION_BODY_CONFIRM.format({ skuName: n.name });
     }
 }
-function W(e, t, n, r, i) {
-    let a = g.Z.Messages.GIFT_CONFIRMATION_BODY_OWNED.format({ onGoToLibrary: i });
+function j(e, t, n, r, i) {
+    let a = T.Z.Messages.GIFT_CONFIRMATION_BODY_OWNED.format({ onGoToLibrary: i });
     return null != (n || r ? void 0 : e)
         ? a
         : null == t
@@ -266,41 +265,41 @@ function W(e, t, n, r, i) {
           : (function (e, t) {
                 switch (e.code) {
                     case m.evJ.INVALID_GIFT_SELF_REDEMPTION:
-                        return g.Z.Messages.GIFT_CONFIRMATION_BODY_SELF_GIFT_NO_PAYMENT;
+                        return T.Z.Messages.GIFT_CONFIRMATION_BODY_SELF_GIFT_NO_PAYMENT;
                     case m.evJ.INVALID_GIFT_REDEMPTION_EXHAUSTED:
-                        return g.Z.Messages.GIFT_CONFIRMATION_BODY_CLAIMED;
+                        return T.Z.Messages.GIFT_CONFIRMATION_BODY_CLAIMED;
                     case m.evJ.INVALID_GIFT_REDEMPTION_OWNED:
-                        return g.Z.Messages.GIFT_ERROR_OWNED;
+                        return T.Z.Messages.GIFT_ERROR_OWNED;
                     case m.evJ.UNKNOWN_GIFT_CODE:
-                        return g.Z.Messages.GIFT_CONFIRMATION_BODY_INVALID;
+                        return T.Z.Messages.GIFT_CONFIRMATION_BODY_INVALID;
                     case m.evJ.INVALID_GIFT_REDEMPTION_SUBSCRIPTION_INCOMPATIBLE:
-                        return g.Z.Messages.GIFT_EMBED_BODY_SUBSCRIPTION_MISMATCH.format({ planName: (0, h.M5)(t, T.p9.TIER_2) ? g.Z.Messages.PREMIUM_TIER_2 : g.Z.Messages.PREMIUM_TIER_1 });
+                        return T.Z.Messages.GIFT_EMBED_BODY_SUBSCRIPTION_MISMATCH.format({ planName: (0, f.M5)(t, I.p9.TIER_2) ? T.Z.Messages.PREMIUM_TIER_2 : T.Z.Messages.PREMIUM_TIER_1 });
                     case m.evJ.INVALID_GIFT_REDEMPTION_SUBSCRIPTION_MANAGED:
-                        return g.Z.Messages.GIFT_CONFIRMATION_BODY_ERROR_SUBSCRIPTION_MANAGED;
+                        return T.Z.Messages.GIFT_CONFIRMATION_BODY_ERROR_SUBSCRIPTION_MANAGED;
                     case m.evJ.INVALID_GIFT_REDEMPTION_INVOICE_OPEN:
-                        return g.Z.Messages.GIFT_CONFIRMATION_BODY_ERROR_INVOICE_OPEN;
+                        return T.Z.Messages.GIFT_CONFIRMATION_BODY_ERROR_INVOICE_OPEN;
                     case m.evJ.INVALID_GIFT_REDEMPTION_FRAUD_REJECTED:
-                        return g.Z.Messages.GIFT_CODE_SMITE_REJECT_HELP_TEXT;
+                        return T.Z.Messages.GIFT_CODE_SMITE_REJECT_HELP_TEXT;
                     case m.evJ.BILLING_NON_REFUNDABLE_PAYMENT_SOURCE:
-                        return g.Z.Messages.GIFT_CODE_PAYMENT_SOURCE_INVALID;
+                        return T.Z.Messages.GIFT_CODE_PAYMENT_SOURCE_INVALID;
                     default:
-                        return g.Z.Messages.GIFT_CONFIRMATION_BODY_UNKNOWN_ERROR;
+                        return T.Z.Messages.GIFT_CONFIRMATION_BODY_UNKNOWN_ERROR;
                 }
             })(t, d.default.getCurrentUser());
 }
-function K(e, t, n) {
+function W(e, t, n) {
     let r = t.applicationId,
         i = e.length > 0 ? e : [r],
         a = i.map((e) => n.getLibraryApplication(r, e, !0)).filter(E.lm);
     return a.length === i.length ? a[0] : null;
 }
-function z(e) {
-    let t = e.trim().split('/').pop().match(b);
+function K(e) {
+    let t = e.trim().split('/').pop().match(L);
     if (null == t) return null;
     let [n, r, i] = t;
     return null == i ? null : i.replace(/-/g, '');
 }
-let q = (e, t) =>
+let z = (e, t) =>
     (0, o.e7)([c.Z], () => {
         if (null == e || !t) return null;
         let n = c.Z.getGiftCode(e);
