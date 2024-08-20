7a7d3d5f8720cef8ec2744b49b2397f43e36a3c6
DiscrapperManager <admin@minerpl.xyz>
2024-08-20 15:24:15 +0000

    canaryRelease: 320083
    Main scripts: 3
    Lazy Loaded Scripts: 858
    Chunks:
     * Main Files: 11169
     * Scripts: 9892
    Strings: 18700
    Routes: 678
    Experiments Count: 372

--- chunks/521332.js
+++ chunks/521332.js
@@ -1,6 +1,6 @@
 n.d(t, {
     Z: function () {
-        return p;
+        return m;
     }
 }),
     n(47120);
@@ -9,72 +9,79 @@ var r = n(735250),
     a = n(442837),
     s = n(481060),
     o = n(230711),
-    l = n(138201),
-    u = n(592125),
-    c = n(155647),
-    d = n(185625),
-    _ = n(981631),
-    E = n(689938),
-    f = n(229223);
-function h(e) {
+    l = n(367907),
+    u = n(138201),
+    c = n(592125),
+    d = n(155647),
+    _ = n(185625),
+    E = n(981631),
+    f = n(689938),
+    h = n(229223);
+function p(e) {
     let { title: t, description: n, onButtonClick: a, trackSettingsUpsellsAction: s } = e,
-        [o, u] = i.useState(!1);
+        [o, l] = i.useState(!1);
     return (
         i.useEffect(() => {
-            s(d.M4.SETTINGS_UPSELLS_VIEWED);
+            s(_.M4.SETTINGS_UPSELLS_VIEWED);
         }, []),
-        (0, r.jsx)(l.Z, {
+        (0, r.jsx)(u.Z, {
             title: t,
             description: n,
-            buttonText: o ? E.Z.Messages.IAR_UPSELLS_APPLIED_BUTTON : E.Z.Messages.IAR_UPSELLS_APPLY_BUTTON,
+            buttonText: o ? f.Z.Messages.IAR_UPSELLS_APPLIED_BUTTON : f.Z.Messages.IAR_UPSELLS_APPLY_BUTTON,
             buttonDisabled: o,
             onButtonPress: () => {
-                a(), u(!0), s(d.M4.SETTINGS_UPSELLS_APPLY_CLICKED);
+                a(), l(!0), s(_.M4.SETTINGS_UPSELLS_APPLY_CLICKED);
             }
         })
     );
 }
-function p(e) {
-    let { settingsUpsells: t, channelId: n, onModalClose: i, reportId: l, reportType: p, reportSubType: m } = e,
-        I = (0, a.e7)([u.Z], () => u.Z.getChannel(n)),
-        T = (0, c.jc)(t, null == I ? void 0 : I.type),
-        g = (0, d.i_)(p, m, l);
-    return 0 === T.length
+function m(e) {
+    let { settingsUpsells: t, channelId: n, onModalClose: i, reportId: u, reportType: m, reportSubType: I } = e,
+        T = (0, a.e7)([c.Z], () => c.Z.getChannel(n)),
+        g = (0, d.jc)(t, null == T ? void 0 : T.type),
+        S = (0, _.i_)(m, I, u);
+    return 0 === g.length
         ? null
         : (0, r.jsxs)('div', {
-              className: f.container,
+              className: h.container,
               children: [
                   (0, r.jsx)(s.Heading, {
-                      variant: 'heading-sm/semibold',
-                      className: f.header,
-                      children: E.Z.Messages.IAR_UPSELLS_SECTION_TITLE
+                      variant: 'text-sm/semibold',
+                      className: h.header,
+                      children: f.Z.Messages.IAR_UPSELLS_SECTION_TITLE
                   }),
                   (0, r.jsx)('div', {
-                      className: f.upsellsContainer,
-                      children: T.map((e, n) => {
+                      className: h.upsellsContainer,
+                      children: g.map((e, n) => {
                           let { getTitle: i, getDescription: a, onApply: s } = e;
                           return (0, r.jsx)(
-                              h,
+                              p,
                               {
                                   title: i(),
                                   description: a(),
                                   onButtonClick: s,
-                                  trackSettingsUpsellsAction: g(t[n])
+                                  trackSettingsUpsellsAction: S(t[n])
                               },
                               n
                           );
                       })
                   }),
-                  (0, r.jsx)('div', {
-                      className: f.navLinkContainer,
-                      children: (0, r.jsx)(s.Button, {
-                          onClick: () => {
-                              o.Z.open(_.oAB.PRIVACY_AND_SAFETY), i();
-                          },
-                          look: s.Button.Looks.LINK,
-                          color: s.Button.Colors.PRIMARY,
-                          children: E.Z.Messages.IAR_UPSELLS_GO_TO_SAFETY_SETTINGS
-                      })
+                  (0, r.jsx)(s.Button, {
+                      className: h.navLinkButton,
+                      onClick: () => {
+                          o.Z.open(E.oAB.PRIVACY_AND_SAFETY),
+                              i(),
+                              l.ZP.trackWithMetadata(E.rMx.IAR_SETTINGS_UPSELLS_ACTION, {
+                                  report_id: u,
+                                  report_type: m.name,
+                                  report_subtype: I,
+                                  action: _.M4.SETTINGS_UPSELLS_GO_TO_SETTINGS_LINK_CLICKED
+                              });
+                      },
+                      look: s.Button.Looks.LINK,
+                      color: s.Button.Colors.LINK,
+                      size: s.Button.Sizes.NONE,
+                      children: f.Z.Messages.IAR_UPSELLS_GO_TO_SAFETY_SETTINGS
                   })
               ]
           });
