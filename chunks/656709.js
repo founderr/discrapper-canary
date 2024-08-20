5237869c1f6110074b56b562c939458cc14e130d
DiscrapperManager <admin@minerpl.xyz>
2024-08-20 20:10:38 +0000

    canaryRelease: 320251
    Main scripts: 3
    Lazy Loaded Scripts: 857
    Chunks:
     * Main Files: 11171
     * Scripts: 9897
    Strings: 18708
    Routes: 679
    Experiments Count: 373

--- chunks/656709.js
+++ chunks/656709.js
@@ -1,14 +1,12 @@
-n.d(t, {
+t.d(n, {
     p: function () {
-        return l;
+        return i;
     }
 });
-var i = n(904245),
-    a = n(790642),
-    s = n(957730);
-function l(e) {
-    let t,
-        { channel: n, content: l, entry: r } = e,
-        o = s.ZP.parse(n, l);
-    return (t = (0, a.bx)('sendMessageWithEmbed') ? { unverified_content: r } : { content: r }), i.Z.sendMessage(n.id, o, void 0, { contentInventoryEntry: t });
+var a = t(904245),
+    l = t(957730);
+function i(e) {
+    let { channel: n, content: t, entry: i } = e,
+        r = l.ZP.parse(n, t);
+    return a.Z.sendMessage(n.id, r, void 0, { contentInventoryEntry: { unverified_content: i } });
 }
--- chunks/790642.js
+++ chunks/790642.js
@@ -1,13 +1,9 @@
 n.d(t, {
     Io: function () {
-        return a;
-    },
-    bx: function () {
-        return o;
+        return i;
     }
 });
-var r = n(818083);
-let i = (0, r.B)({
+let r = (0, n(818083).B)({
     kind: 'user',
     id: '2024-06_content_entry_embed',
     label: 'Content Inventory Embed',
@@ -20,24 +16,7 @@ let i = (0, r.B)({
         }
     ]
 });
-function a(e) {
-    let { enabled: t } = i.getCurrentConfig({ location: e });
-    return t;
-}
-let s = (0, r.B)({
-    kind: 'user',
-    id: '2024-08_content_inventory_entry_signed_hash',
-    label: 'Check Signature On Content Inventory Embed',
-    defaultConfig: { enabled: !1 },
-    treatments: [
-        {
-            id: 1,
-            label: 'Enabled',
-            config: { enabled: !0 }
-        }
-    ]
-});
-function o(e) {
-    let { enabled: t } = s.getCurrentConfig({ location: e });
+function i(e) {
+    let { enabled: t } = r.getCurrentConfig({ location: e });
     return t;
 }
