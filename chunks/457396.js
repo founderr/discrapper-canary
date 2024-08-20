a9d8936180c5b4ffb560472480b036cc68e42d56
DiscrapperManager <admin@minerpl.xyz>
2024-08-20 23:56:55 +0000

    canaryRelease: 320410
    Main scripts: 3
    Lazy Loaded Scripts: 856
    Chunks:
     * Main Files: 11170
     * Scripts: 9899
    Strings: 18710
    Routes: 679
    Experiments Count: 373

--- chunks/457396.js
+++ chunks/457396.js
@@ -1,8 +1,9 @@
 n.d(t, {
     g: function () {
-        return U;
+        return k;
     }
-});
+}),
+    n(47120);
 var i,
     a,
     s = n(735250);
@@ -13,100 +14,107 @@ var r = n(91192),
     c = n(481060),
     d = n(515753),
     u = n(605236),
-    _ = n(594174),
-    h = n(351402),
-    E = n(78839),
-    m = n(626135),
-    I = n(74538),
-    g = n(502087),
-    p = n(647265),
-    T = n(650032),
-    S = n(104494),
-    f = n(639119),
-    C = n(655525),
-    N = n(314684),
-    A = n(52188),
-    v = n(248042),
-    Z = n(924540),
-    L = n(649765),
-    O = n(565626),
-    R = n(701910),
-    x = n(938736),
-    b = n(474936),
-    P = n(981631),
-    M = n(921944),
-    D = n(689938),
-    y = n(54463);
-let j = 'NitroTabButton';
-((i = a || (a = {}))[(i.BOGO_PROMOTION = 0)] = 'BOGO_PROMOTION'), (i[(i.TENURE_REWARD = 1)] = 'TENURE_REWARD'), (i[(i.NEW_PERKS_BADGE = 2)] = 'NEW_PERKS_BADGE'), (i[(i.DISCOUNT_OFFER = 3)] = 'DISCOUNT_OFFER'), (i[(i.TRIAL_OFFER = 4)] = 'TRIAL_OFFER'), (i[(i.LOCALIZED_PRICING = 5)] = 'LOCALIZED_PRICING'), (i[(i.REFERRAL_PROGRAM = 6)] = 'REFERRAL_PROGRAM');
-let U = (e) => {
+    _ = n(243778),
+    h = n(594174),
+    E = n(351402),
+    m = n(78839),
+    I = n(626135),
+    g = n(74538),
+    p = n(502087),
+    T = n(647265),
+    S = n(650032),
+    f = n(104494),
+    C = n(639119),
+    N = n(655525),
+    A = n(314684),
+    v = n(52188),
+    Z = n(248042),
+    L = n(924540),
+    O = n(649765),
+    R = n(565626),
+    x = n(701910),
+    b = n(938736),
+    P = n(227140),
+    M = n(474936),
+    D = n(981631),
+    y = n(921944),
+    j = n(689938),
+    U = n(54463);
+let G = 'NitroTabButton';
+((a = i || (i = {}))[(a.BOGO_PROMOTION = 0)] = 'BOGO_PROMOTION'), (a[(a.TENURE_REWARD = 1)] = 'TENURE_REWARD'), (a[(a.NEW_PERKS_BADGE = 2)] = 'NEW_PERKS_BADGE'), (a[(a.DISCOUNT_OFFER = 3)] = 'DISCOUNT_OFFER'), (a[(a.TRIAL_OFFER = 4)] = 'TRIAL_OFFER'), (a[(a.LOCALIZED_PRICING = 5)] = 'LOCALIZED_PRICING'), (a[(a.REFERRAL_PROGRAM = 6)] = 'REFERRAL_PROGRAM');
+let k = (e) => {
     let t,
         n,
         i,
-        { selected: a, route: U, locationState: G, ...k } = e,
-        w = (0, l.e7)([_.default], () => _.default.getCurrentUser()),
-        B = (0, l.e7)([E.ZP], () => E.ZP.getPremiumTypeSubscription()),
-        H = (0, I.Qo)(w, B),
-        V = (0, r.JA)('nitro'),
-        F = (0, f.N)(),
-        Y = (0, S.Ng)(),
-        W = (0, l.e7)([h.Z], () => h.Z.isLocalizedPromoEnabled) && null == F,
-        z = (0, v.Vi)(),
-        K = (0, O.ZP)(),
-        q = (0, N.$_)(),
-        Q = (0, x.Kn)(j),
-        X = (0, l.e7)([g.Z], () => g.Z.getCreatedAtOverride()),
-        J = null != X ? X : null == w ? void 0 : w.createdAt,
-        $ = a || null != F || null != Y || z || K || null != q || H,
-        { enabled: ee } = T._.useExperiment({ location: j }, { autoTrackExposure: !1 }),
-        et = ee && (null == F ? void 0 : F.trial_id) === b.a7,
-        en = (0, p.N9)({
+        { selected: a, route: k, locationState: w, ...B } = e,
+        H = (0, l.e7)([h.default], () => h.default.getCurrentUser()),
+        V = (0, l.e7)([m.ZP], () => m.ZP.getPremiumTypeSubscription()),
+        F = (0, g.Qo)(H, V),
+        Y = (0, r.JA)('nitro'),
+        W = (0, C.N)(),
+        z = (0, f.Ng)(),
+        K = (0, l.e7)([m.ZP], () => m.ZP.inReverseTrial()),
+        q = (0, l.e7)([E.Z], () => E.Z.isLocalizedPromoEnabled) && null == W,
+        Q = (0, Z.Vi)(),
+        X = (0, R.ZP)(),
+        J = (0, A.$_)(),
+        $ = (0, b.Kn)(G),
+        ee = (0, l.e7)([p.Z], () => p.Z.getCreatedAtOverride()),
+        et = null != ee ? ee : null == H ? void 0 : H.createdAt,
+        en = a || null != W || null != z || Q || X || null != J || F,
+        { enabled: ei } = S._.useExperiment({ location: G }, { autoTrackExposure: !1 }),
+        ea = ei && (null == W ? void 0 : W.trial_id) === M.a7,
+        es = (0, T.N9)({
             location: 'NitroTabButton',
-            showTabForOtherReasons: $
-        });
-    if (!($ = $ || (null != w && null != J && Date.now() - J.getTime() > en))) return null;
-    z
-        ? ((n = (0, s.jsx)(C.Z, {})), (i = 0))
-        : null != q
-          ? ((n = (0, s.jsx)(R.Z, { copy: q })), (i = 1))
-          : K
-            ? ((n = (0, s.jsx)(R.Z, {})), (i = 2))
-            : null != Y
-              ? ((n = (0, s.jsx)(Z.GN, {
-                    userDiscount: Y,
-                    isTabSelected: a,
-                    includesAmountOff: !1
-                })),
-                (i = 3))
-              : null == F || et
-                ? Q
-                    ? ((n = (0, s.jsx)(L.Z, {
-                          isSelected: a,
-                          onSelect: () => (0, u.EW)(o.z.REFERRAL_PROGRAM_PREMIUM_TAB_BADGE, { dismissAction: M.L.TAKE_ACTION })
-                      })),
-                      (i = 6))
-                    : W && ((n = (0, s.jsx)(A.k, { entryPoint: A.U.PrivateMessages })), !a && (t = y.localizeBadge), (i = 5))
-                : ((n = (0, s.jsx)(Z.$H, {
-                      trialOffer: F,
-                      isTabSelected: a
+            showTabForOtherReasons: en
+        }),
+        [er, el] = (0, _.US)(K ? [o.z.REVERSE_TRIAL_NITRO_TAB_BADGE] : [], void 0, !0);
+    if (!(en = en || (null != H && null != et && Date.now() - et.getTime() > es))) return null;
+    Q
+        ? ((n = (0, s.jsx)(N.Z, {})), (i = 0))
+        : null != er && er === o.z.REVERSE_TRIAL_NITRO_TAB_BADGE
+          ? ((n = (0, s.jsx)(P.Z, { copy: j.Z.Messages.PREMIUM_REVERSE_TRIAL_BADGE })), (i = 4))
+          : null != J
+            ? ((n = (0, s.jsx)(x.Z, { copy: J })), (i = 1))
+            : X
+              ? ((n = (0, s.jsx)(x.Z, {})), (i = 2))
+              : null != z
+                ? ((n = (0, s.jsx)(L.GN, {
+                      userDiscount: z,
+                      isTabSelected: a,
+                      includesAmountOff: !1
                   })),
-                  (i = 4));
-    let ei = (0, s.jsx)(d.Qj, {
+                  (i = 3))
+                : null == W || ea
+                  ? $
+                      ? ((n = (0, s.jsx)(O.Z, {
+                            isSelected: a,
+                            onSelect: () => (0, u.EW)(o.z.REFERRAL_PROGRAM_PREMIUM_TAB_BADGE, { dismissAction: y.L.TAKE_ACTION })
+                        })),
+                        (i = 6))
+                      : q && ((n = (0, s.jsx)(v.k, { entryPoint: v.U.PrivateMessages })), !a && (t = U.localizeBadge), (i = 5))
+                  : ((n = (0, s.jsx)(L.$H, {
+                        trialOffer: W,
+                        isTabSelected: a
+                    })),
+                    (i = 4));
+    let eo = (0, s.jsx)(d.Qj, {
         selected: a,
-        route: U,
+        route: k,
         icon: c.NitroWheelIcon,
-        text: D.Z.Messages.PREMIUM,
-        locationState: G,
+        text: j.Z.Messages.PREMIUM,
+        locationState: w,
         onClick: () => {
-            m.default.track(P.rMx.NITRO_TAB_VISITED, {
+            I.default.track(D.rMx.NITRO_TAB_VISITED, {
                 badge_decorator: i,
-                has_premium: H
-            });
+                has_premium: F
+            }),
+                null != er && el(y.L.USER_DISMISS);
         },
-        ...k,
-        ...V,
+        ...B,
+        ...Y,
         className: t,
         children: n
     });
-    return Q ? (0, s.jsx)(L.E, { children: ei }) : ei;
+    return $ ? (0, s.jsx)(O.E, { children: eo }) : eo;
 };
