05bb7940bda63cfd8c3a2affb412ebec00b4afdd
DiscrapperManager <admin@minerpl.xyz>
2024-08-20 17:44:12 +0000

    canaryRelease: 320136
    Main scripts: 3
    Lazy Loaded Scripts: 857
    Chunks:
     * Main Files: 11169
     * Scripts: 9893
    Strings: 18706
    Routes: 678
    Experiments Count: 372

--- chunks/435439.js
+++ chunks/435439.js
@@ -1,28 +1,37 @@
 l.d(n, {
     Z: function () {
-        return d;
+        return f;
     }
 });
 var t = l(735250),
     r = l(481060),
     i = l(374129),
     a = l(639351),
-    u = l(908841),
-    s = l(410441),
-    o = l(206583);
-let c = {
-    [o.kG.SPOTIFY]: u.Z,
-    [o.kG.CRUNCHYROLL]: r.CrunchyrollNeutralIcon,
-    [o.kG.XBOX]: a.Z,
-    [o.kG.PLAYSTATION]: i.Z
-};
-function d(e) {
-    let { type: n, ...l } = e,
-        r = c[n];
-    return null == r
-        ? null
-        : (0, t.jsx)(s.Z, {
-              Icon: r,
-              ...l
-          });
+    s = l(908841),
+    u = l(410441),
+    o = l(206583),
+    c = l(689938);
+let d = {
+        [o.kG.SPOTIFY]: s.Z,
+        [o.kG.CRUNCHYROLL]: r.CrunchyrollNeutralIcon,
+        [o.kG.XBOX]: a.Z,
+        [o.kG.PLAYSTATION]: i.Z
+    },
+    m = {
+        [o.kG.SPOTIFY]: () => c.Z.Messages.SPOTIFY,
+        [o.kG.CRUNCHYROLL]: () => c.Z.Messages.CRUNCHYROLL,
+        [o.kG.XBOX]: () => c.Z.Messages.XBOX,
+        [o.kG.PLAYSTATION]: () => c.Z.Messages.PLAYSTATION
+    };
+function f(e) {
+    var n;
+    let { type: l, 'aria-label': r, ...i } = e,
+        a = d[l];
+    if (null == a) return null;
+    let s = null != r ? r : null === (n = m[l]) || void 0 === n ? void 0 : n.call(m);
+    return (0, t.jsx)(u.Z, {
+        Icon: a,
+        ...i,
+        'aria-label': s
+    });
 }
--- chunks/879046.js
+++ chunks/879046.js
@@ -7,35 +7,39 @@ var t = l(442837),
     r = l(2052),
     i = l(99981),
     a = l(906732),
-    u = l(835473),
-    s = l(914010),
+    s = l(835473),
+    u = l(914010),
     o = l(561308),
     c = l(701488),
     d = l(689938);
 function m(e) {
     let { entry: n } = e,
-        l = (0, u.q)(n.extra.application_id),
+        l = (0, s.q)(n.extra.application_id),
         m = null == l ? void 0 : l.getIconURL(c.Si.LARGE),
-        f = (0, t.e7)([s.Z], () => s.Z.getGuildId()),
+        f = (0, t.e7)([u.Z], () => u.Z.getGuildId()),
+        E = n.extra.activity_name,
         p = (0, r.O)(),
-        { analyticsLocations: E } = (0, a.ZP)(),
-        v = (0, o.kr)(n) && !(0, o.n2)(n) ? d.Z.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYING : d.Z.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYED,
-        C = () => {
-            null != f &&
-                (0, i.q)({
-                    guildId: f,
-                    channelId: void 0,
-                    locationObject: p.location,
-                    initialSelectedApplicationId: n.extra.application_id,
-                    initialSlide: c.ag.SELECT_CHANNEL,
-                    analyticsLocations: E
-                });
+        { analyticsLocations: C } = (0, a.ZP)(),
+        N = (0, o.kr)(n) && !(0, o.n2)(n) ? d.Z.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYING : d.Z.Messages.MEMBER_LIST_CONTENT_POPOUT_USER_PLAYED,
+        v = {
+            onClick: () => {
+                null != f &&
+                    (0, i.q)({
+                        guildId: f,
+                        channelId: void 0,
+                        locationObject: p.location,
+                        initialSelectedApplicationId: n.extra.application_id,
+                        initialSlide: c.ag.SELECT_CHANNEL,
+                        analyticsLocations: C
+                    });
+            },
+            ariaDescription: d.Z.Messages.CONTENT_INVENTORY_OPEN_ACTIVITY_A11Y_DESCRIPTION.format({ title: E })
         };
     return {
         thumbnailUrl: m,
-        title: n.extra.activity_name,
-        onClickTitle: C,
-        onClickThumbnail: C,
-        userDescription: v
+        title: E,
+        titleClickable: v,
+        thumbnailClickable: v,
+        userDescription: N
     };
 }
