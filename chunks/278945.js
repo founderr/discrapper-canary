d5ed7d31abdf0aec7979ae22679fef6d71027568
DiscrapperManager <admin@minerpl.xyz>
2024-08-22 18:51:58 +0000

    canaryRelease: 321040
    Main scripts: 3
    Lazy Loaded Scripts: 858
    Chunks:
     * Main Files: 11172
     * Scripts: 9911
    Strings: 18725
    Routes: 680
    Experiments Count: 373

--- chunks/278945.js
+++ chunks/278945.js
@@ -1,6 +1,6 @@
 n.d(t, {
     r: function () {
-        return c;
+        return d;
     }
 });
 var i = n(355467),
@@ -8,12 +8,13 @@ var i = n(355467),
     s = n(295226),
     r = n(937579),
     l = n(533990),
-    o = n(474936);
-async function c(e) {
+    o = n(474936),
+    c = n(981631);
+async function d(e) {
     !a.ZP.hasFetchedMostRecentPremiumTypeSubscription() && (await (0, i.ou)());
     let t = a.ZP.getMostRecentPremiumTypeSubscription(),
         n = l.Z.getCurrentConfig({ location: 'announcementManager' }, { autoTrackExposure: !1 }).isFollowup;
-    if ((null == t ? void 0 : t.trialId) != null && o.h8.includes(t.trialId)) {
+    if ((null == t ? void 0 : t.status) === c.O0b.ENDED && (null == t ? void 0 : t.trialId) != null && o.h8.includes(t.trialId)) {
         if ((null == e && (await (0, r.T)()), null != s.Z.getUserTrialOffer(o.Rt))) return o.cd.REVERSE_TRIAL_FOLLOWUP_UPSELL;
         if (null != t && !n) return o.cd.REVERSE_TRIAL_ENDED_UPSELL;
     }
