f25bfe79948ba466c59ebf45742d108bae62e858
DiscrapperManager <admin@minerpl.xyz>
2024-08-22 01:36:36 +0000

    canaryRelease: 320878
    Main scripts: 3
    Lazy Loaded Scripts: 856
    Chunks:
     * Main Files: 11171
     * Scripts: 9901
    Strings: 18714
    Routes: 680
    Experiments Count: 372

--- chunks/863750.js
+++ chunks/863750.js
@@ -16,7 +16,6 @@ function c(e, t, n) {
     );
 }
 let l = {
-        disable_alpha_voice_panel: "Disable new Voice Panel UI: Currently the new voice ui is defaulted ON for staff.  It's not feature complete so if there's something you are missing, or you find a problem, you can disable it here.",
         enable_recently_active: 'Enable recently active channels',
         happening_main_tab: 'Enable Happening tab containing summaries',
         theme_setting_in_account_sheet: 'Show theme settings in the Account action sheet',
@@ -29,7 +28,7 @@ let l = {
         only_channel_screen: 'Down with PanelsView, rely on only ChannelScreen!'
     },
     a = {};
-class s extends (r = o.ZP.DeviceSettingsStore) {
+class u extends (r = o.ZP.DeviceSettingsStore) {
     getUserAgnosticState() {
         return { toggleStates: a };
     }
@@ -57,9 +56,9 @@ class s extends (r = o.ZP.DeviceSettingsStore) {
         });
     }
 }
-c(s, 'displayName', 'DevToolsDesignTogglesStore'),
-    c(s, 'persistKey', 'DevToolsDesignTogglesStore'),
-    (t.Z = new s(i.Z, {
+c(u, 'displayName', 'DevToolsDesignTogglesStore'),
+    c(u, 'persistKey', 'DevToolsDesignTogglesStore'),
+    (t.Z = new u(i.Z, {
         DEV_TOOLS_DESIGN_TOGGLE_SET: function (e) {
             a[e.toggle] = e.value;
         }
