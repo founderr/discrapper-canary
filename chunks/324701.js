f3e54cef52f6d8a1c2cf0b830327e805f1ea6a62
DiscrapperManager <admin@minerpl.xyz>
2024-08-22 18:05:57 +0000

    canaryRelease: 321003
    Main scripts: 3
    Lazy Loaded Scripts: 858
    Chunks:
     * Main Files: 11172
     * Scripts: 9911
    Strings: 18725
    Routes: 680
    Experiments Count: 373

--- chunks/324701.js
+++ chunks/324701.js
@@ -1,30 +1,47 @@
 n.d(t, {
     x: function () {
-        return o;
+        return u;
     },
     z: function () {
-        return l;
+        return c;
     }
 });
 var i = n(481060),
-    s = n(192720),
-    a = n(689938);
-function r(e, t) {
-    return t ? (e ? i.ToastType.CLOCK : i.ToastType.BOOKMARK) : i.ToastType.FAILURE;
-}
-async function l(e) {
+    s = n(668781),
+    a = n(585483),
+    r = n(192720),
+    l = n(981631),
+    o = n(689938);
+async function c(e) {
     let { displayToast: t = !1, ...n } = e,
-        l = await (0, s.XA)(n);
-    if (null == l || (null != l && t)) {
-        var o;
-        (0, i.showToast)((0, i.createToast)(((o = null != n.dueAt), null != l ? (o ? a.Z.Messages.MESSAGE_REMINDERS_CREATE_SUCCESS : a.Z.Messages.MESSAGE_BOOKMARKS_CREATE_SUCCESS) : o ? a.Z.Messages.MESSAGE_REMINDERS_CREATE_FAIL : a.Z.Messages.MESSAGE_BOOKMARKS_CREATE_FAIL), r(null != n.dueAt, null != l)));
+        c = await (0, r.XA)(n).catch((e) => {
+            var t, n, r;
+            return (null == e ? void 0 : null === (t = e.body) || void 0 === t ? void 0 : t.code) === l.evJ.TOO_MANY_SAVED_MESSAGES
+                ? (s.Z.show({
+                      title: o.Z.Messages.FOR_LATER_TOO_MANY_TITLE,
+                      body: o.Z.Messages.FOR_LATER_TOO_MANY_BODY.format({ max: '200' }),
+                      confirmText: o.Z.Messages.OKAY,
+                      cancelText: o.Z.Messages.FOR_LATER_MANAGE_MESSAGES,
+                      onCancel: () => a.S.dispatch(l.CkL.TOGGLE_FOR_LATER)
+                  }),
+                  null)
+                : ((0, i.showToast)((0, i.createToast)(null !== (r = null == e ? void 0 : null === (n = e.body) || void 0 === n ? void 0 : n.message) && void 0 !== r ? r : o.Z.Messages.ERROR_GENERIC_TITLE, i.ToastType.FAILURE)), null);
+        });
+    if (t && null != c) {
+        let e = null != n.dueAt ? o.Z.Messages.MESSAGE_REMINDERS_CREATE_SUCCESS : o.Z.Messages.MESSAGE_BOOKMARKS_CREATE_SUCCESS,
+            t = null != n.dueAt ? i.ToastType.CLOCK : i.ToastType.BOOKMARK;
+        (0, i.showToast)((0, i.createToast)(e, t));
     }
 }
-async function o(e) {
+async function u(e) {
     let { displayToast: t = !1, ...n } = e,
-        l = await (0, s.ep)(n);
-    if (null == l || (null != l && t)) {
-        var o;
-        (0, i.showToast)((0, i.createToast)(((o = null != n.dueAt), null != l ? (o ? a.Z.Messages.MESSAGE_REMINDERS_DELETE_SUCCESS : a.Z.Messages.MESSAGE_BOOKMARKS_DELETE_SUCCESS) : o ? a.Z.Messages.MESSAGE_REMINDERS_DELETE_FAIL : a.Z.Messages.MESSAGE_BOOKMARKS_DELETE_FAIL), r(null != n.dueAt, null != l)));
+        s = await (0, r.ep)(n).catch((e) => {
+            var t, n;
+            return (0, i.showToast)((0, i.createToast)(null !== (n = null == e ? void 0 : null === (t = e.body) || void 0 === t ? void 0 : t.message) && void 0 !== n ? n : o.Z.Messages.ERROR_GENERIC_TITLE, i.ToastType.FAILURE)), null;
+        });
+    if (t && null != s) {
+        let e = null != n.dueAt ? o.Z.Messages.MESSAGE_REMINDERS_DELETE_SUCCESS : o.Z.Messages.MESSAGE_BOOKMARKS_DELETE_SUCCESS,
+            t = null != n.dueAt ? i.ToastType.CLOCK : i.ToastType.BOOKMARK;
+        (0, i.showToast)((0, i.createToast)(e, t));
     }
 }
