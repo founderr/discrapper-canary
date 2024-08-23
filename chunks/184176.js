48af46c9a5262b76fdd8bc78f78e10d030527aae
DiscrapperManager <admin@minerpl.xyz>
2024-08-23 17:17:20 +0000

    canaryRelease: 321379
    Main scripts: 3
    Lazy Loaded Scripts: 859
    Chunks:
     * Main Files: 11169
     * Scripts: 9920
    Strings: 18729
    Routes: 680
    Experiments Count: 378

--- chunks/184176.js
+++ chunks/184176.js
@@ -1,59 +1,47 @@
 var s = n(735250);
 n(470079);
-var a = n(512722),
-    r = n.n(a),
-    i = n(442837),
-    l = n(481060),
-    o = n(100527),
-    c = n(906732),
-    d = n(963249),
-    _ = n(594174),
-    u = n(78839),
-    E = n(823188),
-    T = n(474936),
-    I = n(981631),
-    R = n(689938),
-    C = n(653875);
+var a = n(442837),
+    r = n(481060),
+    i = n(100527),
+    l = n(906732),
+    o = n(963249),
+    c = n(78839),
+    d = n(823188),
+    _ = n(474936),
+    u = n(981631),
+    E = n(689938),
+    T = n(653875);
 t.Z = () => {
-    var e;
-    let t = (0, i.e7)([_.default], () => _.default.getCurrentUser());
-    r()(t, 'UserStore.getCurrentUser() should not be null');
-    let n = null !== (e = t.globalName) && void 0 !== e ? e : t.username,
-        a = (0, i.e7)([u.ZP], () => u.ZP.getPremiumTypeSubscription()),
-        g = (null == a ? void 0 : a.trialId) === T.dO ? '1' : '2',
-        p = (0, c.ZP)([o.Z.PREMIUM_MARKETING_REVERSE_TRIAL_BANNER]);
+    let e = (0, a.e7)([c.ZP], () => c.ZP.getPremiumTypeSubscription()),
+        t = (null == e ? void 0 : e.trialId) === _.dO ? '1' : '2',
+        n = (0, l.ZP)([i.Z.PREMIUM_MARKETING_REVERSE_TRIAL_BANNER]);
     return (0, s.jsxs)('div', {
-        className: C.banner,
+        className: T.banner,
         children: [
-            (0, s.jsx)(E.Cy, {
-                text: R.Z.Messages.PREMIUM_TIER_CARD_TRIAL_ACTIVATED,
-                className: C.trialPill,
-                colorOptions: E.VE.PREMIUM_TIER_2_WHITE_FILL
-            }),
-            (0, s.jsx)(l.Heading, {
-                color: 'always-white',
-                variant: 'heading-lg/extrabold',
-                children: R.Z.Messages.REVERSE_TRIAL_NITRO_HOME_HEADER_TITLE.format({ displayName: n })
+            (0, s.jsx)(d.Cy, {
+                text: E.Z.Messages.PREMIUM_TIER_CARD_TRIAL_ACTIVATED,
+                className: T.trialPill,
+                colorOptions: d.VE.PREMIUM_TIER_2_WHITE_FILL
             }),
             (0, s.jsx)('div', {
-                className: C.body,
-                children: (0, s.jsx)(l.Text, {
+                className: T.body,
+                children: (0, s.jsx)(r.Text, {
                     color: 'always-white',
                     variant: 'text-md/medium',
-                    children: R.Z.Messages.REVERSE_TRIAL_NITRO_HOME_HEADER_BODY.format({ duration: g })
+                    children: E.Z.Messages.REVERSE_TRIAL_NITRO_HOME_HEADER_BODY_NEW.format({ weeks: t })
                 })
             }),
-            (0, s.jsx)(l.Button, {
+            (0, s.jsx)(r.Button, {
                 onClick: () => {
-                    (0, d.Z)({
-                        initialPlanId: T.Xh.PREMIUM_MONTH_TIER_2,
-                        analyticsLocations: p.analyticsLocations,
-                        analyticsObject: { object: I.qAy.BUTTON_CTA }
+                    (0, o.Z)({
+                        initialPlanId: _.Xh.PREMIUM_MONTH_TIER_2,
+                        analyticsLocations: n.analyticsLocations,
+                        analyticsObject: { object: u.qAy.BUTTON_CTA }
                     });
                 },
-                look: l.ButtonLooks.OUTLINED,
-                color: l.ButtonColors.WHITE,
-                children: R.Z.Messages.REVERSE_TRIAL_NITRO_HOME_HEADER_BUTTON_LABEL
+                look: r.ButtonLooks.OUTLINED,
+                color: r.ButtonColors.WHITE,
+                children: E.Z.Messages.REVERSE_TRIAL_NITRO_HOME_HEADER_BUTTON_LABEL
             })
         ]
     });
