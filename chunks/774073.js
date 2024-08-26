2012b9e364524fb0017fc5ce369de710fc36445c
DiscrapperManager <admin@minerpl.xyz>
2024-08-26 19:14:18 +0000

    canaryRelease: 321833
    Main scripts: 3
    Lazy Loaded Scripts: 860
    Chunks:
     * Main Files: 11165
     * Scripts: 9928
    Strings: 18738
    Routes: 680
    Experiments Count: 372

--- chunks/774073.js
+++ chunks/774073.js
@@ -1,9 +1,9 @@
 n.d(t, {
     W1: function () {
-        return I;
+        return T;
     },
     z6: function () {
-        return m;
+        return g;
     }
 }),
     n(653041);
@@ -12,44 +12,46 @@ var r,
     a = n(470079),
     s = n(512722),
     o = n.n(s),
-    l = n(683076),
-    u = n(442837),
-    c = n(224706),
-    d = n(669764),
-    _ = n(626135),
-    E = n(539746),
-    f = n(981631);
+    l = n(392711),
+    u = n.n(l),
+    c = n(683076),
+    d = n(442837),
+    _ = n(224706),
+    E = n(669764),
+    f = n(626135),
+    h = n(539746),
+    p = n(981631);
 ((i = r || (r = {})).NoMatch = 'no match'), (i.NSFW = 'nsfw'), (i.NoSummary = 'no summary'), (i.NoReleaseDate = 'no release date'), (i.NoCoverImage = 'no cover image');
-let h = function (e, t) {
+let I = u().throttle(function (e, t) {
         let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : [],
             r = arguments.length > 3 ? arguments[3] : void 0;
-        _.default.track(f.rMx.GAME_PROFILE_ENTRY_POINT_AVAILABLE, {
+        f.default.track(p.rMx.GAME_PROFILE_ENTRY_POINT_AVAILABLE, {
             game_profile_available: e,
             application_id: t,
             rejection_reason: n,
             source: r
         });
-    },
-    p = (e) => {
+    }, 2000),
+    m = (e) => {
         let t = [];
-        return null == e ? ['no match'] : (e.themes.includes(l.i.EROTIC) && t.push('nsfw'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.coverImageUrl || 0 === e.coverImageUrl.length) && t.push('no cover image'), t);
+        return null == e ? ['no match'] : (e.themes.includes(c.i.EROTIC) && t.push('nsfw'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.coverImageUrl || 0 === e.coverImageUrl.length) && t.push('no cover image'), t);
     },
-    I = (e) => 0 === p(e).length,
-    m = (e) => 0 === p(d.Z.getGame(e)).length;
+    T = (e) => 0 === m(e).length,
+    g = (e) => 0 === m(E.Z.getGame(e)).length;
 t.ZP = (e) => {
     let { location: t, applicationId: n = '', autoTrackExposure: r = !0, source: i, trackEntryPointImpression: s = !0 } = e,
-        l = (0, E.xG)(t, r);
+        l = (0, h.xG)(t, r);
     a.useEffect(() => {
-        l && null != n && '' !== n && c.Z.getDetectableGamesSupplemental([n]);
+        l && null != n && '' !== n && _.Z.getDetectableGamesSupplemental([n]);
     }, [n, l]);
-    let _ = (0, u.e7)([d.Z], () => d.Z.getGame(n)),
-        f = a.useMemo(() => null != _ && I(_), [_]);
+    let u = (0, d.e7)([E.Z], () => E.Z.getGame(n)),
+        c = a.useMemo(() => null != u && T(u), [u]);
     return (
         a.useEffect(() => {
-            l && s && !1 === d.Z.isFetching(n) && (o()(null != i, 'Cannot track a Game Profile Entry Point Impressions without a source.'), h(f, n, p(_), i));
-        }, [f, n, l, _, i, s]),
+            l && s && !1 === E.Z.isFetching(n) && (o()(null != i, 'Cannot track a Game Profile Entry Point Impressions without a source.'), I(c, n, m(u), i));
+        }, [c, n, l, u, i, s]),
         {
-            shouldOpenGameProfile: l && f,
+            shouldOpenGameProfile: l && c,
             applicationId: n
         }
     );
