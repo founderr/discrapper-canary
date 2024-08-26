3df0b4e3c502e5083d9c4565486e76a2d124b9d3
DiscrapperManager <admin@minerpl.xyz>
2024-08-26 21:38:38 +0000

    canaryRelease: 321939
    Main scripts: 3
    Lazy Loaded Scripts: 861
    Chunks:
     * Main Files: 11167
     * Scripts: 9930
    Strings: 18745
    Routes: 680
    Experiments Count: 373

--- chunks/675207.js
+++ chunks/675207.js
@@ -1,10 +1,10 @@
 t.r(n),
     t.d(n, {
         VerifyAccountLoading: function () {
-            return N;
+            return m;
         },
         default: function () {
-            return m;
+            return N;
         }
     }),
     t(610138),
@@ -13,8 +13,8 @@ t.r(n),
     t(815648),
     t(47120),
     t(757143);
-var r = t(735250),
-    o = t(470079),
+var o = t(735250),
+    r = t(470079),
     c = t(266067),
     l = t(481060),
     s = t(457330),
@@ -24,74 +24,77 @@ var r = t(735250),
     d = t(981631),
     f = t(689938),
     p = t(934484);
-let g = null != window.opener,
-    C = (e) => new URLSearchParams(e.search);
-function m() {
+let C = null != window.opener,
+    g = (e) => new URLSearchParams(e.search);
+function N() {
     var e;
     let n = (0, c.k6)(),
-        t = C((0, c.TH)()),
+        t = g((0, c.TH)()),
         l = t.get('code'),
         f = t.get('oauth_verifier'),
         p = null !== (e = t.get('state')) && void 0 !== e ? e : '',
-        m = t.get('loading'),
+        N = t.get('loading'),
         { type: v } = (0, c.UO)(),
         b = (0, u.vJ)(v),
-        h = null == f ? (null != l ? l : '') : f;
-    return (o.useEffect(() => {
+        O = null == f ? (null != l ? l : '') : f;
+    return (r.useEffect(() => {
         let e;
-        if (null != m) return;
+        if (null != N) return;
         for (let n of t.keys()) {
             if (!!n.startsWith('openid.')) null == e && (e = {}), (e[n] = t.get(n));
         }
-        let r = (e) => {
-                let { status: r, body: o } = e;
+        let o = (e) => {
+                let { status: o, body: r } = e;
                 if (null != l) {
-                    if (null == o ? void 0 : o.redirect) return n.replace(o.redirect);
-                    if ([200, 204].includes(r)) {
-                        n.replace(d.Z5c.CONNECTIONS_SUCCESS(l)), g && window.close();
+                    if (null == r ? void 0 : r.redirect) {
+                        n.replace(d.Z5c.CONNECTIONS_SUCCESS(l)), (window.location = r.redirect);
+                        return;
+                    }
+                    if ([200, 204].includes(o)) {
+                        n.replace(d.Z5c.CONNECTIONS_SUCCESS(l)), C && window.close();
                         return;
                     }
                     n.replace(''.concat(d.Z5c.CONNECTIONS_ERROR(l), '?').concat(t.toString()));
                 }
             },
-            o = {
-                code: h,
+            r = {
+                code: O,
                 openid_params: e,
                 state: p
             },
             c = (e) => {
-                null != l && i.Z.isSupported(l) && s.Z.callback(l, o, e).then(r, r);
+                null != l && i.Z.isSupported(l) && s.Z.callback(l, r, e).then(o, o);
             };
-        if (g) {
+        if (C) {
             c(!1);
             return;
         }
         let l = (0, u.vJ)(v);
         a.default
             .request(d.Etm.CONNECTIONS_CALLBACK, {
-                ...o,
+                ...r,
                 providerType: l
             })
-            .then(r, (e) => c('RPCError' !== e.name))
+            .then(o, (e) => c('RPCError' !== e.name))
             .then(() => a.default.disconnect());
-    }, [h, n, m, v, t, p]),
+    }, [O, n, N, v, t, p]),
     null != b && i.Z.isSupported(b))
-        ? (0, r.jsx)(N, { platformType: b })
+        ? (0, o.jsx)(m, { platformType: b })
         : null;
 }
-function N(e) {
+function m(e) {
     let { platformType: n } = e;
-    return (0, r.jsxs)(u.UV, {
+    return (0, o.jsxs)(u.UV, {
         platformType: n,
         children: [
-            (0, r.jsx)('div', {
+            (0, o.jsx)('div', {
                 className: p.message,
                 children: f.Z.Messages.CONNECTED_ACCOUNT_VERIFYING.format({ name: platform.name })
             }),
-            (0, r.jsx)(l.Button, {
+            (0, o.jsx)(l.Button, {
                 className: p.btn,
                 disabled: !0,
-                children: (0, r.jsx)(l.Spinner, { itemClassName: p.spinnerItem })
+                children: (0, o.jsx)(l.Spinner, { itemClassName: p.spinnerItem })
             })
         ]
     });
