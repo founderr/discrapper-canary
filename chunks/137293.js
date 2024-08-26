57e6f3dbfeefb6c8e846ceef4fba3aecdf44a32c
DiscrapperManager <admin@minerpl.xyz>
2024-08-26 19:58:30 +0000

    canaryRelease: 321866
    Main scripts: 3
    Lazy Loaded Scripts: 860
    Chunks:
     * Main Files: 11165
     * Scripts: 9928
    Strings: 18739
    Routes: 680
    Experiments Count: 372

---
+++ chunks/137293.js
@@ -0,0 +1,17 @@
+let { createLoader: n } = t(585668),
+    a = n(
+        {
+            JJ0wpK: 'INTL_TESTING_FORM_HEADER',
+            uczI4u: 'INTL_TESTING_VARIABLE_LINK',
+            rdfRys: 'INTL_TESTING_CODE',
+            HMvECw: 'INTL_TESTING_DESCRIPTION',
+            'rmps8/': 'INTL_TESTING_STATIC_LINK',
+            XOdbAw: 'INTL_TESTING_HOOK'
+        },
+        {
+            fr: () => t.e('22610').then(t.t.bind(t, 969358, 19)),
+            'en-US': () => t.e('62411').then(t.bind(t, 174838))
+        },
+        'en-US'
+    );
+s.Z = a.getBinds();
---
+++ chunks/941929.js
@@ -0,0 +1,10 @@
+let { createLoader: n } = t(585668),
+    a = n(
+        { swfLzc: 'INTL_TEST_SECOND_FILE' },
+        {
+            fr: () => t.e('22610').then(t.t.bind(t, 969358, 19)),
+            'en-US': () => t.e('87539').then(t.bind(t, 824964))
+        },
+        'en-US'
+    );
+s.Z = a.getBinds();
