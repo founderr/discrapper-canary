781cb2b4ce001760d7f3961d4247bc436f0b2cd3
DiscrapperManager <admin@minerpl.xyz>
2024-08-27 16:19:08 +0000

    canaryRelease: 322185
    Main scripts: 3
    Lazy Loaded Scripts: 862
    Chunks:
     * Main Files: 11166
     * Scripts: 9934
    Strings: 18779
    Routes: 680
    Experiments Count: 374

--- chunks/49175.js
+++ chunks/49175.js
@@ -4,5 +4,7 @@ e.exports = {
     searchBar: 'searchBar_e0840f',
     sectionTitle: 'sectionTitle_e0840f',
     clearButton: 'clearButton_e0840f',
-    searchEmptyState: 'searchEmptyState_e0840f'
+    searchEmptyState: 'searchEmptyState_e0840f',
+    viewSpamButton: 'viewSpamButton_e0840f',
+    viewSpamButtonColor: 'viewSpamButtonColor_e0840f'
 };
---
+++ chunks/570828.js
@@ -0,0 +1,14 @@
+e.exports = {
+    modal: 'modal_e985aa',
+    heading: 'heading_e985aa',
+    closeButton: 'closeButton_e985aa',
+    modalContent: 'modalContent_e985aa',
+    modalFooter: 'modalFooter_e985aa',
+    clearAllButton: 'clearAllButton_e985aa',
+    spamRequest: 'spamRequest_e985aa',
+    listItem: 'listItem_e985aa',
+    active: 'active_e985aa',
+    listItemContents: 'listItemContents_e985aa',
+    userInfo: 'userInfo_e985aa',
+    actions: 'actions_e985aa'
+};
