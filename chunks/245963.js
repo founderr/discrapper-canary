6361f295b89094bb55366181f80133fd37875242
DiscrapperManager <admin@minerpl.xyz>
2024-08-27 21:38:18 +0000

    canaryRelease: 322386
    Main scripts: 3
    Lazy Loaded Scripts: 863
    Chunks:
     * Main Files: 11170
     * Scripts: 9935
    Strings: 18781
    Routes: 680
    Experiments Count: 375

---
+++ chunks/245963.js
@@ -0,0 +1,10 @@
+let { createLoader: n } = t(289001),
+    a = n(
+        { swfLzc: 'INTL_TEST_SECOND_FILE' },
+        {
+            fr: () => t.e('95670').then(t.t.bind(t, 799026, 19)),
+            'en-US': () => t.e('80952').then(t.bind(t, 680303))
+        },
+        'en-US'
+    );
+s.Z = a.getBinds();
--- chunks/824964.js
+++
@@ -1,1 +0,0 @@
-a.r(e), (e.default = JSON.parse('{"swfLzc":[{"type":0,"value":"This message comes from a separate file"}]}'));
