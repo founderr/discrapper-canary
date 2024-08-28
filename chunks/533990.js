8bbd0a962e8c33908c80203997543cf526236be4
DiscrapperManager <admin@minerpl.xyz>
2024-08-28 22:33:38 +0000

    canaryRelease: 322779
    Main scripts: 3
    Lazy Loaded Scripts: 866
    Chunks:
     * Main Files: 11176
     * Scripts: 9711
    Strings: 18786
    Routes: 681
    Experiments Count: 378

--- chunks/533990.js
+++ chunks/533990.js
@@ -4,15 +4,26 @@ let i = (0, n(818083).B)({
     label: 'Reverse Trials',
     defaultConfig: {
         enabled: !1,
-        isFollowup: !1
+        isFollowup: !1,
+        inExperiment: !1
     },
     treatments: [
+        {
+            id: 0,
+            label: 'Users receives no trial.',
+            config: {
+                enabled: !1,
+                isFollowup: !1,
+                inExperiment: !0
+            }
+        },
         {
             id: 1,
             label: 'Users recieve 2 week trial.',
             config: {
                 enabled: !0,
-                isFollowup: !1
+                isFollowup: !1,
+                inExperiment: !0
             }
         },
         {
@@ -20,7 +31,8 @@ let i = (0, n(818083).B)({
             label: 'User recieves 1 week trial and 1 week followup trial',
             config: {
                 enabled: !0,
-                isFollowup: !0
+                isFollowup: !0,
+                inExperiment: !0
             }
         },
         {
@@ -28,7 +40,8 @@ let i = (0, n(818083).B)({
             label: 'Users recieve 2 week trial.',
             config: {
                 enabled: !0,
-                isFollowup: !1
+                isFollowup: !1,
+                inExperiment: !0
             }
         },
         {
@@ -36,7 +49,8 @@ let i = (0, n(818083).B)({
             label: 'User recieves 1 week trial and 1 week followup trial',
             config: {
                 enabled: !0,
-                isFollowup: !0
+                isFollowup: !0,
+                inExperiment: !0
             }
         }
     ]
