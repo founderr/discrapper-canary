63aec9852107da954f31699b1922f03b04609dc1
DiscrapperManager <admin@minerpl.xyz>
2024-08-29 01:09:05 +0000

    canaryRelease: 322865
    Main scripts: 3
    Lazy Loaded Scripts: 869
    Chunks:
     * Main Files: 11176
     * Scripts: 9714
    Strings: 18789
    Routes: 681
    Experiments Count: 378

--- chunks/290274.js
+++ chunks/290274.js
@@ -1,67 +1,72 @@
 n.d(t, {
     Z: function () {
-        return f;
+        return h;
     }
-});
-var r = n(735250);
-n(470079);
-var i = n(120356),
-    a = n.n(i),
-    s = n(481060),
-    o = n(100527),
-    l = n(906732),
-    u = n(27144),
-    c = n(318134),
-    d = n(848966),
-    _ = n(689938),
-    E = n(139665);
-function f(e) {
-    let { user: t, activity: n, entry: i, display: f, className: h, onSelect: p, onClose: I, onRequestOpen: m } = e,
-        { newActivityCardsEnabled: T } = (0, u.z)({ location: 'UserProfileActivityContextMenu' }),
-        { analyticsLocations: g } = (0, l.ZP)(o.Z.USER_PROFILE_ACTIVITY_CONTEXT_MENU),
-        { onAction: S } = (0, c.Z)({
-            display: f,
+}),
+    n(47120);
+var r = n(735250),
+    i = n(470079),
+    a = n(120356),
+    s = n.n(a),
+    o = n(481060),
+    l = n(100527),
+    u = n(906732),
+    c = n(27144),
+    d = n(318134),
+    _ = n(848966),
+    E = n(689938),
+    f = n(139665);
+function h(e) {
+    let { user: t, activity: n, entry: a, display: h, className: p, onSelect: I, onClose: m, onRequestOpen: T } = e,
+        [g, S] = i.useState(!1),
+        { newActivityCardsEnabled: A } = (0, c.z)({ location: 'UserProfileActivityContextMenu' }),
+        { analyticsLocations: N } = (0, u.ZP)(l.Z.USER_PROFILE_ACTIVITY_CONTEXT_MENU),
+        { onAction: O } = (0, d.Z)({
+            display: h,
             user: t,
             activity: n,
-            entry: i,
-            analyticsLocations: g
+            entry: a,
+            analyticsLocations: N
         }),
-        A = (0, d.Z)({
-            entry: i,
+        R = (0, _.Z)({
+            entry: a,
             activity: n,
             user: t,
-            display: f,
-            onClose: I,
-            onAction: S
+            display: h,
+            onClose: m,
+            onAction: O,
+            isMenuOpen: g
         });
-    return !T || 0 === A.length || t.bot
+    return !A || 0 === R.length || t.bot
         ? null
-        : (0, r.jsx)(s.Popout, {
+        : (0, r.jsx)(o.Popout, {
               align: 'top',
               position: 'right',
               disablePointerEvents: !1,
               onRequestOpen: () => {
-                  S({ action: 'OPEN_MENU' }), null == m || m();
+                  O({ action: 'OPEN_MENU' }), S(!0), null == T || T();
               },
               renderPopout: (e) => {
                   let { closePopout: t } = e;
-                  return (0, r.jsx)(s.Menu, {
+                  return (0, r.jsx)(o.Menu, {
                       navId: 'content-inventory-context',
-                      onClose: t,
-                      'aria-label': _.Z.Messages.USER_PROFILE_ACTIVITY_ACTION_MENU_A11Y_LABEL,
-                      onSelect: p,
-                      children: (0, r.jsx)(s.MenuGroup, { children: A })
+                      onClose: () => {
+                          t(), S(!1);
+                      },
+                      'aria-label': E.Z.Messages.USER_PROFILE_ACTIVITY_ACTION_MENU_A11Y_LABEL,
+                      onSelect: I,
+                      children: (0, r.jsx)(o.MenuGroup, { children: R })
                   });
               },
               children: (e) =>
-                  (0, r.jsx)(s.Clickable, {
+                  (0, r.jsx)(o.Clickable, {
                       ...e,
                       onContextMenu: (t) => {
                           t.preventDefault(), e.onClick(t);
                       },
-                      className: a()(E.contextMenu, h),
-                      children: (0, r.jsx)(s.MoreHorizontalIcon, {
-                          color: s.tokens.colors.INTERACTIVE_NORMAL,
+                      className: s()(f.contextMenu, p),
+                      children: (0, r.jsx)(o.MoreHorizontalIcon, {
+                          color: o.tokens.colors.INTERACTIVE_NORMAL,
                           size: 'xs'
                       })
                   })
--- chunks/774073.js
+++ chunks/774073.js
@@ -1,9 +1,9 @@
 n.d(t, {
     W1: function () {
-        return T;
+        return I;
     },
     z6: function () {
-        return g;
+        return m;
     }
 }),
     n(653041);
@@ -12,46 +12,45 @@ var r,
     a = n(470079),
     s = n(512722),
     o = n.n(s),
-    l = n(392711),
-    u = n.n(l),
-    c = n(683076),
-    d = n(442837),
-    _ = n(224706),
-    E = n(669764),
-    f = n(626135),
-    h = n(539746),
-    p = n(981631);
+    l = n(683076),
+    u = n(442837),
+    c = n(224706),
+    d = n(669764),
+    _ = n(626135),
+    E = n(539746),
+    f = n(981631);
 ((i = r || (r = {})).NoMatch = 'no match'), (i.NSFW = 'nsfw'), (i.NoSummary = 'no summary'), (i.NoReleaseDate = 'no release date'), (i.NoCoverImage = 'no cover image');
-let I = u().throttle(function (e, t) {
+let h = function (e, t) {
         let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : [],
             r = arguments.length > 3 ? arguments[3] : void 0;
-        f.default.track(p.rMx.GAME_PROFILE_ENTRY_POINT_AVAILABLE, {
+        _.default.track(f.rMx.GAME_PROFILE_ENTRY_POINT_AVAILABLE, {
             game_profile_available: e,
             application_id: t,
             rejection_reason: n,
             source: r
         });
-    }, 2000),
-    m = (e) => {
-        let t = [];
-        return null == e ? ['no match'] : (e.themes.includes(c.i.EROTIC) && t.push('nsfw'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.coverImageUrl || 0 === e.coverImageUrl.length) && t.push('no cover image'), t);
     },
-    T = (e) => 0 === m(e).length,
-    g = (e) => 0 === m(E.Z.getGame(e)).length;
+    p = (e) => {
+        let t = [];
+        return null == e ? ['no match'] : (e.themes.includes(l.i.EROTIC) && t.push('nsfw'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.summary || 0 === e.summary.length) && t.push('no summary'), (null == e.coverImageUrl || 0 === e.coverImageUrl.length) && t.push('no cover image'), t);
+    },
+    I = (e) => 0 === p(e).length,
+    m = (e) => 0 === p(d.Z.getGame(e)).length;
 t.ZP = (e) => {
     let { location: t, applicationId: n = '', autoTrackExposure: r = !0, source: i, trackEntryPointImpression: s = !0 } = e,
-        l = (0, h.xG)(t, r);
+        l = a.useRef(!1),
+        _ = (0, E.xG)(t, r);
     a.useEffect(() => {
-        l && null != n && '' !== n && _.Z.getDetectableGamesSupplemental([n]);
-    }, [n, l]);
-    let u = (0, d.e7)([E.Z], () => E.Z.getGame(n)),
-        c = a.useMemo(() => null != u && T(u), [u]);
+        _ && null != n && '' !== n && c.Z.getDetectableGamesSupplemental([n]);
+    }, [n, _]);
+    let f = (0, u.e7)([d.Z], () => d.Z.getGame(n)),
+        m = a.useMemo(() => (null != f ? I(f) : null), [f]);
     return (
         a.useEffect(() => {
-            l && s && !1 === E.Z.isFetching(n) && (o()(null != i, 'Cannot track a Game Profile Entry Point Impressions without a source.'), I(c, n, m(u), i));
-        }, [c, n, l, u, i, s]),
+            !l.current && _ && s && !1 === d.Z.isFetching(n) && null != f && null != m && (o()(null != i, 'Cannot track a Game Profile Entry Point Impressions without a source.'), h(m, n, p(f), i), (l.current = !0));
+        }, [m, n, _, f, i, s]),
         {
-            shouldOpenGameProfile: l && c,
+            shouldOpenGameProfile: _ && !0 === m,
             applicationId: n
         }
     );
