a82852b993e6bb0228459ded08b5d5357c56fac6
DiscrapperManager <admin@minerpl.xyz>
2024-08-30 17:16:33 +0000

    canaryRelease: 323395
    Main scripts: 3
    Lazy Loaded Scripts: 871
    Chunks:
     * Main Files: 11177
     * Scripts: 9695
    Strings: 18821
    Routes: 681
    Experiments Count: 377

--- chunks/388146.js
+++
@@ -1,7 +0,0 @@
-e.exports = {
-    dot: 'dot_ccd671',
-    maskPopout: 'maskPopout_ccd671',
-    maskSecondary: 'maskSecondary_ccd671',
-    maskBlack: 'maskBlack_ccd671',
-    alert: 'alert_ccd671'
-};
---
+++ chunks/391347.js
@@ -0,0 +1,7 @@
+e.exports = {
+    dot: 'dot_d373b2',
+    maskPopout: 'maskPopout_d373b2',
+    maskSecondary: 'maskSecondary_d373b2',
+    maskBlack: 'maskBlack_d373b2',
+    alert: 'alert_d373b2'
+};
