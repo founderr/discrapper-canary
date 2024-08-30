95cff23e6f456514b840ef09b96ab0096d7598a5
DiscrapperManager <admin@minerpl.xyz>
2024-08-30 00:02:55 +0000

    canaryRelease: 323255
    Main scripts: 3
    Lazy Loaded Scripts: 868
    Chunks:
     * Main Files: 11177
     * Scripts: 9690
    Strings: 18811
    Routes: 681
    Experiments Count: 376

--- chunks/602147.js
+++ chunks/602147.js
@@ -1,62 +1,42 @@
 n.d(t, {
     o: function () {
-        return C;
+        return _;
     }
 });
 var i = n(735250),
     l = n(470079),
-    r = n(704215),
-    a = n(481060),
-    s = n(212093),
-    o = n(963202),
-    c = n(605236),
-    u = n(831565),
-    d = n(703656),
-    h = n(683301),
-    p = n(603592),
-    _ = n(981631),
-    f = n(731455),
-    g = n(689938),
-    m = n(973660);
-function C() {
-    let e = (0, u.a)({ location: 'discovery_button' }),
-        t = window.location.pathname.startsWith(_.Z5c.GUILD_DISCOVERY),
-        n = h.ZP.getCurrentCategoryId() === f.Gj.Clans;
+    r = n(481060),
+    a = n(212093),
+    s = n(831565),
+    o = n(703656),
+    c = n(683301),
+    u = n(603592),
+    d = n(981631),
+    h = n(731455),
+    p = n(689938);
+function _() {
+    let e = (0, s.a)({ location: 'discovery_button' }),
+        t = window.location.pathname.startsWith(d.Z5c.GUILD_DISCOVERY),
+        n = c.ZP.getCurrentCategoryId() === h.Gj.Clans;
     if (e) {
-        (0, d.uL)(_.Z5c.GLOBAL_DISCOVERY);
+        (0, o.uL)(d.Z5c.GLOBAL_DISCOVERY);
         return;
     }
-    t && !n && ((0, s.AQ)(), (0, s.uY)(f.Hk, !0));
-    let i = (0, d.s1)().location.search;
-    (0, d.uL)(_.Z5c.GUILD_DISCOVERY, { search: i });
+    t && !n && ((0, a.AQ)(), (0, a.uY)(h.Hk, !0));
+    let i = (0, o.s1)().location.search;
+    (0, o.uL)(d.Z5c.GUILD_DISCOVERY, { search: i });
 }
-let I = l.forwardRef(function (e, t) {
-    let { selected: n, className: s, onClick: d } = e,
-        { clanDiscoveryEnabled: h } = (0, o.nk)('guild_discovery'),
-        _ = (0, u.v)({ location: 'guild_discovery_button' }),
-        f = (0, c.wE)(r.z.NEW_GAMING_DISCOVERY_NOTIF),
-        I = l.useMemo(
-            () => ({
-                lowerBadgeSize: {
-                    width: 12,
-                    height: 12
-                }
-            }),
-            []
-        ),
-        E = _ ? g.Z.Messages.DISCOVER : g.Z.Messages.GUILD_DISCOVERY_TOOLTIP;
-    return (
-        h && !f && (I.lowerBadge = (0, i.jsx)('div', { className: m.guildNotif })),
-        (0, i.jsx)(p.Z, {
-            id: 'guild-discover-button',
-            ref: t,
-            className: s,
-            onClick: null != d ? d : C,
-            selected: n,
-            tooltip: E,
-            icon: a.CompassIcon,
-            ...I
-        })
-    );
+let f = l.forwardRef(function (e, t) {
+    let { selected: n, className: l, onClick: a } = e,
+        o = (0, s.v)({ location: 'guild_discovery_button' }) ? p.Z.Messages.DISCOVER : p.Z.Messages.GUILD_DISCOVERY_TOOLTIP;
+    return (0, i.jsx)(u.Z, {
+        id: 'guild-discover-button',
+        ref: t,
+        className: l,
+        onClick: null != a ? a : _,
+        selected: n,
+        tooltip: o,
+        icon: r.CompassIcon
+    });
 });
-t.Z = I;
+t.Z = f;
--- chunks/973660.js
+++
@@ -1,1 +0,0 @@
-e.exports = { guildNotif: 'guildNotif_e3bac1' };
