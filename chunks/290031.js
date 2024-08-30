626aaed083a49c691fd7ea3aa6489562f90dd5ec
DiscrapperManager <admin@minerpl.xyz>
2024-08-30 19:36:20 +0000

    canaryRelease: 323482
    Main scripts: 3
    Lazy Loaded Scripts: 870
    Chunks:
     * Main Files: 11177
     * Scripts: 9698
    Strings: 18833
    Routes: 681
    Experiments Count: 378

--- chunks/290031.js
+++ chunks/290031.js
@@ -1,9 +1,10 @@
 n.d(t, {
     O: function () {
-        return r;
+        return i;
     }
 });
-let r = (0, n(818083).B)({
+var r = n(818083);
+let i = (0, r.B)({
     kind: 'user',
     id: '2024-03_nitro_tenure_reward_card',
     label: 'Nitro Tenure Reward Card',
@@ -13,6 +14,19 @@ let r = (0, n(818083).B)({
             id: 1,
             label: 'Enables the nitro tenure reward card for users',
             config: { enableNitroTenureRewardCard: !0 }
+        }
+    ]
+});
+(0, r.B)({
+    kind: 'user',
+    id: '2024-08_nitro_tenure_rewards_backfill',
+    label: 'Nitro Tenure Reward Backfill',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enables the nitro tenure reward backfill for the user',
+            config: { enabled: !0 }
         }
     ]
 });
