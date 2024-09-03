b4a38eed540d9e6351ce813a3e27ee854652fe2f
DiscrapperManager <admin@minerpl.xyz>
2024-09-03 22:07:31 +0000

    canaryRelease: 323877
    Main scripts: 3
    Lazy Loaded Scripts: 870
    Chunks:
     * Main Files: 11177
     * Scripts: 9716
    Strings: 18869
    Routes: 681
    Experiments Count: 382

--- chunks/207874.js
+++ chunks/207874.js
@@ -1,6 +1,6 @@
 n.d(t, {
     Z: function () {
-        return h;
+        return d;
     }
 });
 var i = n(735250),
@@ -10,44 +10,42 @@ var i = n(735250),
     o = n(663389),
     a = n(5967),
     c = n(996733),
-    u = n(981631),
-    d = n(689938);
-function h(e) {
+    u = n(689938);
+function d(e) {
     let { style: t } = e,
         n = (0, c.Z7)(),
-        h = s.useCallback((e, t) => {
+        d = s.useCallback((e, t) => {
             t.stopPropagation(), t.preventDefault(), (0, c.QH)(e);
         }, []),
-        m = s.useCallback(() => {
+        h = s.useCallback(() => {
             (0, c.QH)(!1), (0, c.yN)('');
         }, []),
-        p = s.useRef(null),
-        f = (0, l.e7)([o.Z], () => o.Z.getSection());
+        m = s.useRef(null),
+        p = (0, l.e7)([o.Z], () => o.Z.getSection());
     return (
         s.useEffect(() => {
             let e = (e) => {
                 var t;
-                let n = p.current,
-                    i = null === (t = (0, a.uB)(e)) || void 0 === t ? void 0 : t.activeElement;
-                if (!(i !== p.current && (0, a.VG)(i))) f !== u.oAB.PROFILE_CUSTOMIZATION && (null == n || n.focus());
+                let n = m.current;
+                (null === (t = (0, a.uB)(e)) || void 0 === t ? void 0 : t.activeElement) === document.body && (null == n || n.focus());
             };
             return window.addEventListener('keydown', e), () => window.removeEventListener('keydown', e);
-        }, [f]),
+        }, [p]),
         (0, i.jsx)('div', {
             style: { marginBottom: '8px' },
             children: (0, i.jsx)(r.ZP, {
-                ref: p,
+                ref: m,
                 size: r.ZP.Sizes.MEDIUM,
                 query: n,
-                onClear: m,
+                onClear: h,
                 onQueryChange: c.yN,
-                placeholder: d.Z.Messages.SEARCH,
+                placeholder: u.Z.Messages.SEARCH,
                 className: t,
                 inputProps: {
-                    'aria-label': d.Z.Messages.USER_SETTINGS_SEARCH_BAR,
+                    'aria-label': u.Z.Messages.USER_SETTINGS_SEARCH_BAR,
                     'aria-expanded': !0,
-                    onFocus: (e) => h(!0, e),
-                    onBlur: (e) => h(!1, e)
+                    onFocus: (e) => d(!0, e),
+                    onBlur: (e) => d(!1, e)
                 }
             })
         })
