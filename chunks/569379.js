d410761d7ca61aff2da993846d498f3a393a6338
DiscrapperManager <admin@minerpl.xyz>
2024-09-03 21:07:47 +0000

    canaryRelease: 323823
    Main scripts: 3
    Lazy Loaded Scripts: 870
    Chunks:
     * Main Files: 11176
     * Scripts: 9712
    Strings: 18868
    Routes: 681
    Experiments Count: 381

--- chunks/569379.js
+++ chunks/569379.js
@@ -1,15 +1,15 @@
 n.d(t, {
     EK: function () {
-        return h;
+        return E;
     },
     GX: function () {
-        return f;
+        return h;
     },
     eQ: function () {
-        return _;
+        return C;
     },
     vf: function () {
-        return C;
+        return f;
     }
 }),
     n(47120);
@@ -22,46 +22,49 @@ var s = n(470079),
     c = n(113434),
     d = n(497505),
     u = n(918701),
-    p = n(585500),
-    x = n(566078),
-    m = n(46140),
-    g = n(689938);
-function _(e) {
+    p = n(665430),
+    x = n(585500),
+    m = n(566078),
+    g = n(46140),
+    _ = n(689938);
+function C(e) {
     var t, n;
     let s = (0, o.e7)([l.default], () => l.default.locale),
-        { percentComplete: r } = (0, c.Rf)(e);
-    if ((null === (t = e.userStatus) || void 0 === t ? void 0 : t.completedAt) != null) return g.Z.Messages.QUESTS_COMPLETION_COMPLETE;
-    if ((null === (n = e.userStatus) || void 0 === n ? void 0 : n.enrolledAt) != null && r > 0) {
-        let e = (0, i.T3)(s, r, { roundingMode: 'floor' });
-        return g.Z.Messages.QUESTS_COMPLETION_PROGRESS_STARTED_V2.format({ percent: e });
+        r = (0, c.Rf)(e),
+        [a] = (0, c.me)(e, r),
+        x = (0, p.pF)({ location: g.dr.QUESTS_BAR });
+    if ((null === (t = e.userStatus) || void 0 === t ? void 0 : t.completedAt) != null) return _.Z.Messages.QUESTS_COMPLETION_COMPLETE;
+    if ((null === (n = e.userStatus) || void 0 === n ? void 0 : n.enrolledAt) != null && r.percentComplete > 0) {
+        let e = (0, i.T3)(s, r.percentComplete, { roundingMode: 'floor' });
+        return _.Z.Messages.QUESTS_COMPLETION_PROGRESS_STARTED_V2.format({ percent: e });
     }
-    return (0, u.$J)(e) ? g.Z.Messages.QUEST_BAR_TITLE_START_PLAYING : g.Z.Messages.QUESTS_COMPLETION_PROGRESS_NOT_STARTED;
+    return x && a === d.LI.SELECT ? _.Z.Messages.QUESTS_REWARD_CODE_SELECT_PLATFORM_PLACEHOLDER : (0, u.$J)(e) ? _.Z.Messages.QUEST_BAR_TITLE_START_PLAYING : _.Z.Messages.QUESTS_COMPLETION_PROGRESS_NOT_STARTED;
 }
-function C(e, t, n) {
+function f(e, t, n) {
     var s;
-    let o = (0, c.B6)(x.r.build(e.config).rewardsExpireAt),
+    let o = (0, c.B6)(m.r.build(e.config).rewardsExpireAt),
         r = (0, c.Rf)(e),
         a = (null === (s = e.userStatus) || void 0 === s ? void 0 : s.completedAt) != null,
         l = (0, c.z)(e),
-        i = (0, p.D)({
+        i = (0, x.D)({
             quest: e,
-            location: m.dr.QUESTS_BAR,
+            location: g.dr.QUESTS_BAR,
             questContent: d.jn.QUEST_BAR_V2,
             taskDetails: r,
             useV2Variants: !0
         });
-    if (a) return g.Z.Messages.QUESTS_CLAIM_BY_DATE.format({ expirationDate: o });
-    if (t) return n === d.LI.SELECT ? g.Z.Messages.QUEST_MULTIPLATFORM_SELECT_SUBTITLE : i;
+    if (a) return _.Z.Messages.QUESTS_CLAIM_BY_DATE.format({ expirationDate: o });
+    if (t) return n === d.LI.SELECT ? _.Z.Messages.QUEST_MULTIPLATFORM_SELECT_FUN_SUBTITLE : i;
     if (r.percentComplete > 0)
         return l
             ? (0, u.AV)({
                   quest: e,
                   taskDetails: r
               })
-            : g.Z.Messages.QUEST_BAR_TITLE_START_PLAYING;
-    return g.Z.Messages.QUESTS_COMPLETION_PROGRESS_NOT_STARTED_SUBTITLE;
+            : _.Z.Messages.QUEST_BAR_TITLE_START_PLAYING;
+    return _.Z.Messages.QUESTS_COMPLETION_PROGRESS_NOT_STARTED_SUBTITLE;
 }
-function f() {
+function h() {
     let [{ spring: e }, t] = (0, r.useSpring)(() => ({ spring: 0 }), 'animate-always');
     return {
         completionSpring: e,
@@ -74,7 +77,7 @@ function f() {
         }, [t])
     };
 }
-function h(e, t) {
+function E(e, t) {
     var n;
     let s = (0, c.z)(e),
         o = null !== (n = (0, a.i6)()) && void 0 !== n ? n : 0,
