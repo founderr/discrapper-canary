1302bfeb1d23d3c46262c481a8f3d50fd8de766e
DiscrapperManager <admin@minerpl.xyz>
2024-09-04 13:02:23 +0000

    canaryRelease: 324071
    Main scripts: 3
    Lazy Loaded Scripts: 870
    Chunks:
     * Main Files: 11177
     * Scripts: 9720
    Strings: 18874
    Routes: 681
    Experiments Count: 382

---
+++ chunks/138012.js
@@ -0,0 +1,145 @@
+n(47120), n(653041);
+var i,
+    a = n(512722),
+    s = n.n(a),
+    r = n(442837),
+    l = n(570140),
+    o = n(881052);
+function c(e, t, n) {
+    return (
+        t in e
+            ? Object.defineProperty(e, t, {
+                  value: n,
+                  enumerable: !0,
+                  configurable: !0,
+                  writable: !0
+              })
+            : (e[t] = n),
+        e
+    );
+}
+let d = new Map(),
+    u = new Map(),
+    _ = null;
+class h {
+    handleSearchCountStart() {
+        (this.error = null), (this.isFetching = !0);
+    }
+    handleSearchCountSuccess(e) {
+        (this.counts = e), (this.isFetching = !1);
+    }
+    handleSearchCountFailure(e) {
+        (this.error = new o.Hx(e)), (this.isFetching = !1);
+    }
+    constructor() {
+        c(this, 'counts', null), c(this, 'isFetching', !1), c(this, 'error', null);
+    }
+}
+class E {
+    handleSearchStart() {
+        (this.error = null), (this.isFetching = !0), (this.lastFetchTimestamp = Date.now());
+    }
+    handleSearchFailure(e) {
+        (this.isFetching = !1), (this.error = new o.Hx(e));
+    }
+    handleSearchSuccess(e) {
+        let { total: t, guilds: n } = e;
+        (this.error = null), (this.isFetching = !1), (this.isInitialFetchComplete = !0), (this.total = t);
+        let i = [...this.guilds];
+        n.forEach((e) => i.push(this.toDiscoverableGuild(e))), (this.guilds = i), (this.offset = i.length);
+    }
+    toDiscoverableGuild(e) {
+        return {
+            id: e.id,
+            name: e.name,
+            description: e.description,
+            splash: e.splash,
+            banner: e.banner,
+            icon: e.icon,
+            features: new Set(e.features),
+            presenceCount: e.approximate_presence_count,
+            memberCount: e.approximate_member_count,
+            premiumSubscriptionCount: e.premium_subscription_count,
+            preferredLocale: e.preferred_locale,
+            discoverySplash: e.discovery_splash,
+            emojis: e.emojis,
+            emojiCount: e.emoji_count
+        };
+    }
+    constructor() {
+        c(this, 'guilds', []), c(this, 'error', null), c(this, 'offset', null), c(this, 'total', null), c(this, 'isFetching', !1), c(this, 'isInitialFetchComplete', !1), c(this, 'lastFetchTimestamp', null);
+    }
+}
+function m(e) {
+    var t;
+    __DEV__ && s()(e.startsWith('search-counts'), 'Unexpected id format for global discovery search counts.');
+    let n = null !== (t = u.get(e)) && void 0 !== t ? t : new h();
+    return u.set(e, n), n;
+}
+function I(e) {
+    var t;
+    __DEV__ && s()(e.startsWith('search-results'), 'Unexpected id format for global discovery search state.');
+    let n = null !== (t = d.get(e)) && void 0 !== t ? t : new E();
+    return d.set(e, n), n;
+}
+function g(e, t) {
+    let n = d.get(e);
+    return null != n ? t(n) : null;
+}
+class p extends (i = r.ZP.Store) {
+    getGuilds(e) {
+        return g(e, (e) => e.guilds);
+    }
+    getIsFetching(e) {
+        return g(e, (e) => e.isFetching);
+    }
+    getIsInitialFetchComplete(e) {
+        return g(e, (e) => e.isInitialFetchComplete);
+    }
+    getOffset(e) {
+        return g(e, (e) => e.offset);
+    }
+    getAlgoliaSearchIndex() {
+        return _;
+    }
+    getIsAlgoliaInitialized() {
+        return null != _;
+    }
+}
+c(p, 'displayName', 'GlobalDiscoveryServersSearchStore'),
+    new p(l.Z, {
+        CONNECTION_OPEN: function () {
+            d.clear(), u.clear(), (_ = null);
+        },
+        GLOBAL_DISCOVERY_SERVERS_SEARCH_INITIALIZED: function (e) {
+            let { algoliaSearchIndex: t } = e;
+            _ = t;
+        },
+        GLOBAL_DISCOVERY_SERVERS_SEARCH_START: function (e) {
+            let { id: t } = e;
+            I(t).handleSearchStart();
+        },
+        GLOBAL_DISCOVERY_SERVERS_SEARCH_SUCCESS: function (e) {
+            let { id: t, total: n, guilds: i } = e;
+            I(t).handleSearchSuccess({
+                total: n,
+                guilds: i
+            });
+        },
+        GLOBAL_DISCOVERY_SERVERS_SEARCH_FAILURE: function (e) {
+            let { id: t, error: n } = e;
+            I(t).handleSearchFailure(n);
+        },
+        GLOBAL_DISCOVERY_SERVERS_SEARCH_COUNT_START: function (e) {
+            let { id: t } = e;
+            m(t).handleSearchCountStart();
+        },
+        GLOBAL_DISCOVERY_SERVERS_SEARCH_COUNT_SUCCESS: function (e) {
+            let { id: t, categoryCounts: n } = e;
+            m(t).handleSearchCountSuccess(n);
+        },
+        GLOBAL_DISCOVERY_SERVERS_SEARCH_COUNT_FAILURE: function (e) {
+            let { id: t, error: n } = e;
+            m(t).handleSearchCountFailure(n);
+        }
+    });
