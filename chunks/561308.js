27a30f62c846694cf704587021bebcccff83e605
DiscrapperManager <admin@minerpl.xyz>
2024-09-05 20:34:36 +0000

    canaryRelease: 324829
    Main scripts: 3
    Lazy Loaded Scripts: 875
    Chunks:
     * Main Files: 11158
     * Scripts: 9757
    Strings: 18894
    Routes: 682
    Experiments Count: 384

--- chunks/561308.js
+++ chunks/561308.js
@@ -1,63 +1,63 @@
 n.d(t, {
     GE: function () {
-        return y;
+        return L;
     },
     GL: function () {
-        return m;
+        return T;
     },
     Jd: function () {
-        return b;
+        return M;
     },
     Nq: function () {
-        return R;
+        return v;
     },
     Ol: function () {
-        return A;
+        return N;
     },
     PJ: function () {
-        return g;
+        return S;
     },
     T_: function () {
         return I;
     },
     V5: function () {
-        return v;
-    },
-    ap: function () {
-        return L;
-    },
-    bT: function () {
-        return D;
-    },
-    dw: function () {
         return C;
     },
+    ap: function () {
+        return D;
+    },
+    bT: function () {
+        return b;
+    },
+    dw: function () {
+        return y;
+    },
     ig: function () {
-        return w;
-    },
-    kr: function () {
-        return S;
-    },
-    n2: function () {
-        return N;
-    },
-    q_: function () {
-        return P;
-    },
-    qy: function () {
         return x;
     },
-    vU: function () {
-        return M;
+    kr: function () {
+        return A;
     },
-    yA: function () {
+    n2: function () {
         return O;
     },
+    q_: function () {
+        return U;
+    },
+    qy: function () {
+        return G;
+    },
+    vU: function () {
+        return P;
+    },
+    yA: function () {
+        return R;
+    },
     yh: function () {
-        return T;
+        return g;
     },
     zo: function () {
-        return U;
+        return w;
     }
 }),
     n(47120);
@@ -104,63 +104,73 @@ let h = (e) => {
             seconds: a(n)
         });
     },
-    m = (e, t, n) => {
-        let r = a()(n),
-            i = a()(_.default.extractTimestamp(e.id)),
-            s = r.diff(i, 's');
-        if (s < d.Z.Seconds.MINUTE) return f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_SECONDS_AGO.format({ count: s });
-        if (s < d.Z.Seconds.HOUR) {
-            let e = Math.round(s / d.Z.Seconds.MINUTE);
-            return f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_MINUTES_AGO.format({ count: e });
-        }
-        if (s < 12 * d.Z.Seconds.HOUR) {
-            let e = Math.round(s / d.Z.Seconds.HOUR);
-            return f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_HOURS_AGO.format({ count: e });
-        } else if (s < 9 * d.Z.Seconds.DAY) {
-            let e = Math.round(s / d.Z.Seconds.DAY);
-            return f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_DAYS_AGO.format({ count: e });
-        } else if (s < 4 * d.Z.Seconds.WEEK) {
-            let e = Math.round(s / (7 * d.Z.Seconds.DAY));
-            return f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_WEEKS_AGO.format({ count: e });
-        }
-        let o = Math.round(s / d.Z.Seconds.DAYS_30);
-        return f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_MONTHS_AGO.format({ count: o });
+    m = {
+        secondsAgo: (e) => f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_SECONDS_AGO.format({ count: e }),
+        minutesAgo: (e) => f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_MINUTES_AGO.format({ count: e }),
+        hoursAgo: (e) => f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_HOURS_AGO.format({ count: e }),
+        daysAgo: (e) => f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_DAYS_AGO.format({ count: e }),
+        weeksAgo: (e) => f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_WEEKS_AGO.format({ count: e }),
+        monthsAgo: (e) => f.Z.Messages.MEMBER_LIST_CONTENT_FEED_TIMESTAMP_MONTHS_AGO.format({ count: e })
     },
-    T = function (e, t) {
-        let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : Date.now();
-        return S(e) ? I(e, n) : m(e, t, n);
+    T = function (e, t, n) {
+        let { formatSet: r = m } = arguments.length > 3 && void 0 !== arguments[3] ? arguments[3] : {},
+            i = a()(n),
+            s = a()(_.default.extractTimestamp(e.id)),
+            o = i.diff(s, 's');
+        if (o < d.Z.Seconds.MINUTE) return r.secondsAgo(o);
+        if (o < d.Z.Seconds.HOUR) {
+            let e = Math.round(o / d.Z.Seconds.MINUTE);
+            return r.minutesAgo(e);
+        }
+        if (o < 12 * d.Z.Seconds.HOUR) {
+            let e = Math.round(o / d.Z.Seconds.HOUR);
+            return r.hoursAgo(e);
+        } else if (o < 9 * d.Z.Seconds.DAY) {
+            let e = Math.round(o / d.Z.Seconds.DAY);
+            return r.daysAgo(e);
+        } else if (o < 4 * d.Z.Seconds.WEEK) {
+            let e = Math.round(o / (7 * d.Z.Seconds.DAY));
+            return r.weeksAgo(e);
+        }
+        let l = Math.round(o / d.Z.Seconds.DAYS_30);
+        return r.monthsAgo(l);
+    },
+    g = function (e, t) {
+        let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : Date.now(),
+            r = arguments.length > 3 && void 0 !== arguments[3] ? arguments[3] : {};
+        return A(e) ? I(e, n) : T(e, t, n, r);
     };
-function g(e, t) {
+function S(e, t) {
     return e.traits.find((e) => e.type === t);
-}
-function S(e) {
-    var t, n;
-    return null !== (n = null === (t = g(e, s.N.IS_LIVE)) || void 0 === t ? void 0 : t.is_live) && void 0 !== n && n;
 }
 function A(e) {
     var t, n;
-    return null !== (n = null === (t = g(e, s.N.FIRST_TIME)) || void 0 === t ? void 0 : t.first_time) && void 0 !== n && n;
+    return null !== (n = null === (t = S(e, s.N.IS_LIVE)) || void 0 === t ? void 0 : t.is_live) && void 0 !== n && n;
 }
 function N(e) {
-    return null != e.expires_at && new Date(e.expires_at) < new Date();
+    var t, n;
+    return null !== (n = null === (t = S(e, s.N.FIRST_TIME)) || void 0 === t ? void 0 : t.first_time) && void 0 !== n && n;
 }
 function O(e) {
-    var t;
-    return null === (t = g(e, s.N.DURATION_SECONDS)) || void 0 === t ? void 0 : t.duration_seconds;
+    return null != e.expires_at && new Date(e.expires_at) < new Date();
 }
 function R(e) {
     var t;
-    return null === (t = g(e, s.N.AGGREGATE_RANGE)) || void 0 === t ? void 0 : t.range;
+    return null === (t = S(e, s.N.DURATION_SECONDS)) || void 0 === t ? void 0 : t.duration_seconds;
 }
 function v(e) {
     var t;
-    return null === (t = g(e, s.N.MARATHON)) || void 0 === t ? void 0 : t.marathon;
+    return null === (t = S(e, s.N.AGGREGATE_RANGE)) || void 0 === t ? void 0 : t.range;
 }
 function C(e) {
-    let t = g(e, s.N.RESURRECTED);
-    return (null == t ? void 0 : t.resurrected_last_played) != null ? new Date(t.resurrected_last_played) : void 0;
+    var t;
+    return null === (t = S(e, s.N.MARATHON)) || void 0 === t ? void 0 : t.marathon;
 }
 function y(e) {
+    let t = S(e, s.N.RESURRECTED);
+    return (null == t ? void 0 : t.resurrected_last_played) != null ? new Date(t.resurrected_last_played) : void 0;
+}
+function L(e) {
     let {
         months: t = 0,
         weeks: n = 0,
@@ -175,7 +185,7 @@ function y(e) {
         days: t > 0 || n > 0 ? 0 : i
     });
 }
-function L(e) {
+function D(e) {
     if (null == e || '' === e) return null;
     let t = /\w+ (\d+), \w+ (\d+)/.exec(e);
     return null == t
@@ -185,7 +195,7 @@ function L(e) {
               episodeNum: t[2]
           });
 }
-function D(e, t) {
+function b(e, t) {
     var n, r, i, a;
     let s = null !== (i = null == t ? void 0 : null === (n = t.size) || void 0 === n ? void 0 : n[0]) && void 0 !== i ? i : void 0,
         o = null !== (a = null == t ? void 0 : null === (r = t.size) || void 0 === r ? void 0 : r[1]) && void 0 !== a ? a : void 0,
@@ -198,21 +208,21 @@ function D(e, t) {
                 : void 0;
     return null != l && null != e ? ''.concat(e, ' (').concat(l, ')') : null != e ? e : l;
 }
-function b(e) {
+function M(e) {
     return e.content_type === l.s.TOP_GAME;
 }
-function M(e) {
-    var t;
-    return null === (t = g(e, s.N.STREAK_DAYS)) || void 0 === t ? void 0 : t.streak_count_days;
-}
 function P(e) {
-    let t = M(e);
+    var t;
+    return null === (t = S(e, s.N.STREAK_DAYS)) || void 0 === t ? void 0 : t.streak_count_days;
+}
+function U(e) {
+    let t = P(e);
     if (null == t || t < 3) return !1;
     let n = _.default.extractTimestamp(e.id);
     return !(Date.now() - n > 48 * d.Z.Millis.HOUR) && !0;
 }
-function U(e) {
-    let t = O(e);
+function w(e) {
+    let t = R(e);
     if (null == t)
         return {
             text: null,
@@ -229,11 +239,11 @@ function U(e) {
               tooltipText: f.Z.Messages.MEMBER_LIST_CONTENT_FEED_PLAYED_FOR_HOURS.format({ hours: n })
           };
 }
-function w(e) {
-    var t;
-    return null === (t = g(e, s.N.TRENDING_CONTENT)) || void 0 === t ? void 0 : t.trending;
-}
 function x(e) {
+    var t;
+    return null === (t = S(e, s.N.TRENDING_CONTENT)) || void 0 === t ? void 0 : t.trending;
+}
+function G(e) {
     let t = (0, o.e7)([E.Z], () => E.Z.getMatchingActivity(e)),
         [n, r] = (0, c.Z)([null == t ? void 0 : t.application_id, 'application_id' in e.extra ? e.extra.application_id : void 0]),
         i = (0, o.e7)([u.ZP], () => u.ZP.getEmbeddedActivityForUserId(e.author_id, null == n ? void 0 : n.id));
