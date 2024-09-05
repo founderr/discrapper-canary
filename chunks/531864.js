f898a351e39aa0c285d70029fe1b9cc2825b56de
DiscrapperManager <admin@minerpl.xyz>
2024-09-05 00:46:43 +0000

    canaryRelease: 324540
    Main scripts: 3
    Lazy Loaded Scripts: 873
    Chunks:
     * Main Files: 11155
     * Scripts: 9745
    Strings: 18879
    Routes: 681
    Experiments Count: 383

--- chunks/531864.js
+++ chunks/531864.js
@@ -1,8 +1,8 @@
 n(47120);
 var a = n(735250),
-    r = n(470079),
-    s = n(120356),
-    o = n.n(s),
+    s = n(470079),
+    r = n(120356),
+    o = n.n(r),
     i = n(979554),
     l = n(876917),
     c = n(597688),
@@ -13,88 +13,119 @@ let p = {
         x: 160,
         y: 160
     },
-    g = {
-        x: 200,
-        y: 600
-    },
-    C = [
+    g = [
         {
             left: 0,
             top: 20,
             rotation: -32,
             size: p,
-            skuId: '1232072859485208687'
+            skuId: '1212569433839636530'
         },
         {
-            left: 192,
+            left: 110,
             top: 48,
             rotation: -24,
-            size: g,
-            skuId: '1139323099687436419'
+            size: p,
+            skuId: '1144308439720394944'
         },
         {
-            left: 256,
+            left: 230,
             top: 12,
             rotation: 8,
-            size: p,
-            skuId: '1262491690900656261'
-        },
-        {
-            left: 384,
-            top: 44,
-            rotation: -48,
-            size: p,
-            skuId: '1252404767737778217'
-        },
-        {
-            left: 512,
-            top: 52,
-            rotation: 12,
-            size: g,
-            skuId: '1139323100568244355'
-        },
-        {
-            left: 640,
-            top: 28,
-            rotation: -4,
             size: p,
             skuId: '1228251144065777765'
         },
         {
-            left: 768,
+            left: 354,
+            top: 44,
+            rotation: -48,
+            size: p,
+            skuId: '1262491137394868308'
+        },
+        {
+            left: 470,
+            top: 52,
+            rotation: 12,
+            size: p,
+            skuId: '1252404767737778217'
+        },
+        {
+            left: 600,
             top: 28,
             rotation: -4,
             size: p,
+            skuId: '1197344326133502032'
+        },
+        {
+            left: 740,
+            top: 12,
+            rotation: -32,
+            size: p,
+            skuId: '1232071712695386162'
+        },
+        {
+            left: 870,
+            top: 40,
+            rotation: -20,
+            size: p,
+            skuId: '1220513977683935373'
+        },
+        {
+            left: 1010,
+            top: 30,
+            rotation: 15,
+            size: p,
             skuId: '1144046002110738634'
+        },
+        {
+            left: 1140,
+            top: 52,
+            rotation: -18,
+            size: p,
+            skuId: '1271174324375519273'
+        },
+        {
+            left: 1270,
+            top: 32,
+            rotation: 25,
+            size: p,
+            skuId: '1237653964582031400'
+        },
+        {
+            left: 1400,
+            top: 33,
+            rotation: -5,
+            size: p,
+            skuId: '1217625794382401577'
         }
     ];
 t.Z = (e) => {
-    let { peaking: t, transitioning: n, style: s } = e,
+    let { peaking: t, transitioning: n, style: r } = e,
         p = window.innerHeight,
-        [g, m] = r.useState(!1),
-        _ = C.map((e) => {
+        [C, m] = s.useState(!1),
+        _ = g.map((e) => {
             let { skuId: t } = e;
             return c.Z.getProduct(t);
         });
     return (
-        r.useEffect(() => {
+        s.useEffect(() => {
             n &&
                 setTimeout(() => {
                     m(!0);
                 }, u.lb);
         }, [n]),
         (0, a.jsx)('div', {
-            style: s,
+            style: r,
             className: o()(f.jumbleWrapper, {
                 [f.peaking]: t,
-                [f.transitioned]: g
+                [f.transitioned]: C
             }),
-            children: C.map((e, t) => {
-                var r, s;
+            children: g.map((e, t) => {
+                var s, r;
                 let { top: o, left: c, rotation: g, size: C, skuId: m } = e,
-                    b = null === (r = _[t]) || void 0 === r ? void 0 : r.items[0],
-                    h = null === (s = _[t]) || void 0 === s ? void 0 : s.type,
-                    x = h === i.Z.AVATAR_DECORATION ? 384 : 512;
+                    h = null === (s = _[t]) || void 0 === s ? void 0 : s.items[0],
+                    b = null === (r = _[t]) || void 0 === r ? void 0 : r.type,
+                    x = b === i.Z.AVATAR_DECORATION ? 384 : 512;
                 return (0, a.jsxs)(
                     'div',
                     {
@@ -109,11 +140,11 @@ t.Z = (e) => {
                             transitionDuration: ''.concat(u.lb - 200 * Math.random(), 'ms')
                         },
                         children: [
-                            null != b && h === i.Z.AVATAR_DECORATION && (0, a.jsx)(d.R, { item: b }),
-                            null != b &&
-                                h === i.Z.PROFILE_EFFECT &&
+                            null != h && b === i.Z.AVATAR_DECORATION && (0, a.jsx)(d.R, { item: h }),
+                            null != h &&
+                                b === i.Z.PROFILE_EFFECT &&
                                 (0, a.jsx)(l.Z, {
-                                    profileEffectId: b.id,
+                                    profileEffectId: h.id,
                                     isPurchased: !1,
                                     isHovering: !0
                                 })
