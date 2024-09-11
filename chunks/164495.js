8aa8d70f7ea5db905f20645c492ada774e97a01d
DiscrapperManager <admin@minerpl.xyz>
2024-09-11 00:43:29 +0000

    canaryRelease: 326150
    Main scripts: 3
    Lazy Loaded Scripts: 874
    Chunks:
     * Main Files: 11090
     * Scripts: 9627
    Strings: 18908
    Routes: 682
    Experiments Count: 367

--- chunks/164495.js
+++ chunks/164495.js
@@ -1,96 +1,104 @@
 n.d(t, {
     y: function () {
-        return C;
+        return f;
     }
 });
 var s = n(735250);
 n(470079);
 var o = n(481060),
-    r = n(617136),
-    a = n(497505),
-    l = n(918701),
-    i = n(796111),
-    c = n(667105),
-    d = n(46140),
-    u = n(689938),
-    p = n(128220);
-let x = (e) => {
+    r = n(782568),
+    a = n(617136),
+    l = n(497505),
+    i = n(918701),
+    c = n(796111),
+    d = n(667105),
+    u = n(46140),
+    p = n(689938),
+    x = n(128220);
+let m = (e) => {
         let { quest: t } = e;
-        return (0, s.jsx)(o.Button, {
-            className: p.cta,
-            size: o.Button.Sizes.SMALL,
-            onClick: () =>
-                (0, l.FE)(t, {
-                    content: a.jn.QUEST_BAR_V2,
-                    ctaContent: r.jZ.OPEN_GAME_LINK
-                }),
-            children: u.Z.Messages.QUESTS_GET_THIS_GAME
-        });
+        return (0, i.KM)(t)
+            ? (0, s.jsx)(o.Button, {
+                  className: x.cta,
+                  size: o.Button.Sizes.SMALL,
+                  onClick: () => (0, r.Z)('https://support.discord.com/hc/en-us/articles/4422142836759-Activities-on-Discord'),
+                  children: p.Z.Messages.QUESTS_LEARN_MORE_V2
+              })
+            : (0, s.jsx)(o.Button, {
+                  className: x.cta,
+                  size: o.Button.Sizes.SMALL,
+                  onClick: () =>
+                      (0, i.FE)(t, {
+                          content: l.jn.QUEST_BAR_V2,
+                          ctaContent: a.jZ.OPEN_GAME_LINK
+                      }),
+                  children: p.Z.Messages.QUESTS_GET_THIS_GAME
+              });
     },
-    m = (e) => {
+    g = (e) => {
         let { quest: t } = e;
         return (0, s.jsx)(o.Button, {
             fullWidth: !0,
-            className: p.cta,
+            className: x.cta,
             onClick: () =>
-                (0, l.gI)(
+                (0, i.gI)(
                     { quest: t },
                     {
-                        content: a.jn.QUEST_BAR_V2,
-                        ctaContent: r.jZ.CONNECT_CONSOLE
+                        content: l.jn.QUEST_BAR_V2,
+                        ctaContent: a.jZ.CONNECT_CONSOLE
                     }
                 ),
             size: o.Button.Sizes.SMALL,
-            children: u.Z.Messages.QUESTS_CONSOLE_CONNECTION_HELPER_STEPS_CTA
+            children: p.Z.Messages.QUESTS_CONSOLE_CONNECTION_HELPER_STEPS_CTA
         });
     },
-    g = (e) => {
+    _ = (e) => {
         let { quest: t, useReducedMotion: n, isExpanded: r } = e,
-            l = (0, c.hf)({
+            a = (0, d.hf)({
                 quest: t,
-                location: a.jn.QUEST_BAR_V2
+                location: l.jn.QUEST_BAR_V2
             });
         return (0, s.jsx)(o.ShinyButton, {
             fullWidth: !0,
             size: o.Button.Sizes.SMALL,
-            onClick: l,
+            onClick: a,
             pauseAnimation: n || !r,
-            className: p.cta,
-            buttonShineClassName: p.shine,
-            children: u.Z.Messages.QUESTS_CLAIM_REWARD
+            className: x.cta,
+            buttonShineClassName: x.shine,
+            children: p.Z.Messages.QUESTS_CLAIM_REWARD
         });
     },
-    _ = (e) => {
+    C = (e) => {
         var t;
-        let { quest: n, useReducedMotion: o, isExpanded: r, awaitingConsoleConnections: l, hasMadeProgress: c, isProgressing: u, activeScreen: p } = e,
-            _ = (null === (t = n.userStatus) || void 0 === t ? void 0 : t.completedAt) != null,
-            C = (0, i.P)({ location: d.dr.QUESTS_BAR });
-        if (_)
-            return (0, s.jsx)(g, {
+        let { quest: n, useReducedMotion: o, isExpanded: r, awaitingConsoleConnections: a, hasMadeProgress: i, isProgressing: d, activeScreen: p } = e,
+            x = (null === (t = n.userStatus) || void 0 === t ? void 0 : t.completedAt) != null,
+            C = (0, c.P)({ location: u.dr.QUESTS_BAR });
+        if (x)
+            return (0, s.jsx)(_, {
                 quest: n,
                 useReducedMotion: o,
                 isExpanded: r
             });
-        if (p === a.LI.CONSOLE && l && !C) return (0, s.jsx)(m, { quest: n });
-        if (p !== a.LI.SELECT && !c && !u) return (0, s.jsx)(x, { quest: n });
+        if (p === l.LI.CONSOLE && a && !C) return (0, s.jsx)(g, { quest: n });
+        if (p !== l.LI.SELECT && !i && !d) return (0, s.jsx)(m, { quest: n });
         return null;
     };
-function C(e) {
+function f(e) {
     return (0, s.jsxs)('div', {
-        className: p.ctaButtons,
+        className: x.ctaButtons,
         children: [
             e.showBackButton &&
                 (0, s.jsx)(o.Button, {
-                    className: p.backButton,
-                    innerClassName: p.backButtonInner,
+                    className: x.backButton,
+                    innerClassName: x.backButtonInner,
                     look: 'blank',
                     grow: !1,
                     fullWidth: !1,
                     size: 'none',
                     onClick: e.onBack,
-                    children: (0, s.jsx)(o.ChevronSmallLeftIcon, { className: p.backIcon })
+                    children: (0, s.jsx)(o.ChevronSmallLeftIcon, { className: x.backIcon })
                 }),
-            (0, s.jsx)(_, { ...e })
+            (0, s.jsx)(C, { ...e })
         ]
     });
 }
