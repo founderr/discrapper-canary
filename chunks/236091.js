9c3ac2eff0954982576ce169dc502ca0a4aef0df
DiscrapperManager <admin@minerpl.xyz>
2024-09-11 23:32:26 +0000

    canaryRelease: 326476
    Main scripts: 3
    Lazy Loaded Scripts: 874
    Chunks:
     * Main Files: 11091
     * Scripts: 9637
    Strings: 18924
    Routes: 682
    Experiments Count: 371

--- chunks/236091.js
+++ chunks/236091.js
@@ -7,40 +7,39 @@ var s = n(120356),
     o = n(481060),
     c = n(358221),
     u = n(594190),
-    d = n(836157),
-    h = n(199902),
-    m = n(924557),
-    p = n(435064),
-    _ = n(354459),
-    f = n(689938),
-    E = n(162910);
+    d = n(199902),
+    h = n(924557),
+    m = n(435064),
+    p = n(354459),
+    _ = n(689938),
+    f = n(162910);
 t.Z = (e) => {
     let { className: t, channelId: n } = e,
-        s = (0, m.Go)(),
-        { enableDecoupledGameClipping: g } = d.Z.useExperiment({ location: 'ClipsEnabledIndicator' }, { autoTrackExposure: !1 });
-    return (0, l.e7)([h.Z, c.Z, p.Z, u.ZP], () => {
+        s = (0, h.Go)(),
+        E = m.Z.isDecoupledGameClippingEnabled();
+    return (0, l.e7)([d.Z, c.Z, m.Z, u.ZP], () => {
         var e;
         if (null == n) return !1;
-        let t = h.Z.getCurrentUserActiveStream(),
+        let t = d.Z.getCurrentUserActiveStream(),
             i = null === (e = u.ZP.getVisibleGame()) || void 0 === e ? void 0 : e.windowHandle;
-        return (null != t && !!p.Z.getSettings().clipsEnabled && !!s) || (null != i && !!p.Z.getSettings().decoupledClipsEnabled && !!g) || c.Z.getParticipants(n).some((e) => e.type === _.fO.USER && p.Z.isClipsEnabledForUser(e.user.id));
+        return (null != t && !!m.Z.getSettings().clipsEnabled && !!s) || (null != i && !!m.Z.getSettings().decoupledClipsEnabled && !!E) || c.Z.getParticipants(n).some((e) => e.type === p.fO.USER && m.Z.isClipsEnabledForUser(e.user.id));
     })
         ? (0, i.jsxs)(o.TooltipContainer, {
-              text: f.Z.Messages.CLIPS_ENABLED_VIEWER_TOOLTIP,
+              text: _.Z.Messages.CLIPS_ENABLED_VIEWER_TOOLTIP,
               color: o.TooltipColors.GREY,
-              className: a()(t, E.clipsEnabledIndicator),
+              className: a()(t, f.clipsEnabledIndicator),
               children: [
                   (0, i.jsx)(o.IconBadge, {
                       icon: o.ClipsIcon,
                       shape: o.BadgeShapes.ROUND_LEFT,
                       color: r.Z.unsafe_rawColors.PRIMARY_500.css,
-                      className: E.clipBadgeIcon
+                      className: f.clipBadgeIcon
                   }),
                   (0, i.jsx)(o.TextBadge, {
-                      text: f.Z.Messages.CLIPS_ENABLED_BADGE,
+                      text: _.Z.Messages.CLIPS_ENABLED_BADGE,
                       shape: o.BadgeShapes.ROUND_RIGHT,
                       color: r.Z.unsafe_rawColors.PRIMARY_500.css,
-                      className: E.clipBadgeText
+                      className: f.clipBadgeText
                   })
               ]
           })
