783ceeae926df943a461de4a6b47dcba40ae85d6
DiscrapperManager <admin@minerpl.xyz>
2024-09-11 20:54:20 +0000

    canaryRelease: 326379
    Main scripts: 3
    Lazy Loaded Scripts: 874
    Chunks:
     * Main Files: 11092
     * Scripts: 9632
    Strings: 18922
    Routes: 682
    Experiments Count: 370

--- chunks/881998.js
+++ chunks/881998.js
@@ -2,31 +2,41 @@ var i,
     s,
     a,
     r,
-    l = n(442837),
-    o = n(570140),
-    c = n(592125),
-    u = n(757266),
-    d = n(375954);
-let _ = null;
-class E extends (r = l.ZP.Store) {
+    l,
+    o,
+    c = n(442837),
+    u = n(570140),
+    d = n(592125),
+    _ = n(757266),
+    E = n(375954);
+((i = l || (l = {})).NOT_FETCHED = 'NOT_FETCHED'), (i.FETCHING = 'FETCHING'), (i.FETCHED = 'FETCHED');
+let I = null,
+    m = 'NOT_FETCHED';
+class T extends (o = c.ZP.Store) {
     initialize() {
-        this.waitFor(c.Z, u.Z, d.Z);
+        this.waitFor(d.Z, _.Z, E.Z);
     }
     getApps() {
-        return _;
+        return I;
+    }
+    getFetchState() {
+        return m;
     }
 }
-(a = 'AuthorizedAppsStore'),
-    (s = 'displayName') in (i = E)
-        ? Object.defineProperty(i, s, {
-              value: a,
+(r = 'AuthorizedAppsStore'),
+    (a = 'displayName') in (s = T)
+        ? Object.defineProperty(s, a, {
+              value: r,
               enumerable: !0,
               configurable: !0,
               writable: !0
           })
-        : (i[s] = a),
-    (t.Z = new E(o.Z, {
+        : (s[a] = r),
+    (t.Z = new T(u.Z, {
+        USER_AUTHORIZED_APPS_REQUEST: function () {
+            m = 'FETCHING';
+        },
         USER_AUTHORIZED_APPS_UPDATE: function (e) {
-            _ = e.apps;
+            (m = 'FETCHED'), (I = e.apps);
         }
     }));
