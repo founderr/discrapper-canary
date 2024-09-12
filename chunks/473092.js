286cd95abe89399f1d506d25eb285814687dc043
DiscrapperManager <admin@minerpl.xyz>
2024-09-12 17:38:59 +0000

    canaryRelease: 326566
    Main scripts: 3
    Lazy Loaded Scripts: 874
    Chunks:
     * Main Files: 11070
     * Scripts: 9635
    Strings: 18934
    Routes: 682
    Experiments Count: 368

--- chunks/473092.js
+++ chunks/473092.js
@@ -1,12 +1,12 @@
 n.d(t, {
     KQ: function () {
-        return _;
+        return d;
     },
     Lm: function () {
-        return f;
+        return E;
     },
     MC: function () {
-        return d;
+        return c;
     },
     NM: function () {
         return s;
@@ -15,7 +15,7 @@ n.d(t, {
         return a;
     },
     qc: function () {
-        return E;
+        return _;
     }
 });
 var r,
@@ -24,9 +24,8 @@ var r,
     s,
     o = n(594174),
     l = n(626135),
-    u = n(699816),
-    c = n(981631);
-function d(e, t) {
+    u = n(981631);
+function c(e, t) {
     let { channelId: n, warningId: r, senderId: i, warningType: a } = t;
     l.default.track(e, {
         channel_id: n,
@@ -35,9 +34,9 @@ function d(e, t) {
         other_user_id: i
     });
 }
-function _(e) {
+function d(e) {
     let { channelId: t, warningId: n, senderId: r, warningType: i, viewName: a, isNudgeWarning: s } = e;
-    l.default.track(c.rMx.SAFETY_WARNING_VIEWED, {
+    l.default.track(u.rMx.SAFETY_WARNING_VIEWED, {
         channel_id: t,
         warning_id: null != n ? parseInt(n) : void 0,
         warning_type: i,
@@ -46,9 +45,9 @@ function _(e) {
         is_nudge_warning: s
     });
 }
-function E(e) {
+function _(e) {
     let { channelId: t, warningId: n, senderId: r, warningType: i, cta: a, isNudgeWarning: s } = e;
-    l.default.track(c.rMx.SAFETY_WARNING_CTA_CLICKED, {
+    l.default.track(u.rMx.SAFETY_WARNING_CTA_CLICKED, {
         channel_id: t,
         warning_id: null != n ? parseInt(n) : void 0,
         warning_type: i,
@@ -57,10 +56,8 @@ function E(e) {
         is_nudge_warning: s
     });
 }
-function f(e) {
-    var t;
-    let n = null === (t = o.default.getCurrentUser()) || void 0 === t ? void 0 : t.nsfwAllowed,
-        r = (0, u.Sx)(e);
-    return !1 === n || r;
+function E() {
+    var e;
+    return !1 === (null === (e = o.default.getCurrentUser()) || void 0 === e ? void 0 : e.nsfwAllowed);
 }
 ((r = a || (a = {})).SAFETY_WARNING_BANNER = 'safety_warning_banner'), (r.SAFETY_TAKEOVER_MODAL = 'safety_takeover_modal'), (r.SAFETY_TOOLS_NUDGE_TOOLTIP = 'safety_tools_nudge_tooltip'), (r.SAFETY_TOOLS_BUTTON = 'safety_tools_button'), ((i = s || (s = {})).OPEN_MORE_TIPS = 'open_more_tips'), (i.USER_BANNER_DISMISS = 'user_banner_dismiss'), (i.USER_BANNER_BLOCK_CONFIRM = 'user_banner_block_confirm'), (i.USER_BANNER_BLOCK_AND_REPORT_CONFIRM = 'user_banner_block_and_report_confirm'), (i.USER_BANNER_BLOCK_CANCEL = 'user_banner_block_cancel'), (i.USER_BANNER_OPEN_SAFETY_TOOLS = 'user_banner_open_safety_tools'), (i.USER_MODAL_BLOCK_CONFIRM = 'user_modal_block_confirm'), (i.USER_MODAL_BLOCK_CANCEL = 'user_modal_block_cancel'), (i.USER_TAKEOVER_MODAL_DISMISS = 'user_takeover_modal_dismiss'), (i.USER_TAKEOVER_MODAL_TAKE_ACTION = 'user_takeover_modal_take_action'), (i.USER_TAKEOVER_MODAL_SAFETY_TIPS = 'user_takeover_modal_safety_tips'), (i.USER_TAKEOVER_MODAL_BLOCK = 'user_takeover_modal_block'), (i.USER_TAKEOVER_MODAL_UNBLOCK = 'user_takeover_modal_unblock'), (i.USER_TAKEOVER_MODAL_REPORT = 'user_takeover_modal_report'), (i.USER_TAKEOVER_MODAL_NO_FILTR = 'user_takeover_modal_no_filtr'), (i.USER_TAKEOVER_MODAL_THROUGHLINE = 'user_takeover_modal_throughline'), (i.USER_TAKEOVER_MODAL_CTL = 'user_takeover_modal_ctl'), (i.USER_TAKEOVER_MODAL_CTL_SMS = 'user_takeover_modal_ctl_sms'), (i.USER_TAKEOVER_MODAL_CTL_WEB = 'user_takeover_modal_ctl_web'), (i.USER_SAFETY_TOOLS_BUTTON_CLICK = 'user_safety_tools_button_click'), (i.USER_SAFETY_TOOLS_BLOCK_CONFIRM = 'user_safety_tools_block_confirm'), (i.USER_SAFETY_TOOLS_BLOCK_AND_REPORT_CONFIRM = 'user_safety_tools_block_and_report_confirm'), (i.USER_SAFETY_TOOLS_BLOCK_CANCEL = 'user_safety_tools_block_cancel'), (i.USER_SAFETY_TOOLS_UNBLOCK = 'user_safety_tools_unblock'), (i.USER_SAFETY_TOOLS_REPORT = 'user_safety_tools_report'), (i.USER_SAFETY_TOOLS_NO_FILTR = 'user_safety_tools_no_filtr'), (i.USER_SAFETY_TOOLS_THROUGHLINE = 'user_safety_tools_throughline'), (i.USER_SAFETY_TOOLS_CTL = 'user_safety_tools_ctl'), (i.USER_SAFETY_TOOLS_CTL_SMS = 'user_safety_tools_ctl_sms'), (i.USER_SAFETY_TOOLS_CTL_WEB = 'user_safety_tools_ctl_web'), (i.USER_SAFETY_TOOLS_VIBING_WUMPUS = 'user_safety_tools_vibing_wumpus'), (i.USER_SAFETY_TOOLS_SAFETY_TIPS = 'user_safety_tools_safety_tips'), (i.USER_SAFETY_TOOLS_ABOUT_SAFETY_ALERTS = 'user_safety_tools_about_safety_alerts'), (i.USER_SAFETY_TOOLS_ABOUT_SAFETY_ALERTS_DISMISS = 'user_safety_tools_about_safety_alerts_dismiss'), (i.USER_SAFETY_TOOLS_DISMISS = 'user_safety_tools_dismiss'), (i.USER_MODAL_MUTE = 'user_modal_mute'), (i.USER_MODAL_UNMUTE = 'user_modal_unmute'), (i.FEEDBACK_UPVOTE = 'feedback_upvote'), (i.FEEDBACK_DOWNVOTE = 'feedback_downvote');
