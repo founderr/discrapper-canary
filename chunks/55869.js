0ca1e160663f2c12ba53b24c4aa9425f02bf1318
DiscrapperManager <admin@minerpl.xyz>
2024-09-13 01:43:45 +0000

    canaryRelease: 326854
    Main scripts: 3
    Lazy Loaded Scripts: 873
    Chunks:
     * Main Files: 11049
     * Scripts: 9619
    Strings: 18889
    Routes: 679
    Experiments Count: 367

---
+++ chunks/55869.js
@@ -0,0 +1,1 @@
+e.exports = { guildNotif: 'guildNotif_e3bac1' };
--- chunks/602147.js
+++ chunks/602147.js
@@ -1,63 +1,81 @@
 n.d(t, {
     o: function () {
-        return N;
+        return Z;
     }
-});
+}),
+    n(47120);
 var i = n(735250),
     l = n(470079),
     r = n(636977),
-    a = n(481060),
-    s = n(212093),
-    o = n(963202),
-    c = n(831565),
-    u = n(571457),
-    d = n(836768),
-    h = n(341907),
-    p = n(703656),
-    _ = n(683301),
-    f = n(603592),
-    m = n(981631),
-    g = n(731455),
-    C = n(49898),
-    I = n(46140),
-    E = n(689938);
-function N(e, t) {
-    let n = (0, c.a)({ location: 'discovery_button' }),
-        i = window.location.pathname.startsWith(m.Z5c.GUILD_DISCOVERY),
-        l = _.ZP.getCurrentCategoryId() === g.Gj.Clans;
+    a = n(704215),
+    s = n(481060),
+    o = n(212093),
+    c = n(169559),
+    u = n(706140),
+    d = n(831565),
+    h = n(571457),
+    p = n(836768),
+    _ = n(341907),
+    f = n(703656),
+    m = n(683301),
+    g = n(603592),
+    C = n(981631),
+    I = n(731455),
+    E = n(921944),
+    N = n(49898),
+    x = n(46140),
+    S = n(689938),
+    v = n(55869);
+function Z(e, t) {
+    let n = (0, d.a)({ location: 'discovery_button' }),
+        i = window.location.pathname.startsWith(C.Z5c.GUILD_DISCOVERY),
+        l = m.ZP.getCurrentCategoryId() === I.Gj.Clans;
     if (n) {
-        if (e === C.F$.QUESTS) {
-            (0, h.navigateToQuestHome)(I.dr.DISCOVERY_COMPASS, r.j.DISCOVERY_COMPASS);
+        if (e === N.F$.QUESTS) {
+            (0, _.navigateToQuestHome)(x.dr.DISCOVERY_COMPASS, r.j.DISCOVERY_COMPASS);
             return;
         }
-        if (e === C.F$.SERVERS) {
-            (0, p.uL)(m.Z5c.GLOBAL_DISCOVERY_SERVERS);
+        if (e === N.F$.SERVERS) {
+            (0, f.uL)(C.Z5c.GLOBAL_DISCOVERY_SERVERS);
             return;
         }
-        (0, p.uL)(m.Z5c.GLOBAL_DISCOVERY);
+        (0, f.uL)(C.Z5c.GLOBAL_DISCOVERY);
         return;
     }
-    i && !l && ((0, s.AQ)(), (0, s.uY)(t, !0));
-    let a = (0, p.s1)().location.search;
-    (0, p.uL)(m.Z5c.GUILD_DISCOVERY, { search: a });
+    i && !l && ((0, o.AQ)(), (0, o.uY)(t, !0));
+    let a = (0, f.s1)().location.search;
+    (0, f.uL)(C.Z5c.GUILD_DISCOVERY, { search: a });
 }
-let x = l.forwardRef(function (e, t) {
-    let { selected: n, className: r, onClick: s } = e,
-        { clanDiscoveryEnabled: h } = (0, o.nk)('guild_discovery'),
-        p = (0, c.v)({ location: 'guild_discovery_button' }) ? E.Z.Messages.DISCOVER : E.Z.Messages.GUILD_DISCOVERY_TOOLTIP,
-        _ = d.d.getState().selectedTab,
-        m = h ? g.Gj.Clans : g.Hk,
-        I = l.useCallback(() => {
-            null == s || s(), (0, u.k5)({ source: C.JU.GUILDS_BAR_ICON }), N(_, m);
-        }, [s, _, m]);
-    return (0, i.jsx)(f.Z, {
+let T = l.forwardRef(function (e, t) {
+    let { selected: n, className: r, onClick: o } = e,
+        _ = (0, d.v)({ location: 'guild_discovery_button' }) ? S.Z.Messages.DISCOVER : S.Z.Messages.GUILD_DISCOVERY_TOOLTIP,
+        f = p.d.getState().selectedTab,
+        m = (0, c.Z)('discovery_button'),
+        C = m ? I.Gj.Clans : I.Hk,
+        [x, T] = (0, u.cv)([a.z.GLOBAL_DISCOVERY_RAPIDASH_NOTIF]),
+        L = l.useCallback(() => {
+            null == o || o(), (0, h.k5)({ source: N.JU.GUILDS_BAR_ICON }), Z(f, C), T(E.L.DISMISS);
+        }, [o, f, C, T]),
+        A = null != x && m,
+        b = l.useMemo(
+            () => ({
+                lowerBadgeSize: {
+                    width: 12,
+                    height: 12
+                },
+                lowerBadge: A ? (0, i.jsx)('div', { className: v.guildNotif }) : void 0
+            }),
+            [A]
+        );
+    return (0, i.jsx)(g.Z, {
         id: 'guild-discover-button',
         ref: t,
         className: r,
-        onClick: I,
+        onClick: L,
         selected: n,
-        tooltip: p,
-        icon: a.CompassIcon
+        tooltip: _,
+        icon: s.CompassIcon,
+        ...b
     });
 });
-t.Z = x;
+t.Z = T;
