f5ef578c2ba4e108a629fae68f303d00706dff1c
DiscrapperManager <admin@minerpl.xyz>
2024-09-16 16:30:55 +0000

    canaryRelease: 327264
    Main scripts: 3
    Lazy Loaded Scripts: 873
    Chunks:
     * Main Files: 11051
     * Scripts: 9633
    Strings: 18900
    Routes: 679
    Experiments Count: 368

--- chunks/800927.js
+++ chunks/800927.js
@@ -1,4 +1,4 @@
-n(757143);
+n(757143), n(627494);
 var r = n(512722),
     i = n.n(r),
     a = n(302454),
@@ -6,9 +6,9 @@ var r = n(512722),
 let o = /\n{2,}$/,
     l = /(?:^|\n)( *)$/,
     u = '(?:[*-]|\\d+\\.)',
-    c = '( *)(' + u + ') +',
-    d = RegExp('^' + c),
-    _ = RegExp(c + '[^\\n]*(?:\\n(?!\\1' + u + ' )[^\\n]*)*(\n|$)', 'gm'),
+    c = '(%INDENT_CAPTURE_PATTERN%)(' + u + ') +',
+    d = RegExp('^' + c.replace('%INDENT_CAPTURE_PATTERN%', ' *')),
+    _ = c + '[^\\n]*(?:\\n(?!%INDENT_CAPTURE_PATTERN%' + u + ' )[^\\n]*)*(\n|$)',
     E = / *\n$/,
     f = RegExp('^( *)(' + u + ') [\\s\\S]+?(?:\\n(?! )(?!\\1' + u + ' )|$)'),
     h = /^[ \t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]+$/,
@@ -26,31 +26,36 @@ let o = /\n{2,}$/,
             let r = e[2],
                 a = r.length > 1,
                 s = a ? Math.min(1000000000, Math.max(1, +r)) : void 0,
-                l = e[0].replace(o, '\n').match(_);
-            i()(null != l, 'markup list items can not be parsed.');
-            let u = !1;
+                l = e[0].replace(o, '\n'),
+                u = d.exec(l),
+                c = null != u ? u[0].length : 0,
+                f = null != u ? u[1].length : 0,
+                h = ' {'.concat(f, ',').concat(f + 1, '}'),
+                I = RegExp(_.replaceAll('%INDENT_CAPTURE_PATTERN%', h), 'gm'),
+                m = RegExp('^ {1,' + c + '}', 'gm'),
+                T = l.match(I);
+            i()(null != T, 'markup list items can not be parsed.');
+            let S = !1;
             return {
                 ordered: a,
                 start: s,
-                items: l.map((e, r) => {
+                items: T.map((e, r) => {
                     let i;
-                    let a = d.exec(e),
-                        s = RegExp('^ {1,' + (null != a ? a[0].length : 0) + '}', 'gm'),
-                        o = e.replace(s, '').replace(d, ''),
-                        c = r === l.length - 1,
-                        _ = -1 !== o.indexOf('\n\n') || (c && u);
-                    u = _;
-                    let f = n.inline,
-                        h = n._list,
-                        I = n._listLevel;
-                    (n._list = !0), (n._listLevel = (null != I ? I : 0) + 1), _ ? ((n.inline = !1), (i = o.replace(E, '\n\n'))) : ((n.inline = !0), (i = o.replace(E, '')));
-                    let m = p(
+                    let a = e.replace(d, '').replace(m, ''),
+                        s = r === T.length - 1,
+                        o = -1 !== a.indexOf('\n\n') || (s && S);
+                    S = o;
+                    let l = n.inline,
+                        u = n._list,
+                        c = n._listLevel;
+                    (n._list = !0), (n._listLevel = (null != c ? c : 0) + 1), o ? ((n.inline = !1), (i = a.replace(E, '\n\n'))) : ((n.inline = !0), (i = a.replace(E, '')));
+                    let _ = p(
                         t(i, {
                             ...n,
                             allowHeading: !1
                         })
                     );
-                    return (n.inline = f), (n._list = h), (n._listLevel = I), m;
+                    return (n.inline = l), (n._list = u), (n._listLevel = c), _;
                 })
             };
         }
