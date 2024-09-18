f53d7afee8fbe02fa1b221ef1f19b42d6f505023
DiscrapperManager <admin@minerpl.xyz>
2024-09-18 21:26:10 +0000

    canaryRelease: 328290
    Main scripts: 3
    Lazy Loaded Scripts: 877
    Chunks:
     * Main Files: 11068
     * Scripts: 9626
    Strings: 18920
    Routes: 679
    Experiments Count: 367

--- chunks/269210.js
+++ chunks/269210.js
@@ -1,9 +1,9 @@
 t.d(n, {
     G: function () {
-        return m;
+        return v;
     },
     U: function () {
-        return p;
+        return m;
     }
 });
 var l = t(735250);
@@ -16,28 +16,29 @@ var i = t(557533),
     u = t(752843),
     c = t(330711),
     d = t(56273),
-    f = t(103077);
-function p(e) {
-    let { description: n, imgSrc: t, renderPurchaseButton: i, onPurchase: p, subscriptionType: m, title: v, onDetails: I, benefitItems: S, benefitsSummary: h } = e;
+    f = t(103077),
+    p = t(692547);
+function m(e) {
+    let { description: n, imgSrc: t, renderPurchaseButton: i, onPurchase: m, subscriptionType: v, title: I, onDetails: S, benefitItems: h, benefitsSummary: g } = e;
     return (0, l.jsx)(s.tE, {
         children: (0, l.jsxs)('div', {
             className: r()(d.container, f.hoverCard),
             tabIndex: 0,
             onClick: () => {
-                null != I ? I() : null != p && p();
+                null != S ? S() : null != m && m();
             },
             children: [
                 (0, l.jsxs)('div', {
                     className: d.card,
                     children: [
-                        null != m && (0, l.jsx)(u.Z, { type: m }),
+                        null != v && (0, l.jsx)(u.Z, { type: v }),
                         (0, l.jsxs)('div', {
                             className: d.body,
                             children: [
                                 (0, l.jsx)(a.x, {
                                     tag: 'div',
                                     variant: 'heading-lg/semibold',
-                                    children: v
+                                    children: I
                                 }),
                                 (0, l.jsx)('div', {
                                     className: d.image,
@@ -48,7 +49,7 @@ function p(e) {
                                                   alt: ''
                                               })
                                             : (0, l.jsx)(o.V, {
-                                                  color: 'white',
+                                                  color: p.Z.colors.ICON_PRIMARY,
                                                   size: 'custom',
                                                   height: 48,
                                                   width: 48
@@ -58,7 +59,7 @@ function p(e) {
                                     ? i({
                                           className: d.purchaseBtn,
                                           onClick: (e) => {
-                                              e.stopPropagation(), null == p || p();
+                                              e.stopPropagation(), null == m || m();
                                           }
                                       })
                                     : null,
@@ -74,16 +75,16 @@ function p(e) {
                         })
                     ]
                 }),
-                null != h &&
+                null != g &&
                     (0, l.jsx)('div', {
                         className: d.benefitsSummary,
                         children: (0, l.jsx)(a.x, {
                             color: 'interactive-normal',
                             variant: 'text-sm/medium',
-                            children: c.Z.Messages.Storefront.BENEFITS_SUMMARY.format({ count: h })
+                            children: c.Z.Messages.Storefront.BENEFITS_SUMMARY.format({ count: g })
                         })
                     }),
-                null != S &&
+                null != h &&
                     (0, l.jsx)('div', {
                         className: d.benefits,
                         children: (0, l.jsx)('div', {
@@ -96,18 +97,18 @@ function p(e) {
                                         variant: 'eyebrow',
                                         children: c.Z.Messages.Storefront.BENEFITS_TITLE
                                     }),
-                                    S.length > 3
+                                    h.length > 3
                                         ? (0, l.jsxs)(l.Fragment, {
                                               children: [
-                                                  S.slice(0, 3),
+                                                  h.slice(0, 3),
                                                   (0, l.jsx)(a.x, {
                                                       variant: 'text-md/semibold',
                                                       color: 'text-secondary',
-                                                      children: c.Z.Messages.Storefront.MORE_BENEFITS.format({ count: S.length - 3 })
+                                                      children: c.Z.Messages.Storefront.MORE_BENEFITS.format({ count: h.length - 3 })
                                                   })
                                               ]
                                           })
-                                        : S
+                                        : h
                                 ]
                             })
                         })
@@ -116,7 +117,7 @@ function p(e) {
         })
     });
 }
-function m(e) {
+function v(e) {
     let { icon: n, name: t, description: i } = e;
     return (0, l.jsxs)('div', {
         className: d.cardBenefit,
