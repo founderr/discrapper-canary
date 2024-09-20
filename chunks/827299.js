a2c5c65f43e0c2a0838a30ec33d6001634691ee7
DiscrapperManager <admin@minerpl.xyz>
2024-09-20 01:01:30 +0000

    canaryRelease: 328892
    Main scripts: 3
    Lazy Loaded Scripts: 876
    Chunks:
     * Main Files: 11077
     * Scripts: 9632
    Strings: 18934
    Routes: 682
    Experiments Count: 366

--- chunks/827299.js
+++ chunks/827299.js
@@ -1,118 +1,123 @@
 n.d(t, {
     K: function () {
-        return E;
+        return d;
     }
 }),
-    n(47120);
+    n(47120),
+    n(653041);
 var r = n(470079),
     i = n(399606);
-function a(e, t, n) {
-    if (!t.has(e)) throw TypeError('attempted to ' + n + ' private field on non-instance');
-    return t.get(e);
-}
-function s(e, t) {
+function a(e, t) {
     var n,
         r,
-        i = a(e, t, 'get');
+        i = (function (e, t, n) {
+            if (!t.has(e)) throw TypeError('attempted to get private field on non-instance');
+            return t.get(e);
+        })(e, t, 'get');
     return (n = e), (r = i).get ? r.get.call(n) : r.value;
 }
-function o(e, t, n) {
+function s(e, t, n) {
     !(function (e, t) {
         if (t.has(e)) throw TypeError('Cannot initialize the same private elements twice on an object');
     })(e, t),
         t.set(e, n);
 }
-function l(e, t, n) {
-    var r = a(e, t, 'set');
-    return (
-        !(function (e, t, n) {
-            if (t.set) t.set.call(e, n);
-            else {
-                if (!t.writable) throw TypeError('attempted to set read only private field');
-                t.value = n;
-            }
-        })(e, r, n),
-        n
-    );
-}
-var u = new WeakMap(),
-    c = new WeakMap();
-class d {
+var o = new WeakMap(),
+    l = new WeakMap();
+class u {
+    doesDataNeedValidation(e) {
+        return !0 === a(this.search(e), l).isStale;
+    }
     getOrCreate(e) {
-        return null == s(this, u)[e] && (s(this, u)[e] = new d()), s(this, u)[e];
+        return null == a(this, o)[e] && (a(this, o)[e] = new u()), a(this, o)[e];
     }
     getState(e) {
         var t;
-        return s(this.search(e), c);
+        return a(this.search(e), l);
     }
     loadingDone(e) {
-        s(this.search(e), c).isLoading = !1;
+        let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1],
+            n = this.search(e);
+        (a(n, l).isLoading = !1), t && (a(n, l).isStale = !1);
     }
     loadingStart(e) {
-        s(this.search(e), c).isLoading = !0;
-    }
-    markBranchStale(e) {
-        let t = this;
-        for (let n of e) t = t.getOrCreate(n);
-        s(t, c).stale = !0;
-    }
-    pruneStaleBranches(e) {
-        let t = this;
-        for (let n of e) if (!0 === s((t = t.getOrCreate(n)), c).stale) return l(t, u, {}), l(t, c, {}), !0;
-        return !s(t, c).hasOwnProperty('isLoading');
+        a(this.search(e), l).isLoading = !0;
     }
     search(e) {
-        if (null == e) return new d();
+        if (null == e) return new u();
         let t = this;
         for (let n of e) t = t.getOrCreate(n);
         return t;
     }
+    subscribe(e, t) {
+        a(this.search(e), l).validateData = t;
+    }
+    validate(e) {
+        let t = this.search(e),
+            n = [];
+        'function' == typeof a(t, l).validateData && n.push(a(t, l).validateData);
+        let r = Object.values(a(t, o));
+        for (; r.length > 0; ) {
+            let e = r.pop();
+            null != e && ((a(e, l).isStale = !0), r.push(...Object.values(a(e, o))), 'function' == typeof a(e, l).validateData && n.push(a(e, l).validateData));
+        }
+        (a(t, l).isStale = !0), n.forEach((e) => e());
+    }
     constructor() {
-        o(this, u, {
+        s(this, o, {
             writable: !0,
             value: {}
         }),
-            o(this, c, {
+            s(this, l, {
                 writable: !0,
                 value: {}
             });
     }
 }
-let _ = new d();
-function E(e, t) {
+let c = new u();
+function d(e, t) {
     let { get: n, load: a, useStateHook: s, queryId: o, dangerousAbortOnCleanup: l = !1 } = t;
     return function () {
-        for (var t = arguments.length, u = Array(t), c = 0; c < t; c++) u[c] = arguments[c];
-        let d = o(...u),
+        for (var t = arguments.length, u = Array(t), d = 0; d < t; d++) u[d] = arguments[d];
+        let _ = (0, r.useMemo)(() => o(...u), u),
             E = s([e], () => n(...u), u),
-            f = !0 === _.getState(d).isLoading,
-            [h, p] = (0, r.useState)();
-        return (
-            (0, r.useEffect)(() => {
-                let e = !0 === _.getState(d).isLoading;
-                if (null == d || !0 === e) return;
-                let t = !1,
-                    r = n(...u);
-                s === i.Wu ? r.length > 0 && (t = !0) : null != r && (t = !0);
-                let o = _.pruneStaleBranches(d);
-                if (t && !o) return;
-                _.loadingStart(d);
-                let c = new AbortController();
+            [f, h] = (0, r.useState)(),
+            p = !0 === c.getState(_).isLoading,
+            I = (0, r.useRef)(u),
+            m = (0, r.useCallback)(() => {
+                if (null == _ || !0 === p) return;
+                let e = !1;
+                s === i.Wu ? E.length > 0 && (e = !0) : null != E && (e = !0);
+                let t = c.doesDataNeedValidation(_);
+                if (e && !t) return;
+                c.loadingStart(_);
+                let n = new AbortController();
                 return (
-                    a(c.signal, ...u)
-                        .then((e) => (_.loadingDone(d), e))
+                    a(n.signal, ...I.current)
+                        .then((e) => (c.loadingDone(_, !0), e))
                         .catch((e) => {
-                            _.loadingDone(d), !c.signal.aborted && p(e);
+                            c.loadingDone(_), !n.signal.aborted && h(e);
                         }),
                     () => {
-                        l && c.abort();
+                        l && n.abort();
                     }
                 );
-            }, u),
+            }, [E, _, p]);
+        return (
+            (0, r.useEffect)(
+                () => (
+                    m(),
+                    c.subscribe(_, m),
+                    () => {
+                        c.subscribe(_, void 0);
+                    }
+                ),
+                [_, m]
+            ),
             {
                 data: E,
-                error: h,
-                isLoading: f
+                error: f,
+                isLoading: p
             }
         );
     };
