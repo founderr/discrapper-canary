a28247ed955490fdf02a87b62d49e3fe92316a44
DiscrapperManager <admin@minerpl.xyz>
2024-09-20 00:56:20 +0000

    canaryRelease: 328889
    Main scripts: 3
    Lazy Loaded Scripts: 876
    Chunks:
     * Main Files: 11077
     * Scripts: 9632
    Strings: 18934
    Routes: 682
    Experiments Count: 366

---
+++ chunks/209960.js
@@ -0,0 +1,1 @@
+e.exports = { noBoxShadowMargin: 'noBoxShadowMargin_a7ae01' };
--- chunks/511341.js
+++ chunks/511341.js
@@ -1,57 +1,65 @@
 n.d(t, {
     Z: function () {
-        return m;
+        return N;
     }
 });
 var r = n(735250),
     i = n(470079),
-    a = n(907040),
-    s = n(823379),
-    o = n(51144),
-    l = n(785717),
-    u = n(448197),
-    c = n(698305),
-    d = n(825801),
-    _ = n(228168),
-    E = n(185923),
-    f = n(420212),
-    h = n(231338),
-    p = n(689938);
-let I = (e) => {
+    a = n(442837),
+    s = n(780384),
+    o = n(907040),
+    l = n(210887),
+    u = n(823379),
+    c = n(51144),
+    d = n(785717),
+    _ = n(448197),
+    E = n(698305),
+    f = n(825801),
+    h = n(652853),
+    p = n(228168),
+    I = n(185923),
+    m = n(420212),
+    T = n(231338),
+    S = n(689938),
+    g = n(209960);
+let A = (e) => {
     let { emoji: t, username: n, sourceType: r, sourceDetails: i } = e,
         a = ':'.concat(t.name, ':');
     switch (r) {
-        case _.n_.ACTIVITY:
-            let o = p.Z.Messages.USER_PROFILE_REACTED_TO_ACTIVITY.format({ username: n });
-            return ''.concat(d.jd).concat(o, '*\n').concat(a);
-        case _.n_.AVATAR:
-            let l = p.Z.Messages.USER_PROFILE_REACTED_TO_AVATAR.format({ username: n });
-            return ''.concat(d.jd).concat(l, '*\n').concat(a);
-        case _.n_.STATUS:
-            let u = p.Z.Messages.USER_PROFILE_REACTED_TO_STATUS.format({ username: n });
-            return null != i ? ''.concat(d.jd).concat(u, '*').concat('\n> '.concat(i), '\n').concat(a) : ''.concat(d.jd).concat(u, '*\n').concat(a);
+        case p.n_.ACTIVITY:
+            let s = S.Z.Messages.USER_PROFILE_REACTED_TO_ACTIVITY.format({ username: n });
+            return ''.concat(f.jd).concat(s, '*\n').concat(a);
+        case p.n_.AVATAR:
+            let o = S.Z.Messages.USER_PROFILE_REACTED_TO_AVATAR.format({ username: n });
+            return ''.concat(f.jd).concat(o, '*\n').concat(a);
+        case p.n_.STATUS:
+            let l = S.Z.Messages.USER_PROFILE_REACTED_TO_STATUS.format({ username: n });
+            return null != i ? ''.concat(f.jd).concat(l, '*').concat('\n> '.concat(i), '\n').concat(a) : ''.concat(f.jd).concat(l, '*\n').concat(a);
         default:
-            (0, s.vE)(r);
+            (0, u.vE)(r);
     }
 };
-function m(e) {
-    let { user: t, guildId: n, sourceType: s, sourceDetails: p, onInteraction: m, setPopoutRef: T, setInteractionToastShown: S, setInteractionTypeSent: g, onClose: A } = e,
-        { trackUserProfileAction: N } = (0, l.KZ)(),
-        { sendReact: O } = (0, u.Q)(s),
-        R = i.useRef(null);
+function N(e) {
+    let { user: t, guildId: n, sourceType: u, sourceDetails: S, onInteraction: N, setPopoutRef: O, setInteractionToastShown: R, setInteractionTypeSent: v, onClose: C } = e,
+        { trackUserProfileAction: y } = (0, d.KZ)(),
+        { sendReact: L } = (0, _.Q)(u),
+        { theme: D } = (0, h.z)(),
+        b = (0, a.e7)([l.Z], () => l.Z.theme),
+        M = (0, s.wj)(b) ? !(0, s.wj)(D) : (0, s.wj)(D),
+        P = i.useRef(null);
     i.useEffect(() => {
-        null == T || T(null == R ? void 0 : R.current);
-    }, [R, T]),
+        null == O || O(null == P ? void 0 : P.current);
+    }, [P, O]),
         i.useEffect(() => {
             let e = (e) => {
-                e.key === f.vn.ESCAPE &&
+                e.key === m.vn.ESCAPE &&
                     (e.stopPropagation(),
-                    null == m ||
-                        m({
+                    null == N ||
+                        N({
                             interactionType: null,
                             interactionSourceType: null
                         }),
-                    null == A || A());
+                    null == C || C());
             };
             return (
                 document.addEventListener('keydown', e),
@@ -59,19 +67,19 @@ function m(e) {
                     document.removeEventListener('keydown', e);
                 }
             );
-        }, [A, m]);
-    let v = async (e) => {
+        }, [C, N]);
+    let U = async (e) => {
         if (null == e) return;
-        N({ action: O });
-        let n = I({
+        y({ action: L });
+        let n = A({
             emoji: e,
-            username: o.ZP.getName(t),
-            sourceType: s,
-            sourceDetails: p
+            username: c.ZP.getName(t),
+            sourceType: u,
+            sourceDetails: S
         });
-        g(null), S(!0);
+        v(null), R(!0);
         try {
-            await (0, c.Z)({
+            await (0, E.Z)({
                 userId: t.id,
                 content: n,
                 location: 'UserProfileReactReplyBar',
@@ -79,24 +87,25 @@ function m(e) {
                 whenReady: !1
             });
         } catch (e) {}
-        g(_.P.REACT),
+        v(p.P.REACT),
             setTimeout(() => {
-                S(!1);
-            }, d._1);
+                R(!1);
+            }, f._1);
     };
-    return (0, r.jsx)(a.Z, {
+    return (0, r.jsx)(o.Z, {
+        headerClassName: M ? g.noBoxShadowMargin : void 0,
         guildId: null != n ? n : void 0,
-        closePopout: h.dG,
+        closePopout: T.dG,
         onSelectEmoji: async (e, t) => {
-            await v(e),
+            await U(e),
                 t &&
-                    (null == m ||
-                        m({
+                    (null == N ||
+                        N({
                             interactionType: null,
                             interactionSourceType: null
                         }),
-                    null == A || A());
+                    null == C || C());
         },
-        pickerIntention: E.Hz.PROFILE
+        pickerIntention: I.Hz.PROFILE
     });
 }
