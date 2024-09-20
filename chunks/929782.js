acd0054b2fc54c42ba21a9475aadf76200f9d878
DiscrapperManager <admin@minerpl.xyz>
2024-09-20 00:51:15 +0000

    canaryRelease: 328888
    Main scripts: 3
    Lazy Loaded Scripts: 876
    Chunks:
     * Main Files: 11076
     * Scripts: 9632
    Strings: 18934
    Routes: 682
    Experiments Count: 366

--- chunks/929782.js
+++ chunks/929782.js
@@ -1,13 +1,13 @@
 n.r(t),
     n.d(t, {
         enable: function () {
-            return T;
+            return g;
         },
         isNotSupported: function () {
-            return I;
+            return T;
         },
         trackToggleSelfMute: function () {
-            return S;
+            return A;
         }
     });
 var r = n(735250);
@@ -15,16 +15,18 @@ n(470079);
 var i = n(481060),
     a = n(570140),
     s = n(710845),
-    o = n(131951),
-    l = n(556296),
-    u = n(451478),
-    c = n(626135),
-    d = n(554174),
-    _ = n(981631),
-    E = n(761274),
-    f = n(689938);
-let h = new s.Z('AudioActionCreators');
-function p() {
+    o = n(592125),
+    l = n(131951),
+    u = n(944486),
+    c = n(556296),
+    d = n(451478),
+    _ = n(626135),
+    E = n(554174),
+    f = n(981631),
+    h = n(761274),
+    p = n(689938);
+let I = new s.Z('AudioActionCreators');
+function m() {
     (0, i.openModalLazy)(async () => {
         let { default: e } = await n.e('82935').then(n.bind(n, 431583));
         return (t) =>
@@ -34,38 +36,38 @@ function p() {
             });
     });
 }
-function I() {
+function T() {
     return (
-        !o.Z.isSupported() &&
+        !l.Z.isSupported() &&
         ((0, i.openModal)((e) =>
             (0, r.jsx)(i.ConfirmModal, {
-                header: f.Z.Messages.UNSUPPORTED_BROWSER,
-                confirmText: f.Z.Messages.DOWNLOAD_APP,
-                cancelText: f.Z.Messages.CANCEL,
-                onConfirm: p,
+                header: p.Z.Messages.UNSUPPORTED_BROWSER,
+                confirmText: p.Z.Messages.DOWNLOAD_APP,
+                cancelText: p.Z.Messages.CANCEL,
+                onConfirm: m,
                 confirmButtonColor: i.Button.Colors.BRAND,
                 ...e,
                 children: (0, r.jsx)(i.Text, {
                     variant: 'text-md/normal',
-                    children: f.Z.Messages.UNSUPPORTED_BROWSER_DETAILS
+                    children: p.Z.Messages.UNSUPPORTED_BROWSER_DETAILS
                 })
             })
         ),
         !0)
     );
 }
-function m(e) {
-    c.default.track(_.rMx.PERMISSIONS_ACKED, {
+function S(e) {
+    _.default.track(f.rMx.PERMISSIONS_ACKED, {
         type: 'audio',
         action: e
     });
 }
-function T() {
+function g() {
     let e = !(arguments.length > 0) || void 0 === arguments[0] || arguments[0];
-    return I()
+    return T()
         ? Promise.resolve(!1)
-        : (c.default.track(_.rMx.PERMISSIONS_REQUESTED, { type: 'audio' }),
-          o.Z.getMediaEngine()
+        : (_.default.track(f.rMx.PERMISSIONS_REQUESTED, { type: 'audio' }),
+          l.Z.getMediaEngine()
               .enable()
               .then(
                   () => {
@@ -74,35 +76,38 @@ function T() {
                           enabled: !0,
                           unmute: e
                       }),
-                          m(E.PQ.ACCEPTED);
+                          S(h.PQ.ACCEPTED);
                   },
                   (e) => {
                       switch (e) {
-                          case _.ETv.NO_DEVICES_FOUND:
-                              m(E.PQ.NO_DEVICES);
+                          case f.ETv.NO_DEVICES_FOUND:
+                              S(h.PQ.NO_DEVICES);
                               break;
-                          case _.ETv.PERMISSION_DENIED:
-                              m(E.PQ.DENIED);
+                          case f.ETv.PERMISSION_DENIED:
+                              S(h.PQ.DENIED);
                               break;
-                          case _.ETv.PERMISSION_DISMISSED:
-                              m(E.PQ.DISMISSED);
+                          case f.ETv.PERMISSION_DISMISSED:
+                              S(h.PQ.DISMISSED);
                               break;
                           default:
-                              m(E.PQ.ERROR), h.warn('unknown getUserMedia error: '.concat(e));
+                              S(h.PQ.ERROR), I.warn('unknown getUserMedia error: '.concat(e));
                       }
                   }
               )
               .then(() => !0));
 }
-function S(e) {
+function A(e) {
     let { usedKeybind: t = !1, location: n } = e,
-        r = l.Z.getKeybindForAction(_.kg4.TOGGLE_MUTE, !1, !0);
-    c.default.track(_.rMx.INPUT_MUTE_TOGGLED, {
-        enabled: !o.Z.isSelfMute(),
-        custom_keybind_assigned: null != r && r.id !== l.E.id,
+        r = c.Z.getKeybindForAction(f.kg4.TOGGLE_MUTE, !1, !0),
+        i = u.Z.getVoiceChannelId(),
+        a = null != i ? o.Z.getChannel(i) : null;
+    _.default.track(f.rMx.INPUT_MUTE_TOGGLED, {
+        enabled: !l.Z.isSelfMute(),
+        custom_keybind_assigned: null != r && r.id !== c.E.id,
         used_keybind: t,
-        app_in_focus: u.Z.isFocused(),
-        overlay_activated: null != (0, d.Z)(),
+        app_in_focus: d.Z.isFocused(),
+        overlay_activated: null != (0, E.Z)(),
+        voice_channel_type: null != a ? a.type : null,
         location: n
     });
 }
