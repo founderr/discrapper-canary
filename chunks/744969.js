c37356d7f2d548752ec93795b31b1ad43391ba5d
DiscrapperManager <admin@minerpl.xyz>
2024-09-25 15:57:39 +0000

    canaryRelease: 330336
    Main scripts: 3
    Lazy Loaded Scripts: 883
    Chunks:
     * Main Files: 11084
     * Scripts: 9673
    Strings: 18987
    Routes: 676
    Experiments Count: 367

---
+++ chunks/744969.js
@@ -0,0 +1,133 @@
+t.d(n, {
+    s: function () {
+        return x;
+    }
+}),
+    t(789020);
+var l = t(735250),
+    i = t(470079),
+    a = t(442837),
+    r = t(544891),
+    o = t(481060),
+    s = t(570140),
+    c = t(171246),
+    u = t(509545),
+    d = t(55563),
+    m = t(551428),
+    f = t(74538),
+    _ = t(937615),
+    p = t(147496),
+    C = t(519896),
+    h = t(981631),
+    I = t(689938);
+let E = (0, a.Kb)([d.Z, u.Z, m.Z], {
+    queryId: (e) => (null != e ? ['premium-button', e] : null),
+    get: (e) => {
+        var n;
+        if (null == e) return;
+        let t = d.Z.get(e),
+            l = null !== (n = u.Z.getForSKU(e)) && void 0 !== n ? n : [],
+            i = m.Z.getForSKU(e),
+            a = d.Z.getParentSKU(e);
+        if (null != t && null != i && (null == t || t.type !== h.epS.SUBSCRIPTION || null != a))
+            return {
+                parentSku: a,
+                sku: t,
+                storeListing: i,
+                subscriptionPlans: l
+            };
+    },
+    load: async (e, n) => {
+        if (null == n) return;
+        let t = { url: h.ANM.STOREFRONT_PREMIUM_BUTTON(n) },
+            { body: l } = await r.tn.get(t);
+        s.Z.dispatch({
+            type: 'STORE_LISTINGS_FETCH_SUCCESS',
+            storeListings: l.store_listings
+        }),
+            null != l.subscription_plans &&
+                s.Z.dispatch({
+                    type: 'SUBSCRIPTION_PLANS_FETCH_SUCCESS',
+                    skuId: n,
+                    subscriptionPlans: l.subscription_plans
+                }),
+            null != l.skus &&
+                l.skus.forEach((e) => {
+                    s.Z.dispatch({
+                        type: 'SKU_FETCH_SUCCESS',
+                        sku: e
+                    });
+                });
+    },
+    useStateHook: a.cj
+});
+function x(e) {
+    let { data: n } = E(e),
+        t = null == n ? void 0 : n.parentSku,
+        a = null == n ? void 0 : n.sku,
+        r = null == n ? void 0 : n.storeListing,
+        s = null == n ? void 0 : n.subscriptionPlans,
+        u = i.useCallback(() => {
+            if ((null == a ? void 0 : a.applicationId) == null || (null == a ? void 0 : a.id) == null || (null == t ? void 0 : t.id) == null || (null == a ? void 0 : a.flags) == null) return null;
+            (0, o.openModal)((e) => {
+                let { onClose: n, transitionState: i } = e;
+                return (0, l.jsx)(C.SubscriptionDetailsModal, {
+                    appId: a.applicationId,
+                    groupListingId: t.id,
+                    subscriptionType: (0, c.KW)(a.flags) ? 'user' : 'guild',
+                    onClose: n,
+                    skuId: a.id,
+                    transitionState: i
+                });
+            });
+        }, [null == a ? void 0 : a.applicationId, null == a ? void 0 : a.id, null == t ? void 0 : t.id, null == a ? void 0 : a.flags]),
+        d = i.useCallback(() => {
+            if (null == a) return null;
+            (0, o.openModal)((e) => {
+                let { onClose: n, transitionState: t } = e;
+                return (0, l.jsx)(p.ItemDetailsModal, {
+                    appId: a.applicationId,
+                    skuId: a.id,
+                    onClose: n,
+                    transitionState: t
+                });
+            });
+        }, [a]);
+    if (null != a && null != r && (null == a || a.type !== h.epS.SUBSCRIPTION || null != t)) {
+        if (null == e || (null != a && !a.available))
+            return {
+                disabled: !0,
+                label: I.Z.Messages.INTERACTION_PREMIUM_BUTTON_UNAVAILABLE
+            };
+        if (a.type === h.epS.SUBSCRIPTION) {
+            if (null == s || 0 === s.length)
+                return {
+                    disabled: !0,
+                    label: I.Z.Messages.INTERACTION_PREMIUM_BUTTON_UNAVAILABLE
+                };
+            let e = s[0],
+                n = (0, f.aS)(e.id);
+            return {
+                disabled: !1,
+                label: I.Z.Messages.INTERACTION_PREMIUM_BUTTON_CTA.format({
+                    skuName: a.name,
+                    price: (0, _.T4)(n.amount, n.currency)
+                }),
+                onClick: u
+            };
+        }
+        return null == a.price
+            ? {
+                  disabled: !0,
+                  label: I.Z.Messages.INTERACTION_PREMIUM_BUTTON_UNAVAILABLE
+              }
+            : {
+                  disabled: !1,
+                  label: I.Z.Messages.INTERACTION_PREMIUM_BUTTON_CTA.format({
+                      skuName: a.name,
+                      price: (0, _.T4)(a.price.amount, a.price.currency)
+                  }),
+                  onClick: d
+              };
+    }
+}
--- chunks/937615.js
+++ chunks/937615.js
@@ -1,44 +1,46 @@
 n.d(t, {
     T3: function () {
-        return u;
+        return c;
     },
     T4: function () {
-        return o;
-    },
-    og: function () {
         return l;
     },
+    og: function () {
+        return u;
+    },
     xg: function () {
-        return c;
+        return d;
     }
 }),
     n(411104);
 var r = n(221513),
     i = n(706454),
-    a = n(474936),
-    s = n(689938);
-function o(e, t, n) {
+    a = n(74538),
+    s = n(474936),
+    o = n(689938);
+function l(e, t, n) {
     var a;
     let s = null !== (a = null == n ? void 0 : n.localeOverride) && void 0 !== a ? a : i.default.locale;
     return (0, r.T4)(e, t, s, n);
 }
-function l(e, t, n) {
-    if (t === a.rV.YEAR) return s.Z.Messages.BILLING_PRICE_PER_YEAR.format({ price: e });
-    if (t === a.rV.MONTH && 1 === n) return s.Z.Messages.BILLING_PRICE_PER_MONTH.format({ price: e });
-    if (t === a.rV.MONTH && n > 1)
-        return s.Z.Messages.BILLING_PRICE_PER_MULTI_MONTHS.format({
+function u(e, t, n) {
+    if (t === s.rV.YEAR) return o.Z.Messages.BILLING_PRICE_PER_YEAR.format({ price: e });
+    if (t === s.rV.MONTH && 1 === n) return o.Z.Messages.BILLING_PRICE_PER_MONTH.format({ price: e });
+    if (t === s.rV.MONTH && n > 1)
+        return o.Z.Messages.BILLING_PRICE_PER_MULTI_MONTHS.format({
             price: e,
             intervalCount: n
         });
     throw Error('Unsupported interval type: '.concat(t, ', and interval count: ').concat(n));
 }
-function u(e, t) {
+function c(e, t) {
     return Intl.NumberFormat(e, {
         style: 'percent',
         minimumFractionDigits: 0
     }).format(t);
 }
-function c(e) {
-    let t = 'interval_count' in e ? e.interval_count : e.intervalCount;
-    return l(o(e.price, e.currency), e.interval, t);
+function d(e) {
+    let t = 'interval_count' in e ? e.interval_count : e.intervalCount,
+        n = (0, a.aS)(e.id);
+    return u(l(n.amount, n.currency), e.interval, t);
 }
