a5787bdb74852449f6988403f1d49e384e0348be
DiscrapperManager <admin@minerpl.xyz>
2024-09-26 01:47:23 +0000

    canaryRelease: 330657
    Main scripts: 3
    Lazy Loaded Scripts: 843
    Chunks:
     * Main Files: 12673
     * Scripts: 8088
    Strings: 19002
    Routes: 678
    Experiments Count: 366

--- chunks/784384.js
+++ chunks/784384.js
@@ -1,64 +1,70 @@
-n.d(t, {
-    H: function () {
+t.d(n, {
+    HY: function () {
         return g;
     },
-    y: function () {
+    pU: function () {
         return _;
+    },
+    y: function () {
+        return E;
     }
 }),
-    n(47120),
-    n(390547);
-var l = n(442837),
-    s = n(933557),
-    a = n(926491),
-    r = n(378233),
-    i = n(131704),
-    o = n(592125),
-    u = n(496675),
-    c = n(699516),
-    d = n(594174),
-    h = n(823379),
-    m = n(981631),
-    f = n(689938);
-function g(e, t, n) {
-    let l = n instanceof i.Sf;
-    if (t.isNSFW() && !(l && n.isNSFW())) return { label: f.Z.Messages.MESSAGE_FORWARDING_NSFW_NOT_ALLOWED };
-    if (l && (0, i.Km)(n.type)) {
-        if ((e.attachments.length > 0 || e.messageSnapshots.some((e) => e.message.attachments.length > 0)) && !u.Z.can(m.Plq.ATTACH_FILES, n)) return { label: f.Z.Messages.MESSAGE_CHANNEL_ATTACHMENTS_DISABLED };
-        if ((e.embeds.length > 0 || e.messageSnapshots.some((e) => e.message.embeds.length > 0)) && !u.Z.can(m.Plq.EMBED_LINKS, n)) return { label: f.Z.Messages.MESSAGE_CHANNEL_EMBEDS_DISABLED };
-        let t = [
+    t(47120),
+    t(390547);
+var l = t(442837),
+    s = t(933557),
+    a = t(926491),
+    r = t(378233),
+    i = t(131704),
+    o = t(592125),
+    u = t(496675),
+    c = t(699516),
+    d = t(594174),
+    h = t(823379),
+    f = t(981631),
+    m = t(689938);
+function g(e, n, t) {
+    let l = t instanceof i.Sf;
+    if (n.isNSFW() && !(l && t.isNSFW())) return { label: m.Z.Messages.MESSAGE_FORWARDING_NSFW_NOT_ALLOWED };
+    if (l && (0, i.Km)(t.type)) {
+        if ((e.attachments.length > 0 || e.messageSnapshots.some((e) => e.message.attachments.length > 0)) && !u.Z.can(f.Plq.ATTACH_FILES, t)) return { label: m.Z.Messages.MESSAGE_CHANNEL_ATTACHMENTS_DISABLED };
+        if ((e.embeds.length > 0 || e.messageSnapshots.some((e) => e.message.embeds.length > 0)) && !u.Z.can(f.Plq.EMBED_LINKS, t)) return { label: m.Z.Messages.MESSAGE_CHANNEL_EMBEDS_DISABLED };
+        let n = [
             ...(0, r.cv)(e),
             ...e.messageSnapshots.flatMap((e) => {
-                let { message: t } = e;
-                return (0, r.cv)(t);
+                let { message: n } = e;
+                return (0, r.cv)(n);
             })
         ];
         if (
-            t.length > 0 &&
-            !u.Z.can(m.Plq.USE_EXTERNAL_STICKERS, n) &&
-            t.some((e) =>
-                (function (e, t) {
-                    let n = a.Z.getStickerById(e.id);
-                    return !!(null != n && (0, r.J8)(n)) && (n.guild_id !== t.guild_id || void 0);
-                })(e, n)
+            n.length > 0 &&
+            !u.Z.can(f.Plq.USE_EXTERNAL_STICKERS, t) &&
+            n.some((e) =>
+                (function (e, n) {
+                    let t = a.Z.getStickerById(e.id);
+                    return !!(null != t && (0, r.J8)(t)) && (t.guild_id !== n.guild_id || void 0);
+                })(e, t)
             )
         )
-            return { label: f.Z.Messages.MESSAGE_CHANNEL_EXTERNAL_STICKERS_DISABLED };
-        if ((e.hasFlag(m.iLy.IS_VOICE_MESSAGE) || e.messageSnapshots.some((e) => e.message.hasFlag(m.iLy.IS_VOICE_MESSAGE))) && !u.Z.can(m.Plq.SEND_VOICE_MESSAGES, n)) return { label: f.Z.Messages.MESSAGE_CHANNEL_VOICE_MESSAGES_DISABLED };
+            return { label: m.Z.Messages.MESSAGE_CHANNEL_EXTERNAL_STICKERS_DISABLED };
+        if ((e.hasFlag(f.iLy.IS_VOICE_MESSAGE) || e.messageSnapshots.some((e) => e.message.hasFlag(f.iLy.IS_VOICE_MESSAGE))) && !u.Z.can(f.Plq.SEND_VOICE_MESSAGES, t)) return { label: m.Z.Messages.MESSAGE_CHANNEL_VOICE_MESSAGES_DISABLED };
     }
 }
-function _(e) {
-    let t = (0, l.Wu)(
+function _(e, n) {
+    return null != e.rateLimitPerUser && !!(e.rateLimitPerUser > 0) && !(n.can(f.Plq.MANAGE_CHANNELS, e) || n.can(f.Plq.MANAGE_MESSAGES, e));
+}
+function E(e) {
+    let n = (0, l.Wu)(
         [o.Z, u.Z],
         () =>
             e
                 .map((e) => {
-                    let { type: t, id: n } = e;
-                    return 'channel' === t ? o.Z.getChannel(n) : null;
+                    let { type: n, id: t } = e;
+                    return 'channel' === n ? o.Z.getChannel(t) : null;
                 })
                 .filter(h.lm)
-                .filter((e) => null != e.rateLimitPerUser && !!(e.rateLimitPerUser > 0) && !(u.Z.can(m.Plq.MANAGE_CHANNELS, e) || u.Z.can(m.Plq.MANAGE_MESSAGES, e))),
+                .filter((e) => _(e, u.Z)),
         [e]
     );
-    return (0, l.Wu)([d.default, c.Z], () => t.map((e) => (0, s.F6)(e, d.default, c.Z, !0)), [t]);
+    return (0, l.Wu)([d.default, c.Z], () => n.map((e) => (0, s.F6)(e, d.default, c.Z, !0)), [n]);
 }
--- chunks/859155.js
+++ chunks/859155.js
@@ -1,36 +1,38 @@
-n(411104);
-var l = n(991637),
-    s = n.n(l),
-    a = n(388123),
-    r = n(904245),
-    i = n(957730),
-    o = n(592125);
-let u = {
-    async sendForward(e, t, n) {
-        let l = o.Z.getChannel(t),
+t(411104);
+var l = t(991637),
+    s = t.n(l),
+    a = t(388123),
+    r = t(904245),
+    i = t(957730),
+    o = t(592125),
+    u = t(496675),
+    c = t(784384);
+let d = {
+    async sendForward(e, n, t) {
+        let l = o.Z.getChannel(n),
             s = o.Z.getChannel(e.channel_id);
         if (null == s) throw Error('Unable to find original channel for message');
         if (null == l) throw Error('Unable to find destination channel for message');
-        let u = i.ZP.parse(l, ''),
-            c = {
+        let d = i.ZP.parse(l, ''),
+            h = {
                 guild_id: s.guild_id,
                 channel_id: e.channel_id,
                 message_id: e.id,
                 type: a.U.FORWARD,
                 forward_only:
-                    (null == n ? void 0 : n.onlyAttachmentIds) != null || (null == n ? void 0 : n.onlyEmbedIndices) != null
+                    (null == t ? void 0 : t.onlyAttachmentIds) != null || (null == t ? void 0 : t.onlyEmbedIndices) != null
                         ? {
-                              attachment_ids: n.onlyAttachmentIds,
-                              embed_indices: n.onlyEmbedIndices
+                              attachment_ids: t.onlyAttachmentIds,
+                              embed_indices: t.onlyEmbedIndices
                           }
                         : void 0
             };
-        await r.Z.sendMessage(l.id, u, !1, {
-            messageReference: c,
+        await r.Z.sendMessage(l.id, d, !1, {
+            messageReference: h,
             eagerDispatch: !1
         }),
-            (null == n ? void 0 : n.withMessage) != null && (await r.Z.sendMessage(l.id, i.ZP.parse(l, n.withMessage), !1));
+            (null == t ? void 0 : t.withMessage) != null && !(0, c.pU)(l, u.Z) && (await r.Z.sendMessage(l.id, i.ZP.parse(l, t.withMessage), !1));
     },
-    sendForwards: (e, t, n) => s()(t.map((t) => u.sendForward(e, t, n)))
+    sendForwards: (e, n, t) => s()(n.map((n) => d.sendForward(e, n, t)))
 };
-t.Z = u;
+n.Z = d;
