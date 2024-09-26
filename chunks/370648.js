5fb1fa18a9cedce221d525feb3423e52c7b2973a
DiscrapperManager <admin@minerpl.xyz>
2024-09-26 00:04:14 +0000

    canaryRelease: 330614
    Main scripts: 3
    Lazy Loaded Scripts: 843
    Chunks:
     * Main Files: 12673
     * Scripts: 8088
    Strings: 19006
    Routes: 678
    Experiments Count: 366

--- chunks/370648.js
+++ chunks/370648.js
@@ -1,20 +1,22 @@
 var i = n(735250),
-    a = n(648113),
-    s = n(17845),
-    r = n(374939),
-    l = n(283293),
-    o = n(689938);
+    a = n(470079),
+    s = n(648113),
+    r = n(17845),
+    l = n(374939),
+    o = n(283293),
+    c = n(689938);
 t.Z = function () {
-    let e = (0, a.C)();
-    return (0, i.jsxs)(l.Z, {
+    let e = (0, s.C)(),
+        t = a.useMemo(() => [c.Z.Messages.GLOBAL_DISCOVERY_APPS_FEATURED_TITLE_PART_ONE, c.Z.Messages.GLOBAL_DISCOVERY_APPS_FEATURED_TITLE_PART_TWO, c.Z.Messages.GLOBAL_DISCOVERY_APPS_FEATURED_TITLE_PART_THREE], []);
+    return (0, i.jsxs)(o.Z, {
         onScroll: () => {},
         children: [
-            (0, i.jsx)(s.Z, {
-                title: o.Z.Messages.GLOBAL_DISCOVERY_APPS_FEATURED_TITLE.format({}),
+            (0, i.jsx)(r.Z, {
+                title: t,
                 description: '',
                 className: e
             }),
-            (0, i.jsx)(r.Z, { children: null })
+            (0, i.jsx)(l.Z, { children: null })
         ]
     });
 };
