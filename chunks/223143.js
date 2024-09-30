8d537032f87bd25b23027af11ac739645dc26e1e
DiscrapperManager <admin@minerpl.xyz>
2024-09-30 20:51:03 +0000

    canaryRelease: 331741
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 12704
     * Scripts: 8057
    Strings: 19036
    Routes: 679
    Experiments Count: 365

--- chunks/223143.js
+++ chunks/223143.js
@@ -1,69 +1,60 @@
 n.d(t, {
     Z: function () {
-        return _;
+        return u;
     },
     c: function () {
-        return d;
+        return l;
     }
 });
 var r = n(47120);
-var i = n(470079),
-    a = n(399606),
-    o = n(634894),
-    s = n(335131),
-    l = n(337679),
-    u = n(1870),
-    c = n(267097);
-function d() {
+var i = n(399606),
+    a = n(634894),
+    o = n(1870),
+    s = n(267097);
+function l() {
     let e = 'useFetchPurchases';
-    (0, o.j)({
+    (0, a.j)({
         location: e + ' auto on',
         autoTrackExposure: !0
     }),
-        (0, o.j)({
+        (0, a.j)({
             location: e + ' auto off',
             autoTrackExposure: !1
         });
-    let [t, n, r, c, d, _] = (0, a.Wu)([u.Z], () => [u.Z.isFetching, u.Z.isClaiming, u.Z.fetchError, u.Z.claimError, u.Z.purchases, u.Z.hasPreviouslyFetched]),
-        { shouldFakePurchaseSuccessFlowLocally: E } = (0, l.Z)({ location: 'useFetchPurchases' });
-    return (
-        (0, i.useEffect)(() => {
-            if (!E || !(d.size > 0)) (0, s.qg)();
-        }, [E]),
-        {
-            isClaiming: n,
-            fetchError: r,
-            claimError: c,
-            isFetching: t,
-            purchases: d,
-            hasPreviouslyFetched: _
-        }
-    );
+    let [t, n, r, s, l, u] = (0, i.Wu)([o.Z], () => [o.Z.isFetching, o.Z.isClaiming, o.Z.fetchError, o.Z.claimError, o.Z.purchases, o.Z.hasPreviouslyFetched]);
+    return {
+        isClaiming: n,
+        fetchError: r,
+        claimError: s,
+        isFetching: t,
+        purchases: l,
+        hasPreviouslyFetched: u
+    };
 }
-function _(e) {
+function u(e) {
     var t;
     let n = null == e ? void 0 : e.paymentGateway,
         r = 'useFetchCollectiblesCategoriesAndPurchases';
-    (0, o.j)({
+    (0, a.j)({
         location: r + ' auto on',
         autoTrackExposure: !0
     }),
-        (0, o.j)({
+        (0, a.j)({
             location: r + ' auto off',
             autoTrackExposure: !1
         });
-    let { isFetching: i, categories: a, error: s, refreshCategories: l } = (0, c.Z)({ paymentGateway: n }),
-        { isClaiming: u, fetchError: _, claimError: E, isFetching: f, purchases: h, hasPreviouslyFetched: p } = d(),
+    let { isFetching: i, categories: o, error: u, refreshCategories: c } = (0, s.Z)({ paymentGateway: n }),
+        { isClaiming: d, fetchError: _, claimError: E, isFetching: f, purchases: h, hasPreviouslyFetched: p } = l(),
         m = i || f;
     return {
         isFetching: m,
         isFetchingCategories: i,
         isFetchingPurchases: f,
-        isClaiming: u,
-        categories: a,
+        isClaiming: d,
+        categories: o,
         purchases: h,
-        error: null !== (t = null != s ? s : _) && void 0 !== t ? t : E,
-        refreshCategories: l,
+        error: null !== (t = null != u ? u : _) && void 0 !== t ? t : E,
+        refreshCategories: c,
         hasPreviouslyFetched: p
     };
 }
