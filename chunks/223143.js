07f42750676d8b6560369221aaf9c13987b0b6b1
DiscrapperManager <admin@minerpl.xyz>
2024-10-01 22:16:14 +0000

    canaryRelease: 332259
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 12717
     * Scripts: 8059
    Strings: 19049
    Routes: 681
    Experiments Count: 368

--- chunks/223143.js
+++ chunks/223143.js
@@ -23,15 +23,20 @@ function c() {
             location: e + ' auto off',
             autoTrackExposure: !1
         });
-    let [t, n, r, i, s, u] = (0, a.Wu)([l.Z], () => [l.Z.isFetching, l.Z.isClaiming, l.Z.fetchError, l.Z.claimError, l.Z.purchases, l.Z.hasPreviouslyFetched]);
-    return {
-        isClaiming: n,
-        fetchError: r,
-        claimError: i,
-        isFetching: t,
-        purchases: s,
-        hasPreviouslyFetched: u
-    };
+    let [t, n, r, u, c, d] = (0, a.Wu)([l.Z], () => [l.Z.isFetching, l.Z.isClaiming, l.Z.fetchError, l.Z.claimError, l.Z.purchases, l.Z.hasPreviouslyFetched]);
+    return (
+        (0, i.useEffect)(() => {
+            (0, s.qg)();
+        }, []),
+        {
+            isClaiming: n,
+            fetchError: r,
+            claimError: u,
+            isFetching: t,
+            purchases: c,
+            hasPreviouslyFetched: d
+        }
+    );
 }
 function d(e) {
     var t;
@@ -45,24 +50,18 @@ function d(e) {
             location: r + ' auto off',
             autoTrackExposure: !1
         });
-    let { isFetching: a, categories: l, error: d, refreshCategories: _ } = (0, u.Z)({ paymentGateway: n }),
-        { isClaiming: E, fetchError: f, claimError: h, isFetching: p, purchases: m, hasPreviouslyFetched: I } = c(),
-        T = a || p,
-        g = null !== (t = null != d ? d : f) && void 0 !== t ? t : h;
-    return (
-        (0, i.useEffect)(() => {
-            (0, s.qg)();
-        }, []),
-        {
-            isFetching: T,
-            isFetchingCategories: a,
-            isFetchingPurchases: p,
-            isClaiming: E,
-            categories: l,
-            purchases: m,
-            error: g,
-            refreshCategories: _,
-            hasPreviouslyFetched: I
-        }
-    );
+    let { isFetching: i, categories: a, error: s, refreshCategories: l } = (0, u.Z)({ paymentGateway: n }),
+        { isClaiming: d, fetchError: _, claimError: E, isFetching: f, purchases: h, hasPreviouslyFetched: p } = c(),
+        m = i || f;
+    return {
+        isFetching: m,
+        isFetchingCategories: i,
+        isFetchingPurchases: f,
+        isClaiming: d,
+        categories: a,
+        purchases: h,
+        error: null !== (t = null != s ? s : _) && void 0 !== t ? t : E,
+        refreshCategories: l,
+        hasPreviouslyFetched: p
+    };
 }
