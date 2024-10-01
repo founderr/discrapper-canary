a521fd1b2c44a54f73d25be4c4ba94fcb2ba2260
DiscrapperManager <admin@minerpl.xyz>
2024-10-01 17:03:48 +0000

    canaryRelease: 332006
    Main scripts: 3
    Lazy Loaded Scripts: 847
    Chunks:
     * Main Files: 12707
     * Scripts: 8058
    Strings: 19043
    Routes: 681
    Experiments Count: 367

--- chunks/291574.js
+++ chunks/291574.js
@@ -1,9 +1,11 @@
-var r = n(818083);
+var r = n(818083),
+    i = n(987338);
 t.Z = (0, r.B)({
     kind: 'user',
     id: '2024-09_app_dms',
     label: 'App DMs Experiment',
     defaultConfig: { enabled: !1 },
+    commonTriggerPoint: i.$P.CONNECTION_OPEN,
     treatments: [
         {
             id: 1,
