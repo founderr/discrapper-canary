f4974a58bbf7de037ca5654b8cd5a4e05524371c
DiscrapperManager <admin@minerpl.xyz>
2024-10-02 21:53:40 +0000

    canaryRelease: 332606
    Main scripts: 3
    Lazy Loaded Scripts: 848
    Chunks:
     * Main Files: 12726
     * Scripts: 8073
    Strings: 19057
    Routes: 681
    Experiments Count: 369

--- chunks/38516.js
+++ chunks/38516.js
@@ -1,17 +1,17 @@
-a(411104);
-var t = a(735250);
-a(470079);
-var i = a(120356),
-    o = a.n(i),
-    r = a(643872),
-    l = a(481060),
-    s = a(810568),
-    c = a(689938),
-    d = a(51527),
-    u = a(825718);
+n(411104);
+var t = n(735250);
+n(470079);
+var i = n(120356),
+    o = n.n(i),
+    r = n(643872),
+    l = n(481060),
+    s = n(810568),
+    c = n(689938),
+    d = n(51527),
+    u = n(825718);
 let m = [r.p.OFFICIAL, r.p.TWITTER, r.p.YOUTUBE];
 function p(e) {
-    let { website: n, trackClick: a } = e,
+    let { website: a, trackClick: n } = e,
         {
             action: i,
             icon: o,
@@ -39,52 +39,54 @@ function p(e) {
                 default:
                     throw Error('Unknown IGDB website category');
             }
-        })(n);
+        })(a);
     return (0, t.jsx)(
         l.Anchor,
         {
             className: u.linkAnchor,
             title: d,
-            href: n.url,
+            href: a.url,
             onClick: () => {
-                a(i);
+                n(i);
             },
             target: '_blank',
             children: o
         },
-        n.url
+        a.url
     );
 }
-n.Z = function (e) {
-    let { websites: n, trackClick: a } = e;
-    if (null == n || 0 === n.length) return null;
-    let i = n
+a.Z = function (e) {
+    let { detectedGame: a, trackClick: n } = e;
+    if (null == a.websites) return null;
+    let i = a.websites
         .filter((e) => {
-            let { category: n } = e;
-            return m.includes(n);
+            let { category: a } = e;
+            return m.includes(a);
         })
-        .sort((e, n) => e.category - n.category);
-    return (0, t.jsxs)('div', {
-        children: [
-            (0, t.jsx)(l.Heading, {
-                className: d.sectionHeader,
-                variant: 'text-xs/semibold',
-                color: 'header-secondary',
-                children: c.Z.Messages.GAME_PROFILE_LINKS
-            }),
-            (0, t.jsx)('div', {
-                className: o()(d.row, d.gapMd),
-                children: i.map((e) =>
-                    (0, t.jsx)(
-                        p,
-                        {
-                            website: e,
-                            trackClick: a
-                        },
-                        e.url
-                    )
-                )
-            })
-        ]
-    });
+        .sort((e, a) => e.category - a.category);
+    return 0 === i.length
+        ? null
+        : (0, t.jsxs)('div', {
+              children: [
+                  (0, t.jsx)(l.Heading, {
+                      className: d.sectionHeader,
+                      variant: 'text-xs/semibold',
+                      color: 'header-secondary',
+                      children: c.Z.Messages.GAME_PROFILE_LINKS
+                  }),
+                  (0, t.jsx)('div', {
+                      className: o()(d.row, d.gapMd),
+                      children: i.map((e) =>
+                          (0, t.jsx)(
+                              p,
+                              {
+                                  website: e,
+                                  trackClick: n
+                              },
+                              e.url
+                          )
+                      )
+                  })
+              ]
+          });
 };
