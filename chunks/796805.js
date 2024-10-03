31a75c2ba83f6ccbd1a9bc99d1157dee6564b2e3
DiscrapperManager <admin@minerpl.xyz>
2024-10-03 00:35:13 +0000

    canaryRelease: 332714
    Main scripts: 3
    Lazy Loaded Scripts: 849
    Chunks:
     * Main Files: 12729
     * Scripts: 8072
    Strings: 19057
    Routes: 681
    Experiments Count: 370

--- chunks/796805.js
+++ chunks/796805.js
@@ -1,5 +1,6 @@
 e.exports = {
+    similarGameImageClickable: 'similarGameImageClickable_bd82e2 similarGameImageWrapper_bd82e2',
     similarGameImage: 'similarGameImage_bd82e2',
-    loadingArtwork: 'loadingArtwork_bd82e2',
+    loadingArtwork: 'loadingArtwork_bd82e2 similarGameImage_bd82e2 similarGameImageWrapper_bd82e2',
     loadingHeading: 'loadingHeading_bd82e2'
 };
