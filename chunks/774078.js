233f699daaec455a746c52142b619b0c318fcdbf
DiscrapperManager <admin@minerpl.xyz>
2024-10-03 18:37:18 +0000

    canaryRelease: 332858
    Main scripts: 3
    Lazy Loaded Scripts: 849
    Chunks:
     * Main Files: 12729
     * Scripts: 8074
    Strings: 19078
    Routes: 681
    Experiments Count: 371

--- chunks/774078.js
+++ chunks/774078.js
@@ -1,24 +1,20 @@
 n.d(t, {
     Z: function () {
-        return o;
+        return s;
     }
 });
-var r = n(718922),
-    i = n(55935),
-    a = n(745735);
-function o(e) {
+var r = n(470079),
+    i = n(718922),
+    a = n(55935),
+    o = n(745735);
+function s(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : 1000,
         n = arguments.length > 2 ? arguments[2] : void 0,
-        o = arguments.length > 3 && void 0 !== arguments[3] && arguments[3],
-        s = (0, i.TD)(Date.now(), e),
-        l = (0, r.Z)();
-    return (
-        (0, a.Z)(
-            () => {
-                if ((0 !== s.days || 0 !== s.hours || 0 !== s.minutes || 0 !== s.seconds) && !o) l(), null == n || n();
-            },
-            o ? null : t
-        ),
-        s
-    );
+        s = arguments.length > 3 && void 0 !== arguments[3] && arguments[3],
+        l = (0, a.TD)(Date.now(), e),
+        u = (0, i.Z)(),
+        c = (0, r.useCallback)(() => {
+            if ((0 !== l.days || 0 !== l.hours || 0 !== l.minutes || 0 !== l.seconds) && !s) u(), null == n || n();
+        }, [l, s, u, n]);
+    return (0, o.Z)(c, s ? null : t), l;
 }
