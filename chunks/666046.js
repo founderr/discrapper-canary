8abea468fa2192b8ba9a171aca8eff1621631890
DiscrapperManager <admin@minerpl.xyz>
2024-10-04 22:23:42 +0000

    canaryRelease: 333342
    Main scripts: 3
    Lazy Loaded Scripts: 849
    Chunks:
     * Main Files: 12734
     * Scripts: 8037
    Strings: 19107
    Routes: 681
    Experiments Count: 370

---
+++ chunks/666046.js
@@ -0,0 +1,15 @@
+e.exports = {
+    content: 'content_d9bd92',
+    header: 'header_d9bd92',
+    tip: 'tip_d9bd92',
+    footer: 'footer_d9bd92',
+    radioGroup: 'radioGroup_d9bd92',
+    applicationSearch: 'applicationSearch_d9bd92',
+    gameSuggestion: 'gameSuggestion_d9bd92',
+    otherFeedbackInput: 'otherFeedbackInput_d9bd92',
+    searchBar: 'searchBar_d9bd92',
+    gameInfoBox: 'gameInfoBox_d9bd92',
+    gameIcon: 'gameIcon_d9bd92',
+    smolGameIcon: 'smolGameIcon_d9bd92',
+    gameTitle: 'gameTitle_d9bd92'
+};
