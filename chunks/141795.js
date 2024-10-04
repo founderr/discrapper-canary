7fa56104c449f2e64a8e6751aca7b08cf78ee4e0
DiscrapperManager <admin@minerpl.xyz>
2024-10-04 17:19:38 +0000

    canaryRelease: 333177
    Main scripts: 3
    Lazy Loaded Scripts: 849
    Chunks:
     * Main Files: 12732
     * Scripts: 8038
    Strings: 19105
    Routes: 681
    Experiments Count: 371

--- chunks/141795.js
+++ chunks/141795.js
@@ -64,26 +64,29 @@ class y extends S.ZP {
         );
     }
     retryOpts() {
-        if (this.item.platform === S.ow.REACT_NATIVE)
-            return E.l.getCurrentConfig({ location: 'CloudUpload' }).enabled
-                ? {
-                      timeout: {
-                          response: 10 * I.Z.Millis.SECOND,
-                          deadline: 30 * I.Z.Millis.MINUTE
-                      },
-                      backoff: new l.Z(0.5 * I.Z.Millis.SECOND, 60 * I.Z.Millis.SECOND),
-                      retries: 8
-                  }
-                : {
-                      timeout: 1 * I.Z.Millis.HOUR,
-                      backoff: new l.Z(0.5 * I.Z.Millis.SECOND, 30 * I.Z.Millis.MINUTE),
-                      retries: 12
-                  };
-        return {
-            timeout: 1 * I.Z.Millis.HOUR,
-            retries: 12,
-            backoff: new l.Z()
-        };
+        return this.item.platform === S.ow.REACT_NATIVE
+            ? {
+                  timeout: 1 * I.Z.Millis.HOUR,
+                  backoff: new l.Z(0.5 * I.Z.Millis.SECOND, 30 * I.Z.Millis.MINUTE),
+                  retries: 12
+              }
+            : {
+                  timeout: 1 * I.Z.Millis.HOUR,
+                  retries: 12,
+                  backoff: new l.Z()
+              };
+    }
+    createAttachmentUrlRetryOpts() {
+        return this.item.platform === S.ow.REACT_NATIVE && E.l.getCurrentConfig({ location: 'CloudUpload' }).enabled
+            ? {
+                  timeout: {
+                      response: 30 * I.Z.Millis.SECOND,
+                      deadline: 30 * I.Z.Millis.MINUTE
+                  },
+                  backoff: new l.Z(0.5 * I.Z.Millis.SECOND, 60 * I.Z.Millis.SECOND),
+                  retries: 8
+              }
+            : this.retryOpts();
     }
     async uploadFileToCloudAsChunks(e) {
         if (null == this.responseUrl || '' === this.responseUrl) throw Error('_uploadFileToCloudAsChunks - responseUrl is not set');
@@ -270,7 +273,7 @@ class y extends S.ZP {
                     await u.tn.post({
                         url: i.getCreateAttachmentURL(this.channelId),
                         body: { files: [r] },
-                        ...this.retryOpts()
+                        ...this.createAttachmentUrlRetryOpts()
                     })
             );
             this.setResponseUrl(e.body.attachments[0].upload_url), this.setUploadedFilename(e.body.attachments[0].upload_filename);
