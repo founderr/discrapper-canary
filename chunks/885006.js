729fecb15664123c72a93c306159e2d1a277a42b
DiscrapperManager <admin@minerpl.xyz>
2024-10-05 00:51:12 +0000

    canaryRelease: 333424
    Main scripts: 3
    Lazy Loaded Scripts: 891
    Chunks:
     * Main Files: 11140
     * Scripts: 9631
    Strings: 19107
    Routes: 681
    Experiments Count: 370

--- chunks/885006.js
+++ chunks/885006.js
@@ -1,27 +1,27 @@
 t.d(n, {
-    b: function () {
+    Z: function () {
         return i;
     }
 }),
     t(47120);
 var a = t(470079);
-function i() {
-    var e, n;
-    let [t, i] = a.useState({
-        width: null !== (e = window.innerWidth) && void 0 !== e ? e : 1080,
-        height: null !== (n = window.innerHeight) && void 0 !== n ? n : 1080
+function i(e) {
+    var n, t, i;
+    let [o, r] = a.useState({
+        width: null !== (t = window.innerWidth) && void 0 !== t ? t : 1080,
+        height: null !== (i = window.innerHeight) && void 0 !== i ? i : 1080
     });
     return (
         a.useLayoutEffect(() => {
             function e() {
                 let e = window.innerWidth;
-                i({
+                r({
                     width: e,
                     height: window.innerHeight
                 });
             }
             return e(), window.addEventListener('resize', e), () => window.removeEventListener('resize', e);
         }, []),
-        t
+        o
     );
 }
