f5c6a68b39648b81275e66eac446c55c6a8db24d
DiscrapperManager <admin@minerpl.xyz>
2024-10-07 22:08:31 +0000

    canaryRelease: 333675
    Main scripts: 3
    Lazy Loaded Scripts: 890
    Chunks:
     * Main Files: 11186
     * Scripts: 10013
    Strings: 19110
    Routes: 681
    Experiments Count: 372

--- chunks/973007.js
+++ chunks/973007.js
@@ -1,55 +1,61 @@
 var t = n(735250);
 n(470079);
 var r = n(481060),
-    i = n(823188),
-    l = n(474936),
-    a = n(377628);
-function o(e) {
-    let { onClick: s, isGift: n, priceOptions: l, showHalloweenGift: o } = e;
+    i = n(184508),
+    l = n(823188),
+    a = n(474936),
+    o = n(377628);
+function C(e) {
+    let { onClick: s, isGift: n, priceOptions: i, showHalloweenGift: a, shouldUseTaglineXPCopy: C } = e;
     return (0, t.jsx)(r.Clickable, {
         onClick: s,
         style: { cursor: 'pointer' },
-        children: (0, t.jsx)(i.wp, {
-            className: a.tier2MarketingCard,
+        children: (0, t.jsx)(l.wp, {
+            className: o.tier2MarketingCard,
             ctaButton: null,
-            showWumpus: !o,
-            showHalloweenGift: o,
+            showWumpus: !a && !C,
+            showHalloweenGift: a,
             showYearlyPrice: !0,
             isGift: n,
             isModal: !0,
-            priceOptions: l
+            priceOptions: i,
+            shouldUseTaglineXPCopy: C
         })
     });
 }
-function C(e) {
-    let { onClick: s, isGift: n, priceOptions: l } = e;
+function d(e) {
+    let { onClick: s, isGift: n, priceOptions: i, shouldUseTaglineXPCopy: a } = e;
     return (0, t.jsx)(r.Clickable, {
         onClick: s,
         style: { cursor: 'pointer' },
-        children: (0, t.jsx)(i.Gq, {
-            className: a.tier0MarketingCard,
+        children: (0, t.jsx)(l.Gq, {
+            className: o.tier0MarketingCard,
             ctaButton: null,
-            showWumpus: !0,
+            showWumpus: !a,
             showYearlyPrice: !0,
             isGift: n,
-            priceOptions: l
+            priceOptions: i,
+            shouldUseTaglineXPCopy: a
         })
     });
 }
 s.Z = function (e) {
-    let { onSelectSku: s, isGift: n, priceOptions: r, shouldShowHalloweenTier2Card: i } = e;
+    let { onSelectSku: s, isGift: n, priceOptions: r, shouldShowHalloweenTier2Card: l } = e,
+        o = (0, i.tY)('PremiumMarketingHome');
     return (0, t.jsxs)(t.Fragment, {
         children: [
-            (0, t.jsx)(o, {
-                onClick: () => s(l.Si.TIER_2),
+            (0, t.jsx)(C, {
+                onClick: () => s(a.Si.TIER_2),
                 isGift: n,
                 priceOptions: r,
-                showHalloweenGift: i
+                showHalloweenGift: l,
+                shouldUseTaglineXPCopy: o
             }),
-            (0, t.jsx)(C, {
-                onClick: () => s(l.Si.TIER_0),
+            (0, t.jsx)(d, {
+                onClick: () => s(a.Si.TIER_0),
                 isGift: n,
-                priceOptions: r
+                priceOptions: r,
+                shouldUseTaglineXPCopy: o
             })
         ]
     });
