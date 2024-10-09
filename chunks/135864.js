a502fd59f5d8d81da90a82097a08c7f880d46530
DiscrapperManager <admin@minerpl.xyz>
2024-10-09 18:43:58 +0000

    canaryRelease: 334288
    Main scripts: 3
    Lazy Loaded Scripts: 890
    Chunks:
     * Main Files: 11196
     * Scripts: 10044
    Strings: 19113
    Routes: 681
    Experiments Count: 371

--- chunks/135864.js
+++ chunks/135864.js
@@ -11,45 +11,46 @@ var i = n(735250),
     E = n(703656),
     h = n(836768),
     m = n(766219),
-    I = n(49898),
-    p = n(981631),
-    g = n(46140),
-    T = n(689938),
-    f = n(696165);
-function S(e) {
+    I = n(906899),
+    p = n(49898),
+    g = n(981631),
+    T = n(46140),
+    f = n(689938),
+    S = n(696165);
+function C(e) {
     let { tab: t } = e,
         n = (0, h.d)((e) => e.selectedTab === t, l.Z),
         s = a.useMemo(
             () =>
                 (function (e) {
                     switch (e) {
-                        case I.F$.SERVERS:
+                        case p.F$.SERVERS:
                             return (0, i.jsx)(d.ServerIcon, { color: 'currentColor' });
-                        case I.F$.APPS:
+                        case p.F$.APPS:
                             return (0, i.jsx)(d.AppsIcon, { color: 'currentColor' });
-                        case I.F$.QUESTS:
+                        case p.F$.QUESTS:
                             return (0, i.jsx)(d.QuestsIcon, { color: 'currentColor' });
                     }
                 })(t),
             [t]
         ),
         u = a.useMemo(() => (0, m.sm)(t), [t]),
-        T = a.useCallback(() => {
+        f = a.useCallback(() => {
             switch (((0, c.j)(() => h.d.setState({ selectedTab: t })), t)) {
-                case I.F$.QUESTS:
-                    return (0, _.navigateToQuestHome)(g.dr.DISCOVERY_SIDEBAR, o.j.DISCOVERY_SIDEBAR);
-                case I.F$.APPS:
-                    return (0, E.uL)(p.Z5c.GLOBAL_DISCOVERY_APPS);
-                case I.F$.SERVERS:
-                    return (0, E.uL)(p.Z5c.GLOBAL_DISCOVERY_SERVERS);
+                case p.F$.QUESTS:
+                    return (0, _.navigateToQuestHome)(T.dr.DISCOVERY_SIDEBAR, o.j.DISCOVERY_SIDEBAR);
+                case p.F$.APPS:
+                    return (0, E.uL)(g.Z5c.GLOBAL_DISCOVERY_APPS);
+                case p.F$.SERVERS:
+                    return (0, I.R)();
             }
         }, [t]);
     return (0, i.jsxs)(d.Clickable, {
-        onClick: T,
-        className: r()(f.navItem, { [f.selected]: n }),
+        onClick: f,
+        className: r()(S.navItem, { [S.selected]: n }),
         children: [
             (0, i.jsx)('div', {
-                className: f.navItemIcon,
+                className: S.navItemIcon,
                 children: s
             }),
             (0, i.jsx)(d.Text, {
@@ -63,18 +64,18 @@ function S(e) {
 t.Z = function () {
     let e = (0, u.h)({ location: 'GlobalDiscoverySidebar' });
     return (0, i.jsxs)('div', {
-        className: f.container,
+        className: S.container,
         children: [
             (0, i.jsx)('div', {
-                className: f.header,
+                className: S.header,
                 children: (0, i.jsx)(d.Heading, {
                     variant: 'text-lg/semibold',
-                    children: T.Z.Messages.GLOBAL_DISCOVERY_TITLE
+                    children: f.Z.Messages.GLOBAL_DISCOVERY_TITLE
                 })
             }),
             (0, i.jsx)('nav', {
-                className: f.nav,
-                children: I.mc.map((t) => (t !== I.F$.APPS || e ? (0, i.jsx)(S, { tab: t }, t) : null))
+                className: S.nav,
+                children: p.mc.map((t) => (t !== p.F$.APPS || e ? (0, i.jsx)(C, { tab: t }, t) : null))
             })
         ]
     });
--- chunks/602147.js
+++ chunks/602147.js
@@ -1,6 +1,6 @@
 n.d(t, {
     o: function () {
-        return S;
+        return x;
     }
 }),
     n(47120);
@@ -13,45 +13,46 @@ var i = n(735250),
     c = n(243778),
     u = n(571457),
     d = n(836768),
-    h = n(667105),
-    p = n(341907),
-    f = n(703656),
+    h = n(906899),
+    p = n(667105),
+    f = n(341907),
+    m = n(703656),
     _ = n(603592),
-    m = n(981631),
-    g = n(921944),
-    C = n(49898),
-    I = n(46140),
-    E = n(689938),
-    N = n(55869);
-function S(e) {
+    g = n(981631),
+    C = n(921944),
+    I = n(49898),
+    E = n(46140),
+    N = n(689938),
+    S = n(55869);
+function x(e) {
     switch (e) {
-        case C.F$.QUESTS:
-            return (0, p.navigateToQuestHome)(I.dr.DISCOVERY_COMPASS, r.j.DISCOVERY_COMPASS);
-        case C.F$.APPS:
-            return (0, f.uL)(m.Z5c.GLOBAL_DISCOVERY_APPS);
-        case C.F$.SERVERS:
-            return (0, f.uL)(m.Z5c.GLOBAL_DISCOVERY_SERVERS);
+        case I.F$.QUESTS:
+            return (0, f.navigateToQuestHome)(E.dr.DISCOVERY_COMPASS, r.j.DISCOVERY_COMPASS);
+        case I.F$.APPS:
+            return (0, m.uL)(g.Z5c.GLOBAL_DISCOVERY_APPS);
+        case I.F$.SERVERS:
+            return (0, h.R)();
         default:
-            return (0, f.uL)(m.Z5c.GLOBAL_DISCOVERY);
+            return (0, m.uL)(g.Z5c.GLOBAL_DISCOVERY);
     }
 }
-let x = l.forwardRef(function (e, t) {
-    let { selected: n, className: r, onClick: p } = e,
+let v = l.forwardRef(function (e, t) {
+    let { selected: n, className: r, onClick: h } = e,
         f = d.d.getState().selectedTab,
         m = (0, o.Z)('discovery_button'),
-        x = (0, h.Ws)({ location: I.dr.CONFLICT_CHECKS }),
-        [v, T] = (0, c.US)([a.z.RAPIDASH_MORE_GUILDS_UPSELL], g.R.SIDEBAR, !0),
-        Z = v === a.z.RAPIDASH_MORE_GUILDS_UPSELL && m && x,
-        b = l.useCallback(() => {
-            null == p || p(), (0, u.k5)({ source: C.JU.GUILDS_BAR_ICON }), S(f), Z && T(g.L.DISMISS);
-        }, [p, f, T, Z]),
-        A = l.useMemo(
+        g = (0, p.Ws)({ location: E.dr.CONFLICT_CHECKS }),
+        [v, T] = (0, c.US)([a.z.RAPIDASH_MORE_GUILDS_UPSELL], C.R.SIDEBAR, !0),
+        Z = v === a.z.RAPIDASH_MORE_GUILDS_UPSELL && m && g,
+        A = l.useCallback(() => {
+            null == h || h(), (0, u.k5)({ source: I.JU.GUILDS_BAR_ICON }), x(f), Z && T(C.L.DISMISS);
+        }, [h, f, T, Z]),
+        b = l.useMemo(
             () => ({
                 lowerBadgeSize: {
                     width: 12,
                     height: 12
                 },
-                lowerBadge: Z ? (0, i.jsx)('div', { className: N.guildNotif }) : void 0
+                lowerBadge: Z ? (0, i.jsx)('div', { className: S.guildNotif }) : void 0
             }),
             [Z]
         );
@@ -59,11 +60,11 @@ let x = l.forwardRef(function (e, t) {
         id: 'guild-discover-button',
         ref: t,
         className: r,
-        onClick: b,
+        onClick: A,
         selected: n,
-        tooltip: E.Z.Messages.DISCOVER,
+        tooltip: N.Z.Messages.DISCOVER,
         icon: s.CompassIcon,
-        ...A
+        ...b
     });
 });
-t.Z = x;
+t.Z = v;
