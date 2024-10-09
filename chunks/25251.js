e363f3a6b8e9c3d1a791280634715c132f651d18
DiscrapperManager <admin@minerpl.xyz>
2024-10-09 21:16:33 +0000

    canaryRelease: 334375
    Main scripts: 3
    Lazy Loaded Scripts: 891
    Chunks:
     * Main Files: 11197
     * Scripts: 10049
    Strings: 19115
    Routes: 681
    Experiments Count: 372

--- chunks/25251.js
+++ chunks/25251.js
@@ -5,42 +5,40 @@ var a,
     l,
     u = n(392711),
     c = n(442837),
-    d = n(570140),
-    _ = n(358085),
-    E = n(729436);
-let f = [],
-    h = [],
-    p = !1;
-let I = f,
-    m = {},
-    T = null;
-let S = 0,
-    g = (e) => {
-        I = (0, u.cloneDeep)(e);
+    d = n(570140);
+let _ = [],
+    E = [],
+    f = !1;
+let h = _,
+    p = {},
+    I = null;
+let m = 0,
+    T = (e) => {
+        h = (0, u.cloneDeep)(e);
         let t = {};
-        I.forEach((e) => {
-            if (((t[e.id] = (0, _.isAndroid)() ? (0, E.QE)(e) : e), h.length > 0)) {
+        h.forEach((e) => {
+            if (((t[e.id] = e), E.length > 0)) {
                 var n;
-                t[e.id].config.effects = null !== (n = (0, u.sample)(h)) && void 0 !== n ? n : [];
+                t[e.id].config.effects = null !== (n = (0, u.sample)(E)) && void 0 !== n ? n : [];
             }
         }),
-            (m = t);
+            (p = t);
     },
-    A = () => {
-        g(f), (T = null), (i = void 0), (p = !1);
+    S = () => {
+        T(_), (I = null), (i = void 0), (f = !1);
     };
-class N extends (l = c.ZP.Store) {
+class g extends (l = c.ZP.Store) {
     get isFetching() {
-        return p;
+        return f;
     }
     get fetchError() {
         return r;
     }
     get profileEffects() {
-        return I;
+        return h;
     }
     get tryItOutId() {
-        return T;
+        return I;
     }
     canFetch() {
         return null == i || Date.now() >= i;
@@ -49,11 +47,11 @@ class N extends (l = c.ZP.Store) {
         return null != i && null == r;
     }
     getProfileEffectById(e) {
-        return null != e ? m[e] : void 0;
+        return null != e ? p[e] : void 0;
     }
 }
 (o = 'ProfileEffectStore'),
-    (s = 'displayName') in (a = N)
+    (s = 'displayName') in (a = g)
         ? Object.defineProperty(a, s, {
               value: o,
               enumerable: !0,
@@ -61,23 +59,23 @@ class N extends (l = c.ZP.Store) {
               writable: !0
           })
         : (a[s] = o),
-    (t.Z = new N(d.Z, {
+    (t.Z = new g(d.Z, {
         USER_PROFILE_EFFECTS_FETCH: () => {
-            p = !0;
+            f = !0;
         },
         USER_PROFILE_EFFECTS_FETCH_SUCCESS: (e) => {
             let { profileEffects: t } = e;
-            (r = void 0), (i = Date.now() + 300000), (S = 0), g(0 === t.length ? f : t), (p = !1);
+            (r = void 0), (i = Date.now() + 300000), (m = 0), T(0 === t.length ? _ : t), (f = !1);
         },
         USER_PROFILE_EFFECTS_FETCH_FAILURE: (e) => {
             let { error: t } = e;
-            (r = t), (i = Date.now() + Math.min(60000 * 2 ** S, 3600000)), ++S, g(f), (p = !1);
+            (r = t), (i = Date.now() + Math.min(60000 * 2 ** m, 3600000)), ++m, T(_), (f = !1);
         },
         PROFILE_EFFECTS_SET_TRY_IT_OUT: (e) => {
             let { id: t } = e;
-            T = t;
+            I = t;
         },
         LOGOUT: (e) => {
-            A();
+            S();
         }
     }));
--- chunks/729436.js
+++ chunks/729436.js
@@ -1,12 +1,9 @@
 n.d(t, {
-    Cc: function () {
-        return l;
+    C: function () {
+        return o;
     },
-    Hd: function () {
+    H: function () {
         return a;
-    },
-    QE: function () {
-        return s;
     }
 }),
     n(724458);
@@ -17,19 +14,12 @@ let a = (e) =>
             var n, r;
             return (null !== (n = e.zIndex) && void 0 !== n ? n : 0) - (null !== (r = t.zIndex) && void 0 !== r ? r : 0);
         }),
-    s = (e) => {
-        if ('1228233381897179137' === e.id || '1228234629429985300' === e.id) {
-            var t;
-            (null === (t = e.config.effects) || void 0 === t ? void 0 : t.length) >= 2 && (e.config.effects[1].start = 2832);
-        }
-        return e;
-    },
-    o = (e, t) => Math.floor(Math.random() * (t - e + 1) + e),
-    l = (e) =>
+    s = (e, t) => Math.floor(Math.random() * (t - e + 1) + e),
+    o = (e) =>
         r.useMemo(() => {
             if (null == e) return e;
             let t = (0, i.cloneDeep)(e),
-                n = o(
+                n = s(
                     0,
                     t.effects.reduce((e, t) => {
                         var n;
