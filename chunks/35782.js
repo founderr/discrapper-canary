b1e7e7d6b5450375c5d7fcb6e5cc212871afed58
DiscrapperManager <admin@minerpl.xyz>
2024-10-10 17:48:00 +0000

    canaryRelease: 334619
    Main scripts: 3
    Lazy Loaded Scripts: 891
    Chunks:
     * Main Files: 11198
     * Scripts: 10052
    Strings: 19115
    Routes: 681
    Experiments Count: 372

--- chunks/35782.js
+++ chunks/35782.js
@@ -6,11 +6,191 @@ n.d(t, {
 let { Themes: r } = n(15202).V,
     i = {
         SemanticColorExperiments: {
+            BACKGROUND_MESSAGE_HOVER: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_66',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_3',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_88',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_81',
+                        opacity: 1
+                    }
+                }
+            },
+            BACKGROUND_PRIMARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_64',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_1',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_100',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_76',
+                        opacity: 1
+                    }
+                }
+            },
+            BACKGROUND_SECONDARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_67',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_4',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_100',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_76',
+                        opacity: 1
+                    }
+                }
+            },
+            BACKGROUND_TERTIARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_72',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_5',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_100',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_83',
+                        opacity: 1
+                    }
+                }
+            },
+            BG_BASE_SECONDARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_67',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_3',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_86',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_83',
+                        opacity: 1
+                    }
+                }
+            },
+            BG_BASE_TERTIARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_68',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_4',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_96',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_78',
+                        opacity: 1
+                    }
+                }
+            },
             BG_MOD_STRONG: {
                 'visual-refresh': {
                     [r.LIGHT]: {
                         raw: 'PRIMARY_400',
                         opacity: 0.24
+                    }
+                }
+            },
+            CHANNEL_ICON: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_29',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_40',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_36',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_33',
+                        opacity: 1
+                    }
+                }
+            },
+            CHANNELS_DEFAULT: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_29',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_40',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_36',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_33',
+                        opacity: 1
+                    }
+                }
+            },
+            HEADER_SECONDARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_29',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_40',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_36',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_33',
+                        opacity: 1
                     }
                 }
             },
@@ -35,6 +215,126 @@ let { Themes: r } = n(15202).V,
                     [r.LIGHT]: {
                         raw: 'BLACK',
                         opacity: 0.04
+                    }
+                }
+            },
+            TEXT_LINK: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'BLUE_NEW_39',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'BLUE_NEW_53',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'BLUE_NEW_50',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'BLUE_NEW_46',
+                        opacity: 1
+                    }
+                }
+            },
+            TEXT_MUTED: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_21',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_46',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_33',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_27',
+                        opacity: 1
+                    }
+                }
+            },
+            TEXT_NORMAL: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_5',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_66',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_17',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_11',
+                        opacity: 1
+                    }
+                }
+            },
+            TEXT_PRIMARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_5',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_66',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_17',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_11',
+                        opacity: 1
+                    }
+                }
+            },
+            TEXT_SECONDARY: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'NEUTRAL_24',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'NEUTRAL_40',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'NEUTRAL_36',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'NEUTRAL_30',
+                        opacity: 1
+                    }
+                }
+            },
+            TEXT_WARNING: {
+                'visual-refresh': {
+                    [r.DARK]: {
+                        raw: 'YELLOW_NEW_37',
+                        opacity: 1
+                    },
+                    [r.LIGHT]: {
+                        raw: 'YELLOW_NEW_52',
+                        opacity: 1
+                    },
+                    [r.MIDNIGHT]: {
+                        raw: 'YELLOW_NEW_48',
+                        opacity: 1
+                    },
+                    [r.DARKER]: {
+                        raw: 'YELLOW_NEW_43',
+                        opacity: 1
                     }
                 }
             }
