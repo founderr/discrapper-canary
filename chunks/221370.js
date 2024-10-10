8d1d4407bcf92f6298a05f03233cf3ceddc33534
DiscrapperManager <admin@minerpl.xyz>
2024-10-10 19:54:31 +0000

    canaryRelease: 334701
    Main scripts: 3
    Lazy Loaded Scripts: 891
    Chunks:
     * Main Files: 11211
     * Scripts: 10046
    Strings: 19130
    Routes: 681
    Experiments Count: 374

--- chunks/221370.js
+++ chunks/221370.js
@@ -1,52 +1,92 @@
-a.d(n, {
+t.d(n, {
     Z: function () {
-        return E;
+        return I;
     }
 }),
-    a(47120),
-    a(653041);
-var t = a(735250),
-    r = a(470079),
-    s = a(120356),
-    i = a.n(s),
-    o = a(758713),
-    l = a(657707),
-    c = a(51527);
-let d = [o.z.DESKTOP, o.z.XBOX, o.z.PLAYSTATION, o.z.NINTENDO];
-function u(e) {
-    let { platform: n } = e;
+    t(47120),
+    t(653041);
+var a = t(735250),
+    r = t(470079),
+    s = t(120356),
+    i = t.n(s),
+    o = t(758713),
+    l = t(657707),
+    c = t(481060),
+    d = t(689938),
+    u = t(51527);
+let _ = [o.z.DESKTOP, o.z.XBOX, o.z.PLAYSTATION, o.z.NINTENDO];
+function E(e) {
+    let { platform: n, ...t } = e;
     switch (n) {
         case o.z.DESKTOP:
-            return (0, t.jsx)(l.pzj, { size: 'xs' });
+            return (0, a.jsx)(l.pzj, {
+                size: 'xs',
+                ...t
+            });
         case o.z.XBOX:
-            return (0, t.jsx)(l.Mko, { size: 'xs' });
+            return (0, a.jsx)(l.Mko, {
+                size: 'xs',
+                ...t
+            });
         case o.z.PLAYSTATION:
-            return (0, t.jsx)(l.Tsp, { size: 'xs' });
+            return (0, a.jsx)(l.Tsp, {
+                size: 'xs',
+                ...t
+            });
         case o.z.NINTENDO:
-            return (0, t.jsx)(l.aPH, { size: 'xs' });
+            return (0, a.jsx)(l.aPH, {
+                size: 'xs',
+                ...t
+            });
         default:
             return null;
     }
 }
-function _(e) {
+function m(e) {
     let { platforms: n } = e;
-    return (0, t.jsx)('div', {
-        className: i()(c.row, c.gapSm),
+    return (0, a.jsx)('div', {
+        className: i()(u.row, u.gapSm),
         style: { alignItems: 'center' },
-        children: n.map((e) => (0, t.jsx)(u, { platform: e }, e))
+        children: n.map((e) =>
+            (0, a.jsx)(
+                c.Tooltip,
+                {
+                    text: (function (e) {
+                        switch (e) {
+                            case o.z.DESKTOP:
+                                return d.Z.Messages.GAME_PROFILE_PLATFORM_DESKTOP;
+                            case o.z.XBOX:
+                                return d.Z.Messages.GAME_PROFILE_PLATFORM_XBOX;
+                            case o.z.PLAYSTATION:
+                                return d.Z.Messages.GAME_PROFILE_PLATFORM_PLAYSTATION;
+                            case o.z.NINTENDO:
+                                return d.Z.Messages.GAME_PROFILE_PLATFORM_NINTENDO;
+                            default:
+                                return null;
+                        }
+                    })(e),
+                    children: (n) =>
+                        (0, a.jsx)(E, {
+                            ...n,
+                            platform: e
+                        })
+                },
+                e
+            )
+        )
     });
 }
-function E(e) {
-    let { detectedGame: n, className: a } = e,
+function I(e) {
+    let { detectedGame: n, className: t } = e,
         s = r.useMemo(() => {
             let e = new Set(n.platforms),
-                a = [...e];
-            return !e.has(o.z.DESKTOP) && (e.has(o.z.MACOS) || e.has(o.z.LINUX)) && a.push(o.z.DESKTOP), a.filter((e) => d.includes(e));
+                t = [...e];
+            return !e.has(o.z.DESKTOP) && (e.has(o.z.MACOS) || e.has(o.z.LINUX)) && t.push(o.z.DESKTOP), t.filter((e) => _.includes(e));
         }, [n.platforms]);
     return 0 === s.length
         ? null
-        : (0, t.jsx)('div', {
-              className: i()(c.column, c.gapLg, a),
-              children: s.length > 0 && (0, t.jsx)(_, { platforms: s })
+        : (0, a.jsx)('div', {
+              className: i()(u.column, u.gapLg, t),
+              children: s.length > 0 && (0, a.jsx)(m, { platforms: s })
           });
 }
--- chunks/38516.js
+++ chunks/38516.js
@@ -1,17 +1,17 @@
-a(411104);
-var t = a(735250);
-a(470079);
-var r = a(120356),
-    s = a.n(r),
-    i = a(643872),
-    o = a(481060),
-    l = a(810568),
-    c = a(689938),
-    d = a(51527),
-    u = a(825718);
+t(411104);
+var a = t(735250);
+t(470079);
+var r = t(120356),
+    s = t.n(r),
+    i = t(643872),
+    o = t(481060),
+    l = t(810568),
+    c = t(689938),
+    d = t(51527),
+    u = t(825718);
 let _ = [i.p.OFFICIAL, i.p.TWITTER, i.p.YOUTUBE];
 function E(e) {
-    let { website: n, trackClick: a } = e,
+    let { website: n, trackClick: t } = e,
         {
             action: r,
             icon: s,
@@ -20,43 +20,46 @@ function E(e) {
             switch (e.category) {
                 case i.p.OFFICIAL:
                     return {
-                        icon: (0, t.jsx)(o.GlobeEarthIcon, { colorClass: u.linkIcon }),
+                        icon: (0, a.jsx)(o.GlobeEarthIcon, { colorClass: u.linkIcon }),
                         action: l.as.WebsiteLink,
                         title: c.Z.Messages.GAME_PROFILE_LINK_OFFICIAL
                     };
                 case i.p.TWITTER:
                     return {
-                        icon: (0, t.jsx)(o.XNeutralIcon, { colorClass: u.linkIcon }),
+                        icon: (0, a.jsx)(o.XNeutralIcon, { colorClass: u.linkIcon }),
                         action: l.as.XLink,
                         title: c.Z.Messages.GAME_PROFILE_LINK_TWITTER
                     };
                 case i.p.YOUTUBE:
                     return {
                         action: l.as.YouTubeLink,
-                        icon: (0, t.jsx)(o.YoutubeNeutralIcon, { colorClass: u.linkIcon }),
+                        icon: (0, a.jsx)(o.YoutubeNeutralIcon, { colorClass: u.linkIcon }),
                         title: c.Z.Messages.GAME_PROFILE_LINK_YOUTUBE
                     };
                 default:
                     throw Error('Unknown IGDB website category');
             }
         })(n);
-    return (0, t.jsx)(
-        o.Anchor,
-        {
-            className: u.linkAnchor,
-            title: d,
-            href: n.url,
-            onClick: () => {
-                a(r);
-            },
-            target: '_blank',
-            children: s
-        },
-        n.url
-    );
+    return (0, a.jsx)(o.Tooltip, {
+        text: n.url,
+        children: (e) => {
+            let { onClick: i, ...l } = e;
+            return (0, a.jsx)(o.Anchor, {
+                ...l,
+                className: u.linkAnchor,
+                title: d,
+                href: n.url,
+                onClick: () => {
+                    t(r), null == i || i();
+                },
+                target: '_blank',
+                children: s
+            });
+        }
+    });
 }
 n.Z = function (e) {
-    let { detectedGame: n, trackClick: a } = e;
+    let { detectedGame: n, trackClick: t } = e;
     if (null == n.websites) return null;
     let r = n.websites
         .filter((e) => {
@@ -66,22 +69,22 @@ n.Z = function (e) {
         .sort((e, n) => e.category - n.category);
     return 0 === r.length
         ? null
-        : (0, t.jsxs)('div', {
+        : (0, a.jsxs)('div', {
               children: [
-                  (0, t.jsx)(o.Heading, {
+                  (0, a.jsx)(o.Heading, {
                       className: d.sectionHeader,
                       variant: 'text-xs/semibold',
                       color: 'header-secondary',
                       children: c.Z.Messages.GAME_PROFILE_LINKS
                   }),
-                  (0, t.jsx)('div', {
+                  (0, a.jsx)('div', {
                       className: s()(d.row, d.gapMd),
                       children: r.map((e) =>
-                          (0, t.jsx)(
+                          (0, a.jsx)(
                               E,
                               {
                                   website: e,
-                                  trackClick: a
+                                  trackClick: t
                               },
                               e.url
                           )
