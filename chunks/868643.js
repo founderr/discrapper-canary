323d05139f3cee7b1bdc3ab194afe0b65ddbf0bf
DiscrapperManager <admin@minerpl.xyz>
2024-10-10 23:10:55 +0000

    canaryRelease: 334856
    Main scripts: 3
    Lazy Loaded Scripts: 892
    Chunks:
     * Main Files: 11213
     * Scripts: 10057
    Strings: 19155
    Routes: 682
    Experiments Count: 376

--- chunks/868643.js
+++ chunks/868643.js
@@ -1,34 +1,35 @@
-i.d(n, {
+t.d(n, {
     a: function () {
-        return _;
+        return f;
     },
     h: function () {
-        return u;
+        return _;
     }
 }),
-    i(789020);
-var t = i(470079),
-    o = i(442837),
-    a = i(430198),
-    c = i(592125),
-    r = i(630388),
-    l = i(981631);
-let s = l.iLy.CROSSPOSTED | l.iLy.FAILED_TO_MENTION_SOME_ROLES_IN_THREAD | l.iLy.GUILD_FEED_HIDDEN | l.iLy.HAS_SNAPSHOT | l.iLy.HAS_THREAD | l.iLy.IS_CROSSPOST | l.iLy.IS_VOICE_MESSAGE | l.iLy.SHOULD_SHOW_LINK_NOT_DISCORD_WARNING | l.iLy.SUPPRESS_EMBEDS | l.iLy.SUPPRESS_NOTIFICATIONS | l.iLy.URGENT;
+    t(789020);
+var i = t(470079),
+    r = t(442837),
+    a = t(430198),
+    l = t(592125),
+    u = t(430824),
+    o = t(630388),
+    s = t(981631);
+let c = s.iLy.CROSSPOSTED | s.iLy.FAILED_TO_MENTION_SOME_ROLES_IN_THREAD | s.iLy.GUILD_FEED_HIDDEN | s.iLy.HAS_SNAPSHOT | s.iLy.HAS_THREAD | s.iLy.IS_CROSSPOST | s.iLy.IS_VOICE_MESSAGE | s.iLy.SHOULD_SHOW_LINK_NOT_DISCORD_WARNING | s.iLy.SUPPRESS_EMBEDS | s.iLy.SUPPRESS_NOTIFICATIONS | s.iLy.URGENT;
 function d(e) {
-    return (!!(e.state !== l.yb.SEND_FAILED && l.V$x.FORWARDABLE.has(e.type)) && null == e.poll && null == e.activity && null == e.call && null == e.activityInstance && 0 === (0, r.Ge)(e.flags, s)) || !1;
-}
-function u(e) {
-    var n;
-    if (!d(e)) return !1;
-    let i = null === (n = c.Z.getChannel(e.channel_id)) || void 0 === n ? void 0 : n.guild_id;
-    return !(null != i && a.Z.isChannelOrThreadParentGated(i, e.channel_id)) && !0;
+    return (!!(e.state !== s.yb.SEND_FAILED && s.V$x.FORWARDABLE.has(e.type)) && null == e.poll && null == e.activity && null == e.call && null == e.activityInstance && 0 === (0, o.Ge)(e.flags, c)) || !1;
 }
 function _(e) {
-    let n = (0, o.e7)([a.Z, c.Z], () => {
-        var n;
-        if (null == e) return !1;
-        let i = null === (n = c.Z.getChannel(e.channel_id)) || void 0 === n ? void 0 : n.guild_id;
-        return null != i && a.Z.isChannelOrThreadParentGated(i, e.channel_id);
+    var n;
+    if (!d(e)) return !1;
+    let t = null === (n = l.Z.getChannel(e.channel_id)) || void 0 === n ? void 0 : n.guild_id;
+    return !(null != t && a.Z.isChannelOrThreadParentGated(t, e.channel_id)) && !0;
+}
+function f(e) {
+    let n = (0, r.e7)([u.Z, a.Z, l.Z], () => {
+        var n, t;
+        if (null == e) return !0;
+        let i = null === (n = l.Z.getChannel(e.channel_id)) || void 0 === n ? void 0 : n.guild_id;
+        return !(null == i || (null === (t = u.Z.getGuild(i)) || void 0 === t ? void 0 : t.hasFeature(s.oNc.FORWARDING_DISABLED))) || (null != i && a.Z.isChannelOrThreadParentGated(i, e.channel_id));
     });
-    return t.useMemo(() => null != e && d(e), [e]) && !n;
+    return i.useMemo(() => !n && null != e && d(e), [n, e]);
 }
