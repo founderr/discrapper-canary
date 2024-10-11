54eec06a7d58241f033b0b3c40fc0f4599f50afc
DiscrapperManager <admin@minerpl.xyz>
2024-10-11 17:51:55 +0000

    canaryRelease: 335000
    Main scripts: 3
    Lazy Loaded Scripts: 894
    Chunks:
     * Main Files: 11217
     * Scripts: 10057
    Strings: 19177
    Routes: 682
    Experiments Count: 376

---
+++ chunks/660579.js
@@ -0,0 +1,40 @@
+n.d(t, {
+    Z: function () {
+        return s;
+    }
+}),
+    n(47120);
+var r = n(735250),
+    i = n(470079),
+    a = n(825801);
+function s(e) {
+    let { user: t, sourceType: n, className: s, children: o } = e,
+        l = i.useRef(null),
+        [u, c] = i.useState(!1),
+        d = () => {
+            c(!0);
+        },
+        _ = () => {
+            c(!1);
+        };
+    return (0, r.jsxs)('div', {
+        className: s,
+        onMouseOver: d,
+        onMouseLeave: _,
+        onFocus: d,
+        onBlur: (e) => {
+            var t;
+            !(null === (t = l.current) || void 0 === t ? void 0 : t.contains(e.relatedTarget)) && _();
+        },
+        ref: l,
+        children: [
+            o,
+            (0, r.jsx)(a.ZP, {
+                user: t,
+                sourceType: n,
+                isVisible: u,
+                isExpandable: !1
+            })
+        ]
+    });
+}
--- chunks/671412.js
+++ chunks/671412.js
@@ -1,82 +1,50 @@
 n.d(t, {
     Z: function () {
-        return h;
+        return E;
     }
-}),
-    n(47120);
-var r = n(735250),
-    i = n(470079),
-    a = n(120356),
-    s = n.n(a),
-    o = n(510659),
-    l = n(899007),
-    u = n(287954),
-    c = n(825801),
-    d = n(64621),
-    _ = n(228168),
-    E = n(8621);
-function f(e) {
-    let { user: t, sourceType: n, className: a, children: s } = e,
-        o = i.useRef(null),
-        [l, u] = i.useState(!1),
-        d = () => {
-            u(!0);
-        },
-        _ = () => {
-            u(!1);
-        };
-    return (0, r.jsxs)('div', {
-        className: a,
-        onMouseOver: d,
-        onMouseLeave: _,
-        onFocus: d,
-        onBlur: (e) => {
-            var t;
-            !(null === (t = o.current) || void 0 === t ? void 0 : t.contains(e.relatedTarget)) && _();
-        },
-        ref: o,
-        children: [
-            s,
-            (0, r.jsx)(c.ZP, {
-                user: t,
-                sourceType: n,
-                isVisible: l,
-                isExpandable: !1
-            })
-        ]
-    });
-}
-function h(e) {
-    let { user: t, profileType: n, animateOnHover: i, onOpenProfile: a, ...c } = e,
-        h = _.n_.AVATAR,
-        { interactionSource: p } = (0, o.Xo)(),
-        I = s()(E.avatar, {
-            [E.hoisted]: p === _.n_.AVATAR || p === _.n_.STATUS,
-            [E.biteSize]: n === _.y0.BITE_SIZE,
-            [E.fullSize]: n === _.y0.FULL_SIZE,
-            [E.panel]: n === _.y0.PANEL
+});
+var r = n(735250);
+n(470079);
+var i = n(120356),
+    a = n.n(i),
+    s = n(510659),
+    o = n(899007),
+    l = n(287954),
+    u = n(660579),
+    c = n(64621),
+    d = n(228168),
+    _ = n(8621);
+function E(e) {
+    let { user: t, profileType: n, animateOnHover: i, onOpenProfile: E, ...f } = e,
+        h = d.n_.AVATAR,
+        { interactionSource: p } = (0, s.Xo)(),
+        I = a()(_.avatar, {
+            [_.hoisted]: p === d.n_.AVATAR || p === d.n_.STATUS,
+            [_.biteSize]: n === d.y0.BITE_SIZE,
+            [_.fullSize]: n === d.y0.FULL_SIZE,
+            [_.panel]: n === d.y0.PANEL
         });
-    return (0, r.jsx)(d.Z, {
-        sourceType: _.n_.AVATAR,
+    return (0, r.jsx)(c.Z, {
+        sourceType: d.n_.AVATAR,
         user: t,
-        children: (0, r.jsx)(u.Z, {
+        children: (0, r.jsx)(l.Z, {
             user: t,
             className: I,
             profileType: n,
             sourceType: h,
-            ...c,
+            ...f,
             children: () =>
-                (0, r.jsx)(f, {
+                (0, r.jsx)(u.Z, {
                     className: I,
                     user: t,
                     sourceType: h,
-                    children: (0, r.jsx)(l.Z, {
+                    children: (0, r.jsx)(o.Z, {
                         user: t,
                         animateOnHover: i,
-                        onOpenProfile: a,
+                        onOpenProfile: E,
                         profileType: n,
-                        className: E.withReactReply,
-                        ...c
+                        className: _.withReactReply,
+                        ...f
                     })
                 })
         })
