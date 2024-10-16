783415bb16f9eb31433382b68690a57915ca87a5
DiscrapperManager <admin@minerpl.xyz>
2024-10-16 23:05:23 +0000

    canaryRelease: 336028
    Main scripts: 3
    Lazy Loaded Scripts: 889
    Chunks:
     * Main Files: 11235
     * Scripts: 10069
    Strings: 19218
    Routes: 681
    Experiments Count: 383

--- chunks/961556.js
+++ chunks/961556.js
@@ -1,6 +1,6 @@
 n.d(t, {
     Z: function () {
-        return u;
+        return o;
     }
 }),
     n(47120);
@@ -8,46 +8,37 @@ var i = n(735250);
 n(470079);
 var l = n(278074),
     r = n(788307),
-    a = n(6074),
-    s = n(26033),
-    o = n(689938),
-    c = n(23952);
-function u(e) {
-    let { entry: t, className: n, textClassName: u } = e,
-        d = (0, l.EQ)(t)
-            .when(s.dX, (e) => {
+    a = n(26033),
+    s = n(689938);
+function o(e) {
+    let { entry: t, className: n, textClassName: o } = e,
+        c = (0, l.EQ)(t)
+            .when(a.dX, (e) => {
                 let { extra: t } = e;
-                return o.Z.Messages.MEMBER_LIST_CONTENT_FEED_PLAYED_GAME.format({ gameName: t.game_name });
+                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_PLAYED_GAME.format({ gameName: t.game_name });
             })
-            .when(s.dU, (e) => {
+            .when(a.dU, (e) => {
                 let { extra: t } = e;
-                return o.Z.Messages.MEMBER_LIST_CONTENT_FEED_LISTENED_TO_MEDIA.format({ media: t.entries[0].media.title });
+                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_LISTENED_TO_MEDIA.format({ media: t.entries[0].media.title });
             })
-            .when(s.KF, (e) => {
+            .when(a.KF, (e) => {
                 let { extra: t } = e;
-                return o.Z.Messages.MEMBER_LIST_CONTENT_FEED_LISTENED_TO_MEDIA.format({ media: t.media.title });
+                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_LISTENED_TO_MEDIA.format({ media: t.media.title });
             })
-            .when(s.y0, (e) => {
+            .when(a.y0, (e) => {
                 let { extra: t } = e;
-                return o.Z.Messages.MEMBER_LIST_CONTENT_FEED_WATCHED_MEDIA.format({ mediaTitle: t.media_title });
+                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_WATCHED_MEDIA.format({ mediaTitle: t.media_title });
             })
-            .when(s.Mq, (e) => {
+            .when(a.Mq, (e) => {
                 let { extra: t } = e;
-                return o.Z.Messages.MEMBER_LIST_CONTENT_FEED_PLAYED_GAME.format({ gameName: t.activity_name });
+                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_PLAYED_GAME.format({ gameName: t.activity_name });
             })
             .exhaustive();
-    return (0, i.jsxs)('div', {
+    return (0, i.jsx)('div', {
         className: n,
-        children: [
-            (0, i.jsx)(r.DD, {
-                text: d,
-                className: u
-            }),
-            (0, i.jsx)(a.Z, {
-                width: 16,
-                height: 16,
-                className: c.icon
-            })
-        ]
+        children: (0, i.jsx)(r.DD, {
+            text: c,
+            className: o
+        })
     });
 }
