baa9f518882bd53357e2e4f54fd5f4f5ab12bec5
DiscrapperManager <admin@minerpl.xyz>
2024-10-16 18:22:48 +0000

    canaryRelease: 335828
    Main scripts: 3
    Lazy Loaded Scripts: 886
    Chunks:
     * Main Files: 11248
     * Scripts: 10051
    Strings: 19207
    Routes: 681
    Experiments Count: 381

--- chunks/180970.js
+++ chunks/180970.js
@@ -1,7 +1,7 @@
 n.r(t),
     n.d(t, {
         default: function () {
-            return u;
+            return b;
         }
     });
 var a = n(735250),
@@ -9,34 +9,39 @@ var a = n(735250),
     o = n(77866),
     l = n(481060),
     r = n(332664),
-    i = n(987562),
-    c = n(689938);
-function u(e) {
-    let { onClose: t, transitionState: u } = e,
-        d = (0, o.Z)(i.CE),
-        b = s.useCallback((e) => {
-            let { rating: t, problem: s } = e;
-            if (null != t)
-                null != s &&
-                    (0, l.openModalLazy)(async () => {
-                        let { default: e } = await n.e('14466').then(n.bind(n, 729328));
-                        return (t) =>
-                            (0, a.jsx)(e, {
-                                body: c.Z.Messages.CALL_FEEDBACK_CONFIRMATION,
-                                ...t
-                            });
-                    });
-        }, []);
+    i = n(626135),
+    c = n(987562),
+    u = n(981631),
+    d = n(689938);
+function b(e) {
+    let { onClose: t, transitionState: b } = e,
+        m = (0, o.Z)(c.CE);
+    s.useEffect(() => {
+        i.default.track(u.rMx.OPEN_MODAL, { type: 'Block User Feedback' });
+    }, []);
+    let f = s.useCallback((e) => {
+        let { rating: t, problem: s } = e;
+        if (null != t)
+            null != s &&
+                (0, l.openModalLazy)(async () => {
+                    let { default: e } = await n.e('14466').then(n.bind(n, 729328));
+                    return (t) =>
+                        (0, a.jsx)(e, {
+                            body: d.Z.Messages.CALL_FEEDBACK_CONFIRMATION,
+                            ...t
+                        });
+                });
+    }, []);
     return (0, a.jsx)(r.Z, {
         modalType: 'block_user',
         header: 'header goes here',
         body: 'body goes here',
         problemTitle: 'section header goes here',
-        problems: d,
-        feedbackProblems: [i.F5.OTHER],
-        onSubmit: b,
+        problems: m,
+        feedbackProblems: [c.F5.OTHER],
+        onSubmit: f,
         onClose: t,
-        transitionState: u,
-        otherKey: i.F5.OTHER
+        transitionState: b,
+        otherKey: c.F5.OTHER
     });
 }
