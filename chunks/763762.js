596983c1e7604deccc5b621155fc208d990afb08
DiscrapperManager <admin@minerpl.xyz>
2024-10-17 21:36:51 +0000

    canaryRelease: 336381
    Main scripts: 3
    Lazy Loaded Scripts: 886
    Chunks:
     * Main Files: 11243
     * Scripts: 10074
    Strings: 19246
    Routes: 683
    Experiments Count: 383

--- chunks/763762.js
+++ chunks/763762.js
@@ -1,31 +1,15 @@
 n.d(t, {
     Z: function () {
-        return o;
+        return l;
     }
 });
 var i = n(442837),
-    s = n(911969),
-    a = n(213459),
-    l = n(621853),
-    r = n(981631);
-function o(e, t) {
-    var n;
-    let o = (0, i.e7)([l.Z], () => {
+    s = n(621853),
+    a = n(981631);
+function l(e) {
+    let t = (0, i.e7)([s.Z], () => {
         var t;
-        return null !== e ? (null === (t = l.Z.getUserProfile(null != e ? e : r.lds)) || void 0 === t ? void 0 : t.application) : void 0;
+        return null !== e ? (null === (t = s.Z.getUserProfile(null != e ? e : a.lds)) || void 0 === t ? void 0 : t.application) : void 0;
     });
-    return null ===
-        (n = (0, a.v1)(
-            t,
-            {
-                commandTypes: [s.yU.PRIMARY_ENTRY_POINT, s.yU.CHAT, s.yU.MESSAGE, s.yU.USER]
-            },
-            {
-                applicationId: null == o ? void 0 : o.id,
-                allowFetch: !0,
-                allowApplicationState: !0
-            }
-        ).commands.find((e) => e.applicationId === (null == o ? void 0 : o.id))) || void 0 === n
-        ? void 0
-        : n.section.application;
+    return null != t ? t : void 0;
 }
