e2b2a928c9d9e8a343461929043d297985915847
DiscrapperManager <admin@minerpl.xyz>
2024-10-17 00:51:29 +0000

    canaryRelease: 336103
    Main scripts: 3
    Lazy Loaded Scripts: 889
    Chunks:
     * Main Files: 11238
     * Scripts: 10070
    Strings: 19219
    Routes: 681
    Experiments Count: 384

--- chunks/975907.js
+++ chunks/975907.js
@@ -1,39 +1,41 @@
 n.d(t, {
     M: function () {
-        return r;
+        return l;
     }
 }),
     n(47120);
 var i = n(470079),
     a = n(288385),
-    s = n(674588);
-function r() {
+    s = n(674588),
+    r = n(979007);
+function l() {
     let [e, t] = i.useState(''),
-        [n, r] = i.useState(''),
-        [l, o] = i.useState(!1),
-        c = i.useCallback((e) => {
+        [n, l] = i.useState(''),
+        [o, c] = i.useState(!1),
+        d = i.useCallback((e) => {
             t(e);
         }, []),
-        d = i.useCallback(() => {
-            t(''), r(''), o(!1);
-        }, []),
         u = i.useCallback(() => {
+            t(''), l(''), c(!1);
+        }, []),
+        _ = i.useCallback(() => {
             s.yC({
                 query: e,
                 options: {
                     page: 1,
-                    source: a.F.APP_DIRECTORY
+                    source: a.F.APP_DIRECTORY,
+                    pageSize: r.IV
                 }
             }),
-                o(!0),
-                r(e);
+                c(!0),
+                l(e);
         }, [e]);
     return {
         searchQuery: e,
         searchFetchedQuery: n,
-        onSearchTextChange: c,
-        onClearSearch: d,
-        onSearchSubmit: u,
-        hasSearchView: l
+        onSearchTextChange: d,
+        onClearSearch: u,
+        onSearchSubmit: _,
+        hasSearchView: o
     };
 }
