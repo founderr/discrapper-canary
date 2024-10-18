a7daeb4a48f20037054181617baf7c9c53dcaa17
DiscrapperManager <admin@minerpl.xyz>
2024-10-18 22:47:18 +0000

    canaryRelease: 336935
    Main scripts: 3
    Lazy Loaded Scripts: 887
    Chunks:
     * Main Files: 11251
     * Scripts: 10097
    Strings: 19286
    Routes: 684
    Experiments Count: 382

--- chunks/141594.js
+++ chunks/141594.js
@@ -8,42 +8,40 @@ var i = n(470079),
     c = n(594174),
     d = n(51144),
     u = n(874703),
-    _ = n(105622),
-    E = n(302800),
-    h = n(921944);
+    _ = n(302800),
+    E = n(921944);
 t.Z = () => {
-    var e, t, n, m, I, p, g;
-    let T = (0, r.e7)([c.default], () => c.default.getCurrentUser()),
-        S = (0, _.J)('useCollectiblesShopTabNewBadge'),
-        f = (0, r.e7)([u.Z], () => u.Z.getMarketingBySurface(a.K.DESKTOP_SHOP_BUTTON)),
-        C = (null == f ? void 0 : f.dismissibleContent) === l.z.COLLECTIBLES_SHOP_ENTRY_MARKETING,
-        N = i.useMemo(() => ((0, d.Fc)(T) && S && null != f && !C ? [f.dismissibleContent] : []), [T, S, f, C]),
-        [A, v] = (0, o.US)(N, void 0, !0),
-        [Z, L] = (0, o.XR)(C && (null == f ? void 0 : f.type) === s.Z.COACHTIP ? l.z.COLLECTIBLES_SHOP_ENTRY_MARKETING : null, null !== (e = null == f ? void 0 : f.version) && void 0 !== e ? e : 0, void 0, !0),
-        R = null;
-    if ((null != A || null != Z) && S && null != f && f.type === s.Z.COACHTIP) {
-        R = {
-            type: E.k2.COACHTIP,
-            title: () => f.title,
-            body: () => f.body,
-            imageSrc: f.avatar,
-            assetIds: f.decorations,
-            entrypointBackgroundStyle: null === (t = f.refTargetBackground) || void 0 === t ? void 0 : t.style
+    var e, t, n, h, m, I, p;
+    let g = (0, r.e7)([c.default], () => c.default.getCurrentUser()),
+        T = (0, r.e7)([u.Z], () => u.Z.getMarketingBySurface(a.K.DESKTOP_SHOP_BUTTON)),
+        S = (null == T ? void 0 : T.dismissibleContent) === l.z.COLLECTIBLES_SHOP_ENTRY_MARKETING,
+        f = i.useMemo(() => ((0, d.Fc)(g) && null != T && !S ? [T.dismissibleContent] : []), [g, T, S]),
+        [C, N] = (0, o.US)(f, void 0, !0),
+        [A, v] = (0, o.XR)(S && (null == T ? void 0 : T.type) === s.Z.COACHTIP ? l.z.COLLECTIBLES_SHOP_ENTRY_MARKETING : null, null !== (e = null == T ? void 0 : T.version) && void 0 !== e ? e : 0, void 0, !0),
+        Z = null;
+    if ((null != C || null != A) && null != T && T.type === s.Z.COACHTIP) {
+        Z = {
+            type: _.k2.COACHTIP,
+            title: () => T.title,
+            body: () => T.body,
+            imageSrc: T.avatar,
+            assetIds: T.decorations,
+            entrypointBackgroundStyle: null === (t = T.refTargetBackground) || void 0 === t ? void 0 : t.style
         };
-        let e = null === (n = f.refTargetBackground) || void 0 === n ? void 0 : n.asset;
+        let e = null === (n = T.refTargetBackground) || void 0 === n ? void 0 : n.asset;
         null != e &&
-            (R.entryPointBackgroundAssets = {
-                srcDark: null === (m = e.resting) || void 0 === m ? void 0 : m.dark,
-                srcLight: null === (I = e.resting) || void 0 === I ? void 0 : I.light,
-                srcDarkHovered: null === (p = e.hovered) || void 0 === p ? void 0 : p.dark,
-                srcLightHovered: null === (g = e.hovered) || void 0 === g ? void 0 : g.light
+            (Z.entryPointBackgroundAssets = {
+                srcDark: null === (h = e.resting) || void 0 === h ? void 0 : h.dark,
+                srcLight: null === (m = e.resting) || void 0 === m ? void 0 : m.light,
+                srcDarkHovered: null === (I = e.hovered) || void 0 === I ? void 0 : I.dark,
+                srcLightHovered: null === (p = e.hovered) || void 0 === p ? void 0 : p.light
             });
     }
     return {
-        collectiblesShopTabNewBadgeDisplayOptions: R,
+        collectiblesShopTabNewBadgeDisplayOptions: Z,
         dismissCollectiblesShopTabNewBadge: () => {
-            let e = C ? L : v;
-            null != R && e(h.L.AUTO);
+            let e = S ? v : N;
+            null != Z && e(E.L.AUTO);
         }
     };
 };
--- chunks/954709.js
+++ chunks/954709.js
@@ -2,9 +2,8 @@ var i = n(664081),
     a = n(570140),
     s = n(317770),
     r = n(863750),
-    l = n(335131),
-    o = n(105622);
-class c extends s.Z {
+    l = n(335131);
+class o extends s.Z {
     _initialize() {
         a.Z.subscribe('POST_CONNECTION_OPEN', this.handlePostConnectionOpen);
     }
@@ -13,7 +12,7 @@ class c extends s.Z {
     }
     handlePostConnectionOpen() {
         let e = r.Z.get('shop_include_unpublished');
-        (0, o.d)('CollectiblesMarketingManager') && (0, l.R2)({ release: e ? i.P.BETA : i.P.PROD });
+        (0, l.R2)({ release: e ? i.P.BETA : i.P.PROD });
     }
 }
-t.Z = new c();
+t.Z = new o();
