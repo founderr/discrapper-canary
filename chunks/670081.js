968f122351d47207dd0f0506d906bd77a9e731a7
DiscrapperManager <admin@minerpl.xyz>
2024-10-23 18:16:06 +0000

    canaryRelease: 338181
    Main scripts: 3
    Lazy Loaded Scripts: 888
    Chunks:
     * Main Files: 11250
     * Scripts: 10138
    Strings: 19336
    Routes: 685
    Experiments Count: 386

--- chunks/670081.js
+++ chunks/670081.js
@@ -5,7 +5,6 @@ n.d(t, {
     }
 }),
     ((i = r || (r = {}))[(i.POST_ENROLLMENT_CTA = 1)] = 'POST_ENROLLMENT_CTA'),
-    (i[(i.PLAYTIME_TASK = 2)] = 'PLAYTIME_TASK'),
     (i[(i.QUEST_BAR_V2 = 3)] = 'QUEST_BAR_V2'),
     (i[(i.EXCLUDE_RUSSIA = 5)] = 'EXCLUDE_RUSSIA'),
     (i[(i.IN_HOUSE_CONSOLE_QUEST = 6)] = 'IN_HOUSE_CONSOLE_QUEST'),
