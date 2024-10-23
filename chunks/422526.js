c65cbef6ddce870a55cce13ab11aba34f2dc7b09
DiscrapperManager <admin@minerpl.xyz>
2024-10-23 22:18:29 +0000

    canaryRelease: 338389
    Main scripts: 3
    Lazy Loaded Scripts: 888
    Chunks:
     * Main Files: 11254
     * Scripts: 10137
    Strings: 19345
    Routes: 686
    Experiments Count: 388

---
+++ chunks/422526.js
@@ -0,0 +1,14 @@
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2024-10_updated_fetch_subscription_plans_for_sku_error',
+    label: 'Updated Fetch Subscription Plans For SKU Error',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable updated error throwing',
+            config: { enabled: !0 }
+        }
+    ]
+});
+t.Z = r;
--- chunks/821849.js
+++ chunks/821849.js
@@ -1,61 +1,62 @@
 n.d(t, {
     GZ: function () {
-        return d;
-    },
-    Gn: function () {
         return _;
     },
-    Y2: function () {
+    Gn: function () {
         return E;
     },
-    mE: function () {
+    Y2: function () {
         return f;
+    },
+    mE: function () {
+        return h;
     }
 }),
     n(47120);
 var r = n(544891),
     i = n(570140),
     a = n(34756),
-    s = n(351402),
-    o = n(122289),
-    l = n(355467),
-    u = n(981631),
-    c = n(474936);
-async function d(e, t, n, c, d) {
+    s = n(422526),
+    o = n(351402),
+    l = n(122289),
+    u = n(355467),
+    c = n(981631),
+    d = n(474936);
+async function _(e, t, n, d, _) {
     i.Z.dispatch({
         type: 'SUBSCRIPTION_PLANS_FETCH',
         skuId: e
     });
     try {
         let a = {
-                url: u.ANM.STORE_PUBLISHED_LISTINGS_SUBSCRIPTION_PLANS(e),
+                url: c.ANM.STORE_PUBLISHED_LISTINGS_SUBSCRIPTION_PLANS(e),
                 oldFormErrors: !0
             },
-            o = {};
-        null != t && (o.country_code = t), null != n && (o.payment_source_id = n), null != c && (o.include_unpublished = c), null != d && (o.revenue_surface = d), (a.query = o), !s.Z.ipCountryCodeLoaded && (await (0, l.GE)());
-        let _ = await r.tn.get(a);
+            s = {};
+        null != t && (s.country_code = t), null != n && (s.payment_source_id = n), null != d && (s.include_unpublished = d), null != _ && (s.revenue_surface = _), (a.query = s), !o.Z.ipCountryCodeLoaded && (await (0, u.GE)());
+        let l = await r.tn.get(a);
         i.Z.dispatch({
             type: 'SUBSCRIPTION_PLANS_FETCH_SUCCESS',
             skuId: e,
-            subscriptionPlans: _.body
+            subscriptionPlans: l.body
         });
-    } catch (t) {
-        throw (
-            (i.Z.dispatch({
-                type: 'SUBSCRIPTION_PLANS_FETCH_FAILURE',
-                skuId: e
-            }),
-            (0, o.q2)(t),
-            new a.Z(t))
-        );
+    } catch (n) {
+        i.Z.dispatch({
+            type: 'SUBSCRIPTION_PLANS_FETCH_FAILURE',
+            skuId: e
+        }),
+            (0, l.q2)(n);
+        let { enabled: t } = s.Z.getCurrentConfig({ location: 'fetchSubscriptionPlansForSKU' });
+        if (t) throw new a.Z('Failed to fetch subscription plans for SKU '.concat(e), n);
+        throw new a.Z(n);
     }
 }
-function _(e, t) {
-    return Promise.all(e.filter((e) => e !== c.Si.NONE).map((e) => d(e, t)));
+function E(e, t) {
+    return Promise.all(e.filter((e) => e !== d.Si.NONE).map((e) => _(e, t)));
 }
-function E(e, t, n) {
-    return Promise.all(c.YQ.filter((e) => e !== c.Si.NONE).map((r) => d(r, e, t, void 0, n)));
+function f(e, t, n) {
+    return Promise.all(d.YQ.filter((e) => e !== d.Si.NONE).map((r) => _(r, e, t, void 0, n)));
 }
-function f() {
+function h() {
     i.Z.dispatch({ type: 'SUBSCRIPTION_PLANS_RESET' });
 }
