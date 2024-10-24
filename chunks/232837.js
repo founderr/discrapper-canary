38b6a7ded31292b19009e9041714f1e8e8192015
DiscrapperManager <admin@minerpl.xyz>
2024-10-24 19:28:28 +0000

    canaryRelease: 338749
    Main scripts: 3
    Lazy Loaded Scripts: 889
    Chunks:
     * Main Files: 11254
     * Scripts: 10154
    Strings: 19367
    Routes: 687
    Experiments Count: 389

--- chunks/232837.js
+++ chunks/232837.js
@@ -8,63 +8,64 @@ var t = n(200651);
 n(192379);
 var a = n(990547),
     o = n(481060),
-    i = n(287734),
-    r = n(594174),
+    r = n(287734),
+    i = n(594174),
     _ = n(626135),
     c = n(33194),
     d = n(807031),
     l = n(343544),
-    N = n(110223),
-    u = n(981631),
-    E = n(689938);
+    E = n(110223),
+    N = n(981631),
+    u = n(689938);
 function C(e) {
-    let { channelId: s, blockedUserId: n, transitionState: C, onClose: m } = e;
+    let { channelId: s, blockedUserId: n, transitionState: C, onClose: A } = e;
     if (!(0, d.KR)({ location: 'warning_modal' })) return null;
-    let x = r.default.getUser(n),
+    let x = i.default.getUser(n),
         I = [
             {
                 icon: (0, t.jsx)(o.UserIcon, {}),
-                text: E.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_PRESENCE.format({ userName: null == x ? void 0 : x.username })
+                text: u.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_PRESENCE.format({ userName: null == x ? void 0 : x.username })
             },
             {
                 icon: (0, t.jsx)(o.MicrophoneIcon, {}),
-                text: E.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_AUDIBLE
+                text: u.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_AUDIBLE
             }
         ];
     return (0, t.jsx)(l.Z, {
-        headerText: E.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_ALERT,
-        descriptionText: E.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_DESCRIPTION,
+        headerText: u.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_ALERT_V2,
+        secondaryHeaderText: u.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_LEAVE_MODAL,
+        descriptionText: u.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_DESCRIPTION,
         infoRows: I,
         onDismissAndStay: () => {
-            m(),
+            A(),
                 (0, c.g6)(s),
-                _.default.track(u.rMx.VOICE_CHANNEL_BLOCKED_USER_WARNING_ENGAGEMENT, {
-                    action: N.q.CLICK_TO_STAY,
+                _.default.track(N.rMx.VOICE_CHANNEL_BLOCKED_USER_WARNING_ENGAGEMENT, {
+                    action: E.q.CLICK_TO_STAY,
                     channel_id: s,
                     blocked_user_ids: [n],
-                    warning_surface: N.fz.POST_JOIN_MODAL
+                    warning_surface: E.fz.POST_JOIN_MODAL
                 });
         },
         onDismissAndLeave: () => {
-            m(),
-                i.default.disconnect(),
-                _.default.track(u.rMx.VOICE_CHANNEL_BLOCKED_USER_WARNING_ENGAGEMENT, {
-                    action: N.q.CLICK_TO_LEAVE,
+            A(),
+                r.default.disconnect(),
+                _.default.track(N.rMx.VOICE_CHANNEL_BLOCKED_USER_WARNING_ENGAGEMENT, {
+                    action: E.q.CLICK_TO_LEAVE,
                     channel_id: s,
                     blocked_user_ids: [n],
-                    warning_surface: N.fz.POST_JOIN_MODAL
+                    warning_surface: E.fz.POST_JOIN_MODAL
                 });
         },
-        leaveButtonText: E.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_LEAVE,
-        stayButtonText: E.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_STAY,
+        leaveButtonText: u.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_LEAVE,
+        stayButtonText: u.Z.Messages.VOICE_CHANNEL_BLOCKED_USER_WARNING_STAY,
         transitionState: C,
-        onClose: m,
+        onClose: A,
         impression: {
             impressionName: a.ImpressionNames.VOICE_CHANNEL_BLOCKED_USER_WARNING,
             impressionProperties: {
                 channel_id: s,
                 blocked_user_ids: [n],
-                warning_surface: N.fz.POST_JOIN_MODAL
+                warning_surface: E.fz.POST_JOIN_MODAL
             }
         }
     });
