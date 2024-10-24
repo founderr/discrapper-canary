8c22f6d503cfc0353f75b68026340c782a3b15bb
DiscrapperManager <admin@minerpl.xyz>
2024-10-24 17:03:05 +0000

    canaryRelease: 338623
    Main scripts: 3
    Lazy Loaded Scripts: 889
    Chunks:
     * Main Files: 11254
     * Scripts: 10151
    Strings: 19366
    Routes: 686
    Experiments Count: 388

--- chunks/961556.js
+++ chunks/961556.js
@@ -12,33 +12,47 @@ var l = n(278074),
     s = n(689938);
 function o(e) {
     let { entry: t, className: n, textClassName: o } = e,
-        c = (0, l.EQ)(t)
+        { message: c, title: u } = (0, l.EQ)(t)
             .when(a.dX, (e) => {
                 let { extra: t } = e;
-                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_PLAYED_GAME.format({ gameName: t.game_name });
-            })
-            .when(a.dU, (e) => {
-                let { extra: t } = e;
-                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_LISTENED_TO_MEDIA.format({ media: t.entries[0].media.title });
-            })
-            .when(a.KF, (e) => {
-                let { extra: t } = e;
-                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_LISTENED_TO_MEDIA.format({ media: t.media.title });
-            })
-            .when(a.y0, (e) => {
-                let { extra: t } = e;
-                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_WATCHED_MEDIA.format({ mediaTitle: t.media_title });
+                return {
+                    message: s.Z.Messages.USER_ACTIVITY_STATUS_RECENTLY_PLAYED,
+                    title: t.game_name
+                };
             })
             .when(a.Mq, (e) => {
                 let { extra: t } = e;
-                return s.Z.Messages.MEMBER_LIST_CONTENT_FEED_PLAYED_GAME.format({ gameName: t.activity_name });
+                return {
+                    message: s.Z.Messages.USER_ACTIVITY_STATUS_RECENTLY_PLAYED,
+                    title: t.activity_name
+                };
             })
-            .exhaustive();
-    return (0, i.jsx)('div', {
-        className: n,
-        children: (0, i.jsx)(r.DD, {
-            text: c,
-            className: o
-        })
-    });
+            .when(a.y0, (e) => {
+                let { extra: t } = e;
+                return {
+                    message: s.Z.Messages.USER_ACTIVITY_STATUS_RECENTLY_WATCHED,
+                    title: t.media_title
+                };
+            })
+            .when(a.dU, (e) => {
+                var t, n;
+                let { extra: i } = e;
+                return {
+                    message: s.Z.Messages.USER_ACTIVITY_STATUS_RECENTLY_LISTENED_TO,
+                    title: null === (n = i.entries[0]) || void 0 === n ? void 0 : null === (t = n.media) || void 0 === t ? void 0 : t.title
+                };
+            })
+            .otherwise(() => ({
+                message: null,
+                title: null
+            }));
+    return null == u || '' === u
+        ? null
+        : (0, i.jsx)('div', {
+              className: n,
+              children: (0, i.jsx)(r.DD, {
+                  text: c.format({ title: u }),
+                  className: o
+              })
+          });
 }
