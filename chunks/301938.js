7f89b9183f8da4663bc418b1d469f998d44888d6
DiscrapperManager <admin@minerpl.xyz>
2024-10-25 18:33:50 +0000

    canaryRelease: 339190
    Main scripts: 3
    Lazy Loaded Scripts: 889
    Chunks:
     * Main Files: 11264
     * Scripts: 10157
    Strings: 19388
    Routes: 687
    Experiments Count: 391

--- chunks/301938.js
+++ chunks/301938.js
@@ -1,75 +1,88 @@
 n.d(t, {
     Z: function () {
-        return p;
+        return f;
     }
 }),
     n(47120),
+    n(411104),
     n(773603);
 var s = n(200651),
     r = n(192379),
     i = n(120356),
     a = n.n(i),
     l = n(990547),
-    o = n(388905),
-    c = n(585483),
-    u = n(573261),
-    d = n(720196),
-    _ = n(981631),
-    h = n(689938),
-    E = n(340214),
-    g = n(113207);
-function p(e) {
-    let { setOriginalEmail: t, setSlide: i, ready: p, token: m } = e,
-        [f, I] = r.useState(!1),
-        [N, T] = r.useState(null),
-        [A, x] = r.useState(''),
-        C = r.useRef(null);
+    o = n(481060),
+    c = n(479531),
+    u = n(388905),
+    d = n(585483),
+    _ = n(573261),
+    h = n(720196),
+    E = n(981631),
+    g = n(689938),
+    p = n(340214),
+    m = n(113207);
+function f(e) {
+    let { setOriginalEmail: t, setSlide: i, ready: f, token: I } = e,
+        [N, T] = r.useState(!1),
+        [A, x] = r.useState(null),
+        [C, R] = r.useState(null),
+        [S, O] = r.useState(''),
+        Z = r.useRef(null);
     return (
         r.useEffect(() => {
-            if (p) {
+            if (f) {
                 var e;
-                null === (e = C.current) || void 0 === e || e.focus();
+                null === (e = Z.current) || void 0 === e || e.focus();
             }
-        }, [p]),
+        }, [f]),
         (0, s.jsxs)('div', {
             children: [
-                (0, s.jsx)(o.Ee, {
-                    src: null == N ? n(26230) : n(935227),
-                    className: a()(g.marginBottom20, g.marginTop8)
+                (0, s.jsx)(u.Ee, {
+                    src: null == C ? n(26230) : n(935227),
+                    className: a()(m.marginBottom20, m.marginTop8)
                 }),
-                (0, s.jsx)(o.Dx, { children: h.Z.Messages.ACCOUNT_REVERT_NEW_PASSWORD_SLIDE_TITLE }),
-                (0, s.jsxs)(o.gO, {
-                    className: a()(g.marginBottom20, g.marginTop20),
+                (0, s.jsx)(u.Dx, { children: g.Z.Messages.ACCOUNT_REVERT_NEW_PASSWORD_SLIDE_TITLE }),
+                null != C && '' !== C
+                    ? (0, s.jsx)(o.Text, {
+                          variant: 'text-sm/normal',
+                          color: 'text-danger',
+                          children: C
+                      })
+                    : null,
+                (0, s.jsxs)(u.gO, {
+                    className: a()(m.marginBottom20, m.marginTop20),
                     children: [
-                        (0, s.jsx)(o.II, {
+                        (0, s.jsx)(u.II, {
                             name: 'password',
                             type: 'password',
-                            label: h.Z.Messages.FORM_LABEL_NEW_PASSWORD,
-                            setRef: C,
-                            className: g.marginBottom20,
-                            value: A,
-                            onChange: x,
-                            error: N,
+                            label: g.Z.Messages.FORM_LABEL_NEW_PASSWORD,
+                            setRef: Z,
+                            className: m.marginBottom20,
+                            value: S,
+                            onChange: O,
+                            error: A,
                             autoComplete: 'new-password',
                             maxLength: 72,
-                            placeholder: h.Z.Messages.ACCOUNT_REVERT_NEW_PASSWORD_INPUT_PLACEHOLDER
+                            placeholder: g.Z.Messages.ACCOUNT_REVERT_NEW_PASSWORD_INPUT_PLACEHOLDER
                         }),
-                        (0, s.jsx)(o.zx, {
-                            className: g.marginTop8,
+                        (0, s.jsx)(u.zx, {
+                            className: m.marginTop8,
                             onClick: () => {
-                                if (!f) {
-                                    if (0 === A.length) {
-                                        T(h.Z.Messages.PASSWORD_REQUIRED), c.S.dispatch(_.CkL.WAVE_EMPHASIZE);
+                                if (!N) {
+                                    if (0 === S.length) {
+                                        x(g.Z.Messages.PASSWORD_REQUIRED), d.S.dispatch(E.CkL.WAVE_EMPHASIZE);
                                         return;
                                     }
                                     return (
-                                        null != N && T(null),
-                                        I(!0),
-                                        u.Z.post({
-                                            url: _.ANM.ACCOUNT_REVERT,
+                                        null != C && R(null),
+                                        null != A && x(null),
+                                        t(''),
+                                        T(!0),
+                                        _.Z.post({
+                                            url: E.ANM.ACCOUNT_REVERT,
                                             body: {
-                                                token: m,
-                                                password: A
+                                                token: I,
+                                                password: S
                                             },
                                             trackedActionData: { event: l.NetworkActionNames.ACCOUNT_REVERT }
                                         })
@@ -77,30 +90,51 @@ function p(e) {
                                                 let {
                                                     body: { email: n }
                                                 } = e;
-                                                x(''), t(n), i(d.n.SUCCESS);
+                                                O(''), t(n), i(h.n.SUCCESS);
                                             })
                                             .catch((e) => {
-                                                var t, n;
-                                                T(null !== (n = e.message) && void 0 !== n ? n : null === (t = e.body) || void 0 === t ? void 0 : t.message), c.S.dispatch(_.CkL.WAVE_EMPHASIZE);
+                                                if (e instanceof Error) R(g.Z.Messages.FORM_ERROR_GENERIC.format({ statusPageURL: E.yXt.STATUS }));
+                                                else {
+                                                    let t = new c.Z(e);
+                                                    t.hasFieldErrors()
+                                                        ? x(t.getAnyErrorMessage())
+                                                        : R(
+                                                              (function (e) {
+                                                                  switch (e) {
+                                                                      case E.evJ.ACCOUNT_REVERT_INVALID_TOKEN:
+                                                                          return g.Z.Messages.ACCOUNT_REVERT_ERROR_INVALID_TOKEN;
+                                                                      case E.evJ.ACCOUNT_REVERT_EMAIL_ALREADY_TAKEN:
+                                                                          return g.Z.Messages.ACCOUNT_REVERT_ERROR_EMAIL_ALREADY_TAKEN;
+                                                                      case E.evJ.ACCOUNT_REVERT_ACCOUNT_NOT_FOUND:
+                                                                          return g.Z.Messages.ACCOUNT_REVERT_ERROR_ACCOUNT_NOT_FOUND;
+                                                                      default:
+                                                                          return g.Z.Messages.FORM_ERROR_GENERIC.format({ statusPageURL: E.yXt.STATUS });
+                                                                  }
+                                                              })(t.code).toString()
+                                                          );
+                                                }
+                                                d.S.dispatch(E.CkL.WAVE_EMPHASIZE);
                                             })
-                                            .finally(() => I(!1))
+                                            .finally(() => {
+                                                T(!1);
+                                            })
                                     );
                                 }
                             },
-                            submitting: f,
-                            disabled: f,
-                            children: h.Z.Messages.ACCOUNT_REVERT_RECOVER_MY_ACCOUNT_BUTTON
+                            submitting: N,
+                            disabled: N,
+                            children: g.Z.Messages.ACCOUNT_REVERT_RECOVER_MY_ACCOUNT_BUTTON
                         })
                     ]
                 }),
                 (0, s.jsx)('div', {
-                    className: E.buttonContainer,
-                    children: (0, s.jsx)(o.zx, {
-                        color: o.zx.Colors.PRIMARY,
+                    className: p.buttonContainer,
+                    children: (0, s.jsx)(u.zx, {
+                        color: u.zx.Colors.PRIMARY,
                         onClick: () => {
-                            x(''), i(d.n.START);
+                            O(''), i(h.n.START);
                         },
-                        children: h.Z.Messages.ACCOUNT_REVERT_NEW_PASSWORD_BACK_BUTTON
+                        children: g.Z.Messages.ACCOUNT_REVERT_NEW_PASSWORD_BACK_BUTTON
                     })
                 })
             ]
