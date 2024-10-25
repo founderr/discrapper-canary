a2b13c6600968bef7acef4c11579c24f2289465f
DiscrapperManager <admin@minerpl.xyz>
2024-10-25 15:09:35 +0000

    canaryRelease: 339077
    Main scripts: 3
    Lazy Loaded Scripts: 889
    Chunks:
     * Main Files: 11268
     * Scripts: 10155
    Strings: 19385
    Routes: 687
    Experiments Count: 392

--- chunks/33194.js
+++ chunks/33194.js
@@ -2,15 +2,30 @@ n.d(t, {
     Iu: function () {
         return i;
     },
-    g6: function () {
+    OS: function () {
         return a;
+    },
+    g6: function () {
+        return o;
+    },
+    v3: function () {
+        return s;
     }
 });
-let r = (0, n(903797).Z)((e) => ({ channelDismissTimestamps: {} }));
+let r = (0, n(903797).Z)((e) => ({
+    channelDismissTimestamps: {},
+    queuedWarning: !1
+}));
 function i(e) {
     return r.getState().channelDismissTimestamps[e];
 }
-function a(e) {
+function a() {
+    return r.getState().queuedWarning;
+}
+function s() {
+    r.setState({ queuedWarning: !1 });
+}
+function o(e) {
     r.setState((t) => ({
         channelDismissTimestamps: {
             ...t.channelDismissTimestamps,
--- chunks/414509.js
+++ chunks/414509.js
@@ -6,45 +6,52 @@ n.d(t, {
     n(47120);
 var r = n(147913),
     i = n(592125),
-    a = n(699516),
-    s = n(944486),
-    o = n(979651),
-    l = n(70956),
-    u = n(280006),
-    c = n(33194),
-    d = n(807031),
-    _ = n(189275),
-    E = n(451092);
-let f = 1 * l.Z.Millis.HOUR;
-function h(e) {
+    a = n(19780),
+    s = n(699516),
+    o = n(70956),
+    l = n(280006),
+    u = n(33194);
+n(452369);
+var c = n(807031),
+    d = n(189275),
+    _ = n(451092);
+n(334431);
+let E = 1 * o.Z.Millis.HOUR;
+function f(e) {
     let { channelId: t } = e;
     if (null == t) return;
     let n = i.Z.getChannel(t);
     if (null != n && n.isGroupDM()) {
-        let e = n.recipients.filter((e) => a.Z.isBlocked(e));
+        let e = n.recipients.filter((e) => s.Z.isBlocked(e));
         e.length > 0 &&
-            u.E.getCurrentConfig({ location: 'channel_select' }, { autoTrackExposure: !0 }).showAsModal &&
+            l.E.getCurrentConfig({ location: 'channel_select' }, { autoTrackExposure: !0 }).showAsModal &&
             !n.blockedUserWarningDismissed &&
-            (0, _.O)({
+            (0, d.O)({
                 channelId: t,
                 blockedUserIds: e
             });
     }
 }
+function h(e) {
+    let { state: t } = e;
+}
 function p(e) {
-    return (0, c.Iu)(e) > Date.now() - f;
+    return (0, u.Iu)(e) > Date.now() - E;
 }
 class I extends r.Z {
     handleBlockedUserVoiceChannelJoin(e, t) {
-        let n = s.Z.getVoiceChannelId();
-        if (!(e !== n || !o.Z.isInChannel(n) || p(n))) (0, d.wC)({ location: 'warning_manager' }) && (0, E.H)(n, t);
+        let n = a.Z.getChannelId();
+        if (e === n && null != i.Z.getChannel(e)) (0, c.wC)({ location: 'warning_manager' }) && !p(e) && (0, _.H)(n, t);
     }
     constructor(...e) {
         var t, n, r;
         super(...e),
             (t = this),
             (n = 'actions'),
-            (r = { CHANNEL_SELECT: h }),
+            (r = {
+                CHANNEL_SELECT: f,
+                APP_STATE_UPDATE: h
+            }),
             n in t
                 ? Object.defineProperty(t, n, {
                       value: r,
