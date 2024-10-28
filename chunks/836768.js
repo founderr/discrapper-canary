a2b92daa33c409c0135774ff99ae30a1dc8bbb38
DiscrapperManager <admin@minerpl.xyz>
2024-10-28 20:54:22 +0000

    canaryRelease: 339652
    Main scripts: 3
    Lazy Loaded Scripts: 885
    Chunks:
     * Main Files: 11438
     * Scripts: 10154
    Strings: 19368
    Routes: 686
    Experiments Count: 389

--- chunks/836768.js
+++ chunks/836768.js
@@ -1,64 +1,12 @@
 n.d(t, {
-    OA: function () {
-        return E;
-    },
-    Sx: function () {
-        return _;
-    },
-    qe: function () {
-        return f;
-    },
-    uf: function () {
-        return h;
+    u: function () {
+        return s;
     }
 });
-var r = n(192379),
-    i = n(903797),
-    a = n(60071),
-    s = n(782690),
-    o = n(731965),
-    l = n(433517);
-let u = { selectedTab: n(49898).F$.SERVERS },
-    c = (0, i.Z)(
-        (0, a.tJ)(() => u, {
-            name: 'global-discovery-ui-store',
-            getStorage: () => ({
-                getItem: (e) => {
-                    var t;
-                    return null !== (t = l.K.get(e)) && void 0 !== t ? t : null;
-                },
-                setItem: (e, t) => l.K.set(e, t),
-                removeItem: (e) => l.K.remove(e)
-            }),
-            partialize: (e) => ({ selectedTab: e.selectedTab })
-        })
-    );
-function d(e) {
-    return (t) => t[e];
+var r = n(972959);
+let i = { selectedTab: n(49898).F$.SERVERS },
+    a = (0, r.H)(() => i);
+function s() {
+    a.setState(i);
 }
-let _ = (e) => {
-    (0, o.j)(() => c.setState(e));
-};
-function E() {
-    var e;
-    return (
-        (e = 'selectedTab'),
-        c(
-            r.useMemo(() => {
-                var t;
-                return (t = e), (e) => e[t];
-            }, [e]),
-            s.Z
-        )
-    );
-}
-function f() {
-    return (function (e) {
-        var t;
-        let n = ((t = e), (e) => e[t]);
-        return n(c.getState());
-    })('selectedTab');
-}
-function h() {
-    _(u);
-}
+t.Z = a;
