7fac458e74eed1de0ca5351f6b5f5f8c115a27b8
DiscrapperManager <admin@minerpl.xyz>
2024-11-05 18:29:28 +0000

    canaryRelease: 341775
    Main scripts: 3
    Lazy Loaded Scripts: 892
    Chunks:
     * Main Files: 11586
     * Scripts: 10212
    Strings: 0
    Routes: 688
    Experiments Count: 394

--- chunks/688707.js
+++ chunks/688707.js
@@ -1,17 +1,17 @@
 n(47120);
-var i = n(243814),
-    r = n(920456),
-    a = n(996106),
-    l = n(452426),
-    s = n(186901),
-    o = n(981631);
+var i = n(243814);
+n(920456);
+var r = n(996106),
+    a = n(452426),
+    l = n(186901),
+    s = n(981631);
 t.Z = {
-    [o.Etm.SET_USER_ACHIEVEMENT]: {
+    [s.Etm.SET_USER_ACHIEVEMENT]: {
         scope: {
-            [s.Gp.ANY]: [i.x.RPC, s.lH]
+            [l.Gp.ANY]: [i.x.RPC, l.lH]
         },
         validation: (e) =>
-            (0, l.Z)(e)
+            (0, a.Z)(e)
                 .required()
                 .keys({
                     achievement_id: e.string().min(2).max(128),
@@ -19,36 +19,21 @@ t.Z = {
                 }),
         handler(e) {
             let {
-                    socket: t,
-                    args: { achievement_id: n, percent_complete: i }
-                } = e,
-                l = t.application.id;
-            if (null == l) throw new a.Z({ errorCode: o.lTL.INVALID_COMMAND }, 'No application.');
-            return r.Z.setUserAchievement(l, n, i)
-                .catch((e) => {
-                    let { status: t, body: n } = e;
-                    switch (t) {
-                        case 429:
-                            throw new a.Z({ errorCode: o.lTL.RATE_LIMITED }, 'You are being rate limited.');
-                        case 403:
-                            if (null != n && n.code === o.evJ.USER_NOT_AUTHORIZED_FOR_APPLICATION) throw new a.Z({ errorCode: o.lTL.UNAUTHORIZED_FOR_ACHIEVEMENT }, 'Not authorized to update this achievement.');
-                            throw new a.Z({ errorCode: o.lTL.UNKNOWN_ERROR }, 'Unknown error from API. status='.concat(t, '.'));
-                        default:
-                            throw new a.Z({ errorCode: o.lTL.UNKNOWN_ERROR }, 'Unknown error from API. status='.concat(t, '.'));
-                    }
-                })
-                .then(() => null);
+                socket: t,
+                args: { achievement_id: n, percent_complete: i }
+            } = e;
+            if (null == t.application.id) throw new r.Z({ errorCode: s.lTL.INVALID_COMMAND }, 'No application.');
+            return null;
         }
     },
-    [o.Etm.GET_USER_ACHIEVEMENTS]: {
+    [s.Etm.GET_USER_ACHIEVEMENTS]: {
         scope: {
-            [s.Gp.ANY]: [i.x.RPC, s.lH]
+            [l.Gp.ANY]: [i.x.RPC, l.lH]
         },
         handler(e) {
-            let { socket: t } = e,
-                n = t.application.id;
-            if (null == n) throw new a.Z({ errorCode: o.lTL.INVALID_COMMAND }, 'No application.');
-            return r.Z.fetchForApplication(n).then((e) => e || []);
+            let { socket: t } = e;
+            if (null == t.application.id) throw new r.Z({ errorCode: s.lTL.INVALID_COMMAND }, 'No application.');
+            return Promise.resolve([]);
         }
     }
 };
