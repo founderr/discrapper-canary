15d396430c2141942b992693a22bc52e5f40bb79
DiscrapperManager <admin@minerpl.xyz>
2024-11-05 17:10:14 +0000

    canaryRelease: 341731
    Main scripts: 3
    Lazy Loaded Scripts: 892
    Chunks:
     * Main Files: 11585
     * Scripts: 10211
    Strings: 0
    Routes: 688
    Experiments Count: 393

--- chunks/126743.js
+++ chunks/126743.js
@@ -1,9 +1,10 @@
-t.exports = {
+e.exports = {
     header: 'header_a70452',
     headerImage: 'headerImage_a70452',
     title: 'title_a70452',
     content: 'content_a70452',
     buttonGroup: 'buttonGroup_a70452',
+    leave: 'leave_a70452',
     infoGroup: 'infoGroup_a70452',
     infoRow: 'infoRow_a70452'
 };
--- chunks/444812.js
+++ chunks/444812.js
@@ -1,1 +1,1 @@
-t.exports = '/assets/8a9a3fad79236820ed1b.svg';
+e.exports = '/assets/8a9a3fad79236820ed1b.svg';
