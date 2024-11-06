33624fd8e2768c507e24f14879395af867c86f8c
DiscrapperManager <admin@minerpl.xyz>
2024-11-06 17:51:05 +0000

    canaryRelease: 342178
    Main scripts: 3
    Lazy Loaded Scripts: 896
    Chunks:
     * Main Files: 11602
     * Scripts: 10222
    Strings: 0
    Routes: 688
    Experiments Count: 394

--- chunks/64621.js
+++ chunks/64621.js
@@ -1,6 +1,6 @@
 n.d(t, {
     Z: function () {
-        return I;
+        return S;
     }
 }),
     n(47120);
@@ -10,99 +10,104 @@ var r = n(200651),
     s = n(704215),
     o = n(481060),
     l = n(980591),
-    u = n(243778),
-    c = n(970731),
-    d = n(594174),
-    f = n(718107),
-    _ = n(652853),
-    h = n(228168),
-    p = n(388032),
-    m = n(671511),
-    g = n(895982),
-    E = n(685741),
-    v = n(20818);
-function I(e) {
+    u = n(381586),
+    c = n(243778),
+    d = n(970731),
+    f = n(594174),
+    _ = n(718107),
+    h = n(652853),
+    p = n(228168),
+    m = n(388032),
+    g = n(671511),
+    E = n(895982),
+    v = n(685741),
+    I = n(20818);
+function S(e) {
     var t, n;
-    let { sourceType: I, children: S, user: T } = e,
-        { profileType: b } = (0, _.z)(),
-        { avatarReactReplyEnabled: y, statusReactReplyEnabled: A } = (0, f.t)({ location: 'UserProfilePopoutInteractionCoachmark' }),
-        N = (0, a.e7)([d.default], () => {
+    let { sourceType: S, children: T, user: b } = e,
+        { profileType: y } = (0, h.z)(),
+        { avatarReactReplyEnabled: A, statusReactReplyEnabled: N } = (0, _.t)({ location: 'UserProfilePopoutInteractionCoachmark' }),
+        C = (0, a.e7)([f.default], () => {
             var e;
-            return (null === (e = d.default.getCurrentUser()) || void 0 === e ? void 0 : e.id) === T.id;
+            return (null === (e = f.default.getCurrentUser()) || void 0 === e ? void 0 : e.id) === b.id;
         }),
-        C = I === h.n_.AVATAR && y,
-        R = I === h.n_.STATUS && !y && A,
-        O = !N && b === h.y0.BITE_SIZE && (C || R),
-        [D, L] = i.useState(!1);
-    (0, l.Z)(() => L(!0), O ? 200 : null);
-    let x = D ? [s.z.USER_PROFILE_POPOUT_INTERACTION_COACHMARK] : [],
-        [w, M] = (0, u.US)(x);
+        R = S === p.n_.AVATAR && A,
+        O = S === p.n_.STATUS && !A && N,
+        D = !C && y === p.y0.BITE_SIZE && (R || O),
+        [L, x] = i.useState(!1);
+    (0, l.Z)(() => x(!0), D ? 200 : null);
     let {
-        asset: P,
-        assetClassName: k,
-        className: U,
-        content: G,
-        header: B
-    } = ((t = A),
-    (n = y),
-    t && n
-        ? {
-              asset: E,
-              assetClassName: m.profileImage,
-              className: m.profileCoachmark,
-              content: p.intl.string(p.t['yzj+dn']),
-              header: p.intl.string(p.t['R/RaKy'])
-          }
-        : t
-          ? {
-                asset: v,
-                assetClassName: void 0,
-                className: m.statusCoachmark,
-                content: p.intl.string(p.t.MYYTWV),
-                header: p.intl.string(p.t['X/My/v'])
-            }
-          : {
-                asset: g,
-                assetClassName: m.avatarImage,
-                className: m.avatarCoachmark,
-                content: p.intl.string(p.t['65191t']),
-                header: p.intl.string(p.t['3FagAQ'])
-            });
+            asset: w,
+            assetClassName: M,
+            className: P,
+            content: k,
+            header: U,
+            dismissibleContentType: G
+        } = ((t = N),
+        (n = A),
+        t && n
+            ? {
+                  asset: v,
+                  assetClassName: g.profileImage,
+                  className: g.profileCoachmark,
+                  content: m.intl.string(m.t['yzj+dn']),
+                  header: m.intl.string(m.t['R/RaKy']),
+                  dismissibleContentType: s.z.USER_PROFILE_POPOUT_INTERACTION_COACHMARK
+              }
+            : t
+              ? {
+                    asset: I,
+                    assetClassName: void 0,
+                    className: g.statusCoachmark,
+                    content: m.intl.string(m.t.MYYTWV),
+                    header: m.intl.string(m.t['X/My/v']),
+                    dismissibleContentType: s.z.USER_PROFILE_POPOUT_INTERACTION_COACHMARK_STATUS
+                }
+              : {
+                    asset: E,
+                    assetClassName: g.avatarImage,
+                    className: g.avatarCoachmark,
+                    content: m.intl.string(m.t['65191t']),
+                    header: m.intl.string(m.t['3FagAQ']),
+                    dismissibleContentType: s.z.USER_PROFILE_POPOUT_INTERACTION_COACHMARK_AVATAR
+                }),
+        B = (0, u.N)(s.z.USER_PROFILE_POPOUT_INTERACTION_COACHMARK),
+        [Z, F] = (0, c.US)(L && !B ? [G] : []);
     return (0, r.jsx)(o.Popout, {
         align: 'center',
-        position: I === h.n_.AVATAR ? 'left' : 'right',
-        shouldShow: null != w,
-        spacing: I === h.n_.AVATAR ? 30 : 16,
+        position: S === p.n_.AVATAR ? 'left' : 'right',
+        shouldShow: null != Z,
+        spacing: S === p.n_.AVATAR ? 30 : 16,
         renderPopout: (e) => {
             let { position: t, closePopout: n, setPopoutRef: i } = e;
             return (0, r.jsx)(o.Dialog, {
                 ref: i,
-                children: (0, r.jsx)(c.ZP, {
-                    className: U,
+                children: (0, r.jsx)(d.ZP, {
+                    className: P,
                     asset: (0, r.jsx)('img', {
                         alt: '',
-                        src: P,
-                        className: k
+                        src: w,
+                        className: M
                     }),
-                    buttonCTA: p.intl.string(p.t['+IrDzM']),
-                    buttonLayout: c._F.STACKED,
+                    buttonCTA: m.intl.string(m.t['+IrDzM']),
+                    buttonLayout: d._F.STACKED,
                     buttonProps: {
                         color: o.ButtonColors.BRAND_INVERTED,
                         size: o.Button.Sizes.SMALL
                     },
-                    caretPosition: 'right' === t ? c.DF.LEFT_CENTER : c.DF.RIGHT_CENTER,
+                    caretPosition: 'right' === t ? d.DF.LEFT_CENTER : d.DF.RIGHT_CENTER,
                     content: (0, r.jsx)(o.Text, {
                         variant: 'text-xs/normal',
                         color: 'always-white',
-                        children: G
+                        children: k
                     }),
-                    header: B,
-                    headerClassName: m.tooltipHeader,
-                    markAsDismissed: M,
+                    header: U,
+                    headerClassName: g.tooltipHeader,
+                    markAsDismissed: F,
                     onClick: n
                 })
             });
         },
-        children: () => S
+        children: () => T
     });
 }
