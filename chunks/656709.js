a2d890db11200bffc06389a5dd6147d14e85c09f
DiscrapperManager <admin@minerpl.xyz>
2024-11-06 17:18:16 +0000

    canaryRelease: 342165
    Main scripts: 3
    Lazy Loaded Scripts: 896
    Chunks:
     * Main Files: 11601
     * Scripts: 10223
    Strings: 0
    Routes: 688
    Experiments Count: 394

--- chunks/656709.js
+++ chunks/656709.js
@@ -6,7 +6,10 @@ n.d(e, {
 var i = n(904245),
     l = n(957730);
 function r(t) {
-    let { channel: e, content: n, entry: r, whenReady: o } = t,
-        a = l.ZP.parse(e, n);
-    return i.Z.sendMessage(e.id, a, o, { contentInventoryEntry: { unverified_content: r } });
+    let { channel: e, content: n, entry: r, whenReady: o, doNotNotifyOnError: a } = t,
+        s = l.ZP.parse(e, n);
+    return i.Z.sendMessage(e.id, s, o, {
+        contentInventoryEntry: { unverified_content: r },
+        doNotNotifyOnError: a
+    });
 }
