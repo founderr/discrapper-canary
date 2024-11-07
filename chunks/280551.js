5bfd964edaa5838c93ffc1d1af8fe1a9c7f18a84
DiscrapperManager <admin@minerpl.xyz>
2024-11-07 21:07:58 +0000

    canaryRelease: 342782
    Main scripts: 3
    Lazy Loaded Scripts: 899
    Chunks:
     * Main Files: 11637
     * Scripts: 10223
    Strings: 0
    Routes: 688
    Experiments Count: 400

--- chunks/280551.js
+++ chunks/280551.js
@@ -4,4 +4,10 @@ n.d(t, {
     }
 });
 var r = n(998502);
-let i = null != window.DiscordNative ? r.ZP.getDiscordUtils().createDateFormatter : void 0;
+let i =
+    null != window.DiscordNative
+        ? function (e, t) {
+              var n, i;
+              return null === (i = r.ZP.getDiscordUtils()) || void 0 === i ? void 0 : null === (n = i.createDateFormatter) || void 0 === n ? void 0 : n.call(i, e, t);
+          }
+        : void 0;
