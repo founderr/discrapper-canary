6fd0999eaff9d6d0487699f67ef11165b2624dbc
DiscrapperManager <admin@minerpl.xyz>
2024-11-07 05:35:11 +0000

    canaryRelease: 342569
    Main scripts: 3
    Lazy Loaded Scripts: 899
    Chunks:
     * Main Files: 11624
     * Scripts: 10224
    Strings: 0
    Routes: 688
    Experiments Count: 397

--- chunks/549616.js
+++ chunks/549616.js
@@ -22,9 +22,9 @@ function a(e, t, n) {
 }
 class s extends i.Z {
     static fromServer(e) {
-        let { products: t, logo: n, hero_ranking: i, pdp_bg: a, mobile_bg: o, success_modal_bg: l, mobile_banner: u, hero_banner: c, featured_block: d, ...f } = e;
+        let { products: t, logo: n, hero_ranking: i, pdp_bg: a, mobile_bg: o, success_modal_bg: l, mobile_banner: u, hero_banner: c, featured_block: d, hero_logo: f, ..._ } = e;
         return new s({
-            ...super.fromServer(f),
+            ...super.fromServer(_),
             products: t.reduce((e, t) => {
                 let n = r.Z.fromServer(t);
                 return 0 === n.items.length ? e : (e.push(n), e);
@@ -36,10 +36,11 @@ class s extends i.Z {
             successModalBg: l,
             mobileBanner: u,
             heroBanner: c,
-            featuredBlock: d
+            featuredBlock: d,
+            heroLogo: f
         });
     }
     constructor(e) {
-        super(e), a(this, 'products', void 0), a(this, 'logo', void 0), a(this, 'heroRanking', void 0), a(this, 'pdpBg', void 0), a(this, 'mobileBg', void 0), a(this, 'successModalBg', void 0), a(this, 'mobileBanner', void 0), a(this, 'heroBanner', void 0), a(this, 'featuredBlock', void 0), (this.products = e.products), (this.logo = e.logo), (this.heroRanking = e.heroRanking), (this.pdpBg = e.pdpBg), (this.mobileBg = e.mobileBg), (this.successModalBg = e.successModalBg), (this.mobileBanner = e.mobileBanner), (this.heroBanner = e.heroBanner), (this.featuredBlock = e.featuredBlock);
+        super(e), a(this, 'products', void 0), a(this, 'logo', void 0), a(this, 'heroRanking', void 0), a(this, 'pdpBg', void 0), a(this, 'mobileBg', void 0), a(this, 'successModalBg', void 0), a(this, 'mobileBanner', void 0), a(this, 'heroBanner', void 0), a(this, 'featuredBlock', void 0), a(this, 'heroLogo', void 0), (this.products = e.products), (this.logo = e.logo), (this.heroRanking = e.heroRanking), (this.pdpBg = e.pdpBg), (this.mobileBg = e.mobileBg), (this.successModalBg = e.successModalBg), (this.mobileBanner = e.mobileBanner), (this.heroBanner = e.heroBanner), (this.featuredBlock = e.featuredBlock), (this.heroLogo = e.heroLogo);
     }
 }
