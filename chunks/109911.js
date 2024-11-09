327d2aec5e414af02520e57069bce065ce333694
DiscrapperManager <admin@minerpl.xyz>
2024-11-09 01:20:36 +0000

    canaryRelease: 343301
    Main scripts: 3
    Lazy Loaded Scripts: 899
    Chunks:
     * Main Files: 11659
     * Scripts: 10222
    Strings: 0
    Routes: 686
    Experiments Count: 402

--- chunks/109911.js
+++ chunks/109911.js
@@ -1,16 +1,14 @@
 n.d(t, {
     Ji: function () {
-        return l;
+        return s;
     },
     QX: function () {
-        return u;
+        return o;
     }
 });
-var r,
-    i,
-    a = n(818083);
+var r = n(818083);
 n(171393);
-let s = (0, a.B)({
+let i = (0, r.B)({
         kind: 'user',
         id: '2024-07_icymi',
         label: 'In-case-you-missed-it tab',
@@ -23,7 +21,7 @@ let s = (0, a.B)({
             }
         ]
     }),
-    o = (0, a.B)({
+    a = (0, r.B)({
         kind: 'user',
         id: '2024-10_icymi_sorting_fe',
         label: 'FE experiment for sorting on ICYMI',
@@ -36,16 +34,16 @@ let s = (0, a.B)({
             }
         ]
     });
-function l(e) {
+function s(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1],
-        n = s.getCurrentConfig(
+        n = i.getCurrentConfig(
             { location: e },
             {
                 disable: !0,
                 autoTrackExposure: t
             }
         ).enabled,
-        r = o.getCurrentConfig(
+        r = a.getCurrentConfig(
             { location: e },
             {
                 disable: !0,
@@ -54,36 +52,20 @@ function l(e) {
         ).enabled;
     return n || r;
 }
-((r = i || (i = {})).BUTTON = 'notifications_button'),
-    (r.SEGMENTED_CONTROL = 'notifications_segmented_control'),
-    (0, a.B)({
-        kind: 'user',
-        id: '2024-10_merge_notifications_tab',
-        label: 'Merge notifications tab',
-        defaultConfig: {
-            enabled: !1,
-            variant: 'notifications_button'
-        },
-        treatments: [
-            {
-                id: 1,
-                label: 'Merge notifications tab - keep as button on the top right of ICYMI tab',
-                config: {
-                    enabled: !0,
-                    variant: 'notifications_button'
-                }
-            },
-            {
-                id: 2,
-                label: 'Merge notifications tab - keep as segmented section in ICYMI tab',
-                config: {
-                    enabled: !0,
-                    variant: 'notifications_segmented_control'
-                }
-            }
-        ]
-    }),
-    (0, a.B)({
+(0, r.B)({
+    kind: 'user',
+    id: '2024-10_merge_notifications_tab',
+    label: 'Merge notifications tab',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Merge notifications tab - keep as button on the top right of ICYMI tab',
+            config: { enabled: !0 }
+        }
+    ]
+}),
+    (0, r.B)({
         kind: 'user',
         id: '2024-07_icymi_negative_items',
         label: 'icymi negative content (debugging only)',
@@ -96,7 +78,7 @@ function l(e) {
             }
         ]
     });
-let u = (0, a.B)({
+let o = (0, r.B)({
     kind: 'user',
     id: '2024-10_icymi_legacy',
     label: 'Legacy ICYMI experiment',
