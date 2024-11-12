33cc254304dca317f8fde266e3349b83de3c6360
DiscrapperManager <admin@minerpl.xyz>
2024-11-12 23:46:25 +0000

    canaryRelease: 343737
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11645
     * Scripts: 10236
    Strings: 0
    Routes: 686
    Experiments Count: 402

--- chunks/644916.js
+++ chunks/644916.js
@@ -7,9 +7,16 @@ var r = n(115434),
     i = n(704215),
     a = n(874703);
 function s(e) {
-    if (e === i.z.COLLECTIBLES_SHOP_ENTRY_MARKETING) {
-        var t, n;
-        return null !== (n = null === (t = a.Z.getMarketingBySurface(r.K.DESKTOP_SHOP_BUTTON)) || void 0 === t ? void 0 : t.version) && void 0 !== n ? n : 0;
+    switch (e) {
+        case i.z.COLLECTIBLES_SHOP_ENTRY_MARKETING:
+            var t, n;
+            return null !== (n = null === (t = a.Z.getMarketingBySurface(r.K.DESKTOP_SHOP_BUTTON)) || void 0 === t ? void 0 : t.version) && void 0 !== n ? n : 0;
+        case i.z.GIFTING_PROMOTION_ICON:
+        case i.z.GIFTING_PROMOTION_DESKTOP_FIRST_TIME_COACHMARK:
+        case i.z.GIFTING_PROMOTION_MOBILE_FIRST_TIME_HALFSHEET:
+        case i.z.GIFTING_PROMOTION_REMINDER:
+            return 1;
+        default:
+            return 0;
     }
-    return 0;
 }
