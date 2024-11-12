f914283d58e45e10f83a17951d2d5522753c5e7a
DiscrapperManager <admin@minerpl.xyz>
2024-11-12 17:39:41 +0000

    canaryRelease: 343474
    Main scripts: 3
    Lazy Loaded Scripts: 898
    Chunks:
     * Main Files: 11663
     * Scripts: 10220
    Strings: 0
    Routes: 686
    Experiments Count: 402

--- chunks/900735.js
+++ chunks/900735.js
@@ -14,14 +14,12 @@ var l = n(699516),
 let s = i.Z.Millis.WEEK,
     c = i.Z.Millis.DAYS_30,
     u = () => {
-        if (Math.random() >= 0.1) return !1;
+        if (!(0, o.nZ)('block_user_feedback_utils') || Math.random() >= 0.1) return !1;
         let e = l.Z.getSinces();
-        return (
-            !!Object.keys(e).some((t) => {
-                let n = Date.now() - Date.parse(e[t]);
-                return l.Z.isBlocked(t) && n > s && n < c;
-            }) && (0, o.nZ)('block_user_feedback_utils')
-        );
+        return Object.keys(e).some((t) => {
+            let n = Date.now() - Date.parse(e[t]);
+            return l.Z.isBlocked(t) && n > s && n < c;
+        });
     },
     d = (e, t, n, l) => {
         a.default.track(r.rMx.BLOCK_USER_FEEDBACK_SUBMITTED, {
