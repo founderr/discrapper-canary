1a2a53bdf5ddfaa8af909779dec3b7ba4c59859b
DiscrapperManager <admin@minerpl.xyz>
2024-11-13 01:01:15 +0000

    canaryRelease: 343826
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11645
     * Scripts: 10239
    Strings: 0
    Routes: 686
    Experiments Count: 402

--- chunks/823941.js
+++ chunks/823941.js
@@ -1,57 +1,60 @@
 n.d(t, {
     Z: function () {
-        return N;
+        return y;
     },
     n: function () {
-        return S;
+        return N;
     }
-});
+}),
+    n(627341);
 var r = n(200651),
     a = n(192379),
-    s = n(120356),
-    i = n.n(s),
-    l = n(180650),
-    o = n(399606),
-    c = n(481060),
-    d = n(906732),
-    u = n(963249),
-    m = n(594174),
-    h = n(754347),
-    p = n(74538),
-    f = n(884697),
-    g = n(624377),
-    C = n(141011),
-    b = n(813083),
-    x = n(67938),
-    v = n(372654),
-    _ = n(215023),
-    k = n(474936),
-    j = n(388032),
-    E = n(462972),
-    I = n(275001);
-let S = (0, f.IC)(96),
-    T = (e) => {
+    i = n(120356),
+    s = n.n(i),
+    l = n(278074),
+    o = n(180650),
+    c = n(399606),
+    d = n(481060),
+    u = n(607070),
+    m = n(906732),
+    h = n(963249),
+    p = n(594174),
+    f = n(754347),
+    g = n(74538),
+    C = n(884697),
+    b = n(624377),
+    x = n(141011),
+    v = n(813083),
+    _ = n(67938),
+    k = n(372654),
+    j = n(215023),
+    E = n(474936),
+    I = n(388032),
+    S = n(462972),
+    T = n(275001);
+let N = (0, C.IC)(96),
+    B = (e) => {
         let { category: t } = e,
-            { analyticsLocations: n } = (0, d.ZP)(),
-            s = a.useRef(null),
-            i = (0, o.e7)([m.default], () => m.default.getCurrentUser());
-        return p.ZP.canUseCollectibles(i)
+            { analyticsLocations: n } = (0, m.ZP)(),
+            i = a.useRef(null),
+            s = (0, c.e7)([p.default], () => p.default.getCurrentUser());
+        return g.ZP.canUseCollectibles(s)
             ? (0, r.jsx)(r.Fragment, { children: t.summary })
             : (0, r.jsx)(r.Fragment, {
-                  children: j.intl.format(j.t['9hafRk'], {
+                  children: I.intl.format(I.t['9hafRk'], {
                       getPremium: (e) =>
-                          (0, r.jsx)(c.Clickable, {
-                              innerRef: s,
-                              className: E.getPremiumHook,
+                          (0, r.jsx)(d.Clickable, {
+                              innerRef: i,
+                              className: S.getPremiumHook,
                               onClick: () => {
-                                  (0, u.Z)({
-                                      subscriptionTier: k.Si.TIER_2,
+                                  (0, h.Z)({
+                                      subscriptionTier: E.Si.TIER_2,
                                       analyticsLocations: n,
-                                      returnRef: s
+                                      returnRef: i
                                   });
                               },
                               tag: 'span',
-                              children: (0, r.jsx)(c.Text, {
+                              children: (0, r.jsx)(d.Text, {
                                   variant: 'text-md/normal',
                                   color: 'always-white',
                                   tag: 'span',
@@ -61,64 +64,69 @@ let S = (0, f.IC)(96),
                   })
               });
     };
-function N(e) {
-    var t, n, a, s;
-    let { category: o, className: d, hideLimitedTimeBadge: u = !1 } = e,
-        { backgroundColors: m } = (0, g.Z)(o.styles),
-        p = !!(null === (t = _.Ve[o.skuId]) || void 0 === t ? void 0 : t.showDarkBannerText);
-    return o.skuId === l.T.CHANCE
-        ? (0, r.jsx)(x.O, { category: o })
-        : (0, r.jsxs)(C.Z, {
-              asset: o.banner,
-              className: i()(E.shopBanner, d),
-              style:
-                  null != m
-                      ? {
-                            background: ''.concat((0, v.nH)(m), ' border-box border-box'),
-                            outlineColor: m.border.toHslString()
-                        }
-                      : void 0,
-              children: [
-                  o.skuId === l.T.BAND &&
-                      (0, r.jsx)('img', {
-                          src: I,
-                          alt: '',
-                          className: E.animationAsset
-                      }),
-                  (null === (n = _.Ve[o.skuId]) || void 0 === n ? void 0 : n.addAttributionLogo)
-                      ? (0, r.jsxs)('div', {
-                            className: E.discordLogo,
-                            children: [
-                                (0, r.jsx)(c.ClydeIcon, {
-                                    size: 'custom',
-                                    width: 28,
-                                    height: 28,
-                                    color: 'currentColor',
-                                    className: E.discordIcon
-                                }),
-                                (0, r.jsx)(h.Z, { className: E.discordWordmark })
-                            ]
-                        })
-                      : (0, r.jsx)(c.Spacer, { size: 28 }),
-                  (null === (a = _.Ve[o.skuId]) || void 0 === a ? void 0 : a.addLogo)
-                      ? (0, r.jsx)('img', {
-                            className: E.categoryLogo,
-                            src: (0, f.uV)(o.logo, { size: S }),
-                            alt: o.name,
-                            style: { maxWidth: null === (s = _.Ve[o.skuId]) || void 0 === s ? void 0 : s.logoMaxWidth }
-                        })
-                      : (0, r.jsx)(c.Spacer, { size: 96 }),
-                  (0, r.jsx)(c.Text, {
-                      className: i()(E.summary, { [E.blackSummary]: p }),
-                      variant: 'text-md/normal',
-                      children: o.skuId === l.T.DISXCORE ? (0, r.jsx)(T, { category: o }) : o.summary
-                  }),
-                  !u &&
-                      (0, r.jsx)(b.Z, {
-                          category: o,
-                          className: E.limitedTimeBadge,
-                          display: 'banner'
-                      })
-              ]
-          });
+function y(e) {
+    var t, n, a, i;
+    let { category: m, className: h, hideLimitedTimeBadge: p = !1 } = e,
+        { backgroundColors: g } = (0, b.Z)(m.styles),
+        E = !!(null === (t = j.Ve[m.skuId]) || void 0 === t ? void 0 : t.showDarkBannerText),
+        I = (0, c.e7)([u.Z], () => u.Z.useReducedMotion);
+    if (m.skuId === o.T.CHANCE) return (0, r.jsx)(_.O, { category: m });
+    let y = (0, l.EQ)(m.skuId)
+        .with(o.T.BAND, () => T)
+        .with(o.T.WARRIOR, () => 'https://cdn.discordapp.com/assets/content/db9fb34f490b777a6e9712b129f9e23ad930595d2df73ca85d2b54f247806e01.png')
+        .otherwise(() => void 0);
+    return (0, r.jsxs)(x.Z, {
+        asset: m.banner,
+        className: s()(S.shopBanner, h),
+        style:
+            null != g
+                ? {
+                      background: ''.concat((0, k.nH)(g), ' border-box border-box'),
+                      outlineColor: g.border.toHslString()
+                  }
+                : void 0,
+        children: [
+            !I &&
+                void 0 !== y &&
+                (0, r.jsx)('img', {
+                    src: y,
+                    alt: '',
+                    className: S.animationAsset
+                }),
+            (null === (n = j.Ve[m.skuId]) || void 0 === n ? void 0 : n.addAttributionLogo)
+                ? (0, r.jsxs)('div', {
+                      className: S.discordLogo,
+                      children: [
+                          (0, r.jsx)(d.ClydeIcon, {
+                              size: 'custom',
+                              width: 28,
+                              height: 28,
+                              color: 'currentColor',
+                              className: S.discordIcon
+                          }),
+                          (0, r.jsx)(f.Z, { className: S.discordWordmark })
+                      ]
+                  })
+                : (0, r.jsx)(d.Spacer, { size: 28 }),
+            (null === (a = j.Ve[m.skuId]) || void 0 === a ? void 0 : a.addLogo)
+                ? (0, r.jsx)('img', {
+                      className: S.categoryLogo,
+                      src: (0, C.uV)(m.logo, { size: N }),
+                      alt: m.name,
+                      style: { maxWidth: null === (i = j.Ve[m.skuId]) || void 0 === i ? void 0 : i.logoMaxWidth }
+                  })
+                : (0, r.jsx)(d.Spacer, { size: 96 }),
+            (0, r.jsx)(d.Text, {
+                className: s()(S.summary, { [S.blackSummary]: E }),
+                variant: 'text-md/normal',
+                children: m.skuId === o.T.DISXCORE ? (0, r.jsx)(B, { category: m }) : m.summary
+            }),
+            !p &&
+                (0, r.jsx)(v.Z, {
+                    category: m,
+                    className: S.limitedTimeBadge,
+                    display: 'banner'
+                })
+        ]
+    });
 }
