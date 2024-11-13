ff0388c39b3aa6a3ddc773e3f36c84e84802b72d
DiscrapperManager <admin@minerpl.xyz>
2024-11-13 21:18:21 +0000

    canaryRelease: 344085
    Main scripts: 3
    Lazy Loaded Scripts: 902
    Chunks:
     * Main Files: 11628
     * Scripts: 10239
    Strings: 0
    Routes: 686
    Experiments Count: 402

--- chunks/244190.js
+++ chunks/244190.js
@@ -1,6 +1,7 @@
 e.exports = {
     galleryContainer: 'galleryContainer_b24655',
     gallery: 'gallery_b24655',
-    galleryItem: 'galleryItem_b24655',
-    selected: 'selected_b24655'
+    galleryItemOverlay: 'galleryItemOverlay_b24655',
+    selected: 'selected_b24655',
+    galleryItem: 'galleryItem_b24655'
 };
