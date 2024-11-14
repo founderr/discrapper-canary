7c61274abf65bc34ef32313f42919e623786e93d
DiscrapperManager <admin@minerpl.xyz>
2024-11-14 23:30:35 +0000

    canaryRelease: 344693
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11624
     * Scripts: 10207
    Strings: 0
    Routes: 687
    Experiments Count: 402

--- chunks/886253.js
+++ chunks/886253.js
@@ -1,104 +1,112 @@
 t.d(n, {
     A: function () {
-        return p;
+        return v;
     }
 });
 var o = t(200651);
 t(192379);
 var l = t(442837),
-    i = t(481060),
-    r = t(264043),
+    r = t(481060),
+    i = t(264043),
     s = t(812206),
-    a = t(572004),
-    c = t(601911),
-    d = t(504211),
-    u = t(533159),
-    m = t(981631),
-    x = t(388032),
-    h = t(213891),
-    f = t(566809);
-function p(e) {
-    let { appId: n, skuId: t, transitionState: p, onClose: v, onHeaderTitleClick: _, children: j, footer: I } = e,
+    a = t(352057),
+    c = t(572004),
+    d = t(601911),
+    u = t(504211),
+    m = t(533159),
+    x = t(981631),
+    f = t(388032),
+    p = t(213891),
+    h = t(566809);
+function v(e) {
+    let { appId: n, skuId: t, transitionState: v, onClose: _, onHeaderTitleClick: j, children: I, footer: g } = e,
+        k = (0, a.h)({
+            location: 'store_modal',
+            autoTrackExposure: !1
+        }),
         N = (0, l.e7)(
-            [s.Z, r.Z],
+            [s.Z, i.Z],
             () => {
                 var e;
-                return null !== (e = s.Z.getApplication(n)) && void 0 !== e ? e : r.Z.getApplicationRecord(n);
+                return null !== (e = s.Z.getApplication(n)) && void 0 !== e ? e : i.Z.getApplicationRecord(n);
             },
             [n]
         );
     if (null == N) return null;
-    let k = (0, c.y)(N, 25),
-        { termsOfServiceUrl: g, privacyPolicyUrl: C } = N;
-    return (0, o.jsxs)(i.ModalRoot, {
-        transitionState: p,
-        size: i.ModalSize.DYNAMIC,
-        className: f.modal,
+    let C = (0, d.y)(N, 25),
+        { termsOfServiceUrl: S, privacyPolicyUrl: b } = N;
+    return (0, o.jsxs)(r.ModalRoot, {
+        transitionState: v,
+        size: r.ModalSize.DYNAMIC,
+        className: h.modal,
         children: [
-            (0, o.jsxs)(i.ModalHeader, {
-                className: h.modalHeader,
+            (0, o.jsxs)(r.ModalHeader, {
+                className: p.modalHeader,
                 children: [
-                    (0, o.jsxs)(i.Clickable, {
-                        className: h.modalTitle,
-                        onClick: _,
+                    (0, o.jsxs)(r.Clickable, {
+                        className: p.modalTitle,
+                        onClick: j,
                         children: [
-                            null != k &&
+                            null != C &&
                                 (0, o.jsx)('img', {
-                                    src: k.href,
+                                    src: C.href,
                                     alt: '',
-                                    className: f.appIcon
+                                    className: h.appIcon
                                 }),
-                            (0, o.jsx)(i.Heading, {
+                            (0, o.jsx)(r.Heading, {
                                 variant: 'heading-md/semibold',
                                 children: N.name
                             })
                         ]
                     }),
-                    (0, o.jsx)(i.ModalCloseButton, {
-                        onClick: v,
-                        className: h.modalCloseBtn
+                    (0, o.jsx)(r.ModalCloseButton, {
+                        onClick: _,
+                        className: p.modalCloseBtn
                     })
                 ]
             }),
-            j,
-            (0, o.jsxs)(i.ModalFooter, {
-                className: f.footer,
+            I,
+            (0, o.jsxs)(r.ModalFooter, {
+                className: h.footer,
                 children: [
-                    null != g || null != C
-                        ? (0, o.jsx)(i.Text, {
+                    null != S || null != b
+                        ? (0, o.jsx)(r.Text, {
                               color: 'header-primary',
                               variant: 'text-sm/normal',
-                              children: (0, o.jsx)(u.Z, {
-                                  termsOfServiceUrl: g,
-                                  privacyPolicyUrl: C
+                              children: (0, o.jsx)(m.Z, {
+                                  termsOfServiceUrl: S,
+                                  privacyPolicyUrl: b
                               })
                           })
-                        : (0, o.jsx)(i.Text, {
+                        : (0, o.jsx)(r.Text, {
                               color: 'header-primary',
                               variant: 'text-xs/normal',
-                              children: x.intl.string(x.t['3ZY+0N'])
+                              children: f.intl.string(f.t['3ZY+0N'])
                           }),
                     (0, o.jsxs)('div', {
-                        className: f.footerButtons,
+                        className: h.footerButtons,
                         children: [
-                            a.wS &&
-                                (0, o.jsx)(i.Button, {
-                                    look: i.Button.Looks.FILLED,
-                                    size: i.Button.Sizes.ICON,
-                                    color: i.Button.Colors.PRIMARY,
-                                    'aria-label': x.intl.string(x.t.WqhZsr),
-                                    className: f.linkButton,
-                                    innerClassName: f.innerLinkButton,
+                            c.wS &&
+                                (0, o.jsx)(r.Button, {
+                                    look: r.Button.Looks.FILLED,
+                                    size: r.Button.Sizes.ICON,
+                                    color: r.Button.Colors.PRIMARY,
+                                    'aria-label': f.intl.string(f.t.WqhZsr),
+                                    className: h.linkButton,
+                                    innerClassName: h.innerLinkButton,
                                     onClick: () => {
-                                        let e = ''.concat(location.protocol, '//').concat(location.host).concat(m.Z5c.APPLICATION_DIRECTORY_PROFILE_STORE_SKU(n, t));
-                                        (0, a.JG)(e), (0, i.showToast)((0, i.createToast)(x.intl.string(x.t['L/PwZW']), i.ToastType.SUCCESS)), (0, d.X)(n, d.B.DETAILS_MODAL, t);
+                                        let e = ''
+                                            .concat(location.protocol, '//')
+                                            .concat(location.host)
+                                            .concat(k ? x.Z5c.GLOBAL_DISCOVERY_APPS_PROFILE_STORE_SKU(n, t) : x.Z5c.APPLICATION_DIRECTORY_PROFILE_STORE_SKU(n, t));
+                                        (0, c.JG)(e), (0, r.showToast)((0, r.createToast)(f.intl.string(f.t['L/PwZW']), r.ToastType.SUCCESS)), (0, u.X)(n, u.B.DETAILS_MODAL, t);
                                     },
-                                    children: (0, o.jsx)(i.LinkIcon, {
+                                    children: (0, o.jsx)(r.LinkIcon, {
                                         size: 'xs',
                                         color: 'currentColor'
                                     })
                                 }),
-                            I
+                            g
                         ]
                     })
                 ]
