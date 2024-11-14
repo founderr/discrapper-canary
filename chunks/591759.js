3e038981eabdcb1bbec1010e5a59c8afae83ca89
DiscrapperManager <admin@minerpl.xyz>
2024-11-14 21:14:14 +0000

    canaryRelease: 344577
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11624
     * Scripts: 10206
    Strings: 0
    Routes: 686
    Experiments Count: 402

--- chunks/591759.js
+++ chunks/591759.js
@@ -4,43 +4,49 @@ var r = n(628735),
     a = n(392711),
     s = n.n(a),
     o = n(729594),
-    l = n(981631);
-let u = /(?:^|\.)(?:discordapp|discord|discordmerch)\.com$/i,
-    c = i().v4().source,
-    d = RegExp('(?:'.concat('(?:(?:[a-z]+:)?//)', '|www\\.)').concat('(?:\\S+(?::\\S*)?@)?', '(?:localhost|').concat(c, '|').concat('(?:[a-z\\u00a1-\\uffff0-9-_]+\\.)+').concat('(?:(?:[a-z\\u00a1-\\uffff]{2,}))', ')').concat('(?::\\d{2,5})?').concat('(?:[/?#][^\\s"]*)?'), 'ig'),
-    f = new Set([window.GLOBAL_ENV.CDN_HOST, window.GLOBAL_ENV.INVITE_HOST, window.GLOBAL_ENV.GIFT_CODE_HOST, window.GLOBAL_ENV.GUILD_TEMPLATE_HOST]);
-function _(e) {
+    l = n(591759),
+    u = n(981631);
+let c = /(?:^|\.)(?:discordapp|discord|discordmerch)\.com$/i,
+    d = i().v4().source,
+    f = RegExp('(?:'.concat('(?:(?:[a-z]+:)?//)', '|www\\.)').concat('(?:\\S+(?::\\S*)?@)?', '(?:localhost|').concat(d, '|').concat('(?:[a-z\\u00a1-\\uffff0-9-_]+\\.)+').concat('(?:(?:[a-z\\u00a1-\\uffff]{2,}))', ')').concat('(?::\\d{2,5})?').concat('(?:[/?#][^\\s"]*)?'), 'ig'),
+    _ = new Set([window.GLOBAL_ENV.CDN_HOST, window.GLOBAL_ENV.INVITE_HOST, window.GLOBAL_ENV.GIFT_CODE_HOST, window.GLOBAL_ENV.GUILD_TEMPLATE_HOST]);
+function p(e) {
     var t;
     let n = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
     if (null == e) return !1;
-    return u.test(e) || (n && ((t = e), f.has(t.toLowerCase())));
-}
-function p(e) {
-    return null != e && 'discord:' === e;
+    return c.test(e) || (n && ((t = e), _.has(t.toLowerCase())));
 }
 function h(e) {
-    let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
-    return null != e && _(o.parse(e).hostname, t);
+    return null != e && 'discord:' === e;
 }
 function m(e) {
-    return null != e && p(o.parse(e).protocol);
+    let t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
+    if (null != e) {
+        var n;
+        let r = null === (n = l.Z.toURLSafe(e)) || void 0 === n ? void 0 : n.hostname;
+        if (null != r && p(r, t)) return !0;
+    }
+    return !1;
+}
+function g(e) {
+    return null != e && h(o.parse(e).protocol);
 }
 t.Z = {
-    URL_REGEX: d,
-    isDiscordHostname: _,
+    URL_REGEX: f,
+    isDiscordHostname: p,
     isDiscordLocalhost: function (e, t) {
         return null != e && null != t && (window.location.host === e || !1);
     },
-    isDiscordProtocol: p,
-    isDiscordUrl: h,
-    isDiscordUri: m,
+    isDiscordProtocol: h,
+    isDiscordUrl: m,
+    isDiscordUri: g,
     isDiscordCdnUrl: function (e) {
         return null != e && o.parse(e).hostname === window.GLOBAL_ENV.CDN_HOST;
     },
-    isDiscordUrlOrUri: (e) => h(e) || m(e),
+    isDiscordUrlOrUri: (e) => m(e) || g(e),
     isAppRoute: (e) => {
         let t = e.toLowerCase();
-        return t.startsWith('/channels/') || t.startsWith(l.Z5c.APPLICATION_STORE) || t.startsWith(l.Z5c.APPLICATION_LIBRARY) || t.startsWith(l.Z5c.MESSAGE_REQUESTS) || t.startsWith(l.Z5c.FAMILY_CENTER) || t.startsWith(l.Z5c.ACTIVITIES) || t.startsWith(l.Z5c.COLLECTIBLES_SHOP) || t.startsWith(l.Z5c.ACTIVITY);
+        return t.startsWith('/channels/') || t.startsWith(u.Z5c.APPLICATION_STORE) || t.startsWith(u.Z5c.APPLICATION_LIBRARY) || t.startsWith(u.Z5c.MESSAGE_REQUESTS) || t.startsWith(u.Z5c.FAMILY_CENTER) || t.startsWith(u.Z5c.ACTIVITIES) || t.startsWith(u.Z5c.COLLECTIBLES_SHOP) || t.startsWith(u.Z5c.ACTIVITY);
     },
     format: (e) => o.format(e),
     formatPathWithQuery: (e, t) =>
