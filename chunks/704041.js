4599893c59e9a2ba2b191b2c95c45c95bd19b98b
DiscrapperManager <admin@minerpl.xyz>
2024-11-14 04:18:19 +0000

    canaryRelease: 344344
    Main scripts: 3
    Lazy Loaded Scripts: 902
    Chunks:
     * Main Files: 11624
     * Scripts: 10210
    Strings: 0
    Routes: 686
    Experiments Count: 403

--- chunks/704041.js
+++ chunks/704041.js
@@ -100,6 +100,11 @@ t.Z = (e) => {
             guildId: t,
             leaderboardId: g._,
             intervalOffset: 0
+        }),
+        p = (0, _.Z)({
+            guildId: t,
+            leaderboardId: g._,
+            intervalOffset: -1
         });
     return ((0, s.useFocusLock)(d),
     !(function (e) {
@@ -165,12 +170,12 @@ t.Z = (e) => {
                                   ? null
                                   : (0, r.jsxs)(r.Fragment, {
                                         children: [
-                                            (0, r.jsx)(h.Z, {
-                                                className: b.previousChampion,
-                                                guildId: t,
-                                                leaderboardId: o,
-                                                intervalOffset: -1
-                                            }),
+                                            null != p &&
+                                                (0, r.jsx)(h.Z, {
+                                                    className: b.previousChampion,
+                                                    guildId: t,
+                                                    leaderboard: p
+                                                }),
                                             (0, r.jsx)(y, {
                                                 guildId: t,
                                                 leaderboardId: o
--- chunks/98518.js
+++ chunks/98518.js
@@ -1,4 +1,6 @@
 e.exports = {
     container: 'container_bc01cf',
-    title: 'title_bc01cf'
+    title: 'title_bc01cf',
+    username: 'username_bc01cf',
+    heroArt: 'heroArt_bc01cf'
 };
