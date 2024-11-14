ccb32ff770c007f605398c34fcf7c0f61c7f07a7
DiscrapperManager <admin@minerpl.xyz>
2024-11-14 22:33:34 +0000

    canaryRelease: 344644
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11624
     * Scripts: 10207
    Strings: 0
    Routes: 687
    Experiments Count: 402

--- chunks/493043.js
+++ chunks/493043.js
@@ -9,12 +9,18 @@ var i = n(984173),
     a = n(769140),
     s = n(382425);
 function o(e) {
-    let { location: t, profileGuildId: n, ...o } = e,
-        { activityReactReplyEnabled: l, allProfileReactReplyEnabled: u } = (0, i.F)({ location: t });
-    return l || u
+    let { location: t, profileGuildId: n, user: o, currentUser: l, ...u } = e,
+        { activityReactReplyEnabled: c, allProfileReactReplyEnabled: d } = (0, i.F)({ location: t });
+    return o.id !== l.id && (c || d)
         ? (0, r.jsx)(s.Z, {
+              currentUser: l,
+              user: o,
               profileGuildId: n,
-              ...o
+              ...u
           })
-        : (0, r.jsx)(a.Z, { ...o });
+        : (0, r.jsx)(a.Z, {
+              currentUser: l,
+              user: o,
+              ...u
+          });
 }
