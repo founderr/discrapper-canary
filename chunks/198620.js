85767c3c1d8c4dd162e96ac1f496f5701eb0a644
DiscrapperManager <admin@minerpl.xyz>
2024-11-14 21:56:42 +0000

    canaryRelease: 344600
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11624
     * Scripts: 10207
    Strings: 0
    Routes: 686
    Experiments Count: 402

--- chunks/198620.js
+++ chunks/198620.js
@@ -1,89 +1,95 @@
 n.d(t, {
     B_: function () {
-        return A;
+        return N;
     },
     MO: function () {
-        return m;
-    },
-    _M: function () {
         return g;
     },
+    _M: function () {
+        return E;
+    },
     k5: function () {
-        return T;
+        return y;
     },
     q5: function () {
-        return E;
+        return v;
     }
 }),
     n(47120);
 var r,
     i,
-    a = n(544891),
-    s = n(70956),
-    o = n(591759),
-    l = n(981631);
-let u = (e) => (null == e ? void 0 : e.split(':')[0]);
-function c(e) {
+    a,
+    s = n(544891),
+    o = n(70956),
+    l = n(591759),
+    u = n(981631);
+let c = (e) => (null == e ? void 0 : e.split(':')[0]);
+function d(e) {
     return 'localhost' === e || '127.0.0.1' === e;
 }
-let d = new Set(['/attachments/', '/ephemeral-attachments/']),
-    f = new Set(['/external/']),
-    _ = 1 * s.Z.Millis.HOUR,
-    p = new Set([window.GLOBAL_ENV.CDN_HOST, null === (r = window.GLOBAL_ENV.MEDIA_PROXY_ENDPOINT) || void 0 === r ? void 0 : r.substring(2)].map(u).filter(Boolean)),
-    h = new Set([null === (i = window.GLOBAL_ENV.IMAGE_PROXY_ENDPOINT) || void 0 === i ? void 0 : i.substring(2)].map(u).filter(Boolean));
-function m(e) {
-    let t = p.has(e.hostname),
+let f = new Set(['/attachments/', '/ephemeral-attachments/']),
+    _ = new Set(['/external/']),
+    p = 1 * o.Z.Millis.HOUR,
+    h = new Set([window.GLOBAL_ENV.CDN_HOST, null === (r = window.GLOBAL_ENV.MEDIA_PROXY_ENDPOINT) || void 0 === r ? void 0 : r.substring(2)].map(c).filter(Boolean)),
+    m = new Set(
+        (null !== (a = null === (i = window.GLOBAL_ENV.IMAGE_PROXY_ENDPOINTS) || void 0 === i ? void 0 : i.split(',')) && void 0 !== a ? a : [])
+            .map((e) => e.substring(2))
+            .map(c)
+            .filter(Boolean)
+    );
+function g(e) {
+    let t = h.has(e.hostname),
         n = !1,
-        r = Array.from(d).some((t) => e.pathname.startsWith(t));
+        r = Array.from(f).some((t) => e.pathname.startsWith(t));
     return (t || n) && r;
 }
-function g(e) {
+function E(e) {
     var t;
-    let n = h.has(e.hostname),
+    let n = m.has(e.hostname),
         r = !1,
-        i = Array.from(f).some((t) => e.pathname.startsWith(t));
+        i = Array.from(_).some((t) => e.pathname.startsWith(t));
     return (n || r) && i;
 }
-function E(e) {
-    let t = o.Z.toURLSafe(e);
+function v(e) {
+    let t = l.Z.toURLSafe(e);
     if (null == t) return e;
     for (let e of ['ex', 'is', 'hm']) t.searchParams.delete(e);
     return t;
 }
-function v(e) {
+function I(e) {
     let t = (function (e) {
         let t = e.searchParams.get('ex'),
             n = parseInt(null != t ? t : '', 16);
-        return isNaN(n) ? void 0 : n * s.Z.Millis.SECOND;
+        return isNaN(n) ? void 0 : n * o.Z.Millis.SECOND;
     })(e);
-    return null == t || t <= Date.now() + _;
-}
-function I(e) {
-    let t = o.Z.toURLSafe(e.url);
-    return null != t && v(t);
+    return null == t || t <= Date.now() + p;
 }
 function b(e) {
-    if (null == e) return !1;
-    let t = o.Z.toURLSafe(e.url);
-    return !!(null != t && m(t)) && v(t);
+    let t = l.Z.toURLSafe(e.url);
+    return null != t && I(t);
 }
 function S(e) {
-    var t;
-    return b(e.image) || (null === (t = e.images) || void 0 === t ? void 0 : t.some(b)) || b(e.video);
+    if (null == e) return !1;
+    let t = l.Z.toURLSafe(e.url);
+    return !!(null != t && g(t)) && I(t);
 }
 function T(e) {
-    return e.attachments.some(I) || e.embeds.some(S);
+    var t;
+    return S(e.image) || (null === (t = e.images) || void 0 === t ? void 0 : t.some(S)) || S(e.video);
 }
-async function y(e) {
-    let t = await a.tn.post({
-        url: l.ANM.ATTACHMENTS_REFRESH_URLS,
+function y(e) {
+    return e.attachments.some(b) || e.embeds.some(T);
+}
+async function A(e) {
+    let t = await s.tn.post({
+        url: u.ANM.ATTACHMENTS_REFRESH_URLS,
         body: { attachment_urls: [e] }
     });
     return t.ok ? t.body.refreshed_urls[0].refreshed : void 0;
 }
-async function A(e) {
-    let t = o.Z.toURLSafe(e);
-    if (null == t || !v(t)) return e;
-    let n = await y(e);
+async function N(e) {
+    let t = l.Z.toURLSafe(e);
+    if (null == t || !I(t)) return e;
+    let n = await A(e);
     return null != n ? n : e;
 }
