8e3062b367832ab66229ce713c079fc0d1536487
DiscrapperManager <admin@minerpl.xyz>
2024-11-18 05:39:04 +0000

    canaryRelease: 345312
    Main scripts: 3
    Lazy Loaded Scripts: 902
    Chunks:
     * Main Files: 11792
     * Scripts: 10205
    Strings: 0
    Routes: 681
    Experiments Count: 401

--- chunks/390289.js
+++ chunks/390289.js
@@ -1,4 +1,4 @@
-n(47120);
+n(47120), n(789020);
 var i,
     l = n(442837),
     a = n(570140),
@@ -7,10 +7,13 @@ var i,
     o = n(944486),
     c = n(9156),
     d = n(70956),
-    u = n(709054),
-    h = n(221259),
-    p = n(490897);
-function m(e, t, n) {
+    u = n(630388),
+    h = n(709054),
+    p = n(221259),
+    m = n(981631),
+    f = n(490897),
+    g = n(526761);
+function C(e, t, n) {
     return (
         t in e
             ? Object.defineProperty(e, t, {
@@ -23,86 +26,91 @@ function m(e, t, n) {
         e
     );
 }
-let f = 2 * d.Z.Millis.MINUTE,
-    g = 5 * f,
-    C = d.Z.Millis.WEEK,
-    x = { channels: {} },
-    v = new Set(),
-    _ = null,
-    I = 0,
-    E = 0;
-function b() {
-    if (null == _ || !S(_)) return !1;
-    let e = N(_);
-    if (e.lastActionTime > Date.now() - d.Z.Millis.DAY && e.viewDuration > g) return !1;
+let x = 2 * d.Z.Millis.MINUTE,
+    _ = 5 * x,
+    v = d.Z.Millis.WEEK,
+    I = { channels: {} },
+    E = new Set(),
+    b = null,
+    Z = 0,
+    N = 0;
+function S() {
+    if (null == b || !A(b)) return !1;
+    let e = j(b);
+    if (e.lastActionTime > Date.now() - d.Z.Millis.DAY && e.viewDuration > _) return !1;
     let t = Date.now();
-    (e.lastActionTime = t), (e.viewDuration += t - I), (I = t);
+    (e.lastActionTime = t), (e.viewDuration += t - Z), (Z = t);
 }
-function Z() {
+function T() {
     return (
-        0 !== E && (clearInterval(E), (E = 0)),
+        0 !== N && (clearInterval(N), (N = 0)),
         c.ZP.useNewNotifications &&
-            (E = setInterval(() => {
-                b() && j.emitChange();
+            (N = setInterval(() => {
+                S() && M.emitChange();
             }, 15 * d.Z.Millis.SECOND)),
         !1
     );
 }
-function N(e) {
-    return e in x.channels
-        ? x.channels[e]
-        : ((x.channels[e] = {
+function j(e) {
+    return e in I.channels
+        ? I.channels[e]
+        : ((I.channels[e] = {
               lastActionTime: 0,
               viewDuration: 0,
               numSends: 0
           }),
-          x.channels[e]);
+          I.channels[e]);
 }
-function S(e) {
-    if (!c.ZP.useNewNotifications || v.has(e)) return !1;
+function A(e) {
+    if (!c.ZP.useNewNotifications || E.has(e)) return !1;
     let t = s.Z.getBasicChannel(e);
-    if (null == t || null == t.guild_id) return !1;
+    if (null == t || null == t.guild_id || c.ZP.isGuildOrCategoryOrChannelMuted(t.guild_id, t.id) || y(t.guild_id, t.id) || y(t.guild_id, t.parent_id)) return !1;
     let n = c.ZP.resolveUnreadSetting(t);
-    return c.ZP.getChannelUnreadSetting(t.guild_id, t.id) === p.i.UNSET && n !== p.i.ALL_MESSAGES;
+    return c.ZP.getChannelUnreadSetting(t.guild_id, t.id) === f.i.UNSET && n !== f.i.ALL_MESSAGES;
 }
-class T extends (i = l.ZP.PersistedStore) {
+function y(e, t) {
+    if (null == t) return !1;
+    let n = c.ZP.getChannelOverrides(e)[t];
+    return null != n && (!!((null != n.message_notifications && n.message_notifications !== m.bL.NULL) || (null != n.flags && (0, u.EB)(n.flags, g.ic.UNREADS_ALL_MESSAGES | g.ic.UNREADS_ONLY_MENTIONS))) || !1);
+}
+class P extends (i = l.ZP.PersistedStore) {
     initialize(e) {
-        null != e && (x.channels = e.channels), this.syncWith([c.ZP], Z), this.waitFor(c.ZP, o.Z, s.Z);
+        null != e && (I.channels = e.channels), this.syncWith([c.ZP], T), this.waitFor(c.ZP, o.Z, s.Z);
     }
     getState() {
-        return x;
+        return I;
     }
     getLastActionTime(e) {
         var t, n;
-        return null !== (n = null === (t = x.channels[e]) || void 0 === t ? void 0 : t.lastActionTime) && void 0 !== n ? n : 0;
+        return null !== (n = null === (t = I.channels[e]) || void 0 === t ? void 0 : t.lastActionTime) && void 0 !== n ? n : 0;
     }
     maybeAutoUpgradeChannel(e) {
-        if (!S(e)) return !1;
+        if (!A(e)) return !1;
         let t = s.Z.getBasicChannel(e);
         if (null == t || null == t.guild_id) return !1;
-        let n = x.channels[e];
-        return !(null == n || n.lastActionTime < Date.now() - C) && (!!(n.numSends >= 2) || !!(n.viewDuration >= f)) && (delete x.channels[e], v.add(e), (0, h.IG)(t.guild_id, t.id, p.i.ALL_MESSAGES), !0);
+        let n = I.channels[e];
+        return !(null == n || n.lastActionTime < Date.now() - v) && (!!(n.numSends >= 2) || !!(n.viewDuration >= x)) && (delete I.channels[e], E.add(e), (0, p.IG)(t.guild_id, t.id, f.i.ALL_MESSAGES), !0);
     }
 }
-m(T, 'displayName', 'UnreadSettingNoticeStore2'), m(T, 'persistKey', 'UnreadSettingNoticeStore2');
-let j = new T(a.Z, {
+C(P, 'displayName', 'UnreadSettingNoticeStore2'), C(P, 'persistKey', 'UnreadSettingNoticeStore2');
+let M = new P(a.Z, {
     CHANNEL_SELECT: function () {
-        let e = b();
-        return (_ = o.Z.getChannelId()), (I = Date.now()), e;
+        let e = S();
+        return (b = o.Z.getChannelId()), (Z = Date.now()), e;
     },
     CONNECTION_OPEN: function () {
-        (_ = o.Z.getChannelId()), (I = Date.now()), Z();
-        let e = Date.now() - C;
-        u.default.forEach(x.channels, (t, n) => {
+        (b = o.Z.getChannelId()), (Z = Date.now()), T();
+        let e = Date.now() - v;
+        h.default.forEach(I.channels, (t, n) => {
             let { lastActionTime: i } = t;
-            i < e && delete x.channels[n];
+            i < e && delete I.channels[n];
         });
     },
     MESSAGE_CREATE: function (e) {
         var t;
-        if (e.optimistic || e.isPushNotification || (null === (t = e.message.author) || void 0 === t ? void 0 : t.id) !== r.default.getId() || !S(e.channelId)) return !1;
-        let n = N(e.channelId);
+        if (e.optimistic || e.isPushNotification || (null === (t = e.message.author) || void 0 === t ? void 0 : t.id) !== r.default.getId() || !A(e.channelId)) return !1;
+        let n = j(e.channelId);
         (n.lastActionTime = Date.now()), n.numSends++;
     }
 });
-t.Z = j;
+t.Z = M;
