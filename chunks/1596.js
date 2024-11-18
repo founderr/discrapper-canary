068093c3f280e3578fc12ef889f701cd7f3fc1be
DiscrapperManager <admin@minerpl.xyz>
2024-11-18 19:03:14 +0000

    canaryRelease: 345440
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11792
     * Scripts: 10203
    Strings: 0
    Routes: 681
    Experiments Count: 400

--- chunks/1596.js
+++ chunks/1596.js
@@ -1,42 +1,66 @@
 n.d(t, {
     Z: function () {
-        return l;
+        return u;
     }
 });
 var r = n(200651);
 n(192379);
 var i = n(481060),
     a = n(138201),
-    s = n(388032),
-    o = n(860343);
-function l() {
-    return (0, r.jsxs)('div', {
-        className: o.infoGroup,
-        children: [
-            (0, r.jsx)(a.Z, {
-                title: s.intl.string(s.t['WBxi8/']),
-                icon: i.ChatXIcon
-            }),
-            (0, r.jsx)(a.Z, {
-                title: s.intl.string(s.t.frCwaW),
-                icon: i.ChatEyeIcon
-            }),
-            (0, r.jsx)(a.Z, {
-                title: s.intl.string(s.t.D0pstr),
-                icon: i.BellSlashIcon
-            }),
-            (0, r.jsx)(a.Z, {
-                title: s.intl.string(s.t['66d4UF']),
-                icon: i.UserCircleIcon
-            }),
-            (0, r.jsx)(a.Z, {
-                title: s.intl.string(s.t.uAYpiY),
-                icon: i.GameControllerIcon
-            }),
-            (0, r.jsx)(a.Z, {
-                title: s.intl.string(s.t.lR4Mc3),
-                icon: i.UserMinusIcon
-            })
-        ]
+    s = n(843931),
+    o = n(388032),
+    l = n(860343);
+function u() {
+    let e = (0, s.YC)({ location: 'BlockingContext' });
+    return (0, r.jsx)('div', {
+        className: l.infoGroup,
+        children: e
+            ? (0, r.jsxs)(r.Fragment, {
+                  children: [
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t.fjFJFR),
+                          description: o.intl.string(o.t['8SIMPz']),
+                          icon: i.CircleCheckIcon
+                      }),
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t.QCrmqa),
+                          description: o.intl.string(o.t.TKDMoK),
+                          icon: i.CircleCheckIcon
+                      }),
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t['lkm/a2']),
+                          description: o.intl.string(o.t['8j3qaG']),
+                          icon: i.WarningIcon
+                      })
+                  ]
+              })
+            : (0, r.jsxs)(r.Fragment, {
+                  children: [
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t['WBxi8/']),
+                          icon: i.ChatXIcon
+                      }),
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t.frCwaW),
+                          icon: i.ChatEyeIcon
+                      }),
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t.D0pstr),
+                          icon: i.BellSlashIcon
+                      }),
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t['66d4UF']),
+                          icon: i.UserCircleIcon
+                      }),
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t.uAYpiY),
+                          icon: i.GameControllerIcon
+                      }),
+                      (0, r.jsx)(a.Z, {
+                          title: o.intl.string(o.t.lR4Mc3),
+                          icon: i.UserMinusIcon
+                      })
+                  ]
+              })
     });
 }
--- chunks/843931.js
+++ chunks/843931.js
@@ -1,22 +1,43 @@
 n.d(t, {
-    c: function () {
+    YC: function () {
         return i;
+    },
+    cC: function () {
+        return a;
     }
 });
 let r = (0, n(818083).B)({
     kind: 'user',
     id: '2024-10_blocking_context',
     label: 'Blocking Context',
-    defaultConfig: { enabled: !1 },
+    defaultConfig: {
+        enabled: !1,
+        condensed: !1
+    },
     treatments: [
         {
             id: 1,
             label: 'Show blocking context in block confirmation modal',
-            config: { enabled: !0 }
+            config: {
+                enabled: !0,
+                condensed: !1
+            }
+        },
+        {
+            id: 2,
+            label: 'Show condensed blocking context',
+            config: {
+                enabled: !0,
+                condensed: !0
+            }
         }
     ]
 });
 function i(e) {
+    let { location: t } = e;
+    return r.useExperiment({ location: t }, { autoTrackExposure: !0 }).condensed;
+}
+function a(e) {
     let { location: t } = e;
     return r.getCurrentConfig({ location: t }, { autoTrackExposure: !0 }).enabled;
 }
