1107854c1f524bf205ab6e73420135283dab3c63
DiscrapperManager <admin@minerpl.xyz>
2024-11-18 18:21:09 +0000

    canaryRelease: 345411
    Main scripts: 3
    Lazy Loaded Scripts: 901
    Chunks:
     * Main Files: 11792
     * Scripts: 10203
    Strings: 0
    Routes: 681
    Experiments Count: 400

--- chunks/16084.js
+++ chunks/16084.js
@@ -1,76 +1,79 @@
 o.d(t, {
     $N: function () {
-        return h;
+        return R;
     },
     ZZ: function () {
-        return I;
+        return A;
     },
     df: function () {
         return f;
     },
     pB: function () {
-        return U;
+        return m;
     },
     uE: function () {
-        return R;
+        return y;
     },
     x2: function () {
         return C;
     },
     xA: function () {
-        return A;
+        return U;
     }
 }),
     o(411104);
-var n = o(544891),
-    r = o(570140),
-    i = o(881052),
-    a = o(128069),
-    d = o(34756),
-    c = o(115130),
-    l = o(55563),
-    _ = o(695103),
-    s = o(122289),
-    u = o(823379),
-    p = o(936101),
-    b = o(73346),
-    E = o(355467),
-    S = o(981631);
-async function h(e, t) {
-    if (null == l.Z.get(t)) {
-        r.Z.dispatch({
+var n = o(311570),
+    r = o(544891),
+    i = o(570140),
+    a = o(881052),
+    d = o(128069),
+    c = o(34756),
+    l = o(115130),
+    _ = o(55563),
+    s = o(695103),
+    u = o(122289),
+    p = o(823379),
+    b = o(936101),
+    E = o(73346),
+    S = o(355467),
+    h = o(981631);
+async function R(e, t, o) {
+    if (null == _.Z.get(t)) {
+        i.Z.dispatch({
             type: 'SKU_FETCH_START',
             skuId: t
         });
         try {
-            let o = _.Z.inTestModeForApplication(e) || c.Z.inDevModeForApplication(e),
-                n = await (0, b.Kb)(o ? S.ANM.STORE_SKU(t) : S.ANM.STORE_PUBLISHED_LISTINGS_SKU(t));
-            r.Z.dispatch({
+            let r = s.Z.inTestModeForApplication(e) || l.Z.inDevModeForApplication(e),
+                a = { url: r ? h.ANM.STORE_SKU(t) : h.ANM.STORE_PUBLISHED_LISTINGS_SKU(t) };
+            o === n.v.VARIANTS_GROUP && (a.query = { variants_return_style: o });
+            let d = await (0, E.Kb)(a);
+            i.Z.dispatch({
                 type: 'SKU_FETCH_SUCCESS',
-                sku: o ? n.body : n.body.sku
+                sku: r ? d.body : d.body.sku
             }),
-                !o &&
-                    r.Z.dispatch({
+                !r &&
+                    i.Z.dispatch({
                         type: 'STORE_LISTING_FETCH_SUCCESS',
-                        storeListing: n.body
+                        storeListing: d.body
                     });
         } catch (e) {
             throw (
-                (r.Z.dispatch({
+                (i.Z.dispatch({
                     type: 'SKU_FETCH_FAIL',
                     skuId: t
                 }),
-                new d.Z('Failed to fetch SKU '.concat(t)))
+                new c.Z('Failed to fetch SKU '.concat(t)))
             );
         }
     }
 }
-async function R(e) {
+async function y(e) {
     let t = !(arguments.length > 1) || void 0 === arguments[1] || arguments[1];
-    if (!(_.Z.inTestModeForApplication(e) || c.Z.inDevModeForApplication(e)) && t) throw Error('this should only be used in test mode');
-    let o = (await (0, b.Kb)(S.ANM.APPLICATION_SKUS(e))).body;
+    if (!(s.Z.inTestModeForApplication(e) || l.Z.inDevModeForApplication(e)) && t) throw Error('this should only be used in test mode');
+    let o = (await (0, E.Kb)(h.ANM.APPLICATION_SKUS(e))).body;
     return (
-        r.Z.dispatch({
+        i.Z.dispatch({
             type: 'SKUS_FETCH_SUCCESS',
             skus: o
         }),
@@ -78,88 +81,88 @@ async function R(e) {
     );
 }
 async function C(e, t, o, n) {
-    let d;
-    let l = {
+    let r;
+    let c = {
         payment_source_id: o,
         gift: null == n ? void 0 : n.isGift
     };
-    (_.Z.inTestModeForApplication(e) || c.Z.inDevModeForApplication(e)) && (l.test_mode = !0),
-        r.Z.dispatch({
+    (s.Z.inTestModeForApplication(e) || l.Z.inDevModeForApplication(e)) && (c.test_mode = !0),
+        i.Z.dispatch({
             type: 'SKU_PURCHASE_PREVIEW_FETCH',
             skuId: t
         });
     try {
-        (d = await (0, b.Kb)({
-            url: S.ANM.STORE_SKU_PURCHASE(t),
-            query: l,
+        (r = await (0, E.Kb)({
+            url: h.ANM.STORE_SKU_PURCHASE(t),
+            query: c,
             oldFormErrors: !0
         })),
-            r.Z.dispatch({
+            i.Z.dispatch({
                 type: 'SKU_PURCHASE_PREVIEW_FETCH_SUCCESS',
                 skuId: t,
                 paymentSourceId: o,
-                price: d.body
+                price: r.body
             });
     } catch (o) {
-        r.Z.dispatch({
+        i.Z.dispatch({
             type: 'SKU_PURCHASE_PREVIEW_FETCH_FAILURE',
             skuId: t
         });
-        let e = o instanceof i.HF ? o : new i.HF(o);
-        if (e.code === a.SM.BILLING_BUNDLE_ALREADY_PURCHASED || e.code === a.SM.BILLING_BUNDLE_PARTIALLY_OWNED) throw e;
+        let e = o instanceof a.HF ? o : new a.HF(o);
+        if (e.code === d.SM.BILLING_BUNDLE_ALREADY_PURCHASED || e.code === d.SM.BILLING_BUNDLE_PARTIALLY_OWNED) throw e;
     }
-    return d;
+    return r;
 }
-let y = { isGift: !1 };
-async function I(e, t, o) {
+let I = { isGift: !1 };
+async function A(e, t, o) {
     let {
-        paymentSource: d,
-        expectedAmount: l,
-        expectedCurrency: b,
-        analyticsLoadId: h,
+        paymentSource: n,
+        expectedAmount: c,
+        expectedCurrency: _,
+        analyticsLoadId: E,
         isGift: R,
-        giftInfoOptions: C,
-        subscriptionPlanId: I,
-        loadId: f,
-        countryCode: A
+        giftInfoOptions: y,
+        subscriptionPlanId: C,
+        loadId: A,
+        countryCode: f
     } = {
-        ...y,
+        ...I,
         ...o
     };
-    r.Z.wait(() => {
-        r.Z.dispatch({
+    i.Z.wait(() => {
+        i.Z.dispatch({
             type: 'SKU_PURCHASE_START',
             applicationId: e,
             skuId: t
         });
     });
-    let U = _.Z.inTestModeForApplication(e) || c.Z.inDevModeForApplication(e);
+    let U = s.Z.inTestModeForApplication(e) || l.Z.inDevModeForApplication(e);
     try {
         let e = {
             gift: R,
-            sku_subscription_plan_id: I,
-            gateway_checkout_context: await (0, s.cn)(d),
-            load_id: f
+            sku_subscription_plan_id: C,
+            gateway_checkout_context: await (0, u.cn)(n),
+            load_id: A
         };
         if (U) e.test_mode = !0;
         else {
-            if (null != d && ((e.payment_source_id = d.id), (e.payment_source_token = await (0, E.Zv)(d)), S.QL.has(d.type))) {
-                let t = await (0, E.EH)(d.type);
-                e.return_url = (0, n.K0)() + S.ANM.BILLING_POPUP_BRIDGE_CALLBACK_REDIRECT_PREFIX(d.type, null != t ? t : '', 'success');
+            if (null != n && ((e.payment_source_id = n.id), (e.payment_source_token = await (0, S.Zv)(n)), h.QL.has(n.type))) {
+                let t = await (0, S.EH)(n.type);
+                e.return_url = (0, r.K0)() + h.ANM.BILLING_POPUP_BRIDGE_CALLBACK_REDIRECT_PREFIX(n.type, null != t ? t : '', 'success');
             }
-            null != l && (e.expected_amount = l), null != b && (e.expected_currency = b), (e.gift_info_options = C), null != A && (e.country_code = A), (e.purchase_token = (0, p.d)());
+            null != c && (e.expected_amount = c), null != _ && (e.expected_currency = _), (e.gift_info_options = y), null != f && (e.country_code = f), (e.purchase_token = (0, b.d)());
         }
-        let o = await n.tn.post({
-            url: S.ANM.STORE_SKU_PURCHASE(t),
+        let o = await r.tn.post({
+            url: h.ANM.STORE_SKU_PURCHASE(t),
             body: e,
-            context: { load_id: h },
+            context: { load_id: E },
             oldFormErrors: !0
         });
         return (
-            r.Z.dispatch({
+            i.Z.dispatch({
                 type: 'SKU_PURCHASE_SUCCESS',
                 skuId: t,
-                libraryApplications: null != o.body.library_applications ? o.body.library_applications.filter(u.lm) : [],
+                libraryApplications: null != o.body.library_applications ? o.body.library_applications.filter(p.lm) : [],
                 entitlements: o.body.entitlements,
                 giftCode: o.body.gift_code
             }),
@@ -168,39 +171,39 @@ async function I(e, t, o) {
                 redirectConfirmation: !1
             }
         );
-    } catch (n) {
-        let o = n instanceof i.HF ? n : new i.HF(n);
+    } catch (r) {
+        let o = r instanceof a.HF ? r : new a.HF(r);
         if (
-            ((o.code === a.SM.CONFIRMATION_REQUIRED || o.code === a.SM.AUTHENTICATION_REQUIRED) &&
-                r.Z.dispatch({
+            ((o.code === d.SM.CONFIRMATION_REQUIRED || o.code === d.SM.AUTHENTICATION_REQUIRED) &&
+                i.Z.dispatch({
                     type: 'SKU_PURCHASE_AWAIT_CONFIRMATION',
                     skuId: t,
                     isGift: R
                 }),
-            r.Z.dispatch({
+            i.Z.dispatch({
                 type: 'SKU_PURCHASE_FAIL',
                 applicationId: e,
                 skuId: t,
                 error: o
             }),
-            o.code !== a.SM.CONFIRMATION_REQUIRED)
+            o.code !== d.SM.CONFIRMATION_REQUIRED)
         )
             throw o;
-        if (!n.body.payment_id) throw (0, E.SQ)('payment id cannot be null on redirected confirmations.');
-        return (0, E.sk)(n.body, d);
+        if (!r.body.payment_id) throw (0, S.SQ)('payment id cannot be null on redirected confirmations.');
+        return (0, S.sk)(r.body, n);
     }
 }
 async function f(e) {
-    r.Z.wait(() => {
-        r.Z.dispatch({
+    i.Z.wait(() => {
+        i.Z.dispatch({
             type: 'VIRTUAL_CURRENCY_REDEEM_START',
             skuId: e
         });
     });
     try {
-        let t = (await n.tn.post({ url: S.ANM.USER_REDEEM_VIRTUAL_CURRENCY(e) })).body;
+        let t = (await r.tn.post({ url: h.ANM.USER_REDEEM_VIRTUAL_CURRENCY(e) })).body;
         return (
-            r.Z.dispatch({
+            i.Z.dispatch({
                 type: 'VIRTUAL_CURRENCY_REDEEM_SUCCESS',
                 skuId: e,
                 entitlements: t
@@ -208,30 +211,30 @@ async function f(e) {
             t
         );
     } catch (o) {
-        let t = o instanceof i.HF ? o : new i.HF(o);
-        r.Z.dispatch({
+        let t = o instanceof a.HF ? o : new a.HF(o);
+        i.Z.dispatch({
             type: 'VIRTUAL_CURRENCY_REDEEM_FAIL',
             skuId: e,
             error: t
         });
     }
 }
-async function A() {
+async function U() {
     try {
-        let e = { purchase_token: (0, p.d)() };
+        let e = { purchase_token: (0, b.d)() };
         return {
             ...(
-                await n.tn.post({
-                    url: S.ANM.STORE_EMAIL_RESEND_PAYMENT_VERIFICATION,
+                await r.tn.post({
+                    url: h.ANM.STORE_EMAIL_RESEND_PAYMENT_VERIFICATION,
                     body: e,
                     oldFormErrors: !0
                 })
             ).body
         };
     } catch (e) {
-        throw e instanceof i.HF ? e : new i.HF(e);
+        throw e instanceof a.HF ? e : new a.HF(e);
     }
 }
-function U() {
-    r.Z.dispatch({ type: 'SKU_PURCHASE_CLEAR_ERROR' });
+function m() {
+    i.Z.dispatch({ type: 'SKU_PURCHASE_CLEAR_ERROR' });
 }
--- chunks/712297.js
+++ chunks/712297.js
@@ -1,6 +1,6 @@
 n.d(e, {
     Z: function () {
-        return A;
+        return a;
     }
 }),
     n(47120);
@@ -9,14 +9,15 @@ var r = n(192379),
     i = n(16084),
     u = n(881052),
     S = n(128069),
-    o = n(855775),
-    l = n(55563),
-    _ = n(474936);
-function A(t) {
-    let { applicationId: e, skuIDs: n, currentPaymentSourceId: A, isGift: a } = t,
-        T = r.useMemo(() => n.filter((t) => !_.YQ.includes(t)), [JSON.stringify(n)]),
-        I = (0, E.e7)([l.Z], () => T.every((t) => !l.Z.isFetching(t) && null != l.Z.get(t))),
-        { previewErrorsById: c, setErrorById: R } = (function () {
+    o = n(228624),
+    l = n(855775),
+    _ = n(55563),
+    A = n(474936);
+function a(t) {
+    let { applicationId: e, skuIDs: n, currentPaymentSourceId: a, isGift: T } = t,
+        I = r.useMemo(() => n.filter((t) => !A.YQ.includes(t)), [JSON.stringify(n)]),
+        c = (0, E.e7)([_.Z], () => I.every((t) => !_.Z.isFetching(t) && null != _.Z.get(t))),
+        { previewErrorsById: R, setErrorById: C } = (function () {
             let [t, e] = r.useState({});
             return {
                 previewErrorsById: t,
@@ -31,46 +32,47 @@ function A(t) {
                 )
             };
         })(),
-        C = (0, E.cj)(
-            [l.Z],
+        N = (0, E.cj)(
+            [_.Z],
             () => {
                 let t = {};
-                for (let n of T) {
+                for (let n of I) {
                     var e;
-                    t[n] = null !== (e = l.Z.get(n)) && void 0 !== e ? e : void 0;
+                    t[n] = null !== (e = _.Z.get(n)) && void 0 !== e ? e : void 0;
                 }
                 return t;
             },
-            [T]
-        );
+            [I]
+        ),
+        s = (0, o.h)('useSkus');
     r.useEffect(() => {
-        for (let t of T) !l.Z.isFetching(t) && null == l.Z.get(t) && (0, i.$N)(e, t);
-    }, [e, T]);
-    let N = (0, E.cj)(
-        [o.Z],
+        for (let t of I) !_.Z.isFetching(t) && null == _.Z.get(t) && (0, i.$N)(e, t, s);
+    }, [e, I, s]);
+    let M = (0, E.cj)(
+        [l.Z],
         () => {
             let t = {};
-            for (let n of T) {
+            for (let n of I) {
                 var e;
-                t[n] = null !== (e = o.Z.getPricesForSku(n)) && void 0 !== e ? e : void 0;
+                t[n] = null !== (e = l.Z.getPricesForSku(n)) && void 0 !== e ? e : void 0;
             }
             return t;
         },
-        [T]
+        [I]
     );
     return (
         r.useEffect(() => {
-            for (let t of T)
-                !o.Z.isFetchingSKU(t) &&
-                    (0, i.x2)(e, t, A, { isGift: a }).catch((e) => {
-                        e instanceof u.HF && (e.code === S.SM.BILLING_BUNDLE_ALREADY_PURCHASED || e.code === S.SM.BILLING_BUNDLE_PARTIALLY_OWNED) && R(t, e);
+            for (let t of I)
+                !l.Z.isFetchingSKU(t) &&
+                    (0, i.x2)(e, t, a, { isGift: T }).catch((e) => {
+                        e instanceof u.HF && (e.code === S.SM.BILLING_BUNDLE_ALREADY_PURCHASED || e.code === S.SM.BILLING_BUNDLE_PARTIALLY_OWNED) && C(t, e);
                     });
-        }, [e, T, A, a, R]),
+        }, [e, I, a, T, C]),
         {
-            hasFetchedSkus: I,
-            skusById: C,
-            skuPricePreviewsById: N,
-            previewErrorsById: c
+            hasFetchedSkus: c,
+            skusById: N,
+            skuPricePreviewsById: M,
+            previewErrorsById: R
         }
     );
 }
