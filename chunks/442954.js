e13d9e3de49ec2bca73144de29e3d59db32921ca
DiscrapperManager <admin@minerpl.xyz>
2024-11-19 03:00:44 +0000

    canaryRelease: 345784
    Main scripts: 3
    Lazy Loaded Scripts: 903
    Chunks:
     * Main Files: 11803
     * Scripts: 10198
    Strings: 0
    Routes: 681
    Experiments Count: 388

--- chunks/442954.js
+++ chunks/442954.js
@@ -1,6 +1,6 @@
 n.d(t, {
     Z: function () {
-        return u;
+        return x;
     }
 }),
     n(47120);
@@ -9,10 +9,13 @@ var r = n(200651),
     l = n(120356),
     i = n.n(l),
     o = n(481060),
-    c = n(246992),
-    s = n(986310),
-    d = n(907555);
-function u() {
+    c = n(138464),
+    s = n(585483),
+    d = n(246992),
+    u = n(981631),
+    h = n(986310),
+    m = n(907555);
+function x() {
     let e = {
             BOGOAnnouncementModal: async () => {
                 let { default: e } = await n.e('34049').then(n.bind(n, 868508));
@@ -31,40 +34,60 @@ function u() {
             label: e,
             value: e
         })),
-        [l, u] = a.useState(t[0].value);
+        [l, x] = a.useState(t[0].value);
     return (0, r.jsx)(o.ScrollerThin, {
-        className: i()(s.panel),
-        children: (0, r.jsx)('div', {
-            className: d.panelInner,
-            children: (0, r.jsxs)('section', {
-                className: d.section,
-                children: [
-                    (0, r.jsx)(o.Heading, {
-                        variant: 'heading-md/semibold',
-                        children: 'Open a Modal'
-                    }),
-                    (0, r.jsxs)('div', {
-                        className: d.inputRow,
-                        children: [
-                            (0, r.jsx)(o.Select, {
-                                className: d.input,
-                                options: t,
-                                isSelected: (e) => l === e,
-                                placeholder: 'Trial ID',
-                                serialize: (e) => String(e),
-                                select: (e) => u(e),
-                                popoutLayerContext: c.O$
-                            }),
-                            (0, r.jsx)(o.Button, {
+        className: i()(h.panel),
+        children: (0, r.jsxs)('div', {
+            className: m.panelInner,
+            children: [
+                (0, r.jsxs)('section', {
+                    className: m.section,
+                    children: [
+                        (0, r.jsx)(o.Heading, {
+                            variant: 'heading-md/semibold',
+                            children: 'Open a Modal'
+                        }),
+                        (0, r.jsxs)('div', {
+                            className: m.inputRow,
+                            children: [
+                                (0, r.jsx)(o.Select, {
+                                    className: m.input,
+                                    options: t,
+                                    isSelected: (e) => l === e,
+                                    placeholder: 'Trial ID',
+                                    serialize: (e) => String(e),
+                                    select: (e) => x(e),
+                                    popoutLayerContext: d.O$
+                                }),
+                                (0, r.jsx)(o.Button, {
+                                    onClick: () => {
+                                        (0, o.openModalLazy)(e[l]);
+                                    },
+                                    children: 'Open'
+                                })
+                            ]
+                        })
+                    ]
+                }),
+                (0, r.jsxs)('section', {
+                    className: m.section,
+                    children: [
+                        (0, r.jsx)(o.Heading, {
+                            variant: 'heading-md/semibold',
+                            children: 'Wow moment'
+                        }),
+                        (0, r.jsx)('div', {
+                            className: m.inputRow,
+                            children: (0, r.jsx)(o.Button, {
                                 onClick: () => {
-                                    (0, o.openModalLazy)(e[l]);
+                                    (0, c.H)(!0), s.S.dispatch(u.CkL.PREMIUM_SUBSCRIPTION_CREATED);
                                 },
                                 children: 'Open'
                             })
-                        ]
-                    })
-                ]
-            })
+                        })
+                    ]
+                })
+            ]
         })
     });
 }
