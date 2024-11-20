1fb53f298e1da8387b98bb90a6b23f56593796ae
DiscrapperManager <admin@minerpl.xyz>
2024-11-20 03:44:34 +0000

    canaryRelease: 346200
    Main scripts: 3
    Lazy Loaded Scripts: 905
    Chunks:
     * Main Files: 11808
     * Scripts: 10207
    Strings: 0
    Routes: 682
    Experiments Count: 386

--- chunks/801606.js
+++ chunks/801606.js
@@ -1,14 +1,34 @@
 n.d(t, {
     Z: function () {
-        return s;
+        return l;
     }
-});
-var r = n(697426),
-    i = n(375954),
-    a = n(710111);
-function s(e, t, n) {
-    var s, o;
-    let l = i.Z.getMessage(e, t),
-        u = null == l ? void 0 : null === (s = l.soundboardSounds) || void 0 === s ? void 0 : s.find((e) => String(e.sound_id) === String(n));
-    if (null != u) return (0, r.o3)(u, null !== (o = u.guild_id) && void 0 !== o ? o : a.X8);
+}),
+    n(47120);
+var r = n(388123),
+    i = n(697426),
+    a = n(375954),
+    s = n(710111);
+function o(e, t) {
+    var n;
+    return null == e ? void 0 : null === (n = e.soundboardSounds) || void 0 === n ? void 0 : n.find((e) => String(e.sound_id) === String(t));
+}
+function l(e, t, n) {
+    var l, u, c;
+    let d = a.Z.getMessage(e, t);
+    if (null == d) return;
+    let f =
+        (null == d ? void 0 : null === (l = d.messageReference) || void 0 === l ? void 0 : l.type) === r.U.FORWARD
+            ? (function (e, t) {
+                  let n;
+                  for (let r of e) {
+                      let e = o(r.message, t);
+                      if (null != e) {
+                          n = e;
+                          break;
+                      }
+                  }
+                  return n;
+              })(null !== (u = null == d ? void 0 : d.messageSnapshots) && void 0 !== u ? u : [], n)
+            : o(d, n);
+    if (null != f) return (0, i.o3)(f, null !== (c = f.guild_id) && void 0 !== c ? c : s.X8);
 }
