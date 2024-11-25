da660c30e586ca68a46cd6d96a5605760ae8fe59
DiscrapperManager <admin@minerpl.xyz>
2024-11-25 21:03:36 +0000

    canaryRelease: 348041
    Main scripts: 3
    Lazy Loaded Scripts: 912
    Chunks:
     * Main Files: 11828
     * Scripts: 10247
    Strings: 0
    Routes: 683
    Experiments Count: 385

--- chunks/993397.js
+++ chunks/993397.js
@@ -1,41 +1,109 @@
-n(47120);
+n(627341), n(47120);
 var i = n(200651),
     l = n(192379),
     r = n(120356),
     a = n.n(r),
-    s = n(481060),
-    o = n(492593),
-    c = n(534469),
-    d = n(511010),
-    u = n(834129),
-    h = n(981631),
-    p = n(388032),
-    m = n(590008),
-    f = n(916315);
-function g(e) {
-    let { expanded: t, onClick: n, count: l, compact: r, collapsedReason: a } = e;
-    return (0, i.jsx)(o.Z, {
+    s = n(278074),
+    o = n(481060),
+    c = n(922611),
+    d = n(492593),
+    u = n(534469),
+    h = n(511010),
+    p = n(834129),
+    m = n(981631),
+    f = n(388032),
+    g = n(447817),
+    C = n(46124);
+function x(e) {
+    let { expanded: t, onClick: n, count: l, compact: r, collapsedReason: a } = e,
+        u = (0, c.Do)({ location: 'CollapsedItemMessage' }),
+        h = (0, s.EQ)({
+            stealthRemediationEnabled: u,
+            expanded: t
+        })
+            .returnType()
+            .with(
+                {
+                    stealthRemediationEnabled: !0,
+                    expanded: !0
+                },
+                () => f.intl.string(f.t.fgq1go)
+            )
+            .with(
+                {
+                    stealthRemediationEnabled: !0,
+                    expanded: !1
+                },
+                () => f.intl.string(f.t.XJuakJ)
+            )
+            .with(
+                {
+                    stealthRemediationEnabled: !1,
+                    expanded: !1
+                },
+                () => f.intl.formatToPlainString(f.t.wUJXIi, { count: l })
+            )
+            .with(
+                {
+                    stealthRemediationEnabled: !1,
+                    expanded: !0
+                },
+                () => f.intl.formatToPlainString(f.t['5XFZjY'], { count: l })
+            )
+            .exhaustive(),
+        m = (0, s.EQ)({
+            stealthRemediationEnabled: u,
+            collapsedReason: a
+        })
+            .with(
+                {
+                    stealthRemediationEnabled: !0,
+                    collapsedReason: f.t.VFWjc3
+                },
+                () =>
+                    (0, i.jsx)(o.EyeSlashIcon, {
+                        size: 'md',
+                        color: 'currentColor',
+                        className: g.blockedIcon
+                    })
+            )
+            .with(
+                {
+                    stealthRemediationEnabled: !0,
+                    collapsedReason: f.t['+FcYMz']
+                },
+                () =>
+                    (0, i.jsx)(o.DenyIcon, {
+                        size: 'md',
+                        color: 'currentColor',
+                        className: g.blockedIcon
+                    })
+            )
+            .otherwise(() =>
+                (0, i.jsx)(o.XSmallIcon, {
+                    size: 'md',
+                    color: 'currentColor',
+                    className: g.blockedIcon
+                })
+            );
+    return (0, i.jsx)(d.Z, {
         compact: r,
         role: 'group',
-        childrenMessageContent: (0, i.jsx)(u.Z, {
+        childrenMessageContent: (0, i.jsx)(p.Z, {
             compact: r,
-            className: m.blockedSystemMessage,
-            iconNode: (0, i.jsx)(s.XSmallIcon, {
-                size: 'md',
-                color: 'currentColor',
-                className: m.blockedIcon
-            }),
+            className: g.blockedSystemMessage,
+            iconNode: m,
             children: (0, i.jsxs)('div', {
-                className: m.blockedMessageText,
+                className: g.blockedMessageText,
                 children: [
-                    p.intl.format(a, { count: l }),
+                    f.intl.format(a, { count: l }),
                     ' \u2014',
                     ' ',
-                    (0, i.jsx)(s.Clickable, {
+                    (0, i.jsx)(o.Clickable, {
                         tag: 'span',
                         onClick: n,
-                        className: m.blockedAction,
-                        children: t ? p.intl.formatToPlainString(p.t['5XFZjY'], { count: l }) : p.intl.formatToPlainString(p.t.wUJXIi, { count: l })
+                        className: g.blockedAction,
+                        children: h
                     })
                 ]
             })
@@ -44,20 +112,20 @@ function g(e) {
 }
 t.Z = l.memo(function (e) {
     var t;
-    let { messages: n, channel: r, compact: s = !1, unreadId: o, collapsedReason: u } = e,
-        { hasJumpTarget: p = !1 } = n,
-        [C, x] = l.useState(p),
-        _ = l.useCallback(() => x((e) => !e), []),
+    let { messages: n, channel: r, compact: s = !1, unreadId: o, collapsedReason: c } = e,
+        { hasJumpTarget: d = !1 } = n,
+        [p, f] = l.useState(d),
+        _ = l.useCallback(() => f((e) => !e), []),
         v = n.hasUnread ? n.content.length - 1 : n.content.length;
     return (0, i.jsxs)('div', {
         className: a()({
-            [f.groupStart]: !0,
-            [m.expanded]: C
+            [C.groupStart]: !0,
+            [g.expanded]: p
         }),
         children: [
-            n.hasUnread && (!C || (null === (t = n.content[0]) || void 0 === t ? void 0 : t.type) === h.ys_.DIVIDER)
+            n.hasUnread && (!p || (null === (t = n.content[0]) || void 0 === t ? void 0 : t.type) === m.ys_.DIVIDER)
                 ? (0, i.jsx)(
-                      d.Z,
+                      h.Z,
                       {
                           isUnread: !0,
                           id: o
@@ -66,23 +134,23 @@ t.Z = l.memo(function (e) {
                   )
                 : null,
             (0, i.jsx)(
-                g,
+                x,
                 {
                     count: v,
                     compact: s,
-                    expanded: C,
+                    expanded: p,
                     onClick: _,
-                    collapsedReason: u
+                    collapsedReason: c
                 },
                 'collapsed-message-item'
             ),
-            C
+            p
                 ? n.content.map((e, t) => {
-                      if (e.type === h.ys_.DIVIDER && t > 0) {
+                      if (e.type === m.ys_.DIVIDER && t > 0) {
                           var l, a;
                           let e = null !== (a = null === (l = n.content[t + 1]) || void 0 === l ? void 0 : l.isGroupStart) && void 0 !== a && a;
                           return (0, i.jsx)(
-                              d.Z,
+                              h.Z,
                               {
                                   isUnread: !0,
                                   isBeforeGroup: e,
@@ -91,12 +159,12 @@ t.Z = l.memo(function (e) {
                               'divider'
                           );
                       }
-                      if (e.type === h.ys_.MESSAGE || e.type === h.ys_.THREAD_STARTER_MESSAGE) {
-                          let t = e.type === h.ys_.THREAD_STARTER_MESSAGE ? c.Ru : c.ZP;
+                      if (e.type === m.ys_.MESSAGE || e.type === m.ys_.THREAD_STARTER_MESSAGE) {
+                          let t = e.type === m.ys_.THREAD_STARTER_MESSAGE ? u.Ru : u.ZP;
                           return (0, i.jsx)(
                               t,
                               {
-                                  className: m.__invalid_blocked,
+                                  className: g.__invalid_blocked,
                                   compact: s,
                                   channel: r,
                                   message: e.content,
