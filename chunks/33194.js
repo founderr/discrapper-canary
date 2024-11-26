22f9e0d10e9ff2233d022dab3e1f162ffe6daa9b
DiscrapperManager <admin@minerpl.xyz>
2024-11-26 15:54:34 +0000

    canaryRelease: 348314
    Main scripts: 3
    Lazy Loaded Scripts: 910
    Chunks:
     * Main Files: 11834
     * Scripts: 10244
    Strings: 0
    Routes: 683
    Experiments Count: 385

--- chunks/33194.js
+++ chunks/33194.js
@@ -1,32 +1,42 @@
 n.d(t, {
     Iu: function () {
-        return i;
+        return s;
     },
     OS: function () {
-        return a;
-    },
-    g6: function () {
         return o;
     },
+    g6: function () {
+        return u;
+    },
     v3: function () {
-        return s;
+        return l;
     }
 });
-let r = (0, n(15729).U)((e) => ({
-    channelDismissTimestamps: {},
-    queuedWarning: !1
-}));
-function i(e) {
-    return r.getState().channelDismissTimestamps[e];
+var r = n(15729),
+    i = n(877124);
+let a = (0, r.U)(
+    (0, i.tJ)(
+        (e) => ({
+            channelDismissTimestamps: {},
+            queuedWarning: !1
+        }),
+        {
+            name: 'shared-spaces-warning-storage',
+            storage: (0, i.FL)(() => localStorage)
+        }
+    )
+);
+function s(e) {
+    return a.getState().channelDismissTimestamps[e];
 }
-function a() {
-    return r.getState().queuedWarning;
+function o() {
+    return a.getState().queuedWarning;
 }
-function s() {
-    r.setState({ queuedWarning: !1 });
+function l() {
+    a.setState({ queuedWarning: !1 });
 }
-function o(e) {
-    r.setState((t) => ({
+function u(e) {
+    a.setState((t) => ({
         channelDismissTimestamps: {
             ...t.channelDismissTimestamps,
             [e]: Date.now()
