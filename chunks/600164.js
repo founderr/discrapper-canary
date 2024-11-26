a074e870a16fa37561df083e71a851ca80189795
DiscrapperManager <admin@minerpl.xyz>
2024-11-26 18:45:58 +0000

    canaryRelease: 348389
    Main scripts: 3
    Lazy Loaded Scripts: 910
    Chunks:
     * Main Files: 11838
     * Scripts: 10246
    Strings: 0
    Routes: 683
    Experiments Count: 385

--- chunks/600164.js
+++ chunks/600164.js
@@ -29,61 +29,49 @@ let u = {
         WRAP_REVERSE: l.wrapReverse
     },
     _ = (e) => {
-        let { children: t, className: n, shrink: a, grow: l, basis: u, style: c, wrap: d = !1, ...f } = e,
-            _ = {
-                className: (n = null != n ? n : o.flexChild),
-                style: {
-                    flexGrow: l,
-                    flexShrink: a,
-                    flexBasis: u,
-                    ...c
-                },
-                ...f
-            };
-        if (!d && 'string' != typeof t && 1 === i.Children.count(t)) {
-            let e = i.Children.only(t);
-            return (
-                (_.style = {
-                    ..._.style,
-                    ...e.props.style
-                }),
-                (_.className = s()(e.props.className, n)),
-                i.cloneElement(e, _)
-            );
-        }
+        let { children: t, className: n, direction: i = u.HORIZONTAL, justify: a = c.START, align: l = d.STRETCH, wrap: _ = f.NO_WRAP, shrink: p = 1, grow: h = 1, basis: m = 'auto', style: g, ...E } = e;
         return (0, r.jsx)('div', {
-            ..._,
+            style: {
+                flexShrink: p,
+                flexGrow: h,
+                flexBasis: m,
+                ...g
+            },
+            className: s()(o.flex, i, a, l, _, n),
+            ...E,
             children: t
         });
     };
-_.defaultProps = {
-    shrink: 1,
-    grow: 1,
-    basis: 'auto',
-    wrap: !1
-};
-let p = (e) => {
-    let { children: t, className: n, direction: i = u.HORIZONTAL, justify: a = c.START, align: l = d.STRETCH, wrap: _ = f.NO_WRAP, shrink: p, grow: h, basis: m, style: g, ...E } = e;
+(_.Child = (e) => {
+    let { children: t, className: n, shrink: a = 1, grow: l = 1, basis: u = 'auto', style: c, wrap: d = !1, ...f } = e,
+        _ = {
+            className: (n = null != n ? n : o.flexChild),
+            style: {
+                flexGrow: l,
+                flexShrink: a,
+                flexBasis: u,
+                ...c
+            },
+            ...f
+        };
+    if (!d && 'string' != typeof t && 1 === i.Children.count(t)) {
+        let e = i.Children.only(t);
+        return (
+            (_.style = {
+                ..._.style,
+                ...e.props.style
+            }),
+            (_.className = s()(e.props.className, n)),
+            i.cloneElement(e, _)
+        );
+    }
     return (0, r.jsx)('div', {
-        style: {
-            flexShrink: p,
-            flexGrow: h,
-            flexBasis: m,
-            ...g
-        },
-        className: s()(o.flex, i, a, l, _, n),
-        ...E,
+        ..._,
         children: t
     });
-};
-(p.defaultProps = {
-    shrink: 1,
-    grow: 1,
-    basis: 'auto'
 }),
-    (p.Child = _),
-    (p.Direction = u),
-    (p.Align = d),
-    (p.Justify = c),
-    (p.Wrap = f),
-    (t.Z = p);
+    (_.Direction = u),
+    (_.Align = d),
+    (_.Justify = c),
+    (_.Wrap = f),
+    (t.Z = _);
