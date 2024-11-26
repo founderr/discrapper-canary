d2ba53a1887396d06e39e566865580e12926e977
DiscrapperManager <admin@minerpl.xyz>
2024-11-26 23:57:57 +0000

    canaryRelease: 348656
    Main scripts: 3
    Lazy Loaded Scripts: 912
    Chunks:
     * Main Files: 11858
     * Scripts: 10257
    Strings: 0
    Routes: 683
    Experiments Count: 386

--- chunks/512303.js
+++ chunks/512303.js
@@ -1,72 +1,81 @@
 t.d(n, {
     Z: function () {
-        return f;
+        return E;
     }
 });
-var i = t(200651);
-t(192379);
-var l = t(373793),
-    r = t(481060),
-    a = t(700582),
-    o = t(592180),
-    s = t(565138),
-    u = t(430824),
-    d = t(594174),
-    c = t(388032),
-    g = t(500054);
-function f(e) {
-    var n, t, f;
-    if (!(0, o.a)(e)) return null;
-    let m = null === (n = e.interactionMetadata) || void 0 === n ? void 0 : n.authorizing_integration_owners[l.Y.USER_INSTALL],
-        h = null === (t = e.interactionMetadata) || void 0 === t ? void 0 : t.authorizing_integration_owners[l.Y.GUILD_INSTALL],
-        E = d.default.getUser(m),
-        I = u.Z.getGuild(h),
-        v = d.default.getUser(null === (f = e.interactionMetadata) || void 0 === f ? void 0 : f.user.id),
-        p = null;
+var i = t(200651),
+    l = t(192379),
+    r = t(373793),
+    a = t(442837),
+    o = t(481060),
+    s = t(232567),
+    u = t(700582),
+    d = t(592180),
+    c = t(565138),
+    g = t(430824),
+    f = t(594174),
+    m = t(388032),
+    h = t(500054);
+function E(e) {
+    var n, t, E;
+    let I = null === (n = e.interactionMetadata) || void 0 === n ? void 0 : n.authorizing_integration_owners[r.Y.USER_INSTALL],
+        v = null === (t = e.interactionMetadata) || void 0 === t ? void 0 : t.authorizing_integration_owners[r.Y.GUILD_INSTALL],
+        p = null === (E = e.interactionMetadata) || void 0 === E ? void 0 : E.user.id,
+        M = (0, a.e7)([f.default], () => f.default.getUser(I)),
+        Z = (0, a.e7)([g.Z], () => g.Z.getGuild(v)),
+        x = (0, a.e7)([f.default], () => f.default.getUser(p));
+    if (
+        (l.useEffect(() => {
+            null == M && null != I && (0, s.PR)(I), null == x && null != p && (0, s.PR)(p);
+        }, [M, I, x, p]),
+        !(0, d.a)(e))
+    )
+        return null;
+    let S = null;
     return (
-        null != I
-            ? (p = (0, i.jsx)(r.MenuItem, {
-                  className: g.interactionInfoMenuItem,
+        null != Z
+            ? (S = (0, i.jsx)(o.MenuItem, {
+                  className: h.interactionInfoMenuItem,
                   disabled: !0,
                   iconLeft: () =>
-                      (0, i.jsx)(s.Z, {
-                          guild: I,
-                          size: s.Z.Sizes.MINI
+                      (0, i.jsx)(c.Z, {
+                          guild: Z,
+                          size: c.Z.Sizes.MINI
                       }),
                   id: 'integration-owner',
-                  label: I.name,
-                  subtext: c.intl.formatToPlainString(c.t.ShLXXF, { application: e.author.username })
+                  label: Z.name,
+                  subtext: m.intl.formatToPlainString(m.t.ShLXXF, { application: e.author.username })
               }))
-            : null != E &&
-              (p = (0, i.jsx)(r.MenuItem, {
-                  className: g.interactionInfoMenuItem,
+            : null != M &&
+              (S = (0, i.jsx)(o.MenuItem, {
+                  className: h.interactionInfoMenuItem,
                   disabled: !0,
                   iconLeft: () =>
-                      (0, i.jsx)(a.Z, {
-                          user: E,
-                          size: r.AvatarSizes.SIZE_20
+                      (0, i.jsx)(u.Z, {
+                          user: M,
+                          size: o.AvatarSizes.SIZE_20
                       }),
                   id: 'integration-owner',
-                  label: E.username,
-                  subtext: c.intl.formatToPlainString(c.t.ShLXXF, { application: e.author.username })
+                  label: M.username,
+                  subtext: m.intl.formatToPlainString(m.t.ShLXXF, { application: e.author.username })
               })),
-        (0, i.jsxs)(r.MenuItem, {
+        (0, i.jsxs)(o.MenuItem, {
             id: 'view-interaction-info',
-            label: c.intl.string(c.t.Rjezb2),
+            label: m.intl.string(m.t.Rjezb2),
             children: [
-                p,
-                null != v
-                    ? (0, i.jsx)(r.MenuItem, {
-                          className: g.interactionInfoMenuItem,
+                S,
+                null != x
+                    ? (0, i.jsx)(o.MenuItem, {
+                          className: h.interactionInfoMenuItem,
                           disabled: !0,
                           iconLeft: () =>
-                              (0, i.jsx)(a.Z, {
-                                  user: v,
-                                  size: r.AvatarSizes.SIZE_20
+                              (0, i.jsx)(u.Z, {
+                                  user: x,
+                                  size: o.AvatarSizes.SIZE_20
                               }),
                           id: 'interaction-user',
-                          label: v.username,
-                          subtext: c.intl.string(c.t['04gxNj'])
+                          label: x.username,
+                          subtext: m.intl.string(m.t['04gxNj'])
                       })
                     : null
             ]
