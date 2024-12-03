1d4929c8cccfc91c3e184ebe9a26f8d4beff7366
DiscrapperManager <admin@minerpl.xyz>
2024-12-03 18:57:48 +0000

    canaryRelease: 349728
    Main scripts: 3
    Lazy Loaded Scripts: 910
    Chunks:
     * Main Files: 11882
     * Scripts: 10248
    Strings: 0
    Routes: 683
    Experiments Count: 382

---
+++ chunks/633656.js
@@ -0,0 +1,14 @@
+e.exports = {
+    container: 'container_efc1ac',
+    body: 'body_efc1ac',
+    pointer: 'pointer_efc1ac',
+    popoutTarget: 'popoutTarget_efc1ac',
+    text: 'text_efc1ac',
+    coachmark: 'coachmark_efc1ac',
+    staticImage: 'staticImage_efc1ac',
+    animation: 'animation_efc1ac',
+    dmButtonContainer: 'dmButtonContainer_efc1ac',
+    dmButton: 'dmButton_efc1ac',
+    closeButton: 'closeButton_efc1ac',
+    closeIcon: 'closeIcon_efc1ac'
+};
