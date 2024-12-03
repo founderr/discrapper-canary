76f3abff1727ac3e0966e0ffb6bc02913770a5a6
DiscrapperManager <admin@minerpl.xyz>
2024-12-03 01:58:02 +0000

    canaryRelease: 349569
    Main scripts: 3
    Lazy Loaded Scripts: 910
    Chunks:
     * Main Files: 11883
     * Scripts: 10249
    Strings: 0
    Routes: 683
    Experiments Count: 385

--- chunks/586742.js
+++ chunks/586742.js
@@ -1,34 +1,41 @@
 n.d(t, {
     Z: function () {
-        return I;
+        return b;
     }
 });
 var i = n(200651),
-    l = n(192379),
-    o = n(120356),
-    r = n.n(o),
+    o = n(192379),
+    l = n(120356),
+    r = n.n(l),
     s = n(442837),
     a = n(239091),
     u = n(765250),
-    d = n(393238),
-    c = n(358221),
+    c = n(393238),
+    d = n(358221),
     h = n(933557),
     f = n(352978),
     p = n(27457),
     m = n(796638),
-    g = n(592125),
-    v = n(944486),
-    E = n(906037),
-    Z = n(501787),
-    C = n(65154),
-    x = n(827202);
+    v = n(592125),
+    g = n(131951),
+    E = n(944486),
+    x = n(906037),
+    C = n(501787),
+    Z = n(65154),
+    _ = n(827202);
+function I(e) {
+    var t;
+    let { participant: n, channel: o, context: l } = e,
+        r = null === (t = n.user) || void 0 === t ? void 0 : t.id;
+    return (0, s.e7)([g.Z], () => null != n.user && null != l && null != o && g.Z.isLocalVideoDisabled(r, l), [r, n.user, l, o]) ? null : (0, i.jsx)(p.ZP, { ...e });
+}
 function S(e) {
-    let { context: t = C.Yn.DEFAULT, participants: o, locked: s, widget: u, channel: d, width: c, height: h, containerRef: m } = e,
-        g = (e, l) => {
-            (0, a.jW)(l, async () => {
-                let { default: l } = await Promise.all([n.e('79695'), n.e('69220'), n.e('351')]).then(n.bind(n, 881351));
+    let { context: t = Z.Yn.DEFAULT, participants: l, locked: s, widget: u, channel: c, width: d, height: h, containerRef: p } = e,
+        m = (e, o) => {
+            (0, a.jW)(o, async () => {
+                let { default: o } = await Promise.all([n.e('79695'), n.e('69220'), n.e('351')]).then(n.bind(n, 881351));
                 return (n) =>
-                    (0, i.jsx)(l, {
+                    (0, i.jsx)(o, {
                         ...n,
                         user: e.user,
                         showMediaItems: !0,
@@ -36,9 +43,9 @@ function S(e) {
                     });
             });
         },
-        v = o.length > 0 && null != t && null != d && (!s || u.pinned);
-    l.useEffect(() => {
-        (0, E.m3)(
+        v = l.length > 0 && null != t && null != c && (!s || u.pinned);
+    o.useEffect(() => {
+        (0, x.m3)(
             {
                 locked: s,
                 pinned: u.pinned,
@@ -48,69 +55,67 @@ function S(e) {
             v
         );
     }, [s, u, v]);
-    let Z = 'boolean' != typeof u.meta.horizontal || u.meta.horizontal;
+    let g = 'boolean' != typeof u.meta.horizontal || u.meta.horizontal;
     return v
         ? (0, i.jsx)('div', {
-              ref: m,
+              ref: p,
               className: r()({
-                  [x.videoList]: !0,
-                  [x.vertical]: !Z
+                  [_.videoList]: !0,
+                  [_.vertical]: !g
               }),
               style: { opacity: u.opacity },
-              children: o.map((e) => {
-                  let { user: n } = e;
-                  return null == n || null == d || null == t
-                      ? null
-                      : (0, i.jsx)(
-                            p.ZP,
-                            {
-                                participant: e,
-                                width: c,
-                                className: x.tile,
-                                containerStyle: {
-                                    width: c,
-                                    height: h
-                                },
-                                fit: f.L.COVER,
-                                channel: d,
-                                inPopout: !0,
-                                inCall: !0,
-                                noBorder: !0,
-                                onContextMenu: s ? void 0 : g,
-                                forceIdle: s
-                            },
-                            n.id
-                        );
-              })
+              children: l.map((e) =>
+                  (0, i.jsx)(
+                      I,
+                      {
+                          participant: e,
+                          width: d,
+                          className: _.tile,
+                          containerStyle: {
+                              width: d,
+                              height: h
+                          },
+                          fit: f.L.COVER,
+                          channel: c,
+                          inPopout: !0,
+                          inCall: !0,
+                          noBorder: !0,
+                          onContextMenu: s ? void 0 : m,
+                          forceIdle: s,
+                          context: t
+                      },
+                      e.id
+                  )
+              )
           })
         : null;
 }
-function I(e) {
-    let t = (0, s.e7)([v.Z, g.Z], () => g.Z.getChannel(v.Z.getVoiceChannelId())),
+function b(e) {
+    let t = (0, s.e7)([E.Z, v.Z], () => v.Z.getChannel(E.Z.getVoiceChannelId())),
         n = (0, h.ZP)(t),
-        o = (0, s.Wu)([c.Z], () => (null != t ? c.Z.getVideoParticipants(t.id) : [])),
-        r = (0, s.e7)([c.Z], () => (null != t ? c.Z.getParticipantsVersion(t.id) : 0)),
+        l = (0, s.Wu)([d.Z], () => (null != t ? d.Z.getVideoParticipants(t.id) : [])),
+        r = (0, s.e7)([d.Z], () => (null != t ? d.Z.getParticipantsVersion(t.id) : 0)),
         a = 'boolean' != typeof e.widget.meta.horizontal || e.widget.meta.horizontal,
-        { width: f, height: p, ref: E } = (0, d.Z)(),
-        { visibleParticipants: C, participantTileWidth: x } = (0, m.ZB)(a ? (null != f ? f : e.width) : null != p ? p : e.height, o, {
-            tileWidth: Z.OVERLAY_TILE_WIDTH,
-            tileMinWidth: Z.OVERLAY_TILE_MIN_WIDTH,
-            tileMargin: Z.OVERLAY_TILE_MARGIN,
+        { width: f, height: p, ref: g } = (0, c.Z)(),
+        { visibleParticipants: x, participantTileWidth: Z } = (0, m.ZB)(a ? (null != f ? f : e.width) : null != p ? p : e.height, l, {
+            tileWidth: C.OVERLAY_TILE_WIDTH,
+            tileMinWidth: C.OVERLAY_TILE_MIN_WIDTH,
+            tileMargin: C.OVERLAY_TILE_MARGIN,
             limit: 8,
             cropSelfVideo: !0,
             version: r
         }),
-        I = {
+        _ = {
             id: e.widget.id,
             size: e.widget.size,
             containerWidth: e.width,
             containerHeight: e.height
         },
-        _ = l.useRef(I);
+        I = o.useRef(_);
     return (
-        l.useLayoutEffect(() => void (_.current = I)),
-        l.useLayoutEffect(() => {
-            let { size: e, id: t, containerWidth: n, containerHeight: i } = _.current;
+        o.useLayoutEffect(() => void (I.current = _)),
+        o.useLayoutEffect(() => {
+            let { size: e, id: t, containerWidth: n, containerHeight: i } = I.current;
             ((a && e.height > e.width) || (!a && e.width > e.height)) &&
                 (0, u.nv)({
                     widgetId: t,
@@ -125,11 +130,11 @@ function I(e) {
             ...e,
             channel: t,
             title: null != n ? n : '',
-            participants: C,
+            participants: x,
             participantsVersion: r,
-            width: a ? x : null != f ? f : e.width,
-            height: a ? (null != p ? p : e.height) : x,
-            containerRef: E
+            width: a ? Z : null != f ? f : e.width,
+            height: a ? (null != p ? p : e.height) : Z,
+            containerRef: g
         })
     );
 }
