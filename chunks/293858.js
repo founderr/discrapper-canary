4747fc3291f8b246b4ba23fce5304c3ece460064
DiscrapperManager <admin@minerpl.xyz>
2024-12-03 17:46:18 +0000

    canaryRelease: 349689
    Main scripts: 3
    Lazy Loaded Scripts: 912
    Chunks:
     * Main Files: 11881
     * Scripts: 10251
    Strings: 0
    Routes: 683
    Experiments Count: 383

--- chunks/293858.js
+++ chunks/293858.js
@@ -1,72 +1,81 @@
 n.d(t, {
     b: function () {
-        return _;
+        return g;
     }
 });
 var i = n(200651),
     l = n(192379),
     s = n(512722),
     r = n.n(s),
-    a = n(881602),
-    C = n(155491),
-    o = n(317269),
-    d = n(669079),
-    u = n(296848),
-    c = n(987209),
-    p = n(563132),
-    x = n(592627),
-    h = n(614277),
-    f = n(698708);
-function _(e) {
+    a = n(605236),
+    C = n(881602),
+    o = n(155491),
+    d = n(717401),
+    u = n(286961),
+    c = n(317269),
+    p = n(669079),
+    x = n(296848),
+    h = n(987209),
+    f = n(563132),
+    _ = n(592627),
+    j = n(614277),
+    L = n(698708),
+    m = n(921944);
+function g(e) {
     var t, n;
     let s,
-        { handleClose: _, planGroup: j, onSubscriptionConfirmation: L, renderPurchaseConfirmation: m, postSuccessGuild: g, followupSKUInfo: E, continueSession: S } = e,
-        { activeSubscription: y, paymentSources: M, paymentSourceId: Z, selectedPlan: I, selectedSkuId: v, step: P, updatedSubscription: T, startingPremiumSubscriptionPlanIdRef: N } = (0, p.usePaymentContext)(),
-        { isGift: A, giftRecipient: U, giftCode: k, hasSentMessage: b, isSendingMessage: O, sendGiftMessage: R } = (0, c.wD)(),
-        w = (0, C.Z2)();
-    r()(null != I, 'Expected plan to selected'), r()(null != v, 'Expected selectedSkuId'), r()(null != P, 'Step should be set');
-    let H = l.useCallback(() => {
-        _(), null == L || L();
-    }, [_, L]);
+        { handleClose: g, planGroup: E, onSubscriptionConfirmation: S, renderPurchaseConfirmation: y, postSuccessGuild: M, followupSKUInfo: Z, continueSession: I } = e,
+        { activeSubscription: v, paymentSources: P, paymentSourceId: T, selectedPlan: N, selectedSkuId: A, step: U, updatedSubscription: k, startingPremiumSubscriptionPlanIdRef: b } = (0, f.usePaymentContext)(),
+        { isGift: O, giftRecipient: R, giftCode: w, hasSentMessage: H, isSendingMessage: B, sendGiftMessage: F } = (0, h.wD)(),
+        G = (0, d.id)(N, O),
+        W = (0, u.Z)(),
+        D = (0, o.Z2)();
+    r()(null != N, 'Expected plan to selected'), r()(null != A, 'Expected selectedSkuId'), r()(null != U, 'Step should be set');
+    let Y = l.useCallback(() => {
+        g(), null == S || S();
+    }, [g, S]);
     return (
         l.useEffect(() => {
-            if (!(!A || null == U || null == k || b || O || (0, d.pO)(U))) a.F.getCurrentConfig({ location: '36b986_1' }).enabled && R({ onSubscriptionConfirmation: L });
-        }, [R, A, U, k, b, O, L]),
+            if (!(!O || null == R || null == w || H || B || (0, p.pO)(R))) C.F.getCurrentConfig({ location: '36b986_1' }).enabled && F({ onSubscriptionConfirmation: S });
+        }, [F, O, R, w, H, B, S]),
+        l.useEffect(() => {
+            null != W && null != W.reminderNotice && G && (0, a.wH)(W.reminderNotice.dismissibleContent, W.dismissibleContentVersion, { dismissAction: m.L.INDIRECT_ACTION });
+        }, [W, G]),
         (s =
-            null != m
-                ? m(I, H, T)
-                : S
-                  ? (0, i.jsx)(o.VY, {})
-                  : A
-                    ? (0, i.jsx)(o.TB, {
-                          planId: I.id,
-                          onClose: H,
-                          halloweenDecoPurchase: w
+            null != y
+                ? y(N, Y, k)
+                : I
+                  ? (0, i.jsx)(c.VY, {})
+                  : O
+                    ? (0, i.jsx)(c.TB, {
+                          planId: N.id,
+                          onClose: Y,
+                          halloweenDecoPurchase: D
                       })
-                    : N.current === I.id
-                      ? (0, i.jsx)(o.ZP, {
-                            planId: I.id,
-                            postSuccessGuild: g,
-                            onClose: H,
-                            paymentSourceType: null === (t = M[null != Z ? Z : '']) || void 0 === t ? void 0 : t.type
+                    : b.current === N.id
+                      ? (0, i.jsx)(c.ZP, {
+                            planId: N.id,
+                            postSuccessGuild: M,
+                            onClose: Y,
+                            paymentSourceType: null === (t = P[null != T ? T : '']) || void 0 === t ? void 0 : t.type
                         })
-                      : (0, i.jsx)(o.ZP, {
-                            followupSKUInfo: E,
-                            startingPremiumSubscriptionPlanId: N.current,
-                            planId: I.id,
-                            onClose: H,
-                            isDowngrade: null != y && (0, u.GY)(y, I.id, j),
-                            paymentSourceType: null === (n = M[null != Z ? Z : '']) || void 0 === n ? void 0 : n.type
+                      : (0, i.jsx)(c.ZP, {
+                            followupSKUInfo: Z,
+                            startingPremiumSubscriptionPlanId: b.current,
+                            planId: N.id,
+                            onClose: Y,
+                            isDowngrade: null != v && (0, x.GY)(v, N.id, E),
+                            paymentSourceType: null === (n = P[null != T ? T : '']) || void 0 === n ? void 0 : n.type
                         })),
         (0, i.jsxs)(i.Fragment, {
             children: [
-                (0, i.jsxs)(h.C3, {
-                    children: [(0, i.jsx)(f.Z, {}), s]
+                (0, i.jsxs)(j.C3, {
+                    children: [(0, i.jsx)(L.Z, {}), s]
                 }),
-                A
-                    ? (0, i.jsx)(x.Z, {
-                          onClose: H,
-                          halloweenDecoPurchase: w
+                O
+                    ? (0, i.jsx)(_.Z, {
+                          onClose: Y,
+                          halloweenDecoPurchase: D
                       })
                     : null
             ]
