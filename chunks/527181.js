a7649173cb2d010d08b307508211a09e305f3f0a
DiscrapperManager <admin@minerpl.xyz>
2024-12-04 19:40:33 +0000

    canaryRelease: 350300
    Main scripts: 3
    Lazy Loaded Scripts: 909
    Chunks:
     * Main Files: 11873
     * Scripts: 10265
    Strings: 0
    Routes: 683
    Experiments Count: 382

---
+++ chunks/527181.js
@@ -0,0 +1,14 @@
+let r = (0, n(818083).B)({
+    kind: 'user',
+    id: '2024-12_dcf_dismissal_no_op',
+    label: 'DCF Dismissal No Op',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Dismissing an already dismissed DC will have no effect',
+            config: { enabled: !0 }
+        }
+    ]
+});
+t.Z = r;
--- chunks/605236.js
+++ chunks/605236.js
@@ -1,76 +1,78 @@
 n.d(t, {
     EW: function () {
-        return R;
+        return O;
     },
     Fo: function () {
-        return y;
-    },
-    H4: function () {
-        return S;
-    },
-    kk: function () {
         return A;
     },
-    un: function () {
-        return T;
+    H4: function () {
+        return y;
     },
-    wE: function () {
+    kk: function () {
+        return N;
+    },
+    un: function () {
         return b;
     },
+    wE: function () {
+        return S;
+    },
     wH: function () {
-        return O;
+        return D;
     }
 }),
     n(47120);
 var r = n(442837),
     i = n(704215),
     a = n(570140),
-    s = n(561746),
-    o = n(675478),
-    l = n(581883),
-    u = n(626135),
-    c = n(915486),
-    d = n(713284),
-    f = n(261376),
-    _ = n(883904),
-    p = n(68985),
-    h = n(952193),
-    m = n(211644),
-    g = n(57207),
-    E = n(644916),
-    v = n(921944),
-    I = n(981631);
-function T(e) {
-    var t;
-    if ((0, g.B)(e)) return !0;
-    let n = null === (t = l.Z.settings.userContent) || void 0 === t ? void 0 : t.dismissedContents;
-    return null != n && (0, c.jl)(n, e);
-}
+    s = n(527181),
+    o = n(561746),
+    l = n(675478),
+    u = n(581883),
+    c = n(626135),
+    d = n(915486),
+    f = n(713284),
+    _ = n(261376),
+    p = n(883904),
+    h = n(68985),
+    m = n(952193),
+    g = n(211644),
+    E = n(57207),
+    v = n(644916),
+    I = n(921944),
+    T = n(981631);
 function b(e) {
-    return (0, r.e7)([l.Z], () => T(e));
+    var t;
+    let n = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
+    if (!n && (0, E.B)(e)) return !0;
+    let r = null === (t = u.Z.settings.userContent) || void 0 === t ? void 0 : t.dismissedContents;
+    return null != r && (0, d.jl)(r, e);
 }
 function S(e) {
+    return (0, r.e7)([u.Z], () => b(e));
+}
+function y(e) {
     var t, n;
-    if ((0, g.B)(e))
+    if ((0, E.B)(e))
         return {
             isDismissed: !0,
             lastDismissedVersion: null
         };
-    let r = null === (n = l.Z.settings.userContent) || void 0 === n ? void 0 : null === (t = n.recurringDismissibleContentStates[e]) || void 0 === t ? void 0 : t.lastDismissedVersion,
-        i = (0, E.t)(e);
+    let r = null === (n = u.Z.settings.userContent) || void 0 === n ? void 0 : null === (t = n.recurringDismissibleContentStates[e]) || void 0 === t ? void 0 : t.lastDismissedVersion,
+        i = (0, v.t)(e);
     return {
         isDismissed: null != r && r >= i,
         lastDismissedVersion: r
     };
 }
-function y(e, t) {
+function A(e, t) {
     var n, r;
-    if ((0, g.B)(e))
+    if ((0, E.B)(e))
         return {
             isDismissed: !0,
             lastDismissedAtMs: null
         };
-    let i = null === (r = l.Z.settings.userContent) || void 0 === r ? void 0 : null === (n = r.recurringDismissibleContentStates[e]) || void 0 === n ? void 0 : n.lastDismissedAtMs,
+    let i = null === (r = u.Z.settings.userContent) || void 0 === r ? void 0 : null === (n = r.recurringDismissibleContentStates[e]) || void 0 === n ? void 0 : n.lastDismissedAtMs,
         a = null != i && '0' !== i ? (Number.isNaN(Number(i)) ? void 0 : Number(i)) : void 0;
     if (void 0 === a)
         return {
@@ -89,14 +91,14 @@ function y(e, t) {
         lastDismissedAtMs: a
     };
 }
-function A(e, t, n) {
+function N(e, t, n) {
     if (
         !(
-            (0, m.cI)(e) ||
-            p.Z.hasUserHitDCCap(e) ||
+            (0, g.cI)(e) ||
+            h.Z.hasUserHitDCCap(e) ||
             (function (e) {
-                let t = h.$.has(e),
-                    { enabled: n } = s.Z.getCurrentConfig({ location: 'isUserSubjectToDCFHoldout' }, { autoTrackExposure: !t });
+                let t = m.$.has(e),
+                    { enabled: n } = o.Z.getCurrentConfig({ location: 'isUserSubjectToDCFHoldout' }, { autoTrackExposure: !t });
                 return n && !t;
             })(e)
         ) &&
@@ -104,22 +106,22 @@ function A(e, t, n) {
     )
         a.Z.dispatch({
             type: 'DCF_EVENT_LOGGED',
-            eventType: d.D.DC_SHOW_REQUEST,
+            eventType: f.D.DC_SHOW_REQUEST,
             dismissibleContent: e
         }),
-            (0, m.f0)({
+            (0, g.f0)({
                 content: e,
                 groupName: null == t ? void 0 : t.groupName,
                 onAdded: () => {
                     var n;
-                    let [r, a] = (0, m.Aq)();
-                    (0, _.cm)(e),
-                        u.default.track(I.rMx.DISMISSIBLE_CONTENT_SHOWN, {
+                    let [r, a] = (0, g.Aq)();
+                    (0, p.cm)(e),
+                        c.default.track(T.rMx.DISMISSIBLE_CONTENT_SHOWN, {
                             type: i.z[e],
                             content_count: r,
                             fatigable_content_count: a,
                             group_name: null == t ? void 0 : t.groupName,
-                            bypass_fatigue: f.O.has(e),
+                            bypass_fatigue: _.O.has(e),
                             guild_id: null == t ? void 0 : t.guildId,
                             version: null == t ? void 0 : t.version
                         }),
@@ -127,32 +129,32 @@ function A(e, t, n) {
                 }
             });
 }
-function N(e) {
+function C(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {};
-    ((0, m.cI)(e) || t.forceTrack) &&
+    ((0, g.cI)(e) || t.forceTrack) &&
         (function (e, t) {
             var n;
-            let [r] = (0, m.Aq)(),
-                a = p.Z.getRenderedAtTimestamp(e),
+            let [r] = (0, g.Aq)(),
+                a = h.Z.getRenderedAtTimestamp(e),
                 s = new Date(),
                 o = null == a ? null : s.getTime() - a;
-            u.default.track(I.rMx.DISMISSIBLE_CONTENT_DISMISSED, {
+            c.default.track(T.rMx.DISMISSIBLE_CONTENT_DISMISSED, {
                 type: i.z[e],
-                action: null !== (n = null == t ? void 0 : t.dismissAction) && void 0 !== n ? n : v.L.UNKNOWN,
+                action: null !== (n = null == t ? void 0 : t.dismissAction) && void 0 !== n ? n : I.L.UNKNOWN,
                 content_count: r,
                 group_name: null == t ? void 0 : t.groupName,
-                bypass_fatigue: f.O.has(e),
+                bypass_fatigue: _.O.has(e),
                 guild_id: null == t ? void 0 : t.guildId,
                 shown_duration: o,
                 version: null == t ? void 0 : t.version
             });
         })(e, t),
-        (0, _.Vr)(e);
+        (0, p.Vr)(e);
 }
-function C(e) {
+function R(e) {
     let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {},
-        n = !p.Z.hasUserHitDCCap();
-    (0, m.gE)(
+        n = !h.Z.hasUserHitDCCap();
+    (0, g.gE)(
         {
             content: e,
             groupName: null == t ? void 0 : t.groupName
@@ -160,11 +162,12 @@ function C(e) {
         n
     );
 }
-async function R(e) {
-    let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {};
-    N(e, t), await (0, o.nm)(e), C(e, t);
+async function O(e) {
+    let t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {},
+        { enabled: n } = s.Z.getCurrentConfig({ location: 'markDismissibleContentAsDismissed' });
+    if (!(n && b(e, !0))) C(e, t), await (0, l.nm)(e), R(e, t);
 }
-async function O(e, t) {
+async function D(e, t) {
     let n = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : {};
-    N(e, n), await (0, o.Bn)(e, t), C(e, n);
+    C(e, n), await (0, l.Bn)(e, t), R(e, n);
 }
