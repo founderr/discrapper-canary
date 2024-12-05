0087c5cc8b1b35d8473bcbeb9e8ce1f8ef27054d
DiscrapperManager <admin@minerpl.xyz>
2024-12-05 16:24:48 +0000

    canaryRelease: 350655
    Main scripts: 3
    Lazy Loaded Scripts: 911
    Chunks:
     * Main Files: 11874
     * Scripts: 10265
    Strings: 0
    Routes: 683
    Experiments Count: 382

--- chunks/605984.js
+++ chunks/605984.js
@@ -11,13 +11,11 @@ var r = e(385956),
     o = e(88101),
     d = e(276060);
 function c(n, t) {
-    var e;
-    let c = (0, i.G)(n),
-        s = (0, r.v)(n),
-        f = (0, d.t)({ location: t }),
-        E = (0, l.S)(n, u.pj.LIKELY_ATO),
-        _ = (0, a.h)(n).length > 0,
-        p = null != (0, o.P)(n),
-        m = ((e = (null == E ? void 0 : E.expiry) != null && Date.parse(E.expiry) < Date.now()), e);
-    if (!!f && !c && !s && !m && !_ && !p) return E;
+    let e = (0, i.G)(n),
+        c = (0, r.v)(n),
+        s = (0, d.t)({ location: t }),
+        f = (0, l.S)(n, u.pj.LIKELY_ATO),
+        E = (0, a.h)(n).length > 0,
+        _ = null != (0, o.P)(n);
+    if (!!s && !e && !c && !E && !_) return f;
 }
