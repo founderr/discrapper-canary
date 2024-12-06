97e33d59248e856cfe4616707f46de1aa8a465b5
DiscrapperManager <admin@minerpl.xyz>
2024-12-06 15:32:36 +0000

    canaryRelease: 351139
    Main scripts: 3
    Lazy Loaded Scripts: 914
    Chunks:
     * Main Files: 11880
     * Scripts: 10276
    Strings: 0
    Routes: 684
    Experiments Count: 382

--- chunks/453470.js
+++ chunks/453470.js
@@ -1,87 +1,73 @@
 n.d(t, {
     M: function () {
-        return E;
+        return _;
     }
 });
 var i = n(200651),
     l = n(192379),
     r = n(286379),
-    a = n(442837),
-    s = n(481060),
-    o = n(87051),
-    c = n(797614),
-    d = n(681678),
-    u = n(378298),
-    h = n(359119),
-    p = n(473092),
-    m = n(177342),
-    f = n(9156),
-    g = n(621600),
-    C = n(870991),
-    x = n(981631),
-    v = n(388032),
-    _ = n(162787);
-function I(e) {
-    let { senderId: t, channelId: n, warningId: l } = e,
-        r = (0, a.e7)([f.ZP], () => f.ZP.isChannelMuted(null, n));
+    a = n(481060),
+    s = n(87051),
+    o = n(797614),
+    c = n(681678),
+    d = n(378298),
+    u = n(359119),
+    h = n(473092),
+    p = n(177342),
+    m = n(621600),
+    f = n(870991),
+    g = n(981631),
+    C = n(388032),
+    x = n(162787);
+function v(e) {
+    let { senderId: t, channelId: n, warningId: l, onClick: r } = e;
     return (0, i.jsxs)('div', {
-        className: _.safetyAction,
+        className: x.safetyAction,
         children: [
             (0, i.jsxs)('div', {
-                className: _.safetyActionText,
+                className: x.safetyActionText,
                 children: [
-                    (0, i.jsx)(s.Text, {
+                    (0, i.jsx)(a.Text, {
                         variant: 'text-md/semibold',
                         color: 'header-primary',
-                        children: v.intl.string(v.t['UVp+4+'])
+                        children: C.intl.string(C.t['UVp+4+'])
                     }),
-                    (0, i.jsx)(s.Text, {
+                    (0, i.jsx)(a.Text, {
                         variant: 'text-xs/medium',
                         color: 'header-secondary',
-                        children: v.intl.string(v.t.VS47BA)
+                        children: C.intl.string(C.t.VS47BA)
                     })
                 ]
             }),
-            (0, i.jsx)(s.Button, {
-                size: s.Button.Sizes.SMALL,
-                color: s.Button.Colors.PRIMARY,
-                'aria-label': r ? v.intl.string(v.t['6QK4Hx']) : v.intl.string(v.t['UVp+4+']),
-                onClick: r
-                    ? () => {
-                          o.Z.updateChannelOverrideSettings(null, n, { muted: !1 }, g.ZB.Unmuted),
-                              d.Z.showUnmuteSuccessToast(t, n),
-                              (0, p.qc)({
-                                  channelId: n,
-                                  warningId: l,
-                                  senderId: t,
-                                  warningType: h.pj.STRANGER_DANGER,
-                                  cta: p.NM.USER_MODAL_UNMUTE
-                              });
-                      }
-                    : () => {
-                          o.Z.updateChannelOverrideSettings(null, n, { muted: !0 }, g.ZB.Muted),
-                              d.Z.showMuteSuccessToast(t, n),
-                              (0, p.qc)({
-                                  channelId: n,
-                                  warningId: l,
-                                  senderId: t,
-                                  warningType: h.pj.STRANGER_DANGER,
-                                  cta: p.NM.USER_MODAL_MUTE
-                              });
-                      },
-                children: r ? v.intl.string(v.t['6QK4Hx']) : v.intl.string(v.t['UVp+4+'])
+            (0, i.jsx)(a.Button, {
+                size: a.Button.Sizes.SMALL,
+                color: a.Button.Colors.PRIMARY,
+                'aria-label': C.intl.string(C.t.ftIK2N),
+                onClick: () => {
+                    s.Z.updateChannelOverrideSettings(null, n, { muted: !0 }, m.ZB.Muted),
+                        c.Z.showMuteSuccessToast(t, n),
+                        (0, h.qc)({
+                            channelId: n,
+                            warningId: l,
+                            senderId: t,
+                            warningType: u.pj.STRANGER_DANGER,
+                            cta: h.NM.USER_MODAL_MUTE
+                        }),
+                        r();
+                },
+                children: C.intl.string(C.t['UVp+4+'])
             })
         ]
     });
 }
-function E(e) {
-    let { channelId: t, warningId: a, senderId: o } = e,
-        d = (0, C.E4)(),
-        f = l.useCallback(() => {
-            (0, u.T)(t, [a]);
-        }, [t, a]),
-        g = () => {
-            (0, s.openModalLazy)(async () => {
+function _(e) {
+    let { channelId: t, warningId: s, senderId: c } = e,
+        m = (0, f.E4)(),
+        _ = l.useCallback(() => {
+            (0, d.T)(t, [s]);
+        }, [t, s]),
+        I = () => {
+            (0, a.openModalLazy)(async () => {
                 let { default: e } = await n.e('59385').then(n.bind(n, 480884));
                 return (n) => {
                     let { transitionState: l, onClose: r } = n;
@@ -89,22 +75,22 @@ function E(e) {
                         transitionState: l,
                         onClose: r,
                         channelId: t,
-                        warningId: a,
-                        senderId: o,
-                        description: v.intl.string(v.t['/uid3t']),
-                        safetyTips: d.map((e, t) =>
+                        warningId: s,
+                        senderId: c,
+                        description: C.intl.string(C.t['/uid3t']),
+                        safetyTips: m.map((e, t) =>
                             (0, i.jsxs)(
                                 'div',
                                 {
-                                    className: _.tipContent,
+                                    className: x.tipContent,
                                     children: [
-                                        (0, i.jsx)(s.Text, {
+                                        (0, i.jsx)(a.Text, {
                                             variant: 'text-md/medium',
                                             color: 'header-primary',
                                             children: e.title
                                         }),
                                         (0, i.jsx)(
-                                            s.Text,
+                                            a.Text,
                                             {
                                                 variant: 'text-sm/medium',
                                                 color: 'header-secondary',
@@ -117,46 +103,49 @@ function E(e) {
                                 t
                             )
                         ),
-                        actions: (0, i.jsx)(I, {
-                            senderId: o,
+                        actions: (0, i.jsx)(v, {
+                            senderId: c,
                             channelId: t,
-                            warningId: a
+                            warningId: s,
+                            onClick: () => {
+                                _(), r();
+                            }
                         }),
-                        learnMore: v.intl.format(v.t.UkH129, { learnMoreLink: C.D8 })
+                        learnMore: C.intl.format(C.t.UkH129, { learnMoreLink: f.D8 })
                     });
                 };
             });
         };
     return (
         l.useEffect(() => {
-            (0, p.MC)(x.rMx.SAFETY_WARNING_VIEWED, {
+            (0, h.MC)(g.rMx.SAFETY_WARNING_VIEWED, {
                 channelId: t,
-                warningId: a,
-                senderId: o,
-                warningType: h.pj.LIKELY_ATO
+                warningId: s,
+                senderId: c,
+                warningType: u.pj.LIKELY_ATO
             }),
-                c.Z.increment({ name: r.V.SAFETY_WARNING_VIEW });
-        }, [t, a, o]),
-        (0, i.jsx)(m.Q, {
+                o.Z.increment({ name: r.V.SAFETY_WARNING_VIEW });
+        }, [t, s, c]),
+        (0, i.jsx)(p.Q, {
             channelId: t,
-            warningId: a,
-            senderId: o,
-            warningType: h.pj.LIKELY_ATO,
-            header: v.intl.string(v.t.R8UsiI),
-            description: v.intl.string(v.t.lI8nQk),
-            onDismiss: f,
+            warningId: s,
+            senderId: c,
+            warningType: u.pj.LIKELY_ATO,
+            header: C.intl.string(C.t.R8UsiI),
+            description: C.intl.string(C.t.lI8nQk),
+            onDismiss: _,
             buttons: [
                 {
-                    text: v.intl.string(v.t.tC1pvL),
-                    color: s.Button.Colors.BRAND,
+                    text: C.intl.string(C.t.tC1pvL),
+                    color: a.Button.Colors.BRAND,
                     onclick: () => {
-                        g(),
-                            (0, p.qc)({
+                        I(),
+                            (0, h.qc)({
                                 channelId: t,
-                                warningId: a,
-                                senderId: o,
-                                warningType: h.pj.LIKELY_ATO,
-                                cta: p.NM.OPEN_MORE_TIPS
+                                warningId: s,
+                                senderId: c,
+                                warningType: u.pj.LIKELY_ATO,
+                                cta: h.NM.OPEN_MORE_TIPS
                             });
                     }
                 }
