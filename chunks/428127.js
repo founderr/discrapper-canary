d668e817f2f0e652d7e4350d9ed3ad4ddf304619
DiscrapperManager <admin@minerpl.xyz>
2024-12-10 18:55:46 +0000

    canaryRelease: 352078
    Main scripts: 3
    Lazy Loaded Scripts: 913
    Chunks:
     * Main Files: 11896
     * Scripts: 10285
    Strings: 0
    Routes: 684
    Experiments Count: 381

--- chunks/428127.js
+++ chunks/428127.js
@@ -1,52 +1,53 @@
 n.d(t, {
     Z: function () {
-        return x;
+        return I;
     }
 });
 var i = n(200651),
     l = n(192379),
     r = n(120356),
-    o = n.n(r),
-    a = n(752877),
+    a = n.n(r),
+    o = n(752877),
     s = n(442837),
     c = n(692547),
     u = n(481060),
-    d = n(492162),
-    h = n(237583),
-    p = n(451478),
-    m = n(934415),
-    f = n(968847),
-    g = n(388032),
-    C = n(706091);
-let _ = {
+    d = n(540059),
+    h = n(492162),
+    p = n(237583),
+    m = n(451478),
+    f = n(934415),
+    g = n(968847),
+    C = n(388032),
+    _ = n(706091);
+let v = {
     friction: 30,
     tension: 300
 };
-function v(e) {
+function x(e) {
     var t;
     let { guildChannels: n, jumpToVoiceChannels: r } = e,
-        a = n.getCategoryFromSection(n.voiceChannelsSectionNumber),
-        s = (0, d.Z)(n.id),
-        p = l.useCallback(
+        o = n.getCategoryFromSection(n.voiceChannelsSectionNumber),
+        s = (0, h.Z)(n.id),
+        d = l.useCallback(
             (e) => {
                 e.preventDefault(), e.stopPropagation(), r();
             },
             [r]
         ),
-        f = null !== (t = null == a ? void 0 : a.getChannelRecords()) && void 0 !== t ? t : [],
-        _ = (0, m.c4)({
-            channels: f,
+        m = null !== (t = null == o ? void 0 : o.getChannelRecords()) && void 0 !== t ? t : [],
+        g = (0, f.c4)({
+            channels: m,
             selectedChannelId: null,
             selectedVoiceChannelId: null,
             voiceStates: s
         });
     return (0, i.jsxs)(u.Clickable, {
-        className: o()(C.bar),
-        onClick: p,
+        className: a()(_.bar),
+        onClick: d,
         children: [
             (0, i.jsx)(u.VoiceNormalIcon, {
                 size: 'custom',
-                className: C.voiceChannelsIcon,
+                className: _.voiceChannelsIcon,
                 width: 14,
                 height: 14,
                 color: c.Z.unsafe_rawColors.GREEN_330.css
@@ -54,13 +55,13 @@ function v(e) {
             (0, i.jsx)(u.Text, {
                 variant: 'text-xs/semibold',
                 color: 'text-positive',
-                className: C.barText,
-                children: g.intl.format(g.t['fDlr+P'], { count: _.length })
+                className: _.barText,
+                children: C.intl.format(C.t['fDlr+P'], { count: g.length })
             }),
-            (0, i.jsx)(h.Z, {
+            (0, i.jsx)(p.Z, {
                 guildId: n.id,
-                className: C.voiceChannelsUsers,
-                users: _.slice(0, 4),
+                className: _.voiceChannelsUsers,
+                users: g.slice(0, 4),
                 renderMoreUsers: () => null,
                 max: 4,
                 size: u.AvatarSizes.SIZE_16
@@ -68,78 +69,79 @@ function v(e) {
         ]
     });
 }
-function x(e) {
-    let { position: t, guildChannels: n, guildChannelsVersion: r, jumpToVoiceChannels: c, jumpToChannel: d } = e,
-        { bottomBar: h, topBar: m } = (0, s.cj)([f.Z], () => f.Z.getUnreadStateForGuildId(n.id)),
-        x = (0, s.e7)([p.Z], () => p.Z.isFocused()),
-        { mode: I, mentionCount: b, targetChannelId: E } = 'bottom' === t ? h : m,
-        S = I === f.x.HIDDEN,
-        Z = (0, u.useSpring)(
+function I(e) {
+    let { position: t, guildChannels: n, guildChannelsVersion: r, jumpToVoiceChannels: c, jumpToChannel: h } = e,
+        p = (0, d.Q3)('UnreadBar'),
+        { bottomBar: f, topBar: I } = (0, s.cj)([g.Z], () => g.Z.getUnreadStateForGuildId(n.id)),
+        b = (0, s.e7)([m.Z], () => m.Z.isFocused()),
+        { mode: E, mentionCount: S, targetChannelId: Z } = 'bottom' === t ? f : I,
+        N = E === g.x.HIDDEN,
+        y = (0, u.useSpring)(
             {
-                to: { transform: S ? ('bottom' === t ? 'translateY(180%)' : 'translateY(-180%)') : 'translateY(0%)' },
-                config: _
+                to: { transform: N ? ('bottom' === t ? 'translateY(180%)' : 'translateY(-180%)') : 'translateY(0%)' },
+                config: v
             },
-            x ? 'respect-motion-settings' : 'animate-never'
+            b ? 'respect-motion-settings' : 'animate-never'
         ),
-        N = l.useCallback(
+        T = l.useCallback(
             (e) => {
-                e.preventDefault(), e.stopPropagation(), null != E && d(E);
+                e.preventDefault(), e.stopPropagation(), null != Z && h(Z);
             },
-            [d, E]
+            [h, Z]
         );
     return (0, i.jsx)('div', {
-        className: o()(C.container, {
-            [C.top]: 'top' === t,
-            [C.bottom]: 'bottom' === t
+        className: a()(_.container, {
+            [_.top]: 'top' === t,
+            [_.bottom]: 'bottom' === t
         }),
-        children: (0, i.jsx)(a.animated.div, {
-            className: C.containerPadding,
-            style: Z,
-            'aria-hidden': S,
+        children: (0, i.jsx)(o.animated.div, {
+            className: _.containerPadding,
+            style: y,
+            'aria-hidden': N,
             children:
-                I === f.x.HIDDEN
-                    ? (0, i.jsx)('div', { className: o()(C.bar, C.emptyBar) })
-                    : I === f.x.UNREAD
+                E === g.x.HIDDEN
+                    ? (0, i.jsx)('div', { className: a()(_.bar, _.emptyBar) })
+                    : E === g.x.UNREAD
                       ? (0, i.jsxs)(u.Clickable, {
-                            className: C.bar,
-                            onClick: N,
+                            className: _.bar,
+                            onClick: T,
                             children: [
                                 'bottom' === t
                                     ? (0, i.jsx)(u.ChevronSmallDownIcon, {
                                           size: 'custom',
                                           color: 'currentColor',
-                                          className: C.unreadIcon,
+                                          className: _.unreadIcon,
                                           height: 14,
                                           width: 14
                                       })
                                     : (0, i.jsx)(u.ChevronSmallUpIcon, {
                                           size: 'custom',
                                           color: 'currentColor',
-                                          className: C.unreadIcon,
+                                          className: _.unreadIcon,
                                           height: 14,
                                           width: 14
                                       }),
                                 (0, i.jsx)(u.Text, {
                                     variant: 'text-xs/semibold',
                                     color: 'interactive-normal',
-                                    className: C.barText,
-                                    children: g.intl.string(g.t.FCRiT0)
+                                    className: _.barText,
+                                    children: C.intl.string(C.t.FCRiT0)
                                 })
                             ]
                         })
-                      : I === f.x.MENTIONS
+                      : E === g.x.MENTIONS
                         ? (0, i.jsx)(u.Clickable, {
-                              className: o()(C.bar, C.mentionsBar),
-                              onClick: N,
+                              className: a()(_.bar, _.mentionsBar),
+                              onClick: T,
                               children: (0, i.jsx)(u.Text, {
                                   variant: 'text-xs/semibold',
-                                  color: 'status-danger-text',
-                                  className: C.barText,
-                                  children: g.intl.format(g.t.EQcLys, { count: b })
+                                  color: p ? 'text-primary' : 'status-danger-text',
+                                  className: _.barText,
+                                  children: C.intl.format(C.t.EQcLys, { count: S })
                               })
                           })
-                        : I === f.x.VOICE_CHANNELS
-                          ? (0, i.jsx)(v, {
+                        : E === g.x.VOICE_CHANNELS
+                          ? (0, i.jsx)(x, {
                                 jumpToVoiceChannels: c,
                                 guildChannels: n,
                                 guildChannelsVersion: r
