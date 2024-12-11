6bfde73356fed9173cbe1b4c83e660388c7774e2
DiscrapperManager <admin@minerpl.xyz>
2024-12-11 20:24:24 +0000

    canaryRelease: 352588
    Main scripts: 3
    Lazy Loaded Scripts: 914
    Chunks:
     * Main Files: 11899
     * Scripts: 10308
    Strings: 0
    Routes: 684
    Experiments Count: 382

--- chunks/955839.js
+++ chunks/955839.js
@@ -1,57 +1,71 @@
 r.d(t, {
     q: function () {
-        return o;
+        return d;
     }
 }),
     r(47120),
     r(653041);
 var i = r(192379),
-    s = r(442837),
-    a = r(232567),
-    n = r(281494),
-    l = r(276444);
-let o = (e) => {
-    let { searchQuery: t, selectedUsers: r, limit: o } = e,
-        C = (0, s.e7)([l.Z], () => l.Z.getRecipientStatus()),
-        [d, c] = i.useState(0),
-        [x, h] = i.useState([]),
-        [p, f] = i.useState(!1),
+    s = r(512722),
+    a = r.n(s),
+    n = r(442837),
+    l = r(232567),
+    o = r(281494),
+    C = r(276444);
+let d = (e) => {
+    let { searchQuery: t, selectedUsers: r, limit: s } = e,
+        d = (0, n.e7)([C.Z], () => C.Z.getRecipientStatus()),
+        c = (0, n.e7)([C.Z], () => C.Z.getReferralsRemaining()),
+        [x, f] = i.useState(0),
+        [h, p] = i.useState([]),
         [u, m] = i.useState(!1),
-        [_, j] = i.useState(new Map());
-    i.useEffect(() => {
-        g(0, o);
-    }, [t]);
-    let g = async (e, i) => {
-        if (!p && !u && null != e)
-            try {
-                f(!0);
-                let s = [..._.values()];
-                for (let [e, t] of C)
-                    if (t === n.Fe.PENDING && !_.has(e)) {
-                        let t = await (0, a.PR)(e);
-                        s.push(t);
-                    }
-                let l = await (0, n.iF)(e, t, i);
-                h((t) => {
-                    let i = l.users.filter((e) => !r.has(e.id));
-                    return ((s = s.filter((e) => !r.has(e.id))), 0 === e) ? [...r.values(), ...s.values(), ...i] : [...t, ...i];
-                }),
-                    j((e) => {
-                        for (let t of s) e.set(t.id, t);
-                        return e;
+        [_, j] = i.useState(!1),
+        [g, L] = i.useState(new Map());
+    a()(null != c, 'Referrals remaining should not be null'),
+        i.useEffect(() => {
+            c > 0 ? y(0, s) : S();
+        }, [t, c]);
+    let S = async () => {
+            let e = new Map();
+            for (let [t, r] of d)
+                if (r === o.Fe.PENDING && !g.has(t)) {
+                    let r = await (0, l.PR)(t);
+                    e.set(r.id, r);
+                }
+            L(e), p(Array.from(e.values()));
+        },
+        y = async (e, i) => {
+            if (!u && !_ && null != e && 0 !== c)
+                try {
+                    m(!0);
+                    let s = [...g.values()];
+                    for (let [e, t] of d)
+                        if (t === o.Fe.PENDING && !g.has(e)) {
+                            let t = await (0, l.PR)(e);
+                            s.push(t);
+                        }
+                    let a = await (0, o.iF)(e, t, i);
+                    p((t) => {
+                        let i = a.users.filter((e) => !r.has(e.id));
+                        return ((s = s.filter((e) => !r.has(e.id))), 0 === e) ? [...r.values(), ...s.values(), ...i] : [...t, ...i];
                     }),
-                    c(l.nextIndex);
-            } catch (e) {
-                m(!0);
-            } finally {
-                f(!1);
-            }
-    };
+                        L((e) => {
+                            let t = new Map(e);
+                            for (let e of s) t.set(e.id, e);
+                            return t;
+                        }),
+                        f(a.nextIndex);
+                } catch (e) {
+                    j(!0);
+                } finally {
+                    m(!1);
+                }
+        };
     return {
-        eligibleUsers: x,
-        fetchUsers: () => g(d, o),
-        hasError: u,
-        isFetching: p,
-        resendUsers: _
+        eligibleUsers: h,
+        fetchUsers: () => y(x, s),
+        hasError: _,
+        isFetching: u,
+        resendUsers: g
     };
 };
