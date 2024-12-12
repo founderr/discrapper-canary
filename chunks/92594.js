f414979702c3484e725bfbb7300d40334ca7175c
DiscrapperManager <admin@minerpl.xyz>
2024-12-12 03:36:42 +0000

    canaryRelease: 352858
    Main scripts: 3
    Lazy Loaded Scripts: 913
    Chunks:
     * Main Files: 11906
     * Scripts: 10306
    Strings: 0
    Routes: 684
    Experiments Count: 384

--- chunks/92594.js
+++ chunks/92594.js
@@ -1,78 +1,80 @@
 n.d(t, {
     t: function () {
-        return f;
+        return j;
     }
 }),
     n(47120);
 var i = n(200651),
     l = n(192379),
-    s = n(570140),
-    r = n(366939),
-    a = n(16084),
-    o = n(128069),
-    C = n(643939),
-    d = n(45595),
-    u = n(823188),
-    c = n(563132),
-    p = n(474936),
-    x = n(388032),
-    h = n(988113);
-function f() {
-    let { purchaseError: e, paymentError: t, setPurchaseError: n, selectedSkuId: f, selectedPlan: _, setSelectedSkuId: j, setSelectedPlanId: m, setSelectedPlanNotification: L } = (0, c.usePaymentContext)(),
-        [g, E] = l.useState(!1),
-        [S, I] = l.useState(!1),
-        [M, v] = l.useState(!1);
+    s = n(120356),
+    r = n.n(s),
+    a = n(570140),
+    o = n(366939),
+    C = n(16084),
+    d = n(128069),
+    u = n(643939),
+    c = n(45595),
+    p = n(823188),
+    x = n(563132),
+    h = n(474936),
+    f = n(388032),
+    _ = n(988113);
+function j() {
+    let { purchaseError: e, paymentError: t, setPurchaseError: n, selectedSkuId: s, selectedPlan: j, setSelectedSkuId: m, setSelectedPlanId: L, setSelectedPlanNotification: g } = (0, x.usePaymentContext)(),
+        [E, S] = l.useState(!1),
+        [I, M] = l.useState(!1),
+        [v, y] = l.useState(!1);
     return (
         l.useEffect(() => {
             let n = setTimeout(() => {
-                if (f === p.Si.TIER_2) ((null == e ? void 0 : e.code) === o.SM.BILLING_INSUFFICIENT_FUNDS || (null == t ? void 0 : t.code) === o.SM.BILLING_INSUFFICIENT_FUNDS) && v(!0);
+                if (s === h.Si.TIER_2) ((null == e ? void 0 : e.code) === d.SM.BILLING_INSUFFICIENT_FUNDS || (null == t ? void 0 : t.code) === d.SM.BILLING_INSUFFICIENT_FUNDS) && y(!0);
             }, 1500);
             return () => clearTimeout(n);
-        }, [t, e, f, v]),
+        }, [t, e, s, y]),
         (0, i.jsxs)('div', {
-            className: h.upsellContainer,
-            hidden: S,
+            className: r()(_.upsellContainer, v ? _.interactable : null),
+            hidden: I,
             children: [
-                (0, i.jsx)(d.H, {}),
-                (0, i.jsx)(d.Z, {
-                    show: M,
-                    children: (0, i.jsx)(C.L, {
-                        message: x.intl.format(x.t.dk8zl5, {
+                (0, i.jsx)(c.H, {}),
+                (0, i.jsx)(c.Z, {
+                    show: v,
+                    children: (0, i.jsx)(u.L, {
+                        message: f.intl.format(f.t.dk8zl5, {
                             perkHover: (e, t) =>
                                 (0, i.jsx)(
                                     'span',
                                     {
-                                        onMouseEnter: () => E(!0),
-                                        onMouseLeave: () => E(!1),
+                                        onMouseEnter: () => S(!0),
+                                        onMouseLeave: () => S(!1),
                                         children: e
                                     },
                                     'post-failure-nitro-basic-upsell-perk-hover'
                                 )
                         }),
                         handleClick: () => {
-                            n(null), s.Z.wait(a.pB), s.Z.wait(r.fw), L(x.intl.string(x.t['/5p4ws'])), j(p.Si.TIER_0);
-                            let e = p.Xh.PREMIUM_MONTH_TIER_0;
-                            (null == _ ? void 0 : _.id) === p.Xh.PREMIUM_YEAR_TIER_2 && (e = p.Xh.PREMIUM_YEAR_TIER_0), m(e), I(!0);
+                            n(null), a.Z.wait(C.pB), a.Z.wait(o.fw), g(f.intl.string(f.t['/5p4ws'])), m(h.Si.TIER_0);
+                            let e = h.Xh.PREMIUM_MONTH_TIER_0;
+                            (null == j ? void 0 : j.id) === h.Xh.PREMIUM_YEAR_TIER_2 && (e = h.Xh.PREMIUM_YEAR_TIER_0), L(e), M(!0);
                         },
-                        handleClose: () => I(!0),
+                        handleClose: () => M(!0),
                         handleImageClick: () => {},
-                        buttonText: x.intl.string(x.t['7aa9vr']),
+                        buttonText: f.intl.string(f.t['7aa9vr']),
                         useInitialGlow: !0,
                         useGlowOnHover: !0,
                         upsellType: 'post-failure-nitro-basic-upsell',
-                        themeOverride: C.T.NITRO_BASIC
+                        themeOverride: u.T.NITRO_BASIC
                     })
                 }),
-                g &&
+                E &&
                     (0, i.jsxs)('div', {
-                        className: h.tooltip,
+                        className: _.tooltip,
                         children: [
-                            (0, i.jsx)(u.lq, {
-                                className: h.tier0Card,
+                            (0, i.jsx)(p.lq, {
+                                className: _.tier0Card,
                                 showWumpus: !0,
                                 showYearlyPrice: !0
                             }),
-                            (0, i.jsx)('div', { className: h.tooltipTail })
+                            (0, i.jsx)('div', { className: _.tooltipTail })
                         ]
                     })
             ]
