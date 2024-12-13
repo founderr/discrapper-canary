30779c0944ef844c59c93bce47ff511b32bdb66a
DiscrapperManager <admin@minerpl.xyz>
2024-12-13 18:24:52 +0000

    canaryRelease: 353478
    Main scripts: 3
    Lazy Loaded Scripts: 912
    Chunks:
     * Main Files: 11899
     * Scripts: 10318
    Strings: 0
    Routes: 685
    Experiments Count: 381

--- chunks/419672.js
+++ chunks/419672.js
@@ -1,6 +1,6 @@
 n(47120);
-var i = n(200651),
-    o = n(192379),
+var o = n(200651),
+    i = n(192379),
     s = n(481060),
     l = n(230711),
     r = n(187819),
@@ -14,12 +14,12 @@ var i = n(200651),
     x = n(683913);
 e.Z = function (t) {
     let { onClose: e, channelId: n } = t,
-        [A, b] = o.useState(!1),
+        [A, b] = i.useState(!1),
         p = (0, r._p)({ location: 'safety-tools-about-safety-alerts' }),
-        E = o.useCallback(() => {
+        E = i.useCallback(() => {
             p ? l.Z.open(T.oAB.CONTENT_AND_SOCIAL, null, { scrollPosition: S.to.DM_SAFETY_ALERTS_V2 }) : l.Z.open(T.oAB.PRIVACY_AND_SAFETY, null, { scrollPosition: S.to.DM_SAFETY_ALERTS }), e();
         }, [p, e]),
-        R = o.useCallback(() => {
+        h = i.useCallback(() => {
             !A &&
                 (b(!0),
                 (0, c.Uj)(n)
@@ -34,27 +34,28 @@ e.Z = function (t) {
                         b(!1), (0, s.showToast)((0, s.createToast)(_.intl.string(_.t.R0RpRU), s.ToastType.FAILURE));
                     }));
         }, [n, e, A]);
-    return (0, i.jsxs)(d.Z, {
+    return (0, o.jsxs)(d.Z, {
         style: x.wrapperStyle,
         children: [
-            (0, i.jsx)(s.Text, {
-                variant: 'text-md/medium',
+            (0, o.jsx)(s.Text, {
+                variant: 'text-md/normal',
+                color: 'header-secondary',
                 children: _.intl.format(_.t['njJ/Cg'], { learnMoreLink: a.Z.getArticleURL(T.BhN.SAFETY_ALERTS) })
             }),
-            (0, i.jsx)(s.Button, {
+            (0, o.jsx)(s.Button, {
                 className: x.settingsButton,
                 onClick: E,
                 fullWidth: !0,
                 children: _.intl.string(_.t.qpWnyM)
             }),
-            (0, i.jsx)('div', {
-                children: (0, i.jsx)(s.Text, {
+            (0, o.jsx)('div', {
+                children: (0, o.jsx)(s.Text, {
                     variant: 'text-sm/normal',
                     color: 'text-muted',
                     className: x.reportFalsePositiveText,
                     children: _.intl.format(_.t['2uYViI'], {
                         reportFalsePositiveHook: (t, e) =>
-                            (0, i.jsx)(
+                            (0, o.jsx)(
                                 s.Button,
                                 {
                                     className: x.reportFalsePositiveButton,
@@ -62,7 +63,7 @@ e.Z = function (t) {
                                     color: s.Button.Colors.LINK,
                                     look: s.Button.Looks.LINK,
                                     disabled: A,
-                                    onClick: R,
+                                    onClick: h,
                                     children: t
                                 },
                                 e
