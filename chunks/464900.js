649072085cfc642ef3683b1975a386d81c9a8c79
DiscrapperManager <admin@minerpl.xyz>
2024-12-16 23:48:18 +0000

    canaryRelease: 354219
    Main scripts: 3
    Lazy Loaded Scripts: 912
    Chunks:
     * Main Files: 11906
     * Scripts: 10328
    Strings: 0
    Routes: 687
    Experiments Count: 383

---
+++ chunks/464900.js
@@ -0,0 +1,13 @@
+(0, r(818083).B)({
+    kind: 'user',
+    id: '2024-12_uplift_trial_nux',
+    label: 'Uplift Trial NUX',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Users will see the uplift trial NUX',
+            config: { enabled: !0 }
+        }
+    ]
+});
