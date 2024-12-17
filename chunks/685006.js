6cdcf6556ccb3f728826fbc49f7699583bd1697c
DiscrapperManager <admin@minerpl.xyz>
2024-12-17 16:38:21 +0000

    canaryRelease: 354392
    Main scripts: 3
    Lazy Loaded Scripts: 912
    Chunks:
     * Main Files: 11906
     * Scripts: 10325
    Strings: 0
    Routes: 686
    Experiments Count: 382

--- chunks/685006.js
+++ chunks/685006.js
@@ -1,6 +1,6 @@
 n.d(t, {
     Z: function () {
-        return _;
+        return I;
     }
 });
 var i = n(200651),
@@ -8,80 +8,82 @@ var i = n(200651),
     r = n(120356),
     a = n.n(r),
     s = n(481060),
-    o = n(100527),
-    c = n(367907),
-    d = n(450936),
-    u = n(368478),
-    h = n(499254),
-    p = n(827498),
-    m = n(213459),
-    f = n(541716),
-    g = n(61356),
-    C = n(981631),
-    x = n(314734),
-    v = n(700559);
-function _(e) {
+    o = n(911969),
+    c = n(100527),
+    d = n(367907),
+    u = n(450936),
+    h = n(368478),
+    p = n(499254),
+    m = n(827498),
+    f = n(213459),
+    g = n(541716),
+    C = n(61356),
+    x = n(981631),
+    v = n(314734),
+    _ = n(700559);
+function I(e) {
     let { channel: t, entryPointCommandButtonRef: n, type: r } = e,
-        { isInitialLoading: a, application: s, primaryEntryPointCommand: u } = (0, d.Z)({ channel: t }),
-        x = null == s ? void 0 : s.id,
-        v = (0, g.Z)({ type: r }),
-        { sectionCommands: _ } = (0, m.If)(t, null != x ? x : ''),
-        E = l.useCallback(() => {
-            v
-                ? h.yT(p.ti.DISMISSED)
-                : (h.__(p._b.TEXT, f.Ie.NORMAL, { applicationId: x }),
-                  (0, c.yw)(C.rMx.APPLICATION_COMMAND_SECTION_SELECTED, {
-                      application_id: x,
-                      location: o.Z.APP_DMS_ENTRY_POINT_COMMAND_BUTTON,
-                      source: p._b.TEXT
+        { isInitialLoading: a, application: s, primaryEntryPointCommand: h } = (0, u.Z)({ channel: t }),
+        v = null == s ? void 0 : s.id,
+        _ = (0, C.Z)({ type: r }),
+        { sectionCommands: I } = (0, f.If)(t, null != v ? v : ''),
+        b = l.useCallback(() => {
+            _
+                ? p.yT(m.ti.DISMISSED)
+                : (p.__(m._b.TEXT, g.Ie.NORMAL, { applicationId: v }),
+                  (0, d.yw)(x.rMx.APPLICATION_COMMAND_SECTION_SELECTED, {
+                      application_id: v,
+                      location: c.Z.APP_DMS_ENTRY_POINT_COMMAND_BUTTON,
+                      source: m._b.TEXT
                   }));
-        }, [x, v]);
-    return a || null == s || null == _ || 0 === _.length
+        }, [v, _]),
+        Z = null != I && I.filter((e) => e.type === o.yU.CHAT || e.type === o.yU.PRIMARY_ENTRY_POINT).length > 0;
+    return a || null == s || !1 === Z
         ? null
-        : (0, i.jsx)(I, {
+        : (0, i.jsx)(E, {
               channel: t,
               application: s,
-              primaryEntryPointCommand: u,
-              onOpenButtonPress: E,
+              primaryEntryPointCommand: h,
+              onOpenButtonPress: b,
               entryPointCommandButtonRef: n,
               type: r
           });
 }
-function I(e) {
+function E(e) {
     let { channel: t, application: n, primaryEntryPointCommand: l, onOpenButtonPress: r, entryPointCommandButtonRef: o, type: c } = e,
-        d = (0, g.Z)({ type: c }),
+        d = (0, C.Z)({ type: c }),
         {
-            isExecutingLaunchInteraction: h,
+            isExecutingLaunchInteraction: u,
             buttonText: p,
             hasActiveMatchingEmbeddedActivity: m,
             isEmbeddedApp: f,
-            currentEmbeddedActivity: C,
-            channelRecipientUserId: _,
+            currentEmbeddedActivity: g,
+            channelRecipientUserId: x,
             setIsExecutingLaunchInteraction: I,
             analyticsLocations: E
-        } = (0, u.Z)({
+        } = (0, h.Z)({
             channel: t,
             primaryEntryPointCommand: l,
             application: n,
             showAppLauncherPopup: d
         }),
-        b = (0, u.J)({
+        b = (0, h.J)({
             channel: t,
             application: n,
             isEmbeddedApp: f,
             hasActiveMatchingEmbeddedActivity: m,
-            currentEmbeddedActivity: C,
+            currentEmbeddedActivity: g,
             onOpenButtonPress: r,
-            channelRecipientUserId: _,
+            channelRecipientUserId: x,
             setIsExecutingLaunchInteraction: I,
             analyticsLocations: E
         }),
         Z = m ? s.Button.Colors.RED : s.Button.Colors.BRAND;
     return (0, i.jsx)(s.Button, {
-        className: a()(v.entryPointAppCommandButton, x.Id),
+        className: a()(_.entryPointAppCommandButton, v.Id),
         size: s.Button.Sizes.LARGE,
         color: Z,
-        submitting: h,
+        submitting: u,
         onClick: b,
         buttonRef: o,
         children: p
