b5e84b9b39ead37e4895dbad77ea66393496bdfc
DiscrapperManager <admin@minerpl.xyz>
2024-12-18 21:36:54 +0000

    canaryRelease: 354982
    Main scripts: 3
    Lazy Loaded Scripts: 921
    Chunks:
     * Main Files: 11938
     * Scripts: 10322
    Strings: 0
    Routes: 687
    Experiments Count: 374

--- chunks/453470.js
+++ chunks/453470.js
@@ -1,6 +1,6 @@
 n.d(t, {
     M: function () {
-        return _;
+        return E;
     }
 });
 var i = n(200651),
@@ -15,59 +15,62 @@ var i = n(200651),
     h = n(473092),
     p = n(177342),
     m = n(621600),
-    f = n(870991),
-    g = n(981631),
-    C = n(388032),
-    x = n(162787);
-function v(e) {
+    f = n(276060),
+    g = n(870991),
+    C = n(981631),
+    x = n(134612),
+    v = n(388032),
+    _ = n(162787);
+function I(e) {
     let { onClick: t } = e;
     return (0, i.jsxs)('div', {
-        className: x.safetyAction,
+        className: _.safetyAction,
         children: [
             (0, i.jsxs)('div', {
-                className: x.safetyActionText,
+                className: _.safetyActionText,
                 children: [
                     (0, i.jsx)(a.Text, {
                         variant: 'text-md/semibold',
                         color: 'header-primary',
-                        children: C.intl.string(C.t.ftIK2N)
+                        children: v.intl.string(v.t.ftIK2N)
                     }),
                     (0, i.jsx)(a.Text, {
                         variant: 'text-xs/medium',
                         color: 'header-secondary',
-                        children: C.intl.string(C.t.w2ve0t)
+                        children: v.intl.string(v.t.w2ve0t)
                     })
                 ]
             }),
             (0, i.jsx)(a.Button, {
                 size: a.Button.Sizes.SMALL,
                 color: a.Button.Colors.PRIMARY,
-                'aria-label': C.intl.string(C.t.ftIK2N),
+                'aria-label': v.intl.string(v.t.ftIK2N),
                 onClick: t,
-                children: C.intl.string(C.t.ftIK2N)
+                children: v.intl.string(v.t.ftIK2N)
             })
         ]
     });
 }
-function _(e) {
-    let { channelId: t, warningId: _, senderId: I } = e,
-        E = (0, f.E4)(),
-        b = l.useCallback(() => {
-            (0, d.T)(t, [_]);
-        }, [t, _]),
-        Z = (e) => {
+function E(e) {
+    let { channelId: t, warningId: E, senderId: b } = e,
+        Z = (0, g.E4)(),
+        N = l.useCallback(() => {
+            (0, d.T)(t, [E]);
+        }, [t, E]),
+        S = (0, f.t)({ location: x.zr }),
+        T = (e) => {
             s.Z.updateChannelOverrideSettings(null, t, { muted: !0 }, m.ZB.Muted),
-                c.Z.showMuteSuccessToast(I, t),
+                c.Z.showMuteSuccessToast(b, t),
                 (0, h.qc)({
                     channelId: t,
-                    warningId: _,
-                    senderId: I,
+                    warningId: E,
+                    senderId: b,
                     warningType: u.pj.LIKELY_ATO,
                     cta: e
                 }),
-                b();
+                N();
         },
-        N = () => {
+        j = () => {
             (0, a.openModalLazy)(async () => {
                 let { default: e } = await n.e('59385').then(n.bind(n, 480884));
                 return (n) => {
@@ -76,14 +79,14 @@ function _(e) {
                         transitionState: l,
                         onClose: r,
                         channelId: t,
-                        warningId: _,
-                        senderId: I,
-                        description: C.intl.string(C.t['/uid3t']),
-                        safetyTips: E.map((e, t) =>
+                        warningId: E,
+                        senderId: b,
+                        description: v.intl.string(v.t['/uid3t']),
+                        safetyTips: Z.map((e, t) =>
                             (0, i.jsxs)(
                                 'div',
                                 {
-                                    className: x.tipContent,
+                                    className: _.tipContent,
                                     children: [
                                         (0, i.jsx)(a.Text, {
                                             variant: 'text-md/medium',
@@ -104,72 +107,73 @@ function _(e) {
                                 t
                             )
                         ),
-                        actions: (0, i.jsx)(v, {
-                            senderId: I,
+                        actions: (0, i.jsx)(I, {
+                            senderId: b,
                             channelId: t,
-                            warningId: _,
+                            warningId: E,
                             onClick: () => {
-                                Z(h.NM.USER_MODAL_MUTE), r();
+                                T(h.NM.USER_MODAL_MUTE), r();
                             }
                         }),
                         learnMore: (0, i.jsx)(a.Clickable, {
                             onClick: () =>
                                 (0, h.qc)({
                                     channelId: t,
-                                    warningId: _,
-                                    senderId: I,
+                                    warningId: E,
+                                    senderId: b,
                                     warningType: u.pj.LIKELY_ATO,
                                     cta: h.NM.USER_MODAL_LEARN_MORE
                                 }),
                             children: (0, i.jsx)(a.Heading, {
                                 variant: 'heading-sm/medium',
                                 color: 'text-link',
-                                children: C.intl.format(C.t.UkH129, { learnMoreLink: f.D8 })
+                                children: v.intl.format(v.t.UkH129, { learnMoreLink: g.D8 })
                             })
                         })
                     });
                 };
             });
         };
-    return (
-        l.useEffect(() => {
-            (0, h.MC)(g.rMx.SAFETY_WARNING_VIEWED, {
+    return (l.useEffect(() => {
+        S &&
+            ((0, h.MC)(C.rMx.SAFETY_WARNING_VIEWED, {
                 channelId: t,
-                warningId: _,
-                senderId: I,
+                warningId: E,
+                senderId: b,
                 warningType: u.pj.LIKELY_ATO
             }),
-                o.Z.increment({ name: r.V.SAFETY_WARNING_VIEW });
-        }, [t, _, I]),
-        (0, i.jsx)(p.Q, {
-            channelId: t,
-            warningId: _,
-            senderId: I,
-            warningType: u.pj.LIKELY_ATO,
-            header: C.intl.string(C.t.R8UsiI),
-            description: C.intl.string(C.t.lI8nQk),
-            onDismiss: b,
-            buttons: [
-                {
-                    text: C.intl.string(C.t.tC1pvL),
-                    color: a.Button.Colors.BRAND,
-                    onclick: () => {
-                        N(),
-                            (0, h.qc)({
-                                channelId: t,
-                                warningId: _,
-                                senderId: I,
-                                warningType: u.pj.LIKELY_ATO,
-                                cta: h.NM.OPEN_MORE_TIPS
-                            });
-                    }
-                },
-                {
-                    text: C.intl.string(C.t.ftIK2N),
-                    color: a.Button.Colors.PRIMARY,
-                    onclick: () => Z(h.NM.USER_BANNER_MUTE)
-                }
-            ]
-        })
-    );
+            o.Z.increment({ name: r.V.SAFETY_WARNING_VIEW }));
+    }, [t, E, b, S]),
+    S)
+        ? (0, i.jsx)(p.Q, {
+              channelId: t,
+              warningId: E,
+              senderId: b,
+              warningType: u.pj.LIKELY_ATO,
+              header: v.intl.string(v.t.R8UsiI),
+              description: v.intl.string(v.t.lI8nQk),
+              onDismiss: N,
+              buttons: [
+                  {
+                      text: v.intl.string(v.t.tC1pvL),
+                      color: a.Button.Colors.BRAND,
+                      onclick: () => {
+                          j(),
+                              (0, h.qc)({
+                                  channelId: t,
+                                  warningId: E,
+                                  senderId: b,
+                                  warningType: u.pj.LIKELY_ATO,
+                                  cta: h.NM.OPEN_MORE_TIPS
+                              });
+                      }
+                  },
+                  {
+                      text: v.intl.string(v.t.ftIK2N),
+                      color: a.Button.Colors.PRIMARY,
+                      onclick: () => T(h.NM.USER_BANNER_MUTE)
+                  }
+              ]
+          })
+        : null;
 }
--- chunks/605984.js
+++ chunks/605984.js
@@ -1,6 +1,6 @@
 n.d(t, {
     k: function () {
-        return d;
+        return c;
     }
 });
 var i = n(385956),
@@ -8,14 +8,12 @@ var i = n(385956),
     r = n(359119),
     a = n(897769),
     s = n(403485),
-    o = n(88101),
-    c = n(276060);
-function d(e, t) {
-    let n = (0, l.G)(e),
-        d = (0, i.v)(e),
-        u = (0, c.t)({ location: t }),
-        h = (0, a.S)(e, r.pj.LIKELY_ATO),
-        p = (0, s.h)(e).length > 0,
-        m = null != (0, o.P)(e);
-    if (!!u && !n && !d && !p && !m) return h;
+    o = n(88101);
+function c(e) {
+    let t = (0, l.G)(e),
+        n = (0, i.v)(e),
+        c = (0, a.S)(e, r.pj.LIKELY_ATO),
+        d = (0, s.h)(e).length > 0,
+        u = null != (0, o.P)(e);
+    if (!t && !n && !d && !u) return c;
 }
