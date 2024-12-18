d1116325242f9df583fa4a75a138c4d63fa782f3
DiscrapperManager <admin@minerpl.xyz>
2024-12-18 20:28:37 +0000

    canaryRelease: 354930
    Main scripts: 3
    Lazy Loaded Scripts: 921
    Chunks:
     * Main Files: 11938
     * Scripts: 10322
    Strings: 0
    Routes: 687
    Experiments Count: 374

---
+++ chunks/596691.js
@@ -0,0 +1,21 @@
+r.d(n, {
+    K: function () {
+        return s;
+    }
+});
+var i = r(818083),
+    a = r(987338);
+let s = (0, i.B)({
+    kind: 'user',
+    id: '2024-08_mobile_perguild_collectibles_edit',
+    label: 'Mobile Per-guild Collectibles Edit',
+    defaultConfig: { enabled: !1 },
+    treatments: [
+        {
+            id: 1,
+            label: 'Enable Mobile Per-guild Collectibles Edit',
+            config: { enabled: !0 }
+        }
+    ],
+    commonTriggerPoint: a.$P.OPEN_USER_SETTINGS
+});
