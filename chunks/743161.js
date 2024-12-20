ddbb9723705779b9eaee24e52e50ffc2811eb335
DiscrapperManager <admin@minerpl.xyz>
2024-12-20 22:44:25 +0000

    canaryRelease: 355669
    Main scripts: 3
    Lazy Loaded Scripts: 923
    Chunks:
     * Main Files: 11938
     * Scripts: 10334
    Strings: 0
    Routes: 687
    Experiments Count: 374

--- chunks/743161.js
+++ chunks/743161.js
@@ -3,28 +3,29 @@ var o = e(200651),
     s = e(192379),
     l = e(442837),
     u = e(481060),
-    c = e(541099),
-    r = e(827498),
-    a = e(421591),
-    i = e(314734),
+    a = e(541099),
+    c = e(827498),
+    i = e(421591),
+    r = e(314734),
     d = e(388032),
     h = e(380165);
 t.default = s.memo(function (n) {
     let { channel: t, ...e } = n,
-        f = (0, l.e7)([c.Z], () => c.Z.shouldShowModal());
+        f = (0, l.e7)([a.Z], () => a.Z.shouldShowModal());
     return (
         s.useEffect(() => {
-            !f && (0, u.closeModalInAllContexts)(i.e9);
+            !f && (0, u.closeModalInAllContexts)(r.e9);
         }),
         (0, o.jsx)('span', {
-            style: i.u$,
+            style: r.u$,
             children: (0, o.jsx)(u.ModalRoot, {
                 className: h.root,
                 'aria-label': d.intl.string(d.t.shUONj),
+                size: u.ModalSize.DYNAMIC,
                 ...e,
-                children: (0, o.jsx)(a.Z, {
+                children: (0, o.jsx)(i.Z, {
                     channel: t,
-                    entrypoint: r._b.VOICE
+                    entrypoint: c._b.VOICE
                 })
             })
         })
