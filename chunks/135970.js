0e4a92f8b81e21c347658bcf581454ec1992a0c2
DiscrapperManager <admin@minerpl.xyz>
2024-12-20 00:13:53 +0000

    canaryRelease: 355387
    Main scripts: 3
    Lazy Loaded Scripts: 922
    Chunks:
     * Main Files: 11937
     * Scripts: 10332
    Strings: 0
    Routes: 687
    Experiments Count: 373

--- chunks/135970.js
+++ chunks/135970.js
@@ -1,5 +1,6 @@
 t.exports = {
     body: 'body_bf926b',
+    modalContentOverride: 'modalContentOverride_bf926b',
     sliderBody: 'sliderBody_bf926b',
     sliderBodyLarge: 'sliderBodyLarge_bf926b',
     sliderBodyDynamic: 'sliderBodyDynamic_bf926b'
